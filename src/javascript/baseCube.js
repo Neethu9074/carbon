1b8df9175c4f09c5dbbc646293460a2b415d3852
Simon Thiele <simon.thiele@codecentric.de>
2015-02-20 15:17:40 +0100

    states and state objects

--- src/javascript/baseCube.js
+++ src/javascript/baseCube.js
@@ -9,40 +9,40 @@ var textTexture = require('./extensions/textTextureFacade');
 require('./extensions/OBJLoader');
 
 exports.BaseCube = function BaseCube(app, x, y, width, height, depth) {
-	this.init();
+  this.init();
 
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = depth; //3D -> 2D for pathfinding (z becomes y)
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = depth; //3D -> 2D for pathfinding (z becomes y)
 
-	this.dimension = {
-		x: (width / 2.0) + x,
-		y: (height / 2.0),
-		z: (-depth / 2.0) - y,
-		width: width,
-		height: height,
-		depth: depth
-	};
+  this.dimension = {
+    x: (width / 2.0) + x,
+    y: (height / 2.0),
+    z: (-depth / 2.0) - y,
+    width: width,
+    height: height,
+    depth: depth
+  };
 
-	//create cubes
-	var group = new THREE.Object3D();
-	//create main cube mesh
-	this.createCube(this.dimension, this.name, group);
+  //create cubes
+  var group = new THREE.Object3D();
+  //create main cube mesh
+  this.createCube(app, this.dimension, this.name, group);
 
-	//create helper objects
-	var collisionCube = createCollisionCube(this.dimension, this.name);
-	this.setStatic(collisionCube);
+  //create helper objects
+  var collisionCube = createCollisionCube(this.dimension, this.name);
+  this.setStatic(collisionCube);
 
-	//create the label
-	var lodLabels = createLODLabels(this.dimension,
-		this.name,
-		this.opacityAnimatedMaterials);
+  //create the label
+  var lodLabels = createLODLabels(this.dimension,
+    this.name,
+    this.opacityAnimatedMaterials);
 
-	//group.add(lodLabels);
+  //group.add(lodLabels);
 
-	this.setMesh(group);
-	this.setCollisionMesh(collisionCube);
+  this.setMesh(group);
+  this.setCollisionMesh(collisionCube);
 };
 
 //inherence from SceneObject
@@ -50,175 +50,192 @@ exports.BaseCube.prototype = new sceneObj.SceneObject();
 exports.BaseCube.prototype.constructor = exports.BaseCube;
 
 exports.BaseCube.prototype.setStatic = function(mesh) {
-	//position will not change, so set to static which gives a perfomance boost
-	mesh.matrixAutoUpdate = false;
-	mesh.updateMatrix();
+  //position will not change, so set to static which gives a perfomance boost
+  mesh.matrixAutoUpdate = false;
+  mesh.updateMatrix();
 };
 
 exports.BaseCube.prototype.collectMaterials = function() {
-	var obj = this.getMesh();
-	var find = function(mats, object) {
-		for (var i = 0; i < object.children.length; i++) {
-			var item = object.children[i];
-			if (item.material !== undefined) {
-				mats.push(item.material);
-			}
-			find(mats, item);
-		}
-	};
+  var obj = this.getMesh();
+  var find = function(mats, object) {
+    for (var i = 0; i < object.children.length; i++) {
+      var item = object.children[i];
+      if (item.material !== undefined) {
+        mats.push(item.material);
+      }
+      find(mats, item);
+    }
+  };
 
-	var materials = [];
-	find(materials, obj);
-	return materials;
+  var materials = [];
+  find(materials, obj);
+  return materials;
 };
 
-exports.BaseCube.prototype.createCube = function(dimension, name, group) {
-	var width = dimension.width,
-		height = dimension.height,
-		depth = dimension.depth;
+exports.BaseCube.prototype.createCube = function(app, dimension, name, group) {
+  var width = dimension.width,
+    height = dimension.height,
+    depth = dimension.depth;
 
-	//need a new material, each for each cube...
-	var material = new THREE.MeshLambertMaterial({
-		color: colors.midBlue,
-		side: THREE.DoubleSide,
-		transparent: true,
-		opacity: 1,
-		blending: THREE.NormalBlending,
-		map: textures.cubeTexture
-	});
+  //need a new material, each for each cube...
+  var material = new THREE.MeshLambertMaterial({
+    color: colors.midBlue,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 1,
+    blending: THREE.NormalBlending,
+    map: textures.cubeTexture
+  });
 
-	var cube = new THREE.Mesh(
-		new THREE.BoxGeometry(width, height, depth),
-		material);
+  var cube = new THREE.Mesh(
+    new THREE.BoxGeometry(width, height, depth),
+    material);
 
-	var setStatic = this.setStatic;
-	var createBB = this.createBoundingCube;
+  var setStatic = this.setStatic;
+  var loader = new THREE.OBJLoader();
+  // load a resource
+  loader.load(
+    // resource URL
+    'obj/cube.obj',
+    // Function when resource is loaded
+    function(object) {
+      object = object.children[0];
+      //set position and then static
+      object.position.set(dimension.x, 0, dimension.z);
+      object.scale.set(width, height, depth);
 
-	// instantiate a loader
-	var loader = new THREE.OBJLoader();
-	// load a resource
-	loader.load(
-		// resource URL
-		'obj/cube.obj',
-		// Function when resource is loaded
-		function(object) {
-			object = object.children[0];
-			//set position and then static
-			object.position.set(dimension.x, 0, dimension.z);
-			object.scale.set(width, height, depth);
+      object.material = material;
 
-			var shader = THREE.EdgeShader;
-			object.material = material;
+      //set name to identify later
+      object.name = name;
 
-			//set name to identify later
-			object.name = name;
+      setStatic(object);
+      group.add(object);
 
-			setStatic(object);
-			group.add(object);
-		}
-	);
+      fadeIn(app, object.material);
+    }
+  );
 };
+
+function fadeIn(app, material) {
+  //setup fade in animation
+  var from = {
+    v: 0
+  };
+  var to = {
+    v: 1
+  };
+
+  //1sec animation duration
+  var tween = new app.tweenEngine.Tween(from).to(to, 1000);
+  tween.onUpdate(function() {
+    material.opacity = from.v;
+  });
+
+  tween.start();
+  tween.easing(app.tweenEngine.Easing.Cubic.InOut);
+}
 
 //this cube is used for collision / ray detection. In the app file,
 //the collision objects are stored in a seperate collection to minimize
 //collision cecking. the collision cube is a little bit bigger
 //than the original cube.
 function createCollisionCube(dimension, name) {
-	var offset = 0.01;
-	var cube = new THREE.Mesh(
-		new THREE.BoxGeometry(
-			dimension.width + offset,
-			dimension.height + offset,
-			dimension.depth + offset),
-		new THREE.MeshBasicMaterial({
-			color: colors.lightBlue
-		}));
+  var offset = 0.01;
+  var cube = new THREE.Mesh(
+    new THREE.BoxGeometry(
+      dimension.width + offset,
+      dimension.height + offset,
+      dimension.depth + offset),
+    new THREE.MeshBasicMaterial({
+      color: colors.lightBlue
+    }));
 
-	cube.position.set(dimension.x, dimension.y, dimension.z);
+  cube.position.set(dimension.x, dimension.y, dimension.z);
 
-	//set name to identify later
-	cube.name = name;
-	return cube;
+  //set name to identify later
+  cube.name = name;
+  return cube;
 }
 
 function createLabel(dimension, name) {
-	var width = dimension.width,
-		height = dimension.height,
-		depth = dimension.depth;
+  var width = dimension.width,
+    height = dimension.height,
+    depth = dimension.depth;
 
-	width -= 0.5; //transform to the right
-	var labelHeight = 1;
-	var aspect = width;
-	var topOfCube = height - 1.4;
-	var frontEdgePosition = (depth / 2) - (labelHeight / 2) - 0.5;
+  width -= 0.5; //transform to the right
+  var labelHeight = 1;
+  var aspect = width;
+  var topOfCube = height - 1.4;
+  var frontEdgePosition = (depth / 2) - (labelHeight / 2) - 0.5;
 
-	//get a texture from the facade
-	var tex = textTexture.createTexture(name, aspect);
-	//set to linear because the texture is not power of 2 (64x64, 32x32, ...)
-	tex.minFilter = THREE.LinearFilter;
+  //get a texture from the facade
+  var tex = textTexture.createTexture(name, aspect);
+  //set to linear because the texture is not power of 2 (64x64, 32x32, ...)
+  tex.minFilter = THREE.LinearFilter;
 
-	var labelMat = new THREE.MeshBasicMaterial({
-		map: tex,
-		transparent: true,
-		depthWrite: false
-	});
+  var labelMat = new THREE.MeshBasicMaterial({
+    map: tex,
+    transparent: true,
+    depthWrite: false
+  });
 
-	var geometry = new THREE.PlaneBufferGeometry(width, labelHeight, 1, 1);
-	var plane = new THREE.Mesh(geometry, labelMat);
-	plane.position.y = topOfCube;
-	plane.position.z = frontEdgePosition;
-	plane.rotateOnAxis(new THREE.Vector3(1, 0, 0), -90 * math.DegToRad);
+  var geometry = new THREE.PlaneBufferGeometry(width, labelHeight, 1, 1);
+  var plane = new THREE.Mesh(geometry, labelMat);
+  plane.position.y = topOfCube;
+  plane.position.z = frontEdgePosition;
+  plane.rotateOnAxis(new THREE.Vector3(1, 0, 0), -90 * math.DegToRad);
 
-	plane.matrixAutoUpdate = false;
-	plane.updateMatrix();
+  plane.matrixAutoUpdate = false;
+  plane.updateMatrix();
 
-	//set name to identify later
-	plane.name = name;
-	return plane;
+  //set name to identify later
+  plane.name = name;
+  return plane;
 }
 
 exports.BaseCube.prototype.createBoundingCube = function(
-	cube, dimension, name) {
-	var egh = new THREE.EdgesHelper(cube, colors.lightBlue);
+  cube, dimension, name) {
+  var egh = new THREE.EdgesHelper(cube, colors.lightBlue);
 
-	egh.position.set(dimension.x, dimension.y, dimension.z);
-	this.setStatic(egh);
+  egh.position.set(dimension.x, dimension.y, dimension.z);
+  this.setStatic(egh);
 
-	//set name to identify later
-	egh.name = name;
-	return egh;
+  //set name to identify later
+  egh.name = name;
+  return egh;
 }
 
 function createLODLabels(dimension, name, matCollection) {
-	var label = createLabel(dimension, name);
+  var label = createLabel(dimension, name);
 
-	//LOD for label
-	var lod = new THREE.LOD();
-	lod.addLevel(label, 20);
-	lod.addLevel(new THREE.Object3D(), 100);
+  //LOD for label
+  var lod = new THREE.LOD();
+  lod.addLevel(label, 20);
+  lod.addLevel(new THREE.Object3D(), 100);
 
-	lod.position.add(new THREE.Vector3(dimension.x, dimension.y, dimension.z));
+  lod.position.add(new THREE.Vector3(dimension.x, dimension.y, dimension.z));
 
-	return lod;
+  return lod;
 }
 
 // tests, if a point is seen by the camera
 exports.BaseCube.prototype.isVisible = function(point, camera) {
-	var maxValue = -0.7;
-	//get the view vector of the camera
-	var V = new THREE.Vector3(0, 0, -1);
-	V.applyQuaternion(camera.quaternion);
-	V.normalize();
+  var maxValue = -0.7;
+  //get the view vector of the camera
+  var V = new THREE.Vector3(0, 0, -1);
+  V.applyQuaternion(camera.quaternion);
+  V.normalize();
 
-	//get the pointing vector from object to camera
-	var O = new THREE.Vector3()
-		.copy(camera.position)
-		.sub(point)
-		.normalize();
+  //get the pointing vector from object to camera
+  var O = new THREE.Vector3()
+    .copy(camera.position)
+    .sub(point)
+    .normalize();
 
-	var alpha = O.dot(V);
-	if (alpha < maxValue) {
-		return true;
-	}
-	return false;
+  var alpha = O.dot(V);
+  if (alpha < maxValue) {
+    return true;
+  }
+  return false;
 };
--- src/javascript/serverCube.js
+++ src/javascript/serverCube.js
@@ -5,195 +5,282 @@ var math = require('./math');
 var baseCube = require('./baseCube');
 var gc = require('./groundSpaceControl2D');
 var software = require('./softwareCube');
+require('./extensions/OBJLoader');
+
 
 var DIRECTION = {
-	IN: {
-		id: 1
-	},
-	OUT: {
-		id: 2
-	}
+  IN: {
+    id: 1
+  },
+  OUT: {
+    id: 2
+  }
 };
 
+var STATE = {
+  OK: {
+    id: 1
+  },
+  WARNING: {
+    id: 2
+  },
+  ERROR: {
+    id: 3
+  }
+}
+
 var htmlContent = [
-	['<img src="images/Ok.png">'].join('\n'), ['<img src="images/Warning.png">'].join(
-		'\n'), ['<img src="images/Error.png">'].join('\n')
+  ['<img src="images/Ok.png">'].join('\n'), ['<img src="images/Warning.png">']
+  .join('\n'), ['<img src="images/Error.png">'].join('\n')
 ];
 
 exports.ServerCube = function ServerCube(app, x, y, w, h) {
-	if (app === undefined || x === undefined ||
-		y === undefined || w === undefined) {
-		return undefined;
-	}
-	var scaleFactor = 16;
+  if (app === undefined || x === undefined ||
+    y === undefined || w === undefined) {
+    return undefined;
+  }
+  var scaleFactor = 16;
 
-	x *= scaleFactor;
-	y *= scaleFactor;
-	x += 2;
-	y += 2;
-	var width = scaleFactor * w - 6;
-	var height = 3;
-	var depth = scaleFactor * h - 6;
-	baseCube.BaseCube.call(this, app, x, y, width, height, depth);
+  x *= scaleFactor;
+  y *= scaleFactor;
+  x += 2;
+  y += 2;
+  var width = scaleFactor * w - 6;
+  var height = 3;
+  var depth = scaleFactor * h - 6;
+  baseCube.BaseCube.call(this, app, x, y, width, height, depth);
 
-	//a collection which stores all cubes inside this server cube
-	this.children = [];
+  //set state
+  var r = Math.ceil(Math.random() * 3);
+  if (r === 1) {
+    this.state = STATE.OK;
+  } else if (r === 2) {
+    this.state = STATE.WARNING;
+  } else {
+    this.state = STATE.ERROR;
+  }
 
-	//create a ground control for inner software cubes
-	this.groundControl = new gc.GroundSpaceControl2D(10, 1); //10x10, gap: 0
+  //a collection which stores all cubes inside this server cube
+  this.children = [];
 
-	//register for update to calculate distance and fading
-	this.registerForUpdate();
+  this.stateSymbol = new THREE.Mesh();
+  this.createStateSymbol(this.state);
+  console.log(this.stateSymbol);
 
-	//tween parameters
-	this.minDistanceForTransparency = 16.5;
-	this.tweenDirection = DIRECTION.OUT;
+  //create a ground control for inner software cubes
+  this.groundControl = new gc.GroundSpaceControl2D(10, 1); //10x10, gap: 0
 
-	//stores all materials, that are animated due to animation process
-	this.opacityAnimatedMaterials = this.collectMaterials();
+  //register for update to calculate distance and fading
+  this.registerForUpdate();
 
-	this.details = getDetailsGroup(this.dimension);
-	this.setStatic(this.details);
+  //tween parameters
+  this.minDistanceForTransparency = 16.5;
+  this.tweenDirection = DIRECTION.OUT;
 
-	//save the css stuff
-	this.cssObject = createCSS3DTestStuff(app, this.dimension);
-	this.setTransparency(app, {
-		v: 0
-	}, {
-		v: 1
-	}, 0);
-	this.hideDetails(app, this);
+  //stores all materials, that are animated due to animation process
+  this.opacityAnimatedMaterials = this.collectMaterials();
+
+  this.details = getDetailsGroup(this.dimension);
+  this.setStatic(this.details);
+
+  //save the css stuff
+  this.cssObject = createCSS3DTestStuff(this.state, app, this.dimension);
+  this.setTransparency(app, {
+    v: 0
+  }, {
+    v: 1
+  }, 0);
+  this.hideDetails(app, this);
 };
 
 //inherence from SceneObject
 exports.ServerCube.prototype = new baseCube.BaseCube();
 exports.ServerCube.prototype.constructor = exports.ServerCube;
 
+exports.ServerCube.prototype.createStateSymbol = function(state) {
+  var dimension = this.dimension;
+  var group = this.getMesh();
+  var symbol = this.stateSymbol;
+
+  var options = {
+    path: 'obj/error.obj',
+    color: 0xFF0000
+  };
+  if (state === STATE.OK) {
+    options = {
+      path: 'obj/ok.obj',
+      color: 0x00FF00
+    };
+  } else if (state === STATE.WARNING) {
+    options = {
+      path: 'obj/warning.obj',
+      color: 0xFFFF00
+    };
+  }
+
+  var material = new THREE.MeshBasicMaterial({
+    color: options.color
+  });
+
+  var loader = new THREE.OBJLoader();
+  // load a resource
+  loader.load(
+    // resource URL
+    options.path,
+    // Function when resource is loaded
+    function(object) {
+      object = object.children[0];
+      object.position.set(dimension.x, 5, dimension.z - (dimension.depth /
+        3));
+      object.material = material;
+      object.rotation.x = 90 * math.DegToRad;
+      object.scale.set(1, 1, 1);
+
+      //save for later use
+      symbol.children.push(object);
+
+      //set name to identify later
+      object.name = name;
+      group.add(object);
+    }
+  );
+};
+
 exports.ServerCube.prototype.addSoftware = function(app, options) {
-	//calculte next free field
-	var xy = this.groundControl.getNearestFreeField(2);
-	xy.x += this.dimension.x - (this.dimension.width / 2);
-	xy.y = -this.dimension.y + (3 / 2) - xy.y;
+  //calculte next free field
+  var xy = this.groundControl.getNearestFreeField(2);
+  xy.x += this.dimension.x - (this.dimension.width / 2);
+  xy.y = -this.dimension.y + (3 / 2) - xy.y;
 
-	//create the cube
-	var swCube = new software.SoftwareCube(app, xy);
+  //create the cube
+  var swCube = new software.SoftwareCube(app, xy);
 
-	this.children.push(swCube);
-	console.log('software added to server', swCube);
+  this.children.push(swCube);
+  console.log('software added to server', swCube);
 };
 
 function getDetailsGroup(dimension) {
-	//need a new material, each for each cube...
-	var material = new THREE.MeshLambertMaterial({
-		color: 0xF000F0
-	});
-	var cube = new THREE.Mesh(new THREE.BoxGeometry(3, 1, 3), material);
+  //need a new material, each for each cube...
+  var material = new THREE.MeshLambertMaterial({
+    color: 0xF000F0
+  });
+  var cube = new THREE.Mesh(new THREE.BoxGeometry(3, 1, 3), material);
 
-	cube.position.set(dimension.x, 1, dimension.z);
-	cube.name = 'detail cube';
-	return cube;
+  cube.position.set(dimension.x, 1, dimension.z);
+  cube.name = 'detail cube';
+  return cube;
 }
 
-function createCSS3DTestStuff(app, dimension) {
-	var pos = new THREE.Vector3(dimension.x, dimension.y * 2, dimension.z);
-	var scaleX = dimension.width / 10.6;
-	var scaleY = dimension.depth / 10.6;
+function createCSS3DTestStuff(state, app, dimension) {
+  var content = htmlContent[2];
+  if (state === STATE.OK) {
+    content = htmlContent[0];
+  } else if (state === STATE.WARNING) {
+    content = htmlContent[1];
+  }
 
-	var number = document.createElement('div');
-	number.className = 'number';
-	number.innerHTML = htmlContent[Math.floor(Math.random() * 3)];
-	var object = new THREE.CSS3DObject(number);
-	object.scale.set(scaleX / 40, scaleY / 40, 1);
-	object.position.copy(pos);
-	object.rotation.x = -90 * math.DegToRad;
+  var pos = new THREE.Vector3(dimension.x, dimension.y * 2, dimension.z);
+  var scaleX = dimension.width / 10.6;
+  var scaleY = dimension.depth / 10.6;
 
-	return object;
+  var number = document.createElement('div');
+  number.className = 'number';
+  number.innerHTML = content;
+  var object = new THREE.CSS3DObject(number);
+  object.scale.set(scaleX / 40, scaleY / 40, 1);
+  object.position.copy(pos);
+  object.rotation.x = -90 * math.DegToRad;
+
+  return object;
 }
 
 exports.ServerCube.prototype.update = function(app) {
-	var objectPos = this.collisionMesh.position;
-	var cam = app.mainCamera;
+  //rotate statesymbol
+  this.stateSymbol.children[0].rotation.z += app.deltaTime * 1;
 
-	var distance = new THREE.Vector3()
-		.copy(cam.position)
-		.sub(objectPos)
-		.length();
+  var objectPos = this.collisionMesh.position;
+  var cam = app.mainCamera;
 
-	if (distance < this.minDistanceForTransparency) {
-		var tweenDirection = DIRECTION.IN;
-	} else {
-		tweenDirection = DIRECTION.OUT;
-	}
+  var distance = new THREE.Vector3()
+    .copy(cam.position)
+    .sub(objectPos)
+    .length();
 
-	//if direction changed and object is visible
-	if (this.tweenDirection !== tweenDirection) {
-		this.tweenDirection = tweenDirection;
+  if (distance < this.minDistanceForTransparency) {
+    var tweenDirection = DIRECTION.IN;
+  } else {
+    tweenDirection = DIRECTION.OUT;
+  }
 
-		if (tweenDirection === DIRECTION.IN) {
-			//fade in and then show details
-			app.scene2D.remove(this.cssObject);
-			//remove collision object from octree to get access to the details
-			app.octree.remove(this.getCollisionMesh());
-			app.octree.update();
-			this.setTransparency(app, {
-				v: 1
-			}, {
-				v: 0.2
-			}, 250, this.showDetails);
-		} else {
-			//fade out and then hide details
-			this.setTransparency(app, {
-				v: 0.2
-			}, {
-				v: 1
-			}, 250, this.hideDetails);
-		}
-	}
+  //if direction changed and object is visible
+  if (this.tweenDirection !== tweenDirection) {
+    this.tweenDirection = tweenDirection;
+
+    if (tweenDirection === DIRECTION.IN) {
+      //fade in and then show details
+      app.scene2D.remove(this.cssObject);
+      //remove collision object from octree to get access to the details
+      app.octree.remove(this.getCollisionMesh());
+      app.octree.update();
+      this.setTransparency(app, {
+        v: 1
+      }, {
+        v: 0.2
+      }, 250, this.showDetails);
+    } else {
+      //fade out and then hide details
+      this.setTransparency(app, {
+        v: 0.2
+      }, {
+        v: 1
+      }, 250, this.hideDetails);
+    }
+  }
 };
 
 exports.ServerCube.prototype.setTransparency = function(
-	app, from, to, delay, f) {
-	//stores all materials, that are animated due to animation process
-	this.opacityAnimatedMaterials = this.collectMaterials();
+  app, from, to, delay, f) {
+  //stores all materials, that are animated due to animation process
+  this.opacityAnimatedMaterials = this.collectMaterials();
 
-	//save the material!
-	var mats = this.opacityAnimatedMaterials;
-	var dly = delay === undefined ? 500 : delay; //default 500ms delay
-	var func = f;
-	var cube = this;
+  //save the material!
+  var mats = this.opacityAnimatedMaterials;
+  var dly = delay === undefined ? 500 : delay; //default 500ms delay
+  var func = f;
+  var cube = this;
 
-	//1sec animation duration
-	var tween = new app.tweenEngine.Tween(from).to(to, 1000);
-	tween.onUpdate(function() {
-		for (var i = 0; i < mats.length; i++) {
-			mats[i].opacity = from.v;
-		}
-	});
+  //1sec animation duration
+  var tween = new app.tweenEngine.Tween(from).to(to, 1000);
+  tween.onUpdate(function() {
+    for (var i = 0; i < mats.length; i++) {
+      mats[i].opacity = from.v;
+    }
+  });
 
-	tween.delay(dly);
-	tween.start();
-	tween.easing(app.tweenEngine.Easing.Cubic.InOut);
-	tween.onComplete(function() {
-		if (func !== undefined) {
-			func(app, cube);
-		}
-	});
+  tween.delay(dly);
+  tween.start();
+  tween.easing(app.tweenEngine.Easing.Cubic.InOut);
+  tween.onComplete(function() {
+    if (func !== undefined) {
+      func(app, cube);
+    }
+  });
 };
 
 exports.ServerCube.prototype.hideDetails = function(app, cube) {
-	for (var i = 0; i < cube.children.length; i++) {
-		app.removeObject(cube.children[i]);
-	}
+  for (var i = 0; i < cube.children.length; i++) {
+    app.removeObject(cube.children[i]);
+  }
 
-	app.scene2D.add(cube.cssObject);
+  app.scene2D.add(cube.cssObject);
 
-	//add collision object to octree to enable raypicking for this cube
-	app.octree.add(cube.getCollisionMesh());
-	app.octree.update();
+  //add collision object to octree to enable raypicking for this cube
+  app.octree.add(cube.getCollisionMesh());
+  app.octree.update();
 };
 
 exports.ServerCube.prototype.showDetails = function(app, cube) {
-	for (var i = 0; i < cube.children.length; i++) {
-		app.addObject(cube.children[i]);
-	}
+  for (var i = 0; i < cube.children.length; i++) {
+    app.addObject(cube.children[i]);
+  }
 };
