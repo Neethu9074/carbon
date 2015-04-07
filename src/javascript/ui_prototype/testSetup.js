30efbb6c4ea6e5d648422e5b69b97d6e712e061c
Simon Thiele <simon.thiele@codecentric.de>
2015-04-07 07:27:43 +0200

    pull

--- src/javascript/ui_prototype/testSetup.js
+++ src/javascript/ui_prototype/testSetup.js
@@ -11,10 +11,11 @@ import {
 }
 from '../log';
 const logger = createLogger('testSetup.js');
-const tempConnections = [];
-
 
 export function setup(app) {
+/*
+  const tempConnections = [];
+
   let host = app.addRandomHost();
   let hostB = app.addRandomHost();
   let hostC = app.addRandomHost();
@@ -24,11 +25,11 @@ export function setup(app) {
   tempConnections.push(a);
   tempConnections.push(b);
 
-
   host.addContainer( { id: 'uuid1', pid: '1' } );
   host.addContainer( { id: 'uuid2', pid: '2' } );
   host.addContainer( { id: 'uuid3', pid: '3' } );
   host.addContainer( { id: 'uuid4', pid: '4' } );
+*/
 
   const max = 2;
   let step = 1;
