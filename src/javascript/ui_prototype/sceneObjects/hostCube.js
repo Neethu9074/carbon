3ea87ecc57e2d7d3fc5c68059bc465c84d06092a
Simon Thiele <simon.thiele@codecentric.de>
2015-04-07 10:40:31 +0200

    need loadash import

--- src/javascript/ui_prototype/sceneObjects/hostCube.js
+++ src/javascript/ui_prototype/sceneObjects/hostCube.js
@@ -14,6 +14,7 @@ import * as states from '../cubeStates';
 
 import Layouter from '../layouterContainer';
 import logging from 'instalog';
+import _ from 'lodash';
 import TWEEN from 'tween.js'
 
 //const logger = logging.createLogger('hostCube.js');
