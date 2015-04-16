829a974c442c6445fb3fc92530eeb497020c1042
Simon Thiele <simon.thiele@codecentric.de>
2015-04-16 19:56:13 +0200

    add map colors and move colors to color pbj

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+var colors = {
+  "cyan": "#2dfffe",
+  "razzmatazz": "#e50066",
+  "easternBlue": "#1d90a9",
+  "governorBay": "#3d2dcf",
+  "toryBlue": "#0b5cad",
+  "dodgerBlue": "#15aafe",
+  "black": 0x000000,
+  "mineShaft": 0x202020,
+  "mapcyan": 0x2dfffe,
+  "orient": 0x015473,
+  "redBerry": 0x880000,
+  "gold": 0xffd000,
+  "flushOrange": 0xff8000
+};
+
 module.exports = {
   "common": {
     "fontFamily": "sans-serif",
@@ -7,12 +23,22 @@ module.exports = {
   },
   "charts": {
     "colors": {
-      "cyan": "#2dfffe",
-      "razzmatazz": "#e50066",
-      "easternBlue": "#1d90a9",
-      "governorBay": "#3d2dcf",
-      "toryBlue": "#0b5cad",
-      "dodgerBlue": "#15aafe"
+      "cyan": colors.cyan,
+      "razzmatazz": colors.razzmatazz,
+      "easternBlue": colors.easternBlue,
+      "governorBay": colors.governorBay,
+      "toryBlue": colors.toryBlue,
+      "dodgerBlue": colors.dodgerBlue
     }
+  },
+  "map": {
+    "renderClearColor": colors.black,
+    "ambientColor": colors.mineShaft,
+    "groundColor": colors.mapCyan,
+    "lightBlue": colors.mapCyan,
+    "midBlue": colors.orient,
+    "errorRed": colors.redBerry,
+    "warningYellow": colors.gold,
+    "connectionColor": colors.flushOrange
   }
 };
