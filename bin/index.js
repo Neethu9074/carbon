7cdb60dadaef3eff983745773f7ba5760ed041cc
Ben Ripkens <bripkens.dev@gmail.com>
2015-04-17 09:03:52 +0200

    The JavaScript defines the style

--- bin/index.js
+++ bin/index.js
@@ -2,58 +2,37 @@
 
 var fs = require('fs');
 var path = require('path');
+var themes = require('../js');
 
+var stylesheet = '';
+Object.keys(themes.consts).forEach(function(theme) {
+  walkConstants([], themes.consts[theme]);
+});
+stylesheet = stylesheet.trim();
+fs.writeFileSync(
+  path.join(__dirname, '../less/constants.less'),
+  stylesheet
+);
 
-var variables = turnToNestedStructure(getVariables());
-writeToJsFile(variables);
-console.log('Successfully build the JS version of the variables.');
-
-function getVariables() {
-  var variableRegex = /@([0-9a-z-_]+):[ ]*([^;]*);/ig;
-  var content = fs.readFileSync(
-    path.join(__dirname, '../less/variables.less'),
-    { encoding: 'utf8' }
-  );
-
-  var match;
-  var result = {};
-  while ((match = variableRegex.exec(content)) !== null) {
-    result[match[1]] = match[2];
-  }
-  return result;
-}
-
-function turnToNestedStructure(variables) {
-  var nestedVariables = {};
-  Object.keys(variables).forEach(function(key) {
-    deepSet(nestedVariables, key, variables[key]);
-  });
-  return nestedVariables;
-}
-
-function deepSet(obj, key, value) {
-  var path = key.split('__').map(camelCasify);
-  for (var i = 0, len = path.length - 1; i < len; i++) {
-    var pathElement = path[i];
-    if (obj[pathElement] === undefined) {
-      obj[pathElement] = {};
+function walkConstants(path, obj) {
+  Object.keys(obj).forEach(function(key) {
+    var value = obj[key];
+    if (typeof value === 'string') {
+      addConstant(path.concat(key), value);
+    } else if (typeof value === 'object') {
+      walkConstants(path.concat(key), value);
+      stylesheet += '\n';
     }
-    obj = obj[pathElement];
-  }
-  obj[path[path.length - 1]] = value;
-}
-
-function camelCasify(s) {
-  return s.replace(/-([a-z0-9])/i, function(match, g1) {
-    return g1.toUpperCase();
   });
 }
 
-function writeToJsFile(variables) {
-  var content = '"use strict";\n\n' +
-    'module.exports = ' + JSON.stringify(variables, 0, 2) + ';';
-  fs.writeFileSync(
-    path.join(__dirname, '../js/index.js'),
-    content
-  );
+function addConstant(path, value) {
+  var key = toSpinalCase(path.join('__'));
+  stylesheet += '@' + key + ': ' + value + ';\n';
+}
+
+function toSpinalCase(name) {
+  return name.replace(/([a-z])([A-Z])/, function(match, g1, g2) {
+    return g1 + '-' + g2.toLowerCase();
+  })
 }
--- js/index.js
+++ js/index.js
@@ -1,44 +1,32 @@
-"use strict";
+'use strict';
 
-var colors = {
-  "cyan": "#2dfffe",
-  "razzmatazz": "#e50066",
-  "easternBlue": "#1d90a9",
-  "governorBay": "#3d2dcf",
-  "toryBlue": "#0b5cad",
-  "dodgerBlue": "#15aafe",
-  "black": 0x000000,
-  "mineShaft": 0x202020,
-  "mapcyan": 0x2dfffe,
-  "orient": 0x015473,
-  "redBerry": 0x880000,
-  "gold": 0xffd000,
-  "flushOrange": 0xff8000
-};
-
-module.exports = {
-  "common": {
-    "fontFamily": "sans-serif",
-    "fontColor": "#ddd"
-  },
-  "charts": {
-    "colors": {
-      "cyan": colors.cyan,
-      "razzmatazz": colors.razzmatazz,
-      "easternBlue": colors.easternBlue,
-      "governorBay": colors.governorBay,
-      "toryBlue": colors.toryBlue,
-      "dodgerBlue": colors.dodgerBlue
+// first level is the color theme's name
+exports.consts = {
+  night: {
+    common: {
+      fontFamily: 'sans-serif',
+      fontColor: '#ddd'
+    },
+    charts: {
+      colors: {
+        cyan: '#2dfffe',
+        razzmatazz: '#e50066',
+        easternBlue: '#1d90a9',
+        governorBay: '#3d2dcf',
+        toryBlue: '#0b5cad',
+        dodgerBlue: '#15aafe'
+      }
+    },
+    map: {
+      colors: {
+        black: '#000000',
+        mineShaft: '#202020',
+        cyan: '#2dfffe',
+        orient: '#015473',
+        redBerry: '#880000',
+        gold: '#ffd000',
+        flushOrange: '#ff8000'
+      }
     }
-  },
-  "map": {
-    "renderClearColor": colors.black,
-    "ambientColor": colors.mineShaft,
-    "groundColor": colors.mapCyan,
-    "lightBlue": colors.mapCyan,
-    "midBlue": colors.orient,
-    "errorRed": colors.redBerry,
-    "warningYellow": colors.gold,
-    "connectionColor": colors.flushOrange
   }
 };
