ca7ec6d6a77f4508d53e8966f5c38747694b215f
Ben Ripkens <bripkens.dev@gmail.com>
2015-05-08 10:02:24 +0200

    Screw it, throw out all comments

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,9 @@ gulp.task('webpack:build', ['clean'], function(callback) {
       }
     }),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: /\/DONOTKEEPANYCOMMENTS/
+    }),
     new webpack.BannerPlugin(getBanner())
   );
 
