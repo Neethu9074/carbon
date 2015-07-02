55f028c50c3df5e0d3ff55619da7c5f6b1044e14
Olaf Kilian <olaf.kilian@symanex.com>
2015-07-02 20:49:09 +0200

    Improving output of container build script

--- release.sh
+++ release.sh
@@ -5,13 +5,17 @@ imageName="registry.internal.instana.io/instana/ui-client"
 qualifiedImageName="$imageName:$version"
 echo $qualifiedImageName
 
-echo "Building Docker image..."
+echo "Building Docker image ..."
 sudo docker build -t "$qualifiedImageName" .
+
+echo "Tagging Docker image ..."
+echo "> current"
 sudo docker tag -f "$qualifiedImageName" "$imageName:current"
 
-if [[ "$INSTANA_RELEASE_CONTAINERS" == "true" ]]; then
+if [[ $INSTANA_RELEASE_CONTAINERS == "true" ]]; then
+  echo "> stable"
   sudo docker tag -f "$qualifiedImageName" "$imageName:stable"
 fi
 
-echo "Publishing Docker image..."
-sudo docker push $imageName
+echo "Publishing Docker image ..."
+sudo docker push "$imageName"
