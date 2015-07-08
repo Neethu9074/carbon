34eb1146e46877ff90b7f595ee53242dd7cf6f43
Simon Thiele <simon.thiele@codecentric.de>
2015-07-08 13:20:38 +0200

    change default color

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var colors = {
   darkGrey: '#20272C',
   grey: '#44535D',
   highlight: '#9FFFFF',
-  default: '#51626B', // '#829EB1',
+  default: '#667B86',
   warning: '#FFD823',
   danger: '#FF4229'
 };
