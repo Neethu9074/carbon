63c31c948dcace83253a9213f5ec889c99798f17
Olaf Kilian <olaf.kilian@symanex.com>
2015-08-19 17:08:17 +0200

    Update container build script for new versioning

--- release.sh
+++ release.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 VERSION=$1
-IMAGE=ui-client
+IMAGE="ui-client"
 
 if [ -z "$IMAGE" ]; then echo "no image provided"; exit 1; fi
 if [ -z "$VERSION" ]; then echo "no version provided"; exit 2; fi
 
 echo "Building Docker image ..."
-echo "> $VERSION"
-docker build -t registry.internal.instana.io/instana/$IMAGE:$VERSION .
-
-echo "Tagging Docker image ..."
-echo "> current"
-docker tag -f registry.internal.instana.io/instana/$IMAGE:$VERSION registry.internal.instana.io/instana/$IMAGE:current
-
-if [[ $INSTANA_RELEASE_CONTAINERS == "true" ]]; then
-  echo "> stable"
-  docker tag -f registry.internal.instana.io/instana/$IMAGE:$VERSION registry.internal.instana.io/instana/$IMAGE:stable
-fi
+docker build -t registry.internal.instana.io/instana/$IMAGE .
+docker tag -f registry.internal.instana.io/instana/$IMAGE registry.internal.instana.io/instana/$IMAGE:$VERSION
 
 echo "Publishing Docker image ..."
 docker push registry.internal.instana.io/instana/$IMAGE
