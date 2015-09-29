ba9cbf9289eae474db7d068551d03d03816f9ff5
simonThiele <simon.thiele@instana.com>
2015-09-29 16:00:56 +0200

    define for sidebar

--- in-themes/night.js
+++ in-themes/night.js
@@ -6,31 +6,37 @@ var base = require('instana-ui-theme/dist/night/config.json');
 
 var common = require('./common');
 
-var colors = {
-  white: '#ffffff',
-  lightestGrey: '#dce3e6',
-  lightGrey: '#6b8088',
-  grey: '#435b64',
-  darkGrey: '#2d4048',
-  darkestGrey: '#203036',
-  cyan: '#9fffff',
-  health: [
-    '#e3e2b8',
-    '#eae18a',
-    '#f1e05c',
-    '#f8df2e',
-    '#ffde00',
-    '#ffbf08',
-    '#ffa010',
-    '#ff8019',
-    '#ff6121',
-    '#ff4229'
-  ]
-};
+var white = '#ffffff';
+var lightestGrey = '#dce3e6';
+var lightGrey = '#6b8088';
+var grey = '#435b64';
+var darkGrey = '#2d4048';
+var darkestGrey = '#203036';
+var cyan = '#9fffff';
+var health = [
+  '#e3e2b8',
+  '#eae18a',
+  '#f1e05c',
+  '#f8df2e',
+  '#ffde00',
+  '#ffbf08',
+  '#ffa010',
+  '#ff8019',
+  '#ff6121',
+  '#ff4229'
+];
 
 module.exports = _.defaultsDeep({
   sidebar: {
-    background: 'rgb(45, 64, 72)'
+    header: {
+      background: darkGrey,
+      heading: white,
+      plugin: lightestGrey
+    },
+    viewDashboardButton: {
+      background: darkestGrey
+    },
+    background: white
   },
   footer: {
     height: 32
@@ -43,18 +49,18 @@ module.exports = _.defaultsDeep({
       },
       cubeBasicColor: '#e9edef',
       clearColor: '#445b63',
-      warning: colors.warning,
-      critical: colors.danger,
+      warning: '#FF0000',
+      critical: '#00FF00',
       groundDots: '#809199'
     },
     stickyNotes: {
-      nodeHightlightBackgroundColor: colors.darkGrey
+      nodeHightlightBackgroundColor: darkGrey
     },
     tooltips: {
-      font: colors.white,
-      background: colors.black,
-      critical: colors.warning,
-      danger: colors.danger
+      font: white,
+      background: '#000000',
+      critical: '#FF0000',
+      danger: '#00FF00'
     }
   }
 }, base, common);
