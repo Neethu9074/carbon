ce51bef18b4893e2adc174e2af59e441df08c047
simonThiele <simon.thiele@instana.com>
2015-10-06 15:52:59 +0200

    remove unused props from shader

--- in-map/src/SingleMeshFactory/SingleMeshPointsFactory/pointFragmentShader.glsl
+++ in-map/src/SingleMeshFactory/SingleMeshPointsFactory/pointFragmentShader.glsl
@@ -1,9 +1,6 @@
 precision mediump float;
 precision mediump int;
 
-varying vec3 vPosition;
-varying vec3 vColor;
-
 uniform sampler2D texture;
 
 void main() {
--- in-map/src/SingleMeshFactory/SingleMeshPointsFactory/pointVertexShader.glsl
+++ in-map/src/SingleMeshFactory/SingleMeshPointsFactory/pointVertexShader.glsl
@@ -5,7 +5,7 @@ uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
 
 attribute vec3 position;
-attribute vec3 color;
+
 
 void main() {
   vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
