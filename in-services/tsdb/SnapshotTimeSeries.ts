7eafa976343e923e5232eecabf245f87914cd68a
Ben Ripkens <bripkens.dev@gmail.com>
2015-11-09 10:08:48 +0100

    Remove the TSDB

--- in-services/tsdb/SnapshotTimeSeries.ts
+++
@@ -1,7 +0,0 @@
-import {List} from 'immutable';
-import {Timestamp, Snapshot} from './types';
-import TimeSeries from './TimeSeries';
-
-export default class SnapshotTimeSeries extends TimeSeries<List<Snapshot>> {
-
-}
