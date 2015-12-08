e0dc940a36cacaba11d9604622cc3c20fa80d653
Ben Ripkens <bripkens.dev@gmail.com>
2015-12-08 09:10:31 +0100

    Schöner sterben 2.0

--- deployment/in-server.sh
+++ deployment/in-server.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-exec 1> >(logger -s -t in-server) 2>&1
-
 DEBUG=instana-nodejs-sensor:* \
   /opt/www/node_modules/babel/bin/babel-node.js \
-  /opt/www/index.js
+  /opt/www/index.js 2>&1 | logger -t in-server
