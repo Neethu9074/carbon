13c1a23be2d91d809367a6d753331f492360c349
Ben Ripkens <bripkens.dev@gmail.com>
2016-01-11 13:54:24 +0100

    Show swap activity in host dashboard

--- build/gulp/environments.js
+++ build/gulp/environments.js
@@ -71,6 +71,13 @@ module.exports = {
     tenantUnit: 'rewedigital',
     groundskeeperDomain: 'instana.io'
   },
+  drivenow: {
+    uiBackendUrl: 'https://drivenow.instana.io/api',
+    groundskeeperUrl: 'https://drivenow.instana.io',
+    tenant: 'drivenow',
+    tenantUnit: 'drivenow',
+    groundskeeperDomain: 'instana.io'
+  },
   centerdevice: {
     uiBackendUrl: 'https://centerdevice.instana.io/api',
     groundskeeperUrl: 'https://centerdevice.instana.io',
