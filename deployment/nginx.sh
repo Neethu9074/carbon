73b1def699ab8272d6e507360ab09bb816d438ad
Ben Ripkens <bripkens.dev@gmail.com>
2016-01-22 06:59:42 +0100

    Improved syslog logging

--- deployment/nginx.sh
+++ deployment/nginx.sh
@@ -4,4 +4,4 @@ set -e
 
 j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf && \
   j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key && \
-  nginx -g "daemon off;" 2>&1 | logger -t nginx
+  nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
