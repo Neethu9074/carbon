8be2ee86a1f3e8ca41cf56b33f6425eb5c989889
Ben Ripkens <bripkens.dev@gmail.com>
2016-01-22 18:21:53 +0100

    Fool with a tool...

--- deployment/nginx.sh
+++ deployment/nginx.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf && \
-  j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key && \
-  nginx -g "daemon off;" 2>&1 | logger -t nginx
+j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf
+j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key
+exec nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
