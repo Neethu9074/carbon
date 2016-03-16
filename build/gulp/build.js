c7dd4ddd8bc6b903fc73cdadf7051596d727dfbe
Ben Ripkens <bripkens.dev@gmail.com>
2016-03-16 20:58:01 +0100

    Always go against the local ui-backend in try-build mode

--- build/gulp/build.js
+++ build/gulp/build.js
@@ -163,7 +163,7 @@ gulp.task('writeTryBuildConfigFile', () => {
 gulp.task('writeTryBuildServerConfigFile', () => {
   var config = {
     baseUrl: 'https://local-instana.instana.io:4000',
-    uiBackendBaseUrl: environments[tryBuildModeOptions.environment].uiBackendUrl
+    uiBackendBaseUrl: 'http://127.0.0.1:8080'
   };
   fs.writeFileSync(
     path.join(paths.targetDir, 'serverConfig.json'),
