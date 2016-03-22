0d43c4bf08e4ca51655d0110b5db4d9587514f67
Ben Ripkens <bripkens.dev@gmail.com>
2016-03-22 14:19:12 +0100

    Stop using babel node

--- deployment/services/in-server.sh
+++ deployment/services/in-server.sh
@@ -7,4 +7,4 @@ source discovery/discovery.general
 
 j2 /opt/www/config.json.j2 > /opt/www/assets/config.json
 j2 /opt/www/serverConfig.json.j2 > /opt/www/serverConfig.json
-exec /opt/www/node_modules/babel/bin/babel-node.js /opt/www/index.js 2>&1 | logger -t ui-client-in-server
+exec node /opt/www/index.js 2>&1 | logger -t ui-client-in-server
