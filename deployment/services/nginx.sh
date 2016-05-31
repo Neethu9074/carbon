e8ced721af59395e5ff6779f82d6d32a2117470d
pavlobaron <pb@pbit.org>
2016-05-31 14:17:33 +0200

    template-ize crt

--- deployment/services/nginx.sh
+++ deployment/services/nginx.sh
@@ -6,4 +6,5 @@ source /opt/discovery/discovery.general
 
 j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf
 j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key
+j2 /etc/ssl/certs/star_instana_io.crt.j2 > /etc/ssl/certs/star_instana_io.crt
 exec nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
