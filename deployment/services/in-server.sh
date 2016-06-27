679ff50d6f1383b30b2c2998965a31ef983f158b
Ben Ripkens <bripkens.dev@gmail.com>
2016-06-27 10:15:46 +0200

    in-server log output is too hard to read

--- deployment/services/in-server.sh
+++ deployment/services/in-server.sh
@@ -6,4 +6,4 @@ source /opt/discovery/discovery.general
 
 j2 /opt/www/config.json.j2 > /opt/www/assets/config.json
 j2 /opt/www/serverConfig.json.j2 > /opt/www/serverConfig.json
-exec node /opt/www/index.js 2>&1 | logger -t ui-client-in-server
+exec node /opt/www/index.js | /opt/www/node_modules/.bin/bunyan 2>&1 | logger -t ui-client-in-server
