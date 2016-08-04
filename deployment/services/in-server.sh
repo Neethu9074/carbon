a5598dbdf9994acaf2d1db25533333da24df4277
pavlobaron <pb@pbit.org>
2016-08-04 12:26:38 +0200

    non-root ui-client

--- deployment/services/in-server.sh
+++ deployment/services/in-server.sh
@@ -7,4 +7,15 @@ source /opt/discovery/discovery.general
 j2 /opt/www/config.json.j2 > /opt/www/assets/config.json
 j2 /opt/www/serverConfig.json.j2 > /opt/www/serverConfig.json
 j2 /opt/www/index.js.j2 > /opt/www/index.js
-exec node /opt/www/index.js | /opt/www/node_modules/.bin/bunyan 2>&1 | logger -t ui-client-in-server
+
+USER=root
+if [[ "$USER_AND_GROUP_NAME" != "" ]]; then
+  grep "${USER_AND_GROUP_NAME}:" /etc/passwd
+  if [[ "$?" != "0" ]]; then
+    groupadd -r $USER_AND_GROUP_NAME --gid=$GROUP_ID && useradd -r -g $USER_AND_GROUP_NAME --uid=$USER_ID $USER_AND_GROUP_NAME
+  fi
+
+  USER=$USER_AND_GROUP_NAME
+fi
+
+exec gosu $USER node /opt/www/index.js | /opt/www/node_modules/.bin/bunyan 2>&1 | logger -t ui-client-in-server
--- deployment/services/nginx.sh
+++ deployment/services/nginx.sh
@@ -8,4 +8,15 @@ j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf
 j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key
 j2 /etc/ssl/certs/star_instana_io.crt.j2 > /etc/ssl/certs/star_instana_io.crt
 j2 /etc/ssl/dhgroup.pem.j2 > /etc/ssl/dhgroup.pem
-exec nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
+
+USER=root
+if [[ "$USER_AND_GROUP_NAME" != "" ]]; then
+  grep "${USER_AND_GROUP_NAME}:" /etc/passwd
+  if [[ "$?" != "0" ]]; then
+    groupadd -r $USER_AND_GROUP_NAME --gid=$GROUP_ID && useradd -r -g $USER_AND_GROUP_NAME --uid=$USER_ID $USER_AND_GROUP_NAME
+  fi
+
+  USER=$USER_AND_GROUP_NAME
+fi
+
+exec gosu $USER nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
