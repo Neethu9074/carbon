50337818e76da8787b021e2ad01456be024bf288
pavlobaron <pb@pbit.org>
2016-08-25 17:19:07 +0200

    allow to provide cert/key through mounted docker volume instead of env

--- deployment/services/nginx.sh
+++ deployment/services/nginx.sh
@@ -15,8 +15,11 @@ set -e
 source /opt/discovery/discovery.general
 
 j2 /etc/nginx/nginx.conf.j2 > /etc/nginx/nginx.conf
-j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key
-j2 /etc/ssl/certs/star_instana_io.crt.j2 > /etc/ssl/certs/star_instana_io.crt
+if [ ! -f /etc/ssl/private/star_instana_io.key ]; then
+  j2 /opt/www/star_instana_io.key.j2 > /etc/ssl/private/star_instana_io.key
+  j2 /etc/ssl/certs/star_instana_io.crt.j2 > /etc/ssl/certs/star_instana_io.crt
+fi
+
 j2 /etc/ssl/dhgroup.pem.j2 > /etc/ssl/dhgroup.pem
 
 exec nginx -g "daemon off;" 2>&1 | logger -t ui-client-nginx
