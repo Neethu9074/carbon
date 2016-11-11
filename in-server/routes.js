fa2a7ee90ae935b051353b9d63e73ebf5e6388ca
Ben Ripkens <bripkens.dev@gmail.com>
2016-11-11 07:49:28 +0100

    Fix typo

--- in-server/routes.js
+++ in-server/routes.js
@@ -136,7 +136,7 @@ function askUiBackendWhetherTheRequestIsAuthorized(req, cb) {
   sendRequest({
     url: serverConfig.uiBackendBaseUrl + '/checkUserAccessPermitted',
     headers: {
-      'Cookie': `${serverConfig.cookie.name}=${req.cookies[serverConfig]}`
+      'Cookie': `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
     },
     timeout: 5000
   }, (error, response) => {
