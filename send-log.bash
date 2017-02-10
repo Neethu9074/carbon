975315102e1fcd8b0490c8de87e76a51b702feec
Ben Ripkens <bripkens@gmail.com>
2017-02-10 16:30:21 +0100

    Add script to generate logs

---
+++ send-log.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+for (( ; ; )) do
+  curl -X POST -v https://internal-logs.instana.io/log/v1 \
+       --header "x-instana-key: 2f83c6e2-6c31-44c2-a060-f808b24defda" \
+       --header "Content-Type: application/json" \
+       -d "{
+    \"@timestamp\": \"$(node -e 'console.log(new Date().toISOString())')\",
+    \"message\": \"this is a test\",
+    \"host\": \"0a:35:2f:ff:fe:85:68:a1\"
+  }"
+
+  sleep 1;
+done
