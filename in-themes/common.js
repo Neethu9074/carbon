1c1cfc16c3a866ef965d54d312207dd91f484d93
Ben Ripkens <bripkens@gmail.com>
2017-04-05 16:04:12 +0200

    Time picker is not visible when a dashboard is open

--- in-themes/common.js
+++ in-themes/common.js
@@ -14,6 +14,7 @@ module.exports = {
     footer: zIndex++,
     fullscreenView: zIndex++,
     footerSelectedTimeNotification: zIndex++,
+    footerTimePicker: zIndex++,
     mapNotes: zIndex++,
 
     searchMenu: zIndex++,
