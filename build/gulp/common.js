807d5b35ba9f071ffd09b60ccfd4dba28ede3cf6
Ben Ripkens <bripkens@gmail.com>
2017-07-05 15:48:11 +0200

    Use the new COM_INSTANA_IMAGE_TAG env var

--- build/gulp/common.js
+++ build/gulp/common.js
@@ -42,8 +42,8 @@ gulp.task('writeBuildInfo', cb => {
     data.branch = process.env.INSTANA_UICLIENT_BRANCH;
   }
 
-  if (process.env.INSTANA_CONTAINER_TAG) {
-    data.tag = process.env.INSTANA_CONTAINER_TAG;
+  if (process.env.COM_INSTANA_IMAGE_TAG) {
+    data.tag = process.env.COM_INSTANA_IMAGE_TAG;
   }
 
   fs.writeFile(paths.buildInfoFileLocation, JSON.stringify(data), cb);
