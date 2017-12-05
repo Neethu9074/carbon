99eec5752954faebc95a79651a0806baa9c1777d
Ben Ripkens <bripkens@gmail.com>
2017-12-05 10:03:01 +0100

    Support icon inspection

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,4 +1,11 @@
+// Ensure that base CSS is loaded and added to the DOM.
 import 'in-themes/foundation.less';
+
+// ensure that some expected global variables exist
+import './globals';
+
+// load all the plugins
+import 'in-forge';
 
 import './ButtonStory';
 import './SvgIconStory';
