42ec46cf86eb05026c1258c215edfc10ecfb101e
Ben Ripkens <bripkens@gmail.com>
2018-01-10 14:59:58 +0100

    Revise data retrieval after today's discoveries

--- dataAccessAndRetrievalModel.js
+++ dataAccessAndRetrievalModel.js
@@ -3,6 +3,8 @@
 // ===========================================================
 //          Common Types
 // ===========================================================
+export type Id = string;
+
 // Time
 export type Millis = number;
 export type Timestamp = Millis;
@@ -26,16 +28,25 @@ export type PaginatedResult<T> = {
   items: [T]
 };
 
+// Error
+export type ErrorCode = 'notFound' | 'serverError';
+export type Error = {
+  message: string,
+  type: ErrorCode
+}
+export type Try<T> = {
+  value: T,
+  errors: Error
+};
+
 // Application 2.0 specific types
-export type LogicalPluginType = 'web' | 'rpc' | 'batch' | 'sdk' | 'messaging' | 'database';
 export type LogicalEntityType = 'application' | 'service' | 'endpoint';
+export type LogicalPluginType = 'web' | 'rpc' | 'batch' | 'sdk' | 'messaging' | 'database';
 export type LogicalEntity = {
-  id: string,
+  id: Id,
   label: string,
   entityType: LogicalEntityType,
-
-  // not available for applications
-  pluginType: ?LogicalPluginType
+  pluginType: [LogicalPluginType]
 };
 
 // ===========================================================
@@ -44,27 +55,28 @@ export type LogicalEntity = {
 
 // Get services
 export type GetServicesOpts = {
-  // only retrieve services of a specific application ID
-  applicationId: ?string,
-  timeframe: Timeframe
+  timeframe: Timeframe,
+  application: ?Id,
 };
-export type GetServicesItem = {
-  id: string,
-  label: string,
-  type: LogicalPluginType,
-  applications: number,
-  endpoints: number,
-  calls: TimestampedMetrics,
-  latency: TimestampedMetrics,
-  errors: TimestampedMetrics
-};
-export type GetServicesResult = [GetServicesItem];
-export type GetServices = (opts: GetServicesOpts) => GetServicesResult;
+export type GetServices = (opts: GetServicesOpts) => Try<[Id]>;
 
 
-// Get application / service / endpoint entity data
 export type GetLogicalEntityOpts = {
-  id: ?string,
+  id: Id,
   timeframe: Timeframe
 };
-export type GetLogicalEntity = (opts: GetLogicalEntityOpts) => LogicalEntity;
+export type GetLogicalEntity = (opts: GetLogicalEntityOpts) => Try<LogicalEntity>;
+
+
+export type GetMetricsOpts = {
+  application: ?Id,
+  service: ?Id,
+  endpoint: ?Id,
+  metric: string,
+  // It can be that we only want to show one large number in the UI which is supposed
+  desiredNumberOfDataPoints: number,
+  // aggregation to use to reduce the available number of data points to the desired number of data points
+  aggregation: 'mean' | 'max' | 'min' | 'p25' | 'p50' | 'p75' | 'p90' | 'p95' | 'p98' | 'p99',
+  timeframe: Timeframe
+}
+export type GetMetrics = (opts: GetMetricsOpts) => Try<TimestampedMetrics>;
