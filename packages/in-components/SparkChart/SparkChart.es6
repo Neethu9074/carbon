e40b793224750c3da12a253f68be594d79ead96c
Simon Thiele <simon.thiele.st@googlemail.com>
2018-01-19 10:14:09 +0100

    setup basic component

---
+++ packages/in-components/SparkChart/SparkChart.es6
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import locals from './SparkChart.mless';
+
+export default function SparkChart({}) {
+  return <div className={locals.sparkChart}>hello!</div>;
+}
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -23,6 +23,7 @@ import './components/ComboBoxStory';
 import './components/IconStory';
 import './components/KeyValuePopup';
 import './components/MultiSelectStory';
+import './components/SparkChart';
 import './components/TopListStory';
 import './components/sdk/CollapsibleStory';
 import './components/sdk/Descriptions';
