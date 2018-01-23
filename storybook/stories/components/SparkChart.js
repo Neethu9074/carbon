60c01ebcc32663f99c239716ff1d9dc9b1a3df76
Simon Thiele <simon.thiele.st@googlemail.com>
2018-01-23 11:24:30 +0100

    clear up line renderer

--- storybook/stories/components/SparkChart.js
+++ storybook/stories/components/SparkChart.js
@@ -4,27 +4,35 @@ import React from 'react';
 import SparkChart from 'in-components/SparkChart';
 import Root from '../_helpers/Root';
 
-storiesOf('components/SparkChart', module).add('simple', () => <Simple />);
+storiesOf('components/SparkChart', module)
+  .add('simple', () => <Simple />)
+  .add('missing datapoints', () => <Missing />);
 
 function Simple() {
   const lotsOfMetrics = [];
   for (let i = 0; i < 40; i++) {
-    lotsOfMetrics.push([i / 40 * 60000, Math.random() * 100]);
+    lotsOfMetrics.push([i / 40 * 6000, Math.random() * 100]);
   }
 
   return (
     <Root>
       <SparkChart
-        timeframe={{ windowSize: 60000, to: 60000 }}
-        metrics={[[0, 1], [10000, 1], [20000, 0], [30000, 2], [60000, 0.5]]}
+        timeframe={{ windowSize: 6000, to: 6000 }}
+        metrics={[[0, 1], [1000, 1], [2000, 0], [3000, 2], [4000, 1], [5000, 2], [6000, 0.5]]}
       />
+      <SparkChart timeframe={{ windowSize: 6000, to: 6000 }} metrics={lotsOfMetrics} />
+    </Root>
+  );
+}
+
+function Missing() {
+  return (
+    <Root>
       <SparkChart
-        timeframe={{ windowSize: 60000, to: 60000 }}
-        metrics={[[10000, 1], [20000, 0], [30000, 2], [60000, 0.5]]}
+        rollup={1000}
+        timeframe={{ windowSize: 6000, to: 6000 }}
+        metrics={[[0, 1], [1000, 1], [3000, 1], [5000, 2], [6000, 1]]}
       />
-      <SparkChart timeframe={{ windowSize: 60000, to: 60000 }} metrics={[[0, 1], [10000, 1], [20000, 0], [30000, 2]]} />
-      <SparkChart timeframe={{ windowSize: 60000, to: 60000 }} metrics={[[10000, 1], [20000, 0], [30000, 2]]} />
-      <SparkChart timeframe={{ windowSize: 60000, to: 60000 }} metrics={lotsOfMetrics} />
     </Root>
   );
 }
