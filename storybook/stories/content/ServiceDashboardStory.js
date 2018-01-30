52e507fa0765e7b886c12c7553649e44aa1d756c
Simon Thiele <simon.thiele.st@googlemail.com>
2018-01-30 10:19:52 +0100

    add single stories for breadcrumb, header and tabs

--- storybook/stories/content/ServiceDashboardStory.js
+++ storybook/stories/content/ServiceDashboardStory.js
@@ -3,21 +3,57 @@ import React from 'react';
 
 import BasicApplicationDashboardWrapper from 'in-applications/Dashboards/BasicApplicationDashboard/BasicApplicationDashboardWrapper';
 import BasicApplicationDashboardHeader from 'in-applications/Dashboards/BasicApplicationDashboard/BasicApplicationDashboardHeader';
+import BreadcrumbHeader from 'in-applications/TabView/components/BreadcrumbHeader';
+import Breadcrumbs from 'in-sdk/components/dashboard/breadcrumb/Breadcrumbs';
 import breadcrumbs from 'in-applications/Dashboards/service/breadcrumbs';
+import DashboardHeader from 'in-applications/TabView/components/Header';
 import { always } from 'in-services/fixedStreams';
 
 import Root from '../_helpers/Root';
 
 storiesOf('content/Service Dashboard', module)
+  .add('Breadcrumb', () => <Breadcrumb />)
+  .add('Header', () => <HeaderStory />)
+  .add('Tabs', () => <Tabs />)
   .add('Pending', () => <Pending />)
   .add('Error', () => <Error />);
+
+function Breadcrumb() {
+  return (
+    <Root>
+      {breadcrumbs && <Breadcrumbs items={breadcrumbs} />}
+      <BreadcrumbHeader />
+    </Root>
+  );
+}
+
+function HeaderStory() {
+  return (
+    <Root>
+      <Header result={createMockResult('foobar')} />
+    </Root>
+  );
+}
+
+function Tabs() {
+  return (
+    <Root>
+      <DashboardHeader
+        tabs={tabMock}
+        result={createMockResult('foobar')}
+        dashboardBasedUrl="/"
+        HeaderComponent={() => null}
+      />
+    </Root>
+  );
+}
 
 function Pending() {
   return (
     <Root>
       <BasicApplicationDashboardWrapper
         get={({ serviceId }) =>
-          createMockResult(serviceId).map(result => {
+          createMockResult$(serviceId).map(result => {
             result.progress.loading = true;
             result.progress.percent = 0.4;
             return result;
@@ -38,7 +74,7 @@ function Error() {
     <Root>
       <BasicApplicationDashboardWrapper
         get={({ serviceId }) =>
-          createMockResult(serviceId).map(result => {
+          createMockResult$(serviceId).map(result => {
             result.errors.push('much errors ahead');
             result.data = { id: result.data.id };
             return result;
@@ -81,15 +117,19 @@ const locationMock = {
   }
 };
 
+function createMockResult$(serviceId) {
+  return always(createMockResult(serviceId));
+}
+
 function createMockResult(serviceId) {
-  return always({
+  return {
     errors: [],
     progress: {},
     data: {
       id: serviceId,
       label: 'foobar'
     }
-  });
+  };
 }
 
 function Summary() {}
