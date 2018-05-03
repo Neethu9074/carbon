Neethu9074/carbon

commit 833f40999996f7bbcf05659b89c33975d1f2335f

Get Application: endpoint no longer requires a filter (#775)
* Get Application: endpoint no longer requires a  filter

* Get Application: reintroduce cache busting property

* Get Application: remove filter from the request made by traces button

* Get Application: update flow type

* Get Application: remove unecessary return statements

packages/in-applications/components/TraceButtonDialogPresenter/TraceList.es6
@@ -27,8 +27,11 @@ export default connectTo(
         <Row
           type="Application"
           iconType="app_application"
-          entityId={applicationId}
-          getEntity={getApplication}
+          getEntity={() =>
+            getApplication({
+              id: applicationId
+            })
+          }
           applicationId={applicationId}
           total={allTracesCount}
           timeframe={timeframe}
@@ -37,8 +40,17 @@ export default connectTo(
         <Row
           type="Service"
           iconType="app_service"
-          entityId={serviceId}
-          getEntity={getService}
+          getEntity={() =>
+            getService({
+              id: serviceId,
+              filter: {
+                application: applicationId,
+                service: serviceId,
+                endpoint: endpointId,
+                timeframe
+              }
+            })
+          }
           applicationId={applicationId}
           serviceId={serviceId}
           total={allTracesCount}
@@ -48,8 +60,17 @@ export default connectTo(
         <Row
           type="Endpoint"
           iconType="app_endpoint"
-          entityId={endpointId}
-          getEntity={getEndpoint}
+          getEntity={() =>
+            getEndpoint({
+              id: endpointId,
+              filter: {
+                application: applicationId,
+                service: serviceId,
+                endpoint: endpointId,
+                timeframe
+              }
+            })
+          }
           applicationId={applicationId}
           serviceId={serviceId}
           endpointId={endpointId}

packages/in-applications/Dashboards/application/ApplicationDashboard.es6
@@ -27,13 +27,7 @@ export default connectTo({ timeframe: timeframe$ }, function ApplicationDashboar
       <Breadcrumbs items={ApplicationBreadcrumbs(props)} />
       <TabView
         result$={getApplication({
-          id: props.applicationId,
-          filter: {
-            application: props.applicationId,
-            service: props.serviceId,
-            endpoint: props.endpointId,
-            timeframe
-          }
+          id: props.applicationId
         })}
         HeaderComponent={Header}
         location={location}

packages/in-applications/NewApplication/NewApplicationWaiter.es6
@@ -55,11 +55,6 @@ export default connectTo(
 function getApp({ applicationId }) {
   return getApplication({
     id: applicationId,
-    filter: {
-      timeframe: {
-        to: Date.now(), // subscription cache busting
-        windowSize: 60 * 1000 // looking at the last minute is sufficien to identify a new application
-      }
-    }
+    requestTime: Date.now() // subscription cache busting
   });
 }

packages/in-applications/breadcrumbs/ApplicationBreadcrumb.es6
@@ -8,11 +8,7 @@ import connectTo from 'in-hoc/connectTo';
 export default connectTo(
   props => ({
     application: getApplication({
-      id: props.applicationId,
-      filter: {
-        application: props.applicationId,
-        timeframe: props.timeframe
-      }
+      id: props.applicationId
     })
   }),
   function ApplicationBreadcrumb({ application, applicationId }) {

packages/in-applications/breadcrumbs/ApplicationBreadcrumbWithSwitcher.es6
@@ -14,11 +14,7 @@ import locals from './ApplicationBreadcrumbWithSwitcher.mless';
 export default connect(
   props => ({
     application: getApplication({
-      id: props.applicationId,
-      filter: {
-        application: props.applicationId,
-        timeframe: props.timeframe
-      }
+      id: props.applicationId
     }),
     applications: getApplications({
       pagination: {

packages/in-applications/components/TraceButtonDialogPresenter/Row.es6
@@ -14,21 +14,13 @@ import Link from 'in-components/Link';
 import locals from './Row.mless';
 
 export default connectTo(
-  ({ label, value, entityId, getEntity, timeframe, applicationId, serviceId, endpointId }) => {
+  ({ label, value, getEntity, timeframe, applicationId, serviceId, endpointId }) => {
     const observables = {};
     if (!value) {
       observables.value = getTracesCount({ timeframe, applicationId, serviceId, endpointId });
     }
-    if (!label && entityId) {
-      observables.label = getEntity({
-        id: entityId,
-        filter: {
-          application: applicationId,
-          service: serviceId,
-          endpoint: endpointId,
-          timeframe
-        }
-      }).map(result => get(result, ['data', 'label'], null));
+    if (!label) {
+      observables.label = getEntity().map(result => get(result, ['data', 'label'], null));
     }
     return observables;
   },

packages/in-types/application.es6
@@ -70,12 +70,9 @@ export type ApplicationItem = {
 export type GetApplications = (query: GetApplicationsQuery) => Observable<Result<PaginatedResult<ApplicationItem>>>;
 
 /* A query specification that yields a single application.
- * The filter might result in a subset of the data for the application being returned, also
- * the filter needs to include the timeframe.
  */
 export type GetApplicationQuery = {
-  id: string,
-  filter: ?Filter
+  id: string
 };
 
 /* The operation that executes the query for a single application and returns the result as an observable. */

commit 7dc20762218ea9383da43ba0a5f1376f19189c07

do not attempt to render trace button row when id is missing
This fixes the "Subscription payload is invalid" on the trace button
on explore dashboards. The problem was probably introduced in
Propbably intro with 833f40999996f7bbcf05659b89c33975d1f2335f
as this removed the check for enitityId in
TraceButtonDialogPresenter/Row.es6

Now we simply do not render the Row at all if the required ID is not
present.

packages/in-applications/components/TraceButtonDialogPresenter/TraceList.es6
@@ -24,60 +24,66 @@ export default connectTo(
     return (
       <section className={locals.traceList}>
         {allTracesCount && <Row label="All Traces" total={allTracesCount} value={allTracesCount} />}
-        <Row
-          type="Application"
-          iconType="app_application"
-          getEntity={() =>
-            getApplication({
-              id: applicationId
-            })
-          }
-          applicationId={applicationId}
-          total={allTracesCount}
-          timeframe={timeframe}
-          backButtonLabels={backButtonLabels}
-        />
-        <Row
-          type="Service"
-          iconType="app_service"
-          getEntity={() =>
-            getService({
-              id: serviceId,
-              filter: {
-                application: applicationId,
-                service: serviceId,
-                endpoint: endpointId,
-                timeframe
-              }
-            })
-          }
-          applicationId={applicationId}
-          serviceId={serviceId}
-          total={allTracesCount}
-          timeframe={timeframe}
-          backButtonLabels={backButtonLabels}
-        />
-        <Row
-          type="Endpoint"
-          iconType="app_endpoint"
-          getEntity={() =>
-            getEndpoint({
-              id: endpointId,
-              filter: {
-                application: applicationId,
-                service: serviceId,
-                endpoint: endpointId,
-                timeframe
-              }
-            })
-          }
-          applicationId={applicationId}
-          serviceId={serviceId}
-          endpointId={endpointId}
-          total={allTracesCount}
-          timeframe={timeframe}
-          backButtonLabels={backButtonLabels}
-        />
+        {applicationId && (
+          <Row
+            type="Application"
+            iconType="app_application"
+            getEntity={() =>
+              getApplication({
+                id: applicationId
+              })
+            }
+            applicationId={applicationId}
+            total={allTracesCount}
+            timeframe={timeframe}
+            backButtonLabels={backButtonLabels}
+          />
+        )}
+        {serviceId && (
+          <Row
+            type="Service"
+            iconType="app_service"
+            getEntity={() =>
+              getService({
+                id: serviceId,
+                filter: {
+                  application: applicationId,
+                  service: serviceId,
+                  endpoint: endpointId,
+                  timeframe
+                }
+              })
+            }
+            applicationId={applicationId}
+            serviceId={serviceId}
+            total={allTracesCount}
+            timeframe={timeframe}
+            backButtonLabels={backButtonLabels}
+          />
+        )}
+        {endpointId && (
+          <Row
+            type="Endpoint"
+            iconType="app_endpoint"
+            getEntity={() =>
+              getEndpoint({
+                id: endpointId,
+                filter: {
+                  application: applicationId,
+                  service: serviceId,
+                  endpoint: endpointId,
+                  timeframe
+                }
+              })
+            }
+            applicationId={applicationId}
+            serviceId={serviceId}
+            endpointId={endpointId}
+            total={allTracesCount}
+            timeframe={timeframe}
+            backButtonLabels={backButtonLabels}
+          />
+        )}
       </section>
     );
   }