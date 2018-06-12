bcb5f0fbaf57a72d80c08d46d33a165ad80bad8b
Bastian Krol <bastian.krol@instana.com>
2018-06-12 07:19:03 +0200

    run prettifier on in-server/routes/index.js to apply standard formatting rules

--- packages/in-server/routes/index.js
+++ packages/in-server/routes/index.js
@@ -12,19 +12,15 @@ const errorPages = require('../errorPages.js');
 const { getCurrentUser } = require('../auth');
 const paths = require('../services/paths');
 
-const router = module.exports = express.Router();
+const router = (module.exports = express.Router());
 
-const compiledTemplate = Handlebars.compile(
-  fs.readFileSync(paths.indexHtmlTemplate, {encoding: 'utf8'})
-);
-
+const compiledTemplate = Handlebars.compile(fs.readFileSync(paths.indexHtmlTemplate, { encoding: 'utf8' }));
 
 const indexJsSri = checkSumMod.getSriIntegrityForFile(paths.indexJs);
 const indexJsChecksum = checkSumMod.getChecksumForFile(paths.indexJs);
 const indexCssChecksum = checkSumMod.getChecksumForFile(paths.indexCss);
 const stringifiedClientConfig = JSON.stringify(clientConfig);
 const stringifiedBuildInformation = JSON.stringify(buildInformation);
-
 
 // Array of all the JS chunks which may be prefetched by the browser
 //
@@ -36,7 +32,8 @@ const stringifiedBuildInformation = JSON.stringify(buildInformation);
 //     fileName
 //   }
 // ]
-const prefetchItems = fs.readdirSync(paths.bundleDir)
+const prefetchItems = fs
+  .readdirSync(paths.bundleDir)
   .filter(fileName => /^\d+\.[a-z0-9]+\.js$/i.test(fileName))
   .map(fileName => {
     return {
@@ -46,67 +43,69 @@ const prefetchItems = fs.readdirSync(paths.bundleDir)
     };
   });
 
-
 router.get('/', (req, res) => {
   res.vary('*');
   res.set('cache-control', 'private, no-cache, no-store, must-revalidate, max-age=0";');
 
   getCurrentUser(req)
-    .then(([statusCode, userStr]) => Promise.all([
-      getUserSettings(req, res, statusCode, userStr),
-      getSearchFields(req, res)
-    ]))
+    .then(([statusCode, userStr]) =>
+      Promise.all([getUserSettings(req, res, statusCode, userStr), getSearchFields(req, res)])
+    )
     .then(([[statusCode, userStr, userSettings], searchFieldsStr]) =>
-      sendIndex(req, res, statusCode, userStr, userSettings, searchFieldsStr))
+      sendIndex(req, res, statusCode, userStr, userSettings, searchFieldsStr)
+    )
     .catch(err => {
       console.error('Failed to deliver index.html to user:', err);
       errorPages.send500(req, res);
     });
 });
 
-
 function getUserSettings(req, res, getUserStatusCode, userStr) {
   return new Promise((resolve, reject) => {
-    sendRequest({
-      url: serverConfig.uiBackendBaseUrl + '/api/ui/settings',
-      headers: {
-        'Cookie': `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
+    sendRequest(
+      {
+        url: serverConfig.uiBackendBaseUrl + '/api/ui/settings',
+        headers: {
+          Cookie: `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
+        },
+        timeout: 5000
       },
-      timeout: 5000
-    }, (error, response, userSettings) => {
-      if (error) {
-        reject(new Error('Failed to retrieve user settings from ui-backend: ' + String(error)));
-      } else {
-        resolve([response.statusCode, userStr, userSettings]);
+      (error, response, userSettings) => {
+        if (error) {
+          reject(new Error('Failed to retrieve user settings from ui-backend: ' + String(error)));
+        } else {
+          resolve([response.statusCode, userStr, userSettings]);
+        }
       }
-    });
+    );
   });
 }
 
 function getSearchFields(req) {
   return new Promise((resolve, reject) => {
-    sendRequest({
-      url: serverConfig.uiBackendBaseUrl + '/api/search/fields',
-      headers: {
-        'Cookie': `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
+    sendRequest(
+      {
+        url: serverConfig.uiBackendBaseUrl + '/api/search/fields',
+        headers: {
+          Cookie: `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
+        },
+        timeout: 5000
       },
-      timeout: 5000
-    }, (error, response, searchFields) => {
-      if (error) {
-        reject(new Error('Failed to retrieve user settings from ui-backend: ' + String(error)));
-      } else {
-        resolve(searchFields);
+      (error, response, searchFields) => {
+        if (error) {
+          reject(new Error('Failed to retrieve user settings from ui-backend: ' + String(error)));
+        } else {
+          resolve(searchFields);
+        }
       }
-    });
+    );
   });
 }
 
 function sendIndex(req, res, getUserStatusCode, userStr, userSettings, searchFieldsStr) {
   if (getUserStatusCode === 401) {
     const requestedAbsoluteUrl = serverConfig.baseUrl + req.originalUrl;
-    res.redirect(
-      serverConfig.baseUrl + '/auth/signIn?returnUrl=' + encodeURIComponent(requestedAbsoluteUrl)
-    );
+    res.redirect(serverConfig.baseUrl + '/auth/signIn?returnUrl=' + encodeURIComponent(requestedAbsoluteUrl));
     return;
   } else if (getUserStatusCode === 403) {
     errorPages.send403(req, res);
@@ -118,11 +117,7 @@ function sendIndex(req, res, getUserStatusCode, userStr, userSettings, searchFie
   }
 
   // doing this exactly three times as the template requires three nonces
-  const nonces = [
-    uuid.v4(),
-    uuid.v4(),
-    uuid.v4()
-  ];
+  const nonces = [uuid.v4(), uuid.v4(), uuid.v4()];
 
   let cspExtensions = '';
   // Ff this route was called by safari -> add the unsafe inline Content-Security-Policy
@@ -139,20 +134,22 @@ function sendIndex(req, res, getUserStatusCode, userStr, userSettings, searchFie
       ' https://www.google-analytics.com *.instana.io'
   );
 
-  res.send(compiledTemplate({
-    indexJsChecksum,
-    indexJsSri,
-    indexCssChecksum,
-    nonces,
-    googleAnalyticsTrackingId: serverConfig.googleAnalyticsTrackingId,
-    eumTrackingDomain: serverConfig.eum.domain,
-    eumTrackingApiKey: serverConfig.eum.apiKey,
-    backendTraceId: req.get('x-instana-t') || '',
-    prefetchItems,
-    user: userStr,
-    config: stringifiedClientConfig,
-    build: stringifiedBuildInformation,
-    searchFields: searchFieldsStr,
-    settings: userSettings
-  }));
+  res.send(
+    compiledTemplate({
+      indexJsChecksum,
+      indexJsSri,
+      indexCssChecksum,
+      nonces,
+      googleAnalyticsTrackingId: serverConfig.googleAnalyticsTrackingId,
+      eumTrackingDomain: serverConfig.eum.domain,
+      eumTrackingApiKey: serverConfig.eum.apiKey,
+      backendTraceId: req.get('x-instana-t') || '',
+      prefetchItems,
+      user: userStr,
+      config: stringifiedClientConfig,
+      build: stringifiedBuildInformation,
+      searchFields: searchFieldsStr,
+      settings: userSettings
+    })
+  );
 }
