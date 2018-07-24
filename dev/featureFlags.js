ce57b1605c2c74ae2562558327629b23ad71596c
Ben Ripkens <bripkens@gmail.com>
2018-07-24 16:42:53 +0200

    Switch to new feature flag for one zero app data

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -1,7 +1,7 @@
 // needs to be kept in sync with
 // https://github.com/instana/fleet/blob/master/ansible/roles/fleet_deploy_component/templates/ui-client.hcl.j2
 module.exports = {
-  oneZeroAppDataEnabled: true,
+  oneZeroAppDataPresentationEnabled: true,
   twoZeroAppDataPresentationEnabled: true,
   pingComparisonEnabled: true
 };
