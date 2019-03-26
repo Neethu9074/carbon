76c9c4a712ffc9203c8960e3ce08990cf2fd354c
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2019-03-26 10:50:54 +0100

    Make unifiedAlerting default to TRUE

--- packages/in-server/services/resolvers/consul.js
+++ packages/in-server/services/resolvers/consul.js
@@ -34,7 +34,7 @@ exports.getFeatureFlags = (tenant, unit) => cache(`getFeatureFlags:${tenant}:${u
     getBooleanSetting(`settings/${tenant}-${unit}/REDIS_METRIC_WRITING_ENABLED`, true),
     getBooleanSetting(`settings/${tenant}-${unit}/LAST_SEVEN_DAYS_TIME_PRESET_ENABLED`, false),
     getBooleanSetting(`settings/${tenant}-${unit}/CUSTOM_EVENTS_WEBSITE_MONITORING_ENABLED`, false),
-    getBooleanSetting(`settings/${tenant}-${unit}/UNIFIED_ALERTING`, false),
+    getBooleanSetting(`settings/${tenant}-${unit}/UNIFIED_ALERTING`, true),
     getBooleanSetting(`settings/${tenant}-${unit}/RULE_DEPRECATION_VALIDATION_CHECKS_ENABLED`, true),
     getBooleanSetting(`settings/TRACK_URL_PATH_CHANGES`, true)
   ]).then(([
