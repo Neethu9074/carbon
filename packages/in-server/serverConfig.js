4127d28788f2349f13860289e58b3fe6124adda6
Ben Ripkens <bripkens@gmail.com>
2019-05-15 09:26:02 +0200

    Read server config in yaml format and support multiple config locations
    In order to support Kubernetes based configurations and to align with
    backend configurations files.
    
    Support the new config locations and the new format.

--- packages/in-server/serverConfig.js
+++ packages/in-server/serverConfig.js
@@ -1,1 +1,32 @@
-module.exports = require('./serverConfig.json');
+const yaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs');
+
+const possibleConfigFileLocations = [
+  '/etc/instana/ui-client/config.yaml',
+  '/etc/instana/ui-client/config.json',
+  path.join(__dirname, 'serverConfig.yaml'),
+  path.join(__dirname, 'serverConfig.json')
+];
+
+const configFileContent = getConfigFileContent();
+if (!configFileContent) {
+  throw new Error(`Could not locate existing/readable config file at any of the checkable locations. ` +
+    `Checked: ${possibleConfigFileLocations.join(', ')}`);
+}
+module.exports = yaml.safeLoad(configFileContent);
+
+function getConfigFileContent() {
+  for (let i = 0; i < possibleConfigFileLocations.length; i++) {
+    const location = possibleConfigFileLocations[i];
+
+    try {
+      // checks existence and readability at once
+      return fs.readFileSync(location, {encoding: 'utf8'});
+    } catch (e) {
+      continue;
+    }
+  }
+
+  return null;
+}
