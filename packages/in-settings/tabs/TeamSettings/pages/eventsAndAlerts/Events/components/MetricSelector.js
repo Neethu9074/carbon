0a4ae7b86d054fbfb535dee226f06830383aee00
Marco Schaefer <47627413+codecapitano@users.noreply.github.com>
2019-05-23 10:58:27 +0200

    remove old UI elem for specifying custom metrics (#1591)
    When configuring custom events for k8s pod entity type one gets "custom" metric in the metrics drop down. When selecting it, old UI element for specifying custom metrics which we used to have before pops up. See the screenshot.

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/components/MetricSelector.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/components/MetricSelector.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
-import EditableTextInput from 'in-components/EditableTextInput/EditableTextInput';
-import { instanaInternalFeaturesEnabled } from 'in-services/featureFlags';
 import { getPlainMetricList } from 'in-sdk/metrics';
 import ComboBox from 'in-components/ComboBox';
 
 export default function MetricSelector({ id, plugin, onChange, value, metrics }) {
   const metricsList = Array.isArray(metrics) ? metrics.slice() : getPlainMetricList(plugin);
-
-  if (instanaInternalFeaturesEnabled) {
-    metricsList.push({
-      value: 'custom',
-      label: 'custom'
-    });
-  }
 
   metricsList.forEach(metricDef => {
     if (metricDef.origLabel) {
@@ -35,16 +26,5 @@ export default function MetricSelector({ id, plugin, onChange, value, metrics })
     );
   });
 
-  const select = <ComboBox name={id} value={value} options={metricsList} onChange={onChange} />;
-
-  if (value === 'custom') {
-    return (
-      <div>
-        {select}
-        <EditableTextInput text={value} onSave={text => onChange({ value: text })} />
-      </div>
-    );
-  }
-
-  return select;
+  return <ComboBox name={id} value={value} options={metricsList} onChange={onChange} />;
 }
