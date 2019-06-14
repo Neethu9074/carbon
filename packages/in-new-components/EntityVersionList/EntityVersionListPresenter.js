e7e55e8e4c7af0a8ec8fcc7daffc05b4aef0ee39
simonThiele <simon.thiele.st@googlemail.com>
2019-06-14 15:23:45 +0200

    change implementation of the snapshot version table

--- packages/in-new-components/EntityVersionList/EntityVersionListPresenter.js
+++ packages/in-new-components/EntityVersionList/EntityVersionListPresenter.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 
-import { Table, Tbody, Tr, Td } from 'in-components/tables/sharedComponents';
 import { formatDateTime } from 'in-services/formatters/date';
-import { mutateUrl } from 'in-stores/navigation/navigation';
-import { urlQueryKeys } from 'in-stores/time/config';
+import { getFixedTimeframeUrl } from 'in-stores/timeline';
 import PluginIcon from 'in-components/PluginIcon';
 import { getSingular } from 'in-sdk/pluginName';
+import Link from 'in-components/Link';
 
 import locals from './EntityVersionListPresenter.mless';
 
@@ -30,31 +29,28 @@ export default function EntityVersionListPresenter({ plugin, snapshotVersions })
         We could not find a version of this pod in the selected time range. We found other versions in different time
         ranges:
       </p>
-      <Table className={locals.table}>
-        <Tbody>
-          {snapshotVersions.map(({ from, to }, i) => (
-            <Tr
-              key={i}
-              className={locals.row}
-              size="compact"
-              onClick={() =>
-                mutateUrl(location => {
-                  const windowSize = (to || Date.now()) - from;
-                  location.query[urlQueryKeys.autoRefresh] = 'false';
-                  location.query[urlQueryKeys.to] = to || '';
-                  location.query[urlQueryKeys.windowSize] = windowSize;
-                  location.query[urlQueryKeys.focusedMoment] = to ? to - windowSize / 2 : '';
-                })
-              }
-            >
-              <Td className={locals.fromTimestamp}>{formatDateTime(from)}</Td>
+      <ul className={locals.list}>
+        {snapshotVersions.map(({ from, to }, i) => {
+          const windowSize = (to || Date.now()) - from;
 
-              <Td className={locals.toSpan}>to</Td>
-              <Td className={locals.toTimestamp}>{to ? formatDateTime(to) : 'Now'}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          return (
+            <li key={i} size="compact" className={locals.item}>
+              <Link
+                className={locals.link}
+                href$={getFixedTimeframeUrl({
+                  windowSize,
+                  to,
+                  focusedMoment: to ? to - windowSize / 2 : ''
+                })}
+              >
+                <span className={locals.fromTimestamp}>{formatDateTime(from)}</span>
+                <span className={locals.toSpan}>to</span>
+                <span className={locals.toTimestamp}>{to ? formatDateTime(to) : 'Now'}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
