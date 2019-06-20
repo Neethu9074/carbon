dfeaf58ee0c7ba18fb3918cc44a06813b0d3efb8
Ben Ripkens <bripkens@gmail.com>
2019-06-20 20:27:36 +0200

    Only show the agent link when the snapshot exists

--- packages/in-components/Dashboard/components/DashboardBreadcrumb.js
+++ packages/in-components/Dashboard/components/DashboardBreadcrumb.js
@@ -30,7 +30,10 @@ export default connectTo(
       return _physicalHierarchy;
     }),
     closeDashboardLink: getCloseDashboardLink(),
-    agent: isInternalVisible$.flatMap(enabled => (enabled ? getAgentSnapshotId(props.snapshot) : alwaysNull))
+    agent: isInternalVisible$
+      .flatMap(enabled => (enabled ? getAgentSnapshotId(props.snapshot) : alwaysNull))
+      // get snapshot to ensure that the agent snapshot can be found
+      .flatMap(snapshotId => (snapshotId ? getSnapshot(snapshotId) : alwaysNull))
   }),
   function DashboardBreadcrumb({ physicalHierarchy, snapshotId, closeDashboardLink, agent }) {
     if (!physicalHierarchy) {
@@ -43,7 +46,7 @@ export default connectTo(
 
     const items = physicalHierarchy.map(id => <PhysicalHierarchyBreadCrumb key={id} snapshotId={id} />);
     if (agent) {
-      items.unshift(<PhysicalHierarchyBreadCrumb snapshotId={agent} />);
+      items.unshift(<PhysicalHierarchyBreadCrumb snapshotId={agent.get('id')} />);
     }
     items.unshift(
       <Breadcrumb className={locals.homeBreadcrumb} href={closeDashboardLink}>
