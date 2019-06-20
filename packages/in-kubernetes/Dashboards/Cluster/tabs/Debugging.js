c0f1d2cb62561fca545ab31ca16b487a07c8bae5
Will James <william.james@instana.com>
2019-06-20 14:48:19 -0500

    Show Kubernetes debugging information on cluster dashboard details page (#1679)
    * Show Kubernetes debugging information on cluster dashboard details page
    
    * Use internal visibility observable for showing cluster debugging information

---
+++ packages/in-kubernetes/Dashboards/Cluster/tabs/Debugging.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import KeyValueList from 'in-kubernetes/Dashboards/commonComponents/KeyValueList';
+
+export default function Debugging({ cluster }) {
+  const debuggingInfo =
+    cluster.debuggingInfo &&
+    Object.keys(cluster.debuggingInfo).map(key => ({ key, value: cluster.debuggingInfo[key] }));
+  return <KeyValueList title="Debugging Information" items={debuggingInfo} onEmptyText={'No debugging information'} />;
+}
--- packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
+++ packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import { isInternalVisible$ } from 'in-new-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore';
 import ComponentStatusTable from 'in-kubernetes/Dashboards/Cluster/tabs/ComponentStatusTable';
+import Debugging from 'in-kubernetes/Dashboards/Cluster/tabs/Debugging';
+import { Row, Col } from 'in-new-components/layout/Grid';
+import connectTo from 'in-hoc/connectTo';
 
-export default function Details({ data: cluster }) {
-  return <ComponentStatusTable cluster={cluster} />;
-}
+export default connectTo({ isInternalVisible: isInternalVisible$ }, function Details({
+  isInternalVisible,
+  data: cluster
+}) {
+  return (
+    <Fragment>
+      <Row>
+        <Col lg={12}>
+          <ComponentStatusTable cluster={cluster} />
+        </Col>
+      </Row>
+      {isInternalVisible && (
+        <Row>
+          <Col lg={12}>
+            <Debugging cluster={cluster} />
+          </Col>
+        </Row>
+      )}
+    </Fragment>
+  );
+});
