96053c82d0049edbd5cc557e3ff85f24a7a1efb3
Simon Thiele <simon.thiele.st@googlemail.com>
2019-06-25 08:58:57 +0200

    Columnize should add a margin not a padding (#1691)
    * columnize should add a margin not a padding
    
    * implement columnize using grid
    
    * use lg to get a similar behaviour
    
    * children don't need to be an array
    
    * was not aware of the react method to get children as array
    
    * replace magic with floor

--- packages/in-sdk/components/dashboard/Columize.js
+++ packages/in-sdk/components/dashboard/Columize.js
@@ -1,7 +1,24 @@
+import invariant from 'invariant';
 import React from 'react';
 
-import locals from './Columize.mless';
+import { Col, Row } from 'in-new-components/layout/Grid';
 
 export default function Columize({ children }) {
-  return <div className={locals.columize}>{children}</div>;
+  children = React.Children.toArray(children);
+  children = children.filter(Boolean);
+
+  if (children.length === 0) {
+    return null;
+  }
+
+  const sizePerItem = 12 / children.length;
+  invariant(children.length <= 12, 'Columnize is not supported with more than 12 children');
+
+  return (
+    <Row>
+      {React.Children.map(children, child => (
+        <Col lg={Math.floor(sizePerItem)}>{child}</Col>
+      ))}
+    </Row>
+  );
 }
