81a747f6a4ef14b74377ae01a1064848b759e692
Henning Treu <henning.treu@instana.com>
2019-07-08 15:51:45 +0200

    Add sensors list & bundle list to agent monitoring - internal use (#1722)

---
+++ packages/in-forge/plugins/instanaAgent/Dashboard/BundleList.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import connectTo from 'in-hoc/connectTo';
+import { getRawPayloadWithTimestamp } from 'in-stores/snapshot';
+import TimeOfLastUpdateCardTitle from 'in-sdk/components/dashboard/TimeOfLastUpdateCardTitle';
+import Table from 'in-sdk/components/dashboard/Table';
+
+const cols = [
+  {
+    title: 'Name',
+    type: 'string',
+    typeArgs: {
+      getValue(row) {
+        return row.key;
+      }
+    }
+  },
+  {
+    title: 'Version',
+    type: 'string',
+    width: 100,
+    typeArgs: {
+      getValue(row) {
+        return row.version;
+      }
+    }
+  },
+  {
+    title: 'State',
+    type: 'string',
+    width: 100,
+    typeArgs: {
+      getValue(row) {
+        return row.state;
+      }
+    }
+  }
+];
+
+export default connectTo(
+  props => {
+    return {
+      data: getRawPayloadWithTimestamp(props.snapshot.get('id'), 'bundles')
+    };
+  },
+  function BundleList({ data }) {
+    if (!data || !data.get('raw_payload')) {
+      return null;
+    }
+
+    const bundles = data.get('raw_payload');
+    if (bundles.size === 0) {
+      return null;
+    }
+
+    const rows = bundles.toJS().map(value => {
+      return {
+        key: value.name,
+        state: value.state,
+        version: value.version
+      };
+    });
+
+    return (
+      <Table
+        cardTitle={<TimeOfLastUpdateCardTitle title="OSGi Bundle List" timestamp={data.get('timestamp')} />}
+        withoutPadding
+        cols={cols}
+        rows={rows}
+        initialSortColumn={1}
+        initialSortDirection="desc"
+      />
+    );
+  }
+);
---
+++ packages/in-forge/plugins/instanaAgent/Dashboard/SensorList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import connectTo from 'in-hoc/connectTo';
+import { getRawPayloadWithTimestamp } from 'in-stores/snapshot';
+import { number } from 'in-services/formatters/number';
+import Table from 'in-sdk/components/dashboard/Table';
+import TimeOfLastUpdateCardTitle from 'in-sdk/components/dashboard/TimeOfLastUpdateCardTitle';
+
+const cols = [
+  {
+    title: 'Name',
+    type: 'string',
+    typeArgs: {
+      getValue(row) {
+        return row.key;
+      },
+      getContent(value) {
+        return value;
+      }
+    }
+  },
+  {
+    title: 'Usage Count',
+    type: 'number',
+    typeArgs: {
+      getValue(row) {
+        return row.usage;
+      },
+      getContent: number.compact
+    }
+  }
+];
+
+export default connectTo(
+  props => {
+    return {
+      data: getRawPayloadWithTimestamp(props.snapshot.get('id'), 'sensors')
+    };
+  },
+  function SensorList({ data }) {
+    if (!data || !data.get('raw_payload')) {
+      return null;
+    }
+
+    const sensors = data.get('raw_payload');
+    if (sensors.size === 0) {
+      return null;
+    }
+
+    const rows = sensors.toJS().map(sensor => {
+      return {
+        key: sensor.name,
+        usage: sensor.usage
+      };
+    });
+
+    return (
+      <Table
+        cardTitle={<TimeOfLastUpdateCardTitle title="Sensor List" timestamp={data.get('timestamp')} />}
+        withoutPadding
+        cols={cols}
+        rows={rows}
+        initialSortColumn={1}
+        initialSortDirection="desc"
+      />
+    );
+  }
+);
