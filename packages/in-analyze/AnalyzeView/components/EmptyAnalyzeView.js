afa7c846d1785a1a5d35bfff380037dd3cb66738
simonThiele <simon.thiele.st@googlemail.com>
2019-07-19 11:38:32 +0200

    remove the generic link to docs before feature freeze

--- packages/in-analyze/AnalyzeView/components/EmptyAnalyzeView.js
+++ packages/in-analyze/AnalyzeView/components/EmptyAnalyzeView.js
@@ -4,17 +4,17 @@ import EntityPageMainNotification from 'in-new-components/EntityPageMainNotifica
 import { getEntityNameByType, getIconByType } from 'in-analyze/AnalyzeView/dataSources';
 import CenterAlignmentColumn from 'in-components/layout/CenterAlignmentColumn';
 import AnalyzeHeader from 'in-analyze/components/AnalyzeHeader';
-import ArticleContent from 'in-new-components/ArticleContent';
 import Sticky from 'in-components/Sticky';
 
 export default function EmptyAnalyzeView({ type }) {
+  const entityName = getEntityNameByType(type);
   return (
     <Sticky header={<AnalyzeHeader />}>
       <CenterAlignmentColumn>
         <EntityPageMainNotification
           icon={getIconByType(type)}
-          title={`No ${getEntityNameByType(type)} found`}
-          renderExplanation={() => <ArticleContent id="analyzeNoData" />}
+          title={`No ${entityName} found`}
+          explanation={`There were no ${entityName} retrieved for the selected time range.`}
         />
       </CenterAlignmentColumn>
     </Sticky>
