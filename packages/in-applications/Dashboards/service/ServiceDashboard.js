83976656257028565d735601040e4a9d5ea6571d
Dahlia Bock <dlbock@gmail.com>
2019-07-26 16:41:58 -0500

    Revert "Support one-to-many mappings from Instana service => k8s services (#1801)"
    This reverts commit 69f77040e0deaa9fae8081cffa0cf7efe057799e since testing showed that the scenario originally envisioned by PM does not work the way our span processing pipeline works. Reverting this while we await PM decision for next steps.

--- packages/in-applications/Dashboards/service/ServiceDashboard.js
+++ packages/in-applications/Dashboards/service/ServiceDashboard.js
@@ -1,9 +1,7 @@
 import React, { Fragment } from 'react';
 
 import ApplicationEntityHealthIndicatorBehavior from 'in-applications/components/ApplicationEntityHealthIndicatorBehavior';
-import InstanaServiceToKubernetesServicesButton from 'in-kubernetes/components/InstanaServiceToKubernetesServicesButton';
 import InstanaServiceToKubernetesServiceButton from 'in-kubernetes/components/InstanaServiceToKubernetesServiceButton';
-import { isInternalVisible$ } from 'in-new-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore';
 import TechnologyIndicatorList from 'in-applications/components/TechnologyIndicator/TechnologyIndicatorList';
 import EndpointTypeBadgeList from 'in-applications/Dashboards/commonComponents/EndpointTypeBadgeList';
 import HealthIndicatorButtonPresenter from 'in-new-components/health/HealthIndicatorButtonPresenter';
@@ -17,6 +15,7 @@ import { serviceDashboard } from 'in-applications/navigation/paths';
 import { getMatrixParameter } from 'in-stores/navigation/matrix';
 import tabs from 'in-applications/Dashboards/service/tabs/index';
 import getService from 'in-subscription/application/getService';
+import { kubernetesEnabled } from 'in-services/featureFlags';
 import { hasKubernetesAccess } from 'in-stores/permission';
 import { timeConfig$ } from 'in-stores/time/config';
 import Footer from 'in-new-components/Footer';
@@ -60,49 +59,37 @@ function Header(props) {
     <BasicDashboardHeader
       title="Service"
       icon="lib_application_service"
-      renderActions={GetActions}
+      renderActions={Actions}
       renderSubTypes={SubTypes}
       {...props}
     />
   );
 }
 
-function GetActions(props) {
-  return <Actions {...props} />;
+function Actions({ applicationId, serviceId, endpointId, timeConfig }) {
+  return (
+    <Fragment>
+      <AnalyzeTracesButton
+        applicationId={applicationId}
+        serviceId={serviceId}
+        endpointId={endpointId}
+        timeConfig={timeConfig}
+      />
+      {kubernetesEnabled &&
+        hasKubernetesAccess && (
+          <InstanaServiceToKubernetesServiceButton serviceId={serviceId} timeConfig={timeConfig} />
+        )}
+      <ApplicationEntityHealthIndicatorBehavior
+        showOkayOnNoIssues={false}
+        IndicatorPresenter={HealthIndicatorButtonPresenter}
+        applicationId={applicationId}
+        serviceId={serviceId}
+        endpointId={endpointId}
+        timeConfig={timeConfig}
+      />
+    </Fragment>
+  );
 }
-
-const Actions = connectTo(
-  {
-    nToMK8sServiceToInstanaServiceEnabled: isInternalVisible$
-  },
-  function Actions({ applicationId, serviceId, endpointId, timeConfig, nToMK8sServiceToInstanaServiceEnabled }) {
-    return (
-      <Fragment>
-        <AnalyzeTracesButton
-          applicationId={applicationId}
-          serviceId={serviceId}
-          endpointId={endpointId}
-          timeConfig={timeConfig}
-        />
-        {nToMK8sServiceToInstanaServiceEnabled
-          ? hasKubernetesAccess && (
-              <InstanaServiceToKubernetesServicesButton serviceId={serviceId} timeConfig={timeConfig} />
-            )
-          : hasKubernetesAccess && (
-              <InstanaServiceToKubernetesServiceButton serviceId={serviceId} timeConfig={timeConfig} />
-            )}
-        <ApplicationEntityHealthIndicatorBehavior
-          showOkayOnNoIssues={false}
-          IndicatorPresenter={HealthIndicatorButtonPresenter}
-          applicationId={applicationId}
-          serviceId={serviceId}
-          endpointId={endpointId}
-          timeConfig={timeConfig}
-        />
-      </Fragment>
-    );
-  }
-);
 
 function SubTypes({ result }) {
   return (
