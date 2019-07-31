d2e6c748c0145c9835268c16a313ff2f806736ce
Fabian Lange <lange.fabian@gmail.com>
2019-07-31 09:56:16 +0200

    add S3 bucket public snapshot data

--- packages/in-forge/plugins/awsS3/Info.js
+++ packages/in-forge/plugins/awsS3/Info.js
@@ -12,6 +12,7 @@ export default function Info({ snapshot }) {
       <DescriptionItem title="Bucket Owner">{data.get('s3_bucket_owner_name')}</DescriptionItem>
       <DescriptionItem title="Bucket Owner ID">{data.get('s3_bucket_owner_id')}</DescriptionItem>
       <DescriptionItem title="Bucket Created at">{formatDateTime(data.get('s3_bucket_created_at'))}</DescriptionItem>
+      <DescriptionItem title="Bucket Public">{data.get('s3_bucket_public')}</DescriptionItem>
       <DescriptionItem title="Grouping Zone">{data.get('aws_grouping_zone')}</DescriptionItem>
     </DescriptionList>
   );
