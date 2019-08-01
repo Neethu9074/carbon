ba65596cb8b9858231dd508023ebab39939e8fc9
phani-p3 <phani.p3@gmail.com>
2019-08-01 12:29:49 +0200

    Implement comments

--- packages/in-new-components/time/TimeSelectionDialogPresenter/Presets.js
+++ packages/in-new-components/time/TimeSelectionDialogPresenter/Presets.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import SelectableItem from 'in-new-components/time/TimeSelectionDialogPresenter/SelectableItem';
 import { getFixedTimePresets, getLivePresets } from 'in-new-components/time/timePresets';
 import TimePresetsForReleases from 'in-new-components/time/TimePresetsForReleases';
 import Header from 'in-new-components/time/TimeSelectionDialogPresenter/Header';
 import { setActiveDialog, close } from 'in-components/DialogPresenter/store';
-import { Col, Row } from 'in-new-components/layout/Grid';
 import Button from 'in-new-components/Button';
 import Dialog from 'in-new-components/Dialog';
 import locals from './Presets.mless';
@@ -14,35 +13,43 @@ export default function Presets({ timeConfig, onChange }) {
   return (
     <div className={locals.wrapper}>
       <Header>Presets</Header>
-      <Row>
-        <Col lg={6} className={locals.col}>
+      <div className={locals.container}>
+        <div className={locals.left}>
           {getLivePresets().map((preset, i) => (
-            <SelectableItem key={i} timeConfig={timeConfig} newTimeframe={preset} onChange={onChange} />
+            <SelectableItem timeConfig={timeConfig} newTimeframe={preset} onChange={onChange} key={i} />
           ))}
-        </Col>
-        <Col lg={6} className={locals.col}>
+        </div>
+        <div className={locals.right}>
           {getFixedTimePresets().map((preset, i) => (
-            <SelectableItem key={i} timeConfig={timeConfig} newTimeframe={preset} onChange={onChange} />
+            <SelectableItem timeConfig={timeConfig} newTimeframe={preset} onChange={onChange} key={i} />
           ))}
-          <h1 className={locals.header}>Go to a Release</h1>
-          <TimePresetsForReleases onChange={onChange} timeConfig={timeConfig} pageSize={2} />
-          <Button
-            className={locals.button}
-            kind="secondary"
-            onClick={() =>
-              setActiveDialog(
-                <Dialog className={locals.dialog} title="Search for a release" onClose={() => close()}>
-                  <TimePresetsForReleases onChange={onChange} timeConfig={timeConfig} pageSize={5} />
-                </Dialog>
-              )
-            }
-            icon="lib_actions_search"
-            iconHeight={18}
-          >
-            Search for a release
-          </Button>
-        </Col>
-      </Row>
+          <ReleasesPresets onChange={onChange} timeConfig={timeConfig} />
+        </div>
+      </div>
     </div>
+  );
+}
+
+function ReleasesPresets({ onChange, timeConfig }) {
+  return (
+    <Fragment>
+      <h1 className={locals.header}>Go to a Release</h1>
+      <TimePresetsForReleases onChange={onChange} timeConfig={timeConfig} pageSize={2} />
+      <Button
+        className={locals.button}
+        kind="secondary"
+        onClick={() =>
+          setActiveDialog(
+            <Dialog className={locals.dialog} title="Search for a release" onClose={() => close()}>
+              <TimePresetsForReleases onChange={onChange} timeConfig={timeConfig} pageSize={5} />
+            </Dialog>
+          )
+        }
+        icon="lib_actions_search"
+        iconHeight={18}
+      >
+        Search for a release
+      </Button>
+    </Fragment>
   );
 }
