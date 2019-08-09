076346c5bbbd5c5d54a941329f108c7a0974d24f
phani-p3 <phani.p3@gmail.com>
2019-08-09 16:21:29 +0200

    Modify release time-picker as per new design

--- packages/in-new-components/time/TimePresetsForReleases.js
+++ packages/in-new-components/time/TimePresetsForReleases.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import SelectableItem from 'in-new-components/time/TimeSelectionDialogPresenter/SelectableItem';
 import ServerTablePresenter from 'in-components/tables/ServerTable/ServerTablePresenter';
-import { formatDateTime, fromNowAccurately } from 'in-services/formatters/date';
 import WithEmptyStateFallback from 'in-new-components/WithEmptyStateFallback';
 import NoDataAvailable from 'in-new-components/Errors/NoDataAvailable';
-import { setTimeConfig, timeConfig$ } from 'in-stores/time/config';
+import getReleases from 'in-events/subscriptions/getReleases';
 import { close } from 'in-components/DialogPresenter/store';
+import { formatDateTime } from 'in-services/formatters/date';
 import { getModifiedUrlStream } from 'in-stores/navigation';
 import ServerTable from 'in-components/tables/ServerTable';
-import { pendingResult } from 'in-services/fixedObjects';
-import getReleases from 'in-subscription/getReleases';
+import { setTimeConfig } from 'in-stores/time/config';
 import { timeout } from 'reactive-observables';
-import connectTo from 'in-hoc/connectTo';
 import Link from 'in-components/Link';
 
 function getColumnDefinitions({ windowSize }) {
@@ -48,64 +45,53 @@ function getColumnDefinitions({ windowSize }) {
   ];
 }
 
-export default connectTo(props => {
-  const pageSize = props.pageSize;
-
-  return {
-    result: timeout(800)
-      .flatMap(() => getReleasesSubscribeEvent({ pageSize }))
-      .startWith(pendingResult)
-  };
-})(TimePresetsForReleases);
-function TimePresetsForReleases({ timeConfig, onChange, result, pageSize }) {
+export default function TimePresetsForReleases({ timeConfig }) {
   const columnDefinitions = getColumnDefinitions(timeConfig);
-
-  if (pageSize === 2 && result.data && result.data.totalHits > 0) {
-    return getReleasesPresets(result, timeConfig, onChange);
-  } else if (pageSize === 5) {
-    return (
-      <WithEmptyStateFallback
-        center={false}
-        getHasDataToRender={getHasDataToRender}
-        FallbackComponent={() => (
-          <ServerTablePresenter
-            columnDefinitions={columnDefinitions}
-            pageSize={5}
-            query=""
-            page={1}
-            result={{ errors: [], progress: { loading: false }, data: { items: [] } }}
-            renderNoDataAvailable={() => NoDataAvailable}
-          />
-        )}
-      >
-        <ServerTable
-          get={getReleasesSubscribeEvent}
-          getResettingProps={() => ['query']}
-          defaultPageSize={5}
+  return (
+    <WithEmptyStateFallback
+      center={false}
+      getHasDataToRender={getHasDataToRender}
+      FallbackComponent={() => (
+        <ServerTablePresenter
           columnDefinitions={columnDefinitions}
-          paginationResettingProps={{}}
-          getRowProps={() => ({ size: 'compact' })}
-          noDataMessage="No releases found"
-          defaultOrderBy="start"
-          defaultOrderDirection="DESC"
-          searchPlaceholder="Filter..."
+          pageSize={5}
+          query=""
+          page={1}
+          result={{ errors: [], progress: { loading: false }, data: { items: [] } }}
+          renderNoDataAvailable={() => NoDataAvailable}
         />
-      </WithEmptyStateFallback>
-    );
-  } else {
-    return null;
-  }
+      )}
+    >
+      <ServerTable
+        get={getReleasesSubscribeEvent}
+        getResettingProps={() => ['query']}
+        defaultPageSize={5}
+        columnDefinitions={columnDefinitions}
+        paginationResettingProps={{}}
+        getRowProps={() => ({ size: 'compact' })}
+        noDataMessage="No releases found"
+        defaultOrderBy="start"
+        defaultOrderDirection="DESC"
+        searchPlaceholder="Filter..."
+      />
+    </WithEmptyStateFallback>
+  );
 }
 
 function getHasDataToRender() {
-  const pageSize = 5;
-  const page = 1;
-  return timeConfig$
-    .flatMap(timeConfig => getReleasesSubscribeEvent({ page, pageSize, timeConfig }))
+  return timeout(800)
+    .flatMap(() => getReleasesSubscribeEvent({}))
     .map(result => !result.data || result.data.totalHits > 0);
 }
 
-function getReleasesSubscribeEvent({ query = '', page = 1, pageSize = 5, orderBy = 'start', orderDirection = 'DESC' }) {
+export function getReleasesSubscribeEvent({
+  page = 1,
+  pageSize = 5,
+  orderBy = 'start',
+  orderDirection = 'DESC',
+  query = '',
+  timeConfig = null
+}) {
   return getReleases({
     pagination: {
       page,
@@ -116,24 +102,6 @@ function getReleasesSubscribeEvent({ query = '', page = 1, pageSize = 5, orderBy
       direction: orderDirection
     },
     filter: query,
-    timeConfig: null
+    timeConfig: timeConfig
   });
-}
-
-function getReleasesPresets(result, timeConfig, onChange) {
-  return result.data.items.map(item => getReleaseTimePreset(item, timeConfig, onChange));
-}
-
-function getReleaseTimePreset(item, timeConfig, onChange) {
-  let label = item.name;
-  const suffix = fromNowAccurately(item.start);
-  label = label + '  (' + suffix + ' ago)';
-  const to = item.start + timeConfig.windowSize / 2;
-
-  const newTimeConfig = {
-    label: label,
-    windowSize: timeConfig.windowSize,
-    to
-  };
-  return <SelectableItem key={item.id} timeConfig={timeConfig} newTimeframe={newTimeConfig} onChange={onChange} />;
 }
--- packages/in-new-components/time/TimeSelectionDialogPresenter/Presets.js
+++ packages/in-new-components/time/TimeSelectionDialogPresenter/Presets.js
@@ -1,17 +1,36 @@
 import React, { Fragment } from 'react';
 
+import TimePresetsForReleases, { getReleasesSubscribeEvent } from 'in-new-components/time/TimePresetsForReleases';
 import SelectableItem from 'in-new-components/time/TimeSelectionDialogPresenter/SelectableItem';
 import { getFixedTimePresets, getLivePresets } from 'in-new-components/time/timePresets';
-import TimePresetsForReleases from 'in-new-components/time/TimePresetsForReleases';
 import Header from 'in-new-components/time/TimeSelectionDialogPresenter/Header';
 import { setActiveDialog, close } from 'in-components/DialogPresenter/store';
+import { fromNowAccurately } from 'in-services/formatters/date';
 import { releasesEnabled } from 'in-services/featureFlags';
+import { pendingResult } from 'in-services/fixedObjects';
+import { timeout } from 'reactive-observables';
 import Button from 'in-new-components/Button';
 import Dialog from 'in-new-components/Dialog';
+import Tooltip from 'in-components/Tooltip';
+import connectTo from 'in-hoc/connectTo';
 
 import locals from './Presets.mless';
 
-export default function Presets({ timeConfig, onChange }) {
+export default connectTo(props => {
+  const releaseTimeConfig = {
+    to: Date.now(),
+    focusedMoment: Date.now(),
+    autoRefresh: props.timeConfig.autoRefresh,
+    windowSize: 30 * 24 * 60 * 60 * 1000
+  };
+  return {
+    result: timeout(800)
+      .flatMap(() => getReleasesSubscribeEvent({ timeConfig: releaseTimeConfig, page: 1, pageSize: 3 }))
+      .startWith(pendingResult)
+  };
+})(Presets);
+
+function Presets({ timeConfig, onChange, result }) {
   return (
     <div className={locals.wrapper}>
       <Header>Presets</Header>
@@ -25,18 +44,22 @@ export default function Presets({ timeConfig, onChange }) {
           {getFixedTimePresets().map((preset, i) => (
             <SelectableItem timeConfig={timeConfig} newTimeframe={preset} onChange={onChange} key={i} />
           ))}
-          {releasesEnabled && <ReleasesPresets onChange={onChange} timeConfig={timeConfig} />}
+          {releasesEnabled &&
+            result.data &&
+            result.data.totalHits > 0 && (
+              <ReleasesPresets onChange={onChange} timeConfig={timeConfig} result={result} />
+            )}
         </div>
       </div>
     </div>
   );
 }
 
-function ReleasesPresets({ onChange, timeConfig }) {
+function ReleasesPresets({ onChange, timeConfig, result }) {
   return (
     <Fragment>
       <h1 className={locals.header}>Go to a Release</h1>
-      <TimePresetsForReleases onChange={onChange} timeConfig={timeConfig} pageSize={2} />
+      {getReleasesPresets(result, timeConfig, onChange)}
       <Button
         className={locals.button}
         kind="secondary"
@@ -52,5 +75,33 @@ function ReleasesPresets({ onChange, timeConfig }) {
         Search for a release
       </Button>
     </Fragment>
+  );
+}
+
+function getReleasesPresets(result, timeConfig, onChange) {
+  return result.data.items.map(item => getReleaseTimePreset(item, timeConfig, onChange));
+}
+
+function getReleaseTimePreset(item, timeConfig, onChange) {
+  const label = item.name;
+  const suffix = fromNowAccurately(item.start) + ' ago';
+  const to = item.start + timeConfig.windowSize / 2;
+
+  const newTimeConfig = {
+    label: label,
+    windowSize: timeConfig.windowSize,
+    to
+  };
+
+  const selectable = (
+    <div className={locals.releases}>
+      <SelectableItem timeConfig={timeConfig} newTimeframe={newTimeConfig} onChange={onChange} />
+    </div>
+  );
+
+  return (
+    <Tooltip key={item.id} content={suffix}>
+      {selectable}
+    </Tooltip>
   );
 }
