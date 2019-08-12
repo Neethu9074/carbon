c547e98a90d96b09905dd528b9b1c4daa23b98b3
Simon Thiele <simon.thiele.st@googlemail.com>
2019-08-12 15:19:42 +0200

    subscribe against resize events to recalculate the position on resize (#1859)

--- packages/in-new-components/overlays/OverlayPresenter/SingleOverlayPresenter.js
+++ packages/in-new-components/overlays/OverlayPresenter/SingleOverlayPresenter.js
@@ -2,93 +2,103 @@ import React from 'react';
 
 import TooltipCalculator from 'in-components/Tooltip/TooltipCalculator';
 import { evaluateClassNames } from 'in-services/util/classnames';
+import { debouncedResize$ } from 'in-services/browser';
 import toPx from 'in-services/formatters/toPx';
 
 import locals from './SingleOverlayPresenter.mless';
 
 export const overlayClassName = locals.overlay;
 
-export default class SingleOverlayPresenter extends React.PureComponent {
-  componentDidMount() {
-    this.position();
-  }
+import connectTo from 'in-hoc/connectTo';
 
-  componentDidUpdate() {
-    this.position();
-  }
+export default connectTo(
+  {
+    // the result itself is not used but it triggers a componentDidUpdate which recalculates the position
+    debouncedResize: debouncedResize$
+  },
 
-  position() {
-    const tooltipElement = this.tooltipElement;
-    // add the CSS classes for arrow alignment
-    tooltipElement.className = '';
-    tooltipElement.classList.add(locals.overlay);
-    tooltipElement.classList.add(locals[`style--${this.props.kind || 'popover'}`]);
-    if (this.props.inContentArea) {
-      tooltipElement.classList.add(locals.inContentArea);
+  class SingleOverlayPresenter extends React.PureComponent {
+    componentDidMount() {
+      this.position();
     }
-    const focusedElementBox = this.props.relativeTo.getBoundingClientRect();
-    const tooltipElementBox = tooltipElement.getBoundingClientRect();
 
-    const windowWidth = window.innerWidth - getScrollbarWidth();
-    const windowHeight = window.innerHeight;
-    const bounds = {
-      left: 0,
-      top: 0,
-      right: windowWidth,
-      bottom: windowHeight
-    };
-    const tooltip = {
-      left: tooltipElementBox.left,
-      top: tooltipElementBox.top,
-      right: tooltipElementBox.left + tooltipElementBox.width,
-      bottom: tooltipElementBox.top + tooltipElementBox.height,
-      align: this.props.align || 'auto'
-    };
-    const reference = {
-      left: focusedElementBox.left,
-      top: focusedElementBox.top,
-      right: focusedElementBox.left + focusedElementBox.width,
-      bottom: focusedElementBox.top + focusedElementBox.height
-    };
+    componentDidUpdate() {
+      this.position();
+    }
 
-    const result = TooltipCalculator.calculate(bounds, tooltip, reference);
-    set(tooltipElement, 'left', result.left);
-    set(tooltipElement, 'right', result.right !== null ? windowWidth - result.right : null);
-    if (this.props.inContentArea) {
-      if (result.top != null) {
-        set(tooltipElement, 'top', result.top + window.scrollY);
+    position() {
+      const tooltipElement = this.tooltipElement;
+      // add the CSS classes for arrow alignment
+      tooltipElement.className = '';
+      tooltipElement.classList.add(locals.overlay);
+      tooltipElement.classList.add(locals[`style--${this.props.kind || 'popover'}`]);
+      if (this.props.inContentArea) {
+        tooltipElement.classList.add(locals.inContentArea);
       }
-      if (result.bottom != null) {
-        set(tooltipElement, 'top', result.bottom - tooltipElementBox.height + window.scrollY);
+      const focusedElementBox = this.props.relativeTo.getBoundingClientRect();
+      const tooltipElementBox = tooltipElement.getBoundingClientRect();
+
+      const windowWidth = window.innerWidth - getScrollbarWidth();
+      const windowHeight = window.innerHeight;
+      const bounds = {
+        left: 0,
+        top: 0,
+        right: windowWidth,
+        bottom: windowHeight
+      };
+      const tooltip = {
+        left: tooltipElementBox.left,
+        top: tooltipElementBox.top,
+        right: tooltipElementBox.left + tooltipElementBox.width,
+        bottom: tooltipElementBox.top + tooltipElementBox.height,
+        align: this.props.align || 'auto'
+      };
+      const reference = {
+        left: focusedElementBox.left,
+        top: focusedElementBox.top,
+        right: focusedElementBox.left + focusedElementBox.width,
+        bottom: focusedElementBox.top + focusedElementBox.height
+      };
+
+      const result = TooltipCalculator.calculate(bounds, tooltip, reference);
+      set(tooltipElement, 'left', result.left);
+      set(tooltipElement, 'right', result.right !== null ? windowWidth - result.right : null);
+      if (this.props.inContentArea) {
+        if (result.top != null) {
+          set(tooltipElement, 'top', result.top + window.scrollY);
+        }
+        if (result.bottom != null) {
+          set(tooltipElement, 'top', result.bottom - tooltipElementBox.height + window.scrollY);
+        }
+      } else {
+        set(tooltipElement, 'top', result.top);
+        set(tooltipElement, 'bottom', result.bottom != null ? windowHeight - result.bottom : null);
       }
-    } else {
-      set(tooltipElement, 'top', result.top);
-      set(tooltipElement, 'bottom', result.bottom != null ? windowHeight - result.bottom : null);
+
+      if (!this.props.withoutArrow) {
+        tooltipElement.classList.add(locals[`align--${tooltip.align}`]);
+      }
     }
 
-    if (!this.props.withoutArrow) {
-      tooltipElement.classList.add(locals[`align--${tooltip.align}`]);
+    render() {
+      const { content: Content, props, id, autoOpen, autoClose, delayedOpen, delayedClose, inContentArea } = this.props;
+      return (
+        <div
+          data-overlay-id={id}
+          ref={r => (this.tooltipElement = r)}
+          className={evaluateClassNames({
+            [locals.overlay]: true,
+            [locals.inContentArea]: inContentArea
+          })}
+          onMouseEnter={autoOpen ? delayedOpen : undefined}
+          onMouseLeave={autoClose ? delayedClose : undefined}
+        >
+          <Content {...props} />
+        </div>
+      );
     }
   }
-
-  render() {
-    const { content: Content, props, id, autoOpen, autoClose, delayedOpen, delayedClose, inContentArea } = this.props;
-    return (
-      <div
-        data-overlay-id={id}
-        ref={r => (this.tooltipElement = r)}
-        className={evaluateClassNames({
-          [locals.overlay]: true,
-          [locals.inContentArea]: inContentArea
-        })}
-        onMouseEnter={autoOpen ? delayedOpen : undefined}
-        onMouseLeave={autoClose ? delayedClose : undefined}
-      >
-        <Content {...props} />
-      </div>
-    );
-  }
-}
+);
 
 function set(ele, prop, value) {
   if (value == null) {
