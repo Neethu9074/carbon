6454e49619ecdc103aac1fabd613bc3c5b662b6a
Goran Ceko <goran.ceko@instana.com>
2019-08-21 10:31:20 +0200

    Do not display Open files chart on AIX host (#1889)

--- packages/in-forge/plugins/host/hostUtils.js
+++ packages/in-forge/plugins/host/hostUtils.js
@@ -1,6 +1,7 @@
 export function isWindows(snapshot) {
   return !!snapshot.getIn(['data', 'os.name'], '').match(/windows/i);
 }
+
 export function isZos(snapshot) {
   return !!snapshot.getIn(['data', 'os.name'], '').match(/z\/OS/i);
 }
@@ -13,6 +14,10 @@ export function isMacOs(snapshot) {
   return snapshot.getIn(['data', 'os.name'], '').match(/Mac OS/i);
 }
 
+export function isAixOs(snapshot) {
+  return snapshot.getIn(['data', 'os.name'], '').match(/AIX/i);
+}
+
 export function supportsOpenFiles(snapshot) {
-  return !isWindows(snapshot) && !isZos(snapshot);
+  return !isWindows(snapshot) && !isZos(snapshot) && !isAixOs(snapshot);
 }
