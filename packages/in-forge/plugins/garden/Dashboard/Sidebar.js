b082e5b419f3a6ed8b5d58655b14eb716a515d64
Goran Ceko <goran.ceko@instana.com>
2019-08-21 10:57:17 +0200

    Garden Sidebar: Display ports, events and process ids in separate sections and remove groupId

--- packages/in-forge/plugins/garden/Dashboard/Sidebar.js
+++ packages/in-forge/plugins/garden/Dashboard/Sidebar.js
@@ -4,10 +4,16 @@ import RunningComponentsList from 'in-sdk/components/sidebar/RunningComponentsLi
 import CloudfoundryInfo from 'in-forge/plugins/garden/CloudfoundryInfo';
 import Collapsible from 'in-sdk/components/sidebar/Collapsible';
 import { pcfEnabled } from 'in-services/featureFlags';
+import List from 'in-sdk/components/sidebar/List';
 
 import Info from 'in-forge/plugins/garden/Info';
 
 export default function GardenSidebar({ snapshot }) {
+  const data = snapshot.get('data');
+  const ports = data.get('mappedPorts');
+  const events = data.get('events');
+  const processIds = data.get('processIDs');
+
   return (
     <div>
       <Collapsible initiallyOpen>
@@ -18,6 +24,49 @@ export default function GardenSidebar({ snapshot }) {
       </Collapsible>
 
       {pcfEnabled && <CloudfoundryInfo snapshot={snapshot} />}
+
+      {ports &&
+        ports.size > 0 && (
+          <Collapsible>
+            <Collapsible.Header>Ports</Collapsible.Header>
+            <Collapsible.Content>
+              <List>
+                {ports.map(port => (
+                  <List.Item key={port}>{port}</List.Item>
+                ))}
+              </List>
+            </Collapsible.Content>
+          </Collapsible>
+        )}
+
+      {events &&
+        events.size > 0 && (
+          <Collapsible>
+            <Collapsible.Header>Events</Collapsible.Header>
+            <Collapsible.Content>
+              <List>
+                {events.map(item => (
+                  <List.Item key={item}>{item}</List.Item>
+                ))}
+              </List>
+            </Collapsible.Content>
+          </Collapsible>
+        )}
+
+      {processIds &&
+        processIds.size > 0 && (
+          <Collapsible>
+            <Collapsible.Header>Process IDs</Collapsible.Header>
+            <Collapsible.Content>
+              <List>
+                {processIds.map(item => (
+                  <List.Item key={item}>{item}</List.Item>
+                ))}
+              </List>
+            </Collapsible.Content>
+          </Collapsible>
+        )}
+
       <RunningComponentsList snapshotId={snapshot.get('id')} />
     </div>
   );
--- packages/in-forge/plugins/garden/Info.js
+++ packages/in-forge/plugins/garden/Info.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
-import List from 'in-sdk/components/sidebar/List';
 
 export default function GardenInfo({ snapshot }) {
   const data = snapshot.get('data');
-  const ports = data.get('mappedPorts');
-  const processIds = data.get('processIDs');
-  const events = data.get('events');
 
   return (
     <DescriptionList>
@@ -18,39 +14,8 @@ export default function GardenInfo({ snapshot }) {
       <DescriptionItem title="State">{data.get('state')}</DescriptionItem>
       <DescriptionItem title="Status">{data.get('status')}</DescriptionItem>
       <DescriptionItem title="App Id">{data.get('appId')}</DescriptionItem>
-      <DescriptionItem title="Group Id">{data.get('groupId')}</DescriptionItem>
       <DescriptionItem title="Org Id">{data.get('orgId')}</DescriptionItem>
       <DescriptionItem title="Space Id">{data.get('spaceId')}</DescriptionItem>
-
-      {ports && ports.size > 0 ? (
-        <DescriptionItem title="Mapped Ports">
-          <List>
-            {ports.map(port => (
-              <List.Item key={port}>{port}</List.Item>
-            ))}
-          </List>
-        </DescriptionItem>
-      ) : null}
-
-      {events && events.size > 0 ? (
-        <DescriptionItem title="Events">
-          <List>
-            {events.map(event => (
-              <List.Item key={event}>{event}</List.Item>
-            ))}
-          </List>
-        </DescriptionItem>
-      ) : null}
-
-      {processIds && processIds.size > 0 ? (
-        <DescriptionItem title="Process IDs">
-          <List>
-            {processIds.map(processID => (
-              <List.Item key={processID}>{processID}</List.Item>
-            ))}
-          </List>
-        </DescriptionItem>
-      ) : null}
     </DescriptionList>
   );
 }
