4180b2d28e2508a54e6524d4804d194324018f9d
Ben Ripkens <bripkens@gmail.com>
2019-10-02 13:52:25 +0200

    Support additional status codes

--- packages/in-stores/releaseNotes.js
+++ packages/in-stores/releaseNotes.js
@@ -103,13 +103,13 @@ function retrieveLatestReleaseNotes() {
       url: `${releaseNotesBaseUrl}/index.json?cacheBust=${Date.now()}`,
       responseType: 'text'
     });
-    indexObservable.once(indexResponse => {
-      processReleaseNotesIndex(indexResponse, currentlyRunningVersionMajorMinor);
-    });
-    indexObservable.errors().once(() => {
+    indexObservable.once(
+      indexResponse => {
+        processReleaseNotesIndex(indexResponse, currentlyRunningVersionMajorMinor);
+      },
       // ignore HTTP errors silently and try again later
-      tryFetchingReleaseNotesLater();
-    });
+      tryFetchingReleaseNotesLater
+    );
   });
 }
 
@@ -141,11 +141,11 @@ function processReleaseNotesIndex(indexResponse, currentlyRunningVersionMajorMin
       responseType: 'text'
     });
 
-    releaseNotesObservable.once(processReleaseNotesMarkdown);
-    releaseNotesObservable.errors().once(() => {
+    releaseNotesObservable.once(
+      processReleaseNotesMarkdown,
       // ignore HTTP errors silently and try again later
-      tryFetchingReleaseNotesLater();
-    });
+      tryFetchingReleaseNotesLater
+    );
   } catch (e) {
     logger.warn('Could not parse release notes index.');
     tryFetchingReleaseNotesLater();
--- packages/in-websites/definitions/stackTraceLineTranslationStatus.js
+++ packages/in-websites/definitions/stackTraceLineTranslationStatus.js
@@ -70,5 +70,16 @@ export const status = {
       'Could not find original mapping for a combination of line and column number via the source map. This can happen after releases for users that are running outdated versions of your JavaScript files.',
     shouldShowExplanation: true,
     linkToConfigurationDialog: false
+  },
+  15: {
+    explanation: 'Translation is only possible when a reference to a line number is included in the stack trace line.',
+    shouldShowExplanation: true,
+    linkToConfigurationDialog: false
+  },
+  16: {
+    explanation:
+      "This stack trace line's file reference does not point to a JavaScript file. Source map based stack trace line translation only works when the source is a JavaScript file.",
+    shouldShowExplanation: true,
+    linkToConfigurationDialog: false
   }
 };
