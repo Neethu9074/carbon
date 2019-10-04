7a095f1260e4c3b80102a4d29a8f562496a62a03
Ben Ripkens <bripkens@gmail.com>
2019-10-04 09:52:09 +0200

    No longer show the unhandled error popup. We have better ways to learn about these nowadays

--- packages/in-services/unhandledErrors.js
+++ packages/in-services/unhandledErrors.js
@@ -2,10 +2,7 @@ import { setUnhandledErrorHandler } from 'reactive-observables';
 import { createLogger } from 'instalog';
 import { get } from 'lodash';
 
-import { addMessage } from 'in-components/MessageFlyout/stores/messages';
-import { isInstanaEngineer } from 'in-stores/user';
 import { ineum } from 'in-services/tracking/ineum';
-import { config } from 'in-services/config';
 
 const unhandledLogger = createLogger('in-services/unhandledErrors');
 
@@ -20,10 +17,6 @@ export function init() {
     ineum('reportError', e);
 
     unhandledLogger.error(`Unhandled error in observable chain: ${e.message}`, e);
-
-    if (isInstanaEngineer && !(config.tenant === 'instana' && config.tenantUnit === 'current')) {
-      showUnhandledErrorMessage(e);
-    }
   });
 
   window.addEventListener('error', onUnhandledError, false);
@@ -37,25 +30,4 @@ function onUnhandledError(e) {
   } else {
     unhandledLogger.error(`Unhandled error: ${e.message} at ${e.filename}:${e.lineno}`, e.error);
   }
-
-  if (isInstanaEngineer) {
-    showUnhandledErrorMessage(e);
-  }
-}
-
-function showUnhandledErrorMessage(e) {
-  // decouple from any existing pending React updates to ensure that
-  // this message makes it to the user.
-  setTimeout(() => {
-    addMessage(
-      {
-        type: 'error',
-        title: 'This message is only visible for Instana engineers!',
-        content: `An unhandled error occured. Please report this error and how you produced it. Error message: ${
-          e.message
-        }`
-      },
-      'unhandled-error'
-    );
-  }, 0);
 }
