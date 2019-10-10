1d871a35685dfea684932382d0f32419d35c5946
alxbxbx <aleksandar.stankovic@instana.com>
2019-10-10 15:37:01 +0200

    Fix route for Datacenter Dashboard

--- packages/in-vsphere/navigation/routes.js
+++ packages/in-vsphere/navigation/routes.js
@@ -1,7 +1,7 @@
 import { Route } from 'react-router-dom';
 import React, { Fragment } from 'react';
 
-import { DatacenterDashboard } from 'promise-loader?global,vsphere!in-vsphere/Dashboards/DatacenterDashboard';
+import { DatacenterDashboard } from 'promise-loader?global,vsphere!in-vsphere/Dashboards/Datacenter/DatacenterDashboard';
 import VSphereMainView from 'promise-loader?global,vsphere!in-vsphere/VSphereMainView';
 import { createAsyncViewComponent } from 'in-components/routing/createAsyncComponent';
 import { datacenterDashboardFullyQualified } from 'in-vsphere/navigation/paths';
