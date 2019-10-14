4e2b46b9b9f47b90a80fbdf2c2094558ad482974
Igor Milanovic <igor.milanovic@instana.com>
2019-10-14 14:50:59 +0200

    Add snapshot data for Vsphere datacenter entity.

--- packages/in-vsphere/Dashboards/Datacenter/tabs/Summary.js
+++ packages/in-vsphere/Dashboards/Datacenter/tabs/Summary.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 
 import { twoDecimalPlaces, bytesTwoDecimalPlaces, percentage } from 'in-services/formatters/number';
+import EntityHealthIndicator from 'in-new-components/EntityHealthIndicator/EntityHealthIndicator';
+import HealthIndicatorPresenter from 'in-new-components/health/HealthIndicatorPresenter';
 import { valueMissingPlaceholder } from 'in-new-components/valueMissingPlaceholder';
 import InfraMetricKpiCard from 'in-new-components/KpiCard/InfraMetricKpiCard';
 import Chart from 'in-components/Chart/InfrastructureMetricChartBehavior';
@@ -17,12 +19,21 @@ export default function Summary({ timeConfig, data: cluster }) {
 
   return (
     <Fragment>
-      <KpiGridRow sizes={[6, 6]}>
+      <KpiGridRow sizes={[3, 3, 3, 3]}>
         <KpiCard
-          title="Status"
-          value={<Capitalize>{cluster.status || valueMissingPlaceholder}</Capitalize>}
+          title="Overall Status"
+          value={<Capitalize>{cluster.overallStatus || valueMissingPlaceholder}</Capitalize>}
           raw
           borderless
+        />
+        <KpiCard title="ESXi hosts" value={cluster.hosts} raw borderless />
+        <KpiCard title="Virtual machines" value={cluster.vms} raw borderless />
+        <EntityHealthIndicator
+          openIssues={cluster.entityHealthInfo.openIssues.length}
+          maxSeverity={cluster.entityHealthInfo.maxSeverity}
+          IndicatorPresenter={HealthIndicatorPresenter}
+          timeConfig={timeConfig}
+          snapshotId={snapshotId}
         />
       </KpiGridRow>
       <Row>
