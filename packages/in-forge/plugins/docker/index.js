0b336d6217909a33c7f439e62faef02a3bf7066f
eivansavic <eivansavic@gmail.com>
2019-10-14 08:03:49 +0200

    Logging integration - Add check for kubernetes

--- packages/in-forge/plugins/docker/index.js
+++ packages/in-forge/plugins/docker/index.js
@@ -6,6 +6,7 @@ import LoggingIntegrationButtons from 'in-integrations/logging/LoggingIntegratio
 import { addMaxValueLocator, addFormattedValueLocator } from 'in-sdk/metrics';
 import ContainerInfoButton from 'in-forge/plugins/docker/ContainerInfoButton';
 import tableDefinition from 'in-forge/plugins/docker/tableDefinition';
+import { isWithinKubernetes } from 'in-forge/plugins/docker/util';
 import { containerInfoEnabled } from 'in-services/featureFlags';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
 import { hasRestrictedAccess } from 'in-stores/permission';
@@ -35,7 +36,11 @@ registerSnapshotDefinition({
   DashboardHeaderActions({ snapshot, timeConfig }) {
     return (
       <Fragment>
-        <LoggingIntegrationButtons dockerContainerId={snapshot.getIn(['data', 'Id'])} timeConfig={timeConfig} />
+        <LoggingIntegrationButtons
+          isWithinKubernetes={isWithinKubernetes(snapshot)}
+          dockerContainerId={snapshot.getIn(['data', 'Id'])}
+          timeConfig={timeConfig}
+        />
         {containerInfoEnabled && !hasRestrictedAccess && <ContainerInfoButton snapshot={snapshot} />}
       </Fragment>
     );
