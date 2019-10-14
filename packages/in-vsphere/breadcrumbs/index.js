a8555e2ffa627043b14831e4dbf37ab19b6f85b1
alxbxbx <aleksandar.stankovic@instana.com>
2019-10-14 11:28:26 +0200

    Typo fix

--- packages/in-vsphere/breadcrumbs/index.js
+++ packages/in-vsphere/breadcrumbs/index.js
@@ -4,6 +4,6 @@ import DatacenterBreadcrumb from 'in-vsphere/breadcrumbs/DatacenterBreadcrumb';
 import HomeViewBreadcrumb from 'in-vsphere/breadcrumbs/HomeViewBreadcrumb';
 
 export function DatacenterBreadcrumbs(props) {
-  const { applicationId } = props;
-  return [<HomeViewBreadcrumb />, applicationId && <DatacenterBreadcrumb {...props} />];
+  const { datacenterId } = props;
+  return [<HomeViewBreadcrumb />, datacenterId && <DatacenterBreadcrumb {...props} />];
 }
