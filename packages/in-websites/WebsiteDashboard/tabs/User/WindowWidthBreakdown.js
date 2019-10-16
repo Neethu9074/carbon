74bf983bd7cc03f4ac682cb0062a61378ca63d1d
Ben Ripkens <bripkens@gmail.com>
2019-10-16 11:25:57 +0200

    Also support user breakdown

--- packages/in-websites/WebsiteDashboard/tabs/User/WindowWidthBreakdown.js
+++ packages/in-websites/WebsiteDashboard/tabs/User/WindowWidthBreakdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { translateDemocratisationTagFiltersToAnalyzeTagFilters } from 'in-websites/tags';
 import getWindowWidthBreakdown from 'in-websites/subscriptions/getWindowWidthBreakdown';
@@ -11,13 +11,15 @@ import Link from 'in-components/Link';
 
 // Sizes and labels taken from the Chrome developer tools
 const sizes = {
-  '4k': 2560,
-  'Laptop (L)': 1440,
-  Laptop: 1024,
-  Tablet: 768,
-  'Mobile (L)': 425,
-  'Mobile (M)': 375,
-  'Mobile (S)': 320
+  '<= 320px': 320,
+  '<= 375px': 375,
+  '<= 425px': 425,
+  '<= 768px': 768,
+  '<= 1440px': 1440,
+  '<= 1650px': 1650,
+  '<= 1920px': 1920,
+  '<= 2560px': 2560,
+  '> 2560px': 2561
 };
 
 export default connectTo(({ timeConfig, tagFilters }) => ({
@@ -27,18 +29,20 @@ export default connectTo(({ timeConfig, tagFilters }) => ({
     windowWidths: Object.keys(sizes).map(label => sizes[label])
   })
 }))(function WindowWidthBreakdown({ result, tagFilters, websiteLabel }) {
+  const [selectedMetric, onChangeMetric] = useState('pageLoads');
+
   return (
     <TopListCardPresenter
       result={result}
       title="Browser Window Width Breakdown"
-      metrics={['Page Loads']}
-      labels={['Page Loads']}
-      onChangeMetric={() => {}}
-      selectedMetric="Page Loads"
-      showMetricSelectorsForSingleMetrics
+      metrics={['pageLoads', 'users']}
+      labels={['Page Loads', 'Users']}
+      selectedMetric={selectedMetric}
+      onChangeMetric={onChangeMetric}
       getItemsFromResult={r => r.data}
       renderLabel={Label}
-      renderMetric={({ item }) => number.compact(item.pageLoads)}
+      getMetricValueFromItem={(selectedMetric, item) => item[selectedMetric]}
+      selectedMetricFormatter={number.compact}
       tagFilters={tagFilters}
       websiteLabel={websiteLabel}
     />
