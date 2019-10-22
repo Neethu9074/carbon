05a6dd1617e8e14a4ae3cb8f0db99bdc77eacfcb
Simon Thiele <simon.thiele.st@googlemail.com>
2019-10-22 15:43:58 +0200

    Add users email and ToS infos to appcues (#2097)
    * add users email and allSupportAndResearchServices to appcues when the user has accepted the terms and privacies
    
    * only send the email to appcues when the user accepted the help and services

--- packages/in-services/tracking/appcues.js
+++ packages/in-services/tracking/appcues.js
@@ -1,3 +1,5 @@
+import { get } from 'lodash';
+
 import { navigationParameters$ } from 'in-stores/navigation/navigation';
 import { config } from 'in-services/config';
 import { user, role } from 'in-stores/user';
@@ -48,6 +50,20 @@ function onAppcuesLoaded() {
 function identify() {
   if (!Appcues) {
     return;
+  }
+
+  // We are only able and allowed to add user sensitive data like the email when the user accepted the ToS.
+  const accepted = window.instana.termsAndPrivacyAccepted;
+  if (accepted) {
+    const allSupportAndResearchServicesAllowed = get(
+      window.instana,
+      ['termsAndPrivacySettings', 'allSupportAndResearchServices'],
+      false
+    );
+    extraTargetingProperties.allSupportAndResearchServicesAllowed = allSupportAndResearchServicesAllowed;
+    if (allSupportAndResearchServicesAllowed === true) {
+      extraTargetingProperties.email = user.email;
+    }
   }
 
   const targetingProperties = {
