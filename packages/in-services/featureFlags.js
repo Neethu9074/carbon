6e4266bc667b6a10128a9dc49495de32a91b5a6d
alxbxbx <aleksandar.stankovic@instana.com>
2019-10-29 09:27:11 +0100

    Remove unused icons

--- packages/in-services/featureFlags.js
+++ packages/in-services/featureFlags.js
@@ -45,7 +45,7 @@ export const urlShortenerEnabled = isFeatureFlagEnabled('urlShortenerEnabled');
 export const websiteUserBreakdownEnabled = isFeatureFlagEnabled('websiteUserBreakdownEnabled');
 export const optInOptOutEnabled = isFeatureFlagEnabled('optInOptOutEnabled', true);
 export const isOnPrem = isFeatureFlagEnabled('isOnPrem', false);
-export const vsphereEnabled = isFeatureFlagEnabled('vsphereEnabled', true);
+export const vsphereEnabled = isFeatureFlagEnabled('vsphereEnabled', false);
 
 // ########################################################################################
 // Dynamic focus keywords
