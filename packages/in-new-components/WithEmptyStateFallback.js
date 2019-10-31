cd531dae080eed2f08a6f45916a4b63c1e9be5cd
simonThiele <simon.thiele.st@gmail.com>
2019-10-31 10:47:59 +0100

    only parse type to the fallback

--- packages/in-new-components/WithEmptyStateFallback.js
+++ packages/in-new-components/WithEmptyStateFallback.js
@@ -34,7 +34,7 @@ export default connectTo(
 
     const content = FallbackComponent ? (
       typeof FallbackComponent === 'function' ? (
-        <FallbackComponent notFoundComponent={notFoundComponent} {...props} {...fallbackComponentProps} />
+        <FallbackComponent notFoundComponent={notFoundComponent} type={props.type} {...fallbackComponentProps} />
       ) : (
         FallbackComponent
       )
