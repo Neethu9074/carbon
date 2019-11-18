5d28a6f079ec6ce45a1218837bbeca266e913517
Simon Thiele <simon.thiele.st@googlemail.com>
2019-11-18 20:35:17 +0100

    Reduced infra view structure

---
+++ packages/in-subscription/reducedView.js
@@ -0,0 +1,30 @@
+import createSubscription from 'in-subscription/subscription';
+
+export default createSubscription({
+  eventId: 'subscribe-reduced-view',
+
+  transform(observable) {
+    return observable.map(translateProps);
+  }
+});
+
+function translateProps(viewComponent) {
+  if (viewComponent.c) {
+    viewComponent.children = viewComponent.c.map(translateProps);
+  } else {
+    viewComponent.children = [];
+  }
+
+  if (viewComponent.cl) {
+    viewComponent.metadata = { 'container.label': viewComponent.cl };
+  }
+
+  if (viewComponent.s) {
+    viewComponent.snapshotPreview = viewComponent.s;
+    viewComponent.id = viewComponent.s.id;
+  }
+
+  viewComponent.healthInfo = viewComponent.h || null;
+
+  return viewComponent;
+}
