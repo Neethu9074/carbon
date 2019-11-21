cc387d703a68f7a8aad2bc569a03f4d797ce2f8f
Miel Donkers <miel.donkers@instana.com>
2019-11-21 16:12:55 +0100

    Expose inbound calls metrics for custom events (#2269)

--- packages/in-forge/plugins/defaultEntity20/metricDefinitions.js
+++ packages/in-forge/plugins/defaultEntity20/metricDefinitions.js
@@ -3,30 +3,37 @@ import { ms, number, percentage } from 'in-services/formatters/number';
 export default [
   {
     metric: 'count',
-    label: 'Calls/s',
-    category: [],
+    label: 'All Calls/s',
+    category: ['All Calls'],
     min: 0,
     formatter: number
   },
   {
     metrics: ['duration.mean', 'duration.min', 'duration.max'],
-    labels: ['Avg. Latency', 'Min Latency', 'Max Latency'],
-    category: ['Latency'],
+    labels: ['All Calls Avg. Latency', 'All Calls Min Latency', 'All Calls Max Latency'],
+    category: ['All Calls Latency'],
     min: 0,
     formatter: ms
   },
   {
     metrics: ['duration.25th', 'duration.50th', 'duration.75th', 'duration.95th', 'duration.98th', 'duration.99th'],
-    labels: ['Latency 25th', 'Latency 50th', 'Latency 75th', 'Latency 95th', 'Latency 98th', 'Latency 99th'],
-    category: ['Latency'],
+    labels: [
+      'All Calls Latency 25th',
+      'All Calls Latency 50th',
+      'All Calls Latency 75th',
+      'All Calls Latency 95th',
+      'All Calls Latency 98th',
+      'All Calls Latency 99th'
+    ],
+    category: ['All Calls Latency'],
     min: 0,
     formatter: ms,
     isPercentile: true
   },
   {
     metric: 'error_rate',
-    label: 'Error Rate',
-    category: [],
+    label: 'All Calls Error Rate',
+    category: ['All Calls'],
     min: 0,
     formatter: percentage
   }
