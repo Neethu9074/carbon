a35378c07dcc3e0ce34f0e943311112f06461d1a
Yiquan Zhou <yiquan.zhou@instana.com>
2019-11-22 14:09:22 +0100

    clean up application tag blacklist as some of them are removed

--- packages/in-applications/tags.js
+++ packages/in-applications/tags.js
@@ -51,14 +51,10 @@ export const callAnalysisBlacklistedTags = [
 const blacklists = {
   generalBlacklist: (() => {
     const blacklist = {
-      'application.id': true,
-      'source.application.id': true,
-      'service.id': true,
-      'source.service.id': !isInstanaEngineer,
-      'endpoint.id': true,
-      'source.endpoint.id': true,
-      'source.endpoint.name': !isInstanaEngineer,
-      'source.endpoint.type': true,
+      'application.id': !isInstanaEngineer,
+      'service.id': !isInstanaEngineer,
+      'service.rule_id': !isInstanaEngineer,
+      'endpoint.id': !isInstanaEngineer,
       'process.id': true,
       'docker.container.id': true,
       'host.snapshotId': !isInstanaEngineer,
@@ -68,8 +64,6 @@ const blacklists = {
       'cloud.snapshotId': !isInstanaEngineer,
       'call.span_type': !isInstanaEngineer,
       'call.processing_errors': !isInstanaEngineer,
-      'service.rule_id': !isInstanaEngineer,
-      'source.service.rule_id': !isInstanaEngineer,
       'call.http.hostCapturedFromSource': !isInstanaEngineer,
       'call.meta_tags': !isInstanaEngineer
     };
