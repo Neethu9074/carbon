04bfc6db846924ef5ab506bdde22a404278e5388
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-12-02 18:51:08 +0100

    Add SQL display to Couchbase span types (#2316)

--- packages/in-forge/tracing/couchbase/CouchbaseSpanDetailView.js
+++ packages/in-forge/tracing/couchbase/CouchbaseSpanDetailView.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import ErrorDescriptionItem from 'in-sdk/components/traceDetails/ErrorDescriptionItem';
 import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
+import Code from 'in-sdk/components/traceDetails/Code';
+import { formatSql } from 'in-forge/tracing/jdbc/sql';
 
 export default function CouchbaseSpanDetailView({ span }) {
+  const sql = span.getIn(['data', 'couchbase', 'sql']);
+
   return (
     <div>
       <Dl>
@@ -12,6 +16,12 @@ export default function CouchbaseSpanDetailView({ span }) {
         <Di title="Type">{span.getIn(['data', 'couchbase', 'type'])}</Di>
         <ErrorDescriptionItem error={span.getIn(['data', 'couchbase', 'error'])} />
         <Di title="Error Code">{span.getIn(['data', 'couchbase', 'error_code'])}</Di>
+
+        {sql ? (
+          <Di title="SQL" verticalDisplay>
+            <Code code={formatSql(sql)} lang="sql" showLineNumbers={false} />
+          </Di>
+        ) : null}
       </Dl>
     </div>
   );
