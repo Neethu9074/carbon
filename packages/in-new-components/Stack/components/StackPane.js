22de9a492c6c28e997d4dc4e96f36b4b40816e45
Marissa Amaya <mamaya.dev@gmail.com>
2019-12-04 13:53:03 -0600

    add scrollhints

--- packages/in-new-components/Stack/components/StackPane.js
+++ packages/in-new-components/Stack/components/StackPane.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 import StackGroup from 'in-new-components/Stack/components/StackGroup';
+import ScrollHints from 'in-components/ScrollHints';
+
+import locals from './StackPane.mless';
 
 export default function StackPane({ groups }) {
   if (!groups.length) {
     return <div style={{ padding: '1.5em' }}>No Data to Display</div>;
   }
 
-  return groups.map(group => <StackGroup key={group.relationship} group={group} />);
+  return (
+    <ScrollHints className={locals.pane} contentChangeMarker={groups.length}>
+      {groups.map(group => (
+        <StackGroup key={group.relationship} group={group} />
+      ))}
+    </ScrollHints>
+  );
 }
