ecfd25eb949755d5c03af6c858ff98c971bce394
Jean Detoeuf <jean.detoeuf@instana.com>
2019-12-09 14:38:58 +0100

    wording for overriding scope

--- packages/in-applications/constants.js
+++ packages/in-applications/constants.js
@@ -9,14 +9,15 @@ export const boundaryScopes = {
       icon: 'lib_application_boundary_inbound_calls',
       dashboard:
         'The dashboard is displaying information based only on the calls that are performed by the consumers of this application.',
-      overrideDefault: 'The default for this application is Inbound Calls only. You can change this in Configuration.'
+      overrideDefault:
+        'The default scope for this application is Inbound Calls only. You can change this in Configuration.'
     },
     ALL: {
       text: 'All Calls',
       icon: 'lib_application_boundary_all_calls',
       dashboard:
         'The dashboard is displaying information based on all calls that are performed within this application, by both consumers as well as internally.',
-      overrideDefault: 'The default for this application is All Calls. You can change this in Configuration.'
+      overrideDefault: 'The default scope for this application is All Calls. You can change this in Configuration.'
     }
   }
 };
