9305eba14ddcd1fd925a90d3eb91c30d380c2ef6
Marco Schaefer <47627413+codecapitano@users.noreply.github.com>
2019-12-09 16:35:18 +0100

    Filter bar: make filter icon and box-shadow removeable (#2332)
    * Add option to remove box-shadow
    
    * remove filters icon
    
    * Fix eum alerting dialog and website sites
    
    * Allow the Stack button (for Context Guide) to be an internally visible feature (or a tenant-unit specific feature). (#2335)
    
    This is so we can view the Context Guide on customer environments without needing to turn on the tenant-unit specific feature flag.
    
    * hide filter icon

--- storybook/stories/analyse/FilterBar.story.js
+++ storybook/stories/analyse/FilterBar.story.js
@@ -15,6 +15,7 @@ import Bar from 'in-analyze/components/filterBar/Bar/Bar';
 storiesOf('Analyse/FilterBar', module)
   .addDecorator(withKnobs)
   .add('Default', () => <Default />)
+  .add('Without shadow/filters icon', () => <WithoutShadow />)
   .add('Overlay', () => <Overlay />)
   .add('SelectOverlay', () => <SelectBarOverlayStory />)
   .add('NumberOverlay', () => <NumberBarOverlayStory />)
@@ -23,6 +24,23 @@ storiesOf('Analyse/FilterBar', module)
 function Default() {
   return (
     <Bar onClearFilters={action('onClearFilters')}>
+      <BarItem showArrow onClick={action('onBarItemClick')}>
+        By Duration
+      </BarItem>
+      <BarItem showArrow active isOpen onClick={action('onBarItemClick')}>
+        By Meta
+      </BarItem>
+      <BarItem onClick={action('onBarItemClick')}>Erroneous</BarItem>
+      <BarItem active onClick={action('onBarItemClick')}>
+        Synthetic
+      </BarItem>
+    </Bar>
+  );
+}
+
+function WithoutShadow() {
+  return (
+    <Bar onClearFilters={action('onClearFilters')} removeShadow removeFiltersIcon>
       <BarItem showArrow onClick={action('onBarItemClick')}>
         By Duration
       </BarItem>
