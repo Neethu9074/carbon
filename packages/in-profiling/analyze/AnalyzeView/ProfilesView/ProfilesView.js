f483451c0b1042b27d7b7f7473ed16bff6acd23d
simonThiele <simon.thiele.st@gmail.com>
2019-12-10 15:18:06 +0100

    remove breadcrumbs from profile view header
    (cherry picked from commit 9a3f4a6b01a153e754cdea8525abe17162a70ec6)

--- packages/in-profiling/analyze/AnalyzeView/ProfilesView/ProfilesView.js
+++ packages/in-profiling/analyze/AnalyzeView/ProfilesView/ProfilesView.js
@@ -4,23 +4,17 @@ import { compose } from 'recompose';
 import { getTagFilterListForBackendSubscription } from 'in-analyze/applicationFilter';
 import { processIdUrlParameter } from 'in-profiling/navigation/urlParameters';
 import { getDashboardLink } from 'in-stores/navigation/paths/dashboardPaths';
-import Breadcrumbs from 'in-sdk/components/dashboard/breadcrumb/Breadcrumbs';
-import BreadcrumbHeader from 'in-components/breadcrumb/BreadcrumbHeader';
 import { closeProfilesViewLink } from 'in-profiling/navigation/paths';
 import tabs from 'in-profiling/analyze/AnalyzeView/ProfilesView/tabs';
 import TabView from 'in-new-components/LocationAwareTabView/TabView';
 import getProfiles from 'in-profiling/subscriptions/getProfiles';
 import DashboardHeader from 'in-new-components/DashboardHeader';
-import Breadcrumb from 'in-components/breadcrumb/Breadcrumb';
 import { generateUniqueShortId } from 'in-services/util/id';
 import { isEntityOnline } from 'in-stores/snapshot';
 import { getSnapshot } from 'in-stores/snapshot';
 import withUrlState from 'in-hoc/withUrlState';
 import Button from 'in-new-components/Button';
-import SvgIcon from 'in-components/SvgIcon';
-import Tooltip from 'in-components/Tooltip';
 import { getLabel } from 'in-sdk/snapshot';
-import Sticky from 'in-components/Sticky';
 import connect from 'in-hoc/connectTo';
 import Link from 'in-components/Link';
 
@@ -48,32 +42,28 @@ function ProfilesView(props) {
   const { processSnapshot, isOnline, processId, timeConfig, tagFilters, location } = props;
 
   return (
-    <>
-      <Sticky header={<BreadcrumbHeader />}>
-        <TabView
-          // Discard all state when the process ID changes
-          key={processId}
-          HeaderComponent={Header}
-          tabs={tabs}
-          location={location}
-          result$={getProfiles({
-            processSnapshotId: processId,
-            tagFilters: getTagFilterListForBackendSubscription(tagFilters),
-            filter: { timeConfig }
-          }).map(addUniqueIdToProfilesIfPresent)}
-          withProps={({ result }) => ({
-            viewType,
-            setViewType,
-            profiles: result.data,
-            processSnapshot,
-            isOnline
-          })}
-          props={props}
-          withoutBreadcrumb
-          withoutPadding
-        />
-      </Sticky>
-    </>
+    <TabView
+      // Discard all state when the process ID changes
+      key={processId}
+      HeaderComponent={Header}
+      tabs={tabs}
+      location={location}
+      result$={getProfiles({
+        processSnapshotId: processId,
+        tagFilters: getTagFilterListForBackendSubscription(tagFilters),
+        filter: { timeConfig }
+      }).map(addUniqueIdToProfilesIfPresent)}
+      withProps={({ result }) => ({
+        viewType,
+        setViewType,
+        profiles: result.data,
+        processSnapshot,
+        isOnline
+      })}
+      props={props}
+      withoutBreadcrumb
+      withoutPadding
+    />
   );
 }
 
@@ -81,36 +71,31 @@ function Header(props) {
   const label = `Profiles of Process ${props.processSnapshot ? getLabel(props.processSnapshot) : ''}`;
 
   return (
-    <>
-      <Breadcrumbs
-        items={[
-          <Breadcrumb label="Analyze profiles" href$={closeProfilesViewLink} />,
-          props.result.data && <Breadcrumb label={label} />
-        ].filter(Boolean)}
-      />
-      <DashboardHeader
-        {...props}
-        title="Profiles of Process"
-        icon="lib_profiling"
-        label={label}
-        renderButtonLine={renderButtonLine}
-      />
-    </>
+    <DashboardHeader
+      {...props}
+      title="Profiles of Process"
+      icon="lib_profiling"
+      label={label}
+      renderButtonLine={renderButtonLine}
+      contextIcon="lib_profiling"
+      renderContext={renderContext}
+    />
   );
 }
 
 function renderButtonLine({ processId }) {
   return (
-    <>
-      <Button kind="secondary" href$={getDashboardLink(processId, { pathname: '/physical/dashboard' })}>
-        View Infrastructure
-      </Button>
-      <Link href$={closeProfilesViewLink}>
-        <Tooltip content="Close process details">
-          <SvgIcon className={locals.closeIcon} aria-label="Close process details" type="lib_openclose_cancel" />
-        </Tooltip>
-      </Link>
-    </>
+    <Button kind="secondary" href$={getDashboardLink(processId, { pathname: '/physical/dashboard' })}>
+      View Infrastructure
+    </Button>
+  );
+}
+
+function renderContext() {
+  return (
+    <Link className={locals.contextLink} href$={closeProfilesViewLink}>
+      Analyze profiles
+    </Link>
   );
 }
 
