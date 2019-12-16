85840797e56c1df10bdae23812d56e62beb9676a
Ben Blackmore <ben@blackmore.io>
2019-12-16 13:05:21 +0100

    Restructure mobile app & website entry points according to design

--- packages/in-websites/WebsitesList/components/ViewSwitcher.js
+++ packages/in-websites/WebsitesList/components/ViewSwitcher.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import DashboardHeaderShadowModule from 'in-new-components/DashboardHeader/DashboardHeaderShadowModule';
+import ViewSwitcherTabs from 'in-websites/WebsitesList/components/ViewSwitcherTabs';
 import DashboardHeader from 'in-new-components/DashboardHeader';
 
 export default function KubernetesViewSwitcher() {
   return (
     <>
       <DashboardHeader icon="lib_website_inverted" label="Websites" title="Websites" />
+      <ViewSwitcherTabs isWebsites />
       <DashboardHeaderShadowModule />
     </>
   );
