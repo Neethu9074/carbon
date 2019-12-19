a2461b004e1bbf0f9df7a4e1081f350899bc2c3c
simonThiele <simon.thiele.st@gmail.com>
2019-12-19 16:17:00 +0100

    implement shadowmodule in just one module

--- packages/in-new-components/DashboardHeader/DashboardHeaderShadowModule.js
+++ packages/in-new-components/DashboardHeader/DashboardHeaderShadowModule.js
@@ -1,11 +1,9 @@
 import React from 'react';
+
 import DashboardHeaderModule from 'in-new-components/DashboardHeader/DashboardHeaderModule';
+
 import locals from './DashboardHeaderShadowModule.mless';
+
 export default function DashboardHeaderShadowModule() {
-  return (
-    <>
-      <DashboardHeaderModule className={locals.shadowModule} withBottomBorder={false} />
-      <DashboardHeaderModule className={locals.spacingModule} withBottomBorder={false} withTopBorder={false} />
-    </>
-  );
+  return <DashboardHeaderModule className={locals.shadowModule} withBottomBorder={false} />;
 }
