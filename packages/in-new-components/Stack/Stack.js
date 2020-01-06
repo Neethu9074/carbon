5de21c25a6ddbd15c9bede70644750fed3463a0c
Marissa Amaya <mamaya.dev@gmail.com>
2020-01-06 10:44:18 -0600

    show KpiChart (values only)

--- packages/in-new-components/Stack/Stack.js
+++ packages/in-new-components/Stack/Stack.js
@@ -6,7 +6,7 @@ import connectTo from 'in-hoc/connectTo';
 
 export default connectTo(
   ({ id, productArea, timeConfig }) => ({ stackResult: getStack({ id, productArea, timeConfig }) }),
-  function Stack({ stackResult }) {
+  function Stack({ stackResult, activeTabIndex, onTabSelect }) {
     if (!stackResult.data) {
       if (stackResult.errors.length > 0) {
         return <div>Error: {stackResult.errors[0]}</div>;
@@ -15,6 +15,6 @@ export default connectTo(
       return null;
     }
 
-    return <StackPresenter stack={stackResult.data} />;
+    return <StackPresenter stack={stackResult.data} activeTabIndex={activeTabIndex} onTabSelect={onTabSelect} />;
   }
 );
---
+++ packages/in-new-components/Stack/tabs.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'lib_application_invert',
+    text: 'Applications',
+    key: 'application'
+  },
+  {
+    icon: 'lib_infrastructure_inverted',
+    text: 'Infrastructure',
+    key: 'infrastructure'
+  },
+  {
+    icon: 'lib_kubernetes_inverted',
+    text: 'Kubernetes',
+    key: 'kubernetes'
+  }
+];
