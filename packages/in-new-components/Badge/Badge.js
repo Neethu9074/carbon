32beabfd8a13cff9de377b2de8bbfb6af6796d9f
simonThiele <simon.thiele.st@gmail.com>
2020-01-15 08:29:14 +0100

    readd badges

---
+++ packages/in-new-components/Badge/Badge.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { joinClassNames } from 'in-services/util/classnames';
+
+import locals from './Badge.mless';
+
+function Badge({ children, className, color, kind = 'bold' }) {
+  let style;
+  if (kind == 'bold') {
+    style = {
+      background: color
+    };
+  } else if (kind == 'inverted') {
+    style = {
+      color
+    };
+  }
+  return (
+    <span style={style} className={joinClassNames(locals.badge, `${locals[kind]}`, className)}>
+      {children}
+    </span>
+  );
+}
+
+Badge.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  /** Color is background color */
+  color: PropTypes.string,
+  kind: PropTypes.oneOf(['bold', 'inverted'])
+};
+
+Badge.defaultProps = {
+  kind: 'bold'
+};
+
+export default Badge;
