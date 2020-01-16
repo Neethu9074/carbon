0f41ac4d4009e8a98d42c1dfc75c108514799f47
phani-p3 <phani.p3@gmail.com>
2020-01-16 13:24:50 +0100

    Add correct metric for KubernetesPod container count

--- packages/in-forge/plugins/kubernetesPod/metricDefinitions.js
+++ packages/in-forge/plugins/kubernetesPod/metricDefinitions.js
@@ -2,7 +2,7 @@ import { number } from 'in-services/formatters/number';
 
 export default [
   {
-    metrics: ['containers.count', 'cpuRequests', 'cpuLimits', 'memoryRequests', 'memoryLimits', 'restartCount'],
+    metrics: ['container_count', 'cpuRequests', 'cpuLimits', 'memoryRequests', 'memoryLimits', 'restartCount'],
     labels: ['Containers', 'CPU Requests', 'CPU Limits', 'Memory Requests', 'Memory Limits', 'Restarts'],
     min: 0,
     formatter: number
