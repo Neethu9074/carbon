593a95dbb14c080cd18aeab4f595e88ed8c7bda7
Dahlia Bock <dlbock@gmail.com>
2020-01-21 11:38:36 -0600

    Display Stack button on all Kubernetes dashboards. The Kubernetes Service dashboard does not display the entity health indicator, thus cannot use the shared DashboardButtonLine component.

--- packages/in-kubernetes/Dashboards/commonComponents/DashboardButtonLine.js
+++ packages/in-kubernetes/Dashboards/commonComponents/DashboardButtonLine.js
@@ -1,13 +1,19 @@
+import React from 'react';
+
 import HealthIndicatorButtonPresenter from 'in-new-components/health/HealthIndicatorButtonPresenter';
 import EntityHealthIndicator from 'in-new-components/EntityHealthIndicator';
+import StackButton from 'in-new-components/Stack/StackButton';
 
 export default function DashboardButtonLine({ snapshotId, timeConfig }) {
   return (
-    <EntityHealthIndicator
-      showOkayOnNoIssues={false}
-      IndicatorPresenter={HealthIndicatorButtonPresenter}
-      snapshotId={snapshotId}
-      timeConfig={timeConfig}
-    />
+    <>
+      <EntityHealthIndicator
+        showOkayOnNoIssues={false}
+        IndicatorPresenter={HealthIndicatorButtonPresenter}
+        snapshotId={snapshotId}
+        timeConfig={timeConfig}
+      />
+      <StackButton id={snapshotId} timeConfig={timeConfig} />
+    </>
   );
 }
