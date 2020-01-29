ce6627701a4dd73793f4effe1b5c0199fadd570a
Marissa Amaya <mamaya.dev@gmail.com>
2020-01-29 09:27:57 -0600

    add disable/enable scroll effect

--- packages/in-new-components/Stack/StackPresenter.js
+++ packages/in-new-components/Stack/StackPresenter.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { enableBodyScroll, disableBodyScroll } from 'in-components/DisabledBodyScroll';
 import InlineTabNavigation from 'in-new-components/InlineTabNavigation';
 import StackPane from 'in-new-components/Stack/components/StackPane';
 import tabList from 'in-new-components/Stack/tabs';
 export default function StackPresenter({ stack, activeTabIndex, onTabSelect }) {
   const { key } = tabList[activeTabIndex];
+
+  useEffect(() => {
+    disableBodyScroll();
+
+    return enableBodyScroll;
+  });
 
   return (
     <>
