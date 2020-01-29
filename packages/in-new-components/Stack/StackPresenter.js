fc6fc5dfbb2d3eb9023194ba01c5aea3e28a6b57
Marissa Amaya <mamaya.dev@gmail.com>
2020-01-29 09:00:46 -0600

    add new loader

--- packages/in-new-components/Stack/StackPresenter.js
+++ packages/in-new-components/Stack/StackPresenter.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import { IndeterminateLoadingIndicator } from 'in-new-components/LoadingIndicators';
 import InlineTabNavigation from 'in-new-components/InlineTabNavigation';
 import StackPane from 'in-new-components/Stack/components/StackPane';
 import tabList from 'in-new-components/Stack/tabs';
-export default function StackPresenter({ stack, activeTabIndex, onTabSelect }) {
+
+import locals from './StackPresenter.mless';
+export default function StackPresenter({ stack, activeTabIndex, onTabSelect, isLoading }) {
   const { key } = tabList[activeTabIndex];
 
   return (
     <>
       <InlineTabNavigation tabList={tabList} activeTabIndex={activeTabIndex} onTabSelect={onTabSelect} />
-      <StackPane groups={stack[key].groups} area={key} activeTabIndex={activeTabIndex} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <StackPane groups={stack[key].groups} area={key} activeTabIndex={activeTabIndex} isLoading={isLoading} />
+      )}
     </>
   );
 }
+
+const Loader = () => (
+  <div className={locals.pane}>
+    <IndeterminateLoadingIndicator size="96" />
+  </div>
+);
