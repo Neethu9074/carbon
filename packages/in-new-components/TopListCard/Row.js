0b14c04e76ed711520013e61bac632208cc9e3cb
Stepan Herold <stepan.herold@instana.com>
2020-01-29 14:14:25 +0100

    Fixing rendering issue when maxValue is 0 (#2560)
    # Why
    
    TopList bar charts won't get updated when switching between tabs and when all values on the selected tab are 0.
    
    # What
    
    * Prevent division by zero.

--- packages/in-new-components/TopListCard/Row.js
+++ packages/in-new-components/TopListCard/Row.js
@@ -15,7 +15,7 @@ export default function TopListRow(props) {
     renderedMetric,
     renderedContributedItem
   } = props;
-  const percent = Math.min(metricValue / maxValue, 1);
+  const percent = maxValue != 0 ? Math.min(metricValue / maxValue, 1) : 0;
   const positionPercent = `${percent * 100}%`;
   const color = theme.lib.colors.chart.strokeColors100[0];
 
