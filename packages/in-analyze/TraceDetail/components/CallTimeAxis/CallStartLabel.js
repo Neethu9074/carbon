1303adba900a6210130ba66fd7efa0f6832b018d
Miel Donkers <miel.donkers@instana.com>
2020-03-04 15:01:12 +0100

    Show full timestamp for when a trace was started

--- packages/in-analyze/TraceDetail/components/CallTimeAxis/CallStartLabel.js
+++ packages/in-analyze/TraceDetail/components/CallTimeAxis/CallStartLabel.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { getStart } from 'in-analyze/TraceDetail/components/callStartAndEndTime';
 import getElementDimensions from 'in-hoc/getElementDimensions';
 import { joinClassNames } from 'in-services/util/classnames';
-import { formatTime } from 'in-services/formatters/date';
+import { formatDateTime } from 'in-services/formatters/date';
 import SvgIcon from 'in-components/SvgIcon';
 
 import locals from './CallStartLabel.mless';
@@ -14,7 +14,7 @@ export default getElementDimensions(({ startTime, call, className }) => {
   return (
     <span className={joinClassNames(locals.callStartLabel, className)}>
       <SvgIcon className={locals.icon} type="lib_datetime_time" />
-      {`Started: ${formatTime(startTime)}`}
+      {`Started: ${formatDateTime(startTime)}`}
     </span>
   );
 });
