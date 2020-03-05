0e378f0cf6f069d15a05b5f7f175651575cc5ba5
Ben Blackmore <bripkens@gmail.com>
2020-03-05 10:38:45 +0100

    Add support for instana.rocks to our CSP

--- packages/in-server/services/csp.js
+++ packages/in-server/services/csp.js
@@ -1,9 +1,34 @@
-exports.getCsp = nonces => {
-  return (
-    "script-src 'self' " +
-    nonces.map(n => "'nonce-" + n + "'").join(' ') +
-    ' https://cdn.mxpnl.com https://static.zdassets.com https://ekr.zdassets.com https://instana.zendesk.com wss://instana.zendesk.com https://fast.appcues.com *.instana.io'
+const { get } = require('lodash');
+
+const serverConfig = require('../serverConfig.js');
+
+const allowedScriptOrigins = ['*.instana.io'];
+
+if (isRequiringInstanaRocksWhitelisting()) {
+  allowedScriptOrigins.push('*.instana.rocks');
+}
+
+if (serverConfig.mixpanelToken) {
+  allowedScriptOrigins.push('https://cdn.mxpnl.com');
+}
+
+if (serverConfig.appcuesId) {
+  allowedScriptOrigins.push('https://fast.appcues.com');
+}
+
+if (serverConfig.zendeskKey) {
+  allowedScriptOrigins.push(
+    'https://static.zdassets.com',
+    'https://ekr.zdassets.com',
+    'https://instana.zendesk.com',
+    'wss://instana.zendesk.com'
   );
+}
+
+exports.getCsp = nonces => {
+  const stringifiedNonces = nonces.map(n => `'nonce-${n}'`).join(' ');
+  const stringifiedOrigins = allowedScriptOrigins.join(' ');
+  return `script-src 'self' ${stringifiedNonces} ${stringifiedOrigins}`;
 };
 
 exports.findMaxNonces = template => {
@@ -14,3 +39,14 @@ exports.findMaxNonces = template => {
   }
   return 0;
 };
+
+function isRequiringInstanaRocksWhitelisting() {
+  return (
+    isInstanaRocks(get(serverConfig, ['eum', 'domain'])) ||
+    isInstanaRocks(get(serverConfig, ['eum', 'retrievalDomain']))
+  );
+}
+
+function isInstanaRocks(s) {
+  return (s || '').indexOf('.instana.rocks') !== -1;
+}
