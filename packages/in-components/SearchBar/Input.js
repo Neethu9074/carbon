8cd72605e48e2d665ddfae59fcc292b116cea3b7
simonThiele <simon.thiele.st@gmail.com>
2020-03-11 15:08:24 +0100

    restore init behaviour of dfq input

--- packages/in-components/SearchBar/Input.js
+++ packages/in-components/SearchBar/Input.js
@@ -162,6 +162,10 @@ export default getElementDimensions(
         });
 
         editor.on('blur', () => {
+          if (this.editor) {
+            this.updateQuery(trim(this.editor.getValue()));
+          }
+          this.isFocused = false;
           this.state.eventEmitter.emit('blur', true);
         });
 
@@ -169,13 +173,9 @@ export default getElementDimensions(
           .on('blur')
           .throttle(200, { leading: false })
           .subscribe(() => {
-            if (this.editor) {
-              this.updateQuery(trim(this.editor.getValue()));
-            }
             if (!this.isFocused) {
               this.hide();
             }
-            this.isFocused = false;
           });
 
         editor.on('change', (editor, change) => {
