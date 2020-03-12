f359bb15cb1d28f1bbfed463b7864624b1a55186
Ben Blackmore <bripkens@gmail.com>
2020-03-12 15:44:21 +0100

    The default loading dashboard also needs to respect the lightMode prop

--- packages/in-new-components/Loading/DefaultLoadingDashboard/DefaultLoadingDashboard.js
+++ packages/in-new-components/Loading/DefaultLoadingDashboard/DefaultLoadingDashboard.js
@@ -1,32 +1,43 @@
 import React from 'react';
 
 import LeftRightPadding from 'in-components/layout/LeftRightPadding';
+import { evaluateClassNames } from 'in-services/util/classnames';
 import Skeleton from 'in-new-components/Loading/Skeleton';
 import { Row, Col } from 'in-new-components/layout/Grid';
 
 import locals from './DefaultLoadingDashboard.mless';
 
 export default function DefaultLoadingDashboard({ lightMode }) {
+  const kpiClassName = evaluateClassNames({
+    [locals.skeletonKpi]: true,
+    [locals.lightMode]: lightMode
+  });
+
+  const chartClassName = evaluateClassNames({
+    [locals.skeletonChart]: true,
+    [locals.lightMode]: lightMode
+  });
+
   return (
     <LeftRightPadding>
       <Row className={locals.firstRow}>
         <Col lg={4}>
-          <Skeleton lightMode={lightMode} className={locals.skeletonKpi} />
+          <Skeleton lightMode={lightMode} className={kpiClassName} />
         </Col>
         <Col lg={4}>
-          <Skeleton lightMode={lightMode} className={locals.skeletonKpi} />
+          <Skeleton lightMode={lightMode} className={kpiClassName} />
         </Col>
         <Col lg={4}>
-          <Skeleton lightMode={lightMode} className={locals.skeletonKpi} />
+          <Skeleton lightMode={lightMode} className={kpiClassName} />
         </Col>
       </Row>
 
       <Row>
         <Col lg={6}>
-          <Skeleton lightMode={lightMode} className={locals.skeletonChart} />
+          <Skeleton lightMode={lightMode} className={chartClassName} />
         </Col>
         <Col lg={6}>
-          <Skeleton lightMode={lightMode} className={locals.skeletonChart} />
+          <Skeleton lightMode={lightMode} className={chartClassName} />
         </Col>
       </Row>
     </LeftRightPadding>
