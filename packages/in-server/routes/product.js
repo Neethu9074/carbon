6813cb00e1a3eaad93e7b10d937bf540841a7baa
Jean Detoeuf <jean.detoeuf@instana.com>
2020-03-13 08:55:59 -0400

    Use new tag api
    using /api/application-monitoring/catalog/tags instead of deprecated /api/tags

--- packages/in-server/routes/product.js
+++ packages/in-server/routes/product.js
@@ -256,7 +256,7 @@ function getFilterTags(req) {
   return new Promise((resolve, reject) => {
     sendRequest(
       {
-        url: req.uiBackendBaseUrl + '/api/tags',
+        url: req.uiBackendBaseUrl + '/api/application-monitoring/catalog/tags',
         headers: {
           Cookie: `${serverConfig.cookie.name}=${req.cookies[serverConfig.cookie.name]}`
         },
