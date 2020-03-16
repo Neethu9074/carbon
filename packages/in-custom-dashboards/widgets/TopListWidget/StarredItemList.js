a9ca5081dce631c0898468b709722b015297f5c5
simonThiele <simon.thiele.st@gmail.com>
2020-03-16 09:21:39 +0100

    implement starred items in a way it does not uses a reverse state control

--- packages/in-custom-dashboards/widgets/TopListWidget/StarredItemList.js
+++ packages/in-custom-dashboards/widgets/TopListWidget/StarredItemList.js
@@ -1,99 +1,85 @@
-import React, { useState } from 'react';
+import { get } from 'lodash';
+import React from 'react';
 
 import { getUniqueErrors, Error } from 'in-new-components/Errors/ErroneousResultPresenter';
+import WithStarredItems from 'in-custom-dashboards/widgets/TopListWidget/WithStarredItems';
 import { Cell } from 'in-custom-dashboards/widgets/TopListWidget/ItemList';
 import { hasError, isLoading } from 'in-services/util/result';
 import Skeleton from 'in-new-components/Loading/Skeleton';
 import { Ul, Li } from 'in-new-components/lists/List';
-import connectTo from 'in-hoc/connectTo';
 
 import locals from './ItemList.mless';
 
 export default function StarredItemList({ getItem, timeConfig, columnDefinitions, getItemLink, pinnedItemIdsByType }) {
-  const [resolvedMetrics, setResolvedItems] = useState(new Map());
-  const setResolvedItem = (id, item) => {
-    const mainKpiValue = (item && item.mainKpiValue) || 0;
-    if (resolvedMetrics.get(id) === mainKpiValue) {
-      return;
-    }
-    const newMap = new Map(resolvedMetrics);
-    newMap.set(id, mainKpiValue);
-    setResolvedItems(newMap);
-  };
-
-  const items = [];
-  const types = Object.keys(pinnedItemIdsByType);
-  for (let i = 0; i < types.length; i++) {
-    const type = types[i];
-    const ids = pinnedItemIdsByType[type];
-    for (let i2 = 0; i2 < ids.length; i2++) {
-      const id = ids[i2];
-      items.push({ type, id });
-    }
-  }
-
   return (
-    <Ul className={locals.list}>
-      {items.sort((a, b) => resolvedMetrics.get(b.id) - resolvedMetrics.get(a.id)).map((item, rowIndex) => (
-        <Item
-          key={rowIndex}
-          id={item.id}
-          type={item.type}
-          getItem={getItem}
-          timeConfig={timeConfig}
-          getItemLink={getItemLink}
-          setResolvedItem={setResolvedItem}
-          columnDefinitions={columnDefinitions}
-        />
-      ))}
-    </Ul>
+    <WithStarredItems
+      pinnedItemIdsByType={pinnedItemIdsByType}
+      getItem={getItem}
+      timeConfig={timeConfig}
+      render={items =>
+        items && items.length > 0 ? (
+          <Ul className={locals.list}>
+            {items.sort(sort).map(item => (
+              <Item
+                key={item.id}
+                item={item}
+                timeConfig={timeConfig}
+                getItemLink={getItemLink}
+                columnDefinitions={columnDefinitions}
+              />
+            ))}
+          </Ul>
+        ) : null
+      }
+    />
   );
 }
 
-const Item = connectTo(
-  ({ id, type, timeConfig, getItem, setResolvedItem }) => ({
-    result: getItem(id, timeConfig, type).tap(item => setResolvedItem(id, item))
-  }),
+function sort(i1, i2) {
+  const mainKpiValue1 = get(i1, ['result', 'mainKpiValue'], 0);
+  const mainKpiValue2 = get(i2, ['result', 'mainKpiValue'], 0);
 
-  function Item(props) {
-    const { id, type, result, timeConfig, columnDefinitions, getItemLink } = props;
+  return mainKpiValue2 - mainKpiValue1;
+}
 
-    if (!result || isLoading(result)) {
-      const starColumn = columnDefinitions[columnDefinitions.length - 1];
-      return (
-        <Li>
-          <Cell>
-            <Skeleton className={locals.skeleton} />
-          </Cell>
-          <Cell width="1rem" />
-          <Cell width={starColumn.width}>{starColumn.getContent(item, { id, type })}</Cell>
-        </Li>
-      );
-    }
-    if (hasError(result)) {
-      const starColumn = columnDefinitions[columnDefinitions.length - 1];
-      const error = getUniqueErrors(result.errors)[0];
-      return (
-        <Li>
-          <Cell>
-            <Error>{error}</Error>
-          </Cell>
-          <Cell width="1rem" />
-          <Cell width={starColumn.width}>{starColumn.getContent(item, { id, type })}</Cell>
-        </Li>
-      );
-    }
+function Item({ item, timeConfig, getItemLink, columnDefinitions }) {
+  const { id, type, result } = item;
 
-    const item = result.data ? result.data : result;
-
+  if (!result || isLoading(result)) {
+    const starColumn = columnDefinitions[columnDefinitions.length - 1];
     return (
-      <Li className={locals.listItem} href$={getItemLink(item)}>
-        {columnDefinitions.map(({ width, ellipsis, getContent }, i) => (
-          <Cell key={i} width={width} ellipsis={ellipsis}>
-            {getContent(item, { id, type, result, timeConfig })}
-          </Cell>
-        ))}
+      <Li>
+        <Cell>
+          <Skeleton className={locals.skeleton} />
+        </Cell>
+        <Cell width="1rem" />
+        <Cell width={starColumn.width}>{starColumn.getContent(item, { id, type })}</Cell>
       </Li>
     );
   }
-);
+  if (hasError(result)) {
+    const starColumn = columnDefinitions[columnDefinitions.length - 1];
+    const error = getUniqueErrors(result.errors)[0];
+    return (
+      <Li>
+        <Cell>
+          <Error>{error}</Error>
+        </Cell>
+        <Cell width="1rem" />
+        <Cell width={starColumn.width}>{starColumn.getContent(item, { id, type })}</Cell>
+      </Li>
+    );
+  }
+
+  const resolvedItem = result.data ? result.data : result;
+
+  return (
+    <Li className={locals.listItem} href$={getItemLink(resolvedItem)}>
+      {columnDefinitions.map(({ width, ellipsis, getContent }, i) => (
+        <Cell key={i} width={width} ellipsis={ellipsis}>
+          {getContent(resolvedItem, { id, type, result, timeConfig })}
+        </Cell>
+      ))}
+    </Li>
+  );
+}
