878247daf360ceb579003f1986d3bcbdad298b30
Szabolcs Berko <szabolcs.berko@instana.com>
2020-03-16 13:07:55 +0100

    simon sort

--- packages/in-forge/plugins/mySqlDatabase/Dashboard/Content.js
+++ packages/in-forge/plugins/mySqlDatabase/Dashboard/Content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { KpiSection, KpiKeyValue } from 'in-sdk/components/dashboard/KpiSection';
 import DatabasesTable from 'in-forge/plugins/mySqlDatabase/Dashboard/DatabasesTable';
+import { KpiSection, KpiKeyValue } from 'in-sdk/components/dashboard/KpiSection';
 import { isPerformanceDataAvailable } from 'in-forge/plugins/mySqlDatabase/util';
 import DashboardSection from 'in-sdk/components/dashboard/DashboardSection';
 import Chart from 'in-components/Chart/InfrastructureMetricChartBehavior';
