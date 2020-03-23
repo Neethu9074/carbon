d92dd32d24056c63aa3df767158c4d7f2cbd36c6
simonThiele <simon.thiele.st@gmail.com>
2020-03-23 13:26:21 +0100

    remove )

--- packages/in-new-components/MainNavigation/components/ViewSwitcher/ViewSwitcher.js
+++ packages/in-new-components/MainNavigation/components/ViewSwitcher/ViewSwitcher.js
@@ -103,7 +103,8 @@ export default function ViewSwitcher({
       <Infrastructure {...commonProps} />
       <Spacer />
       <Analyze {...commonProps} />
-      <Incidents {...commonProps} />)<Spacer />
+      <Incidents {...commonProps} />
+      <Spacer />
       <View
         id="main-nav-settings"
         label="Settings"
