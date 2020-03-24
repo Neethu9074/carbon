135b2cb336122f09e9f144eba9097b917a2a91e7
Marco Schaefer <marco.schaefer@instana.com>
2020-03-24 11:41:08 +0100

    fix_user-impact-default

--- packages/in-websites/eum-alerting/form/alertDialogFormDefinition.js
+++ packages/in-websites/eum-alerting/form/alertDialogFormDefinition.js
@@ -317,7 +317,7 @@ function createHiddenFieldsForm(timeThreshold, calculateThresholdOnBackend = fal
     .put(
       'alertByNumberOfImpactedUsersEnabled',
       createField({
-        value: !!(timeThreshold && timeThreshold.users !== null)
+        value: !!(timeThreshold && timeThreshold.users)
       })
     )
     .put(
