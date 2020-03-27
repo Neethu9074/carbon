cf11e9d85928550ce534d8899981e9b47b6833e1
Jon Brisbin <jon@jbrisbin.com>
2020-03-27 13:20:06 -0500

    Change + in the label to real item count and a new item for more

--- packages/in-new-components/Stack/components/StackGroup.js
+++ packages/in-new-components/Stack/components/StackGroup.js
@@ -3,21 +3,27 @@ import React from 'react';
 import RELATIONSHIP_MAP from 'in-new-components/Stack/relationships.json';
 import StackItem from 'in-new-components/Stack/components/StackItem';
 import { getSingular, getPlural } from 'in-sdk/pluginName';
-import { Ul } from 'in-new-components/lists/List';
+import { Ul, Li } from 'in-new-components/lists/List';
 
 import locals from './StackGroup.mless';
 
 export default function StackGroup({ group: { relationship, type, items, itemCount }, tab }) {
+  const numMoreItems = itemCount - items.length;
   return (
     <div>
       <div className={locals.groupHead}>
-        {RELATIONSHIP_MAP[relationship]} {itemCount > items.length ? items.length + '+' : itemCount}{' '}
-        {itemCount > 1 ? getPlural(type) : getSingular(type)}
+        {RELATIONSHIP_MAP[relationship]} {itemCount} {itemCount > 1 ? getPlural(type) : getSingular(type)}
       </div>
       <Ul framed="topBottom">
         {items.map(item => (
           <StackItem key={item.id} item={item} tab={tab} />
         ))}
+        ;
+        {numMoreItems > 0 && (
+          <Li className={locals.moreItems} noAlternatingBg>
+            +{numMoreItems} more...
+          </Li>
+        )}
       </Ul>
     </div>
   );
