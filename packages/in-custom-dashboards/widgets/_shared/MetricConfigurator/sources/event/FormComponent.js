ead9a28e40fbeef3c07454a707aa00def49e75f9
Ben Blackmore <bripkens@gmail.com>
2020-04-02 09:38:51 +0200

    Support the same configuration options for the event metric data source as for the other data sources

--- packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/event/FormComponent.js
+++ packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/event/FormComponent.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import TouchedMessages from 'in-components/form/TouchedMessages';
 import { aggregationLabels } from 'in-stores/metric/metric';
+import { Row, Col } from 'in-new-components/layout/Grid';
+import Header from 'in-components/form/Header/Header';
 import FormGroup from 'in-components/form/FormGroup';
 import Select from 'in-components/form/Select';
 import Input from 'in-components/form/Input';
 import Label from 'in-components/form/Label';
 
-export default function FormComponent({ form, onChange, dataSourceFormGroup }) {
+export default function FormComponent({
+  form,
+  onChange,
+  dataSourceFormGroup,
+  labelFormGroup,
+  formatterFormGroup,
+  widgetPreview,
+  timeShiftConfiguration
+}) {
   const metricField = form.get('metric');
   const aggregationField = form.get('aggregation');
   const dynamicFocusQueryField = form.get('dynamicFocusQuery');
@@ -15,37 +25,6 @@ export default function FormComponent({ form, onChange, dataSourceFormGroup }) {
   return (
     <>
       {dataSourceFormGroup}
-
-      <FormGroup>
-        <Label htmlFor="metic-configurator-event-metric">Metric</Label>
-        <Select id="metic-configurator-event-metric" value={metricField.value} disabled>
-          <option value="eventCount">Event Count</option>
-        </Select>
-        <TouchedMessages field={metricField} />
-      </FormGroup>
-
-      <FormGroup>
-        <Label
-          htmlFor="metic-configurator-event-aggregation"
-          hasError={!aggregationField.valid && aggregationField.touched}
-        >
-          Aggregation
-        </Label>
-        <Select id="metic-configurator-event-aggregation" value={aggregationField.value} disabled>
-          {!metricField.valid && <option value="">Please select a metric</option>}
-          {metricField.valid && (
-            <>
-              <option value="">Please select</option>
-              {Object.keys(aggregationLabels).map(aggregation => (
-                <option key={aggregation} value={aggregation}>
-                  {aggregationLabels[aggregation]}
-                </option>
-              ))}
-            </>
-          )}
-        </Select>
-        <TouchedMessages field={aggregationField} />
-      </FormGroup>
 
       <FormGroup>
         <Label
@@ -63,6 +42,57 @@ export default function FormComponent({ form, onChange, dataSourceFormGroup }) {
         />
         <TouchedMessages field={dynamicFocusQueryField} />
       </FormGroup>
+
+      <Header>Customize the widget</Header>
+
+      <Row>
+        <Col lg>
+          <Row>
+            <Col lg>
+              <FormGroup>
+                <Label htmlFor="metic-configurator-event-metric">Metric</Label>
+                <Select id="metic-configurator-event-metric" value={metricField.value} disabled>
+                  <option value="eventCount">Event Count</option>
+                </Select>
+                <TouchedMessages field={metricField} />
+              </FormGroup>
+            </Col>
+
+            <Col lg>
+              <FormGroup>
+                <Label
+                  htmlFor="metic-configurator-event-aggregation"
+                  hasError={!aggregationField.valid && aggregationField.touched}
+                >
+                  Aggregation
+                </Label>
+                <Select id="metic-configurator-event-aggregation" value={aggregationField.value} disabled>
+                  {!metricField.valid && <option value="">Please select a metric</option>}
+                  {metricField.valid && (
+                    <>
+                      <option value="">Please select</option>
+                      {Object.keys(aggregationLabels).map(aggregation => (
+                        <option key={aggregation} value={aggregation}>
+                          {aggregationLabels[aggregation]}
+                        </option>
+                      ))}
+                    </>
+                  )}
+                </Select>
+                <TouchedMessages field={aggregationField} />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          {labelFormGroup}
+
+          {formatterFormGroup}
+
+          {timeShiftConfiguration}
+        </Col>
+
+        {widgetPreview && <Col lg>{widgetPreview}</Col>}
+      </Row>
     </>
   );
 }
