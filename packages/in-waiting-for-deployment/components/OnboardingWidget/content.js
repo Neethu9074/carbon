30821efb2547493cb3574b0820c9e7c4376cafaf
Cedric Ziel <cedric.ziel@instana.com>
2020-04-03 11:21:56 +0200

    Deduplicate type http param for windows agent download

--- packages/in-waiting-for-deployment/components/OnboardingWidget/content.js
+++ packages/in-waiting-for-deployment/components/OnboardingWidget/content.js
@@ -983,9 +983,7 @@ function WindowsInstallerUnattendedContent({ agentKey, agentEndpoint, agentEndpo
       <Description lines={['The latest Windows installer (64Bit) is available at the following address:']} />
       <Script
         lines={[
-          `https://instana.io/assets/agent/${tenant}/${tenantUnit}?agentKey=${toURLstring(agentKey)}&type=${toURLstring(
-            agentKey
-          )}&type=${toURLstring(agentMode === agentModeOptions[0] ? 'exe64' : 'win64offline')}`
+          `https://instana.io/assets/agent/${tenant}/${tenantUnit}?agentKey=${toURLstring(agentKey)}&type=${toURLstring(agentMode === agentModeOptions[0] ? 'exe64' : 'win64offline')}`
         ]}
       />
       <Spacer />
