792cb92ec03ddf92fe64205562b13cf6429f26e1
Marco Schaefer <47627413+codecapitano@users.noreply.github.com>
2020-04-14 12:22:05 +0200

    Make slowness blueprint the first menu item (#2968)

--- packages/in-applications/alerting/data/blueprintConfig.js
+++ packages/in-applications/alerting/data/blueprintConfig.js
@@ -1,17 +1,17 @@
 export const blueprintConfig = Object.freeze([
   {
-    type: 'errorRate',
-    name: 'High Error Rate',
-    headline: 'Error Rate is higher than expected',
-    text:
-      'Receive an alert when the error rate is higher than expected (when compared to your historical data of these services/endpoints)'
-  },
-  {
     type: 'slowness',
     name: 'Slowness',
     headline: 'Latency is higher than expected',
     text:
       'Receive an alert when the latency is higher (your services/endpoints are slower) than expected (from historical data).'
+  },
+  {
+    type: 'errorRate',
+    name: 'High Error Rate',
+    headline: 'Error Rate is higher than expected',
+    text:
+      'Receive an alert when the error rate is higher than expected (when compared to your historical data of these services/endpoints)'
   },
   {
     type: 'logs',
