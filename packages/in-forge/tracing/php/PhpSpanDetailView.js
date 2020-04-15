f70d2504b33cbab6e206af54262752ce58416ce3
Stepan Herold <stepan.herold@instana.com>
2020-04-15 14:51:21 +0200

    Expose php total compile time in span detail (#2979)
    # Why
    
    So far PHP total compile time was displayed as an internal call, which has a couple of issues, see the linked ticket for more details. In future the total compile time should be displayed directly in PHP span details.
    
    # What
    
    - show the total compile time in span details
    
    # References
    
    - https://instana.kanbanize.com/ctrl_board/66/cards/13811/details/

--- packages/in-forge/tracing/php/PhpSpanDetailView.js
+++ packages/in-forge/tracing/php/PhpSpanDetailView.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import { kiloBytesTwoDecimalPlaces, micros } from 'in-services/formatters/number';
 import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
 import { emptyMap } from 'in-services/fixedImmutables';
 import { isBlank } from 'in-services/util/string';
-import { kiloBytesTwoDecimalPlaces } from 'in-services/formatters/number';
 
 export default function HttpSpanDetailView({ span }) {
   const params = span.getIn(['data', 'http', 'params']);
@@ -27,6 +27,10 @@ export default function HttpSpanDetailView({ span }) {
         <Di title="Wordpress Cache Misses">{span.getIn(['data', 'wp', 'cache_misses'])}</Di>
         <Di title="Wordpress Current User">{mapUserId(span.getIn(['data', 'wp', 'user_id']))}</Di>
         <Di title="Peak Memory Usage">{kiloBytesTwoDecimalPlaces(span.getIn(['data', 'php', 'memory']))}</Di>
+        <Di title="Total Compile Time">
+          {span.getIn(['data', 'compile', 'time']) &&
+            micros.fixedCompact(parseInt(span.getIn(['data', 'compile', 'time'])))}
+        </Di>
         {getCustomHeaders(span)}
       </Dl>
     </div>
