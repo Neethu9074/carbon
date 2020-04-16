6848607065ea00e524030aa67a98f3ad1c4eb879
phani-p3 <phani.p3@gmail.com>
2020-04-16 15:54:13 +0200

    fix chart legend

--- packages/in-components/Chart/components/Legend.js
+++ packages/in-components/Chart/components/Legend.js
@@ -49,7 +49,9 @@ function MetricSeries({ chart, axis, config, filteredDataSeries, axisName }) {
       {axis.labels.map((label, i) => {
         const dataSeriesName = `${axisName}-${i}`;
         const isDisabled = filteredDataSeries && filteredDataSeries.has(dataSeriesName);
-        const isToggleable = !axis.nonToggleableSeries || !axis.nonToggleableSeries.has(label);
+        const isToggleable =
+          !axis.nonToggleableSeries ||
+          !(axis.nonToggleableSeries.has(label) || axis.nonToggleableSeries.has(axis.metricIds[i]));
         const timeShift = (axis.timeShifts && axis.timeShifts[i]) || defaultTimeShift;
 
         const content = (
