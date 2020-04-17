448747eff453fef7c22ebabca716cb520a8ff5f4
Miel Donkers <miel.donkers@instana.com>
2020-04-17 16:36:36 +0200

    Update Agent Issues API to resolve snapshots in backend

--- packages/in-forge/plugins/instanaAgent/Dashboard/IssueList.js
+++ packages/in-forge/plugins/instanaAgent/Dashboard/IssueList.js
@@ -1,17 +1,15 @@
-import { compose } from 'recompose';
 import React from 'react';
 
 import getMonitoringIssuesForAgentSnapshot from 'in-subscription/getMonitoringIssuesForAgentSnapshot';
 import getIssueDefinitionForSnapshotAndCode from 'in-sdk/agentMonitoringIssueDefinition';
 import { getDashboardLink } from 'in-stores/navigation/paths/dashboardPaths';
-import { getTimeConfigAtMoment } from 'in-stores/time/config';
 import { formatDateTime } from 'in-services/formatters/date';
 import { compareIgnoreCase } from 'in-services/util/string';
 import { getLabel, getIconSvgPath } from 'in-sdk/snapshot';
+import { emptyList } from 'in-services/fixedImmutables';
 import Table from 'in-sdk/components/dashboard/Table';
-import cursorPaginated from 'in-hoc/cursorPaginated';
-import { getSnapshot } from 'in-stores/snapshot';
 import SvgIcon from 'in-components/SvgIcon';
+import connectTo from 'in-hoc/connectTo';
 import Link from 'in-components/Link';
 
 import locals from './IssueList.mless';
@@ -23,24 +21,20 @@ const cols = [
     typeArgs: {
       comparator: compareIgnoreCase,
       get$(row) {
-        return getSnapshot(row.key, getTimeConfigAtMoment(row.timestamp))
-          .startWith(null)
-          .flatMap(snapshot =>
-            getDashboardLink(row.key).map(href => {
-              const label = snapshot ? getLabel(snapshot) : `Unknown at ${formatDateTime(row.timestamp)}`;
-              return {
-                value: label,
-                content: (
-                  <div className={locals.wrapper}>
-                    {snapshot && <SvgIcon className={locals.icon} size={'s'} iconPath={getIconSvgPath(snapshot)} />}
-                    <Link href={href} className={locals.link}>
-                      {label}
-                    </Link>
-                  </div>
-                )
-              };
-            })
-          );
+        return getDashboardLink(row.key).map(href => {
+          const label = row.snapshot ? getLabel(row.snapshot) : `Unknown at ${formatDateTime(row.timestamp)}`;
+          return {
+            value: label,
+            content: (
+              <div className={locals.wrapper}>
+                {row.snapshot && <SvgIcon className={locals.icon} size={'s'} iconPath={getIconSvgPath(row.snapshot)} />}
+                <Link href={href} className={locals.link}>
+                  {label}
+                </Link>
+              </div>
+            )
+          };
+        });
       }
     }
   },
@@ -49,17 +43,14 @@ const cols = [
     type: 'custom',
     typeArgs: {
       comparator: compareIgnoreCase,
-      get$(row) {
-        return getSnapshot(row.key, getTimeConfigAtMoment(row.timestamp))
-          .startWith(null)
-          .map(snapshot => {
-            const issueDefinition = getIssueDefinitionForSnapshotAndCode(snapshot, row.code);
+      get(row) {
+        const args = row.arguments ? row.arguments.toJS() : {};
+        const issueDefinition = getIssueDefinitionForSnapshotAndCode(row.snapshot, row.code);
 
-            return {
-              value: row.code,
-              content: <issueDefinition.issueDescription.Component {...row.arguments} />
-            };
-          });
+        return {
+          value: row.code,
+          content: <issueDefinition.issueDescription.Component {...args} />
+        };
       }
     }
   },
@@ -69,70 +60,56 @@ const cols = [
     width: 150,
     typeArgs: {
       comparator: compareIgnoreCase,
-      get$(row) {
-        return getSnapshot(row.key, getTimeConfigAtMoment(row.timestamp))
-          .startWith(null)
-          .map(snapshot => {
-            const issueDefinition = getIssueDefinitionForSnapshotAndCode(snapshot, row.code);
-            const label = issueDefinition.explanationLinkLabel;
-            const href = issueDefinition.explanationLinkHref;
-            return {
-              value: label,
-              content: (
-                <Link href={href} className={locals.link} external>
-                  {label}
-                </Link>
-              )
-            };
-          });
+      get(row) {
+        const issueDefinition = getIssueDefinitionForSnapshotAndCode(row.snapshot, row.code);
+        const label = issueDefinition.explanationLinkLabel;
+        const href = issueDefinition.explanationLinkHref;
+        return {
+          value: label,
+          content: (
+            <Link href={href} className={locals.link} external>
+              {label}
+            </Link>
+          )
+        };
       }
     }
   }
 ];
 
-export default compose(
-  cursorPaginated({
-    getResettingProps: () => ['snapshot', 'timeConfig'],
-    get: ({ snapshot, timeConfig, cursor }) => {
-      const snapshotId = snapshot.get('id');
-      return getMonitoringIssuesForAgentSnapshot({
-        timeConfig,
-        snapshotId,
-        pagination: {
-          cursor,
-          retrievalSize: 200 // We don't yet have regular tables that support pagination, so just fetch a big number
-        }
-      });
+export default connectTo(
+  ({ snapshot, timeConfig }) => {
+    const snapshotId = snapshot.get('id');
+    return {
+      result: getMonitoringIssuesForAgentSnapshot({ timeConfig, snapshotId })
+    };
+  },
+  function IssueList({ result, timeConfig }) {
+    if (!result || result.getIn(['progress', 'loading']) || result.getIn(['errors']).length > 0) {
+      // Might want to give an error message instead, but for now settle with not showing the list
+      return null;
     }
-  })
-)(IssueList);
 
-function IssueList(props) {
-  const { items, timeConfig } = props;
-
-  if (!items) {
-    // Might want to give an error message instead, but for now settle with not showing the list
-    return null;
-  }
-
-  const rows = [];
-  items.forEach(event => {
-    rows.push({
-      key: event.entityId,
-      code: event.metadata['agent_monitoring_code'],
-      arguments: event.metadata['agent_monitoring_arguments'],
-      timestamp: timeConfig.focusedMoment || Date.now()
+    const rows = [];
+    result.get('data', emptyList).forEach(event => {
+      rows.push({
+        key: event.get('affectedEntityId'),
+        snapshot: event.get('affectedEntitySnapshot'),
+        code: event.get('agentMonitoringCode'),
+        arguments: event.get('agentMonitoringArguments'),
+        timestamp: timeConfig.focusedMoment || Date.now()
+      });
     });
-  });
 
-  return (
-    <Table
-      cardTitle="Issues"
-      withoutPadding
-      cols={cols}
-      rows={rows}
-      initialSortColumn={0}
-      initialSortDirection="desc"
-    />
-  );
-}
+    return (
+      <Table
+        cardTitle="Issues"
+        withoutPadding
+        cols={cols}
+        rows={rows}
+        initialSortColumn={0}
+        initialSortDirection="desc"
+      />
+    );
+  }
+);
