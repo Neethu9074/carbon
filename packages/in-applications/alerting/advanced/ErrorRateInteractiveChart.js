8513e9426a8b1f033592f49c8a451399c15707e9
Mary Reni <31503772+mrajaian@users.noreply.github.com>
2020-04-21 09:00:45 +0200

    metric name addition (#2997)
    * metric name addition
    
    * slowness metric
    
    * simon sort
    
    Co-authored-by: Mary Reni Rajaian <maryrenirajaian@ip-192-168-178-82.eu-west-1.compute.internal>

--- packages/in-applications/alerting/advanced/ErrorRateInteractiveChart.js
+++ packages/in-applications/alerting/advanced/ErrorRateInteractiveChart.js
@@ -16,9 +16,11 @@ import { getBlueprintObject, debouncedThresholdValueChangedTracker } from 'in-ap
 import ErrorRateAlertingBarChart from 'in-applications/alerting/chart/ErrorRateAlertingBarChart';
 import { applicationsAlertingThresholdOperatorChanged } from 'in-applications/alerting/tracker';
 import { getThresholdWithFixedType } from 'in-new-components/Alerting/utils/formUtils';
+import { ruleMetricNameOptions } from 'in-applications/alerting/form/ruleFormData';
 import ChartContainer from 'in-new-components/Alerting/components/ChartContainer';
 import { getThresholdLabel } from 'in-applications/alerting/form/formUtils';
 import FormGroup from 'in-components/form/FormGroup/FormGroup';
+import { joinClassNames } from 'in-services/util/classnames';
 import { propTypeTimeConfig } from 'in-stores/time/config';
 import ComboBox from 'in-components/ComboBox/ComboBox';
 import Input from 'in-components/form/Input';
@@ -49,6 +51,16 @@ function ErrorRateInteractiveChart({ form, timeConfig, onChange, granularity, de
   return (
     <div className={locals.container}>
       <div className={locals.controls}>
+        <FormGroup>
+          <Label htmlFor="errorRate">Metric</Label>
+          <Input
+            id="errorRate"
+            className={joinClassNames(locals.narrowControl, locals.disabledControl)}
+            name="errorRate"
+            value={ruleMetricNameOptions.errorRate[0].label}
+            disabled
+          />
+        </FormGroup>
         <FormGroup>
           <Label htmlFor="thresholdOperator">Operator</Label>
           <ComboBox
--- packages/in-applications/alerting/advanced/LogsInteractiveChart.js
+++ packages/in-applications/alerting/advanced/LogsInteractiveChart.js
@@ -13,9 +13,11 @@ import { getThresholdValueForPercentageMetric } from 'in-new-components/Alerting
 import { applicationsAlertingThresholdOperatorChanged } from 'in-applications/alerting/tracker';
 import { getThresholdWithFixedType } from 'in-new-components/Alerting/utils/formUtils';
 import LogsAlertingBarChart from 'in-applications/alerting/chart/LogsAlertingBarChart';
+import { ruleMetricNameOptions } from 'in-applications/alerting/form/ruleFormData';
 import ChartContainer from 'in-new-components/Alerting/components/ChartContainer';
 import { getThresholdLabel } from 'in-applications/alerting/form/formUtils';
 import FormGroup from 'in-components/form/FormGroup/FormGroup';
+import { joinClassNames } from 'in-services/util/classnames';
 import { propTypeTimeConfig } from 'in-stores/time/config';
 import ComboBox from 'in-components/ComboBox/ComboBox';
 import Input from 'in-components/form/Input';
@@ -48,6 +50,16 @@ function LogsInteractiveChart({ form, timeConfig, onChange, granularity, debounc
       {hasLogMessageSelected(form) ? (
         <>
           <div className={locals.controls}>
+            <FormGroup>
+              <Label htmlFor="logsCount">Metric</Label>
+              <Input
+                id="logsCount"
+                className={joinClassNames(locals.narrowControl, locals.disabledControl)}
+                name="logsCount"
+                value={ruleMetricNameOptions.logs[0].label}
+                disabled
+              />
+            </FormGroup>
             <FormGroup>
               <Label htmlFor="thresholdOperator">Operator</Label>
               <ComboBox
