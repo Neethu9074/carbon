9bd64291eb2f114286936da80cc5192bda04aa0f
simonThiele <simon.thiele.st@gmail.com>
2020-04-22 13:27:59 +0200

    migrate all application stories

---
+++ storybook/src/Templates/application/CreatingNewApplicationWaiter.story.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import NewApplicationWaiter from 'in-applications/Forms/NewApplication/NewApplicationWaiter';
+
+export default {
+  title: 'Templates|application/NewApplicationWaiter',
+  component: NewApplicationWaiter
+};
+
+export function Default() {
+  return <NewApplicationWaiter match={{ params: { appId: '42' } }} result={{ progress: { loading: true } }} />;
+}
