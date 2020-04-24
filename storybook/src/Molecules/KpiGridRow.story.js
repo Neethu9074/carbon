a284700c9fda42608059d31ed73d39efcbd810d9
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 12:46:04 +0200

    migrate KpiGridRow story

---
+++ storybook/src/Molecules/KpiGridRow.story.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import KpiGridRow from 'in-new-components/KpiGridRow/KpiGridRow';
+import KpiCard from 'in-new-components/KpiCard/KpiCard';
+
+export default {
+  title: 'Molecules|KpiGridRow',
+  component: KpiGridRow
+};
+
+export function Default() {
+  return (
+    <>
+      <h2>Single Row</h2>
+      <KpiGridRow sizes={[3, 3, 3, 3]}>
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+      </KpiGridRow>
+
+      <h2>Multi Row</h2>
+      <KpiGridRow sizes={[3, 3, 3, 3]}>
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+      </KpiGridRow>
+      <KpiGridRow sizes={[3, 3, 3, 3]}>
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+        <KpiCard title="Foo" value="bar" borderless raw />
+      </KpiGridRow>
+    </>
+  );
+}
