6204afa0ab8b6c8a606ab3ae9fe094d3f296e3e6
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 12:42:38 +0200

    migrate iconbutton story

--- storybook/src/Atoms/IconButton.story.js
+++ storybook/src/Atoms/IconButton.story.js
@@ -1,37 +1,47 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 
 import IconButton, { kinds } from 'in-new-components/IconButton/IconButton';
 
+export default {
+  title: 'Atoms|IconButton',
+  component: IconButton
+};
+
 const onClick = action('click');
 
-storiesOf('Components/IconButton', module)
-  .addParameters({ component: IconButton })
-  .add('Default size', () => (
-    <p>
+export function Default() {
+  return (
+    <>
       {kinds.map(kind => (
         <IconButton kind={kind} key={kind} onClick={onClick} type="lib_release_rocket">
           {kind}
         </IconButton>
       ))}
-    </p>
-  ))
-  .add('Custom icon size', () => (
-    <p>
+    </>
+  );
+}
+
+export function CustomIconSize() {
+  return (
+    <>
       {kinds.map(kind => (
         <IconButton kind={kind} key={kind} iconSize="xl" onClick={onClick} type="lib_release_rocket">
           {kind}
         </IconButton>
       ))}
-    </p>
-  ))
-  .add('Disabled', () => (
-    <p>
+    </>
+  );
+}
+
+export function Disabled() {
+  return (
+    <>
       {kinds.map(kind => (
         <IconButton kind={kind} key={kind} iconSize="l" onClick={onClick} type="lib_release_rocket" disabled>
           {kind}
         </IconButton>
       ))}
-    </p>
-  ));
+    </>
+  );
+}
