51f5931a09ab0d5926f596c0975f47ee85d6edf4
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 11:09:23 +0200

    migrate buttonSegments and capitalize stories

---
+++ storybook/src/Atoms/ButtonGroup.story.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ButtonGroup from 'in-new-components/ButtonGroup';
+
+export default {
+  title: 'Atoms|ButtonGroup',
+  component: ButtonGroup
+};
+
+export function Default() {
+  return (
+    <>
+      <ButtonGroup buttonPropsList={[{ text: 'Button 1', key: '1' }]} activeKey="1" />
+      <br />
+      <ButtonGroup buttonPropsList={[{ text: 'Button 1', key: '1' }, { text: 'Button 2', key: '2' }]} activeKey="2" />
+      <br />
+      <ButtonGroup
+        buttonPropsList={[
+          { text: 'Button 1', key: '1' },
+          { text: 'Button 2', key: '2' },
+          { text: 'Button 3', key: '3' },
+          { text: 'Button 4', key: '4' }
+        ]}
+        activeKey="2"
+      />
+    </>
+  );
+}
