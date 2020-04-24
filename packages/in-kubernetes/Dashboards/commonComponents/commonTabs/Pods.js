082a9e88580b4a830819b6d60314db3dbe396b35
Dahlia Bock <dlbock@gmail.com>
2020-04-24 10:40:57 -0500

    Display a different color (that is not red)  for the bar that indicates Pod readiness if the Pod's status is 'Completed'

--- packages/in-kubernetes/Dashboards/commonComponents/commonTabs/Pods.js
+++ packages/in-kubernetes/Dashboards/commonComponents/commonTabs/Pods.js
@@ -83,13 +83,14 @@ const allColumnDefinitions = [
     optional: true,
     sortable: false,
     getContent(item) {
+      const podStatusSummary = get(item, ['pod', 'status', 'statusSummary'], valueMissingPlaceholder);
       const containerStatuses = get(item, ['pod', 'status', 'containerStatuses'], []);
       return (
         <TwoValueBar
           v1={containerStatuses.filter(c => c.ready).length}
           v2={containerStatuses.length}
           v1Color={theme.lib.colors.lightBlue800}
-          v2Color={theme.lib.colors.red800}
+          v2Color={podStatusSummary === 'Completed' ? theme.lib.colors.N400 : theme.lib.colors.red800}
           v1Label="Ready"
           v2Label="Total"
           fullDomain={containerStatuses.length}
