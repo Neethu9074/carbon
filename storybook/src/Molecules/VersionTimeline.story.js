7f6d2c64caee95d70676b44ad9f5c8b1b759d02b
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 13:29:33 +0200

    migrate version timeline story

---
+++ storybook/src/Molecules/VersionTimeline.story.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import VersionTimeline from 'in-new-components/VersionTimeline';
+
+export default {
+  title: 'Molecules|VersionTimeline',
+  component: VersionTimeline
+};
+
+const now = Date.now();
+
+export function Default() {
+  const [selectedVersion, setSelectedVersion] = useState(null);
+
+  const min = 1000 * 60;
+  const from = now - min * 60;
+
+  return (
+    <VersionTimeline
+      versions={[
+        { from: from + min, to: from + 2 * min },
+        { from: from + 3 * min, to: from + 4 * min },
+        { from: from + 5 * min, to: from + 6 * min },
+        { from: from + 6 * min, to: from + 50 * min }
+      ]}
+      from={from}
+      to={now}
+      onVersionClick={version =>
+        setSelectedVersion(selectedVersion && selectedVersion.from === version.from ? null : version)
+      }
+      selectedVersion={selectedVersion}
+    />
+  );
+}
