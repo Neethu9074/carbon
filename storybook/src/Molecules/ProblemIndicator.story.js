c633975cae677920e4f659f4571a007adf02af1a
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 12:47:41 +0200

    migrate ProblemIndicator story

--- storybook/src/Molecules/ProblemIndicator.story.js
+++ storybook/src/Molecules/ProblemIndicator.story.js
@@ -1,21 +1,21 @@
-import { storiesOf } from '@storybook/react';
 import React from 'react';
 
 import ProblemIndicator from 'in-new-components/ProblemIndicator';
 
-import Root from '../_helpers/Root';
+export default {
+  title: 'Molecules|ProblemIndicator',
+  component: ProblemIndicator
+};
 
-storiesOf('Components/ProblemIndicator', module).add('Kinds', () => <Kinds />);
-
-function Kinds() {
+export function Default() {
   return (
-    <Root>
+    <>
       <ProblemIndicator kind="danger" title="Erroneous Trace" />
       <br />
       <ProblemIndicator kind="warning" title="Clock Skew Problems Detected">
         The beacons received by Instana from end-user arrived at inconsistent times / with great delays resulting in an
         unclear activity timeline. Timestamps shown in this view were adapted to restore a meaningful activity timeline.
       </ProblemIndicator>
-    </Root>
+    </>
   );
 }
