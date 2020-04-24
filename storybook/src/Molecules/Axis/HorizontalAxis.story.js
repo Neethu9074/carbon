59c7eb8e793b5bb4060a7c86067f382036c2a589
simonThiele <simon.thiele.st@gmail.com>
2020-04-24 10:51:15 +0200

    migrate axis stories

---
+++ storybook/src/Molecules/Axis/HorizontalAxis.story.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HorizontalAxis from 'in-new-components/Axis/HorizontalAxis';
+
+export default {
+  title: 'Molecules|Axis/Horizontal',
+  component: HorizontalAxis
+};
+
+export function Horizontal() {
+  return (
+    <>
+      <HorizontalAxis align="top" scale={{ from: 0, to: 10 }} fixedTickPositions={[0, 0.2, 0.4, 0.6, 0.8, 1]} />
+      <HorizontalAxis align="bottom" scale={{ from: 0, to: 10 }} />
+    </>
+  );
+}
