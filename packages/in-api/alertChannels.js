66c3dc895993620c7af785762666217cb2310409
Jochen Schalanda <jochen.schalanda@instana.com>
2020-04-24 17:32:31 +0200

    Show Alerting Channels without sensitive data on alerts page (#3062)
    # Why
    
    Users should be able to see configured alerting channels for an alert without being able to see confidential information such as API keys for the respective alerting channel.
    
    # What
    
    The `/api/events/settings/alertingChannels/infos` HTTP resource in the ui-backend now returns a reduced view on alerting channels suitable for displaying to users without the `CanConfigureIntegrations` permission, which is being used in the `AlertChannelsOverview` component on the alerts page.
    
    # References
    
    - [Story](https://instana.kanbanize.com/ctrl_board/37/cards/15640/details/)
    - https://github.com/instana/backend/pull/5964

--- packages/in-api/alertChannels.js
+++ packages/in-api/alertChannels.js
@@ -28,6 +28,17 @@ export function getAlertChannelsByIdsMutable(ids) {
   }).map(response => response.body);
 }
 
+export function getAlertChannelsInfosMutable(ids) {
+  return http({
+    method: 'GET',
+    maxRetries: 3,
+    url: '/api/events/settings/alertingChannels/infos',
+    queryParams: {
+      ids: ids ? ids : []
+    }
+  }).map(response => response.body);
+}
+
 export function getAlertChannel(id) {
   return http({
     method: 'GET',
