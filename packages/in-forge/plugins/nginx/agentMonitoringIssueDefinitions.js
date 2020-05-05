274c7a4a47c07720d1d2c24c86f50310ab4b3792
Miel Donkers <miel.donkers@instana.com>
2020-05-05 14:21:12 +0200

    Update AgentMonitoringIssue descriptions after checking with PM

--- packages/in-forge/plugins/nginx/agentMonitoringIssueDefinitions.js
+++ packages/in-forge/plugins/nginx/agentMonitoringIssueDefinitions.js
@@ -6,26 +6,26 @@ export default {
       Component: function nginxApiNotAccessible({ url }) {
         return (
           <span>
-            The API URL <code>{url}</code> could not be accessed
+            The API URL <code>{url}</code> of this Nginx process is not accessible from the host agent.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#enabling-metrics-collection`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#nginx_api_not_accessible`
   },
   nginx_status_not_accessible: {
     issueDescription: {
       Component: function nginxStatusNotAccessible({ url }) {
         return (
           <span>
-            The status URL <code>{url}</code> could not be accessed
+            The status URL <code>{url}</code> of this Nginx process is not accessible from the host agent.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#enabling-metrics-collection`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#nginx_status_not_accessible`
   },
 
   nginx_api_not_found: {
@@ -33,34 +33,40 @@ export default {
       Component: function nginxApiNotFound({ config }) {
         return (
           <span>
-            The API URL could not be found in the configuration <code>{config}</code>
+            The configuration of this Nginx Plus process in file <code>{config}</code>, does not seem to define a{' '}
+            <code>location</code> that exposes the <code>api</code> data provided by the{' '}
+            <code>ngx_http_api_module</code> module.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#enabling-metrics-collection`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#nginx_api_not_found`
   },
   nginx_status_not_found: {
     issueDescription: {
       Component: function nginxStatusNotFound({ config }) {
         return (
           <span>
-            The status URL could not be found in the configuration <code>{config}</code>
+            The configuration of this Nginx process in file <code>{config}</code>, does not seem to define a{' '}
+            <code>location</code> that exposes the <code>stub_status</code> data provided by the{' '}
+            <code>ngx_http_stub_status_module</code> module.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#enabling-metrics-collection`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#nginx_status_not_found`
   },
-  nginx_config_not_accessible: {
+  nginx_config_location_not_discovered: {
     issueDescription: {
-      Component: function nginxConfigNotAccessible() {
-        return <span>The configuration file could not be found</span>;
+      Component: function nginxConfigLocationNotDiscovered() {
+        return (
+          <span>The host agent cannot determine the location of the configuration file of this Nginx process.</span>
+        );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#enabling-metrics-collection`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/nginx/#nginx_config_location_not_discovered`
   }
 };
--- packages/in-forge/plugins/process/agentMonitoringIssueDefinitions.js
+++ packages/in-forge/plugins/process/agentMonitoringIssueDefinitions.js
@@ -9,53 +9,67 @@ export default {
       Component: function agentJVMBlacklisted() {
         return (
           <span>
-            The Instana agent is not capable of attaching correctly to this Java Virtual Machine. This process will not
-            be traced. Look into the agent logs for more information as to why the JVM has been blacklisted. The agent
-            will try to connect again to this virtual machine every ten minutes, but until it succeeds, no traces will
-            be collected.
+            The host agent cannot attach correctly to this Java Virtual Machine. Therefore, this process will not be
+            traced. Look into the host agent logs for more information as to why the JVM has been blacklisted. The host
+            agent will try to connect again to this virtual machine every ten minutes. However, until it succeeds,
+            neither traces nor metrics will be collected for this JVM.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/jvm`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/jvm/#agent_jvm_blacklisted`
   },
   nodejs_collector_not_installed: {
     issueDescription: {
       Component: function nodejsCollectorNotInstalled() {
         return (
-          <span>The NodeJS Collector is not installed or the Collector failed to register itself with the agent</span>
+          <span>
+            The <code>@instana/collector</code> package is not installed in this Node.js application, or the{' '}
+            <code>@instana/collector</code> package cannot announce itself to the host agent, for example due to
+            networking issues.
+          </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/node-js/installation/`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/node-js/#nodejs_collector_not_installed`
   },
   python_auto_instrumentation_failed: {
     issueDescription: {
       Component: function pythonAutoInstrumentationFailed() {
-        return <span>Instana AutoTrace™ failed to instrument the Python process</span>;
+        return <span>The host agent tried and failed to automatically instrument this Python process.</span>;
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/python/`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/python/#python_auto_instrumentation_failed`
   },
   python_sensor_not_installed: {
     issueDescription: {
       Component: function pythonSensorNotInstalled() {
-        return <span>The Python Sensor is not installed or the Sensor failed to register itself with the agent</span>;
+        return (
+          <span>
+            The <code>instana</code> package is not installed in this Python application, or the <code>instana</code>{' '}
+            package cannot announce itself to the host agent, for example due to networking issues.
+          </span>
+        );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/python/`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/python/#python_sensor_not_installed`
   },
   ruby_sensor_not_installed: {
     issueDescription: {
       Component: function rubySensorNotInstalled() {
-        return <span>The Ruby Sensor is not installed or the Sensor failed to register itself with the agent</span>;
+        return (
+          <span>
+            The <code>instana</code> gem is not installed in this Ruby application, or the <code>instana</code> gem
+            cannot announce itself to the host agent, for example due to networking issues.
+          </span>
+        );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/ruby/`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/ruby/#ruby_sensor_not_installed`
   }
 };
