daa6e5b963d1ba3716bac8fe4c98227396b81f3a
Ben Blackmore <bripkens@gmail.com>
2020-05-08 11:19:43 +0200

    Support deep links (#3125)
    **Why**
    
    Deep links to our trace view, page load view and session view are pretty
    complicated and there is no guarantee that these links won't
    change in the future. This makes hard to build tools that integrate
    with Instana by deep-linking Instana content.
    
    **What**
    
    Expose an extra route that accepts IDs and redirects to the correct views.
    This new route is a contract that can be communicated externally for
    integrators.
    
    For now the following is supported:
    
     - Deep link to a trace: `https://{{UNIT}}-{{TENANT}}.instana.io/#/dl;applications.trace.id={{TRACE_ID}}`
     - Deep link to a website page load: `https://{{UNIT}}-{{TENANT}}.instana.io/#/dl;websites.pageLoad.id={{PAGE_LOAD_ID}};websites.pageLoad.timestamp={{EPOCH_MILLIS_OF_PAGE_LOAD}}`
     - Deep link to a mobile app session: `https://{{UNIT}}-{{TENANT}}.instana.io/#/dl;mobileApps.session.id={{PAGE_LOAD_ID}};mobileApps.session.timestamp={{EPOCH_MILLIS_OF_SESSION}}`
    
    Deep links for websites and mobile apps require an additional timestamp for
    efficient database lookups. Once we have introduced bloom indices on the
    respective fields in ClickHouse, this will no longer be necessary.

---
+++ packages/in-client/js/deepLink.js
@@ -0,0 +1,48 @@
+import { Route } from 'react-router-dom';
+import React from 'react';
+
+import RedirectWithHash from 'in-components/Navigation/RedirectWithHash/RedirectWithHash';
+import { getLinkToTraceDetail } from 'in-analyze/navigation/paths';
+import { getLinkToSession } from 'in-mobile-apps/navigation/paths';
+import { getLinkToPageLoad } from 'in-websites/navigation/paths';
+import { getMatrixParameter } from 'in-stores/navigation/matrix';
+
+const path = '/dl';
+
+export default <Route path={path} component={DeepLink} />;
+
+function DeepLink({ location }) {
+  const to$ =
+    resolveApplicationsTraceIdDeepLink(location) ||
+    resolveWebsitesPageLoadIdDeepLink(location) ||
+    resolveMobileAppsSessionIdDeepLink(location);
+
+  if (to$) {
+    return <RedirectWithHash to$={to$} />;
+  }
+
+  return <RedirectWithHash to="/" />;
+}
+
+function resolveApplicationsTraceIdDeepLink(location) {
+  const traceId = getMatrixParameter(location, path, 'applications.trace.id');
+  if (traceId) {
+    return getLinkToTraceDetail(traceId);
+  }
+}
+
+function resolveWebsitesPageLoadIdDeepLink(location) {
+  const pageLoadId = getMatrixParameter(location, path, 'websites.pageLoad.id');
+  const beaconTimestamp = getMatrixParameter(location, path, 'websites.pageLoad.timestamp');
+  if (pageLoadId && beaconTimestamp) {
+    return getLinkToPageLoad({ pageLoadId, beaconTimestamp });
+  }
+}
+
+function resolveMobileAppsSessionIdDeepLink(location) {
+  const sessionId = getMatrixParameter(location, path, 'mobileApps.session.id');
+  const beaconTimestamp = getMatrixParameter(location, path, 'mobileApps.session.timestamp');
+  if (sessionId && beaconTimestamp) {
+    return getLinkToSession({ sessionId, beaconTimestamp });
+  }
+}
