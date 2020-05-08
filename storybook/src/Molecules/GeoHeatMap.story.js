335468e87f0f149d23c286493e433ecc64a223fe
simonThiele <simon.thiele.st@gmail.com>
2020-05-08 14:17:53 +0200

    split up geo story

--- storybook/src/Molecules/GeoHeatMap.story.js
+++ storybook/src/Molecules/GeoHeatMap.story.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 import GeoHeatMapPresenter from 'in-new-components/GeoHeatMap/GeoHeatMapPresenter';
 import { pendingResult, finishedProgress } from 'in-services/fixedObjects';
-import { Row, Col } from 'in-new-components/layout/Grid';
 import { number } from 'in-services/formatters/number';
 
 export default {
@@ -11,100 +10,99 @@ export default {
   component: GeoHeatMapPresenter
 };
 
+export function Loading() {
+  return <GeoHeatMapPresenter valueFormatter={number.compact} mapCode="world" height={300} result={pendingResult} />;
+}
+
+export function NoData() {
+  return (
+    <GeoHeatMapPresenter
+      valueFormatter={number.compact}
+      mapCode="world"
+      height={300}
+      result={{
+        data: null,
+        time: 1542112104261,
+        errors: [
+          {
+            message: 'Unexpected server error',
+            code: 'SERVER'
+          }
+        ],
+        progress: finishedProgress
+      }}
+    />
+  );
+}
+
+export function EmptyData() {
+  return (
+    <GeoHeatMapPresenter
+      valueFormatter={number.compact}
+      mapCode="world"
+      height={300}
+      result={{
+        data: {
+          items: [],
+          page: 1,
+          pageSize: 200,
+          totalHits: 0
+        },
+        time: 1542112104261,
+        errors: [],
+        progress: finishedProgress
+      }}
+    />
+  );
+}
+
 export function Default() {
   return (
-    <>
-      <Row>
-        <Col xs={6}>
-          <GeoHeatMapPresenter valueFormatter={number.compact} mapCode="world" height={300} result={pendingResult} />
-        </Col>
-        <Col xs={6}>
-          <GeoHeatMapPresenter
-            valueFormatter={number.compact}
-            mapCode="world"
-            height={300}
-            result={{
-              data: null,
-              time: 1542112104261,
-              errors: [
-                {
-                  message: 'Unexpected server error',
-                  code: 'SERVER'
-                }
-              ],
-              progress: finishedProgress
-            }}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={6}>
-          <GeoHeatMapPresenter
-            valueFormatter={number.compact}
-            mapCode="world"
-            height={300}
-            result={{
-              data: {
-                items: [],
-                page: 1,
-                pageSize: 200,
-                totalHits: 0
-              },
-              time: 1542112104261,
-              errors: [],
-              progress: finishedProgress
-            }}
-          />
-        </Col>
-        <Col xs={6}>
-          <GeoHeatMapPresenter
-            valueFormatter={number.compact}
-            mapCode="world"
-            onHomeClick={action('onHomeClick')}
-            onAreaClick={action('onAreaClick')}
-            height={300}
-            result={{
-              data: {
-                ca: {
-                  title: 'Canada',
-                  value: 311
-                },
-                cn: {
-                  title: 'China',
-                  value: 800
-                },
-                eg: {
-                  title: 'Egypt',
-                  value: 342
-                },
-                fr: {
-                  title: 'France',
-                  value: 240
-                },
-                jp: {
-                  title: 'Japan',
-                  value: 200
-                },
-                ru: {
-                  title: 'Russia',
-                  value: 100
-                },
-                gb: {
-                  title: 'United Kingdom',
-                  value: 320
-                },
-                us: {
-                  title: 'United States',
-                  value: 180
-                }
-              },
-              time: 1542112104261,
-              errors: [],
-              progress: finishedProgress
-            }}
-          />
-        </Col>
-      </Row>
-    </>
+    <GeoHeatMapPresenter
+      valueFormatter={number.compact}
+      mapCode="world"
+      onHomeClick={action('onHomeClick')}
+      onAreaClick={action('onAreaClick')}
+      height={300}
+      result={{
+        data: {
+          ca: {
+            title: 'Canada',
+            value: 311
+          },
+          cn: {
+            title: 'China',
+            value: 800
+          },
+          eg: {
+            title: 'Egypt',
+            value: 342
+          },
+          fr: {
+            title: 'France',
+            value: 240
+          },
+          jp: {
+            title: 'Japan',
+            value: 200
+          },
+          ru: {
+            title: 'Russia',
+            value: 100
+          },
+          gb: {
+            title: 'United Kingdom',
+            value: 320
+          },
+          us: {
+            title: 'United States',
+            value: 180
+          }
+        },
+        time: 1542112104261,
+        errors: [],
+        progress: finishedProgress
+      }}
+    />
   );
 }
