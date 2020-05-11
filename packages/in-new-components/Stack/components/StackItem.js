0cab08a96b64c59e307851b525f3adb6d1ded782
Will James <william.james@instana.com>
2020-05-11 12:02:18 -0500

    Fix Misalignment of Context Guide Stack KPIs (#3136)
    # Why
    
    Currently the context guide KPIs can be misaligned because we use a character count for determining the max width of the label. This will always be inaccurate because we do not have a fixed width font.
    
    # What
    
    Use the built in `text-overflow: ellipsis` via our `.ellipsis` mixin in order to ensure the ellipsis for the CG Stack label appears at the right place.
    
    This required setting a `min-width` for the flex box containers for the ellipsized element, and then setting the `span` to have `display: inline-block`.
    
    # Commits
    
    * Fix context guide KPI alignment and improve elipsis handling
    
    * Context guide stack: Ensure icons are aligned when label text is long

--- packages/in-new-components/Stack/components/StackItem.js
+++ packages/in-new-components/Stack/components/StackItem.js
@@ -44,7 +44,7 @@ export default function StackItem({
           ) : (
             <div className={locals.dot} />
           )}
-          <EntityWithIcon label={shortLabel || label} type={type} technologies={technologiesNoK8s} length={52} />
+          <EntityWithIcon label={shortLabel || label} type={type} technologies={technologiesNoK8s} length={100} />
 
           {!isAp && type === plugins.process && <ProfileIndicator processSnapshotId={id} />}
           {showEndpointTypes(endpointTypes)}
