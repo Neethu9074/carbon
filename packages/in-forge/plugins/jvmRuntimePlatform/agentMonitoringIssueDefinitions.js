d4815eb4f7583d82e601fa508005e21b57c0ae02
Donnerbart <4196298+Donnerbart@users.noreply.github.com>
2020-05-12 13:03:14 +0200

    Where is Waldo: Pass recommended Java version via event

--- packages/in-forge/plugins/jvmRuntimePlatform/agentMonitoringIssueDefinitions.js
+++ packages/in-forge/plugins/jvmRuntimePlatform/agentMonitoringIssueDefinitions.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default {
   java_8_outdated_version: {
     issueDescription: {
-      Component: function java8OutdatedVersion({ version }) {
+      Component: function java8OutdatedVersion({ version, recommendedVersion }) {
         return (
           <span>
             The version {version} of this JVM process is outdated.{' '}
-            Java 8 versions older than 1.8.0_252 have several known bugs that may affect, among others, the G1 garbage collector and the Instrumentation API.{' '}
+            Java 8 versions older than 1.8.0_{recommendedVersion} have several known bugs that may affect, among others, the G1 garbage collector and the Instrumentation API.{' '}
             We recommend to update to the latest Java 8 or 11 version.
           </span>
         );
