0b01cd79d5df991c130e2f11b9b1a50bca50e468
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-05-12 09:53:39 +0200

    Fix incorrect blank check

--- packages/in-websites/alerting/alertConfigDialogWithThreshold/AlertConfigDialogWithThreshold.js
+++ packages/in-websites/alerting/alertConfigDialogWithThreshold/AlertConfigDialogWithThreshold.js
@@ -93,7 +93,7 @@ function resolveThresholdRequest(form, granularity, fallbackOnError) {
         getMetricConfigurationForErrors(websiteId, 'SUM', errorCount, stringValue, operator, tagFilters, granularity)
       );
     case errorRate:
-      if (!isBlank(stringValue)) {
+      if (isBlank(stringValue)) {
         return empty;
       }
 
