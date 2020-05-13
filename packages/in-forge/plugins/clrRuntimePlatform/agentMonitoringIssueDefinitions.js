c10434f5e399754ed75899dfd4033730140dc55b
Kristof Berger <kristof.berger@instana.com>
2020-05-13 12:16:06 +0200

    Fix waldo-messages for .NET Full Framework apps

--- packages/in-forge/plugins/clrRuntimePlatform/agentMonitoringIssueDefinitions.js
+++ packages/in-forge/plugins/clrRuntimePlatform/agentMonitoringIssueDefinitions.js
@@ -7,7 +7,7 @@ export default {
         const missing = Array.isArray(missingEnvKeys) ? missingEnvKeys.join(', ') : missingEnvKeys;
         return (
           <span>
-            The process environment for this .NET Core application is not correctly configured for Instana to be able to
+            The process environment for this .NET Full Framework application is not correctly configured for Instana to be able to
             monitor it. The following environment variables must be set on the .NET Core process: <code>{missing}</code>
             . Refer to the documentation for the right values to be set.
           </span>
@@ -23,7 +23,7 @@ export default {
         if (competitor) {
           return (
             <span>
-              It seems that another tool is monitoring this .NET Core process, likely {competitor}. The Instana host
+              It seems that another tool is monitoring this .NET Full Framework process, likely {competitor}. The Instana host
               agent has detected this based on the value of the <code>{invalidEnvKey}</code> environment variable. For
               Instana to be able to trace this .NET Core process, you need to disable the other monitoring tool.
             </span>
@@ -32,7 +32,7 @@ export default {
 
         return (
           <span>
-            The process environment for this .NET Core application is not correctly configured for Instana to be able to
+            The process environment for this .NET Full Framework application is not correctly configured for Instana to be able to
             monitor it. The environment variable <code>{invalidEnvKey}</code> has the wrong value{' '}
             <code>{invalidEnvValue}</code>. Refer to the documentation for guidance on which value to set to the{' '}
             <code>{invalidEnvKey}</code> environment variable.
