854bd7fd221feb1a2bd0a4db420cee8bc917d8e3
Donnerbart <4196298+Donnerbart@users.noreply.github.com>
2020-05-14 16:34:15 +0200

    Where is Waldo: Change java_8u40_not_monitored to java_8_unmonitored_version and pass the "40" as metadata.

--- packages/in-forge/plugins/jvmRuntimePlatform/agentMonitoringIssueDefinitions.js
+++ packages/in-forge/plugins/jvmRuntimePlatform/agentMonitoringIssueDefinitions.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 export default {
-  java_8u40_not_monitored: {
+  java_8_unmonitored_version: {
     issueDescription: {
-      Component: function java8u40NotMonitored({ version }) {
+      Component: function java8UnmonitoredVersion({ version, unmonitoredVersion }) {
         return (
           <span>
-            The Java 8 builds up to 1.8.0_40 have several known issues relating to the implementation of lambdas. Due to
+            The Java 8 builds up to 1.8.0_{unmonitoredVersion} have several known issues relating to the implementation of lambdas. Due to
             these issues this JVM with version {version} will not be monitored.
           </span>
         );
       }
     },
     explanationLinkLabel: `Docs`,
-    explanationLinkHref: `https://docs.instana.io/ecosystem/jvm#java_8u40_not_monitored`
+    explanationLinkHref: `https://docs.instana.io/ecosystem/jvm#java_8_unmonitored_version`
   },
   java_8_outdated_version: {
     issueDescription: {
@@ -22,7 +22,7 @@ export default {
           <span>
             The version {version} of this JVM process is outdated.{' '}
             Java 8 versions older than 1.8.0_{recommendedVersion} have several known bugs that may affect, among others, the G1 garbage collector and the Instrumentation API.{' '}
-            We recommend to update to the latest Java 8 or 11 version.
+            We recommend to update to the latest Java 8 version or to another Java Long Term Support release.
           </span>
         );
       }
