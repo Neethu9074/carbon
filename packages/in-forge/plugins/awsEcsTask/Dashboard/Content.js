bf24033317b7a48a8cc9bde6be5bae5d4dd63691
Bastian Krol <bastian.krol@instana.com>
2020-05-19 18:36:07 +0200

    Add alpha state notification to AWS ECS dashboards

--- packages/in-forge/plugins/awsEcsTask/Dashboard/Content.js
+++ packages/in-forge/plugins/awsEcsTask/Dashboard/Content.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import DashboardNotification from 'in-components/DashboardNotification';
 import DashboardContainerList from './DashboardContainerList';
 
 export default function AwsEcsTaskDashboard({ snapshot }) {
   const snapshotId = snapshot.get('id');
-  return <DashboardContainerList snapshotId={snapshotId} />;
+
+  return (
+    <Fragment>
+      <DashboardNotification type="info">
+        <h3>Under Construction</h3>
+        Instana&apos;s Fargate support is currently in alpha. More data and metrics for AWS ECS Tasks are coming soon.
+      </DashboardNotification>
+      <DashboardContainerList snapshotId={snapshotId} />;
+    </Fragment>
+  );
 }
--- packages/in-forge/plugins/awsEcsTaskDefinition/Dashboard/Content.js
+++ packages/in-forge/plugins/awsEcsTaskDefinition/Dashboard/Content.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import DashboardNotification from 'in-components/DashboardNotification';
 import DashboardVersionsList from './DashboardVersionsList';
 
-export default function AwsEcsTaskDefinitionDashboard(snapshot) {
+export default function AwsEcsTaskDefinitionDashboard({ snapshot }) {
   const snapshotId = snapshot.get('id');
-  return <DashboardVersionsList snapshotId={snapshotId} />;
+  return (
+    <Fragment>
+      <DashboardNotification type="info">
+        <h3>Under Construction</h3>
+        Instana&apos;s Fargate support is currently in alpha. More data and metrics for AWS ECS Task Definitions are
+        coming soon.
+      </DashboardNotification>
+      <DashboardVersionsList snapshotId={snapshotId} />
+    </Fragment>
+  );
 }
