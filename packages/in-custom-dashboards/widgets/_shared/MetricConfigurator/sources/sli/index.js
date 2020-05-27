05312dba5955d960b9cbce68d9d25001fde9f091
maryrenirajaian <mary.rajaian@instana.com>
2020-05-27 20:03:22 +0200

    feature flag to form

--- packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/sli/index.js
+++ packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/sli/index.js
@@ -1,5 +1,8 @@
 import FormComponent from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/sli/FormComponent';
+import { sloConfigurationEnabled } from 'in-services/featureFlags';
+
 export { createForm } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/sli/form';
 export const Form = FormComponent;
 export const source = 'SLI';
 export const label = 'SLI';
+export const enabled = sloConfigurationEnabled;
