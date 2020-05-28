daa773d4c0cd76ea89d6eb14b1d9b6cd08c35100
Ben Blackmore <bripkens@gmail.com>
2020-05-28 17:13:48 +0200

    Improve error handling when WebSocket message transmission fails

--- packages/in-connection/states/AbstractState.js
+++ packages/in-connection/states/AbstractState.js
@@ -76,14 +76,18 @@ export default class AbstractConnectionState extends AbstractState {
   }
 
   send(event, obj) {
-    if (this.sharedState.socket) {
-      this.sharedState.metrics.transmitted++;
-      this.sharedState.socket.send(`${event},${JSON.stringify(obj)}`);
-    } else {
-      logger.info(
-        'Not transmitting event to backend because connection is not currently established. Event Name:',
-        event
-      );
+    try {
+      if (this.sharedState.socket) {
+        this.sharedState.metrics.transmitted++;
+        this.sharedState.socket.send(`${event},${JSON.stringify(obj)}`);
+      } else {
+        logger.info(
+          'Not transmitting event to backend because connection is not currently established. Event Name:',
+          event
+        );
+      }
+    } catch (e) {
+      logger.error('Failed to transmit WebSocket message', event, e);
     }
   }
 }
