447cc4604c6e0c9fe6695f5c9766988550e759c1
Cedric Ziel <cedric.ziel@instana.com>
2020-06-03 11:46:33 +0200

    Add support for laravel and blade span types

---
+++ packages/in-forge/tracing/blade/index.js
@@ -0,0 +1,28 @@
+import { registerSpanDefinition } from 'in-sdk/tracing';
+
+registerSpanDefinition({
+  type: 'blade',
+  category: 'generic',
+  direction: 'local',
+
+  typeName: {
+    singular: 'Blade',
+    plural: 'Blades'
+  },
+
+  detailView: 'BladeSpanDetailView',
+
+  getLabel(span) {
+    const view_name = span.getIn(['data', 'blade', 'view']);
+    if (view_name) {
+      return view_name;
+    }
+
+    const template_path = span.getIn(['data', 'blade', 'path']);
+    if (template_path) {
+      return template_path.split('/').pop();
+    }
+
+    return 'Blade';
+  }
+});
---
+++ packages/in-forge/tracing/laravel/LaravelSpanDetailView.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ErrorDescriptionItem from 'in-sdk/components/traceDetails/ErrorDescriptionItem';
+import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
+
+export default function LaravelSpanDetailView({ span }) {
+  const route = span.getIn(['data', 'laravel', 'route']);
+  const action = span.getIn(['data', 'laravel', 'action']);
+  const controller = span.getIn(['data', 'laravel', 'controller']);
+
+  return (
+    <div>
+      <Dl>
+        {route != null && <Di title="Route">{route}</Di>}
+        {controller != null && <Di title="Controller">{controller}</Di>}
+        {action != null && <Di title="Action">{action}</Di>}
+        <ErrorDescriptionItem error={span.getIn(['data', 'laravel', 'exception'])} />
+      </Dl>
+    </div>
+  );
+}
