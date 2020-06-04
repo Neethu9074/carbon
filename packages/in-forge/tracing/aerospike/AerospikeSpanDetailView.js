7af5504fbd42704567256d924738eb5a880687ca
zivanneskovic <59165974+zivanneskovic@users.noreply.github.com>
2020-06-04 16:56:34 +0200

    Aerospike detail view is extended with new fields

--- packages/in-forge/tracing/aerospike/AerospikeSpanDetailView.js
+++ packages/in-forge/tracing/aerospike/AerospikeSpanDetailView.js
@@ -4,11 +4,31 @@ import ErrorDescriptionItem from 'in-sdk/components/traceDetails/ErrorDescriptio
 import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
 
 export default function AerospikeSpanDetailView({ span }) {
+  const parameters = span.getIn(['data', 'aerospike', 'parameters']);
+  const statement = span.getIn(['data', 'aerospike', 'statement']);
+
   return (
     <div>
       <Dl>
         <Di title="Operation">{span.getIn(['data', 'aerospike', 'op'])}</Di>
+        <Di title="Host">{span.getIn(['data', 'aerospike', 'host'])}</Di>
+        <Di title="Port">{span.getIn(['data', 'aerospike', 'port'])}</Di>
+        <Di title="Namespace">{span.getIn(['data', 'aerospike', 'ns'])}</Di>
+        <Di title="Set name">{span.getIn(['data', 'aerospike', 'setName'])}</Di>
+        <Di title="User key">{span.getIn(['data', 'aerospike', 'userKey'])}</Di>
         <ErrorDescriptionItem error={span.getIn(['data', 'aerospike', 'error'])} />
+
+        {parameters ? (
+                  <Di title="Parametes">
+                    {parameters}
+                  </Di>
+                ) : null}
+
+         {statement ? (
+                <Di title="Statement">
+                  {statement}
+                </Di>
+              ) : null}
       </Dl>
     </div>
   );
