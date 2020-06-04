78ab3821efebed08df016a0b0af31344afd47321
Simon Thiele <simon.thiele.st@googlemail.com>
2020-06-04 13:25:36 +0200

    only show auth tab on internal FF (#3305)
    Co-authored-by: simonThiele <simon.thiele.st@gmail.com>

--- packages/in-settings/ConfigurationView.js
+++ packages/in-settings/ConfigurationView.js
@@ -1,6 +1,7 @@
 import { generatePath, matchPath } from 'react-router';
 import React from 'react';
 
+import { isInternalVisible$ } from 'in-new-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore';
 import { settingsBasePath, teamSettings, userSettingsGeneral } from 'in-settings/navigation/paths';
 import { isAvailable as isGoogleSSOAvailable } from 'in-settings/tabs/AuthSettings/api/googleSSO';
 import RedirectWithHash from 'in-components/Navigation/RedirectWithHash/RedirectWithHash';
@@ -15,6 +16,7 @@ import connectTo from 'in-hoc/connectTo';
 
 export default connectTo(
   {
+    isInternalVisible: isInternalVisible$,
     isGoogleSSOAvailable: isGoogleSSOAvailable(),
     isSamlAvailable: isSamlAvailable(),
     isLdapAvailable: isLdapAvailable()
