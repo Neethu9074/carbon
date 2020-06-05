fa428f967410620d82938efd984143e43e128422
simonThiele <simon.thiele.st@gmail.com>
2020-06-05 16:24:41 +0200

    Revert "remove mobile apps from areas tab (#3325)"
    This reverts commit 3f55b3aaea1004cdefa4555b9b2f8b02c47f43cc.

--- packages/in-settings/tabs/TeamSettings/pages/accessControl/Groups/components/AreasTabControl.js
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/Groups/components/AreasTabControl.js
@@ -9,6 +9,11 @@ const tabList = [
     key: 'websites'
   },
   {
+    icon: 'lib_website_mobile_app',
+    text: 'Mobile Apps',
+    key: 'mobileApps'
+  },
+  {
     icon: 'lib_application_invert',
     text: 'Applications',
     key: 'application'
