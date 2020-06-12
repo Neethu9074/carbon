f21d862a06ceb8a8fd6236e12f072e1ed9dc231e
Robert Hostlowsky <robert.hostlowsky@instana.com>
2020-06-12 10:53:43 +0200

    fix slider font family, fix opacity of rail (make more prominent)
    (cherry picked from commit 8d114425d4efcdf9c9f4975372ddb56e84c60ab4)

--- packages/in-new-components/Slider/base/StyledMuiSliderBase.js
+++ packages/in-new-components/Slider/base/StyledMuiSliderBase.js
@@ -6,12 +6,18 @@ import theme from 'in-themes';
 
 export const StyledMuiSliderBase = withStyles({
   root: {
+    fontFamily: theme.lib.fontFamily,
     color: theme.lib.colors.teal800,
     top: 0,
     lineHeight: 1.5,
     marginTop: 32
   },
+  mark: {
+    width: 4,
+    marginLeft: -2
+  },
   valueLabel: {
+    fontFamily: 'inherit',
     color: theme.lib.colors.white,
     padding: 0,
     top: -22,
@@ -36,11 +42,13 @@ export const StyledMuiSliderBase = withStyles({
     }
   },
   markLabel: {
+    fontFamily: 'inherit',
     ...theme.lib.typography.h100,
     color: theme.lib.colors.N600Light,
     height: 16
   },
   markLabelActive: {
+    fontFamily: 'inherit',
     color: theme.lib.colors.N900Primary,
     height: 16
   },
@@ -53,6 +61,7 @@ export const StyledMuiSliderBase = withStyles({
   },
   rail: {
     height: 2,
+    opacity: 1,
     color: theme.lib.colors.N400
   }
 })(MuiSlider);
