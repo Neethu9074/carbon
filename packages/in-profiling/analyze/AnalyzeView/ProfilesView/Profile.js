6b8e6e3f6e9a0821c9e8ffea70d699379b72f19d
simonThiele <simon.thiele.st@gmail.com>
2020-06-30 14:01:48 +0200

    fix typo

--- packages/in-profiling/analyze/AnalyzeView/ProfilesView/Profile.js
+++ packages/in-profiling/analyze/AnalyzeView/ProfilesView/Profile.js
@@ -148,7 +148,7 @@ export default function Profile({
           {totalNumSamples > 0 &&
             totalNumSamples < 100 && (
               <Tooltip
-                content={`Statistical confidence in percentage distribution is low, because not enough samples where collected (${totalNumSamples} samples) in the selected Timeframe.`}
+                content={`Statistical confidence in percentage distribution is low, because not enough samples were collected (${totalNumSamples} samples) in the selected Timeframe.`}
                 align="rightMiddle"
               >
                 <SvgIcon className={locals.icon} type="lib_approximately_equal" />
