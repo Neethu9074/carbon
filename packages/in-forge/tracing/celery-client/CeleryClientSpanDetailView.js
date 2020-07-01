7ffa46aade038df004bca55042378b0ec6848f12
Peter Giacomo Lombardo <pglombardo@gmail.com>
2020-07-01 17:16:39 +0200

    New Python Celery Span Support (#3470)
    * New Python Celery Span Support
    
    * Remove broker tag

---
+++ packages/in-forge/tracing/celery-client/CeleryClientSpanDetailView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
+import ErrorDescriptionItem from 'in-sdk/components/traceDetails/ErrorDescriptionItem';
+
+export default function CeleryClientSpanDetailView({ span }) {
+  return (
+    <Dl>
+      <Di title="Task">{span.getIn(['data', 'celery', 'task'])}</Di>
+      <Di title="Task ID">{span.getIn(['data', 'celery', 'task_id'])}</Di>
+      <Di title="Retry Reason">{span.getIn(['data', 'celery', 'retry-reason'])}</Di>
+      <ErrorDescriptionItem error={span.getIn(['data', 'celery', 'error'])} />
+    </Dl>
+  );
+}
