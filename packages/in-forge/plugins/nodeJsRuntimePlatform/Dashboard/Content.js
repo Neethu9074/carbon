dd3e7ddfe4ddc532ff9bb4d3ae250b8f543f3a17
Bastian Krol <bastian.krol@instana.com>
2020-07-01 14:12:50 +0200

    hide legacy "init-too-late" warning when monitoring issue is shown

--- packages/in-forge/plugins/nodeJsRuntimePlatform/Dashboard/Content.js
+++ packages/in-forge/plugins/nodeJsRuntimePlatform/Dashboard/Content.js
@@ -5,11 +5,13 @@ import HealthchecksTable from 'in-forge/plugins/nodeJsRuntimePlatform/Dashboard/
 import HttpServersTable from 'in-forge/plugins/nodeJsRuntimePlatform/Dashboard/HttpServersTable';
 import ModuleAnalysisDialog from 'in-forge/plugins/nodeJsRuntimePlatform/ModuleAnalysisDialog';
 import HeapSpacesTable from 'in-forge/plugins/nodeJsRuntimePlatform/Dashboard/HeapSpacesTable';
+import getMonitoringIssuesForSnapshot from 'in-subscription/getMonitoringIssuesForSnapshot';
 import { KpiKeyValue, KpiSection } from 'in-sdk/components/dashboard/KpiSection';
 import { bytes, time, twoDecimalPlaces } from 'in-services/formatters/number';
 import DashboardSection from 'in-sdk/components/dashboard/DashboardSection';
 import Chart from 'in-components/Chart/InfrastructureMetricChartBehavior';
 import DashboardNotification from 'in-components/DashboardNotification';
+import { agentMonitoringIssuesEnabled } from 'in-services/featureFlags';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { Col, Row } from 'in-new-components/layout/Grid';
 import MetricValue from 'in-components/MetricValue';
@@ -17,14 +19,21 @@ import { getCodeView } from 'in-sdk/snapshot';
 import Button from 'in-components/Button';
 import connectTo from 'in-hoc/connectTo';
 
-export default connectTo({
-  isInternalVisible: isInternalVisible$
-})(function NodejsDashboard({ snapshot, timeConfig, isInternalVisible }) {
+export default connectTo(({ snapshot, timeConfig }) => {
+  const snapshotId = snapshot.get('id');
+  return {
+    isInternalVisible: isInternalVisible$,
+    monitoringIssues: getMonitoringIssuesForSnapshot({ timeConfig, snapshotId })
+      .filter(issuesResult => issuesResult && issuesResult.get('data'))
+      .map(issuesResult => issuesResult.get('data'))
+      .startWith(null)
+  };
+})(function NodejsDashboard({ snapshot, timeConfig, isInternalVisible, monitoringIssues }) {
   const snapshotId = snapshot.get('id');
   const gcStatsSupported = snapshot.getIn(['data', 'gc.statsSupported']);
   return (
     <div>
-      {getInitializedTooLateHint(snapshot)}
+      {getInitializedTooLateHint(snapshot, isInternalVisible, monitoringIssues)}
       {getNativeExtensionHint(snapshot)}
 
       <KpiSection>
@@ -185,10 +194,30 @@ function renderEventLoopMetrics(snapshot, timeConfig) {
   );
 }
 
-function getInitializedTooLateHint(snapshot) {
+function getInitializedTooLateHint(snapshot, isInternalVisible, monitoringIssues) {
+  // Note: This mechanism for warning the user about the initialized-too-late issue is deprecated and replaced by the
+  // monitoring issues mechanism. We only keep this mechnism around until
+  // * the agentMonitoringIssuesEnabled feature flag has been enabled for all TUs (or the feature flag has been removed
+  //   entirely) and then also for another few months AND
+  // *  we can reasonably expect that most users have upgraded to a version of @instana/collector that sends monitoring
+  //    issues (support for that arrived in July/August 2020).
+  // The latter condition will probably become true much later than the first, given how sloppy some customers are with
+  // updating @instana/collector.
+
   const probablyInitializedTooLate = snapshot.getIn(['data', 'initTooLate']);
 
   if (!probablyInitializedTooLate) {
+    return null;
+  }
+
+  // Check if we also show the monitoring issue notification for this problem, to avoid showing both (the monitoring
+  // issue notification and the old dashboard notification)
+  const monitoringIssueNotificationExists = monitoringIssues?.find(
+    issue => issue.get('agentMonitoringCode') === 'nodejs_collector_initialized_too_late'
+  );
+  const monitoringIssueNotificationIsShown = isInternalVisible || agentMonitoringIssuesEnabled;
+
+  if (monitoringIssueNotificationExists && monitoringIssueNotificationIsShown) {
     return null;
   }
 
