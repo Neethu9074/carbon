924d5ef0f33612a11c75ab7afb0394b0ba839bf2
Andrey Slotin <andrey.slotin@instana.com>
2020-07-06 17:17:05 +0200

    Format pull start and stop time at the ECS Task info panel

--- packages/in-forge/plugins/awsEcsTask/Info.js
+++ packages/in-forge/plugins/awsEcsTask/Info.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { DescriptionItem, DescriptionList } from 'in-sdk/components/sidebar/DescriptionList';
+import { formatDateTime, fromNow } from 'in-services/formatters/date';
 
 export default function Info({ snapshot }) {
-  const data = snapshot.get('data');
+  const data = snapshot.get('data'),
+    pullStartedAt = data.get('pullStartedAt'),
+    pullStoppedAt = data.get('pullStoppedAt');
 
   return (
     <DescriptionList>
@@ -16,8 +19,12 @@ export default function Info({ snapshot }) {
       <DescriptionItem title="Availability Zone">{data.get('availabilityZone')}</DescriptionItem>
       <DescriptionItem title="Desired Status">{data.get('desiredStatus')}</DescriptionItem>
       <DescriptionItem title="Known Status">{data.get('knownStatus')}</DescriptionItem>
-      <DescriptionItem title="Pull Started">{data.get('pullStartedAt')}</DescriptionItem>
-      <DescriptionItem title="Pull Stopped">{data.get('pullStoppedAt')}</DescriptionItem>
+      <DescriptionItem title="Pull Started">
+        {formatDateTime(pullStartedAt)} ({fromNow(pullStartedAt)})
+      </DescriptionItem>
+      <DescriptionItem title="Pull Stopped">
+        {formatDateTime(pullStoppedAt)} ({fromNow(pullStoppedAt)})
+      </DescriptionItem>
       <DescriptionItem title="CPU Limit">{data.get('limits.cpu')}</DescriptionItem>
       <DescriptionItem title="Memory Limit">{data.get('limits.memory')}</DescriptionItem>
     </DescriptionList>
