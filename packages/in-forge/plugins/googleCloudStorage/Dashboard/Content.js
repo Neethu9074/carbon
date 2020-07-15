6d6ab61aa6470ef21e7c3182b639d412269cccea
Igor Milanovic <igor.milanovic@instana.com>
2020-07-15 09:16:16 +0200

    Renaming

--- packages/in-forge/plugins/googleCloudStorage/Dashboard/Content.js
+++ packages/in-forge/plugins/googleCloudStorage/Dashboard/Content.js
@@ -54,7 +54,7 @@ export default function GoogleCLoudStorageDashboard({ snapshot, timeConfig }) {
         </DashboardSection>
       </Columize>
       <Columize>
-        <DashboardSection title="Object size">
+        <DashboardSection title="Object">
           <Chart
             snapshotId={snapshotId}
             timeConfig={timeConfig}
