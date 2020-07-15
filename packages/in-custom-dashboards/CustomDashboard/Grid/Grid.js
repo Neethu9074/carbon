06fb5b5f74afcca3999e79aaf1b73e208226cef4
Ben Blackmore <bripkens@gmail.com>
2020-07-15 13:06:37 +0200

    custom dashboard Grid timeout is not cleared on unmount

--- packages/in-custom-dashboards/CustomDashboard/Grid/Grid.js
+++ packages/in-custom-dashboards/CustomDashboard/Grid/Grid.js
@@ -1,7 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import ReactGridLayout from 'react-grid-layout';
 import TrackVisibility from 'react-on-screen';
 import 'react-grid-layout/css/styles.css';
-import React, { useState } from 'react';
 import 'react-resizable/css/styles.css';
 import theme from 'in-themes';
 
@@ -14,7 +14,6 @@ import {
 } from 'in-custom-dashboards/CustomDashboard/Grid/settings';
 import { MoreMenu, MoreMenuButton } from 'in-new-components/MoreMenu';
 import { evaluateClassNames } from 'in-services/util/classnames';
-import LifecycleObserver from 'in-components/LifecycleObserver';
 import ErrorBoundary from 'in-components/ErrorBoundary';
 import widgets from 'in-custom-dashboards/widgets';
 import SvgIcon from 'in-components/SvgIcon';
@@ -53,6 +52,10 @@ export default function Grid({
   // browser tick after mounting we re-enable transitions again so that react-grid-layout works
   // as intended.
   const [disabledTransitions, setDisabledTransitions] = useState(true);
+  useEffect(() => {
+    const handle = setTimeout(setDisabledTransitions, 0, false);
+    return () => clearTimeout(handle);
+  }, []);
 
   const layout = config.widgets.map(widget => {
     const { minimumWidth, minimumHeight } = widgets[widget.type];
@@ -68,77 +71,74 @@ export default function Grid({
   });
 
   return (
-    <>
-      <LifecycleObserver onDidMount={() => setTimeout(setDisabledTransitions, 0, false)} />
-      <ReactGridLayout
-        className={evaluateClassNames({
-          [locals.layout]: true,
-          [locals.tvMode]: tvMode
-        })}
-        cols={cols}
-        rowHeight={rowHeightPixels}
-        margin={margin}
-        // Remove the horizontal spacing added by the grid layout to avoid
-        // horizontal overflow.
-        width={width - theme.grid.gutter}
-        containerPadding={containerPadding}
-        layout={layout}
-        breakpoints={breakpoints}
-        isDraggable={isDraggable}
-        isResizable={isResizable}
-        onDragStop={forwardLayoutChange}
-        onResizeStop={forwardLayoutChange}
-        draggableHandle={`.${draggableHandle || locals.dragHandle}`}
-      >
-        {config.widgets.map(widget => {
-          const { Widget, onlyRenderInsideViewport } = widgets[widget.type];
+    <ReactGridLayout
+      className={evaluateClassNames({
+        [locals.layout]: true,
+        [locals.tvMode]: tvMode
+      })}
+      cols={cols}
+      rowHeight={rowHeightPixels}
+      margin={margin}
+      // Remove the horizontal spacing added by the grid layout to avoid
+      // horizontal overflow.
+      width={width - theme.grid.gutter}
+      containerPadding={containerPadding}
+      layout={layout}
+      breakpoints={breakpoints}
+      isDraggable={isDraggable}
+      isResizable={isResizable}
+      onDragStop={forwardLayoutChange}
+      onResizeStop={forwardLayoutChange}
+      draggableHandle={`.${draggableHandle || locals.dragHandle}`}
+    >
+      {config.widgets.map(widget => {
+        const { Widget, onlyRenderInsideViewport } = widgets[widget.type];
 
-          const actions = isConfigurable && (
-            <MoreMenu kind="secondaryDarker" size="compact" className={locals.more}>
-              <MoreMenuButton icon="lib_actions_edit" onClick={() => onEditWidget(widget.id)}>
-                Edit
-              </MoreMenuButton>
-              <MoreMenuButton icon="lib_actions_copy" onClick={() => onDuplicateWidget(widget.id)}>
-                Duplicate
-              </MoreMenuButton>
-              <MoreMenuButton icon="lib_actions_delete" onClick={() => onRemoveWidget(widget.id)}>
-                Delete
-              </MoreMenuButton>
-            </MoreMenu>
-          );
-          const widgetComponent = (
-            <Widget
-              title={widget.title}
-              actions={actions}
-              dragHandle={isDraggable && dragHandle}
-              config={widget.config}
-            />
-          );
+        const actions = isConfigurable && (
+          <MoreMenu kind="secondaryDarker" size="compact" className={locals.more}>
+            <MoreMenuButton icon="lib_actions_edit" onClick={() => onEditWidget(widget.id)}>
+              Edit
+            </MoreMenuButton>
+            <MoreMenuButton icon="lib_actions_copy" onClick={() => onDuplicateWidget(widget.id)}>
+              Duplicate
+            </MoreMenuButton>
+            <MoreMenuButton icon="lib_actions_delete" onClick={() => onRemoveWidget(widget.id)}>
+              Delete
+            </MoreMenuButton>
+          </MoreMenu>
+        );
+        const widgetComponent = (
+          <Widget
+            title={widget.title}
+            actions={actions}
+            dragHandle={isDraggable && dragHandle}
+            config={widget.config}
+          />
+        );
 
-          let content = widgetComponent;
-          if (onlyRenderInsideViewport) {
-            content = (
-              <TrackVisibility once offset={300} tag="div">
-                {({ isVisible }) => isVisible && widgetComponent}
-              </TrackVisibility>
-            );
-          }
-
-          return (
-            <div
-              key={widget.id}
-              className={locals.widget}
-              id={getWidgetId(widget.id)}
-              style={disabledTransitions ? disabledTransitionStyle : undefined}
-            >
-              <ErrorBoundary name={`Custom dashboard widget: ${widget.title}`} meta={widget}>
-                {content}
-              </ErrorBoundary>
-            </div>
+        let content = widgetComponent;
+        if (onlyRenderInsideViewport) {
+          content = (
+            <TrackVisibility once offset={300} tag="div">
+              {({ isVisible }) => isVisible && widgetComponent}
+            </TrackVisibility>
           );
-        })}
-      </ReactGridLayout>
-    </>
+        }
+
+        return (
+          <div
+            key={widget.id}
+            className={locals.widget}
+            id={getWidgetId(widget.id)}
+            style={disabledTransitions ? disabledTransitionStyle : undefined}
+          >
+            <ErrorBoundary name={`Custom dashboard widget: ${widget.title}`} meta={widget}>
+              {content}
+            </ErrorBoundary>
+          </div>
+        );
+      })}
+    </ReactGridLayout>
   );
 
   function forwardLayoutChange(layout) {
