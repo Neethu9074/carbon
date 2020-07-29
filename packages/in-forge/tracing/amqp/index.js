91e5ed081f185acc1d93d488792bf4eedfa82e8e
Ben Blackmore <bripkens@gmail.com>
2020-07-29 08:06:18 +0200

    Fix fallback for missing spans and add missing AMQP span detail view

---
+++ packages/in-forge/tracing/amqp/index.js
@@ -0,0 +1,17 @@
+import { registerSpanDefinition } from 'in-sdk/tracing';
+
+registerSpanDefinition({
+  type: 'amqp',
+  category: 'messaging',
+
+  typeName: {
+    singular: 'AMQP Message',
+    plural: 'AMQP Messages'
+  },
+
+  detailView: 'AmqpSpanDetailView',
+
+  getLabel(span) {
+    return span.getIn(['data', 'amqp', 'command']);
+  }
+});
