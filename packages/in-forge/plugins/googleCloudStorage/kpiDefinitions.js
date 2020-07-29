dc29a00a16f888f01dd1f9ffd42ca1acf1056193
Igor Milanovic <igor.milanovic@instana.com>
2020-07-29 21:10:42 +0200

    fix test for kpi definitions

--- packages/in-forge/plugins/googleCloudStorage/kpiDefinitions.js
+++ packages/in-forge/plugins/googleCloudStorage/kpiDefinitions.js
@@ -1,19 +1,19 @@
-import { number, bytes } from 'in-services/formatters/number';
+import { number, bytesZeroDecimalPlaces } from 'in-services/formatters/number';
 
 export default [
   {
-    label: 'Requests',
+    label: 'Requests per second',
     metric: 'api.request_count',
-    formatter: number
+    formatter: number.detailed
   },
   {
     label: 'Objects Count',
     metric: 'storage.object_count',
-    formatter: number
+    formatter: number.compact
   },
   {
     label: 'Objects Size',
     metric: 'storage.total_bytes',
-    formatter: bytes.detailed
+    formatter: bytesZeroDecimalPlaces
   }
 ];
