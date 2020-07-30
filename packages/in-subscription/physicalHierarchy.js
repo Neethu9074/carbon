b7824f7afc15645eda20c9d75d4434529b4835e9
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-07-30 14:00:29 +0200

    Fix rounding of timestamp when requesting physical-hierarchy (#3514)
    * Fix rounding of timestamp when requesting physical-hierarchy
    
    Follow up of ui-client#3494:
    - similar to the other PR, the applied rounding could lead to no getting any result, in cases where an entity just went online / offline,
      in cases where the rounding would lead to the timestamp being (slightly) outside of the snapshot's lifetime (snapshot.from - snapshot.to)
    
    * Implemented review feedback

--- packages/in-subscription/physicalHierarchy.js
+++ packages/in-subscription/physicalHierarchy.js
@@ -1,21 +1,15 @@
 import { List } from 'immutable';
 
-import { roundToNearestTimeBlock } from 'in-subscription/util';
 import createSubscription from 'in-subscription/subscription';
-import { generateStableHash } from 'in-services/util/id';
 
 export default createSubscription({
   eventId: 'subscribe-physical-hierarchy',
-
-  getId({ snapshotId, timeConfig, includeCluster, includeKubernetes }) {
-    return snapshotId + generateStableHash(roundToNearestTimeBlock(timeConfig)) + includeCluster + includeKubernetes;
-  },
 
   getData(subscriptionId, { snapshotId, timeConfig, includeCluster, includeKubernetes }) {
     return {
       subscriptionId,
       snapshotId,
-      timeConfig: roundToNearestTimeBlock(timeConfig),
+      timeConfig,
       clusterIncluded: includeCluster,
       kubernetesIncluded: includeKubernetes
     };
