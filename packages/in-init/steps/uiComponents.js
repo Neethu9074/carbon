5452202cbb1a05bd8f8fb7615f38b286f61d4858
Ben Blackmore <bripkens@gmail.com>
2020-08-05 08:57:47 +0200

    Bug fix: Actually initialize the mixpanel trackers

--- packages/in-init/steps/uiComponents.js
+++ packages/in-init/steps/uiComponents.js
@@ -15,6 +15,7 @@ require('in-services/favicon').init();
 require('in-components/ErrorBoundary/store').init();
 require('in-services/debuggingBackchannel').init();
 require('in-services/tracking/viewTracking').init();
+require('in-services/tracking/mixpanel').init();
 require('in-services/tracking/appcues').init();
 require('in-stores/isMonitoring').init();
 require('in-new-components/SelectedElementHighlighter').init();
