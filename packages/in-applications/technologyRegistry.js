dcc09c95e691ed440f217719ac040574e144e3e7
Ben Blackmore <bripkens@gmail.com>
2020-08-18 09:07:44 +0200

    Fix async initialization for the technology registry (plugin system logic outside of sdk/forge package is confusing)

--- packages/in-applications/technologyRegistry.js
+++ packages/in-applications/technologyRegistry.js
@@ -1,5 +1,6 @@
 import { get } from 'lodash';
 
+import { ensureInfraPluginsAreEvaluated } from 'in-sdk/asyncEvaluation';
 import { compareIgnoreCase } from 'in-services/util/string';
 
 const registry = {};
@@ -13,14 +14,17 @@ export function addToRegistry({ id, label, icon }) {
 }
 
 export function getIconSvgPath(id) {
+  ensureInfraPluginsAreEvaluated();
   return get(registry, [id, 'icon']);
 }
 
 export function getLabel(id) {
+  ensureInfraPluginsAreEvaluated();
   return get(registry, [id, 'label']);
 }
 
 export function getTechnologyComboBoxItems(restrict = null) {
+  ensureInfraPluginsAreEvaluated();
   return Object.keys(registry)
     .filter(key => restrict == null || restrict.indexOf(key) !== -1)
     .map(key => ({ value: key, label: registry[key].label }))
