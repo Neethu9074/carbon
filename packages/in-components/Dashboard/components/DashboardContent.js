41b58bc36a1d0477dcbc3ae59abe96dfd1b7df76
Miel Donkers <miel.donkers@instana.com>
2020-08-18 22:34:51 +0200

    Update to commit `ec9626767`, now actually fixing it
    The `if` statement was not correct and led to errors in the UI.
    Fixed properly now.

--- packages/in-components/Dashboard/components/DashboardContent.js
+++ packages/in-components/Dashboard/components/DashboardContent.js
@@ -59,8 +59,9 @@ export default connectTo(
     ]);
 
     if (
-      ((!DashboardImpl || !SidebarImpl) && !snapshot && !showVersionSelector) ||
-      (snapshot && snapshotId !== snapshot.get('id'))
+      (!snapshot && !showVersionSelector) ||
+      (snapshot && snapshotId !== snapshot.get('id')) ||
+      (snapshot && (!DashboardImpl || !SidebarImpl))
     ) {
       return (
         <div className={locals.loadingIndicatorWrapper}>
