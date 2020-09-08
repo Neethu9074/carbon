b788986ce6ac738b933d6fc8bf3f4833cbe3ca6e
Jean Detoeuf <jean.detoeuf@instana.com>
2020-09-08 05:21:19 -0400

    fetch tag catalog for group configurator (#4044)

--- packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.js
+++ packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.js
@@ -1,82 +1,6 @@
 import { createGroupingConfigurator } from 'in-new-components/GroupingConfigurator';
+import { getApplicationTagCatalog } from 'in-applications/api/catalog';
 import { successObservableFactory } from 'in-services/util/result';
-
-const tagCatalog = {
-  tags: [
-    {
-      name: 'service.name',
-      type: 'STRING',
-      canApplyToSource: true,
-      canApplyToDestination: true
-    },
-    {
-      name: 'endpoint.name',
-      type: 'STRING',
-      canApplyToSource: true,
-      canApplyToDestination: true
-    },
-    {
-      name: 'call.type',
-      type: 'STRING'
-    }
-  ],
-
-  tagTree: [
-    {
-      type: 'LEVEL',
-      label: 'Application',
-      children: [
-        {
-          type: 'LEVEL',
-          label: 'Endpoint',
-          icon: 'lib_kubernetes_cluster',
-          children: [
-            {
-              type: 'TAG',
-              label: 'Name',
-              icon: 'lib_kubernetes_label',
-              description: 'String - Cluster´s label',
-              tagName: 'endpoint.name'
-            }
-          ]
-        },
-        {
-          type: 'LEVEL',
-          label: 'Service',
-          icon: 'lib_kubernetes_namespace',
-          children: [
-            {
-              type: 'TAG',
-              label: 'Name',
-              icon: 'lib_kubernetes_label',
-              description: 'String - Namespace Name',
-              tagName: 'service.name'
-            }
-          ]
-        }
-      ]
-    },
-    {
-      type: 'LEVEL',
-      label: 'Call',
-      children: [
-        {
-          type: 'LEVEL',
-          label: 'Call',
-          children: [
-            {
-              type: 'TAG',
-              label: 'Type',
-              icon: 'lib_views_tag',
-              description: 'Type of entity',
-              tagName: 'call.type'
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
 
 const suggestions = ['k8s-demo-cluster', 'sb-test-cluster', 'kube-node-lease', 'kube-public'];
 
@@ -84,9 +8,7 @@ const {
   GroupingConfigurator,
   isGroupingConfigurationValid: isGroupingConfigurationValidInternal
 } = createGroupingConfigurator({
-  // see dedicated TagCatalog story for more information about the
-  // TagCatalog structure.
-  getTagCatalog: successObservableFactory(tagCatalog),
+  getTagCatalog: getApplicationTagCatalog,
 
   getSuggestions: successObservableFactory({ suggestions, totalHits: suggestions.length + 10 })
 });
