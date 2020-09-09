53852fb033b7bbb855026892f0bfa2bd728ea0e3
Peter Giacomo Lombardo <nosis@rbx.run>
2020-09-09 17:07:39 +0200

    PythonRuntime: Add new snapshot fields to sidebar (#4060)

--- packages/in-forge/plugins/pythonRuntimePlatform/Info.js
+++ packages/in-forge/plugins/pythonRuntimePlatform/Info.js
@@ -8,10 +8,13 @@ export default function PythonInfo({ snapshot }) {
   return (
     <DescriptionList>
       <DescriptionItem title="Name">{data.get('snapshot.name')}</DescriptionItem>
-      <DescriptionItem title="Runtime Version">{data.get('snapshot.version')}</DescriptionItem>
       <DescriptionItem title="Flavor">{data.get('snapshot.f')}</DescriptionItem>
+      <DescriptionItem title="Runtime Version">{data.get('snapshot.version')}</DescriptionItem>
       <DescriptionItem title="Architecture">{data.get('snapshot.a')}</DescriptionItem>
       <DescriptionItem title="Process ID">{data.get('pid')}</DescriptionItem>
+      <DescriptionItem title="Framework">{data.get('snapshot.fw')}</DescriptionItem>
+      <DescriptionItem title="Activation Method">{data.get('snapshot.m')}</DescriptionItem>
+      <DescriptionItem title="Instana Package Version">{data.get('snapshot.iv')}</DescriptionItem>
     </DescriptionList>
   );
 }
