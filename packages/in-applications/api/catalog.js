72c0fea94a1a657cf8cfc617967fb34b9e20bc76
Yiquan Zhou <yiquan.zhou@instana.com>
2020-09-10 16:51:20 +0200

    fetch ap tags with from timestamp query param (#4064)
    * fetch ap tags with from timestamp query param
    
    * code review

--- packages/in-applications/api/catalog.js
+++ packages/in-applications/api/catalog.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import createObservable from 'in-services/http/observableHttpResult';
 import http from 'in-services/http';
 
@@ -5,12 +7,23 @@ const basePath = '/api/application-monitoring/catalog';
 
 // observables
 
-export function getApplicationTagCatalog() {
+export function getApplicationTagCatalog({ timeConfig }) {
+  const from = timeConfig ? (timeConfig.to || Date.now()) - timeConfig.windowSize : null;
   return createObservable(
     http({
       method: 'GET',
       maxRetries: 3,
-      url: basePath
+      url: basePath,
+      queryParams: {
+        // round down the from timestamp to the beginning of the week to make the caching more efficient
+        from: roundDownToWeek(from)
+      }
     })
   );
+}
+
+function roundDownToWeek(timestamp) {
+  return moment(timestamp)
+    .startOf('week')
+    .valueOf();
 }
