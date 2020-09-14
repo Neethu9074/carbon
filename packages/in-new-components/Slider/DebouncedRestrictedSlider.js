fd21bac6f40f49a809e21b9a4fb8e8f4915b80a2
Mario Schwede <mario.schwede@instana.com>
2020-09-14 12:41:48 +0200

    Improve smart-alert debouncing (#4078)
    - Add maxWait of 1s to DebouncedDistinctSlider.js.js.
    - Add prop-types to DebouncedRestrictedSlider.js.
    - Add DebouncedRestrictedSlider.js to story-book.
    - Reduce the memoization of the alert-preview to 1s.

--- packages/in-new-components/Slider/DebouncedRestrictedSlider.js
+++ packages/in-new-components/Slider/DebouncedRestrictedSlider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { restrictedSliderPropTypes } from 'in-new-components/Slider/proptypes';
 import RestrictedSlider from 'in-new-components/Slider/RestrictedSlider';
 import useDebouncedValue from 'in-hooks/useDebouncedValue';
 
@@ -7,3 +8,5 @@ export default function DebouncedRestrictedSlider(props) {
   const result = useDebouncedValue(props.value, props.onChange, 500);
   return <RestrictedSlider {...props} value={result.value} onChange={result.onChange} />;
 }
+
+DebouncedRestrictedSlider.propTypes = restrictedSliderPropTypes;
---
+++ packages/in-new-components/Slider/proptypes.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export const restrictedSliderPropTypes = {
+  marks: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  max: PropTypes.number.isRequired,
+  min: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  valueLabelFormat: PropTypes.func,
+  valueLabelDisplay: PropTypes.oneOf(['on', 'off', 'auto']),
+  disabled: PropTypes.bool,
+  style: PropTypes.any,
+  value: PropTypes.number.isRequired
+};
