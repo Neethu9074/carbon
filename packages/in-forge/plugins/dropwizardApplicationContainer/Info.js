b9ddee366deebbb779ffd3ddb57398d99e4f4fa6
Goran Ceko <goran.ceko@instana.com>
2020-09-15 08:42:44 +0200

    Add metric counts to Dropwizard sidebar (#4065)
    * Add metric counts to Dropwizard sidebar
    
    * Fix timers and histograms count calculation

--- packages/in-forge/plugins/dropwizardApplicationContainer/Info.js
+++ packages/in-forge/plugins/dropwizardApplicationContainer/Info.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
+import ProcessStartedAtDescriptionItem from 'in-sdk/components/sidebar/ProcessStartedAtDescriptionItem';
 import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
+import { emptyList } from 'in-services/fixedImmutables';
 
-export default function SpringbootInfo({ snapshot }) {
+export default function Info({ snapshot }) {
+  const data = snapshot.get('data');
+
+  const counters = countMetrics(snapshot, 'metrics.counters');
+  const gauges = countMetrics(snapshot, 'metrics.gauges');
+  const histograms = countMetrics(snapshot, 'metrics.histograms') / 3; // mean, 50th, 99th
+  const meters = countMetrics(snapshot, 'metrics.meters');
+  const summaries = countMetrics(snapshot, 'metrics.summaries');
+  const timers = countMetrics(snapshot, 'metrics.timers') / 4; // rate, mean, 50th, 99th
+
   return (
     <DescriptionList>
-      <DescriptionItem title="Dropwizard Version">{snapshot.getIn(['data', 'version'])}</DescriptionItem>
+      <DescriptionItem title="Dropwizard Version">{data.get('version')}</DescriptionItem>
+
+      <DescriptionItem title="Counters">{counters}</DescriptionItem>
+      <DescriptionItem title="Gauges">{gauges}</DescriptionItem>
+      <DescriptionItem title="Histograms">{histograms}</DescriptionItem>
+      <DescriptionItem title="Meters">{meters}</DescriptionItem>
+      <DescriptionItem title="Summaries">{summaries}</DescriptionItem>
+      <DescriptionItem title="Timers">{timers}</DescriptionItem>
+
+      <DescriptionItem title="Process ID">{data.get('pid')}</DescriptionItem>
+      <ProcessStartedAtDescriptionItem snapshotId={snapshot.get('id')} />
     </DescriptionList>
   );
+}
+
+function countMetrics(snapshot, prefix) {
+  const metricCount = snapshot.get('metricIds', emptyList).filter(m => m.startsWith(prefix)).size;
+
+  if (metricCount > 0) {
+    return metricCount;
+  }
+
+  return snapshot.getIn(['data', prefix], emptyList).size;
 }
