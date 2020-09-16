7621ef13c3105ec04526bff990a39d0c05d5e19d
Will James <william.james@instana.com>
2020-09-16 09:29:16 -0500

    Always show Kubernetes Cluster debugging info (#4098)

--- packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
+++ packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
@@ -1,15 +1,10 @@
 import React, { Fragment } from 'react';
 
-import { isInternalVisible$ } from 'in-new-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore';
 import ComponentStatusTable from 'in-kubernetes/Dashboards/Cluster/tabs/ComponentStatusTable';
 import Debugging from 'in-kubernetes/Dashboards/Cluster/tabs/Debugging';
 import { Row, Col } from 'in-new-components/layout/Grid';
-import connectTo from 'in-hoc/connectTo';
 
-export default connectTo({ isInternalVisible: isInternalVisible$ }, function Details({
-  isInternalVisible,
-  data: cluster
-}) {
+export default function Details({ data: cluster }) {
   return (
     <Fragment>
       <Row>
@@ -17,13 +12,11 @@ export default connectTo({ isInternalVisible: isInternalVisible$ }, function Det
           <ComponentStatusTable cluster={cluster} />
         </Col>
       </Row>
-      {isInternalVisible && (
-        <Row>
-          <Col lg={12}>
-            <Debugging cluster={cluster} />
-          </Col>
-        </Row>
-      )}
+      <Row>
+        <Col lg={12}>
+          <Debugging cluster={cluster} />
+        </Col>
+      </Row>
     </Fragment>
   );
-});
+}
