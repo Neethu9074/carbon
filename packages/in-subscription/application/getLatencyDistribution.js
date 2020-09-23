2a3df314fcfa0d8b5e0b95e8490afd40dee12ddf
Jean Detoeuf <jean.detoeuf@instana.com>
2020-09-23 09:23:26 -0400

    removing more latency distribution files

--- packages/in-subscription/application/getLatencyDistribution.js
+++
@@ -1,7 +0,0 @@
-import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
-
-export default createResultSubscriptionFactory({
-  eventId: 'getLatencyDistribution',
-  memoizeFor: 5000,
-  trackSubscriptionStatistics: true
-});
