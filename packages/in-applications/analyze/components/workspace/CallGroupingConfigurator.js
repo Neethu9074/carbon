8732a0ac3608e203d2d03f5707a4401af14106b6
Jean Detoeuf <jean.detoeuf@instana.com>
2020-09-29 12:13:49 -0400

    get tag catalog without disabled tags (#4178)
    tag catalog without disabled tags on grouping for calls

--- packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.js
+++ packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.js
@@ -1,3 +1,5 @@
+import { find } from 'lodash';
+
 import { createGroupingConfigurator } from 'in-new-components/GroupingConfigurator';
 import { getApplicationTagCatalog } from 'in-applications/api/catalog';
 import { successObservableFactory } from 'in-services/util/result';
@@ -8,10 +10,28 @@ const {
   GroupingConfigurator,
   isGroupingConfigurationValid: isGroupingConfigurationValidInternal
 } = createGroupingConfigurator({
-  getTagCatalog: getApplicationTagCatalog,
-
+  getTagCatalog: props =>
+    getApplicationTagCatalog(props).map(response => ({
+      ...response,
+      data: response.data && {
+        ...response.data,
+        tagTree: filteredTree(response.data.tagTree, response.data.tags)
+      }
+    })),
   getSuggestions: successObservableFactory({ suggestions, totalHits: suggestions.length + 10 })
 });
+
+function filteredTree(tree, tags) {
+  return tree
+    .map(element => {
+      if (element.type === 'LEVEL') {
+        const children = filteredTree(element.children, tags).filter(Boolean);
+        return children.length > 0 && { ...element, children: children };
+      }
+      return find(tags, tag => tag.name === element.tagName && tag.allowGroupingForCalls) && element;
+    })
+    .filter(Boolean);
+}
 
 export default GroupingConfigurator;
 
