d7c02be9cae0069fddad7e320f949eea3938ca75
Stepan Herold <stepan.herold@instana.com>
2020-10-02 15:48:16 +0200

    The active tab selection and active metric selection when time shift is on doesn't work as expected (#4227)
    # Why
    
    The active tab selection and active metric selection when time shift is on doesn't work as expected on AP dashboards.
    
    Steps to reproduce are:
    - I reload the "All Services" AP dashboard
    - Set the Latency to be "Distribution"
    - Select "Time Shift:  Previous Window"
    - Latency distribution chart shows time shift for 50th percentile (first item on the menu) rather than the "Distribution (last item on the menu).
    
    # What
    
    Synchronizing active tab and metric selection.

--- packages/in-applications/Dashboards/commonComponents/CallsAndHttp.js
+++ packages/in-applications/Dashboards/commonComponents/CallsAndHttp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ComboChartMetricSelector, TabChartSelector } from 'in-applications/Dashboards/commonComponents/ChartSelectors';
 import CallsErrorsChart from 'in-applications/Dashboards/commonComponents/CallsErrorsChart';
@@ -60,8 +60,27 @@ export default function CallsAndHttp({
   renderPostChartContent,
   renderPostChartContentHttpStatus
 }) {
+  const findMetricByTab = tab => metrics.find(m => m.tab === tab).value;
+  const findTabByMetric = metric => metrics.find(m => m.value === metric).tab;
+
   const [activeTab, setActiveTab] = useState(tabs[0]);
-  const [activeMetric, setActiveMetric] = useState(metrics[0].value);
+  const [activeMetric, setActiveMetric] = useState(findMetricByTab(activeTab));
+
+  useEffect(() => {
+    // if the active tab changes, the active metric must be updated
+    if (activeTab !== findTabByMetric(activeMetric)) {
+      setActiveMetric(findMetricByTab(activeTab));
+    }
+  }, [activeTab]);
+
+  useEffect(() => {
+    // if the active metric changes, the active tab may need to be updated
+    const activeMetricTab = findTabByMetric(activeMetric);
+    if (activeMetricTab !== activeTab) {
+      setActiveTab(activeMetricTab);
+    }
+  }, [activeMetric]);
+
   const timeShiftConfig = useTimeShiftConfig();
 
   const header = timeShiftConfig.offset ? (
