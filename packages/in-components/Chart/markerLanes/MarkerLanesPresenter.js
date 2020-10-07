2116e99943cc3f33c0437548c956c11df40c2002
Marco Schaefer <47627413+codecapitano@users.noreply.github.com>
2020-10-07 09:35:53 +0200

    Control label visibility on per instance basis of MarkerLanesPresenter (#4265)

--- packages/in-components/Chart/markerLanes/MarkerLanesPresenter.js
+++ packages/in-components/Chart/markerLanes/MarkerLanesPresenter.js
@@ -1,22 +1,12 @@
-import React, { useState, Children, cloneElement, useRef, useEffect } from 'react';
-import { create } from 'reactive-observables';
+import React, { useState, Children, cloneElement, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-import { hasMarkersToRenderSignal$ } from 'in-components/Chart/markerLanes/MarkerLane/MarkerLane';
 import { stopPropagationAndPreventDefault } from 'in-services/util/function';
 import { getBlockSizeMillis } from 'in-services/util/dynamicAggregation';
 import { propTypeTimeConfig } from 'in-stores/time/config';
 import { sizes } from 'in-components/SvgIcon/SvgIcon';
 
 import locals from './MarkerLanesPresenter.mless';
-
-const markerLaneLabelVisibleSignalSrc$ = create();
-
-export function setMarkerLaneLabelVisibility(labelVisible) {
-  markerLaneLabelVisibleSignalSrc$.emit(labelVisible);
-}
-
-export const markerLaneLabelVisibleSignal$ = markerLaneLabelVisibleSignalSrc$.emit(false);
 
 const minBlockWidth = sizes.xs;
 export default function MarkerLanesPresenter({
@@ -30,35 +20,27 @@ export default function MarkerLanesPresenter({
 
   const [labelAlignment, setLabelAligment] = useState('left');
   const [hasMarkersToRender, setHasMarkersToRender] = useState(false);
-
-  let laneWithMarkersCount = 0;
-
-  useEffect(() => {
-    let noMarkersInAnyLaneSignalSub = hasMarkersToRenderSignal$.subscribe(hasMarkers => {
-      if (hasMarkers) laneWithMarkersCount++;
-      setHasMarkersToRender(laneWithMarkersCount > 0);
-      setMarkerLaneLabelVisibility(laneWithMarkersCount === 0);
-    });
-    return () => {
-      noMarkersInAnyLaneSignalSub.dispose();
-      noMarkersInAnyLaneSignalSub = null;
-    };
-  }, []);
+  const [laneLabelsVisible, setLaneLabelsVisibility] = useState(false);
 
   return (
     <div
       className={locals.markerLanesContainer}
       onMouseLeave={e => {
         stopPropagationAndPreventDefault(e);
-        if (hasMarkersToRender) markerLaneLabelVisibleSignal$.emit(false);
+        if (hasMarkersToRender) setLaneLabelsVisibility(false);
       }}
     >
       <MarkerLanesWrapper
+        lanes={children}
+        setLaneLabelsVisibility={isVisible => setLaneLabelsVisibility(isVisible)}
         labelAlignment={labelAlignment}
         chartWidth={chartWidth}
         granularity={granularity}
         chartBucketWidth={chartBucketWidth}
-        remainingProps={remainingProps}
+        laneLabelsVisible={(hasMarkersToRender && laneLabelsVisible) || (!hasMarkersToRender && !laneLabelsVisible)}
+        onLaneHasMarkersToRender={() => setHasMarkersToRender(true)}
+        hasMarkersToRender={hasMarkersToRender}
+        {...remainingProps}
       />
       <div
         className={locals[labelAlignment]}
@@ -69,42 +51,53 @@ export default function MarkerLanesPresenter({
       />
     </div>
   );
+}
 
-  function MarkerLanesWrapper({ labelAlignment, chartWidth, granularity, chartBucketWidth, remainingProps }) {
-    const isClustered = chartBucketWidth < minBlockWidth - 2;
+function MarkerLanesWrapper({
+  lanes,
+  setLaneLabelsVisibility,
+  labelAlignment,
+  chartWidth,
+  granularity,
+  chartBucketWidth,
+  laneLabelsVisible,
+  hasMarkersToRender,
+  ...remainingProps
+}) {
+  const isClustered = chartBucketWidth < minBlockWidth - 2;
 
-    const markerLanesWrapperRef = useRef(null);
-    return (
-      <div
-        ref={markerLanesWrapperRef}
-        className={locals.markerLanesWrapper}
-        onMouseEnter={e => {
-          stopPropagationAndPreventDefault(e);
-          markerLaneLabelVisibleSignal$.emit(true);
-        }}
-      >
-        {Children.toArray(children)
-          .filter(Boolean)
-          .map(child => {
-            return cloneElement(child, {
-              ...remainingProps,
-              labelAlignment,
-              clusterSizeMillis: isClustered
-                ? getClusterSizeMillis({
-                    width: chartWidth || markerLanesWrapperRef.current?.getBoundingClientRect()?.width,
-                    windowSize: remainingProps.timeConfig.windowSize,
-                    granularity
-                  })
-                : granularity,
-              isClustered,
-              chartBucketWidth,
-              chartWidth,
-              granularity
-            });
-          })}
-      </div>
-    );
-  }
+  const markerLanesWrapperRef = useRef(null);
+  return (
+    <div
+      ref={markerLanesWrapperRef}
+      className={locals.markerLanesWrapper}
+      onMouseEnter={e => {
+        stopPropagationAndPreventDefault(e);
+        if (hasMarkersToRender) setLaneLabelsVisibility(true);
+      }}
+    >
+      {Children.toArray(lanes)
+        .filter(Boolean)
+        .map(child => {
+          return cloneElement(child, {
+            ...remainingProps,
+            labelAlignment,
+            clusterSizeMillis: isClustered
+              ? getClusterSizeMillis({
+                  width: chartWidth || markerLanesWrapperRef.current?.getBoundingClientRect()?.width,
+                  windowSize: remainingProps.timeConfig.windowSize,
+                  granularity
+                })
+              : granularity,
+            isClustered,
+            chartBucketWidth,
+            chartWidth,
+            granularity,
+            laneLabelsVisible
+          });
+        })}
+    </div>
+  );
 }
 
 function getClusterSizeMillis({ windowSize, width, granularity }) {
