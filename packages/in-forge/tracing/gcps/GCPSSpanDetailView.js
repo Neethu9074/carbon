c7a8789f42cb4f9ef6f145c3ea5894a6ebcce3f2
Andrey Slotin <andrey.slotin@instana.com>
2020-10-13 14:24:36 +0200

    Make GCP PubSub topic optional

--- packages/in-forge/tracing/gcps/GCPSSpanDetailView.js
+++ packages/in-forge/tracing/gcps/GCPSSpanDetailView.js
@@ -4,14 +4,15 @@ import ErrorDescriptionItem from 'in-sdk/components/traceDetails/ErrorDescriptio
 import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
 
 export default function GCPSSpanDetailView({ span }) {
-  const googleCloudPubSub = span.getIn(['data', 'gcps']);
-  const subscription = googleCloudPubSub.get('sub');
+  const googleCloudPubSub = span.getIn(['data', 'gcps']),
+    topic = googleCloudPubSub.get('top'),
+    subscription = googleCloudPubSub.get('sub');
 
   return (
     <Dl>
       <Di title="Operation">{googleCloudPubSub.get('op')}</Di>
       <Di title="Project ID">{googleCloudPubSub.get('projid')}</Di>
-      <Di title="Topic">{googleCloudPubSub.get('top')}</Di>
+      {topic && <Di title="Topic">{googleCloudPubSub.get('top')}</Di>}
       {subscription && <Di title="Subscription">{subscription}</Di>}
       <ErrorDescriptionItem error={googleCloudPubSub.get('error')} />
     </Dl>
