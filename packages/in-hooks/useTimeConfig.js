9ad82c63798a7adecd99a2b5505f5019941102bd
Simon Thiele <simon.thiele.st@googlemail.com>
2020-10-13 18:00:42 +0200

    use useEffect and useState to create a static reference for the timeConfig hook (#4344)
    Co-authored-by: simonThiele <simon.thiele.st@gmail.com>

--- packages/in-hooks/useTimeConfig.js
+++ packages/in-hooks/useTimeConfig.js
@@ -1,9 +1,17 @@
-import { useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import { TimeConfigContext } from 'in-stores/time/TimeConfigContext';
 
 // Just a small alias to make usage of time configs in React components
 // a lot more explicit.
 export default function useTimeConfig() {
-  return useContext(TimeConfigContext);
+  const timeConfig = useContext(TimeConfigContext);
+  const [memoizedTimeConfig, setMemoizedTimeConfig] = useState(timeConfig);
+  useEffect(() => setMemoizedTimeConfig(timeConfig), [
+    timeConfig.to,
+    timeConfig.focusedMoment,
+    timeConfig.windowSize,
+    timeConfig.autoRefresh
+  ]);
+  return memoizedTimeConfig;
 }
