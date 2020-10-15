0a75ba57a64881740d97198f8c39fe05a6cb0614
Nimrod Wandera <wanderanimrod@gmail.com>
2020-10-15 08:35:39 -0500

    set number of items in groups to 20

--- packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
+++ packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
@@ -256,7 +256,7 @@ function ExpandedGroup({ group, backendQueryModel, timeConfig, type, metrics, av
       numSkeletonRows={Math.min(group.count, 5)}
       availableMetrics={availableMetrics}
       timeConfig={timeConfig}
-      retrievalSize={10}
+      retrievalSize={20}
       metrics={metrics}
       order={order}
       type={type}
