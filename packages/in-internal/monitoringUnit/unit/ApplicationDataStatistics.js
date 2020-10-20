1ed4928b5fef10b1d0aba1aa73b986519f485b87
Yoann Buch <yoann.buch@instana.com>
2020-10-20 14:53:48 +0200

    Internal monitoring: span dropping per appdata-processor instance (#4342)
    Why
    We have an internal dashboard which tell us how much we're dropping per tenant unit.
    
    However we sometimes run multiple instances of appdata-processor per tenant unit, and then it becomes hard to know whether we're dropping across all instances or some.
    
    This is important, because if we're dropping across all instances the fix is usually to scale out.
    However if only a few instances are dropping, then that means that the data partitioning is imbalanced due to large traces (partitioning key is the trace id). And there the fix is definitely not scaling out.
    
    What
    Add new chart when there are multiple instances so we can more easily tell whether we're dropping because of large traces or not.
    
    Example 1: Adidas, dropping due to large traces (e.g. 1 instance is dropping 60% while the others 0%)
    Screenshot from 2020-10-13 12-29-14
    
    Example 2: ebayturkey, dropping because overall load is too high
    Screenshot from 2020-10-13 12-29-26
    
    Example 3: tipico, maybe both problems are at play
    Screenshot from 2020-10-13 12-30-11
    
    References
    https://www.notion.so/instana/ZD-15116-Adidas-Span-dropping-234988eabbce428991c43a5fccb0ba8d

--- packages/in-internal/monitoringUnit/unit/ApplicationDataStatistics.js
+++ packages/in-internal/monitoringUnit/unit/ApplicationDataStatistics.js
@@ -1,110 +1,142 @@
 import React, { Fragment } from 'react';
 import theme from 'in-themes';
 
+import { getDropwizardWithContext } from 'in-internal/monitoringUnit/dataRetrieval';
 import DashboardSection from 'in-sdk/components/dashboard/DashboardSection';
 import { percentage, number, millis } from 'in-services/formatters/number';
 import Chart from 'in-components/Chart/InfrastructureMetricChartBehavior';
+import connectTo from 'in-hoc/connectTo';
 
-export default function ApplicationDataStatistics({ timeConfig, tenantUnitId }) {
-  return (
-    <Fragment>
-      <DashboardSection title={`AppData-Processor Instances`}>
-        <Chart
-          snapshotId={tenantUnitId}
-          timeConfig={timeConfig}
-          y1={{
-            formatter: number.compact,
-            metrics: [`appdata-processor.instances`],
-            labels: ['AppData-Processor Processor Instances'],
-            type: 'stackedArea'
-          }}
-        />
-      </DashboardSection>
+export default connectTo(
+  ({ tenant, unit }) => ({
+    appdata_processor_instances: getDropwizardWithContext(
+      'entity.label:"' + tenant + '-' + unit + '-appdata-processor"'
+    )
+  }),
+  function ApplicationDataStatistics({ timeConfig, tenantUnitId, appdata_processor_instances }) {
+    const appdata_processor_labels = appdata_processor_instances.map(instance => instance.host.get('label'));
 
-      <DashboardSection title={`Backend Span Dropping (sum across instances)`}>
-        <Chart
-          snapshotId={tenantUnitId}
-          timeConfig={timeConfig}
-          y1={{
-            min: 0,
-            max: 1,
-            formatter: percentage.detailed,
-            metrics: [`appdata-processor.spanDropping`],
-            labels: ['Dropping rate'],
-            type: 'line'
-          }}
-          y2={{
-            min: 0,
-            formatter: number.compact,
-            metrics: [
-              `appdata-processor.processedSpans`,
-              `appdata-processor.droppedSpansDueToConfiguration`,
-              `appdata-processor.droppedSpansDueToConsistentDropping`,
-              `appdata-processor.droppedSpansDueToBackpressure`,
-              `appdata-processor.droppedSpansDueToHardBackpressure`
-            ],
-            labels: [
-              'Processed',
-              'Dropped due to throttler',
-              'Dropped due to consistent dropping',
-              'Dropped due to backpressure',
-              'Dropped hard due to backpressure (random dropping)'
-            ],
-            colors: [
-              theme.lib.colors.success,
-              theme.lib.colors.red800,
-              theme.lib.colors.orange800,
-              theme.lib.colors.yellow800,
-              theme.lib.colors.pink800
-            ],
-            type: 'stackedArea'
-          }}
-        />
-      </DashboardSection>
+    return (
+      <Fragment>
+        <DashboardSection title={`AppData-Processor Instances`}>
+          <Chart
+            snapshotId={tenantUnitId}
+            timeConfig={timeConfig}
+            y1={{
+              formatter: number.compact,
+              metrics: [`appdata-processor.instances`],
+              labels: ['AppData-Processor Processor Instances'],
+              type: 'stackedArea'
+            }}
+          />
+        </DashboardSection>
 
-      <DashboardSection title={`Span latency (mean across instances)`}>
-        <Chart
-          snapshotId={tenantUnitId}
-          timeConfig={timeConfig}
-          y1={{
-            min: 0,
-            formatter: millis.compact,
-            metrics: [
-              `appdata-processor.spanLatency.mean`,
-              `appdata-processor.spanLatency.50th`,
-              `appdata-processor.spanLatency.99th`
-            ],
-            labels: ['Mean', '50th', '99th'],
-            type: 'line'
-          }}
-        />
-      </DashboardSection>
+        <DashboardSection title={`Backend Span Dropping (sum across instances)`}>
+          <Chart
+            snapshotId={tenantUnitId}
+            timeConfig={timeConfig}
+            y1={{
+              min: 0,
+              max: 1,
+              formatter: percentage.detailed,
+              metrics: [`appdata-processor.spanDropping`],
+              labels: ['Dropping rate'],
+              type: 'line'
+            }}
+            y2={{
+              min: 0,
+              formatter: number.compact,
+              metrics: [
+                `appdata-processor.processedSpans`,
+                `appdata-processor.droppedSpansDueToConfiguration`,
+                `appdata-processor.droppedSpansDueToConsistentDropping`,
+                `appdata-processor.droppedSpansDueToBackpressure`,
+                `appdata-processor.droppedSpansDueToHardBackpressure`,
+                `appdata-processor.droppedSpansDueToPerTraceConfiguration`
+              ],
+              labels: [
+                'Processed',
+                'Dropped due to global throttler',
+                'Dropped due to consistent dropping',
+                'Dropped due to backpressure',
+                'Dropped hard due to backpressure (random dropping)',
+                'Dropped due to trace throttler'
+              ],
+              colors: [
+                theme.lib.colors.success,
+                theme.lib.colors.red800,
+                theme.lib.colors.orange800,
+                theme.lib.colors.yellow800,
+                theme.lib.colors.pink800,
+                theme.lib.colors.purple800
+              ],
+              type: 'stackedArea'
+            }}
+          />
+        </DashboardSection>
 
-      <DashboardSection title={`Acceptor Rate-Limited Span Messages`}>
-        <Chart
-          snapshotId={tenantUnitId}
-          timeConfig={timeConfig}
-          y1={{
-            formatter: number.compact,
-            metrics: [`acceptor.droppedSpanMessagesRateLimited`],
-            labels: ['Acceptor Dropped Span Messages'],
-            type: 'stackedArea'
-          }}
-        />
-      </DashboardSection>
+        {appdata_processor_instances.length > 1 && (
+          <DashboardSection title={`Backend Span Dropping (per instance)`}>
+            <Chart
+              snapshotIds={appdata_processor_instances.map(instance => instance.dropwizard.get('id'))}
+              timeConfig={timeConfig}
+              minRollup={5000}
+              y1={{
+                min: 0,
+                max: 1,
+                formatter: percentage.detailed,
+                metrics: appdata_processor_instances.map(() => 'metrics.gauges.KPI.incoming.span_messages.error_rate'),
+                labels: appdata_processor_labels,
+                type: 'line'
+              }}
+            />
+          </DashboardSection>
+        )}
 
-      <DashboardSection title={`Serverless Acceptor Rate-Limited Span Messages`}>
-        <Chart
-          snapshotId={tenantUnitId}
-          timeConfig={timeConfig}
-          y1={{
-            formatter: number.compact,
-            metrics: [`serverless-acceptor.droppedSpanMessagesRateLimited`],
-            labels: ['Serverless Acceptor Dropped Span Messages'],
-            type: 'stackedArea'
-          }}
-        />
-      </DashboardSection>
-    </Fragment>
-  );
-}
+        <DashboardSection title={`Span latency (mean across instances)`}>
+          <Chart
+            snapshotId={tenantUnitId}
+            timeConfig={timeConfig}
+            y1={{
+              min: 0,
+              formatter: millis.compact,
+              metrics: [
+                `appdata-processor.spanLatency.mean`,
+                `appdata-processor.spanLatency.50th`,
+                `appdata-processor.spanLatency.99th`
+              ],
+              labels: ['Mean', '50th', '99th'],
+              type: 'line'
+            }}
+          />
+        </DashboardSection>
+
+        <DashboardSection title={`Acceptor Rate-Limited Span Messages`}>
+          <Chart
+            snapshotId={tenantUnitId}
+            timeConfig={timeConfig}
+            y1={{
+              formatter: number.compact,
+              metrics: [`acceptor.droppedSpanMessagesRateLimited`],
+              labels: ['Acceptor Dropped Span Messages'],
+              type: 'stackedArea'
+            }}
+          />
+        </DashboardSection>
+
+        <DashboardSection title={`Serverless Acceptor Rate-Limited Span Messages`}>
+          <Chart
+            snapshotId={tenantUnitId}
+            timeConfig={timeConfig}
+            y1={{
+              formatter: number.compact,
+              metrics: [`serverless-acceptor.droppedSpanMessagesRateLimited`],
+              labels: ['Serverless Acceptor Dropped Span Messages'],
+              type: 'stackedArea'
+            }}
+          />
+        </DashboardSection>
+      </Fragment>
+    );
+  }
+);
