7572bec4281f6a81d2a6589cdb9ae96e3327c143
Jean Detoeuf <jean.detoeuf@instana.com>
2020-10-22 09:36:04 -0400

    fix tag catalog filtering (#4423)

--- packages/in-applications/analyze/components/workspace/TagCatalogFilteredTree.js
+++ packages/in-applications/analyze/components/workspace/TagCatalogFilteredTree.js
@@ -2,7 +2,7 @@ export default function filteredTree(tree, tags, filter) {
   return tree
     .map(element => {
       if (element.type === 'LEVEL') {
-        const children = filteredTree(element.children, tags).filter(Boolean);
+        const children = filteredTree(element.children, tags, filter).filter(Boolean);
         return children.length > 0 && { ...element, children: children };
       }
       return (filter ? element.allowFiltering : element.allowGrouping) && element;
