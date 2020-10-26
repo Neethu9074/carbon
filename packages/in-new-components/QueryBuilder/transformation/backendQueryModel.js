d71b764292c533a27bdf9bce6a846345e03764a2
Ben Blackmore <ben.blackmore@instana.com>
2020-10-26 14:11:50 +0100

    Remove dead code

--- packages/in-new-components/QueryBuilder/transformation/backendQueryModel.js
+++ packages/in-new-components/QueryBuilder/transformation/backendQueryModel.js
@@ -175,46 +175,6 @@ function transformLevelsToLogicalExpressions(elements) {
   );
 }
 
-// function toTree(formModel, parentNode) {
-
-// }
-
-// function analyzeBracketLevel(formModel) {
-//   return toTagFilter(formModel[0]);
-// }
-
-// function mapTags(tags) {
-//   return collectExpression(tags, 0).expression.elements[0];
-// }
-
-// function collectExpression(tags, cursor) {
-//   let logicalOperator = OPERATOR_OR;
-//   const elements = [];
-
-//   while (cursor < tags.length) {
-//     const tag = tags[cursor++];
-
-//     if (tag.type === CLOSE_BRACKET) {
-//       break;
-//     }
-//     if (tag.type === OPEN_BRACKET) {
-//       const result = collectExpression(tags, cursor);
-//       elements.push(result.expression);
-//       cursor = result.cursor;
-//     } else if (tag.type === TAG) {
-//       elements.push(toTagFilter(tag));
-//     } else if (tag.type === CONJUNCTION) {
-//       logicalOperator = tag.logicalOperator;
-//     }
-//   }
-
-//   const expression = createTagFilterExpression(logicalOperator, elements);
-//   return {
-//     cursor,
-//     expression
-//   };
-// }
-
 function createTagFilterExpression(logicalOperator, elements) {
   return {
     type: EXPRESSION,
