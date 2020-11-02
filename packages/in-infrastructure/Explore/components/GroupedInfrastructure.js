4bf3a5adb93a12ec232e2c730c70746c63341131
Simon Thiele <simon.thiele.st@googlemail.com>
2020-11-02 08:52:42 +0100

    Nullcheck licenses 190 (#4506)
    Co-authored-by: simonThiele <simon.thiele.st@gmail.com>

--- packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
+++ packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
@@ -191,7 +191,7 @@ function columns({ groupBy, type, getParamsForGroup, metrics, timeConfig, granul
       metrics.map(({ label, metric, formatter = String, aggregation, percentageMetric }) => ({
         width: '12rem',
         getContent({ group }) {
-          const kpi = average(group.metrics[getMetricKey(metric, aggregation)]);
+          const kpi = average(group.metrics[metric]);
           const renderedLabel = <MetricLabel label={label} />;
           return (
             <SparkChart
