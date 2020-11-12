1d0d7e3d6951ae189d787b8dadeca3c425a48e1a
Lars Schmidt <lars.schmidt@instana.com>
2020-11-12 11:27:35 +0100

    remove ui debugging capability (#4589)

--- packages/in-init/steps/uiComponents.js
+++ packages/in-init/steps/uiComponents.js
@@ -11,7 +11,6 @@ require('in-services/unhandledErrors').init();
 require('in-stores/events').init();
 require('in-services/favicon').init();
 require('in-components/ErrorBoundary/store').init();
-require('in-services/debuggingBackchannel').init();
 require('in-services/tracking/tracking').init();
 require('in-services/tracking/mixpanel').init();
 require('in-services/tracking/appcues').init();
