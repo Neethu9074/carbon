d0c76c3779403693d69604c99d9c57564db85072
Stepan Herold <stepan.herold@instana.com>
2020-11-18 13:55:02 +0100

    Use the initial state instead of the stale state when deps change (#4614)
    # Why
    
    Switching between UA2 views _Calls (v2 beta)_ and _Traces (v2 beta)_ leads to an error. The error can be reproduced on test, but not when running ui-client locally. 
    
    The reason for this error is that the `useCursorPagination` hook returns a stale `getCalls` result instead of expected `getTraces` result.
    
    The `useCursorPagination` hook resets the `state` to the initial value using the `useEffect` hook, when any of the `useCursorPagination` hook's dependencies change. However, since the `useEffect` hook's callback is executed after the render phase, the `useCursorPagination` hook returns a result based on the old `state`.
    
    # What
    
    - Storing the previous dependencies.
    - The previous state will be used only if the dependencies didn't change, otherwise the initial state will be used.
    
    Unfortunately, I didn't manage to test this using the React Hooks testing library, because resetting the state in the `useEffect` hook triggers another rerender, which means that the `useCursorPagination` hook gets called a second time, returning an expected result based on the initial state this time. Checking the `result.current` in the test after calling the `rerender` function therefore misses the temporar inconsistent state. The best way to test this is by adding debugging log messages.

--- packages/in-hooks/useCursorPagination.js
+++ packages/in-hooks/useCursorPagination.js
@@ -1,9 +1,18 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
+import shallowEqual from 'fbjs/lib/shallowEqual';
 
 import { pendingResult, emptyArray, indeterminateProgress } from 'in-services/fixedObjects';
 import useObservable from 'in-hooks/useObservable';
 
 export default function useCursorPagination(create, deps = []) {
+  // If 'deps' change, the 'state' will be reset to the 'initialState' value. However, this 'state' change
+  // won't be visible until the next re-render. In order to make sure that we won't use the stale value
+  // of 'state', we need to track the previous value of 'deps' and perform a shallow comparison with the
+  // current 'deps' value. If we detect a change, we will use 'initialState' instead of the stale 'state'
+  // value.
+  const [prevDeps, setPrevDeps] = useState([]);
+  useEffect(() => setPrevDeps(deps), deps);
+
   const [state, setState] = useState(initialState);
   useEffect(() => setState(initialState), deps);
 
@@ -19,7 +28,8 @@ export default function useCursorPagination(create, deps = []) {
     errors,
     items,
     time
-  } = state;
+  } = shallowEqual(prevDeps, deps) ? state : initialState;
+
   const observable = useMemo(() => create({ cursor }), [cursor, reloadCount, ...deps]);
   useEffect(() => setState(awaitItems), [observable, ...deps]);
 
