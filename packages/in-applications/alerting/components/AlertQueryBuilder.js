9a44d85f11fd8d9fabde7b86b7c9257d6d0cbce6
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2020-11-25 12:51:32 +0100

    Use smart alert specific tag catalog (#4648)

--- packages/in-applications/alerting/components/AlertQueryBuilder.js
+++ packages/in-applications/alerting/components/AlertQueryBuilder.js
@@ -3,12 +3,9 @@ import { getApplicationTagCatalog } from 'in-applications/api/catalog';
 import { createQueryBuilder } from 'in-new-components/QueryBuilder';
 import { CALLS } from 'in-applications/analyze/metrics';
 
-// TODO: this component is copy pasted from the CallQueryBuilder.js component.
-// We need to change/adjust this later when smart alerts tagCatalogues are ready to use.
-
 const { QueryBuilder: AlertQueryBuilder, isQueryValid } = createQueryBuilder({
   getTagCatalog: props =>
-    getApplicationTagCatalog({ dataSource: CALLS, useCase: 'FILTERING' })(props).map(response => ({
+    getApplicationTagCatalog({ dataSource: CALLS, useCase: 'SMART_ALERTS' })(props).map(response => ({
       ...response,
       data: response.data && {
         ...response.data,
