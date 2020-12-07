71d77976293708e813db95c70f41b5dad37bbe63
Jean Detoeuf <jean.detoeuf@instana.com>
2020-12-07 13:16:16 -0400

    timeshift on 6 columns for infra metrics

--- packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/FormComponent.js
+++ packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/FormComponent.js
@@ -164,7 +164,7 @@ export default function FormComponent({
         </Col>
       </Row>
       <Row withoutTopMargin>
-        <Col lg={12}>
+        <Col lg={6}>
           {formatterFormGroup}
 
           {timeShiftConfiguration}
