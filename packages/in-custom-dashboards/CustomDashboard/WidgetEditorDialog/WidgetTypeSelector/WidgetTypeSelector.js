66b20a3f0e0203f5fece938dfa580de8c709232f
Syam Pillai <syam.sp0965@gmail.com>
2021-01-21 09:50:14 +0100

    Updated the showcase images (#5024)
    * Updated the showcase images
    
    * Updated the images
    
    * Updated the images with proper coloring and data
    
    * Added key to the showcase image to avoid displaying old image temporarly

--- packages/in-custom-dashboards/CustomDashboard/WidgetEditorDialog/WidgetTypeSelector/WidgetTypeSelector.js
+++ packages/in-custom-dashboards/CustomDashboard/WidgetEditorDialog/WidgetTypeSelector/WidgetTypeSelector.js
@@ -18,7 +18,12 @@ export default function WidgetTypeSelector({ form, onChangeType }) {
     <SideNavigationWrapper sidebar={<WidgetTypeSidebar form={form} onChangeType={onChangeType} />}>
       {widget && widget.showCase && (
         <div className={locals.wrapper}>
-          <img className={locals.img} src={widget.showCase} alt={`Show case of the ${widget.label} widget`} />
+          <img
+            className={locals.img}
+            src={widget.showCase}
+            alt={`Show case of the ${widget.label} widget`}
+            key={selectedType} // Added to notify react about the image has changed to avoid from displaying old image
+          />
         </div>
       )}
     </SideNavigationWrapper>
