45f18f051b9e11723606f40a76063bedbd7567dc
lowsky <217931+lowsky@users.noreply.github.com>
2021-01-29 16:42:15 +0100

    Slo resize (#5092)
    * refactore: simplify header tiles
    
    * extra widget header component
    
    * have list and tile styled header
    
    * refactor simplify optional parameters, rename parameter
    
    * refactoring: make styling easier, move things, small reorganisation
    
    * refactoring: rename style classes
    
    * switch between both header variants based on screen width
    
    * add little shorter date-time formatting
    
    (cherry picked from commit 58302084eab228bd73d79037628e8cf58ee17ec3)

---
+++ packages/in-custom-dashboards/widgets/Slo/Tiles/SloTimeTile_test.js
@@ -0,0 +1,27 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+/* eslint-env mocha */
+import { expect } from 'chai';
+
+import { compactTimeInterval } from 'in-custom-dashboards/widgets/Slo/Tiles/SloTimeTile';
+
+describe('in-custom-dashboards/widgets/Slo/Tiles/SloTimeTile', function() {
+  describe('#compactTimeInterval', function() {
+    function testConversionResule(fromDate, toDate) {
+      const { fromStr, toStr } = compactTimeInterval(fromDate.getTime(), toDate.getTime());
+      return fromStr + ' - ' + toStr;
+    }
+
+    it('should remove first year occurrence if equal', function() {
+      expect(
+        testConversionResule(
+          // < just helps code formatting
+          new Date(2021, 0, 1, 1, 1, 1),
+          new Date(2021, 1, 2, 2, 2, 1)
+        )
+      ).to.be.equal('Jan 01 01:01 - Feb 02, 2021 02:02');
+    });
+  });
+});
