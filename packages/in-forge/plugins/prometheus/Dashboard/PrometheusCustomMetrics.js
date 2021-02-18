25a7f9b5d281cdc1e9880d05e926b62b4176e0dd
alxbxbx <aleksandar.stankovic@instana.com>
2021-02-18 14:14:01 +0100

    Prometheus: Show notification in case of no metrics exposed

--- packages/in-forge/plugins/prometheus/Dashboard/PrometheusCustomMetrics.js
+++ packages/in-forge/plugins/prometheus/Dashboard/PrometheusCustomMetrics.js
@@ -5,9 +5,17 @@
 import React from 'react';
 import CustomMetricsV2, { AVAILABLE_SPECS } from 'in-sdk/components/dashboard/CustomMetricsV2';
 import { withSiMultiplyPrefixThreeDecimalPlaces } from 'in-services/formatters/number';
+import DashboardNotification from 'in-sdk/components/dashboard/DashboardNotification';
 
 export default function PrometheusCustomMetrics({ snapshot, timeConfig, titlePrefix }) {
-  return <CustomMetricsV2 snapshot={snapshot} timeConfig={timeConfig} titlePrefix={titlePrefix} specs={SPECS} />;
+  const metricIds = snapshot.get('metricIds');
+  if (metricIds.size > 0) {
+    return <CustomMetricsV2 snapshot={snapshot} timeConfig={timeConfig} titlePrefix={titlePrefix} specs={SPECS} />;
+  } else {
+    return (
+      <DashboardNotification>There are no metrics exposed on the given Prometheus endpoint.</DashboardNotification>
+    );
+  }
 }
 
 const untyped = {
