78771e4ef8dccbd0e28b96027145fb0b7b511d6f
Ivan Gligorić <ivan.gligoric.23@gmail.com>
2021-02-23 09:35:20 +0100

    AWS MSK: Add throttle byte rate dashboard (#5314)

--- packages/in-forge/plugins/awsMskBroker/metricDefinitions.js
+++ packages/in-forge/plugins/awsMskBroker/metricDefinitions.js
@@ -54,10 +54,22 @@ export default [
     formatter: millis
   },
   {
+    metric: 'produce_throttle_byte_rate',
+    label: 'Produce throttle byte rate',
+    category: ['Throttle byte rate'],
+    formatter: number.perSecond
+  },
+  {
     metric: 'fetch_throttle_time',
     label: 'Fetch throttle time',
     category: ['Throttle time'],
     formatter: millis
+  },
+  {
+    metric: 'fetch_throttle_byte_rate',
+    label: 'Fetch throttle byte rate',
+    category: ['Throttle byte rate'],
+    formatter: number.perSecond
   },
   {
     metric: 'cpu_idle',
@@ -76,6 +88,78 @@ export default [
     label: 'CPU system',
     category: ['CPU'],
     formatter: percentage
+  },
+  {
+    metric: 'memory_free',
+    label: 'Memory free',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'memory_used',
+    label: 'Memory used',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'memory_cached',
+    label: 'Memory cached',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'memory_buffered',
+    label: 'Memory buffered',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'swap_free',
+    label: 'Swap free',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'swap_used',
+    label: 'Swap used',
+    category: ['Memory'],
+    formatter: bytes
+  },
+  {
+    metric: 'network_rx_packets',
+    label: 'Network received packages',
+    category: ['Network'],
+    formatter: number
+  },
+  {
+    metric: 'network_rx_dropped',
+    label: 'Network dropped receive packages',
+    category: ['Network'],
+    formatter: number
+  },
+  {
+    metric: 'network_rx_errors',
+    label: 'Network receive errors',
+    category: ['Network'],
+    formatter: number
+  },
+  {
+    metric: 'network_tx_packets',
+    label: 'Network transmitted packages',
+    category: ['Network'],
+    formatter: number
+  },
+  {
+    metric: 'network_tx_dropped',
+    label: 'Network dropped transmit packages',
+    category: ['Network'],
+    formatter: number
+  },
+  {
+    metric: 'network_tx_errors',
+    label: 'Network transmit errors',
+    category: ['Network'],
+    formatter: number
   },
   {
     metric: 'fetch_consumer_total_time',
