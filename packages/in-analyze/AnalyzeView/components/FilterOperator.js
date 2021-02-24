8abfeba2102d87f449b3bc2511280e7a28089e63
Ben Blackmore <bripkens@gmail.com>
2021-02-24 08:27:17 +0100

    fix: automatic simon sorting (#5365)
    # Why
    
    Automatic Simon sorting is broken for packages such as `in-themes` and
    `in-i18n` where we do not import sub-modules, but instead always
    import the top-level module.
    
    # What
    
    We migrated the module to the [ui-foundation repo], fixed the bug and
    [published] a new version of it.
    
    [ui-foundation repo]: https://github.com/instana/ui-foundation/tree/main/packages/import-sort-style-simon
    [published]: https://github.com/instana/ui-foundation/blob/main/contributing/releasing.md

--- packages/in-analyze/AnalyzeView/components/FilterOperator.js
+++ packages/in-analyze/AnalyzeView/components/FilterOperator.js
@@ -2,12 +2,13 @@
  * (c) Copyright IBM Corp. 2021
  * (c) Copyright Instana Inc.
  */
-import theme from 'in-themes';
+
 import React from 'react';
 
 import Overlay from 'in-new-components/overlays/Overlay';
 import SvgIcon from 'in-components/SvgIcon';
 import Pill from 'in-new-components/Pill';
+import theme from 'in-themes';
 
 import locals from './FilterOperator.mless';
 
