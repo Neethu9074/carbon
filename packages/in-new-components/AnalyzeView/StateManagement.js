9b5e73d03bebf1eb5fd808e9d8322e2c1925018d
Sebastian Wilczek <38422130+sebastianwilczek@users.noreply.github.com>
2021-03-02 11:18:06 +0100

    UA2: Fix chart having no values on empty key group (#5450)

--- packages/in-new-components/AnalyzeView/StateManagement.js
+++ packages/in-new-components/AnalyzeView/StateManagement.js
@@ -429,6 +429,10 @@ export const childrenArgsAsPropTypes = {
 export function addGroupingCriteriaToFormModel(groupBy, groupValue, formModel, groupingTagCatalog) {
   const groupByTagType = groupingTagCatalog?.tags.find(tag => tag.name === groupBy.groupbyTag)?.type;
 
+  if (!groupByTagType) {
+    return formModel;
+  }
+
   let newTagFilter;
   if (groupValue === UNSPECIFIED) {
     newTagFilter = {
