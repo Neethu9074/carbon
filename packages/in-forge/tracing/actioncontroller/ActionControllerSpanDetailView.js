484b48ec66fd779f83cf760d5c03069c62c20b7b
yushjie <yushjie@cn.ibm.com>
2021-03-03 14:48:12 +0800

    i18n support: externalized strings for in-forge/tracing/actioncontroller - asgi

--- packages/in-forge/tracing/actioncontroller/ActionControllerSpanDetailView.js
+++ packages/in-forge/tracing/actioncontroller/ActionControllerSpanDetailView.js
@@ -2,6 +2,7 @@
  * (c) Copyright IBM Corp. 2021
  * (c) Copyright Instana Inc.
  */
+import { t } from 'in-i18n';
 import React from 'react';
 
 import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
@@ -9,10 +10,14 @@ import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
 export default function ActionControllerSpanDetailView({ span }) {
   return (
     <Dl>
-      <Di title="Controller">{span.getIn(['data', 'actioncontroller', 'controller'])}</Di>
-      <Di title="Action">{span.getIn(['data', 'actioncontroller', 'action'])}</Di>
-      <Di title="Error Message">{span.getIn(['data', 'log', 'message'])}</Di>
-      <Di title="Error Type">{span.getIn(['data', 'log', 'parameters'])}</Di>
+      <Di title={t('in-forge:tracing.actioncontroller.controller')}>
+        {span.getIn(['data', 'actioncontroller', 'controller'])}
+      </Di>
+      <Di title={t('in-forge:tracing.actioncontroller.action')}>
+        {span.getIn(['data', 'actioncontroller', 'action'])}
+      </Di>
+      <Di title={t('in-forge:tracing.actioncontroller.errorMessage')}>{span.getIn(['data', 'log', 'message'])}</Di>
+      <Di title={t('in-forge:tracing.actioncontroller.errorType')}>{span.getIn(['data', 'log', 'parameters'])}</Di>
     </Dl>
   );
 }
