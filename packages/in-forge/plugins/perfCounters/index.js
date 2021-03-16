942f9ccb38e5745b6ecae6c6d4bfe0c86607938b
Daljeet Sandu <daljeet-sandu@hotmail.de>
2021-03-16 16:55:04 +0100

    Add blank lines after import (#5684)
    * apply lint --fix
    
    * get rid of year number

--- packages/in-forge/plugins/perfCounters/index.js
+++ packages/in-forge/plugins/perfCounters/index.js
@@ -2,6 +2,7 @@
  * (c) Copyright IBM Corp. 2021
  * (c) Copyright Instana Inc.
  */
+
 import metricDefinitions from 'in-forge/plugins/perfCounters/metricDefinitions';
 import kpiDefinitions from 'in-forge/plugins/perfCounters/kpiDefinitions';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
