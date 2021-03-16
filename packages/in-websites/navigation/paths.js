efbd356785308ade8012ad2e44d0923f1b2adeb2
Sebastian Wilczek <38422130+sebastianwilczek@users.noreply.github.com>
2021-03-16 10:09:11 +0100

    UA2: (Re-)Setting detailID when jumping to Analyze (#5668)

--- packages/in-websites/navigation/paths.js
+++ packages/in-websites/navigation/paths.js
@@ -156,7 +156,16 @@ export function getLinkToCustomEvent(websiteId, { customEventId, pageId } = empt
 }
 
 // tagCatalog - if specified, the formModel will be reset if any of its tags is not available in the tag catalog
-export function getLinkToAnalyze({ beaconType, groupBy, formModel, chartedMetrics, fields, timeConfig, tagCatalog }) {
+export function getLinkToAnalyze({
+  beaconType,
+  groupBy,
+  formModel,
+  chartedMetrics,
+  fields,
+  timeConfig,
+  tagCatalog,
+  detailId
+}) {
   return getModifiedUrlStream(params => {
     params.pathname = analyzePathFullyQualified;
     if (__DEV__) {
@@ -168,6 +177,7 @@ export function getLinkToAnalyze({ beaconType, groupBy, formModel, chartedMetric
     setOrDeleteMatrixParameter(params, analyzeTwoParameters.groupBy, groupBy);
     setOrDeleteMatrixParameter(params, analyzeTwoParameters.fields, fields);
     setOrDeleteMatrixParameter(params, analyzeTwoParameters.chartedMetrics, chartedMetrics);
+    setOrDeleteMatrixParameter(params, analyzeTwoParameters.detailId, detailId);
 
     let updatedFormModel = formModel;
     if (tagCatalog && updatedFormModel?.length > 0) {
