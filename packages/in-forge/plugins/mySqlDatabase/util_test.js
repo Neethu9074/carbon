1af7f478eb6a766898aebfd9cffa506bc60067a3
ivangligoric <ivan.gligoric.23@gmail.com>
2021-03-16 11:23:56 +0100

    Add unit tests | fix regex pattern to allow only versions 5 and 8

---
+++ packages/in-forge/plugins/mySqlDatabase/util_test.js
@@ -0,0 +1,45 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+/* eslint-env mocha */
+
+import { fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import { isPerformanceDataAvailable } from './util';
+
+describe('isPerformanceDataAvailable', () => {
+  it('for valid mysql verson 8 should return true', () => {
+    const snapshot = fromJS({
+      data: {
+        sensorPerformanceSchemaStatus: 'OK',
+        'variables.VERSION': '8.0.21'
+      }
+    });
+
+    expect(isPerformanceDataAvailable(snapshot)).to.equal(true);
+  });
+
+  it('for valid mysql verson 5 should return true', () => {
+    const snapshot = fromJS({
+      data: {
+        sensorPerformanceSchemaStatus: 'OK',
+        'variables.VERSION': '5.7.10'
+      }
+    });
+
+    expect(isPerformanceDataAvailable(snapshot)).to.equal(true);
+  });
+
+  it('for invalid mysql verson should return false', () => {
+    const snapshot = fromJS({
+      data: {
+        sensorPerformanceSchemaStatus: 'OK',
+        'variables.VERSION': '4.0.5'
+      }
+    });
+
+    expect(isPerformanceDataAvailable(snapshot)).to.equal(false);
+  });
+});
