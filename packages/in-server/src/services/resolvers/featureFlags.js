d389ec936eb74a4868a23fe1141e49944600996e
Daljeet Sandu <28733517+Dalje-et@users.noreply.github.com>
2021-03-22 15:42:40 +0100

    set oidc feature flag to false (#5742)

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -197,7 +197,7 @@ module.exports = exports = [
   {
     uiClientKey: 'authenticationOidcEnabled',
     instanaCtlKey: 'feature.authentication.oidc.enabled',
-    defaultValue: true
+    defaultValue: false
   },
   {
     uiClientKey: 'syntheticCallsEnabled',
