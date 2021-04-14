50725030477579c2d06931a365f1d88e9d819e7e
Benjamin Sautermeister <benjamin.sautermeister@instana.com>
2021-04-14 13:55:07 +0200

    Follow-up: Add missing negation (#5994)

--- packages/in-events/components/legacy/EventSpecificationLink.js
+++ packages/in-events/components/legacy/EventSpecificationLink.js
@@ -15,7 +15,7 @@ import { role } from 'in-stores/user';
 import { t } from 'in-i18n';
 
 export default function EventSpecificationLink({ event }) {
-  if (role.canConfigureCustomAlerts) {
+  if (!role.canConfigureCustomAlerts) {
     // at the moment the link of this button generally does not work when the canConfigureCustomAlerts permission is missing,
     // because we generally hide the Events & Alerts section, including the build-in events.
     return null;
