ec73c0d186f05921268aeb8c8d44fc59dd318794
Raj Parekh <80683530+parekh-raj@users.noreply.github.com>
2021-04-26 11:54:25 +0200

    Remove unsafe react methods from entity form (#6121)
    Co-authored-by: Raj Parekh <raj.parekh@instana.com>

--- packages/in-hoc/entityForm.js
+++ packages/in-hoc/entityForm.js
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc.
  */
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useRef, useLayoutEffect } from 'react';
 import { fromJS } from 'immutable';
 
 import LoadingIndicator from 'in-new-components/LoadingIndicators/LoadingIndicator';
@@ -14,87 +14,74 @@ import { t } from 'in-i18n';
 
 export const savingMessage = t('in-hoc:entityFormSaving');
 
+const initialState = {
+  loading: true,
+  error: false,
+  form: null,
+  entity: null,
+  message: t('in-hoc:entityFormLoading'),
+  saveEnabled: true
+};
+
 export default function entityForm(ComposedComponent) {
-  return class extends React.Component {
-    static displayName = getDisplayName(ComposedComponent, 'EntityFormHoc');
+  function FormWithComposedComponent(props) {
+    const [state, setState] = useState(initialState);
+    const responseSubscription = useRef();
+    const errorSubscription = useRef();
 
-    state = {
-      loading: true,
-      error: false,
-      form: null,
-      entity: null,
-      message: t('in-hoc:entityFormLoading'),
-      saveEnabled: true
-    };
+    const { title, entityId } = props;
+    const { form, entity, saveEnabled } = state;
 
-    UNSAFE_componentWillMount() {
-      this.load(this.props);
+    useLayoutEffect(() => {
+      load(props);
+
+      return () => {
+        disposeAsyncAction();
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [entityId]);
+
+    if (!entity) {
+      return <LoadingIndicator />;
     }
 
-    UNSAFE_componentWillReceiveProps(nextProps) {
-      if (this.props.entityId !== nextProps.entityId) {
-        this.load(nextProps);
-      }
-    }
+    return (
+      <Fragment>
+        <Title title={title} />
+        <form onSubmit={onSubmit}>
+          <ComposedComponent
+            {...props}
+            {...state}
+            entity={entity}
+            form={form}
+            onChange={onChange}
+            setForm={form => setState({ ...state, form })}
+            setSaveEnabled={saveEnabled => setState({ ...state, saveEnabled })}
+            saveEnabled={saveEnabled}
+          />
+        </form>
+      </Fragment>
+    );
 
-    componentWillUnmount() {
-      this.disposeAsyncAction();
-    }
-
-    render() {
-      const { title } = this.props;
-      const { form, entity } = this.state;
-
-      if (!entity) {
-        return <LoadingIndicator />;
-      }
-
-      return (
-        <Fragment>
-          <Title title={title} />
-          <form onSubmit={this.onSubmit}>
-            <ComposedComponent
-              {...this.props}
-              {...this.state}
-              entity={entity}
-              form={form}
-              onChange={this.onChange}
-              setForm={form => this.setState({ form })}
-              setSaveEnabled={this.setSaveEnabled}
-              saveEnabled={this.state.saveEnabled}
-            />
-          </form>
-        </Fragment>
-      );
-    }
-
-    disposeAsyncAction = () => {
-      if (this.responseSubscription) {
-        this.responseSubscription.dispose();
-      }
-
-      if (this.errorSubscription) {
-        this.errorSubscription.dispose();
-      }
-    };
-
-    load = ({ entityId, createDefaultEntity, getEntityFromApi }) => {
-      this.disposeAsyncAction();
+    function load({ entityId, createDefaultEntity, getEntityFromApi }) {
+      disposeAsyncAction();
 
       if (!entityId) {
         const entity = fromJS(createDefaultEntity());
-        this.setState({
+        setState({
+          ...state,
           loading: false,
           error: false,
           message: null,
           entity,
           isCreate: true,
-          form: this.props.createForm(entity)
+          form: props.createForm(entity)
         });
         return;
       }
 
-      this.setState({
+      setState({
+        ...state,
         loading: true,
         error: false,
         message: t('in-hoc:entityFormLoading'),
@@ -103,48 +90,55 @@ export default function entityForm(ComposedComponent) {
       });
 
       const apiEntityResult$ = getEntityFromApi(entityId);
-      this.responseSubscription = apiEntityResult$.once(entity => {
-        this.setState({
+      responseSubscription.current = apiEntityResult$.once(entity => {
+        setState({
+          ...state,
           loading: false,
           error: false,
           message: null,
           entity,
-          form: this.props.createForm(entity)
+          form: props.createForm(entity)
         });
       });
 
-      this.errorSubscription = apiEntityResult$.errors().once(() => {
+      errorSubscription.current = apiEntityResult$.errors().once(() => {
         scrollToTopSmoothly();
-        this.setState({
+        setState({
+          ...state,
           loading: false,
           error: true,
           message: t('in-hoc:entityFormFailedToLoadData')
         });
       });
-    };
+    }
 
-    onSubmit = e => {
+    function onSubmit(e) {
       e.preventDefault();
 
-      if (!this.state.form.hierarchyValid) {
-        this.setState({
-          form: this.state.form.setTouched(true, { recurse: true })
+      const { entity, form } = state;
+
+      if (!form.hierarchyValid) {
+        setState({
+          ...state,
+          form: form.setTouched(true, { recurse: true })
         });
         return;
       }
-      const entity = this.state.entity;
-      const form = this.state.form;
-      const result$ = this.props.saveEntity(entity, form);
 
-      this.disposeAsyncAction();
-      this.setState({
+      const result$ = props.saveEntity(entity, form);
+
+      disposeAsyncAction();
+
+      setState({
+        ...state,
         loading: true,
         error: false,
         message: savingMessage
       });
-      this.responseSubscription = result$.once(this.props.openEntities);
 
-      this.errorSubscription = result$.errors().once(error => {
+      responseSubscription.current = result$.once(props.openEntities);
+
+      errorSubscription.current = result$.errors().once(error => {
         let message = error.message;
         if (
           error.response &&
@@ -155,30 +149,46 @@ export default function entityForm(ComposedComponent) {
           message = error.response.body.errors.join(', ');
         }
         scrollToTopSmoothly();
-        this.setState({
+        setState({
+          ...state,
           loading: false,
           error: true,
           message: t('in-hoc:entityFormFailedToSave', { SaveFailureMessage: message })
         });
       });
-    };
+    }
 
-    onChange = (fieldName, value, updateFormDefinition, forceSetValue) => {
+    function disposeAsyncAction() {
+      if (responseSubscription.current) {
+        responseSubscription.current.dispose();
+      }
+
+      if (errorSubscription.current) {
+        errorSubscription.current.dispose();
+      }
+    }
+
+    function onChange(fieldName, value, updateFormDefinition, forceSetValue) {
       if (typeof fieldName === 'function') {
         const updater = fieldName;
-        const updatedForm = updater(this.state.form);
-        this.setState({
+        const updatedForm = updater(state.form);
+
+        setState({
+          ...state,
           form: updatedForm
         });
+
         return updatedForm;
       }
-      const { entity } = this.state;
-      let updatedForm = this.state.form;
+
+      const { entity } = state;
+      let updatedForm = state.form;
+
       if (forceSetValue) {
         updatedForm = updatedForm.put([fieldName], value);
       } else if (Array.isArray(fieldName)) {
         for (let i = 0, length = fieldName.length; i < length; i++) {
-          updatedForm = updatedForm.updateIn([fieldName[i]], setFieldValue.bind(null, value[i]));
+          updatedForm = updatedForm.updateIn([fieldName[i]], setFieldValue(null, value[i]));
         }
       } else {
         updatedForm = updatedForm.updateIn([fieldName], field => field.setValue(value).setTouched(true));
@@ -188,21 +198,20 @@ export default function entityForm(ComposedComponent) {
         updatedForm = updateFormDefinition(updatedForm, entity);
       }
 
-      this.setState({
+      setState({
+        ...state,
         form: updatedForm
       });
 
       return updatedForm;
-    };
+    }
 
-    setSaveEnabled = enabled => {
-      this.setState({
-        saveEnabled: enabled
-      });
-    };
-  };
-}
+    function setFieldValue(value, field) {
+      return field.setValue(value).setTouched(true);
+    }
+  }
 
-function setFieldValue(value, field) {
-  return field.setValue(value).setTouched(true);
+  FormWithComposedComponent.displayName = getDisplayName(ComposedComponent, 'EntityFormHoc');
+
+  return FormWithComposedComponent;
 }
