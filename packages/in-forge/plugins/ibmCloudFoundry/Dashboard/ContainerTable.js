b1526f93fd1994034a88f2d3bc927f8b2a41d08a
Sridhar Venkatesh <svenkatesh@us.ibm.com>
2021-04-26 23:58:11 -0700

    IBM Cloud Foundry UI (#6044)
    * 18297: Create UI for IBM Cloud Foundry
    
    * 18297: Add UI for IBM Cloud Foundry
    
    * Add Range import
    
    * Corrections after initial testing
    
    * Remove % from CPU Utilization
    
    * Remove unused imports
    
    * Corrections from PR suggestions

---
+++ packages/in-forge/plugins/ibmCloudFoundry/Dashboard/ContainerTable.js
@@ -0,0 +1,78 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. 2021
+ */
+
+import { Range } from 'immutable';
+import React from 'react';
+
+import { millis } from 'in-services/formatters/number';
+import Table from 'in-sdk/components/dashboard/Table';
+import { getRawPayload } from 'in-stores/snapshot';
+import connectTo from 'in-hoc/connectTo';
+import { t } from 'in-i18n';
+
+const cols = [
+  {
+    title: t('in-forge:plugins.ibmCloudFoundry.instanceID'),
+    type: 'string',
+    typeArgs: {
+      getValue(row) {
+        return row.name;
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.ibmCloudFoundry.age'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row) {
+        return row.snapshotId;
+      },
+      getMetricName(row) {
+        return `instances.${row.name}.app_container_age`;
+      },
+      getContent: millis.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  }
+];
+
+export default connectTo(
+  props => {
+    return {
+      instanceCount: getRawPayload(props.snapshot.get('id'), 'instanceCount')
+    };
+  },
+  function ContainerTable({ snapshot, timeConfig, instanceCount }) {
+    if (!instanceCount || instanceCount < 1) {
+      return null;
+    }
+
+    const rows = Range(1, instanceCount + 1)
+      .toArray()
+      .map(instanceNumber => {
+        return {
+          key: String(instanceNumber),
+          name: String(instanceNumber),
+          instanceNumber,
+          timeConfig,
+          snapshotId: snapshot.get('id')
+        };
+      });
+
+    return (
+      <Table
+        withoutPadding
+        cardTitle={t('in-forge:plugins.ibmCloudFoundry.titleContainer')}
+        cols={cols}
+        rows={rows}
+        initialSortColumn={1}
+        initialSortDirection="desc"
+        maxItemsPerPage={10}
+      />
+    );
+  }
+);
---
+++ packages/in-forge/plugins/ibmCloudFoundry/Dashboard/DiskTable.js
@@ -0,0 +1,112 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. 2021
+ */
+
+import { Range } from 'immutable';
+import React from 'react';
+
+import PluginDashboardsMarkerLanes from 'in-forge/PluginDashboardsMarkerLanes';
+import Chart from 'in-components/Chart/InfrastructureMetricChartBehavior';
+import { bytes } from 'in-services/formatters/number';
+import Table from 'in-sdk/components/dashboard/Table';
+import { getRawPayload } from 'in-stores/snapshot';
+import connectTo from 'in-hoc/connectTo';
+import { t } from 'in-i18n';
+
+const cols = [
+  {
+    title: t('in-forge:plugins.ibmCloudFoundry.instanceID'),
+    type: 'string',
+    typeArgs: {
+      getValue(row) {
+        return row.name;
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.ibmCloudFoundry.used'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row) {
+        return row.snapshotId;
+      },
+      getMetricName(row) {
+        return `instances.${row.name}.app_disk_bytes_used`;
+      },
+      getContent: bytes.detailed,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.ibmCloudFoundry.total'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row) {
+        return row.snapshotId;
+      },
+      getMetricName(row) {
+        return `instances.${row.name}.app_disk_bytes_total`;
+      },
+      getContent: bytes.detailed,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  }
+];
+
+export default connectTo(
+  props => {
+    return {
+      instanceCount: getRawPayload(props.snapshot.get('id'), 'instanceCount')
+    };
+  },
+  function DiskTable({ snapshot, timeConfig, instanceCount }) {
+    if (!instanceCount || instanceCount < 1) {
+      return null;
+    }
+
+    const rows = Range(1, instanceCount + 1)
+      .toArray()
+      .map(instanceNumber => {
+        return {
+          key: String(instanceNumber),
+          name: String(instanceNumber),
+          instanceNumber,
+          timeConfig,
+          snapshotId: snapshot.get('id')
+        };
+      });
+
+    return (
+      <Table
+        withoutPadding
+        cardTitle={t('in-forge:plugins.ibmCloudFoundry.titleDisk')}
+        cols={cols}
+        rows={rows}
+        getRowDetails={getDetails}
+        maxItemsPerPage={10}
+      />
+    );
+  }
+);
+
+function getDetails(row) {
+  return (
+    <Chart
+      snapshotId={row.snapshotId}
+      timeConfig={row.timeConfig}
+      y1={{
+        min: 0,
+        formatter: bytes.detailed,
+        metrics: ['instances.' + row.name + '.app_disk_bytes_used', 'instances.' + row.name + '.app_disk_bytes_total'],
+        labels: [t('in-forge:plugins.ibmCloudFoundry.used'), t('in-forge:plugins.ibmCloudFoundry.total')],
+        type: 'line'
+      }}
+      renderPostChartContent={PluginDashboardsMarkerLanes}
+    />
+  );
+}
