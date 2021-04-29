5c37737309cf4512ba69fcd5947cd7a7efd1bc45
Ben Blackmore <bripkens@gmail.com>
2021-04-29 10:24:08 +0200

    Update packages/in-waiting-for-deployment/components/OnboardingWidget/contentComponents.js

--- packages/in-waiting-for-deployment/components/OnboardingWidget/contentComponents.js
+++ packages/in-waiting-for-deployment/components/OnboardingWidget/contentComponents.js
@@ -19,7 +19,7 @@ import Select from 'in-components/form/Select';
 import Button from 'in-new-components/Button';
 import Tooltip from 'in-components/Tooltip';
 import { region } from 'in-services/config';
-import Link from 'in-components/Link';
+import { Link } from '@instana/components';
 import { t } from 'in-i18n';
 
 import locals from './content.mless';
