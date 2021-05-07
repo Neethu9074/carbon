8aab967873df79395d3f0bd814d86d6884990262
Ben Blackmore <ben.blackmore@instana.com>
2021-05-07 16:53:37 +0200

    Restore accidentally deleted files

---
+++ packages/in-components/form/binding/SubForm.js
@@ -0,0 +1,36 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { FormContext } from 'in-components/form/binding/FormContext';
+import { pathPropType } from 'in-components/form/binding/paths';
+import FormBound from 'in-components/form/binding/FormBound';
+
+export default function SubForm({ path, autoHide, children }) {
+  return (
+    <FormBound path={path} autoHide={autoHide}>
+      {({ form, setForm, absolutePath, disabled }) => (
+        <FormContext.Provider
+          value={{
+            form,
+            rootPath: absolutePath,
+            setForm,
+            disabled
+          }}
+        >
+          {children}
+        </FormContext.Provider>
+      )}
+    </FormBound>
+  );
+}
+
+SubForm.propTypes = {
+  path: pathPropType.isRequired,
+  autoHide: PropTypes.bool,
+  children: PropTypes.node.isRequired
+};
