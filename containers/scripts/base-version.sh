4b36e67690288110026eefcbb8bc675357c4daf4
Dahlia Bock <dlbock@gmail.com>
2021-05-11 14:45:31 -0500

    [containers] Move BASE_VERSION value into a file with the same name
    This will hopefully simplify things if we were to automate the updating of this value as it's just a single file, with a single value

--- containers/scripts/base-version.sh
+++ containers/scripts/base-version.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -euo pipefail
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # This is the version of the Instana base image that is used for all images
 # Some images need a specific runtime included, others do not.
 # Please see backend/containers/runtimes/README.md for more details on
 # these runtime/base images
-BASE_VERSION=0.0.10
+BASE_VERSION=$(cat $SCRIPTPATH/../BASE_VERSION)
