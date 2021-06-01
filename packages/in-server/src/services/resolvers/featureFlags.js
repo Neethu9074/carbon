a7d8aa3009ef6675e454054416b64a00005c6122
Ben Blackmore <bripkens@gmail.com>
2021-06-01 09:25:40 +0200

    Enable language selector by default (#6443)
    # Why
    
    We want to go live with in-product i18n as part of release
    203.
    
    # What
    
    Enable the feature by default.

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -178,7 +178,7 @@ module.exports = exports = [
   {
     uiClientKey: 'languageSelectorEnabled',
     instanaCtlKey: 'feature.language.selector.enabled',
-    defaultValue: false
+    defaultValue: true
   },
   {
     uiClientKey: 'pseudoLanguageEnabled',
