f7410ac49ee03d0b77c22fcb5d931b66942af5c7
Hunter Madison <hunter.madison@instana.com>
2021-06-10 16:37:12 -0400

    Add type for ActionMailer

---
+++ packages/in-forge/tracing/mail.actionmailer/ActionMailSpanDetailView.js
@@ -0,0 +1,20 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import { Dl, Di } from 'in-new-components/HorizontalDescriptionList';
+import { t } from 'in-i18n';
+
+export default function ActionMailSpanDetailView({ span }) {
+  return (
+    <div>
+      <Dl>
+        <Di title={t('in-forge:tracing.mail.class')}>{span.getIn(['data', 'actionmailer', 'class'])}</Di>
+        <Di title={t('in-forge:tracing.mail.method')}>{span.getIn(['data', 'actionmailer', 'method'])}</Di>
+      </Dl>
+    </div>
+  );
+}
