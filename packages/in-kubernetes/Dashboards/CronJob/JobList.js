f98a11974ac020697dffc9e3eb7232898f948f7e
mansoorpervaiz <mansoorpervaizdev@gmail.com>
2021-06-21 06:51:35 -0400

    59720: Add UI for Job list data (#6494)
    Co-authored-by: Mansoor Pervaiz <mansoor.pervaiz@instana.com>

--- packages/in-kubernetes/Dashboards/CronJob/JobList.js
+++ packages/in-kubernetes/Dashboards/CronJob/JobList.js
@@ -5,113 +5,141 @@
 
 import React from 'react';
 
+import { ColumnizedContent, Li, Ul } from '@instana/components';
+import { LiLoadMore } from '@instana/components';
 import { useObservable } from '@instana/hooks';
+import { KeyValue } from '@instana/components';
 
-import { clusterIdUrlParameter, namespaceIdUrlParameter } from 'in-kubernetes/navigation/urlParameters';
-import createServerTableWithUrlState from 'in-components/tables/ServerTable/ServerTableWithUrlState';
-import SeverityAwareEntityLink from 'in-components/tables/sharedComponents/SeverityAwareEntityLink';
-import EntityHealthIndicator from 'in-components/EntityHealthIndicator/EntityHealthIndicator';
-import withEmptyTableState from 'in-components/tables/ServerTable/WithEmptyTableState';
-import HealthIndicatorPresenter from 'in-components/health/HealthIndicatorPresenter';
-import { urlParameters as timeConfigUrlParameters } from 'in-stores/time/config';
+import EntityPageMainNotification from 'in-components/EntityPageMainNotification/EntityPageMainNotification';
+import ErroneousResultPresenter from 'in-components/Errors/ErroneousResultPresenter';
+import LoadingList from 'in-components/lists/List/sharedComponents/LoadingList';
+import LoadingIndicator from 'in-components/LoadingIndicators/LoadingIndicator';
 import { valueMissingPlaceholder } from 'in-components/valueMissingPlaceholder';
+import CenterAlignmentColumn from 'in-components/layout/CenterAlignmentColumn';
 import getKubernetesJobs from 'in-subscription/kubernetes/getKubernetesJobs';
-import { getDashboardLink } from 'in-stores/navigation/paths/dashboardPaths';
+import { retrievalSize } from 'in-components/AnalyzeView/UngroupedView';
+import useCursorPagination from 'in-hooks/useCursorPagination';
 import { getInfraGranularity } from 'in-stores/metric/metric';
-import EntityLink from 'in-components/EntityLink/EntityLink';
-import { getMetricForFocusedMoment } from 'in-stores/metric';
 import { formatDuration } from 'in-services/formatters/date';
-import TwoValueBar from 'in-components/TwoValueBar';
-import theme from 'in-themes';
+import HealthDot from 'in-components/health/HealthDot';
+import { getHistoricMetric } from 'in-stores/metric';
+import useTimeConfig from 'in-hooks/useTimeConfig';
 import { t } from 'in-i18n';
 
-const pathSegment = '/jobs';
-const matrixPrefix = 'job.';
+const statusToSeverity = {
+  Completed: 0,
+  Running: 1.1,
+  Failed: 10
+};
+
+const notFoundComponent = (
+  <CenterAlignmentColumn>
+    <EntityPageMainNotification
+      icon="lib_missing_data"
+      title="No jobs available"
+      explanation="No jobs   available"
+      changeExplanation={() => 'There were no jobs retrieved for the selected time range.'}
+    />
+  </CenterAlignmentColumn>
+);
 
 const columnDefinitions = [
   {
-    id: 'label',
-    label: t('in-kubernetes:dashboards.name'),
+    id: 'health',
+    width: '1rem',
     getContent(item) {
-      return (
-        <>
-          <SeverityAwareEntityLink label={item.job.label} severity={item.entityHealthInfo.maxSeverity} />
-          {item?.podIds?.map(v => {
-            return (
-              <EntityLink
-                label="pod"
-                href$={getDashboardLink(v, { pathname: '/physical/dashboard' })}
-                icon="lib_kubernetes_pod"
-              />
-            );
-          })}
-        </>
-      );
+      return <HealthDot severity={statusToSeverity[item.status] || statusToSeverity.Running} iconSize={10} />;
+    }
+  },
+  {
+    id: 'name',
+    getContent(item) {
+      return <KeyValue label={t('in-kubernetes:dashboards.name')} value={item.job.label} accentuated />;
     }
   },
   {
     id: 'status',
-    label: t('in-kubernetes:dashboards.status'),
+    width: '10rem',
     getContent(item) {
-      return item.job.status;
-    }
-  },
-  {
-    id: 'completion',
-    label: t('in-kubernetes:dashboards.completion'),
-    optional: true,
-    sortable: false,
-    getContent(item) {
-      return <JobCompletion snapshotId={item.job.id} />;
+      return <KeyValue label={t('in-kubernetes:dashboards.status')} value={item.job.status} theme="blue" accentuated />;
     }
   },
   {
     id: 'age',
-    label: t('in-kubernetes:dashboards.age'),
+    width: '10rem',
     getContent(item) {
-      return item.job.age && formatDuration(item.job.age);
+      return (
+        <KeyValue
+          label={t('in-kubernetes:dashboards.age')}
+          value={formatDuration(item.job?.age)}
+          theme="blue"
+          accentuated
+        />
+      );
     }
   },
   {
-    id: 'health',
-    label: t('in-kubernetes:dashboards.health'),
-    getContent(item, { timeConfig }) {
-      return (
-        <EntityHealthIndicator
-          openIssues={item.entityHealthInfo.openIssues.length}
-          maxSeverity={item.entityHealthInfo.maxSeverity}
-          IndicatorPresenter={HealthIndicatorPresenter}
-          timeConfig={timeConfig}
-          snapshotId={item.job.id}
-          inContentArea
-        />
-      );
+    id: 'pending',
+    width: '10rem',
+    getContent(item) {
+      return <PodMetrics snapshotId={item.job.id} metric="status.failed" label={t('in-kubernetes:pod.pending')} />;
+    }
+  },
+  {
+    id: 'active',
+    width: '10rem',
+    getContent(item) {
+      return <PodMetrics snapshotId={item.job.id} metric="status.active" label={t('in-kubernetes:pod.active')} />;
+    }
+  },
+  {
+    id: 'complete',
+    width: '10rem',
+    getContent(item) {
+      return <PodMetrics snapshotId={item.job.id} metric="status.succeeded" label={t('in-kubernetes:pod.completed')} />;
     }
   }
 ];
 
-const ServerTableWithUrlState = createServerTableWithUrlState({
-  Renderer: withEmptyTableState({
-    columnDefinitions,
-    entityName: 'jobs'
-  }),
-  paginationResettingUrlParameters: [...timeConfigUrlParameters, clusterIdUrlParameter, namespaceIdUrlParameter],
-  columnDefinitions,
-  defaultOrderBy: 'age',
-  defaultOrderDirection: 'ASC',
-  pathSegment,
-  matrixPrefix
-});
-
 export default function Jobs(props) {
-  return <ServerTableWithUrlState cardTitle="Jobs History" get={getTableData} {...props} />;
+  const { timeConfig } = props;
+  const { canLoadMore, progress, loadMore, errors, items } = useCursorPagination(
+    ({ cursor }) =>
+      getTableData({
+        timeConfig: timeConfig,
+        page: cursor,
+        pageSize: retrievalSize,
+        cronJobId: props.cronJobId
+      }),
+    [timeConfig]
+  );
+  const isLoading = progress?.loading;
+  const isInitialLoading = progress?.loading && items?.length === 0;
+  const hasErrors = errors?.length > 0;
+  if (isInitialLoading) {
+    return <LoadingIndicator text={t('in-applications:loadingData')} height={100} />;
+  } else if (hasErrors) {
+    return <ErroneousResultPresenter errors={errors} />;
+  }
+  return (
+    <Ul>
+      {items.map(item => (
+        <Li noAlternatingBg borderRadius="medium" toggleContentOnRowClick highlightOpenState={false} key={item.label}>
+          <ColumnizedContent columnDefinitions={columnDefinitions} {...item} />
+        </Li>
+      ))}
+      {canLoadMore && <LiLoadMore loadMore={loadMore} />}
+      {isLoading && <LoadingList numSkeletonRows={items?.length ? 1 : 3} />}
+      {!isLoading && !items?.length && notFoundComponent}
+    </Ul>
+  );
 }
 
 function getTableData({
   query = '',
   page = 1,
   pageSize = 20,
-  orderBy = 'type',
+  orderBy = 'age',
   orderDirection = 'ASC',
   timeConfig,
   clusterId,
@@ -138,37 +166,17 @@ function getTableData({
   });
 }
 
-function JobCompletion({ snapshotId }) {
-  const podsSucceeded = useObservable(
-    getMetricForFocusedMoment({
+function PodMetrics({ snapshotId, metric, label }) {
+  const timeConfig = useTimeConfig();
+  const podsPending = useObservable(
+    getHistoricMetric({
       snapshotId,
-      metric: 'status.succeeded'
+      metric: metric,
+      timeConfig: timeConfig
     })
       .map(v => v[1])
       .distinct(),
     []
   );
-  const podsActive = useObservable(
-    getMetricForFocusedMoment({
-      snapshotId,
-      metric: 'status.active'
-    })
-      .map(v => v[1])
-      .distinct(),
-    []
-  );
-  return podsSucceeded !== undefined || podsActive !== undefined ? (
-    <TwoValueBar
-      v1={podsSucceeded}
-      v2={podsActive}
-      v1Color={theme.lib.colors.lightBlue800}
-      v2Color={theme.lib.colors.N400}
-      v1Label={t('in-kubernetes:succeeded')}
-      v2Label={t('in-kubernetes:active')}
-      fullDomain={podsSucceeded + podsActive}
-      formatter={v => v}
-    />
-  ) : (
-    valueMissingPlaceholder
-  );
+  return <KeyValue label={label} value={podsPending || valueMissingPlaceholder} theme="blue" accentuated />;
 }
