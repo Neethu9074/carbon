24da23169e410b2e0d0d4a4b271dd6828197fa8b
Syam Pillai <syam.sadasivan.pillai@instana.com>
2021-06-23 09:32:12 +0200

    Updated custom options renderer for ComboBox in settings page (#6591)
    * Updated custom options renderer for ComboBox in settings page
    
    * renamed renderOption to Option

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/components/BuiltInMetricSelector.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/components/BuiltInMetricSelector.js
@@ -3,6 +3,7 @@
  * (c) Copyright Instana Inc.
  */
 
+import classNames from 'classnames';
 import React from 'react';
 
 import { getAllBuiltInMetrics } from 'in-sdk/metrics';
@@ -17,16 +18,23 @@ export default function BuiltInMetricSelector({ id, plugin, onChange, value, isC
       name={id}
       value={value}
       options={metricsList}
-      optionRenderer={renderOption}
+      components={{ Option }}
       onChange={onChange}
       isClearable={isClearable}
     />
   );
 }
 
-function renderOption(option) {
+function Option(props) {
+  const { data: option, innerProps } = props;
+  const selectedOption = props.getValue();
   return (
-    <div className={locals.item}>
+    <div
+      className={classNames(locals.item, {
+        [locals.selected]: option.value === selectedOption?.[0]?.value
+      })}
+      {...innerProps}
+    >
       {option.label}
       <span className={locals.subtleMetric}>({option.metricLabel})</span>
     </div>
