449c83578ac1b14b228a5c1871de59c56f1c84aa
Simon Thiele <simon.thiele.st@googlemail.com>
2021-06-28 10:24:55 +0200

    Dashboard sidebar buttons enhancement (#6631)
    * prepare
    
    * upgrate to get the newest version of the stack
    
    * remove unused style
    
    Co-authored-by: simonThiele <simon.thiele.st@gmail.com>

--- packages/in-map/components/MapSidebar/components/MapSidebarHeader.js
+++ packages/in-map/components/MapSidebar/components/MapSidebarHeader.js
@@ -5,6 +5,8 @@
 
 import React from 'react';
 
+import { Stack } from '@instana/components';
+
 import HealthIndicatorButtonPresenter from 'in-components/health/HealthIndicatorButtonPresenter';
 import ViewDashboardButton from 'in-map/components/MapSidebar/components/ViewDashboardButton';
 import SidebarHeader from 'in-map/components/MapSidebar/components/SidebarHeader';
@@ -17,14 +19,14 @@ export default function MapSidebarHeader({ snapshot, timeConfig }) {
   return (
     <div className={locals.sidebarHeader}>
       <SidebarHeader snapshot={snapshot} />
-      <div className={locals.buttonContainer}>
+      <Stack direction="horizontal" gap="xsmall" wrap>
         <ViewDashboardButton snapshotId={snapshotId} />
         <EntityHealthIndicator
           IndicatorPresenter={HealthIndicatorButtonPresenter}
           snapshotId={snapshotId}
           timeConfig={timeConfig}
         />
-      </div>
+      </Stack>
     </div>
   );
 }
