5fc4a6efd06116399838200280246fde29fb4f22
Ben Blackmore <bripkens@gmail.com>
2021-06-30 08:39:35 +0200

    Add MaxMind attribution (#6642)
    # Why/What
    
    https://instana.kanbanize.com/ctrl_board/103/cards/65621/details/

--- packages/in-components/AboutInstanaDialog/AboutInstanaDialog.js
+++ packages/in-components/AboutInstanaDialog/AboutInstanaDialog.js
@@ -5,10 +5,7 @@
 
 import React from 'react';
 
-import { ColumnizedContent, Ul, Li } from '@instana/components';
-import { KeyValue } from '@instana/components';
-import { Button } from '@instana/components';
-import { Stack } from '@instana/components';
+import { ColumnizedContent, Ul, Li, KeyValue, Button, Stack, Link } from '@instana/components';
 
 import getUiBackendVersion from 'in-subscription/getUiBackendVersion';
 import { graphPath } from 'in-stores/navigation/paths/mainPaths';
@@ -18,64 +15,102 @@ import Dialog from 'in-components/Dialog/Dialog';
 import { goToPath } from 'in-stores/navigation';
 import Lettering from 'in-components/Lettering';
 import { build } from 'in-services/config';
-import connectTo from 'in-hoc/connectTo';
-import { t } from 'in-i18n';
+import { t, Trans } from 'in-i18n';
 
 import locals from './AboutInstanaDialog.mless';
 
-export default connectTo(
-  {
-    uiBackendVersion: getUiBackendVersion()
-  },
-  function AboutInstanaDialog({ uiBackendVersion }) {
-    return (
-      <Dialog onClose={close} title={<Lettering className={locals.lettering} />}>
-        <Stack align="center" space="medium">
-          <Ul>
-            {instanaRegion && (
-              <Li>
-                <ColumnizedContent
-                  columnDefinitions={[
-                    {
-                      width: '10rem',
-                      getContent() {
-                        return t('in-components:aboutInstanaDialog.columnizedContentDeployment');
-                      }
-                    },
-                    {
-                      width: '8rem',
-                      getContent() {
-                        return (
-                          <KeyValue
-                            label={t('in-components:aboutInstanaDialog.labelRegion')}
-                            value={instanaRegion}
-                            accentuated
-                          />
-                        );
-                      }
-                    }
-                  ]}
-                />
-              </Li>
-            )}
-
+export default function AboutInstanaDialog() {
+  const uiBackendVersion = getUiBackendVersion();
+  return (
+    <Dialog onClose={close} title={<Lettering className={locals.lettering} />}>
+      <Stack align="center" space="medium">
+        <Ul>
+          {instanaRegion && (
             <Li>
               <ColumnizedContent
                 columnDefinitions={[
                   {
                     width: '10rem',
                     getContent() {
-                      return t('in-components:aboutInstanaDialog.columnizedContentUserInterface');
+                      return t('in-components:aboutInstanaDialog.columnizedContentDeployment');
                     }
                   },
                   {
                     width: '8rem',
                     getContent() {
                       return (
-                        build.tag && (
+                        <KeyValue
+                          label={t('in-components:aboutInstanaDialog.labelRegion')}
+                          value={instanaRegion}
+                          accentuated
+                        />
+                      );
+                    }
+                  }
+                ]}
+              />
+            </Li>
+          )}
+
+          <Li>
+            <ColumnizedContent
+              columnDefinitions={[
+                {
+                  width: '10rem',
+                  getContent() {
+                    return t('in-components:aboutInstanaDialog.columnizedContentUserInterface');
+                  }
+                },
+                {
+                  width: '8rem',
+                  getContent() {
+                    return (
+                      build.tag && (
+                        <KeyValue
+                          label={t('in-components:aboutInstanaDialog.labelTag')}
+                          value={build.tag}
+                          accentuated
+                        />
+                      )
+                    );
+                  }
+                },
+                {
+                  width: '8rem',
+                  getContent() {
+                    return (
+                      build.revision && (
+                        <KeyValue
+                          label={t('in-components:aboutInstanaDialog.labelCommit')}
+                          value={build.revision.substring(0, 12)}
+                          accentuated
+                        />
+                      )
+                    );
+                  }
+                }
+              ]}
+            />
+          </Li>
+
+          {uiBackendVersion && (
+            <Li>
+              <ColumnizedContent
+                columnDefinitions={[
+                  {
+                    width: '10rem',
+                    getContent() {
+                      return t('in-components:aboutInstanaDialog.columnizedContentBackend');
+                    }
+                  },
+                  {
+                    width: '8rem',
+                    getContent() {
+                      return (
+                        uiBackendVersion.imageTag && (
                           <KeyValue
                             label={t('in-components:aboutInstanaDialog.labelTag')}
-                            value={build.tag}
+                            value={uiBackendVersion.imageTag}
                             accentuated
                           />
                         )
@@ -86,10 +121,10 @@ export default connectTo(
                     width: '8rem',
                     getContent() {
                       return (
-                        build.revision && (
+                        uiBackendVersion.commit && (
                           <KeyValue
                             label={t('in-components:aboutInstanaDialog.labelCommit')}
-                            value={build.revision.substring(0, 12)}
+                            value={uiBackendVersion.commit.substring(0, 12)}
                             accentuated
                           />
                         )
@@ -99,62 +134,46 @@ export default connectTo(
                 ]}
               />
             </Li>
+          )}
 
-            {uiBackendVersion && (
-              <Li>
-                <ColumnizedContent
-                  columnDefinitions={[
-                    {
-                      width: '10rem',
-                      getContent() {
-                        return t('in-components:aboutInstanaDialog.columnizedContentBackend');
-                      }
-                    },
-                    {
-                      width: '8rem',
-                      getContent() {
-                        return (
-                          uiBackendVersion.imageTag && (
-                            <KeyValue
-                              label={t('in-components:aboutInstanaDialog.labelTag')}
-                              value={uiBackendVersion.imageTag}
-                              accentuated
-                            />
-                          )
-                        );
-                      }
-                    },
-                    {
-                      width: '8rem',
-                      getContent() {
-                        return (
-                          uiBackendVersion.commit && (
-                            <KeyValue
-                              label={t('in-components:aboutInstanaDialog.labelCommit')}
-                              value={uiBackendVersion.commit.substring(0, 12)}
-                              accentuated
-                            />
-                          )
-                        );
-                      }
-                    }
-                  ]}
-                />
-              </Li>
-            )}
-          </Ul>
+          <Li>
+            <ColumnizedContent
+              columnDefinitions={[
+                {
+                  width: '10rem',
+                  getContent() {
+                    return t('in-components:aboutInstanaDialog.geo.title');
+                  }
+                },
+                {
+                  getContent() {
+                    return (
+                      <span className={locals.maxMindAttribution}>
+                        <Trans
+                          i18nKey="in-components:aboutInstanaDialog.geo.description"
+                          components={{
+                            linkToMaxMind: <Link external href="https://www.maxmind.com" />
+                          }}
+                        />
+                      </span>
+                    );
+                  }
+                }
+              ]}
+            />
+          </Li>
+        </Ul>
 
-          <Button
-            kind="primaryv2"
-            onClick={() => {
-              goToPath(graphPath);
-              close();
-            }}
-          >
-            {t('in-components:aboutInstanaDialog.buttonGraphShowcase')}
-          </Button>
-        </Stack>
-      </Dialog>
-    );
-  }
-);
+        <Button
+          kind="primaryv2"
+          onClick={() => {
+            goToPath(graphPath);
+            close();
+          }}
+        >
+          {t('in-components:aboutInstanaDialog.buttonGraphShowcase')}
+        </Button>
+      </Stack>
+    </Dialog>
+  );
+}
