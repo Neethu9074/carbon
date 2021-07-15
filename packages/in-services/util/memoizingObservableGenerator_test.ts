058d647bb657064139bc51c20caa9b95bb399a2d
Ben Blackmore <ben.blackmore@instana.com>
2021-07-15 14:47:37 +0200

    Translate

--- packages/in-services/util/memoizingObservableGenerator_test.ts
+++ packages/in-services/util/memoizingObservableGenerator_test.ts
@@ -3,42 +3,64 @@
  * (c) Copyright Instana Inc.
  */
 
-/* eslint-env mocha */
+/* eslint-env jest */
 
-import { create } from '@instana/observables';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import { Observable, create } from '@instana/observables';
 
 import memoize from 'in-services/util/memoizingObservableGenerator';
 
 describe('in-services/util/memoizingObservableGenerator', () => {
-  let creator;
-  let subscriber;
-  let stop;
+  let creator: (n: number) => Observable<number>;
+  let subscriber: jest.Mock;
+  let stop: jest.Mock;
 
   beforeEach(() => {
-    stop = sinon.stub();
-    subscriber = sinon.stub();
-    creator = memoize(arg => create({ stop }).emit(arg), JSON.stringify.bind(JSON));
+    stop = jest.fn();
+    subscriber = jest.fn();
+    creator = memoize<number, number>(
+      arg => create<number>({ stop }).emit(arg),
+      JSON.stringify.bind(JSON),
+      100
+    );
   });
 
-  it('should create new observable', () => {
+  it('must create new observable', () => {
     const observable = creator(4);
     observable.subscribe(subscriber);
-    expect(subscriber).to.have.been.calledWith(4);
+    expect(subscriber.mock.calls[0][0]).toEqual(4);
   });
 
-  it('should reuse existing observable on same id', () => {
-    expect(creator(4)).to.equal(creator(4));
+  it('must reuse existing observable on same id', () => {
+    expect(creator(4)).toEqual(creator(4));
   });
 
-  it('should create separate observable when ID differs', () => {
-    expect(creator(4)).not.to.equal(creator(5));
+  it('must create separate observable when ID differs', () => {
+    expect(creator(4)).not.toEqual(creator(5));
   });
 
-  it('should delay stop calls', () => {
+  it('must delay stop calls', async () => {
     const observable = creator(4);
     observable.subscribe(() => {}).dispose();
-    expect(stop).to.have.callCount(0);
+    // stop must be called async
+    expect(stop.mock.calls.length).toEqual(0);
+    // the stop function must eventually be called
+    return new Promise(resolve => stop.mockImplementation(resolve));
+  });
+
+  it('must support TTI functions', async () => {
+    const ttiFunction = jest.fn();
+    ttiFunction.mockReturnValue(0);
+    creator = memoize<number, number>(
+      arg => create<number>({ stop }).emit(arg),
+      JSON.stringify.bind(JSON),
+      ttiFunction
+    );
+    const observable = creator(4);
+
+    observable.subscribe(() => {}).dispose();
+    expect(ttiFunction.mock.calls.length).toEqual(1);
+    expect(ttiFunction.mock.calls[0][0][0]).toEqual(4);
+
+    return new Promise(resolve => stop.mockImplementation(resolve));
   });
 });
