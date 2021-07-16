4984255b9c71beef8594ea8bdc3e7a87116bf20c
Ben Blackmore <ben.blackmore@instana.com>
2021-07-16 16:16:59 +0200

    Translate in-stores/navigation/routing/parser_test.ts

--- packages/in-stores/navigation/routing/parser_test.ts
+++ packages/in-stores/navigation/routing/parser_test.ts
@@ -4,14 +4,13 @@
  */
 
 /* eslint-env jest */
-import { expect } from 'chai';
 
 import { parseUrl } from 'in-stores/navigation/routing/parser';
 
 describe('in-stores/navigation/routing/parser', () => {
   describe('parseUrl', () => {
     it('must extract root URL', () => {
-      expect(parseUrl('/')).to.deep.equal({
+      expect(parseUrl('/')).toEqual({
         pathname: '/',
         query: {},
         matrix: { '/': {} }
@@ -19,7 +18,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must extract URL without params', () => {
-      expect(parseUrl('/foo')).to.deep.equal({
+      expect(parseUrl('/foo')).toEqual({
         pathname: '/foo',
         query: {},
         matrix: { '/foo': {} }
@@ -27,7 +26,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must extract query parameters', () => {
-      expect(parseUrl('/foo?a=b')).to.deep.equal({
+      expect(parseUrl('/foo?a=b')).toEqual({
         pathname: '/foo',
         query: {
           a: 'b'
@@ -37,7 +36,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must extract query parameters without values', () => {
-      expect(parseUrl('/foo?a')).to.deep.equal({
+      expect(parseUrl('/foo?a')).toEqual({
         pathname: '/foo',
         query: {
           a: ''
@@ -47,7 +46,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must extract multiple query parameters', () => {
-      expect(parseUrl('/foo?a&b=c')).to.deep.equal({
+      expect(parseUrl('/foo?a&b=c')).toEqual({
         pathname: '/foo',
         query: {
           a: '',
@@ -58,7 +57,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must decode query parameters', () => {
-      expect(parseUrl('/foo?b=c%20%2Fd%2520e')).to.deep.equal({
+      expect(parseUrl('/foo?b=c%20%2Fd%2520e')).toEqual({
         pathname: '/foo',
         query: {
           b: 'c /d%20e'
@@ -68,7 +67,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must remove trailing ?', () => {
-      expect(parseUrl('/foo?')).to.deep.equal({
+      expect(parseUrl('/foo?')).toEqual({
         pathname: '/foo',
         query: {},
         matrix: { '/foo': {} }
@@ -76,7 +75,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must support matrix parameters', () => {
-      expect(parseUrl('/foo;k=v')).to.deep.equal({
+      expect(parseUrl('/foo;k=v')).toEqual({
         pathname: '/foo',
         query: {},
         matrix: {
@@ -88,7 +87,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must support matrix parameters for sub paths', () => {
-      expect(parseUrl('/first/second;k=v/third')).to.deep.equal({
+      expect(parseUrl('/first/second;k=v/third')).toEqual({
         pathname: '/first/second/third',
         query: {},
         matrix: {
@@ -102,7 +101,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must support matrix parameters with query parameters', () => {
-      expect(parseUrl('/first;a=b/second;k=v/third?foo=bar')).to.deep.equal({
+      expect(parseUrl('/first;a=b/second;k=v/third?foo=bar')).toEqual({
         pathname: '/first/second/third',
         query: {
           foo: 'bar'
@@ -120,7 +119,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must decode matrix parameters', () => {
-      expect(parseUrl('/foo;b=c%20%2Fd%2520e')).to.deep.equal({
+      expect(parseUrl('/foo;b=c%20%2Fd%2520e')).toEqual({
         pathname: '/foo',
         query: {},
         matrix: {
@@ -132,7 +131,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must not decode matrix parameters recursively', () => {
-      expect(parseUrl('/abc;foo=a%252520b/bar')).to.deep.equal({
+      expect(parseUrl('/abc;foo=a%252520b/bar')).toEqual({
         pathname: '/abc/bar',
         query: {},
         matrix: {
@@ -145,7 +144,7 @@ describe('in-stores/navigation/routing/parser', () => {
     });
 
     it('must support table use case', () => {
-      expect(parseUrl('/table;view=physical;plugin=host?timeline.to&timeline.ws=600000')).to.deep.equal({
+      expect(parseUrl('/table;view=physical;plugin=host?timeline.to&timeline.ws=600000')).toEqual({
         pathname: '/table',
         query: {
           'timeline.to': '',
