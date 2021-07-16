96ccdb409dfaa1178d2c7ccca84da80844809b64
Ben Blackmore <ben.blackmore@instana.com>
2021-07-16 17:01:13 +0200

    Translate in-themes/LocallyChangedTheme.tsx

--- packages/in-themes/LocallyChangedTheme.tsx
+++ packages/in-themes/LocallyChangedTheme.tsx
@@ -3,10 +3,15 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import * as React from 'react';
 
 import { ThemeContext } from '@instana/components';
 
-export default function LocallyChangedTheme({ theme, children }) {
+interface Props {
+  children: React.ReactNode;
+  theme: string;
+}
+
+export default function LocallyChangedTheme({ theme, children }: Props) {
   return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
 }
