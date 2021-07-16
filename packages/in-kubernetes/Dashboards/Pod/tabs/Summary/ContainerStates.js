e50e496d319b3d8938b1e1b54b8f226339c415c4
Nathan Fisher <nfisher@junctionbox.ca>
2021-07-16 15:19:50 -0400

    Add missing health cell for unmonitored containers in pod container dashboard (#6787)
    The table for containers looks broken when a container is unmonitored (e.g. init containers) and has no associated health info. Added a new cell containing an mdash for the health info column.

--- packages/in-kubernetes/Dashboards/Pod/tabs/Summary/ContainerStates.js
+++ packages/in-kubernetes/Dashboards/Pod/tabs/Summary/ContainerStates.js
@@ -155,6 +155,7 @@ export default connectTo(
                       />
                     )}
                   </Td>
+                  <Td>&mdash;</Td>
                 </Tr>
               );
             })}
