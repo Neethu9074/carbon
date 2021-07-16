0545373b63fdde17d9fa1322f2da3a6a49f9c75e
Ben Blackmore <ben.blackmore@instana.com>
2021-07-16 17:24:03 +0200

    Translate in-stores/navigation/urlParameterUtils.ts

--- packages/in-stores/navigation/urlParameterUtils.ts
+++ packages/in-stores/navigation/urlParameterUtils.ts
@@ -3,5 +3,5 @@
  * (c) Copyright Instana Inc.
  */
 
-export const intParser = v => (v != null ? parseInt(v, 10) : 1);
-export const numberParser = v => (v != null ? parseFloat(v) : 1);
+export const intParser = (v: string) => (v != null ? parseInt(v, 10) : 1);
+export const numberParser = (v: string) => (v != null ? parseFloat(v) : 1);
