e1727f071443011045a271ce6eaae9b5a91ba131
Ben Blackmore <ben.blackmore@instana.com>
2021-07-22 09:22:30 +0200

    Translate in-components/QueryBuilder/tagFilter/operators.ts

--- packages/in-components/QueryBuilder/tagFilter/operators.ts
+++ packages/in-components/QueryBuilder/tagFilter/operators.ts
@@ -3,19 +3,21 @@
  * (c) Copyright Instana Inc.
  */
 
-export const EQUALS = 'EQUALS';
-export const CONTAINS = 'CONTAINS';
-export const LESS_THAN = 'LESS_THAN';
-export const GREATER_THAN = 'GREATER_THAN';
-export const NOT_EMPTY = 'NOT_EMPTY';
-export const IS_EMPTY = 'IS_EMPTY';
-export const NOT_EQUAL = 'NOT_EQUAL';
-export const NOT_CONTAIN = 'NOT_CONTAIN';
-export const NOT_BLANK = 'NOT_BLANK';
-export const IS_BLANK = 'IS_BLANK';
-export const STARTS_WITH = 'STARTS_WITH';
-export const ENDS_WITH = 'ENDS_WITH';
-export const NOT_STARTS_WITH = 'NOT_STARTS_WITH';
-export const NOT_ENDS_WITH = 'NOT_ENDS_WITH';
-export const GREATER_OR_EQUAL_THAN = 'GREATER_OR_EQUAL_THAN';
-export const LESS_OR_EQUAL_THAN = 'LESS_OR_EQUAL_THAN';
+import { TagFilterOperator } from 'in-types/backend';
+
+export const EQUALS: TagFilterOperator = 'EQUALS';
+export const CONTAINS: TagFilterOperator = 'CONTAINS';
+export const LESS_THAN: TagFilterOperator = 'LESS_THAN';
+export const GREATER_THAN: TagFilterOperator = 'GREATER_THAN';
+export const NOT_EMPTY: TagFilterOperator = 'NOT_EMPTY';
+export const IS_EMPTY: TagFilterOperator = 'IS_EMPTY';
+export const NOT_EQUAL: TagFilterOperator = 'NOT_EQUAL';
+export const NOT_CONTAIN: TagFilterOperator = 'NOT_CONTAIN';
+export const NOT_BLANK: TagFilterOperator = 'NOT_BLANK';
+export const IS_BLANK: TagFilterOperator = 'IS_BLANK';
+export const STARTS_WITH: TagFilterOperator = 'STARTS_WITH';
+export const ENDS_WITH: TagFilterOperator = 'ENDS_WITH';
+export const NOT_STARTS_WITH: TagFilterOperator = 'NOT_STARTS_WITH';
+export const NOT_ENDS_WITH: TagFilterOperator = 'NOT_ENDS_WITH';
+export const GREATER_OR_EQUAL_THAN: TagFilterOperator = 'GREATER_OR_EQUAL_THAN';
+export const LESS_OR_EQUAL_THAN: TagFilterOperator = 'LESS_OR_EQUAL_THAN';
