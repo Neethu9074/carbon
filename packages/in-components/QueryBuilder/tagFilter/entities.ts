e17b7709a4354db16aa7664e9bf81c7de245a50f
Ben Blackmore <ben.blackmore@instana.com>
2021-07-22 10:23:47 +0200

    Rename in-components/QueryBuilder/tagFilter/entities.ts

--- packages/in-components/QueryBuilder/tagFilter/entities.ts
+++ packages/in-components/QueryBuilder/tagFilter/entities.ts
@@ -3,6 +3,8 @@
  * (c) Copyright Instana Inc.
  */
 
+import {TagFilterEntity} from 'in-types';
+
 export const NOT_APPLICABLE = 'NOT_APPLICABLE';
 export const DESTINATION = 'DESTINATION';
 export const SOURCE = 'SOURCE';
