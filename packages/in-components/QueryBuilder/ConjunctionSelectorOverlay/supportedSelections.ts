cdb99d4b5fc5ee91973117ad891e1251249f6efa
Ben Blackmore <ben.blackmore@instana.com>
2021-07-22 10:33:35 +0200

    Translate in-components/QueryBuilder/ConjunctionSelectorOverlay/supportedSelections.ts

--- packages/in-components/QueryBuilder/ConjunctionSelectorOverlay/supportedSelections.ts
+++ packages/in-components/QueryBuilder/ConjunctionSelectorOverlay/supportedSelections.ts
@@ -5,9 +5,16 @@
 
 import { LogicalOperator } from 'in-types';
 
-export const and = 'AND';
-export const or = 'OR';
+export const and: LogicalOperator = 'AND';
+export const or: LogicalOperator = 'OR';
 export const not = 'NOT';
 export const openBracket = '(';
 export const closeBracket = ')';
 export const clear = '';
+
+export type ConjunctionSelectorSelection =
+  | LogicalOperator
+  | typeof not
+  | typeof openBracket
+  | typeof closeBracket
+  | typeof clear;
