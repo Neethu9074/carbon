55dff915eafeb2d787bce7952f8c6e461c475367
Ben Blackmore <ben.blackmore@instana.com>
2021-07-22 10:23:56 +0200

    Translate in-components/QueryBuilder/tagFilter/entities.ts

--- packages/in-components/QueryBuilder/tagFilter/entities.ts
+++ packages/in-components/QueryBuilder/tagFilter/entities.ts
@@ -3,8 +3,8 @@
  * (c) Copyright Instana Inc.
  */
 
-import {TagFilterEntity} from 'in-types';
+import { TagFilterEntity } from 'in-types';
 
-export const NOT_APPLICABLE = 'NOT_APPLICABLE';
-export const DESTINATION = 'DESTINATION';
-export const SOURCE = 'SOURCE';
+export const NOT_APPLICABLE: TagFilterEntity = 'NOT_APPLICABLE';
+export const DESTINATION: TagFilterEntity = 'DESTINATION';
+export const SOURCE: TagFilterEntity = 'SOURCE';
