2a08ec018cbb9f812eb46d1eaf5c33877e65541e
Syam Pillai <syam.sp0965@gmail.com>
2021-07-23 12:24:51 +0200

    converted in-components/form/TouchedMessages/TouchedMessages.tsx

--- packages/in-components/form/TouchedMessages/TouchedMessages.tsx
+++ packages/in-components/form/TouchedMessages/TouchedMessages.tsx
@@ -11,12 +11,22 @@ import { Trans } from 'in-i18n';
 // @ts-expect-error
 import locals from './TouchedMessages.mless';
 
-export default function TouchedMessages({ field, className }) {
+export interface TouchedMessagesProps {
+  field: Record<string, any>;
+  className: string;
+}
+
+interface FieldMessage {
+  path: string;
+  message: string;
+}
+
+export default function TouchedMessages({ field, className }: TouchedMessagesProps) {
   if (!field?.hierarchyTouched) {
     return null;
   }
 
-  return field.messages.map((message, i) => {
+  return field.messages.map((message: FieldMessage, i: number) => {
     if (message.path) {
       return (
         <ValidationBlock key={i} className={className}>
