af6c79b735ee972364c89b6f4d3c3178b6c4e27d
Ben Blackmore <ben.blackmore@instana.com>
2021-07-23 14:37:43 +0200

    Translate in-custom-dashboards/widgets/Markdown/form.ts

--- packages/in-custom-dashboards/widgets/Markdown/form.ts
+++ packages/in-custom-dashboards/widgets/Markdown/form.ts
@@ -3,14 +3,14 @@
  * (c) Copyright Instana Inc.
  */
 
-import { createField } from 'formalistic';
+import { createField, Field } from 'formalistic';
 
 import { composeAndShortCircuitOnError } from 'in-services/validators/compose';
 import { notUndefinedValidator } from 'in-services/validators/undefined';
 import { notBlankValidator } from 'in-services/validators/string';
 import { stringValidator } from 'in-services/validators/jsonType';
 
-export function createForm(savedState) {
+export function createForm(savedState?: string): Field<string> {
   return createField({
     value: savedState || '',
     validator: composeAndShortCircuitOnError(notUndefinedValidator, stringValidator, notBlankValidator)
