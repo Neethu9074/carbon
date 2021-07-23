06488b8dd35f3d14d43b4f3be4df200c89f24920
Ben Blackmore <ben.blackmore@instana.com>
2021-07-23 17:33:34 +0200

    Translate in-custom-dashboards/widgets/BigNumber/Badge.tsx

--- packages/in-custom-dashboards/widgets/BigNumber/Badge.tsx
+++ packages/in-custom-dashboards/widgets/BigNumber/Badge.tsx
@@ -3,15 +3,25 @@
  * (c) Copyright Instana Inc.
  */
 
-import React, { forwardRef } from 'react';
+import React, { forwardRef, ReactNode } from 'react';
 import { find } from 'lodash';
 
 import * as colors from 'in-custom-dashboards/widgets/BigNumber/comparisonColors';
 
+// @ts-ignore
 import locals from './Badge.mless';
 
-export default forwardRef(function Badge({ children, colorId }, ref) {
-  const { backgroundColor, foregroundColor } = find(colors, ({ id }) => id === colorId);
+export interface BadgeProps {
+  colorId: string;
+  children: ReactNode;
+}
+
+export default forwardRef<HTMLDivElement, BadgeProps>(function Badge({ children, colorId }, ref) {
+  const color = find(colors, ({ id }) => id === colorId);
+  if (!color) {
+    return null;
+  }
+  const { backgroundColor, foregroundColor } = color;
   return (
     <div style={{ backgroundColor, color: foregroundColor }} className={locals.badge} ref={ref}>
       {children}
