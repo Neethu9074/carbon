957a1a61776cadc3cfa5c9cc33b5f5a33e4e867a
Ben Blackmore <ben.blackmore@instana.com>
2021-07-27 12:11:31 +0200

    Fix wrong element presentation

--- packages/in-websites/NewWebsiteFlow/ReadyStep.js
+++ packages/in-websites/NewWebsiteFlow/ReadyStep.js
@@ -20,7 +20,7 @@ export default function ReadyStep({ websiteId, websiteName, websiteLink$, trackS
       <Paragraph>
         <Trans
           i18nKey="in-websites:newWebsiteFlow.readyStepParagraphEverythingIsReadyToMonitorYourWebsite"
-          values={{ websiteName: websiteName, httpHeadTag: new SecureString('<head>') }}
+          values={{ websiteName: websiteName, httpHeadTag: new SecureString('<head />') }}
           components={{
             linkToDocs: <Link href="https://instana.com/docs/website_monitoring/" external />
           }}
