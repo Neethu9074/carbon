c7bd5f74aabec023d755c33fd1c7143bea24867c
Syam Pillai <syam.sp0965@gmail.com>
2021-08-06 11:04:49 +0200

    Translated in-alerting/smart-alerts/components/smart-alert-dialog/advanced/thresholdFormHelper.ts

--- packages/in-alerting/smart-alerts/components/smart-alert-dialog/advanced/thresholdFormHelper.ts
+++ packages/in-alerting/smart-alerts/components/smart-alert-dialog/advanced/thresholdFormHelper.ts
@@ -3,16 +3,21 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Field, MapForm } from 'formalistic';
 import { HISTORIC_BASELINE } from 'in-alerting/smart-alerts/data/thresholdTypes';
 
-export function getFormValueOrDefault(form, key, defaultValue = null) {
-  return form.containsKey(key) ? form.get(key).value : defaultValue;
+export function getFormValueOrDefault(form: MapForm, key: string, defaultValue = null) {
+  const item: Field<any> | undefined = form.get(key) as Field<any> | undefined;
+  return form.containsKey(key) ? item?.value : defaultValue;
 }
 
-export function getThresholdComboBoxValue(form) {
-  let type = form.get('threshold').get('type').value;
+export function getThresholdComboBoxValue(form: MapForm) {
+  const thresholdForm: MapForm = form.get('threshold') as MapForm;
+  const thresholdType: Field<any> | undefined = thresholdForm.get('type') as Field<any> | undefined;
+  const seasonality: Field<any> | undefined = thresholdForm.get('seasonality') as Field<any> | undefined;
+  let type = thresholdType?.value;
   if (type === HISTORIC_BASELINE) {
-    return `${type}.${form.get('threshold').get('seasonality').value}`;
+    return `${type}.${seasonality?.value}`;
   }
   return type;
 }
