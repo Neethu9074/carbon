a1b03deca5a3878d678293b3a51ecf31d0c55863
simonThiele <simon.thiele.st@gmail.com>
2021-08-06 10:05:45 +0200

    translate thresholdTypes

--- packages/in-alerting/smart-alerts/data/thresholdTypes.ts
+++ packages/in-alerting/smart-alerts/data/thresholdTypes.ts
@@ -3,6 +3,8 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-export const STATIC_THRESHOLD = 'staticThreshold';
-export const HISTORIC_BASELINE = 'historicBaseline';
-export const ADAPTIVE_BASELINE = 'adaptiveBaseline';
+import { ThresholdType } from 'in-types';
+
+export const STATIC_THRESHOLD: ThresholdType = 'staticThreshold';
+export const HISTORIC_BASELINE: ThresholdType = 'historicBaseline';
+export const ADAPTIVE_BASELINE: ThresholdType = 'adaptiveBaseline';
