b5d92e4fc5ab239795a57ffc8faa00afd25074f1
Simon Thiele <simon.thiele@instana.com>
2021-08-10 13:08:04 +0200

    translate getLog

--- packages/in-logging/subscriptions/getLog.ts
+++ packages/in-logging/subscriptions/getLog.ts
@@ -4,9 +4,16 @@
  */
 
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
+import { Result, LogItem } from 'in-types';
 import { minutes } from 'in-services/time';
 
-export default createResultSubscriptionFactory({
+interface CreateResultSubscriptionFactoryRequest {}
+interface CreateResultSubscriptionFactoryResponse extends Result<LogItem> {}
+
+export default createResultSubscriptionFactory<
+  CreateResultSubscriptionFactoryRequest,
+  CreateResultSubscriptionFactoryResponse
+>({
   eventId: 'logsV2.getLog',
   memoizeFor: minutes.toMillis(5)
 });
