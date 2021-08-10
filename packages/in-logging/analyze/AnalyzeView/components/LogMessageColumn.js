3da294fc1336300bfab6c9819d4f896fb2e4ac20
Simon Thiele <simon.thiele@instana.com>
2021-08-10 09:47:55 +0200

    remove tag selector component

--- packages/in-logging/analyze/AnalyzeView/components/LogMessageColumn.js
+++ packages/in-logging/analyze/AnalyzeView/components/LogMessageColumn.js
@@ -7,12 +7,8 @@ import React, { useState, useRef, useLayoutEffect } from 'react';
 import classNames from 'classnames';
 
 import LogMessage from 'in-logging/analyze/AnalyzeView/components/LogMessage';
-import { getLinkToTraceDetail } from 'in-analyze/navigation/paths';
 import IconButton from 'in-components/IconButton/IconButton';
 import useResizeObserver from 'in-hooks/useResizeObserver';
-import { LOG_TRACE_ID } from 'in-logging/queryBuilder';
-import Tooltip from 'in-components/Tooltip';
-import { t } from 'in-i18n';
 
 import locals from './LogMessageColumn.mless';
 
@@ -33,44 +29,29 @@ export default function LogMessageColumn(props) {
   }, [wrapperWidth]);
 
   return (
-    <div className={locals.wrapper}>
-      <div className={locals.messageWrapper} ref={ref}>
-        <span
-          className={classNames({
-            [locals.message]: true,
-            [locals.collapsedMessage]: !isExpanded,
-            [locals.messageExpanded]: isExpanded
-          })}
-          ref={messageRef}
-        >
-          <LogMessage
-            tags={tags}
-            message={message}
-            getHrefWithAdditionalTagFilter={getHrefWithAdditionalTagFilter}
-            getHrefToGroupedView={getHrefToGroupedView}
-          />
-        </span>
+    <div className={locals.messageWrapper} ref={ref}>
+      <span
+        className={classNames({
+          [locals.message]: true,
+          [locals.collapsedMessage]: !isExpanded,
+          [locals.messageExpanded]: isExpanded
+        })}
+        ref={messageRef}
+      >
+        <LogMessage
+          tags={tags}
+          message={message}
+          getHrefWithAdditionalTagFilter={getHrefWithAdditionalTagFilter}
+          getHrefToGroupedView={getHrefToGroupedView}
+          isOverflowing={isOverflowing}
+          setIsExpanded={setIsExpanded}
+        />
+      </span>
 
-        <div>
-          {isOverflowing && (
-            <IconButton
-              type={isExpanded ? 'lib_arrow_expand_up' : 'lib_arrow_expand_down'}
-              onClick={() => setIsExpanded(!isExpanded)}
-            />
-          )}
-
-          <TraceIcon tags={tags} />
-        </div>
-      </div>
+      <IconButton
+        type={isExpanded ? 'lib_arrow_expand_up' : 'lib_arrow_expand_down'}
+        onClick={() => setIsExpanded(!isExpanded)}
+      />
     </div>
   );
-}
-
-function TraceIcon({ tags }) {
-  const traceId = tags.filter(({ name }) => name === LOG_TRACE_ID)[0]?.stringValue;
-  return traceId ? (
-    <Tooltip content={t('in-logging:goToTrace')}>
-      <IconButton type="lib_application_trace" href$={getLinkToTraceDetail(traceId)} />
-    </Tooltip>
-  ) : null;
 }
