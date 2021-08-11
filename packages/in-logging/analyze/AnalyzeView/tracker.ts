90eb4dec3531e349b42fc443029b5d37afce5018
Simon Thiele <simon.thiele@instana.com>
2021-08-11 11:55:56 +0200

    translate tracker

--- packages/in-logging/analyze/AnalyzeView/tracker.ts
+++ packages/in-logging/analyze/AnalyzeView/tracker.ts
@@ -19,15 +19,15 @@ import {
   ANALYZE_LOGGING_TIME_SPENT
 } from 'in-services/tracking/tracking';
 
-export const filterAdded = e => track(ANALYZE_LOGGING_QUERY_BUILDER_FILTER_ADDED, e);
-export const queryChanged = e => track(ANALYZE_LOGGING_QUERY_BUILDER_QUERY_CHANGED, e);
-export const groupAdded = e => track(ANALYZE_LOGGING_QUERY_BUILDER_GROUP_ADDED, e);
-export const chartChanged = e => track(ANALYZE_LOGGING_QUERY_BUILDER_CHART_CHANGED, e);
-export const selectedChanged = e => track(ANALYZE_LOGGING_SELECTED_TAGS_CHANGED, e);
-export const loadMoreClicked = e => track(ANALYZE_LOGGING_LOAD_MORE_CLICKED, e);
-export const timeframeUsed = e => track(ANALYZE_LOGGING_TIMEFRAME_USED, e);
-export const logMessageParameterClicked = e => track(ANALYZE_LOGGING_LOG_MESSAGE_PARAMETER_CLICKED, e);
-export const facettedSearchGroupClicked = e => track(ANALYZE_LOGGING_FACETTEDSEARCH_GROUP_CLICKED, e);
-export const facettedSearchItemClicked = e => track(ANALYZE_LOGGING_FACETTEDSEARCH_ITEM_CLICKED, e);
-export const jumpToLogs = e => track(ANALYZE_LOGGING_JUMP_TO_LOGS, e);
-export const timeSpent = e => track(ANALYZE_LOGGING_TIME_SPENT, e);
+export const filterAdded = (e: any) => track(ANALYZE_LOGGING_QUERY_BUILDER_FILTER_ADDED, e);
+export const queryChanged = (e: any) => track(ANALYZE_LOGGING_QUERY_BUILDER_QUERY_CHANGED, e);
+export const groupAdded = (e: any) => track(ANALYZE_LOGGING_QUERY_BUILDER_GROUP_ADDED, e);
+export const chartChanged = (e: any) => track(ANALYZE_LOGGING_QUERY_BUILDER_CHART_CHANGED, e);
+export const selectedChanged = (e: any) => track(ANALYZE_LOGGING_SELECTED_TAGS_CHANGED, e);
+export const loadMoreClicked = (e: any) => track(ANALYZE_LOGGING_LOAD_MORE_CLICKED, e);
+export const timeframeUsed = (e: any) => track(ANALYZE_LOGGING_TIMEFRAME_USED, e);
+export const logMessageParameterClicked = (e: any) => track(ANALYZE_LOGGING_LOG_MESSAGE_PARAMETER_CLICKED, e);
+export const facettedSearchGroupClicked = (e: any) => track(ANALYZE_LOGGING_FACETTEDSEARCH_GROUP_CLICKED, e);
+export const facettedSearchItemClicked = (e: any) => track(ANALYZE_LOGGING_FACETTEDSEARCH_ITEM_CLICKED, e);
+export const jumpToLogs = (e: any) => track(ANALYZE_LOGGING_JUMP_TO_LOGS, e);
+export const timeSpent = (e: any) => track(ANALYZE_LOGGING_TIME_SPENT, e);
