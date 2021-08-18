4706d896e47122d5b8565be6aad1e5cc22c51344
SANDHYA RAVI <SANDHYA.R1@ibm.com>
2021-08-18 19:18:01 +0530

    ui change

--- packages/in-forge/plugins/db2Database/metricDefinitions.js
+++ packages/in-forge/plugins/db2Database/metricDefinitions.js
@@ -79,7 +79,7 @@ export default [
 
       t('in-forge:plugins.db2Database.tablesizes'),
       t('in-forge:plugins.db2Database.tableName'),
-      t('in-forge:plugins.db2Database.cardtab'),
+      t('in-forge:plugins.db2Database.cardTab'),
       t('in-forge:plugins.db2Database.tabSizeKB'),
       t('in-forge:plugins.db2Database.tabSizeMB'),
       t('in-forge:plugins.db2Database.avgRowSize'),
