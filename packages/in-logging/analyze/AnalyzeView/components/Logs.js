1180e9faed4fd7c0277fc4c25224f24d9aea119b
Simon Thiele <simon.thiele@instana.com>
2021-09-02 08:04:15 +0200

    change wording

--- packages/in-logging/analyze/AnalyzeView/components/Logs.js
+++ packages/in-logging/analyze/AnalyzeView/components/Logs.js
@@ -5,6 +5,8 @@
 
 import React, { useMemo } from 'react';
 
+import { Button } from '@instana/components';
+
 import useLogsCursorPagination from 'in-logging/analyze/AnalyzeView/components/hooks/useLogsCursorPagination';
 import { FacetedSearchPresenter } from 'in-logging/analyze/AnalyzeView/components/FacetedSearchPresenter';
 import QueryBuilderWorkspace from 'in-logging/analyze/AnalyzeView/components/QueryBuilderWorkspace';
@@ -116,6 +118,10 @@ export default function Logs(props) {
       onSelectTagHref={onSelectTagHref}
       withCountHeader={false}
       tracker={tracker}
+<<<<<<< Updated upstream
+=======
+      CustomHeaderActions={CustomHeaderActions}
+>>>>>>> Stashed changes
       renderNestedContent={(_, item) => (
         <LogTagsTable
           item={item}
@@ -137,6 +143,23 @@ export default function Logs(props) {
 
 function DetailView() {
   return null;
+}
+
+function CustomHeaderActions({ orderBy, setOrder }) {
+  return (
+    <Button
+      icon={orderBy.direction === 'ASC' ? 'lib_actions_sort_ascending' : 'lib_actions_sort_descending'}
+      kind="secondary"
+      onClick={() =>
+        setOrder({
+          by: orderBy.by,
+          direction: orderBy.direction === 'ASC' ? 'DESC' : 'ASC'
+        })
+      }
+    >
+      {orderBy.direction === 'ASC' ? 'Most recent first' : 'Oldest first'}
+    </Button>
+  );
 }
 
 function getTableData(props) {
