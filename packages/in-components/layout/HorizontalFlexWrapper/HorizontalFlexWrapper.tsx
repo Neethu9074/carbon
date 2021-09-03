8f9d3cda2790addc21fb8ff2b814b423e9417eeb
Raj Parekh <raj.parekh@instana.com>
2021-09-03 15:50:11 +0200

    Translated packages/in-components/layout/HorizontalFlexWrapper/HorizontalFlexWrapper.tsx

--- packages/in-components/layout/HorizontalFlexWrapper/HorizontalFlexWrapper.tsx
+++ packages/in-components/layout/HorizontalFlexWrapper/HorizontalFlexWrapper.tsx
@@ -6,9 +6,18 @@
 import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 
+// @ts-expect-error
 import locals from './HorizontalFlexWrapper.mless';
 
-export default forwardRef(function HorizontalFlexWrapper({ className, children, ...furtherProps }, ref) {
+interface HorizontalFlexWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default forwardRef<HTMLDivElement, HorizontalFlexWrapperProps>(function HorizontalFlexWrapper(
+  { className, children, ...furtherProps }: HorizontalFlexWrapperProps,
+  ref
+) {
   return (
     <div ref={ref} className={classNames(locals.wrapper, className)} {...furtherProps}>
       {children}
