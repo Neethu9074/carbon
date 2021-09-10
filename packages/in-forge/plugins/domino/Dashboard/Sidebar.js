3acf6a689da664cf597d1f849bada739d97da2fc
ivangligoric <ivan.gligoric.23@gmail.com>
2021-09-10 14:34:04 +0200

    Display snapshot data

--- packages/in-forge/plugins/domino/Dashboard/Sidebar.js
+++ packages/in-forge/plugins/domino/Dashboard/Sidebar.js
@@ -5,8 +5,8 @@
 
 import React from 'react';
 
-import ServiceInstancesList from 'in-sdk/components/sidebar/ServiceInstancesList';
 import Collapsible from 'in-sdk/components/sidebar/Collapsible';
+import MetricTypes from '../MetricTypes';
 import { t } from 'in-i18n';
 import Info from '../Info';
 
@@ -15,13 +15,18 @@ export default function DominoSidebar({ snapshot }) {
   return (
     <div>
       <Collapsible initiallyOpen>
-        <Collapsible.Header>{t('in-forge:plugins.domino.dashboard.dominoInfo')}</Collapsible.Header>
+        <Collapsible.Header>{t('in-forge:plugins.domino.dashboard.metricTypes')}</Collapsible.Header>
+        <Collapsible.Content>
+          <MetricTypes snapshot={snapshot} />
+        </Collapsible.Content>
+      </Collapsible>
+
+      <Collapsible initiallyOpen>
+        <Collapsible.Header>{t('in-forge:plugins.domino.dashboard.info')}</Collapsible.Header>
         <Collapsible.Content>
           <Info snapshot={snapshot} />
         </Collapsible.Content>
       </Collapsible>
-
-      <ServiceInstancesList snapshot={snapshot} />
     </div>
   );
 }
--- packages/in-forge/plugins/domino/Info.js
+++ packages/in-forge/plugins/domino/Info.js
@@ -6,25 +6,27 @@
 import React from 'react';
 
 import { DescriptionItem, DescriptionList } from 'in-sdk/components/sidebar/DescriptionList';
-import { emptyList } from 'in-services/fixedImmutables';
 import { t } from 'in-i18n';
 
 export default function Info({ snapshot }) {
-  const counters = countMetrics(snapshot, 'metrics.counters');
+  const data = snapshot.get('data');
 
   return (
     <DescriptionList>
-      <DescriptionItem title={t('in-forge:plugins.prometheus.counters')}>{counters}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.pid')}>{data.get('pid')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.installType')}>{data.get('InstallType')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.kitType')}>{data.get('KitType')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.faultRecoveryBuild')}>
+        {data.get('FaultRecovery_Build')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.mailServer')}>{data.get('MailServer')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.serverKeyFileName')}>
+        {data.get('ServerKeyFileName')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.serverName')}>{data.get('ServerName')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.domino.serverRestarted')}>
+        {data.get('ServerRestarted')}
+      </DescriptionItem>
     </DescriptionList>
   );
-}
-
-function countMetrics(snapshot, prefix) {
-  const metricCount = snapshot.get('metricIds', emptyList).filter(m => m.startsWith(prefix)).size;
-
-  if (metricCount > 0) {
-    return metricCount;
-  }
-
-  return snapshot.getIn(['data', prefix], emptyList).size;
 }
