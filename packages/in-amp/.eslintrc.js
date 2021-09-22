7e5fda625217ab13fb2d313ff2e08d8c5606bb7e
Ben Blackmore <ben.blackmore@instana.com>
2021-09-22 13:36:24 +0200

    Import rules for in-amp

---
+++ packages/in-amp/.eslintrc.js
@@ -0,0 +1,27 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. 2021
+ */
+
+/* eslint-env node */
+
+const { createImportRestrictionRule } = require('../../build/linting/restrictedImportRule');
+
+module.exports = {
+  rules: {
+    ...createImportRestrictionRule({
+      enforceAbsoluteImportPaths: true,
+
+      allowedInPackages: [
+        'in-amp',
+        // TODO this import should not exist here
+        'in-custom-dashboards',
+        'in-components',
+        'in-hooks',
+        'in-services',
+        'in-settings',
+        'in-stores'
+      ]
+    })
+  }
+};
