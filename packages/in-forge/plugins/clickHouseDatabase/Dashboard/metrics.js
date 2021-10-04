ca7af314ea10e54de5e9ea2fae9a387908d6b77a
Yoann Buch <yoann.buch@instana.com>
2021-10-04 11:19:00 +0200

    Add more ClickHouse metrics (#7451)
    Why
    See instana/backend#12402
    
    What
    Update the list of ClickHouse metrics to match the ones extracted by the backend.

--- packages/in-forge/plugins/clickHouseDatabase/Dashboard/metrics.js
+++ packages/in-forge/plugins/clickHouseDatabase/Dashboard/metrics.js
@@ -8,19 +8,23 @@ import { number, bytes } from 'in-services/formatters/number';
 export default [
   // system.metrics
   {
-    metric: 'Merge',
+    metric: 'BackgroundBufferFlushSchedulePoolTask',
     formatter: number
   },
   {
-    metric: 'ReplicatedFetch',
+    metric: 'BackgroundDistributedSchedulePoolTask',
     formatter: number
   },
   {
-    metric: 'ReplicatedSend',
+    metric: 'BackgroundFetchesPoolTask',
     formatter: number
   },
   {
-    metric: 'ReplicatedChecks',
+    metric: 'BackgroundMessageBrokerSchedulePoolTask',
+    formatter: number
+  },
+  {
+    metric: 'BackgroundMovePoolTask',
     formatter: number
   },
   {
@@ -28,7 +32,35 @@ export default [
     formatter: number
   },
   {
+    metric: 'BackgroundSchedulePoolTask',
+    formatter: number
+  },
+  {
+    metric: 'CacheDictionaryUpdateQueueBatches',
+    formatter: number
+  },
+  {
+    metric: 'CacheDictionaryUpdateQueueKeys',
+    formatter: number
+  },
+  {
+    metric: 'ContextLockWait',
+    formatter: number
+  },
+  {
+    metric: 'DelayedInserts',
+    formatter: number
+  },
+  {
+    metric: 'DictCacheRequests',
+    formatter: number
+  },
+  {
     metric: 'DiskSpaceReservedForMerge',
+    formatter: bytes
+  },
+  {
+    metric: 'DistributedFilesToInsert',
     formatter: number
   },
   {
@@ -36,15 +68,15 @@ export default [
     formatter: number
   },
   {
-    metric: 'DistributedFilesToInsert',
+    metric: 'EphemeralNode',
     formatter: number
   },
   {
-    metric: 'QueryPreempted',
+    metric: 'GlobalThread',
     formatter: number
   },
   {
-    metric: 'TCPConnection',
+    metric: 'GlobalThreadActive',
     formatter: number
   },
   {
@@ -56,6 +88,30 @@ export default [
     formatter: number
   },
   {
+    metric: 'LocalThread',
+    formatter: number
+  },
+  {
+    metric: 'LocalThreadActive',
+    formatter: number
+  },
+  {
+    metric: 'MaxDDLEntryID',
+    formatter: number
+  },
+  {
+    metric: 'MemoryTracking',
+    formatter: bytes
+  },
+  {
+    metric: 'Merge',
+    formatter: number
+  },
+  {
+    metric: 'MySQLConnection',
+    formatter: number
+  },
+  {
     metric: 'OpenFileForRead',
     formatter: number
   },
@@ -64,15 +120,59 @@ export default [
     formatter: number
   },
   {
-    metric: 'Read',
+    metric: 'PartMutation',
     formatter: number
   },
   {
-    metric: 'Write',
+    metric: 'PartsCommitted',
     formatter: number
   },
   {
-    metric: 'SendExternalTables',
+    metric: 'PartsCompact',
+    formatter: number
+  },
+  {
+    metric: 'PartsDeleteOnDestroy',
+    formatter: number
+  },
+  {
+    metric: 'PartsDeleting',
+    formatter: number
+  },
+  {
+    metric: 'PartsInMemory',
+    formatter: number
+  },
+  {
+    metric: 'PartsOutdated',
+    formatter: number
+  },
+  {
+    metric: 'PartsPreCommitted',
+    formatter: number
+  },
+  {
+    metric: 'PartsTemporary',
+    formatter: number
+  },
+  {
+    metric: 'PartsTemporary',
+    formatter: number
+  },
+  {
+    metric: 'PartsWide',
+    formatter: number
+  },
+  {
+    metric: 'PostgreSQLConnection',
+    formatter: number
+  },
+  {
+    metric: 'Query',
+    formatter: number
+  },
+  {
+    metric: 'QueryPreempted',
     formatter: number
   },
   {
@@ -80,59 +180,11 @@ export default [
     formatter: number
   },
   {
-    metric: 'ReadonlyReplica',
+    metric: 'RWLockActiveReaders',
     formatter: number
   },
   {
-    metric: 'LeaderReplica',
-    formatter: number
-  },
-  {
-    metric: 'MemoryTracking',
-    formatter: bytes
-  },
-  {
-    metric: 'MemoryTrackingInBackgroundProcessingPool',
-    formatter: bytes
-  },
-  {
-    metric: 'MemoryTrackingForMerges',
-    formatter: bytes
-  },
-  {
-    metric: 'LeaderElection',
-    formatter: number
-  },
-  {
-    metric: 'EphemeralNode',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperWatch',
-    formatter: number
-  },
-  {
-    metric: 'DelayedInserts',
-    formatter: number
-  },
-  {
-    metric: 'ContextLockWait',
-    formatter: number
-  },
-  {
-    metric: 'StorageBufferRows',
-    formatter: number
-  },
-  {
-    metric: 'StorageBufferBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'DictCacheRequests',
-    formatter: number
-  },
-  {
-    metric: 'Revision',
+    metric: 'RWLockActiveWriters',
     formatter: number
   },
   {
@@ -144,69 +196,77 @@ export default [
     formatter: number
   },
   {
-    metric: 'RWLockActiveReaders',
+    metric: 'Read',
     formatter: number
   },
   {
-    metric: 'RWLockActiveWriters',
+    metric: 'ReadonlyReplica',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedChecks',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedFetch',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedSend',
+    formatter: number
+  },
+  {
+    metric: 'Revision',
+    formatter: number
+  },
+  {
+    metric: 'SendExternalTables',
+    formatter: number
+  },
+  {
+    metric: 'SendScalars',
+    formatter: number
+  },
+  {
+    metric: 'StorageBufferBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'StorageBufferRows',
+    formatter: number
+  },
+  {
+    metric: 'TCPConnection',
+    formatter: number
+  },
+  {
+    metric: 'TablesToDropQueueSize',
+    formatter: number
+  },
+  {
+    metric: 'VersionInteger',
+    formatter: number
+  },
+  {
+    metric: 'Write',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperRequest',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperSession',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperWatch',
     formatter: number
   },
 
   // system.events
   {
-    metric: 'Query',
-    formatter: number
-  },
-  {
-    metric: 'SelectQuery',
-    formatter: number
-  },
-  {
-    metric: 'InsertQuery',
-    formatter: number
-  },
-  {
-    metric: 'FileOpen',
-    formatter: number
-  },
-  {
-    metric: 'Seek',
-    formatter: number
-  },
-  {
-    metric: 'ReadBufferFromFileDescriptorRead',
-    formatter: number
-  },
-  {
-    metric: 'ReadBufferFromFileDescriptorReadBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'WriteBufferFromFileDescriptorWrite',
-    formatter: number
-  },
-  {
-    metric: 'WriteBufferFromFileDescriptorWriteBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'ReadCompressedBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'CompressedReadBufferBlocks',
-    formatter: number
-  },
-  {
-    metric: 'CompressedReadBufferBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'IOBufferAllocs',
-    formatter: number
-  },
-  {
-    metric: 'IOBufferAllocBytes',
+    metric: 'ArenaAllocBytes',
     formatter: bytes
   },
   {
@@ -214,15 +274,107 @@ export default [
     formatter: number
   },
   {
-    metric: 'ArenaAllocBytes',
-    formatter: bytes
+    metric: 'CannotRemoveEphemeralNode',
+    formatter: number
+  },
+  {
+    metric: 'CompressedReadBufferBlocks',
+    formatter: number
+  },
+  {
+    metric: 'CompressedReadBufferBytes',
+    formatter: number
+  },
+  {
+    metric: 'ContextLock',
+    formatter: number
+  },
+  {
+    metric: 'CreatedHTTPConnections',
+    formatter: number
+  },
+  {
+    metric: 'CreatedLogEntryForMerge',
+    formatter: number
+  },
+  {
+    metric: 'CreatedReadBufferAIO',
+    formatter: number
+  },
+  {
+    metric: 'CreatedReadBufferOrdinary',
+    formatter: number
+  },
+  {
+    metric: 'DiskReadElapsedMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'DiskWriteElapsedMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'DistributedConnectionFailAtAll',
+    formatter: number
+  },
+  {
+    metric: 'DistributedConnectionFailTry',
+    formatter: number
+  },
+  {
+    metric: 'DistributedConnectionStaleReplica',
+    formatter: number
+  },
+  {
+    metric: 'DuplicatedInsertedBlocks',
+    formatter: number
+  },
+  {
+    metric: 'FailedInsertQuery',
+    formatter: number
+  },
+  {
+    metric: 'FailedQuery',
+    formatter: number
+  },
+  {
+    metric: 'FailedSelectQuery',
+    formatter: number
+  },
+  {
+    metric: 'FileOpen',
+    formatter: number
   },
   {
     metric: 'FunctionExecute',
     formatter: number
   },
   {
-    metric: 'TableFunctionExecute',
+    metric: 'HardPageFaults',
+    formatter: number
+  },
+  {
+    metric: 'IOBufferAllocBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'IOBufferAllocs',
+    formatter: number
+  },
+  {
+    metric: 'InsertQuery',
+    formatter: number
+  },
+  {
+    metric: 'InsertQueryTimeMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'InsertedBytes',
+    formatter: number
+  },
+  {
+    metric: 'InsertedRows',
     formatter: number
   },
   {
@@ -234,112 +386,28 @@ export default [
     formatter: number
   },
   {
-    metric: 'CreatedReadBufferOrdinary',
+    metric: 'Merge',
     formatter: number
   },
   {
-    metric: 'CreatedWriteBufferOrdinary',
+    metric: 'MergeTreeDataWriterBlocks',
     formatter: number
   },
   {
-    metric: 'ReplicatedPartMerges',
+    metric: 'MergeTreeDataWriterBlocksAlreadySorted',
     formatter: number
   },
   {
-    metric: 'InsertedRows',
-    formatter: number
-  },
-  {
-    metric: 'InsertedBytes',
+    metric: 'MergeTreeDataWriterCompressedBytes',
     formatter: bytes
   },
   {
-    metric: 'DuplicatedInsertedBlocks',
+    metric: 'MergeTreeDataWriterRows',
     formatter: number
   },
   {
-    metric: 'ZooKeeperInit',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperTransactions',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperList',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperCreate',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperRemove',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperExists',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperGet',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperSet',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperMulti',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperClose',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperWatchResponse',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperExceptions',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperWaitMicroseconds',
-    formatter: number
-  },
-  {
-    metric: 'ZooKeeperBytesSent',
+    metric: 'MergeTreeDataWriterUncompressedBytes',
     formatter: bytes
-  },
-  {
-    metric: 'ZooKeeperBytesReceived',
-    formatter: bytes
-  },
-  {
-    metric: 'DistributedConnectionStaleReplica',
-    formatter: number
-  },
-  {
-    metric: 'SlowRead',
-    formatter: number
-  },
-  {
-    metric: 'ReplicaPartialShutdown',
-    formatter: number
-  },
-  {
-    metric: 'SelectedParts',
-    formatter: number
-  },
-  {
-    metric: 'SelectedRanges',
-    formatter: number
-  },
-  {
-    metric: 'SelectedMarks',
-    formatter: number
   },
   {
     metric: 'MergedRows',
@@ -354,39 +422,43 @@ export default [
     formatter: number
   },
   {
-    metric: 'MergeTreeDataWriterRows',
+    metric: 'NetworkReceiveElapsedMicroseconds',
     formatter: number
   },
   {
-    metric: 'MergeTreeDataWriterUncompressedBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'MergeTreeDataWriterCompressedBytes',
-    formatter: bytes
-  },
-  {
-    metric: 'MergeTreeDataWriterBlocks',
+    metric: 'NetworkSendElapsedMicroseconds',
     formatter: number
   },
   {
-    metric: 'MergeTreeDataWriterBlocksAlreadySorted',
+    metric: 'NotCreatedLogEntryForMerge',
     formatter: number
   },
   {
-    metric: 'CannotRemoveEphemeralNode',
+    metric: 'OSCPUVirtualTimeMicroseconds',
     formatter: number
   },
   {
-    metric: 'LeaderElectionAcquiredLeadership',
+    metric: 'OSCPUWaitMicroseconds',
     formatter: number
   },
   {
-    metric: 'RegexpCreated',
+    metric: 'OSIOWaitMicroseconds',
     formatter: number
   },
   {
-    metric: 'ContextLock',
+    metric: 'ObsoleteReplicatedParts',
+    formatter: number
+  },
+  {
+    metric: 'Query',
+    formatter: number
+  },
+  {
+    metric: 'QueryMemoryLimitExceeded',
+    formatter: number
+  },
+  {
+    metric: 'QueryTimeMicroseconds',
     formatter: number
   },
   {
@@ -403,6 +475,288 @@ export default [
   },
   {
     metric: 'RWLockWritersWaitMilliseconds',
+    formatter: number
+  },
+  {
+    metric: 'ReadBackoff',
+    formatter: number
+  },
+  {
+    metric: 'ReadBufferAIORead',
+    formatter: number
+  },
+  {
+    metric: 'ReadBufferAIOReadBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'ReadBufferFromFileDescriptorRead',
+    formatter: number
+  },
+  {
+    metric: 'ReadBufferFromFileDescriptorReadBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'ReadCompressedBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'RealTimeMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'RegexpCreated',
+    formatter: number
+  },
+  {
+    metric: 'RejectedInserts',
+    formatter: number
+  },
+  {
+    metric: 'ReplicaPartialShutdown',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedDataLoss',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartChecks',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartChecksFailed',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartFailedFetches',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartFetches',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartFetchesOfMerged',
+    formatter: number
+  },
+  {
+    metric: 'ReplicatedPartMerges',
+    formatter: number
+  },
+  {
+    metric: 'Seek',
+    formatter: number
+  },
+  {
+    metric: '',
+    formatter: number
+  },
+  {
+    metric: 'SelectQuery',
+    formatter: number
+  },
+  {
+    metric: 'SelectQueryTimeMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'SelectedBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'SelectedMarks',
+    formatter: number
+  },
+  {
+    metric: 'SelectedParts',
+    formatter: number
+  },
+  {
+    metric: 'SelectedRanges',
+    formatter: number
+  },
+  {
+    metric: 'SelectedRows',
+    formatter: number
+  },
+  {
+    metric: 'SlowRead',
+    formatter: number
+  },
+  {
+    metric: 'SoftPageFaults',
+    formatter: number
+  },
+  {
+    metric: 'SystemTimeMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'UserTimeMicroseconds',
+    formatter: number
+  },
+  {
+    metric: 'WriteBufferFromFileDescriptorWrite',
+    formatter: number
+  },
+  {
+    metric: 'WriteBufferFromFileDescriptorWriteBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'ZooKeeperBytesReceived',
+    formatter: bytes
+  },
+  {
+    metric: 'ZooKeeperBytesSent',
+    formatter: bytes
+  },
+  {
+    metric: 'ZooKeeperClose',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperCreate',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperExists',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperGet',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperHardwareExceptions',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperInit',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperList',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperMulti',
+    formatter: number
+  },
+  {
+    metric: 'ZooKeeperRemove',
+    formatter: number
+  },
+
+  // system.asynchronous_metrics
+  {
+    metric: 'CompiledExpressionCacheCount',
+    formatter: number
+  },
+  {
+    metric: 'HTTPThreads',
+    formatter: number
+  },
+  {
+    metric: 'InterserverThreads',
+    formatter: number
+  },
+  {
+    metric: 'MarkCacheBytes',
+    formatter: number
+  },
+  {
+    metric: 'MarkCacheFiles',
+    formatter: number
+  },
+  {
+    metric: 'MaxPartCountForPartition',
+    formatter: number
+  },
+  {
+    metric: 'MemoryCode',
+    formatter: number
+  },
+  {
+    metric: 'MemoryDataAndStack',
+    formatter: number
+  },
+  {
+    metric: 'MemoryResident',
+    formatter: number
+  },
+  {
+    metric: 'MemoryShared',
+    formatter: number
+  },
+  {
+    metric: 'MemoryVirtual',
+    formatter: number
+  },
+  {
+    metric: 'NumberOfDatabases',
+    formatter: number
+  },
+  {
+    metric: 'NumberOfTables',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasMaxAbsoluteDelay',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasMaxInsertsInQueue',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasMaxMergesInQueue',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasMaxQueueSize',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasMaxRelativeDelay',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasSumInsertsInQueue',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasSumMergesInQueue',
+    formatter: number
+  },
+  {
+    metric: 'ReplicasSumQueueSize',
+    formatter: number
+  },
+  {
+    metric: 'TCPThreads',
+    formatter: number
+  },
+  {
+    metric: 'TotalBytesOfMergeTreeTables',
+    formatter: bytes
+  },
+  {
+    metric: 'TotalPartsOfMergeTreeTables',
+    formatter: number
+  },
+  {
+    metric: 'TotalRowsOfMergeTreeTables',
+    formatter: number
+  },
+  {
+    metric: 'UncompressedCacheBytes',
+    formatter: bytes
+  },
+  {
+    metric: 'Uptime',
     formatter: number
   }
 ];
