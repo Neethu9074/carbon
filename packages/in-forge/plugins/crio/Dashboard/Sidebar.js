4ab741e27219a6bf8e65fe05180ca0f659e6b525
Goran Ceko <goran.ceko@instana.com>
2021-10-06 16:21:54 +0200

    Display CRI-O labels in the sidebar (#7497)

--- packages/in-forge/plugins/crio/Dashboard/Sidebar.js
+++ packages/in-forge/plugins/crio/Dashboard/Sidebar.js
@@ -7,11 +7,14 @@ import React from 'react';
 
 import RunningComponentsList from 'in-sdk/components/sidebar/RunningComponentsList';
 import KubernetesInfo from 'in-infrastructure/Dashboard/components/KubernetesInfo';
+import KeyValueOverlay from 'in-sdk/components/sidebar/KeyValueOverlay';
 import Collapsible from 'in-sdk/components/sidebar/Collapsible';
 import Info from 'in-forge/plugins/crio/Info';
 import { t } from 'in-i18n';
 
 export default function CrioSidebar({ snapshot }) {
+  const labels = snapshot.getIn(['data', 'labels']);
+
   return (
     <div>
       <Collapsible initiallyOpen>
@@ -20,6 +23,8 @@ export default function CrioSidebar({ snapshot }) {
           <Info snapshot={snapshot} />
         </Collapsible.Content>
       </Collapsible>
+
+      <KeyValueOverlay header={t('in-forge:plugins.crio.dashboard.headerContainerLabels')} data={labels} />
 
       <KubernetesInfo snapshot={snapshot} labels={snapshot.getIn(['data', 'labels'])} />
 
