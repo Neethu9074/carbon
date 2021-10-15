c13bb5a8578a3925350324cc253f26e3cbcaf282
Ben Blackmore <bripkens@gmail.com>
2021-10-15 13:11:29 +0200

    Style mock must return local CSS names (#7603)
    # Why
    
    In our React components we typically do the following:
    
    ```js
    import locals from './Name.mless';
    
    <div className={locals.wrapper} />
    ```
    
    This works fine everywhere aside from our tests. In our
    tests, `locals` is always an empty object causing
    `locals.wrapper` to be `undefined`.
    
    # What
    
    Wrap a proxy around the stylemock so that
    `locals.wrapper === 'local-css:wrapper'`. This makes unit
    and integration testing a bit easier.

--- packages/in-test/styleMock.js
+++ packages/in-test/styleMock.js
@@ -3,5 +3,18 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-// eslint-disable-next-line no-undef
-module.exports = {};
+/* eslint-env node */
+
+// We need to manually re-create the commonJS/ESM compatibility structure,
+// because usage of Jest's moduleNameMapper API does not auto-create this.
+module.exports = {
+  __esModule: true,
+  default: new Proxy(
+    {},
+    {
+      get(target, prop) {
+        return `local-css:${prop}`;
+      }
+    }
+  )
+};
