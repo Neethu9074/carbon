2585c41e9a5f7287bb0fb73592e88416150e970d
Syam Pillai <syam.sp0965@gmail.com>
2021-10-18 14:11:32 +0200

    Added sticky header

--- packages/in-synthetics/Dashboard.tsx
+++ packages/in-synthetics/Dashboard.tsx
@@ -5,14 +5,49 @@
 
 import React from 'react';
 
-import Tests from 'in-synthetics/Tests';
+import { Card } from '@instana/components';
 
-import locals from './Dashboard.mless';
+// @ts-expect-error Module needs to be translated to TS
+import DashboardHeaderShadowModule from 'in-components/DashboardHeader/DashboardHeaderShadowModule';
+// @ts-expect-error Module needs to be translated to TS
+import LeftRightPadding from 'in-components/layout/LeftRightPadding';
+// @ts-expect-error Module needs to be translated to TS
+import ViewTrackingMeta from 'in-components/ViewTrackingMeta';
+// @ts-expect-error Module needs to be translated to TS
+import DashboardHeader from 'in-components/DashboardHeader';
+// @ts-expect-error Module needs to be translated to TS
+import Sticky from 'in-components/Sticky';
+// @ts-expect-error Module needs to be translated to TS
+import Footer from 'in-components/Footer';
+import Tests from 'in-synthetics/Tests';
+import { t } from 'in-i18n';
+
+const header = (
+  <>
+    <DashboardHeader
+      icon="lib_infra_ibmCos"
+      label={t('in-synthetics:dashboard.testList.mainLabel')}
+      title={t('in-synthetics:dashboard.testList.mainLabel')}
+    />
+    <DashboardHeaderShadowModule />
+  </>
+);
 
 export default function Dashboard() {
   return (
-    <article className={locals.dashboard}>
-      <Tests />
-    </article>
+    <Sticky header={header}>
+      <LeftRightPadding>
+        <ViewTrackingMeta
+          data={{
+            productArea: 'EUM: Websites',
+            pageRootName: 'Websites'
+          }}
+        />
+        <Card hasMarginBottom>
+          <Tests />
+        </Card>
+      </LeftRightPadding>
+      <Footer />
+    </Sticky>
   );
 }
