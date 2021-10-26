d57a6f842ab7d31bbc6bcd500e861ca0c70baacf
Syam Pillai <syam.sp0965@gmail.com>
2021-10-26 12:11:39 +0200

    Translated in-components/DashboardHeader/DashboardHeaderModule.tsx

--- packages/in-components/DashboardHeader/DashboardHeaderModule.tsx
+++ packages/in-components/DashboardHeader/DashboardHeaderModule.tsx
@@ -6,6 +6,7 @@
 import classNames from 'classnames';
 import React from 'react';
 
+// @ts-expect-error Module needs to be translated to TS
 import { themes as headerThemes } from 'in-components/DashboardHeader/DashboardHeader';
 import LeftRightPadding from 'in-components/layout/LeftRightPadding';
 
@@ -13,13 +14,21 @@ import locals from './DashboardHeaderModule.mless';
 
 export const themes = headerThemes;
 
+interface Props {
+  className?: string;
+  theme?: 'light' | 'dark' | 'default';
+  withTopBorder?: boolean;
+  withBottomBorder?: boolean;
+  children: React.ReactNode;
+}
+
 export default function DashboardHeaderModule({
-  className,
+  className = '',
   theme = themes.default,
   withTopBorder = true,
   withBottomBorder = false,
   children
-}) {
+}: Props) {
   return (
     <LeftRightPadding
       className={classNames({
