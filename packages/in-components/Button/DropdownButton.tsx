9a883041ad6f367e1ea882d1c95b2c4da43e00c3
Syam Pillai <syam.sp0965@gmail.com>
2021-10-26 12:30:26 +0200

    Translated in-components/Button/DropdownButton.tsx

--- packages/in-components/Button/DropdownButton.tsx
+++ packages/in-components/Button/DropdownButton.tsx
@@ -4,37 +4,38 @@
  */
 
 import classNames from 'classnames';
-import rpt from 'prop-types';
 import React from 'react';
 
+import { Button, ButtonSizes } from '@instana/components';
 import { SvgIcon } from '@instana/components';
-import { Button } from '@instana/components';
 
 import locals from './DropdownButton.mless';
 
-const DropdownButton = React.forwardRef(function DropdownButton(
+interface Props {
+  children: React.ReactNode;
+  expanded: boolean;
+  size: keyof typeof ButtonSizes;
+  className: string;
+}
+
+const DropdownButton = React.forwardRef<HTMLButtonElement, Props>(function DropdownButton(
   { children, expanded, size, className, ...buttonProps },
   ref
 ) {
   return (
     <Button {...buttonProps} size={size} ref={ref} className={classNames(className, locals.dropdownButton)}>
-      {/* Group into one flexbox item */}
-      <span>{children}</span>
+      <>
+        {/* Group into one flexbox item */}
+        <span>{children}</span>
 
-      <SvgIcon
-        type={expanded ? 'lib_arrow_drop_up' : 'lib_arrow_drop_down'}
-        className={classNames(locals.dropdownButtonIndicator, {
-          [`icon-${size}`]: size
-        })}
-      />
+        <SvgIcon
+          type={expanded ? 'lib_arrow_drop_up' : 'lib_arrow_drop_down'}
+          className={classNames(locals.dropdownButtonIndicator, {
+            [`icon-${size}`]: size
+          })}
+        />
+      </>
     </Button>
   );
 });
 export default DropdownButton;
-
-DropdownButton.propTypes = {
-  ...Button.propTypes,
-
-  // Changes the direction of the expansion arrow
-  expanded: rpt.bool
-};
