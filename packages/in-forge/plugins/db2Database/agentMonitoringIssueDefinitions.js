39cc201bc10aee6062365c5369d020c0f61f4dbf
padminik26 <padmiini.k@gmail.com>
2021-10-27 14:01:44 +0530

    Agent Monitoring Warning message

---
+++ packages/in-forge/plugins/db2Database/agentMonitoringIssueDefinitions.js
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. 2021
+ */
+
+import React from 'react';
+
+import { t, Trans } from 'in-i18n';
+
+export default {
+  db2luw_unable_to_connect: {
+    issueDescription: {
+      Component: function UnableToConnect({ connectionUrl, user }) {
+        return (
+          <span>
+            <Trans
+              i18nKey="in-forge:plugins.db2Database.UnableToConnect"
+              values={{ connectionUrl: connectionUrl, user: user }}
+            />
+          </span>
+        );
+      }
+    },
+    explanationLinkLabel: t('in-forge:plugins.db2Database.troubleshootingDocs'),
+    explanationLinkHref: `https://instana.com/docs/ecosystem/db2`
+  },
+  db2luw_unable_to_get_data: {
+    issueDescription: {
+      Component: function UnableToGetData() {
+        return (
+          <span>
+            <Trans i18nKey="in-forge:plugins.db2Database.UnableToGetData" />
+          </span>
+        );
+      }
+    },
+    explanationLinkLabel: t('in-forge:plugins.db2Database.troubleshootingDocs'),
+    explanationLinkHref: `https://instana.com/docs/ecosystem/db2`
+  }
+};
