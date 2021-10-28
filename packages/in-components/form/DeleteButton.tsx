6b299ed3681f46efff3c1bf972d50397c6a11a03
Syam Pillai <syam.sp0965@gmail.com>
2021-10-28 14:49:26 +0200

    Translated in-components/form/DeleteButton.tsx

--- packages/in-components/form/DeleteButton.tsx
+++ packages/in-components/form/DeleteButton.tsx
@@ -3,12 +3,24 @@
  * (c) Copyright Instana Inc.
  */
 
+import { MapForm } from 'formalistic';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Button } from '@instana/components';
+import { Button, ButtonKinds } from '@instana/components';
 
 import { t } from 'in-i18n';
+
+export interface DeleteButtonProps {
+  children: string | React.ReactElement;
+  className: string;
+  form: MapForm;
+  icon: string;
+  isDeleting: boolean;
+  disabled: boolean;
+  kind: keyof typeof ButtonKinds;
+  onClick: () => void;
+}
 
 export default function DeleteButton({
   form,
@@ -19,7 +31,7 @@ export default function DeleteButton({
   kind = 'danger',
   icon,
   onClick
-}) {
+}: DeleteButtonProps) {
   return (
     <Button
       kind={kind}
