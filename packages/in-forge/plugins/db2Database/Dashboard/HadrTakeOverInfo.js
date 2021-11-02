1a097c43e0f8cc4e5626a4be04d4af680f0301e3
vipinmenon <vipinmv1@in.ibm.com>
2021-11-02 12:29:22 +0530

    support for hadr lasttakeovertime

---
+++ packages/in-forge/plugins/db2Database/Dashboard/HadrTakeOverInfo.js
@@ -0,0 +1,33 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import { KpiKeyValue } from 'in-sdk/components/dashboard/KpiSection';
+import { getRawPayloadWithTimestamp } from 'in-stores/snapshot';
+import connectTo from 'in-hoc/connectTo';
+import { t } from 'in-i18n';
+
+export default connectTo(props => {
+  return {
+    data: getRawPayloadWithTimestamp(props.snapshotId, 'hadrrole')
+  };
+}, HadrTakeOverInfo);
+
+function HadrTakeOverInfo({ data }) {
+  if (!data || !data.get('raw_payload')) {
+    return null;
+  }
+
+  const hadrRole = data.get('raw_payload');
+  if (hadrRole.get('HADR_ROLE') === 'PRIMARY' && hadrRole.get('HADR_LAST_TAKEOVER_TIME') != 'NA') {
+    return (
+      <KpiKeyValue label={t('in-forge:plugins.db2Database.dashboard.hadrTakeOverTime')}>
+        {hadrRole.get('HADR_LAST_TAKEOVER_TIME')}
+      </KpiKeyValue>
+    );
+  }
+  return null;
+}
