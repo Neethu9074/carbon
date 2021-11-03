91c7e1aa84ebed254528f0b2a79dbc2c9bfb12f6
Marco <marco.schaefer@instana.com>
2021-11-03 17:04:33 +0100

    Convert to tsx in-custom-dashboards/widgets/Slo/components/PercentageFormInput

---
+++ packages/in-custom-dashboards/widgets/Slo/components/PercentageInput.tsx
@@ -0,0 +1,69 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import Input from 'in-components/form/Input';
+
+interface PercentageInputProps {
+  id: string;
+  value: number | undefined;
+  onChange: (value: number | undefined) => void;
+  hasError?: boolean;
+  decimalPrecision?: number;
+}
+
+export default function PercentageInput({ id, value, onChange, decimalPrecision = 4, hasError }: PercentageInputProps) {
+  const displayValue = formatNumber(value, decimalPrecision);
+
+  return (
+    <Input
+      id={id}
+      value={displayValue}
+      type="number"
+      onChange={e => {
+        let newValue: number | undefined;
+        const targetValue = e.target.valueAsNumber;
+
+        if (!Number.isNaN(targetValue)) {
+          const truncatedTargetValue = truncate(targetValue, decimalPrecision);
+          newValue = round(truncatedTargetValue / 100, decimalPrecision + 2);
+        }
+
+        onChange(newValue);
+      }}
+      hasError={hasError}
+      min="0"
+      max="100"
+      step={getStepSize(displayValue)}
+    />
+  );
+}
+
+function getStepSize(value: number | string): number {
+  const [, digits] = value?.toString()?.split('.') ?? [];
+  if (digits == null) {
+    return 1;
+  }
+
+  return 1 / Math.pow(10, digits.length);
+}
+
+function formatNumber(value: number | undefined, decimalPrecision: number): number | string {
+  if (typeof value !== 'number') {
+    return '';
+  }
+
+  return round(value * 100, decimalPrecision);
+}
+
+function round(value: number | string, decimals: number): number {
+  return parseFloat(Number.parseFloat(`${value}`).toFixed(decimals));
+}
+
+function truncate(value: number, digitsDecimalPrecision: number): number {
+  const trucatedNumber = value.toString().slice(0, value.toString().indexOf('.') + (digitsDecimalPrecision + 1));
+  return Number.parseFloat(trucatedNumber);
+}
---
+++ packages/in-custom-dashboards/widgets/Slo/components/PercentageInput_test.tsx
@@ -0,0 +1,105 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc. 2021
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import PercentageInput from './PercentageInput';
+import { noop } from 'in-services/fixedObjects';
+import Input from 'in-components/form/Input';
+
+describe('in-custom-dashboards/widgets/Slo/components/PercentageInput', () => {
+  it(`displays it's value as a percentage`, () => {
+    // GIVEN
+    const value = 0.1234;
+
+    // WHEN
+    const wrapper = shallow(<PercentageInput value={value} id="abc" onChange={noop} />);
+
+    // THEN
+    expect(wrapper.find(Input).prop('value')).toEqual(12.34);
+  });
+
+  it(`converts percentage input to floating point representaion`, () => {
+    // GIVEN
+    const value = 0.1234;
+    const onChange = jest.fn();
+
+    // WHEN
+    const wrapper = shallow(<PercentageInput value={value} id="abc" onChange={onChange} />);
+    wrapper.find(Input).simulate('change', {
+      target: {
+        valueAsNumber: 22.34
+      }
+    });
+
+    // THEN
+    expect(onChange).toHaveBeenCalledWith(0.2234);
+  });
+
+  it(`truncates input to specified decimal precision`, () => {
+    // GIVEN
+    const value = 0.1234;
+    const decimalPrecision = 2;
+    const onChange = jest.fn();
+
+    // WHEN
+    const wrapper = shallow(
+      <PercentageInput value={value} id="abc" decimalPrecision={decimalPrecision} onChange={onChange} />
+    );
+    wrapper.find(Input).simulate('change', {
+      target: {
+        valueAsNumber: 22.3456
+      }
+    });
+
+    // THEN
+    expect(onChange).toHaveBeenCalledWith(0.2234);
+  });
+
+  it(`should call onChange with undefined if input is NaN`, () => {
+    // GIVEN
+    const value = 0.1234;
+    const onChange = jest.fn();
+
+    // WHEN
+    const wrapper = shallow(<PercentageInput value={value} id="abc" onChange={onChange} />);
+    wrapper.find(Input).simulate('change', {
+      target: {
+        valueAsNumber: Number.NaN // for example if user enters a string
+      }
+    });
+
+    // THEN
+    expect(onChange).toHaveBeenCalledWith(undefined);
+  });
+
+  it(`automatically adjusts step size to the number of digits of the formatted value`, () => {
+    // GIVEN
+    const value1 = 0.12; // 12%
+    const value2 = 0.1234; // 12.34%
+
+    // WHEN
+    const wrapper1 = shallow(<PercentageInput value={value1} id="abc" onChange={noop} />);
+    const wrapper2 = shallow(<PercentageInput value={value2} id="abc" onChange={noop} />);
+
+    // THEN
+    expect(wrapper1.find(Input).prop('step')).toEqual(1);
+    expect(wrapper2.find(Input).prop('step')).toEqual(0.01);
+  });
+
+  it(`correctly propagates error state and id`, () => {
+    // GIVEN
+    const id = 'abc';
+    const hasError = true;
+
+    // WHEN
+    const wrapper = shallow(<PercentageInput value={undefined} id={id} hasError={hasError} onChange={noop} />);
+
+    // THEN
+    expect(wrapper.find(Input).prop('id')).toEqual('abc');
+    expect(wrapper.find(Input).prop('hasError')).toBeTruthy();
+  });
+});
