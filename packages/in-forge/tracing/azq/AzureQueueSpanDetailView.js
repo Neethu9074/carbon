5aba58f47422f3ac9209def6c6c762c52a4d97f1
tamarastanojevic <tamara@LAPTOP-LR5IKMQH.localdomain>
2021-11-08 11:10:52 +0100

    Deleting Sort from detailView for Azure Queue

--- packages/in-forge/tracing/azq/AzureQueueSpanDetailView.js
+++ packages/in-forge/tracing/azq/AzureQueueSpanDetailView.js
@@ -14,10 +14,7 @@ export default function AzureQueueSpanDetailView({ span }) {
       <Dl>
         <Di title={t('in-forge:tracing.azq.queuename')}>{span.getIn(['data', 'azq', 'queuename'])}</Di>
         <Di title={t('in-forge:tracing.azq.messagesuri')}>{span.getIn(['data', 'azq', 'messagesuri'])}</Di>
-        <Di title={t('in-forge:tracing.azq.storageaccountname')}>
-          {span.getIn(['data', 'azq', 'storageaccountname'])}
-        </Di>
-        <Di title={t('in-forge:tracing.azq.sort')}>{span.getIn(['data', 'azq', 'sort'])}</Di>
+        <Di title={t('in-forge:tracing.azq.storageaccountname')}>{span.getIn(['data', 'azq', 'storageaccountname'])}</Di>
         <Di title={t('in-forge:tracing.azq.queueuri')}>{span.getIn(['data', 'azq', 'queueuri'])}</Di>
         <Di title={t('in-forge:tracing.azq.methodname')}>{span.getIn(['data', 'azq', 'methodname'])}</Di>
       </Dl>
