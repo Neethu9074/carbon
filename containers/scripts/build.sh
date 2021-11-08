7d98fe3eb6d743b932feea8adcb3c0c892d263a2
Dahlia Bock <dlbock@gmail.com>
2021-11-08 16:24:06 -0600

    Update instana-twistcli version from 0.1.2 to 0.2.0
    This updates the version of `twistcli` to 21.08.514, which is the same version that's running on https://twistlock.instana.io right now

--- containers/scripts/build.sh
+++ containers/scripts/build.sh
@@ -121,7 +121,7 @@ function _run_docker_build {
 
 function _scan_image() {
   local TAG=$1
-  local INSTANA_TWISTCLI_VERSION='0.1.2'
+  local INSTANA_TWISTCLI_VERSION='0.2.0'
   _log_info "Triggering scan for image ${TAG} with instana-twistcli ${INSTANA_TWISTCLI_VERSION}"
   
   if [[ -f ${COMPONENT_TWISTLOCK_IGNOREFILE} ]]; then
