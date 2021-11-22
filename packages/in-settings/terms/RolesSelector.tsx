6eae4cc132709aba29a4c4cabd070586d6670a1f
Ben Blackmore <bripkens@gmail.com>
2021-11-22 09:13:33 +0100

    Self-defined user role cannot be changed (#8129)
    # Why
    
    We have an error in [SaaS] whenever a user tries to select a role
    for themselves.
    
    # What
    
     - Fix the error (wrong `onChange` API)
     - add a regression test
     - translate the files to TypeScript where possible
     - avoid custom `FormGroup` component implementation
    
    [SaaS]: https://instana.io/s/aYqIALsbSiuIrJ6UW9PDcA

---
+++ packages/in-settings/terms/RolesSelector.tsx
@@ -0,0 +1,67 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import { Field, MapForm } from 'formalistic';
+import React from 'react';
+
+import TouchedMessages from 'in-components/form/TouchedMessages';
+import { roles } from 'in-settings/terms/rolesConfig';
+import FormGroup from 'in-components/form/FormGroup';
+import Select from 'in-components/form/Select';
+import Label from 'in-components/form/Label';
+import Input from 'in-components/form/Input';
+import { t } from 'in-i18n';
+
+import locals from './RolesSelector.mless';
+
+export interface Props {
+  form: MapForm;
+  onChange: (fieldName: string, value: string) => void;
+}
+
+export default function RolesSelector({ form, onChange }: Props) {
+  return (
+    <div className={locals.wrapper}>
+      {(form.get('role') as Field<string>).map(({ value }) => (
+        <>
+          <FormGroup>
+            <Label htmlFor="role-selection">{t('in-settings:terms.role')}</Label>
+            <Select
+              name="role"
+              value={value}
+              onChange={e => onChange('role', e.target.value || '')}
+              id="role-selection"
+            >
+              {roles.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </Select>
+          </FormGroup>
+
+          {form.get('dynamicRole') && (
+            <FormGroup>
+              <Label htmlFor="dynamic-role-selection">{t('in-settings:terms.whatIsYourRole')}</Label>
+              {(form.get('dynamicRole') as Field<string>).map(dynamicRoleField => (
+                <>
+                  <Input
+                    id="dynamic-role-selection"
+                    name="dynamic-role-selection"
+                    type="text"
+                    value={dynamicRoleField.value}
+                    onChange={e => onChange('dynamicRole', e.target.value || '')}
+                    hasError={dynamicRoleField.touched && !dynamicRoleField.valid}
+                  />
+                  <TouchedMessages field={dynamicRoleField} />
+                </>
+              ))}
+            </FormGroup>
+          )}
+        </>
+      ))}
+    </div>
+  );
+}
