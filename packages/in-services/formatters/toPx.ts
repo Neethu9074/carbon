47cc3ed4e5efac276d079e573be1c397846a8bcc
Syam Pillai <syam.sp0965@gmail.com>
2021-11-23 12:59:02 +0100

    Translated in-services/formatters/toPx.ts

--- packages/in-services/formatters/toPx.ts
+++ packages/in-services/formatters/toPx.ts
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc.
  */
 
-export default function toPx(v) {
+export default function toPx(v: number) {
   // handle cases where the DOM style attribute is translating negative to
   // positive pixel values, e.g. left: -10px is translated to left: 10px.
   if (v < 0) return '0px';
