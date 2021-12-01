49e8100fdedb4c3326e491c6a12810f4a431099d
Robert Hostlowsky <robert.hostlowsky@instana.com>
2021-12-01 18:23:11 +0100

    rework ruleFormData.ts

--- packages/in-alerting/smart-alerts/websites/form/ruleFormData.ts
+++ packages/in-alerting/smart-alerts/websites/form/ruleFormData.ts
@@ -117,10 +117,10 @@ export const ruleMetricNameOptions = Object.freeze({
   ]
 });
 
-export function getStatusCodeLabel(value: string) {
+export function getStatusCodeLabel(value: string): string {
   return ruleStatusCodeValueOptions.filter(entry => entry.value === value)[0].label;
 }
 
-export function getRuleOperatorLabel(value: string) {
+export function getRuleOperatorLabel(value: string): string {
   return ruleJsErrorsOperatorOptions.filter(entry => entry.value === value)[0].label;
 }
