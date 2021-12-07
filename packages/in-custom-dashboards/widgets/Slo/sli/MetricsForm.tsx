9ff1278a211cb5105070501d462041b28e71eb5f
thiemok <thiemo.krause@instana.com>
2021-12-07 13:48:55 +0100

    chore(TS migration): migrate in-custom-dashboards/widgets/Slo/sli/MetricsForm

--- packages/in-custom-dashboards/widgets/Slo/sli/MetricsForm.tsx
+++ packages/in-custom-dashboards/widgets/Slo/sli/MetricsForm.tsx
@@ -3,13 +3,20 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Field, Item, MapForm } from 'formalistic';
 import React from 'react';
 
 import { Stack } from '@instana/components';
 
+import {
+  getMetricOptions,
+  MetricEntityType,
+  MetricOption,
+  MetricType
+} from 'in-custom-dashboards/widgets/Slo/sli/metricFormData';
 import { OverridingFieldValidationMessage } from 'in-custom-dashboards/widgets/Slo/components/OverridingFieldValidationMessage';
 import PercentageInput from 'in-custom-dashboards/widgets/Slo/components/PercentageInput';
-import { getMetricOptions } from 'in-custom-dashboards/widgets/Slo/sli/metricFormData';
+import { MonitoringSource } from 'in-custom-dashboards/widgets/Slo/constants';
 import SelectInSection from 'in-components/form/Select/SelectInSection';
 import TouchedMessages from 'in-components/form/TouchedMessages';
 import Sections from 'in-components/workspace/Sections';
@@ -19,18 +26,34 @@ import Header from 'in-components/workspace/Header';
 import Input from 'in-components/form/Input/Input';
 import { t } from 'in-i18n';
 
-export const MetricsForm = ({ entityType, metricEntityType, form, onChange }) => {
-  const metricConfiguration = form.get('metricConfiguration');
+interface MetricsFormProps<S extends MonitoringSource, E extends MetricEntityType<S>> {
+  entityType: S;
+  metricEntityType: E;
+  form: MapForm;
+  onChange: (path: string[], updater: (i: Item) => Item) => void;
+}
+
+export function MetricsForm<S extends MonitoringSource, E extends MetricEntityType<S>>({
+  entityType,
+  metricEntityType,
+  form,
+  onChange
+}: MetricsFormProps<S, E>) {
+  const metricConfiguration = form.get('metricConfiguration') as MapForm;
   if (!metricConfiguration) return null;
 
   const metricOptions = getMetricOptions(entityType, metricEntityType);
 
-  const aggregationValue = metricConfiguration.get('metricAggregation')?.value;
-  const metricName = metricConfiguration.get('metricName')?.value;
+  const metricAggregationField = metricConfiguration.get('metricAggregation') as Field<string>;
+  const metricNameField = metricConfiguration.get('metricName') as Field<MetricType<S, E>>;
+  const thresholdField = metricConfiguration.get('threshold') as Field<number | undefined>;
+
+  const aggregationValue = metricAggregationField?.value;
+  const metricName = metricNameField?.value;
   const metricOption = metricOptions[metricName];
   const percentThreshold = metricName === 'errors';
 
-  const localOnChange = (path, fn) => {
+  const localOnChange = (path: string[], fn: (i: Item) => Item) => {
     onChange(['metricConfiguration', ...path], fn);
   };
 
@@ -42,89 +65,93 @@ export const MetricsForm = ({ entityType, metricEntityType, form, onChange }) =>
         <Header>{t('in-custom-dashboards:widgets.slo.metricsForm.metricThreshold')}</Header>
 
         <Stack gap="xsmall">
-          {metricConfiguration.get('metricName').map(field => (
-            <Sections>
-              <SelectInSection
-                label={t('in-custom-dashboards:widgets.slo.metricsForm.metric')}
-                id="new-sli-metric"
-                value={field.value ?? ''}
-                hasError={!field.valid && field.touched}
-                additionalContent={<TouchedMessages field={field} />}
-                onChange={e =>
-                  onChange([], form => {
-                    const newMetricName = e.target.value;
-                    return form.updateIn(['metricConfiguration', 'metricName'], f =>
-                      f.setValue(newMetricName).setTouched(true)
-                    );
-                  })
-                }
-              >
-                {Object.values(metricOptions).map(({ label, name }) => (
-                  <option value={name} key={name}>
-                    {label}
-                  </option>
-                ))}
-              </SelectInSection>
-            </Sections>
-          ))}
+          <Sections>
+            <SelectInSection
+              label={t('in-custom-dashboards:widgets.slo.metricsForm.metric')}
+              id="new-sli-metric"
+              value={(metricName as string) ?? ''}
+              hasError={!metricNameField.valid && metricNameField.touched}
+              additionalContent={<TouchedMessages field={metricNameField} />}
+              onChange={e => {
+                localOnChange(['metricName'], f => {
+                  const newMetricName = e.target.value as MetricType<S, MetricEntityType<S>>;
+                  return (f as typeof metricNameField).setValue(newMetricName).setTouched(true);
+                });
+              }}
+            >
+              {Object.values<MetricOption<S, E>>(metricOptions).map(({ label, name }) => (
+                <option value={name as string} key={name as string}>
+                  {label}
+                </option>
+              ))}
+            </SelectInSection>
+          </Sections>
 
-          {metricConfiguration.get('metricAggregation').map(field => (
-            <Sections>
-              <SelectInSection
-                label={t('in-custom-dashboards:widgets.slo.metricsForm.aggregation')}
-                id="new-sli-aggregation"
-                value={aggregationValue ?? metricOption.defaultValue}
-                hasError={!field.valid && field.touched}
-                additionalContent={<TouchedMessages field={field} />}
-                onChange={({ target }) =>
-                  localOnChange?.(['metricAggregation'], f => f.setValue(target.value).setTouched(true))
-                }
-              >
-                {metricOption?.options.map(({ label, value }) => (
-                  <option value={value} key={value}>
-                    {label}
-                  </option>
-                ))}
-              </SelectInSection>
-            </Sections>
-          ))}
+          <Sections>
+            <SelectInSection
+              label={t('in-custom-dashboards:widgets.slo.metricsForm.aggregation')}
+              id="new-sli-aggregation"
+              value={aggregationValue ?? metricOption.defaultValue}
+              hasError={!metricAggregationField?.valid && metricAggregationField?.touched}
+              additionalContent={<TouchedMessages field={metricAggregationField} />}
+              onChange={({ target }) =>
+                localOnChange(['metricAggregation'], f =>
+                  (f as typeof metricAggregationField).setValue(target.value).setTouched(true)
+                )
+              }
+            >
+              {metricOption?.options.map(({ label, value }) => (
+                <option value={value} key={value}>
+                  {label}
+                </option>
+              ))}
+            </SelectInSection>
+          </Sections>
 
-          {metricConfiguration.get('threshold').map(field => (
-            <Sections>
-              <Section title={metricOption.unitLabel} titleHtmlFor="new-sli-metric-threshold">
-                {percentThreshold && (
-                  <>
-                    <PercentageInput
-                      id="new-sli-metric-threshold"
-                      value={field.value}
-                      onChange={value => localOnChange(['threshold'], f => f.setValue(value).setTouched(true))}
-                      hasError={!field.valid && field.touched}
-                    />
-                    <TouchedMessages field={field} />
-                  </>
-                )}
+          <Sections>
+            <Section title={metricOption.unitLabel} titleHtmlFor="new-sli-metric-threshold">
+              {percentThreshold && (
+                <>
+                  <PercentageInput
+                    id="new-sli-metric-threshold"
+                    value={thresholdField?.value}
+                    onChange={value =>
+                      localOnChange(['threshold'], f => (f as typeof thresholdField).setValue(value).setTouched(true))
+                    }
+                    hasError={!thresholdField?.valid && thresholdField?.touched}
+                  />
+                  <TouchedMessages field={thresholdField} />
+                </>
+              )}
 
-                {!percentThreshold && (
-                  <>
-                    <Input
-                      id="new-sli-metric-threshold"
-                      type="number"
-                      min="0"
-                      value={field.value}
-                      onChange={e => localOnChange(['threshold'], f => f.setValue(e.target.value).setTouched(true))}
-                      hasError={!field.valid && field.touched}
-                    />
-                    <OverridingFieldValidationMessage
-                      field={field}
-                      message={t('in-custom-dashboards:widgets.slo.metricsForm.valThresholdNotInvalidEmpty')}
-                    />
-                  </>
-                )}
-              </Section>
-            </Sections>
-          ))}
+              {!percentThreshold && (
+                <>
+                  <Input
+                    id="new-sli-metric-threshold"
+                    type="number"
+                    min="0"
+                    value={thresholdField?.value}
+                    onChange={e => {
+                      let newValue: number | undefined = undefined;
+                      if (!Number.isNaN(e.target.value)) {
+                        newValue = e.target.valueAsNumber;
+                      }
+                      localOnChange(['threshold'], f =>
+                        (f as typeof thresholdField).setValue(newValue).setTouched(true)
+                      );
+                    }}
+                    hasError={!thresholdField?.valid && thresholdField?.touched}
+                  />
+                  <OverridingFieldValidationMessage
+                    field={thresholdField}
+                    message={t('in-custom-dashboards:widgets.slo.metricsForm.valThresholdNotInvalidEmpty')}
+                  />
+                </>
+              )}
+            </Section>
+          </Sections>
         </Stack>
       </Stack>
     </>
   );
-};
+}
