1214ef5fb430f91c54180ec8744addaba528b926
Lars Schmidt <Lars.Schmidt1@ibm.com>
2021-12-20 13:55:02 +0100

    update link to privacy policy (#8335)
    # Why
    the location for privacy policy changed
    
    # What
    update accordingly

--- packages/in-settings/terms/dialog/DocumentLinkButtons.js
+++ packages/in-settings/terms/dialog/DocumentLinkButtons.js
@@ -56,7 +56,7 @@ export function PrivacyButton({ withIcon, label, fontSize }) {
       kind="action"
       icon={withIcon ? 'lib_views_external_link' : ''}
       target="_blank"
-      href="https://instana.com/docs/instana-privacy-policy-latest.pdf"
+      href="https://www.ibm.com/docs/en/SSE1JP5_current/static/instana-privacy-policy-latest.pdf"
       noAutoMargin
     >
       {label ? label : t('in-settings:termsDialog.privacyProductPolicy')}
