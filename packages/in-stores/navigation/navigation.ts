73320beb582315f7969681be426ef052fca6e77e
Robert Hostlowsky <robert.hostlowsky@instana.com>
2021-12-22 17:37:56 +0100

    when somebody navigates from events page to analytics-profiling reset DFQ

--- packages/in-stores/navigation/navigation.ts
+++ packages/in-stores/navigation/navigation.ts
@@ -5,6 +5,8 @@
 
 import PropTypes from 'prop-types';
 
+// @ts-expect-error file will need to be converted to typescript
+import { profilingPath } from 'in-components/Profiling/navigation/paths';
 import { isEventsPath, isInfrastructurePath } from 'in-stores/navigation/paths/mainPaths';
 import { applyResets } from 'in-stores/navigation/urlParameterResets';
 import { stringify } from 'in-stores/navigation/routing/stringifier';
@@ -63,7 +65,7 @@ export function goToPath(path: string) {
 
 export function getView(path: string) {
   return getModifiedUrlStream((location: Location) => {
-    // checks the the current and next path if navigating into another area.
+    // checks the current and next path if navigating into another area.
     removeDFQueryFromLocationWhenChangingArea(location, path);
     location.pathname = path;
   });
@@ -75,13 +77,16 @@ export function getView(path: string) {
  * Returns the modified location instance.
  */
 export function removeDFQueryFromLocationWhenChangingArea(location: Location, path: string): Location {
+  const isProfilingPath = (path: string) => path.indexOf(profilingPath) === 0;
   const { pathname: currentPath } = location;
   if (
     location.query.q &&
     // delete the DF query when
     // * navigation from an infrastructure view (map, table) to another, non-infrastructure view, or the other way around or
-    // * navigating from an events page to any other page, or the other way around
+    // * navigating from an analytics-profiling-page to any other page, or the other way around
+    // * navigating from an events-page to any other page, or the other way around
     (isInfrastructurePath(path) !== isInfrastructurePath(currentPath) ||
+      isProfilingPath(path) !== isProfilingPath(currentPath) ||
       isEventsPath(path) !== isEventsPath(currentPath))
   ) {
     delete location.query.q;
