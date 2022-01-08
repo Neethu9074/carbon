60b15a11ddf418baca05a113a884becc18ebd1ac
Hunter Madison <hunter.madison@instana.com>
2022-01-08 15:41:10 -0500

    CrowdStrike Falcon Sensor 1.0: Add UI for New Sensor

---
+++ packages/in-forge/plugins/crowdStrikeFalcon/Dashboard/Content.js
@@ -0,0 +1,17 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import DashboardNotification from 'in-sdk/components/dashboard/DashboardNotification';
+import { t } from 'in-i18n';
+
+export default function CrowdStrikeFalconDashboard() {
+  return (
+    <DashboardNotification type="info">
+      {t('in-forge:plugins.crowdStrikeFalcon.dashboard.thereIsNoFurtherInformationAboutThisEntity')}
+    </DashboardNotification>
+  );
+}
---
+++ packages/in-forge/plugins/crowdStrikeFalcon/Info.js
@@ -0,0 +1,29 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
+import { t } from 'in-i18n';
+
+export default function Info({ snapshot }) {
+  const data = snapshot.get('data');
+
+  return (
+    <div>
+      <DescriptionList>
+        <DescriptionItem title={t('in-forge:plugins.crowdStrikeFalcon.rfm-state')}>
+          {data.get('rfm-state')}
+        </DescriptionItem>
+        <DescriptionItem title={t('in-forge:plugins.crowdStrikeFalcon.rfm-reason')}>
+          {data.get('rfm-reason')}
+        </DescriptionItem>
+        <DescriptionItem title={t('in-forge:plugins.crowdStrikeFalcon.version')}>{data.get('version')}</DescriptionItem>
+        <DescriptionItem title={t('in-forge:plugins.crowdStrikeFalcon.aid')}>{data.get('aid')}</DescriptionItem>
+        <DescriptionItem title={t('in-forge:plugins.crowdStrikeFalcon.cid')}>{data.get('cid')}</DescriptionItem>
+      </DescriptionList>
+    </div>
+  );
+}
