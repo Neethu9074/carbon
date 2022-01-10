1d49cd122a3ddbd742fb850b1f05d2f594be84ca
Vipin Menon <vipinmv1@in.ibm.com>
2022-01-10 19:20:18 +0530

    Db2 LUW Sensor 1.0.21: Metric additions with tablespace migration for IBM Db2LUW

--- packages/in-forge/plugins/db2Database/Dashboard/Content.js
+++ packages/in-forge/plugins/db2Database/Dashboard/Content.js
@@ -75,6 +75,9 @@ export default function Db2Dashboard({ snapshot, timeConfig }) {
           <MetricValue snapshotId={snapshotId} metric="databases.connectionsCount" formatter={number.compact} />
         </KpiKeyValue>
         {data.get('versionCheck') && <HadrTakeOverInfo snapshotId={snapshotId} timeConfig={timeConfig} />}
+        <KpiKeyValue label={t('in-forge:plugins.db2Database.daysLastBackup')}>
+          <MetricValue snapshotId={snapshotId} metric="databases.daysLastBackup" formatter={number.compact} />
+        </KpiKeyValue>
       </KpiSection>
       <DashboardSection title={t('in-forge:plugins.db2Database.dashboard.connections')}>
         <Chart
