a18966aadf4a213327d269433e8d515e4a7b81a3
Thiemo-Krause <thiemo.krause@ibm.com>
2022-01-13 16:42:37 +0100

    chore(TS migration): migrate in-custom-dashboards/widgets/Slo/sli/ServicesSelectBox

--- packages/in-custom-dashboards/widgets/Slo/sli/ServicesSelectBox.tsx
+++ packages/in-custom-dashboards/widgets/Slo/sli/ServicesSelectBox.tsx
@@ -3,10 +3,13 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Field } from 'formalistic';
 import React from 'react';
 
+import { Observable } from '@instana/observables';
 import { useObservable } from '@instana/hooks';
 
+import { ApplicationBoundaryScope, PaginatedResult, Result, ServiceItem, TimeConfig } from 'in-types';
 import SelectInSection from 'in-components/form/Select/SelectInSection';
 import getServices from 'in-applications/subscriptions/getServices';
 import { hasError, isLoading } from 'in-services/util/result';
@@ -14,10 +17,25 @@ import { pendingResult } from 'in-services/fixedObjects';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 import { t } from 'in-i18n';
 
-export default function ServicesSelectBox({ field, applicationId, boundaryScope, value, onChange }) {
+interface ServicesSelectBoxProps {
+  field: Field<never>;
+  applicationId: string;
+  boundaryScope: ApplicationBoundaryScope;
+  value?: string;
+  onChange: (application?: string) => void;
+}
+
+export default function ServicesSelectBox({
+  field,
+  applicationId,
+  boundaryScope,
+  value,
+  onChange
+}: ServicesSelectBoxProps) {
   const timeConfig = useTimeConfig();
 
-  const result = useObservable(getServicesObservable, [applicationId, boundaryScope, timeConfig]) ?? pendingResult;
+  const result: Result<PaginatedResult<ServiceItem>> =
+    useObservable(getServicesObservable, [applicationId, boundaryScope, timeConfig]) ?? pendingResult;
 
   return (
     <SelectInSection
@@ -42,7 +60,11 @@ export default function ServicesSelectBox({ field, applicationId, boundaryScope,
   );
 }
 
-function getServicesObservable([applicationId, boundaryScope, timeConfig]) {
+type GetServicesObservableProps = [string, ApplicationBoundaryScope, TimeConfig];
+
+function getServicesObservable([applicationId, boundaryScope, timeConfig]: GetServicesObservableProps): Observable<
+  Result<PaginatedResult<ServiceItem>>
+> {
   return getServices({
     pagination: {
       page: 1,
@@ -61,7 +83,10 @@ function getServicesObservable([applicationId, boundaryScope, timeConfig]) {
     filter: {
       application: applicationId,
       applicationBoundaryScope: boundaryScope,
-      timeConfig
+      timeConfig,
+      includeInternalCalls: false,
+      includeSyntheticCalls: false,
+      useLongTermDataOnly: false
     },
     contextScope: 'NONE'
   });
