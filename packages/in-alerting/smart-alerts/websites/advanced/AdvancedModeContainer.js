39978fec3de580549b7183f997a7399a86c155af
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2022-01-20 13:44:50 +0100

    Sc pivot reorder sections (#8490)
    * prototypical shuffling of the order of sections in the advanced smart alert dialog
    
    * websites SA: move blueprint selector up
    
    * move additional forms for status code and log selection up
    
    * move blueprint form extension for status-code and log-selection up
    
    * extract threshold from scope section
    
    * i18n
    
    * move threshold UI+chart in its own section
    
    * fix step validation and the indicating of a missing static threshold on the wrong step
    
    * simplifying Advanced Container by extracting parts into own components
    
    Co-authored-by: Robert Hostlowsky <robert.hostlowsky@instana.com>

--- packages/in-alerting/smart-alerts/websites/advanced/AdvancedModeContainer.js
+++ packages/in-alerting/smart-alerts/websites/advanced/AdvancedModeContainer.js
@@ -8,8 +8,7 @@ import React from 'react';
 import {
   websitesAlertingAdditionalPropsAlertLevelChanged,
   websitesAlertingAdditionalPropsDescriptionChanged,
-  websitesAlertingAdditionalPropsTriggerChanged,
-  websitesAlertingBlueprintChanged
+  websitesAlertingAdditionalPropsTriggerChanged
 } from 'in-alerting/smart-alerts/websites/tracker';
 import {
   AlertPreview,
@@ -17,31 +16,18 @@ import {
 } from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/AlertProperties/AlertPreview';
 import AlertPropertiesContainer from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/AlertProperties/AlertPropertiesContainer';
 import { default as GlobalAdvancedModeContainer } from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/AdvancedModeContainer';
-import HistoricBaselineErrorMessage from 'in-alerting/smart-alerts/components/smart-alert-dialog/HistoricBaselineErrorMessage';
 import AlertProperties from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/AlertProperties/AlertProperties';
-import { getFormValueOrDefault } from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/thresholdFormHelper';
 import AlertTagFilterExpressionConfig from 'in-alerting/smart-alerts/websites/components/AlertTagFilterExpressionConfig';
 import WebsiteAlertPropertiesTitleRow from 'in-alerting/smart-alerts/websites/advanced/WebsiteAlertPropertiesTitleRow';
-import BlueprintSelection from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/BlueprintSelection';
 import { getDescriptionPlaceholder, getTitlePlaceholder } from 'in-alerting/smart-alerts/websites/form/formUtils';
 import ConfigureAlertChannel from 'in-alerting/smart-alerts/components/smart-alert-dialog/ConfigureAlertChannel';
-import StatusCodeInteractiveChart from 'in-alerting/smart-alerts/websites/advanced/StatusCodeInteractiveChart';
-import ThroughputInteractiveChart from 'in-alerting/smart-alerts/websites/advanced/ThroughputInteractiveChart';
-import { blueprintConfigs, getBlueprintConfig } from 'in-alerting/smart-alerts/websites/data/blueprintConfig';
-import SlownessInteractiveChart from 'in-alerting/smart-alerts/websites/advanced/SlownessInteractiveChart';
-import JsErrorsInteractiveChart from 'in-alerting/smart-alerts/websites/advanced/JsErrorsInteractiveChart';
+import BluePrintSelectionSection from 'in-alerting/smart-alerts/websites/advanced/BluePrintSelectionSection';
 import { validateCheckForCustomPayload } from 'in-alerting/components/CustomPayload/customPayloadFormUtil';
 import AlertConfigCustomPayload from 'in-alerting/components/CustomPayload/AlertConfigCustomPayload';
 import TimeThresholdConfig from 'in-alerting/smart-alerts/websites/advanced/TimeThresholdConfig';
-import ProvideStatusCode from 'in-alerting/smart-alerts/websites/components/ProvideStatusCode';
+import { ThresholdSection } from 'in-alerting/smart-alerts/websites/advanced/ThresholdSection';
 import { fieldNames } from 'in-alerting/smart-alerts/websites/form/alertDialogFormDefinition';
-import createBlueprintForm from 'in-alerting/smart-alerts/websites/form/blueprintFormCreator';
-import { isHistoricBaseline } from 'in-alerting/smart-alerts/components/utils/baselineUtils';
-import AlertTypeSwitch from 'in-alerting/smart-alerts/websites/components/AlertTypeSwitch';
-import ProvideJsError from 'in-alerting/smart-alerts/websites/components/ProvideJsError';
-import { alertingDialogItemPickerTimeframe } from 'in-alerting/components/constants';
-import { modeAdvanced } from 'in-alerting/smart-alerts/websites/constants';
-import LightCard from 'in-alerting/components/LightCard/LightCard';
+import { getBlueprintConfig } from 'in-alerting/smart-alerts/websites/data/blueprintConfig';
 import { t } from 'in-i18n';
 
 export default function AdvancedModeContainer(props) {
@@ -67,8 +53,22 @@ export default function AdvancedModeContainer(props) {
       navItems={[
         {
           scrollId: '1',
+          label: t('in-alerting:smartAlerts.websites.advanced.triggerLabel'),
+          title: t('in-alerting:smartAlerts.websites.advanced.triggerTitle'),
+          content: (
+            <BluePrintSelectionSection
+              alertType={alertType}
+              form={form}
+              updateForm={updateForm}
+              setSliderState={setSliderState}
+            />
+          )
+        },
+        {
+          scrollId: '2',
           label: t('in-alerting:smartAlerts.websites.advanced.scopeLabel'),
           title: t('in-alerting:smartAlerts.websites.advanced.scopeTitle'),
+          valid: true,
           content: (
             <AlertTagFilterExpressionConfig
               form={form}
@@ -76,108 +76,31 @@ export default function AdvancedModeContainer(props) {
               websiteLabel={websiteLabel}
               QueryBuilderComponent={QueryBuilderComponent}
             />
-          ),
-          checked: true,
-          valid: true
-        },
-        {
-          scrollId: '2',
-          label: t('in-alerting:smartAlerts.websites.advanced.triggerLabel'),
-          title: t('in-alerting:smartAlerts.websites.advanced.triggerTitle'),
-          checked: validateTrigger(form),
-          valid: formFieldsValid(form, ['rule', 'threshold']),
-          content: (
-            <>
-              <BlueprintSelection
-                form={form}
-                updateForm={updateForm}
-                blueprintConfigs={blueprintConfigs}
-                createBlueprintForm={createBlueprintForm}
-                trackBlueprintChange={newBlueprint =>
-                  websitesAlertingBlueprintChanged({ newBlueprint, mode: 'advanced' })
-                }
-              />
-              <AlertTypeSwitch
-                alertType={alertType}
-                renderJsErrors={() => (
-                  <>
-                    <LightCard
-                      title={t('in-alerting:smartAlerts.websites.advanced.JSErrorMessage')}
-                      withoutPadding
-                      darkFrame
-                    >
-                      <ProvideJsError
-                        form={form}
-                        timeConfig={{
-                          windowSize: alertingDialogItemPickerTimeframe
-                        }}
-                        updateForm={updateForm}
-                        onSelectJsError={setSliderState}
-                        mode={modeAdvanced}
-                      />
-                    </LightCard>
-                    <JsErrorsInteractiveChart
-                      blueprintConfig={blueprintConfig}
-                      form={form}
-                      timeConfig={timeConfig}
-                      updateForm={updateForm}
-                      onChartViewConfigChange={onChartViewConfigChange}
-                      selectedChartViewConfigIndex={selectedChartViewConfigIndex}
-                      editMode={editMode}
-                    />
-                  </>
-                )}
-                renderSlowness={() => (
-                  <SlownessInteractiveChart
-                    blueprintConfig={blueprintConfig}
-                    form={form}
-                    timeConfig={timeConfig}
-                    updateForm={updateForm}
-                    onChartViewConfigChange={onChartViewConfigChange}
-                    selectedChartViewConfigIndex={selectedChartViewConfigIndex}
-                    editMode={editMode}
-                  />
-                )}
-                renderStatusCode={() => (
-                  <>
-                    <LightCard
-                      title={t('in-alerting:smartAlerts.websites.advanced.HTTPStatusCode')}
-                      withoutPadding
-                      darkFrame
-                    >
-                      <ProvideStatusCode form={form} updateForm={updateForm} mode={modeAdvanced} />
-                    </LightCard>
-                    <StatusCodeInteractiveChart
-                      blueprintConfig={blueprintConfig}
-                      form={form}
-                      updateForm={updateForm}
-                      timeConfig={timeConfig}
-                      onChartViewConfigChange={onChartViewConfigChange}
-                      selectedChartViewConfigIndex={selectedChartViewConfigIndex}
-                      editMode={editMode}
-                    />
-                  </>
-                )}
-                renderThroughput={() => (
-                  <ThroughputInteractiveChart
-                    blueprintConfig={blueprintConfig}
-                    form={form}
-                    timeConfig={timeConfig}
-                    updateForm={updateForm}
-                    onChartViewConfigChange={onChartViewConfigChange}
-                    selectedChartViewConfigIndex={selectedChartViewConfigIndex}
-                    editMode={editMode}
-                  />
-                )}
-              />
-              {isHistoricBaseline(form.get('threshold').get('type').value) && (
-                <HistoricBaselineErrorMessage thresholdResult={thresholdResult} />
-              )}
-            </>
           )
         },
         {
           scrollId: '3',
+          label: t('in-alerting:smartAlerts.websites.advanced.thresholdLabel'),
+          title: t('in-alerting:smartAlerts.websites.advanced.thresholdTitle'),
+          valid: formFieldsValid(form, ['rule', 'threshold']),
+          checked: formFieldsTouched(form, ['rule', 'threshold']),
+          content: (
+            <ThresholdSection
+              alertType={alertType}
+              blueprintConfig={blueprintConfig}
+              editMode={editMode}
+              form={form}
+              onChartViewConfigChange={onChartViewConfigChange}
+              selectedChartViewConfigIndex={selectedChartViewConfigIndex}
+              thresholdResult={thresholdResult}
+              timeConfig={timeConfig}
+              updateForm={updateForm}
+              websiteLabel={websiteLabel}
+            />
+          )
+        },
+        {
+          scrollId: '4',
           label: t('in-alerting:smartAlerts.websites.advanced.timeThresholdLabel'),
           title: t('in-alerting:smartAlerts.websites.advanced.timeThresholdTitle'),
           checked: true,
@@ -193,7 +116,7 @@ export default function AdvancedModeContainer(props) {
           )
         },
         {
-          scrollId: '4',
+          scrollId: '5',
           label: t('in-alerting:smartAlerts.websites.advanced.alertChannelsLabel'),
           title: t('in-alerting:smartAlerts.websites.advanced.alertChannelsTitle'),
           checked: form.get(fieldNames.alertChannelIds).value.length > 0,
@@ -209,7 +132,7 @@ export default function AdvancedModeContainer(props) {
           )
         },
         {
-          scrollId: '5',
+          scrollId: '6',
           label: t('in-alerting:smartAlerts.websites.advanced.propertiesLabel'),
           title: t('in-alerting:smartAlerts.websites.advanced.propertiesTitle'),
           checked: !!(form.get(fieldNames.name).value || form.get(fieldNames.description).value),
@@ -245,7 +168,7 @@ export default function AdvancedModeContainer(props) {
           )
         },
         {
-          scrollId: '6',
+          scrollId: '7',
           label: t('in-alerting:smartAlerts.websites.advanced.payloadsLabel'),
           title: t('in-alerting:smartAlerts.websites.advanced.payloadsTitle'),
           checked: validateCheckForCustomPayload(form),
@@ -257,20 +180,16 @@ export default function AdvancedModeContainer(props) {
   );
 }
 
-function validateTrigger(form) {
-  const ruleForm = form.get('rule');
-  const alertType = ruleForm.get('alertType').value;
-
-  if (alertType === 'specificJsError') {
-    return Boolean(ruleForm.get('alertType').value && getFormValueOrDefault(ruleForm, 'value'));
-  } else {
-    return true;
-  }
+function formFieldsValid(form, fieldsToCheck) {
+  const invalid = Object.entries(form?.items ?? {})
+    .filter(([fieldName]) => fieldsToCheck?.includes(fieldName))
+    .some(([, { hierarchyValid }]) => !hierarchyValid);
+  return !invalid;
 }
 
-function formFieldsValid(form, fieldsToCheck) {
-  const fieldInvalid = Object.entries(form?.items ?? {})
-    .filter(([field]) => fieldsToCheck?.includes(field))
-    .some(([, { hierarchyValid }]) => !hierarchyValid);
-  return !fieldInvalid;
+function formFieldsTouched(form, fieldsToCheck) {
+  const touched = Object.entries(form?.items ?? {})
+    .filter(([fieldName]) => fieldsToCheck?.includes(fieldName))
+    .some(([, { hierarchyTouched }]) => hierarchyTouched);
+  return touched;
 }
---
+++ packages/in-alerting/smart-alerts/websites/advanced/BluePrintSelectionSection.js
@@ -0,0 +1,55 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ * (c) Copyright Instana Inc. 2022
+ */
+
+import React from 'react';
+
+import BlueprintSelection from 'in-alerting/smart-alerts/components/smart-alert-dialog/advanced/BlueprintSelection';
+import ProvideStatusCode from 'in-alerting/smart-alerts/websites/components/ProvideStatusCode';
+import createBlueprintForm from 'in-alerting/smart-alerts/websites/form/blueprintFormCreator';
+import { websitesAlertingBlueprintChanged } from 'in-alerting/smart-alerts/websites/tracker';
+import AlertTypeSwitch from 'in-alerting/smart-alerts/websites/components/AlertTypeSwitch';
+import { blueprintConfigs } from 'in-alerting/smart-alerts/websites/data/blueprintConfig';
+import ProvideJsError from 'in-alerting/smart-alerts/websites/components/ProvideJsError';
+import { alertingDialogItemPickerTimeframe } from 'in-alerting/components/constants';
+import { modeAdvanced } from 'in-alerting/smart-alerts/websites/constants';
+import LightCard from 'in-alerting/components/LightCard/LightCard';
+import { t } from 'in-i18n';
+
+export default function BluePrintSelectionSection(props) {
+  const { alertType, form, setSliderState, updateForm } = props;
+
+  return (
+    <>
+      <BlueprintSelection
+        form={form}
+        updateForm={updateForm}
+        blueprintConfigs={blueprintConfigs}
+        createBlueprintForm={createBlueprintForm}
+        trackBlueprintChange={newBlueprint => websitesAlertingBlueprintChanged({ newBlueprint, mode: 'advanced' })}
+      />
+      <AlertTypeSwitch
+        alertType={alertType}
+        renderJsErrors={() => (
+          <LightCard title={t('in-alerting:smartAlerts.websites.advanced.JSErrorMessage')} withoutPadding darkFrame>
+            <ProvideJsError
+              form={form}
+              timeConfig={{
+                windowSize: alertingDialogItemPickerTimeframe
+              }}
+              updateForm={updateForm}
+              onSelectJsError={setSliderState}
+              mode={modeAdvanced}
+            />
+          </LightCard>
+        )}
+        renderStatusCode={() => (
+          <LightCard title={t('in-alerting:smartAlerts.websites.advanced.HTTPStatusCode')} withoutPadding darkFrame>
+            <ProvideStatusCode form={form} updateForm={updateForm} mode={modeAdvanced} />
+          </LightCard>
+        )}
+      />
+    </>
+  );
+}
