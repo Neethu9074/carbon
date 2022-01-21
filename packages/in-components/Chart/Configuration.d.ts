1a81682da93a071bcedbb8850863d718be6eef54
Thiemo Krause <Thiemo.Krause@ibm.com>
2022-01-21 09:32:16 +0100

    Refactor 78979 refactor slo sliforms (#8489)
    * chore(TS migration): rename in-components/form/Input/InputInSection
    
    * chore(TS migration): migrate in-components/form/Input/InputInSection
    
    * chore(TS migration): rename in-custom-dashboards/widgets/Slo/sli/WebsiteSliForm
    
    * chore(TS migration): migrate in-custom-dashboards/widgets/Slo/sli/WebsiteSliForm
    
    * chore(TS migration): rename in-alerting/smart-alerts/applications/advanced/InboundOutboundCallsSwitch/config
    
    * chore(TS migration): migrate in-alerting/smart-alerts/applications/advanced/InboundOutboundCallsSwitch/config
    
    * chore(TS migration): rename in-applications/components/OptionBox
    
    * chore(TS migration): migrate in-applications/components/OptionBox
    
    * chore(TS migration): rename in-alerting/smart-alerts/applications/advanced/InboundOutboundCallsSwitch/InboundOrAllCallsOption
    
    * chore(TS migration): migrate in-alerting/smart-alerts/applications/advanced/InboundOutboundCallsSwitch/InboundOrAllCallsOption
    
    * chore(TS migration): rename in-applications/metrics
    
    * chore(TS migration): migrate in-applications/metrics
    
    * chore(TS migration): rename in-applications/subscriptions/getServices
    
    * chore(TS migration): migrate in-applications/subscriptions/getServices
    
    * chore(TS migration): rename in-custom-dashboards/widgets/Slo/sli/ServicesSelectBox
    
    * chore(TS migration): migrate in-custom-dashboards/widgets/Slo/sli/ServicesSelectBox
    
    * refactor(SliDialog): refactor ServicesSelectBox
    
    * Create hook for useServices
    * Refactor out form dependencies
    
    * chore(TS migration): rename in-custom-dashboards/widgets/Slo/sli/ApplicationSliForm
    
    * chore(TS migration): migrate in-custom-dashboards/widgets/Slo/sli/ApplicationSliForm
    
    * Update packages/in-components/form/Input/InputInSection.tsx
    
    Co-authored-by: Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
    
    * chore: address PR comments
    
    Co-authored-by: Robert Hostlowsky <Robert.Hostlowsky@ibm.com>

---
+++ packages/in-components/Chart/Configuration.d.ts
@@ -0,0 +1,8 @@
+/*
+ * (c) Copyright IBM Corp. 2022
+ * (c) Copyright Instana Inc. 2022
+ */
+
+export declare const WIGGLE_ROOM = 5000;
+
+export declare const ANIMATION_DURATION = 1000;
--- packages/in-components/Chart/renderer/types.ts
+++ packages/in-components/Chart/renderer/types.ts
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-import { Config } from 'in-components/Chart/ResultAwareChart.d';
+import { Config } from 'in-components/Chart/types';
 import { ScaleType } from 'in-services/scale';
 
 // TODO: this is very likely incomplete
