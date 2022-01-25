5521289067681e21d267e389ad160aff8fc63a13
Robert Hostlowsky <robert.hostlowsky@instana.com>
2022-01-25 18:02:23 +0100

    chore(TS migration): convert and split in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControl, and
    * when switching to Adaptive, also reset the evaluation to AP
    * i18n
    * add storybook for in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControl

--- packages/in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControl.tsx
+++ packages/in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControl.tsx
@@ -3,71 +3,45 @@
  * (c) Copyright Instana Inc. 2021
  */
 
+import { Field, MapForm } from 'formalistic';
 import React from 'react';
 
-import ReadOnlyAlertEvaluation from 'in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/ReadOnlyAlertEvaluation';
-import alertEvaluationTypes from 'in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/alertEvaluationTypes';
+// @ts-expect-error source needs to be converted to TS
 import createThresholdForm from 'in-alerting/smart-alerts/applications/form/thresholdForm';
-import { STATIC_THRESHOLD } from 'in-alerting/smart-alerts/data/thresholdTypes';
-import CheckboxFancy from 'in-components/form/CheckboxFancy';
-import IconLabel from 'in-alerting/components/IconLabel';
-import theme from 'in-themes';
-import { t } from 'in-i18n';
+import { AlertEvaluationControlPresenter } from 'in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControlPresenter';
+import { ADAPTIVE_BASELINE, STATIC_THRESHOLD } from 'in-alerting/smart-alerts/data/thresholdTypes';
+import { AlertEvaluationType, ThresholdType } from 'in-types';
 
-import locals from 'in-alerting/smart-alerts/applications/advanced/EvaluationSwitch/AlertEvaluationControl.mless';
+interface Props {
+  form: MapForm;
+  updateForm: (form: MapForm) => void;
+  isGlobalSmartAlert?: boolean;
+}
 
-export default function AlertEvaluationControl({ form, updateForm, isGlobalSmartAlert }) {
-  const evaluationType = form.get('evaluationType').value;
-  const alertType = form.get('rule').get('alertType').value;
-  const isBuiltIn = form.get('builtIn').value;
+export default function AlertEvaluationControl({ form, updateForm, isGlobalSmartAlert }: Props) {
+  const evaluationType = (form.get('evaluationType') as Field<AlertEvaluationType>).value;
+  const alertType = ((form.get('rule') as MapForm)!.get('alertType') as Field<string>)!.value;
+  const isBuiltIn = (form.get('builtIn') as Field<boolean>).value;
+  const thresholdType = ((form.get('threshold') as MapForm).get('type') as Field<ThresholdType>)?.value;
+  const isAdaptiveThreshold = thresholdType === ADAPTIVE_BASELINE;
 
-  if (isBuiltIn) {
-    return (
-      <div className={locals.readOnlyAlertEvaluationContainer}>
-        <ReadOnlyAlertEvaluation evaluationType={evaluationType} isGlobalSmartAlert={isGlobalSmartAlert} />
-      </div>
+  const setEvaluationType = (type: AlertEvaluationType) =>
+    updateForm(
+      form
+        .updateIn(['evaluationType'], f => (f as Field<AlertEvaluationType>).setValue(type).setTouched(true))
+        .put(
+          'threshold',
+          createThresholdForm({ ...(form.get('threshold') as Field<object>).toJS(), type: STATIC_THRESHOLD }, alertType)
+        )
     );
-  }
 
   return (
-    <div className={locals.container}>
-      <IconLabel
-        type="lib_alerts_multiple_alerts"
-        text={t('in-alerting:smartAlerts.applications.advanced.evaluationSwitch.individual')}
-        noBottomMargin
-        color={theme.lib.colors.N600Light}
-      />
-      <div className={locals.options}>
-        {Object.keys(alertEvaluationTypes).map(type => (
-          <CheckboxFancy
-            key={type}
-            label={
-              isGlobalSmartAlert
-                ? alertEvaluationTypes[type].globalSelectionText
-                : alertEvaluationTypes[type].selectionText
-            }
-            checked={type === evaluationType}
-            onChange={() =>
-              updateForm(
-                form
-                  .updateIn(['evaluationType'], f => f.setValue(type).setTouched(true))
-                  // As of now, we only support static-threshold when Per-Entity grouping is used.
-                  .put(
-                    'threshold',
-                    createThresholdForm(
-                      {
-                        ...form.get('threshold').toJS(),
-                        type: STATIC_THRESHOLD
-                      },
-                      alertType
-                    )
-                  )
-              )
-            }
-            asRadioButton
-          />
-        ))}
-      </div>
-    </div>
+    <AlertEvaluationControlPresenter
+      evaluationType={evaluationType}
+      isAdaptiveThreshold={isAdaptiveThreshold}
+      isBuiltIn={isBuiltIn}
+      isGlobalSmartAlert={isGlobalSmartAlert}
+      setEvaluationType={setEvaluationType}
+    />
   );
 }
