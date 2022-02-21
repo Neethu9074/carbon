642d620af733b3a723097d72ec0427601acb02aa
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2022-02-21 12:01:20 +0100

    Custom Dashboard Truncation bug (#8706)
    * bug(Fixed truncation error on card)
    
    * bug(changed alignment)
    
    * Refactored the KpiCard to properly break text and show tooltips while reserving space to show card handles
    
    Co-authored-by: Oliver Munkebo <oliver.munkebo@instana.com>
    Co-authored-by: Simon Hofmann <simon.hofmann@instana.com>

--- packages/in-components/KpiCard/KpiCard.tsx
+++ packages/in-components/KpiCard/KpiCard.tsx
@@ -65,6 +65,7 @@ export default function KpiCard({
   resultPrecision
 }: KpiCardProps) {
   const { ref, width } = useResizeObserver<HTMLDivElement>();
+  const hasApproximateData = resultPrecision === 'PRECISION_APPROXIMATE';
 
   let content;
   if (raw || renderValue) {
@@ -114,14 +115,12 @@ export default function KpiCard({
         })}
         ref={ref}
       >
-        <div className={locals.innerTitle}>
-          <Tooltip content={title} align="bottomMiddle">
-            <span className={locals.truncatedTitle}>{title}</span>
-          </Tooltip>
-          {resultPrecision === 'PRECISION_APPROXIMATE' && (
-            <MultiLineToolTipIcon withMargin lines={[t('in-components:approximateDataIndicator.dataRetention')]} />
-          )}
-        </div>
+        <Tooltip content={title} align="bottomLeft">
+          <span className={locals.titleText}>{title}</span>
+        </Tooltip>
+        {hasApproximateData && (
+          <MultiLineToolTipIcon withMargin lines={[t('in-components:approximateDataIndicator.dataRetention')]} />
+        )}
         {iconAction && (
           <div
             className={classNames({
@@ -145,10 +144,10 @@ export default function KpiCard({
             </Button>
           </div>
         )}
+        {actions && <div className={locals.actions}>{actions}</div>}
       </div>
       {content}
       {companionValue && <span className={locals.companion}>{companionValue}</span>}
-      {actions && <div className={locals.actions}>{actions}</div>}
     </div>
   );
 }
