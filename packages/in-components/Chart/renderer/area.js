2d1789a92a95871823574d7d8847269b9212c181
Stepan Herold <Stepan.Herold@ibm.com>
2022-03-16 11:46:01 +0100

    Render lone data points on area, stackArea and integral charts (#8876)
    # Why
    
    Area, stackArea and integral chart renderers do not display lone data points which looks like that there are data missing in the chart. 
    
    # What
    
    The area, stackArea and integral chart renderers will render lone standing data points as circles that will be connected with the time axis with a line in the same color which is used for filling the chart area.
    
    This change is consistent with how the line chart renderer displays lone standing data points as circles.
    
    # References
    
    - https://instana.kanbanize.com/ctrl_board/66/cards/87035/details/

--- packages/in-components/Chart/renderer/area.js
+++ packages/in-components/Chart/renderer/area.js
@@ -3,6 +3,8 @@
  * (c) Copyright Instana Inc.
  */
 
+import { drawCircleWithLine } from './utils';
+
 export default {
   render: ({ dataSeries, colors, colors100, index, scale, config }) => {
     const blocks = config.calculateBlocks(dataSeries);
@@ -17,29 +19,49 @@ export default {
       const firstDataPointXPos = config.xScaleBackBuffer.getRange(firstDataPoint[0]);
       const lastDataPointXPos = config.xScaleBackBuffer.getRange(lastDataPoint[0]);
 
-      config.backBufferCtx.beginPath();
-      config.backBufferCtx.moveTo(firstDataPointXPos, scale.getRange(firstDataPoint[1]));
+      const strokeStyle = colors100[index];
+      const fillStyle = colors[index];
 
-      for (let i = 1; i < block.length; i++) {
-        const dataPoint = block[i];
-        if (!dataPoint) {
-          continue;
+      if (block.length === 1) {
+        // a block with a lone data point
+        const { xPos, yPos } = getPosition(config, firstDataPoint, scale);
+        drawCircleWithLine({
+          renderingContext: config.backBufferCtx,
+          config,
+          xPos,
+          yPos,
+          circleStyle: strokeStyle,
+          lineStyle: fillStyle
+        });
+      } else {
+        config.backBufferCtx.beginPath();
+        config.backBufferCtx.moveTo(firstDataPointXPos, scale.getRange(firstDataPoint[1]));
+        for (let i = 1; i < block.length; i++) {
+          const dataPoint = block[i];
+          if (!dataPoint) {
+            continue;
+          }
+
+          const { xPos, yPos } = getPosition(config, dataPoint, scale);
+          config.backBufferCtx.lineTo(xPos, yPos);
         }
 
-        const xPos = config.xScaleBackBuffer.getRange(dataPoint[0]);
-        const yPos = scale.getRange(dataPoint[1]);
-        config.backBufferCtx.lineTo(xPos, yPos);
+        config.backBufferCtx.strokeStyle = strokeStyle;
+        config.backBufferCtx.lineWidth = 2;
+        config.backBufferCtx.stroke();
+        config.backBufferCtx.lineTo(lastDataPointXPos, config.height - config.timeAxisHeight);
+        config.backBufferCtx.lineTo(firstDataPointXPos, config.height - config.timeAxisHeight);
+
+        config.backBufferCtx.closePath();
+        config.backBufferCtx.fillStyle = fillStyle;
+        config.backBufferCtx.fill();
       }
-
-      config.backBufferCtx.strokeStyle = colors100[index];
-      config.backBufferCtx.lineWidth = 2;
-      config.backBufferCtx.stroke();
-      config.backBufferCtx.lineTo(lastDataPointXPos, config.height - config.timeAxisHeight);
-      config.backBufferCtx.lineTo(firstDataPointXPos, config.height - config.timeAxisHeight);
-
-      config.backBufferCtx.closePath();
-      config.backBufferCtx.fillStyle = colors[index];
-      config.backBufferCtx.fill();
     }
   }
 };
+
+function getPosition(config, dataPoint, scale) {
+  const xPos = config.xScaleBackBuffer.getRange(dataPoint[0]);
+  const yPos = scale.getRange(dataPoint[1]);
+  return { xPos, yPos };
+}
