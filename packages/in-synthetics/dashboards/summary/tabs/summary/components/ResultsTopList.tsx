f04657a81dd098772ebb0abc55a8981c3e4849f8
rui.zhao1 <rui.zhao1@ibm.com>
2022-05-09 21:41:29 -0400

    fix bug

--- packages/in-synthetics/dashboards/summary/tabs/summary/components/ResultsTopList.tsx
+++ packages/in-synthetics/dashboards/summary/tabs/summary/components/ResultsTopList.tsx
@@ -172,7 +172,7 @@ function AdditionalLabel({ item, selectedMetric }: Lab) {
   let formattedTime = '';
   if (metricInPayload.get(selectedMetric) === 'response_time') {
     let startTime = moment.unix(get(item, ['metrics', 'start_time', 0, 1], moment.now()) / 1000);
-    if (startTime.diff(moment.now(), 'days') > -1) {
+    if (startTime.diff(moment.now(), 'days') < -1) {
       // start time is greater than 24 hours, show date time
       formattedTime = formatDateTime(startTime!.valueOf()) as string;
     } else {
