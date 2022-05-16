ff555d2bb0c877e6c84d7b529d8973e4bc295b7a
Raj Parekh <raj.parekh@instana.com>
2022-05-16 17:46:24 +0200

    chore(TS migration): migrate packages/in-websites/subscriptions/getWebsitePaginatedBeaconGroups.ts

--- packages/in-websites/subscriptions/getWebsitePaginatedBeaconGroups.ts
+++ packages/in-websites/subscriptions/getWebsitePaginatedBeaconGroups.ts
@@ -3,9 +3,16 @@
  * (c) Copyright Instana Inc.
  */
 
-import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
+import { WebsitePaginatedBeaconGroupsItem } from '@instana/types/typeDefinitions';
+import { GetWebsitePaginatedBeaconGroupsQuery } from '@instana/types';
 
-export default createResultSubscriptionFactory({
+import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
+import { CursorPaginatedResult, Result } from 'in-types';
+
+export default createResultSubscriptionFactory<
+  GetWebsitePaginatedBeaconGroupsQuery,
+  Result<CursorPaginatedResult<WebsitePaginatedBeaconGroupsItem>>
+>({
   eventId: 'getWebsitePaginatedBeaconGroups',
   disposeSubscriptionOnDocumentHidden: false,
   trackSubscriptionStatistics: true
