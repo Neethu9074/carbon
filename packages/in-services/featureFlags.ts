1faae3e92a9a6ae89a69a8ca2aa43a5e42a2b4d5
Bryson Chisholm <bryson.chisholm@ibm.com>
2022-05-27 10:25:06 -0400

    turn off feature flag

--- packages/in-services/featureFlags.ts
+++ packages/in-services/featureFlags.ts
@@ -115,4 +115,4 @@ export const allowDownloadMetricsFromCharts = isInstanaEmail;
 
 export const perSecondAggregationEnabled = isFeatureFlagEnabled('perSecondAggregationEnabled', false);
 
-export const automationEnabled = isFeatureFlagEnabled('automationEnabled', true);
+export const automationEnabled = isFeatureFlagEnabled('automationEnabled', false);
