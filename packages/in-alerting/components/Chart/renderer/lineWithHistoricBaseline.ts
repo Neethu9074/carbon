f19e6c3542767e943eb9a86d62d6755bc22de4ed
Robert Hostlowsky <robert.hostlowsky@instana.com>
2022-05-31 09:06:17 +0200

    chore(TS migration): migrate in-alerting/components/Chart/renderer/lineWithHistoricBaseline

--- packages/in-alerting/components/Chart/renderer/lineWithHistoricBaseline.ts
+++ packages/in-alerting/components/Chart/renderer/lineWithHistoricBaseline.ts
@@ -5,27 +5,46 @@
 
 import invariant from 'invariant';
 
+// @ts-expect-error modules is not yet migrated to typescript
 import { renderHistoricBaseline } from 'in-alerting/components/Chart/renderer/historicBaseline';
+// @ts-expect-error modules is not yet migrated to typescript
 import line from 'in-components/Chart/renderer/line';
+import { AxisColor, Config, MetricDataSeries } from 'in-components/Chart/types';
+import { RenderAxis, RenderConfig } from 'in-components/Chart/renderer/types';
+import { ScaleType } from 'in-services/scale';
 
 export default {
-  render: ({ colors50, colors100, scale, config, metrics }) => {
+  render: ({
+    colors50,
+    colors100,
+    scale,
+    config,
+    metrics
+  }: {
+    colors50: AxisColor[];
+    colors100: AxisColor[];
+    scale: ScaleType;
+    config: RenderConfig;
+    metrics: MetricDataSeries[];
+  }): void => {
     validateProps(config);
     const metric = metrics[0];
 
     renderHistoricBaseline(config, scale, colors50, colors100);
 
     // historical data
-    line.render({ dataSeries: metric, color: colors100[0], scale, config });
+    line.render({ dataSeries: metric, color: colors100[0]!, scale, config });
   },
-  enrich: (config, axis) => {
+  enrich: (_config: RenderConfig, axis: RenderAxis) => {
+    // @ts-expect-error field actually does not yet exist in Axis
     axis.valuesDependOnEachOther = true;
   }
-};
+} as const;
 
-function validateProps(config) {
+function validateProps(config: Config) {
   if (__DEV__) {
     invariant(
+      // @ts-expect-error threshold it not part of current type Axis
       Number(config.y1.sensitivity) >= 0,
       'Property "sensitivity" is missing in config. Example: y1={{ sensitivity, colors:[], ... }}'
     );
