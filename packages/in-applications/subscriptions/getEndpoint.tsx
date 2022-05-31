82ea6a8c809cbd453ca1e6d44636c4f121453d9b
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2022-05-31 15:25:45 +0200

    TS Migration: GetEndpoint (#9560)
    * chore: rename to tsx
    
    * chore: added types for getEndpoint
    
    Co-authored-by: Oliver Munkebo <oliver.munkebo@instana.com>

--- packages/in-applications/subscriptions/getEndpoint.tsx
+++ packages/in-applications/subscriptions/getEndpoint.tsx
@@ -3,9 +3,11 @@
  * (c) Copyright Instana Inc.
  */
 
+import { GetEndpointQuery, Result, EndpointItem, PaginatedResult } from '@instana/types';
+
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
 
-export default createResultSubscriptionFactory({
+export default createResultSubscriptionFactory<GetEndpointQuery, Result<PaginatedResult<EndpointItem>>>({
   eventId: 'getEndpoint',
   trackSubscriptionStatistics: true
 });
