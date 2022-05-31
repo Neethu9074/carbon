336813207670f09efce001dacf1e2565dd1de3ba
Robert Hostlowsky <robert.hostlowsky@instana.com>
2022-05-31 09:01:04 +0200

    chore(TS migration): migrate in-alerting/components/Chart/renderer/lineWithThreshold

--- packages/in-alerting/components/Chart/renderer/lineWithThreshold.ts
+++ packages/in-alerting/components/Chart/renderer/lineWithThreshold.ts
@@ -5,59 +5,45 @@
 
 import invariant from 'invariant';
 
-import { isGreaterOperator } from 'in-alerting/smart-alerts/components/utils/alertUtils';
+import { renderStaticThresholdLineAndBackgrounds } from 'in-alerting/components/Chart/renderer/renderThresholdAndBackgrounds';
+// @ts-expect-error modules is not yet migrated to typescript
 import line from 'in-components/Chart/renderer/line';
+import { AxisColor, Config, MetricDataSeries } from 'in-components/Chart/types';
+import { RenderAxis, RenderConfig } from 'in-components/Chart/renderer/types';
+import { ScaleType } from 'in-services/scale';
 
 export default {
-  render: ({ colors50, colors100, scale, config, metrics }) => {
+  render: ({
+    colors50,
+    colors100,
+    scale,
+    config,
+    metrics
+  }: {
+    colors50: AxisColor[];
+    colors100: AxisColor[];
+    scale: ScaleType;
+    config: RenderConfig;
+    metrics: MetricDataSeries[];
+  }): void => {
     validateProps(config);
     const metric = metrics[0];
 
     renderStaticThresholdLineAndBackgrounds(config, scale, colors100, colors50);
 
     // historical data
-    line.render({ dataSeries: metric, color: colors100[0], scale, config });
+    line.render({ dataSeries: metric, color: colors100[0]!, scale, config });
   },
-  enrich: (config, axis) => {
+  enrich: (_config: RenderConfig, axis: RenderAxis) => {
+    // @ts-expect-error field actually does not yet exist in Axis
     axis.valuesDependOnEachOther = true;
   }
-};
+} as const;
 
-export function renderStaticThresholdLineAndBackgrounds(config, scale, colors100, colors50) {
-  const { backBufferCtx, markerPaneHeight, scales, xScaleBackBuffer, y1 } = config;
-  const { operator, thresholdLineWidth } = y1;
-
-  const yScale = scales.y1;
-  const chartHeight = scale.getRangeFrom();
-  const chartWidth = xScaleBackBuffer.getRangeTo();
-  const threshold = yScale.getRangeFrom() - yScale.getRange(y1.threshold);
-  const thresholdColor = colors100[1];
-  const alrightColor = colors50[0];
-  const violationColor = colors50[1];
-  const isGreaterOp = operator === undefined || isGreaterOperator(operator);
-
-  backBufferCtx.save();
-  // Background above line
-  backBufferCtx.fillStyle = isGreaterOp ? violationColor : alrightColor;
-  backBufferCtx.fillRect(0, markerPaneHeight, chartWidth, chartHeight - threshold - markerPaneHeight);
-
-  // Background below line
-  backBufferCtx.fillStyle = isGreaterOp ? alrightColor : violationColor;
-  backBufferCtx.fillRect(0, chartHeight - threshold, chartWidth, threshold);
-
-  // Movable line
-  backBufferCtx.beginPath();
-  backBufferCtx.moveTo(0, chartHeight - threshold);
-  backBufferCtx.lineWidth = thresholdLineWidth;
-  backBufferCtx.strokeStyle = thresholdColor;
-  backBufferCtx.lineTo(chartWidth, chartHeight - threshold);
-  backBufferCtx.stroke();
-  backBufferCtx.restore();
-}
-
-function validateProps(config) {
+function validateProps(config: Config) {
   if (__DEV__) {
     invariant(
+      // @ts-expect-error threshold it not part of current type Axis
       Number(config.y1.threshold) >= 0,
       'Property "threshold" is missing in config. Example: y1={{ threshold, colors:[], ... }}'
     );
