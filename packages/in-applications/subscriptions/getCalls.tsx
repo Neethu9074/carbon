97f74ba69077494b267379fcf53770ec4c7bde21
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2022-06-01 10:03:29 +0200

    TS Migration: GetCalls (#9561)
    * chore: migrated getCalls to TSX
    
    * chore: Add query and response types to getCalls
    
    Co-authored-by: Oliver Munkebo <oliver.munkebo@instana.com>

--- packages/in-applications/subscriptions/getCalls.tsx
+++ packages/in-applications/subscriptions/getCalls.tsx
@@ -3,9 +3,11 @@
  * (c) Copyright Instana Inc.
  */
 
+import { GetCallsQuery, Result, CallItem, CursorPaginatedResult } from '@instana/types';
+
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
 
-export default createResultSubscriptionFactory({
+export default createResultSubscriptionFactory<GetCallsQuery, Result<CursorPaginatedResult<CallItem>>>({
   eventId: 'getCalls',
   disposeSubscriptionOnDocumentHidden: false,
   trackSubscriptionStatistics: true
