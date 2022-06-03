89e9895c17dbd91504174f80aed1471361876db3
Timo Hillerns <timo.hillerns@ibm.com>
2022-06-03 13:54:18 +0200

    Add 1xx option

--- packages/in-applications/analyze/AnalyzeView2_0/AnalyzeView.js
+++ packages/in-applications/analyze/AnalyzeView2_0/AnalyzeView.js
@@ -393,6 +393,7 @@ function getFacetedSearchItems({ dataSource, hiddenCalls, onChangeHiddenCalls })
       getMetric,
       orderSuggestions: orderByValue,
       defaultValues: [
+        { name: '1xx', value: '1xx' },
         { name: '2xx', value: '2xx' },
         { name: '3xx', value: '3xx' },
         { name: '4xx', value: '4xx' },
