3191169cfe3f593e100e6bb22aecd05ed77768ab
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2022-06-03 13:18:37 +0200

    AP: DatabaseStatementTopList fixes (#9577)
    * bug: Moved endpoint types
    
    * bug: Moved endpoint types
    
    * bug: Add fallback values for labels and remove metricValue
    
    Co-authored-by: Oliver Munkebo <oliver.munkebo@instana.com>

--- packages/in-applications/Dashboards/commonComponents/database/DatabaseSections.tsx
+++ packages/in-applications/Dashboards/commonComponents/database/DatabaseSections.tsx
@@ -5,12 +5,10 @@
 
 import React, { Fragment } from 'react';
 
-import { Result, EndpointType, BoundaryScope, TimeConfig, ApplicationBoundaryScope } from '@instana/types';
-import { useObservable } from '@instana/hooks';
+import { EndpointType, BoundaryScope, TimeConfig, ApplicationBoundaryScope } from '@instana/types';
 
 import DatabaseStatementTopList from 'in-applications/Dashboards/commonComponents/database/DatabaseStatementTopList';
 import WidgetNotActive from 'in-applications/Dashboards/commonComponents/WidgetNotActive';
-import getEndpointTypes from 'in-applications/subscriptions/getEndpointTypes';
 import { UrlMatrixParamConfig } from 'in-applications/types';
 import { t } from 'in-i18n';
 
@@ -21,29 +19,13 @@ interface DatabaseSectionProps {
   endpointId: string;
   timeConfig: TimeConfig;
   applicationBoundaryScope: ApplicationBoundaryScope;
-  types: EndpointType[];
+  endpointTypes: EndpointType[];
   urlMatrixParamConfig: UrlMatrixParamConfig;
   renderHistoricDataIndicator: boolean;
 }
 
 export default function DatabaseSections(props: DatabaseSectionProps) {
-  const endpointTypes = useObservable(
-    getEndpointTypes({
-      filter: {
-        application: props.applicationId,
-        service: props.serviceId,
-        endpoint: props.endpointId,
-        timeConfig: props.timeConfig,
-        applicationBoundaryScope: props.applicationBoundaryScope,
-        includeInternalCalls: false,
-        includeSyntheticCalls: false,
-        useLongTermDataOnly: false
-      }
-    }).map((result: Result<EndpointType[]>) => result.data),
-    []
-  );
-
-  if (!hasDatabaseEndpoints(endpointTypes)) {
+  if (!hasDatabaseEndpoints(props.endpointTypes)) {
     return null;
   }
 
