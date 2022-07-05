a59bfe974f0bb67b6a1301b3865d52504b0134d2
rui.zhao1 <rui.zhao1@ibm.com>
2022-07-05 18:15:18 -0400

    fix dummy test and getError function

--- packages/in-synthetics/dashboards/details/components/FailedRun.tsx
+++ packages/in-synthetics/dashboards/details/components/FailedRun.tsx
@@ -111,5 +111,5 @@ function getStatus(resultList: Result<PaginatedResult<TestResultListItem>>) {
 }
 
 function getErrors(resultList: Result<PaginatedResult<TestResultListItem>>) {
-  return resultList?.data?.items[0]?.testResultCommonProperties?.errors;
+  return get(resultList.data?.items[0], ['testResultCommonProperties', 'errors', 0], '');
 }
