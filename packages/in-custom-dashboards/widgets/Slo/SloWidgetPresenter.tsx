8713fbdad62d9b905d4a72c8586785e6f2245ffb
Thiemo Krause <Thiemo.Krause@ibm.com>
2022-07-11 16:56:31 +0200

    fix(slowidget): put tracker context provider in the right places (#9801)

--- packages/in-custom-dashboards/widgets/Slo/SloWidgetPresenter.tsx
+++ packages/in-custom-dashboards/widgets/Slo/SloWidgetPresenter.tsx
@@ -5,10 +5,6 @@
 
 import React from 'react';
 
-import {
-  defaultTrackers,
-  SloWidgetTrackerProvider
-} from 'in-custom-dashboards/widgets/Slo/components/SloWidgetTrackerProvider';
 import useMetricAlignedWidgetTimeConfig from 'in-custom-dashboards/widgets/Slo/hooks/useMetricAlignedWidgetTimeConfig';
 import { ensureConfigBackwardCompatibility, SloWidgetConfiguration } from 'in-custom-dashboards/widgets/Slo/form';
 import useSliConfigWithPreview from 'in-custom-dashboards/widgets/Slo/hooks/useSliConfigWithPreview';
@@ -86,26 +82,24 @@ export default function SloWidgetPresenter({ actions, config, isPreview, title, 
   const unifiedProgress = allProgress(sliConfigurationProgress, entityProgress, sloMetricsProgress);
 
   return (
-    <SloWidgetTrackerProvider value={defaultTrackers}>
-      <Widget
-        title={title}
-        entityType={entityType}
-        entity={entity}
-        sliConfiguration={sliConfiguration}
-        slo={slo}
-        sloMetrics={sloMetrics}
-        granularity={chartGranularity}
-        timeWindowType={timeWindowType}
-        timeWindowConfig={chartTimeWindowConfig}
-        status={unifiedStatus}
-        progress={unifiedProgress}
-        errors={sloMetricsError}
-        actions={actions}
-        dragHandle={dragHandle}
-        isPreview={isPreview}
-        disableZooming={isFixed || isRolling}
-      />
-    </SloWidgetTrackerProvider>
+    <Widget
+      title={title}
+      entityType={entityType}
+      entity={entity}
+      sliConfiguration={sliConfiguration}
+      slo={slo}
+      sloMetrics={sloMetrics}
+      granularity={chartGranularity}
+      timeWindowType={timeWindowType}
+      timeWindowConfig={chartTimeWindowConfig}
+      status={unifiedStatus}
+      progress={unifiedProgress}
+      errors={sloMetricsError}
+      actions={actions}
+      dragHandle={dragHandle}
+      isPreview={isPreview}
+      disableZooming={isFixed || isRolling}
+    />
   );
 }
 
