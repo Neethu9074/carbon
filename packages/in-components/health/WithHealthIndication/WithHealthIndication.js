017d96844753ebd8bd40a89ff8d591811ae89faa
Anju Shivan <Anju.Shivan@ibm.com>
2022-07-20 14:53:16 +0530

    Add style to class

--- packages/in-components/health/WithHealthIndication/WithHealthIndication.js
+++ packages/in-components/health/WithHealthIndication/WithHealthIndication.js
@@ -20,7 +20,7 @@ export default function WithHealthIndication({ children, healthInfo, iconSize })
   iconSize = 2 + ((size / 8) | 1) * 2;
 
   return (
-    <div className={locals.wrapper} style={{ paddingTop: `1px` }}>
+    <div className={locals.wrapper}>
       {children}
       <div
         style={{
