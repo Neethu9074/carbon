581683461284395c035e64cd5d3ea365d91e8ab3
Robert Hostlowsky <robert.hostlowsky@ibm.com>
2022-07-21 18:39:57 +0200

    fix broken slider functionality

--- packages/in-components/Slider/DebouncedDistinctSlider.js
+++ packages/in-components/Slider/DebouncedDistinctSlider.js
@@ -10,5 +10,5 @@ import useDebouncedValue from 'in-hooks/useDebouncedValue';
 
 export default function DebouncedDistinctSlider(props) {
   const result = useDebouncedValue(props.value, props.onChange, 500, { maxWait: props.debounceMaxWait ?? 1000 });
-  return <DistinctSlider {...props} value={result.value} onChange={(_event, value) => result.onChange(value)} />;
+  return <DistinctSlider {...props} value={result.value} onChange={value => result.onChange(value)} />;
 }
