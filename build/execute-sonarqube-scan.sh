26eace139a0f36071c11ee624155695f41d29dbf
Lucas De Carvalho Bueno Santos <Lucas.Santos@ibm.com>
2022-08-05 15:42:59 +0200

    Revert to showing only dots when running tests (#9940)

--- build/execute-sonarqube-scan.sh
+++ build/execute-sonarqube-scan.sh
@@ -23,11 +23,11 @@ popd
 
 echo "Generate the lcov coverage report"
 echo "Starting unit tests for client with coverage {"
-yarn test:unit --coverage --debug --detectOpenHandles --forceExit 
+yarn test:unit --coverage --detectOpenHandles --forceExit 
 echo "} Ended unit tests for clientwith coverage"
 pushd packages/in-server
 echo "Starting unit tests for server with coverage {"
-yarn test --coverage --debug --detectOpenHandles --forceExit 
+yarn test --coverage --detectOpenHandles --forceExit 
 echo "} Ended unit tests for server with coverage"
 popd
 
