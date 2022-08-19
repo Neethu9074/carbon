cc538e92912ca897fc8b498feb8cfd5921e92471
itai <itai.wiseman@ibm.com>
2022-08-19 08:20:00 -0400

    when no suggestion use equals with empty value

--- packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
+++ packages/in-infrastructure/Explore/components/GroupedInfrastructure.js
@@ -23,13 +23,13 @@ import { type as TAG_FILTER_TYPE } from 'in-components/QueryBuilder/transformati
 import { addTagFilters } from 'in-components/QueryBuilder/transformation/backendQueryModel';
 import { joinExpressions } from 'in-components/QueryBuilder/transformation/formModel';
 import createGetGroupsSubscription from 'in-infrastructure/subscriptions/getGroups';
-import { EQUALS, NOT_EMPTY } from 'in-components/QueryBuilder/tagFilter/operators';
 import { getUniqueErrors } from 'in-components/Errors/ErroneousResultPresenter';
 import { LOAD_MORE_CONTEXT } from 'in-infrastructure/Explore/services/tracking';
 import { defaultOrder, pluginTag } from 'in-infrastructure/Explore/constants';
 import { emptyObject, indeterminateProgress } from 'in-services/fixedObjects';
 import MetricLabel from 'in-infrastructure/Explore/components/MetricLabel';
 import { getOptionalSnapshotDefinition } from 'in-sdk/snapshot/registry';
+import { EQUALS } from 'in-components/QueryBuilder/tagFilter/operators';
 import { getLinkToExplore } from 'in-infrastructure/navigation/paths';
 import NoDataAvailable from 'in-components/Errors/NoDataAvailable';
 import Header from 'in-components/QueryBuilder/components/Header';
@@ -344,7 +344,7 @@ function addTagsToBackendModel(backendQueryModel, tags) {
 function toTagFilters(tags, tagType, group) {
   return Object.entries(tags).map(([name, value]) => ({
     type: TAG_FILTER_TYPE,
-    operator: getOperator(value),
+    operator: EQUALS,
     name: getName(name, group),
     value: getValue(tagType, value),
     key: getKey(tagType, value, name, group)
@@ -352,10 +352,6 @@ function toTagFilters(tags, tagType, group) {
 }
 
 const defaultGroupIcon = 'lib_views_tag';
-
-function getOperator(value) {
-  return value.indexOf('=') > 0 ? EQUALS : NOT_EMPTY;
-}
 
 function isTagAndKeyConcat(name, group) {
   return group?.groupbyTag?.concat('.', group?.groupbyTagSecondLevelKey) === name;
@@ -370,7 +366,7 @@ function getName(name, group) {
 }
 
 function getValue(tagType, value) {
-  return isKeyValue(tagType, value) ? extractValue(value) : value;
+  return isKeyValue(tagType, value) ? extractValue(value) : '';
 }
 
 function getKey(tagType, value, name, group) {
