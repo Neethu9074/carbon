6bab799d191a0cd2c22b872a14f16576320ed7f5
Jean Detoeuf <jean.detoeuf1@ibm.com>
2022-08-20 19:36:35 +0200

    make the wiggle room customizable

--- packages/in-components/Chart/Configuration.ts
+++ packages/in-components/Chart/Configuration.ts
@@ -79,6 +79,8 @@ export default class Config {
 
   shareMaxAxisDomain?: boolean;
 
+  wiggleRoom: number;
+
   constructor(props: ConfigProps) {
     this.timeAxisHeight = 30;
     this.markerPaneHeight = 22;
@@ -94,6 +96,8 @@ export default class Config {
 
     this.y1$ = create<Axis | undefined>().emit(undefined);
     this.y2$ = create<Axis | undefined>().emit(undefined);
+
+    this.wiggleRoom = WIGGLE_ROOM;
 
     this.update(props);
   }
