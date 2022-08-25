6c6e73b9d7961018855297ce0b98b6b2fa58a8f0
tamarastanojevic <tamara@LAPTOP-LR5IKMQH.localdomain>
2022-08-25 11:05:17 +0200

    Add new header to the new files

--- packages/in-forge/tracing/azf/index.js
+++ packages/in-forge/tracing/azf/index.js
@@ -1,6 +1,7 @@
 /*
- * (c) Copyright IBM Corp. 2022
- * (c) Copyright Instana Inc. 2022
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2022
  */
 
 import { registerSpanDefinition } from 'in-sdk/tracing';
