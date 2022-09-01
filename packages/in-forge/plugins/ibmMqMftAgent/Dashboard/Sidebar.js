79dac506004c2868bd4061929587edc5489969bf
LI JIAN WANG <wlijian@cn.ibm.com>
2022-09-01 17:14:42 +0800

    IBM MQ Managed File Transfer Support (#10018)
    * IBM MQ MFT support

---
+++ packages/in-forge/plugins/ibmMqMftAgent/Dashboard/Sidebar.js
@@ -0,0 +1,24 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2022
+ */
+
+import React, { Fragment } from 'react';
+
+import Collapsible from 'in-sdk/components/sidebar/Collapsible';
+import { t } from 'in-i18n';
+import Info from '../Info';
+
+export default function IbmMqMftAgentSidebar({ snapshot }) {
+  return (
+    <Fragment>
+      <Collapsible initiallyOpen>
+        <Collapsible.Header>{t('in-forge:plugins.ibmMqMftAgent.dashboard.ibmMqMftAgent')}</Collapsible.Header>
+        <Collapsible.Content>
+          <Info snapshot={snapshot} />
+        </Collapsible.Content>
+      </Collapsible>
+    </Fragment>
+  );
+}
