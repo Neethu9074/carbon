c4aad645b58bdf5df41622f7a2cacdfc89690aac
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2022-09-07 15:58:24 +0200

    Fix website sa simple mode hanging custom event chart (#10122)
    * fixing by explicitly setting the threshold type to historic-baseline

--- packages/in-alerting/smart-alerts/websites/components/ProvideCustomEvent.js
+++ packages/in-alerting/smart-alerts/websites/components/ProvideCustomEvent.js
@@ -11,6 +11,8 @@ import { Button } from '@instana/components';
 
 import AlertConfigSlideInContentWrapper from 'in-alerting/smart-alerts/components/smart-alert-dialog/AlertConfigSlideInContentWrapper';
 import CustomEventsList from 'in-alerting/smart-alerts/websites/components/CustomEventsList';
+import createThresholdForm from 'in-alerting/smart-alerts/websites/form/thresholdForm';
+import { HISTORIC_BASELINE } from 'in-alerting/smart-alerts/data/thresholdTypes';
 import HorizontalFlexWrapper from 'in-components/layout/HorizontalFlexWrapper';
 import { modeAdvanced } from 'in-alerting/smart-alerts/websites/constants';
 import DebouncedInput from 'in-components/form/Input/DebouncedInput';
@@ -23,8 +25,25 @@ import locals from 'in-alerting/smart-alerts/websites/components/ProvideCustomEv
 
 export default function ProvideCustomEvent({ form, timeConfig, onSelectCustomEvent, mode, updateForm }) {
   const customEventNameField = form.get('rule').get('customEventName');
-  const onValueChange = value =>
-    updateForm(form.updateIn(['rule', 'customEventName'], f => f.setValue(value ?? '').setTouched(true)));
+
+  const onValueChange = value => {
+    let updatedForm = form.updateIn(['rule', 'customEventName'], f => f.setValue(value ?? '').setTouched(true));
+
+    if (mode !== modeAdvanced) {
+      // here, in simple-mode, the backend could have set the type to static silently,
+      // so we need to reset it, before fetching the new baseline
+      //
+      // This is done, to easily get started in simple mode, and even do not bother the
+      // user with a not-enough-data message
+      const threshold = form.get('threshold').toJS();
+      const thresholdWithHistoricBaseline = { ...threshold, type: HISTORIC_BASELINE };
+      updatedForm = updatedForm
+        .updateIn(['rule', 'customEventName'], f => f.setValue(value ?? '').setTouched(true))
+        .put('threshold', createThresholdForm(thresholdWithHistoricBaseline, 'customEvent').setTouched(true));
+    }
+
+    return updateForm(updatedForm);
+  };
 
   return customEventNameField.map(field => (
     <div className={locals.container}>
@@ -34,7 +53,7 @@ export default function ProvideCustomEvent({ form, timeConfig, onSelectCustomEve
             <CustomEventInput field={field} onValueChange={onValueChange} />
             <SelectCustomEventButton
               form={form}
-              updateForm={updateForm}
+              onValueChange={onValueChange}
               onSelectCustomEvent={onSelectCustomEvent}
               timeConfig={timeConfig}
             />
@@ -46,7 +65,7 @@ export default function ProvideCustomEvent({ form, timeConfig, onSelectCustomEve
         <>
           <SelectCustomEventButton
             form={form}
-            updateForm={updateForm}
+            onValueChange={onValueChange}
             onSelectCustomEvent={onSelectCustomEvent}
             timeConfig={timeConfig}
           />
@@ -63,7 +82,7 @@ export default function ProvideCustomEvent({ form, timeConfig, onSelectCustomEve
   ));
 }
 
-function SelectCustomEventButton({ form, updateForm, onSelectCustomEvent, timeConfig }) {
+function SelectCustomEventButton({ form, onSelectCustomEvent, onValueChange, timeConfig }) {
   return (
     <Button
       onClick={() =>
@@ -75,11 +94,7 @@ function SelectCustomEventButton({ form, updateForm, onSelectCustomEvent, timeCo
                   websiteId={form.get('websiteId').value}
                   tagFilterExpression={form.get('tagFilterExpression').value}
                   timeConfig={timeConfig}
-                  onCustomEventSelect={customEventName => {
-                    updateForm(
-                      form.updateIn(['rule', 'customEventName'], f => f.setValue(customEventName).setTouched(true))
-                    );
-                  }}
+                  onCustomEventSelect={onValueChange}
                   slideOut={() => onSelectCustomEvent({ isVisible: false })}
                 />
               </AlertConfigSlideInContentWrapper>
