Neethu9074/carbon

commit 7008bf8224e5f440761f07cacdf332a35360b084

Migrate UnifiedMetricsChart to typescript

packages/in-components/Chart/ChartWrapper.tsx
@@ -6,11 +6,14 @@
 import invariant from 'invariant';
 import React from 'react';
 
+import { Axis, Config, MetricMap, TimeShift } from 'in-components/Chart/types';
 import { translateOffsetToTimeShiftConfig } from 'in-stores/time/shifting';
+import { MetricData } from 'in-custom-dashboards/widgets/Chart/types';
 import ResultAwareChart from 'in-components/Chart/ResultAwareChart';
 import { getResolvedTimeConfig } from 'in-applications/metrics';
 import { getChartGranularity } from 'in-stores/metric/metric';
 import { deepCopy } from 'in-services/util/object';
+import { Result, TimeConfig } from 'in-types';
 import { t } from 'in-i18n';
 
 // Sample Usage
@@ -58,28 +61,37 @@ import { t } from 'in-i18n';
             }}
           />
  */
-export default function ChartWrapper({ result, ...props }) {
+
+interface Props extends Config {
+  result: Result<MetricData>;
+}
+
+export default function ChartWrapper({ result, ...props }: Props): React.ReactElement {
   return <ResultAwareChart result={result} config={wrapProps(result, props)} />;
 }
 
-function wrapProps(result, props) {
-  if (__DEV__ && props.metricsConfiguration) {
+function wrapProps(result: Result<MetricData>, props: Config): Config {
+  const metricsConfiguration = props.metricsConfiguration;
+  if (__DEV__ && metricsConfiguration) {
     props.y1?.metricIds.forEach(id => {
       invariant(
-        Object.keys(props.metricsConfiguration.metrics).indexOf(id) !== -1,
+        Object.keys(metricsConfiguration.metrics).indexOf(id) !== -1,
         t('in-components:chart.chartWrapperMetricNotfound', { metricId: id })
       );
     });
 
     props.y2?.metricIds.forEach(id => {
       invariant(
-        Object.keys(props.metricsConfiguration.metrics).indexOf(id) !== -1,
+        Object.keys(metricsConfiguration.metrics).indexOf(id) !== -1,
         t('in-components:chart.chartWrapperMetricNotfound', { metricId: id })
       );
     });
 
-    const keys = props.metricsConfiguration.metrics;
+    const keys = metricsConfiguration.metrics;
+    /// todo: check if this is actually needed
+    // @ts-expect-error
     for (let i = 1; i < keys.length; i++) {
+      // @ts-expect-error
       if (this[i] !== this[0]) {
         invariant(false, t('in-components:chart.chartWrapperSameAxisSameValue'));
         break;
@@ -89,32 +101,32 @@ function wrapProps(result, props) {
 
   if (result.errors.length > 0 || result.progress.loading) {
     return {
-      cardTitle: props.cardTitle,
+      title: props.title,
       cardUseMaxAvailableHeight: props.cardUseMaxAvailableHeight,
-      cardHeader: props.cardHeader,
-      renderErrorDetail: props.renderErrorDetail
+      rightHeaderContent: props.rightHeaderContent,
+      renderErrorDetail: props.renderErrorDetail,
+      timeConfig: props.timeConfig
     };
   }
 
-  const propsClone = deepCopy({
+  const propsClone: Config = deepCopy({
     ...props,
     // cardHeader can be defined and it could be a React element. Cloning this is a super expensive
     // operation that is getting more and more expensive the more often this is executed.
     // Also, there is no need to clone this React element, as we aren't manipulating it.
-    cardHeader: undefined,
-    rightCardHeader: undefined
+    rightHeaderContent: undefined
   });
 
   if (propsClone.y1 != null) {
-    propsClone.y1.metrics = propsClone.y1.metricIds.map(id => result.data[id] || []);
-    propsClone.y1.aggregations = propsClone.y1.metricIds.map(id => props.metricsConfiguration.metrics[id].aggregation);
-    determineTimeShifts(propsClone.y1, props.metricsConfiguration.metrics, propsClone.timeConfig);
+    propsClone.y1.metrics = propsClone.y1.metricIds.map(id => result.data?.[id] || []);
+    propsClone.y1.aggregations = propsClone.y1.metricIds.map(id => props.metricsConfiguration?.metrics[id].aggregation);
+    determineTimeShifts(propsClone.y1, propsClone.timeConfig, props.metricsConfiguration?.metrics);
   }
 
   if (propsClone.y2 != null) {
-    propsClone.y2.metrics = propsClone.y2.metricIds.map(id => result.data[id] || []);
-    propsClone.y2.aggregations = propsClone.y2.metricIds.map(id => props.metricsConfiguration.metrics[id].aggregation);
-    determineTimeShifts(propsClone.y2, props.metricsConfiguration.metrics, propsClone.timeConfig);
+    propsClone.y2.metrics = propsClone.y2.metricIds.map(id => result.data?.[id] || []);
+    propsClone.y2.aggregations = propsClone.y2.metricIds.map(id => props.metricsConfiguration?.metrics[id].aggregation);
+    determineTimeShifts(propsClone.y2, propsClone.timeConfig, props.metricsConfiguration?.metrics);
   }
 
   // result.time depends on the time configuration send to the backend. We use it to fixate the
@@ -122,19 +134,20 @@ function wrapProps(result, props) {
   // This change ensures that the axis always represents the current time window.
   const smallestTimeShift = getSmallestTimeShift(propsClone.y1, propsClone.y2);
   propsClone.originalTimeConfig = propsClone.timeConfig;
-  propsClone.timeConfig = getResolvedTimeConfig(propsClone.timeConfig, result.time - smallestTimeShift);
+  propsClone.timeConfig = result.time
+    ? getResolvedTimeConfig(propsClone.timeConfig, result.time - smallestTimeShift)
+    : propsClone.timeConfig;
   propsClone.granularity = propsClone.granularity || getChartGranularity(propsClone.timeConfig);
-  propsClone.cardHeader = props.cardHeader;
-  propsClone.customChartComponent = props.customChartComponent;
+  propsClone.rightHeaderContent = props.rightHeaderContent;
 
   return propsClone;
 }
 
-function determineTimeShifts(axis, metrics, timeConfig) {
+function determineTimeShifts(axis: Axis, timeConfig: TimeConfig, metrics?: MetricMap) {
   let hasTimeShifts = false;
 
   axis.timeShifts = axis.metricIds.map(id => {
-    const timeShift = translateOffsetToTimeShiftConfig(metrics[id].timeShift, timeConfig);
+    const timeShift = translateOffsetToTimeShiftConfig(metrics?.[id].timeShift ?? 0, timeConfig);
     if (timeShift.offset < 0 || timeShift.offset > 0) {
       hasTimeShifts = true;
     }
@@ -149,22 +162,19 @@ function determineTimeShifts(axis, metrics, timeConfig) {
 
 // Time shifts are always negative, e.g. last hour is 1000 * 60 * 60 * -1. This in turn
 // means that the smallest time shift is the largest number
-function getSmallestTimeShift(y1, y2) {
-  let smallestTimeShift = undefined;
-  if (!y1?.timeShifts) {
+function getSmallestTimeShift(y1?: Axis, y2?: Axis): number {
+  if (!y1 || !y1?.timeShifts) {
     return 0;
   }
-  smallestTimeShift = y1.timeShifts.reduce(getSmallestTimeShiftReducer, smallestTimeShift);
+  let smallestTimeShift = y1.timeShifts.reduce(getSmallestTimeShiftReducer, 0);
 
-  if (!y2) {
+  if (!y2 || !y2.timeShifts) {
     return smallestTimeShift;
-  } else if (!y2.timeShifts) {
-    return 0;
   }
   return y2.timeShifts.reduce(getSmallestTimeShiftReducer, smallestTimeShift);
 }
 
-function getSmallestTimeShiftReducer(agg, timeShift) {
+function getSmallestTimeShiftReducer(agg: number, timeShift: TimeShift): number {
   if (timeShift == null) {
     return agg;
   } else if (agg == null) {

packages/in-applications/Dashboards/service/tabs/troubleshooting/AlternativeServicesChart/AlternativeServicesChart.tsx
@@ -10,25 +10,25 @@ import { KeyValue, Li, SvgIcon, Ul } from '@instana/components';
 import { useObservable } from '@instana/hooks';
 
 import AlternativeServicesChartPresenter from 'in-applications/Dashboards/service/tabs/troubleshooting/AlternativeServicesChart/AlternativeServicesChartPresenter';
+import { AggregationType, PaginatedResult, Result, ServiceItem, TagFilterEntity, TimeConfig } from 'in-types';
 import getServicesCorrelatedByTag from 'in-applications/subscriptions/getServicesCorrelatedByTag';
-import { PaginatedResult, Result, ServiceItem, TagFilterEntity, TimeConfig } from 'in-types';
 import { availableCorrelationTags, CorrelationTag } from './correlationTags';
 import { OverlayContentProps } from 'in-components/overlays/Overlay/types';
 import AlternativeServicesTopList from './AlternativeServicesTopList';
 import DropdownButton from 'in-components/Button/DropdownButton';
 import { getChartGranularity } from 'in-stores/metric/metric';
+import { Renderer } from 'in-components/Chart/renderer/types';
 import Overlay from 'in-components/overlays/Overlay/Overlay';
 import { FormatterFn } from 'in-stores/metric/formatters';
 import { Col, Row } from 'in-components/layout/Grid/Grid';
 import { pendingResult } from 'in-services/fixedObjects';
-import { Renderer } from 'in-components/Chart/types';
 
 import locals from './AlternativeServicesChartPresenter.mless';
 
 export interface MetricDefinition {
   label: string;
   metric: string;
-  aggregation: string;
+  aggregation: AggregationType;
   formatter: FormatterFn;
   renderer: Renderer;
   fallbackMetricValue?: [number, number][];
@@ -99,7 +99,7 @@ export default function AlternativeServicesChartWrapper(props: AlternativeServic
           <AlternativeServicesChartPresenter
             {...props}
             result={result}
-            cardHeader={tagSelectionOverlay}
+            rightHeaderContent={tagSelectionOverlay}
             metricDefinition={metricDefinition}
             renderPostChartContent={props.renderInfoBox}
             renderHistoricDataIndicator={hasApproximateData}

packages/in-applications/Dashboards/service/tabs/troubleshooting/AlternativeServicesChart/AlternativeServicesChartPresenter.tsx
@@ -5,9 +5,15 @@
 
 import React, { ReactElement } from 'react';
 
+import {
+  AdditionChartContentProps,
+  Axis,
+  Config,
+  ContextMenuButton,
+  MetricDataSeries
+} from 'in-components/Chart/types';
 // eslint-disable-next-line no-restricted-imports
 import { getResolvedTimeConfig, TimeResult } from 'in-applications/metrics';
-import { AdditionChartContentProps, Config, ContextMenuButton, MetricDataSeries } from 'in-components/Chart/types';
 import { PaginatedResult, Result, ServiceItem, TimeConfig } from 'in-types';
 import ResultAwareChart from 'in-components/Chart/ResultAwareChart';
 import { getChartGranularity } from 'in-stores/metric/metric';
@@ -17,8 +23,8 @@ export interface AlternativeServicesChartPresenterProps {
   result: Result<PaginatedResult<ServiceItem>>;
   metricDefinition: MetricDefinition;
   timeConfig: TimeConfig;
-  cardTitle?: string;
-  cardHeader?: ReactElement;
+  title?: string;
+  rightHeaderContent?: ReactElement;
   translateLabel?: (value: string) => string;
   translateColor?: (value: string) => string;
   metricIds?: string[];
@@ -30,10 +36,10 @@ export interface AlternativeServicesChartPresenterProps {
 
 export default function AlternativeServicesChartPresenter({
   result,
-  cardHeader,
+  rightHeaderContent,
   metricDefinition,
   timeConfig,
-  cardTitle,
+  title,
   translateLabel,
   translateColor,
   metricIds,
@@ -44,24 +50,13 @@ export default function AlternativeServicesChartPresenter({
 }: AlternativeServicesChartPresenterProps) {
   const hasApproximateData = result?.resultPrecisionDetails?.resultPrecision === 'PRECISION_APPROXIMATE';
 
-  const chartConfig: Config = {
-    cardTitle,
-    cardHeader,
-    granularity: getChartGranularity(timeConfig),
-    primaryContextMenuAction,
-    additionalContextMenuButtons,
-    y1: {
-      formatter: metricDefinition.formatter,
-      renderer: metricDefinition.renderer,
-      metrics: [],
-      metricIds: [],
-      labels: [],
-      colors: []
-    },
-    renderPostChartContent,
-    renderHistoricDataIndicator,
-    hasApproximateData,
-    timeConfig
+  const y1: Axis = {
+    formatter: metricDefinition.formatter,
+    renderer: metricDefinition.renderer,
+    metrics: [],
+    metricIds: [],
+    labels: [],
+    colors: []
   };
 
   const validItem = result?.data?.items?.find((item: ServiceItem) => item.metrics[metricDefinition.metric]);
@@ -69,10 +64,10 @@ export default function AlternativeServicesChartPresenter({
     if (!metricIds) {
       metricIds = result?.data?.items?.map((item: ServiceItem) => item.service.id) ?? [];
     }
-    chartConfig.y1.metricIds = metricIds;
+    y1.metricIds = metricIds;
 
-    chartConfig.timeConfig = getResolvedTimeConfig(timeConfig, result as TimeResult);
-    chartConfig.y1.metrics = metricIds.map((id: string) => {
+    timeConfig = getResolvedTimeConfig(timeConfig, result as TimeResult);
+    y1.metrics = metricIds.map((id: string) => {
       const matchingItem = result?.data?.items?.find((item: ServiceItem) => item.service.id === id);
       if (matchingItem) {
         return matchingItem.metrics[metricDefinition.metric] as MetricDataSeries;
@@ -80,22 +75,35 @@ export default function AlternativeServicesChartPresenter({
       return metricDefinition.fallbackMetricValue ?? [];
     });
 
-    chartConfig.y1.aggregations = metricIds.map(() => metricDefinition.aggregation);
+    y1.aggregations = metricIds.map(() => metricDefinition.aggregation);
 
-    chartConfig.y1.labels = metricIds;
+    y1.labels = metricIds;
     if (translateLabel) {
       const translatedLabels = metricIds.map((id: string) => translateLabel(id));
       if (translatedLabels.every((item?: string | undefined) => item)) {
-        chartConfig.y1.labels = translatedLabels;
+        y1.labels = translatedLabels;
       }
     }
     if (translateColor) {
       const colors = metricIds.map((label: string) => translateColor(label));
       if (colors.every((item: string | undefined) => item)) {
-        chartConfig.y1.colors = colors;
+        y1.colors = colors;
       }
     }
   }
+
+  const chartConfig: Config = {
+    title,
+    rightHeaderContent,
+    granularity: getChartGranularity(timeConfig),
+    primaryContextMenuAction,
+    additionalContextMenuButtons,
+    y1,
+    renderPostChartContent,
+    renderHistoricDataIndicator,
+    hasApproximateData,
+    timeConfig
+  };
 
   return <ResultAwareChart result={result} config={(chartConfig as unknown) as Config} />;
 }

packages/in-applications/Dashboards/service/tabs/troubleshooting/TroubleShootingChart.tsx
@@ -9,11 +9,10 @@ import {
   createFormModelFromSyntheticOption,
   createHiddenCallsFromSyntheticOption
 } from 'in-applications/Dashboards/commonComponents/includeSyntheticCalls';
-// @ts-expect-error
 import UnifiedMetricsChart from 'in-custom-dashboards/widgets/Chart/UnifiedMetricsChart';
 import getJumpToAnalyzeHref$ from 'in-applications/components/getJumpToAnalyzeHref';
+import { Metric } from 'in-custom-dashboards/widgets/Chart/types';
 import { createMetricField } from 'in-analyze/navigation/paths';
-import { MetricConfig } from './metricConfigs';
 import { Group, TimeConfig } from 'in-types';
 import { t } from 'in-i18n';
 
@@ -22,7 +21,7 @@ export interface TroubleShootingChartProps {
   problemStatement?: string;
   renderInfoBox?: () => ReactNode;
   serviceId: string;
-  metricConfigs: MetricConfig[];
+  metricConfigs: Metric[];
   groupBy: Group;
   boundaryScope: string;
   syntheticCalls: string;
@@ -45,7 +44,7 @@ export default function TroubleShootingChart({
   return (
     <UnifiedMetricsChart
       title={title}
-      rightHeaderContent={problemStatement && <i>{problemStatement}</i>}
+      rightHeaderContent={problemStatement ? <i>{problemStatement}</i> : undefined}
       automaticallySize={false}
       renderLegend
       renderHistoricDataIndicator

packages/in-applications/Dashboards/service/tabs/troubleshooting/Troubleshooting.tsx
@@ -35,11 +35,10 @@ import InfraReferenceTypesInfoxBox from 'in-applications/Dashboards/service/tabs
 import ServiceMappingRulesInfoBox from 'in-applications/Dashboards/service/tabs/troubleshooting/infobox/ServiceMappingRulesInfoBox';
 import ServiceMappingInfoBox from 'in-applications/Dashboards/service/tabs/troubleshooting/infobox/ServiceMappingInfoBox';
 import InfraLinkingInfoBox from 'in-applications/Dashboards/service/tabs/troubleshooting/infobox/InfraLinkingInfoBox';
-// @ts-expect-error
-import Renderer from 'in-components/Chart/renderer/Renderer';
 import AlternativeServicesInfoBox from './infobox/AlternativeServicesInfoBox';
 import { DESTINATION } from 'in-components/QueryBuilder/tagFilter/entities';
 import { syntheticCallsOptions } from 'in-applications/constants';
+import Renderer from 'in-components/Chart/renderer/Renderer';
 import useTimeShiftConfig from 'in-hooks/useTimeShiftConfig';
 import TroubleShootingChart from './TroubleShootingChart';
 import { number } from 'in-services/formatters/number';

packages/in-applications/Dashboards/service/tabs/troubleshooting/metricConfigs.ts
@@ -6,6 +6,7 @@
 import { tagFilter } from 'in-components/QueryBuilder/transformation/tagFilter';
 import { DESTINATION } from 'in-components/QueryBuilder/tagFilter/entities';
 import { EQUALS } from 'in-components/QueryBuilder/tagFilter/operators';
+import { Metric } from 'in-custom-dashboards/widgets/Chart/types';
 import { Group, TagFilter, TagFilterExpression } from 'in-types';
 
 export const createServiceIdTagFilter = (serviceId: string): TagFilter => {
@@ -29,147 +30,163 @@ export const createTagFilterExpression = (serviceId: string, ...otherFilters: Ta
   };
 };
 
-const getDefaultMetricConfig = (serviceId: string) => {
+function getDefaultMetricConfig(serviceId: string): Metric {
   return {
     metric: 'calls',
     aggregation: 'SUM',
     source: 'APPLICATION',
     tagFilterExpression: createTagFilterExpression(serviceId)
   };
-};
+}
 
 export const groupByServiceMappingOutcome: Group = {
   groupbyTag: 'call.meta_tags',
   groupbyTagSecondLevelKey: 'destination_service_mapping_outcome_detail'
 } as Group;
-export const getServiceMappingMetricConfig = (serviceId: string) => {
+export function getServiceMappingMetricConfig(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByServiceMappingOutcome,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByInfraLinkingOutcome: Group = {
   groupbyTag: 'call.meta_tags',
   groupbyTagSecondLevelKey: 'destination_infra_detection_detail'
 } as Group;
-export const getInfraLinkingMetricConfig = (serviceId: string) => {
+export function getInfraLinkingMetricConfig(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByInfraLinkingOutcome,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByHostname: Group = {
   groupbyTag: 'host.name',
   groupbyTagEntity: DESTINATION
 } as Group;
-export const getGroupByHostnameConfig = (serviceId: string) => {
+export function getGroupByHostnameConfig(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByHostname,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByHttpHost: Group = {
   groupbyTag: 'call.http.host'
 } as Group;
-export const getGroupByHttpHostConfig = (serviceId: string) => {
+export function getGroupByHttpHostConfig(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByHttpHost,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByInfraReferenceType: Group = {
   groupbyTag: 'call.meta_tags',
   groupbyTagSecondLevelKey: 'destination_infra_reference_type'
 } as Group;
-export const getInfraReferenceTypeMetricConfig = (serviceId: string) => {
+export function getInfraReferenceTypeMetricConfig(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByInfraReferenceType,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupBySpanType: Group = {
   groupbyTag: 'call.span_type'
 } as Group;
-export const getGroupBySpanType = (serviceId: string) => {
+export function getGroupBySpanType(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupBySpanType,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByProcessUptime: Group = {
   groupbyTag: 'call.meta_tags',
   groupbyTagSecondLevelKey: 'destination_process_uptime'
 } as Group;
-export const getGroupByProcessUptime = (serviceId: string) => {
+export function getGroupByProcessUptime(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByProcessUptime,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export const groupByServiceRuleId: Group = {
   groupbyTag: 'service.rule_id'
 } as Group;
-export const getGroupByServiceRuleId = (serviceId: string) => {
+export function getGroupByServiceRuleId(serviceId: string): Metric {
   return {
     ...getDefaultMetricConfig(serviceId),
     grouping: [
       {
         by: groupByServiceRuleId,
         direction: 'DESC',
-        maxResults: 20
+        maxResults: 20,
+        includeOthers: false,
+        includeUnmatched: false
       }
     ]
   };
-};
+}
 
 export type MetricConfig = ReturnType<typeof getServiceMappingMetricConfig>;
 

packages/in-applications/metrics.ts
@@ -42,7 +42,7 @@ export function getResolvedTimeConfig(timeConfig: TimeConfig, resultOrTime: numb
 export const getSparkChartGranularity = getChartGranularity;
 
 export function extendMetricConfigurationOnLiveMode(metricsConfiguration: MetricsConfiguration): MetricsConfiguration {
-  const timeConfig = metricsConfiguration.filter.timeConfig;
+  const timeConfig = metricsConfiguration.filter?.timeConfig;
   if (!timeConfig?.autoRefresh) {
     return metricsConfiguration;
   }

commit c61a9f54572ec4fcee88eeecf7a4588994a66c8a

Fix chart time-shifts (#10173)
# Why

Chart time-shifts have been broken since https://github.ibm.com/instana/ui-client/commit/7008bf8224e5f440761f07cacdf332a35360b084. This is because the time-shift calculation no longer works correctly when there is a single time-shifted axis and the time-shifts are not passed to the `ChartWrapper` where the shifted timeConfig is created.

# What

- Pass the `timeShifts` for the axes to the `ChartWrapper`
- Ensure that the minimum time-shift calculation works (right now it will always return zero since the time-shifts are always zero)

# References

- [Story](https://instana.kanbanize.com/ctrl_board/36/cards/104561/details/)

packages/in-components/Chart/ChartWrapper.tsx
@@ -174,7 +174,7 @@ function getSmallestTimeShift(y1?: AxisConfiguration, y2?: AxisConfiguration): n
   if (!y1 || !y1?.timeShifts) {
     return 0;
   }
-  let smallestTimeShift = y1.timeShifts.reduce(getSmallestTimeShiftReducer, 0);
+  let smallestTimeShift = y1.timeShifts.reduce(getSmallestTimeShiftReducer, Number.MIN_SAFE_INTEGER);
 
   if (!y2 || !y2.timeShifts) {
     return smallestTimeShift;

packages/in-custom-dashboards/widgets/Chart/UnifiedMetricsChart.tsx
@@ -186,8 +186,8 @@ function DataLoadingWrapper({
   return (
     <ChartWrapper
       timeConfig={timeConfig}
-      y1={toAxisConfiguration(config, 'y1', config.y1, resultDataAsList)}
-      y2={toAxisConfiguration(config, 'y2', config.y2, resultDataAsList)}
+      y1={toAxisConfiguration(config, 'y1', config.y1, resultDataAsList, timeConfig)}
+      y2={toAxisConfiguration(config, 'y2', config.y2, resultDataAsList, timeConfig)}
       metricsConfiguration={toMetricsConfiguration(config, resultDataAsList, companionResultDataAsList)}
       result={remappedResult}
       companionResult={remappedCompanionResult}
@@ -256,9 +256,7 @@ function addUnifiedMetricsConfigForMetrics(
         resultType,
         granularity: adjustedGranularity,
         timeConfig: timeConfig,
-        timeShift: metricConfiguration.timeShift
-          ? translateOffsetToTimeShiftConfig(metricConfiguration.timeShift, timeConfig)
-          : { offset: 0 }
+        timeShift: translateOffsetToTimeShiftConfig(metricConfiguration.timeShift, timeConfig)
       } as UnifiedMetricConfigurationUnion)
   );
 }
@@ -278,9 +276,7 @@ function addUnifiedMetricsConfigForCompanionMetrics(
         resultType,
         granularity: adjustedGranularity,
         timeConfig: timeConfig,
-        timeShift: metricConfiguration.timeShift
-          ? translateOffsetToTimeShiftConfig(metricConfiguration.timeShift, timeConfig)
-          : { offset: 0 }
+        timeShift: translateOffsetToTimeShiftConfig(metricConfiguration.timeShift, timeConfig)
       } as UnifiedMetricConfigurationUnion)
   );
 }
@@ -447,8 +443,9 @@ export function getMetricIdForGroup(metricId: string, groupLabel: string) {
 export function toAxisConfiguration(
   chartConfig: Config,
   name: string,
-  axis?: Axis,
-  resultDataAsList?: UnifiedMetricsResult[]
+  axis: Axis | undefined,
+  resultDataAsList: UnifiedMetricsResult[] | undefined,
+  timeConfig: TimeConfig
 ): ChartAxis | undefined {
   if (!axis || axis.metrics.length === 0 || !resultDataAsList) {
     return;
@@ -538,6 +535,7 @@ export function toAxisConfiguration(
     max: axis.max,
     calculateStackDifferences: axis.calculateStackDifferences,
     metrics: [],
-    companionMetrics: []
+    companionMetrics: [],
+    timeShifts: axis.metrics.map(({ timeShift }) => translateOffsetToTimeShiftConfig(timeShift, timeConfig))
   };
 }

packages/in-stores/time/shifting.ts
@@ -5,7 +5,7 @@
 
 import { ParameterDefinition } from 'in-stores/navigation/types';
 import { formatDuration } from 'in-services/formatters/date';
-import { TimeConfig, TimeShift } from 'in-types';
+import { Nullish, TimeConfig, TimeShift } from 'in-types';
 import { days, hours } from 'in-services/time';
 import { t } from 'in-i18n';
 
@@ -87,7 +87,7 @@ const defaultTimeShiftConfig: TimeShift = {
   offset: 0
 };
 export function translateOffsetToTimeShiftConfig(
-  timeShift: TimeShiftOffset | TimeShift,
+  timeShift: TimeShiftOffset | TimeShift | Nullish,
   timeConfig: TimeConfig
 ): TimeShift {
   if (timeShift == null) {