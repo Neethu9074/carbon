2be470d9d87bb260a064f7bb4f548baff1bb92cd
Bryson Chisholm <bryson.chisholm@ibm.com>
2022-09-28 10:13:41 -0400

    add back types to file

--- packages/in-subscription/agentResponse.ts
+++ packages/in-subscription/agentResponse.ts
@@ -1,11 +1,17 @@
 /*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc.
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2022
  */
 
 import createSubscription from 'in-subscription/subscription';
 
-export default createSubscription({
+export default createSubscription<any, AgentResponse>({
   eventId: 'subscribe-agent-response',
   memoizeFor: 100
 });
+
+export type AgentResponse = {
+  data: any;
+  error: string;
+};
