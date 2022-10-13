843bcd36d39988d8927fa1b577939fe2d7035c66
Itai Wiseman <Itai.Wiseman@ibm.com>
2022-10-13 07:30:33 -0400

    Implement search functionality in top level entity type explorer (#10302)
    * get all types in one request and search bar
    
    * counter as left header
    
    * search placeholder

--- packages/in-infrastructure/Explore/components/EntityList.js
+++ packages/in-infrastructure/Explore/components/EntityList.js
@@ -3,40 +3,48 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { getGroupTagValue } from 'in-infrastructure/Explore/components/GroupedInfrastructure';
-import CursorPaginatedTable from 'in-components/tables/ServerTable/CursorPaginatedTable';
-import { pagesLoaded } from 'in-infrastructure/Explore/components/InfrastructureList';
+import ServerTablePresenter from 'in-components/tables/ServerTable/ServerTablePresenter';
 import createGetGroupsSubscription from 'in-infrastructure/subscriptions/getGroups';
 import { getLinkToExplore } from 'in-infrastructure/navigation/paths';
 import Header from 'in-components/QueryBuilder/components/Header';
 import useCursorPagination from 'in-hooks/useCursorPagination';
 import EntityLink from 'in-components/EntityLink/EntityLink';
-import { noop } from 'in-services/util/function';
 import { t } from 'in-i18n';
 
-export default function EntityList({
-  retrievalSize = 20,
-  backendQueryModel,
-  timeConfig,
-  setOrder = noop,
-  order,
-  type,
-  tracking
-}) {
-  const {
-    items,
-    loadMore: cursorPaginationDefaultLoadMore,
-    cursor,
-    errors,
-    progress,
-    totalHits,
-    ...tableProps
-  } = useCursorPagination(
+export default function EntityList({ retrievalSize = 20, backendQueryModel, timeConfig, order, type }) {
+  const { items, errors, progress, totalHits } = useCursorPagination(
     ({ cursor }) => getTableData({ timeConfig, retrievalSize, backendQueryModel, order, type, cursor }),
     [timeConfig, retrievalSize, backendQueryModel, type, order]
   );
+
+  const hasErrors = errors?.length > 0;
+  const isLoading = progress?.loading;
+
+  const [data, setResultData] = useState(createResultData(items));
+
+  const onChangeItems = items => {
+    setResultData(createResultData(items));
+  };
+
+  if (!isLoading && data.progress.loading === true) {
+    onChangeItems(items);
+  }
+
+  function createResultData(items) {
+    return {
+      progress: {
+        loading: isLoading
+      },
+      errors: errors,
+      data: {
+        items: items ?? [],
+        page: 1
+      }
+    };
+  }
 
   const columnDefinitions = [
     {
@@ -73,43 +81,62 @@ export default function EntityList({
     }
   ]);
 
-  const hasErrors = errors?.length > 0;
-  const isLoading = progress?.loading;
-
   return (
     <>
-      <Header totalRetainedItemCount={totalHits} hasErrors={hasErrors} isLoading={isLoading} dataSource="entityType" />
-
-      <CursorPaginatedTable
+      <ServerTablePresenter
+        orderBy="label"
+        orderDirection="ASC"
+        result={data}
         columnDefinitions={columnDefinitions}
-        onChange={({ orderBy, orderDirection }) => setOrder({ by: orderBy, direction: orderDirection })}
-        loadMore={() => {
-          cursorPaginationDefaultLoadMore();
-          tracking?.onLoadMore?.(pagesLoaded(cursor?.offset, retrievalSize));
+        onChange={({ query }) => {
+          onChangeItems(items.filter(item => item.tags.type.toLowerCase().includes(query?.toLowerCase())));
         }}
-        progress={progress}
-        {...tableProps}
-        items={items}
-        fixedLayout
-        orderBy={order.by}
-        orderDirection={order.direction}
+        searchPlaceholder={t('in-infrastructure:explore.search')}
+        leftHeader={
+          <Header
+            totalRetainedItemCount={totalHits}
+            hasErrors={hasErrors}
+            isLoading={isLoading}
+            dataSource="entityType"
+          />
+        }
       />
     </>
   );
 }
 
-function getTableData({ timeConfig, backendQueryModel, order, retrievalSize, cursor }) {
+function getTableData(params) {
+  return getGroupsSubscribeEvent(params);
+}
+
+function getGroupsSubscribeEvent({
+  query = '',
+  page = 1,
+  pageSize = 20,
+  orderBy = 'label',
+  orderDirection = 'ASC',
+  timeConfig,
+  backendQueryModel = {
+    type: 'EXPRESSION',
+    logicalOperator: 'AND',
+    elements: []
+  }
+}) {
   return createGetGroupsSubscription({
+    pagination: {
+      page,
+      pageSize,
+      retrievalSize: 200
+    },
+    order: {
+      by: orderBy,
+      direction: orderDirection
+    },
     filter: {
       tagFilterExpression: backendQueryModel,
+      label: query,
       timeConfig
     },
-    pagination: {
-      retrievalSize,
-      cursor
-    },
-    groupBy: ['type'],
-    type: undefined,
-    order
+    groupBy: ['type']
   });
 }
