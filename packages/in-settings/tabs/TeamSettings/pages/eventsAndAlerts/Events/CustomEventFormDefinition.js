7c08a3923cd799b526e126a8cd92a38205ad49ba
kavyanekkalapu <knekkal@us.ibm.com>
2022-10-17 10:26:58 -0400

    Update CustomEventFormDefinition.js

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/CustomEventFormDefinition.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/CustomEventFormDefinition.js
@@ -152,9 +152,8 @@ export function createEventFormDefinition(eventSpec, isCreate) {
         validator: notBlankValidator
       })
     );
-  // if (mutableEvent && mutableEvent.actionIds) {
+
   form = putActionField(form, mutableEvent.actionIds);
-  // }
 
   if (dataSource !== dataSourceSystem) {
     form = putAllDataSourceFields(form, eventSpec);
