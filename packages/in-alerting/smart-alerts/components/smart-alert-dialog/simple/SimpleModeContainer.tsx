6248619ffb0b2b79ffeb639efffdcbd0c2afac1e
Robert Hostlowsky <robert.hostlowsky@ibm.com>
2022-10-19 21:30:47 +0200

    chore(TS migration): migrate packages/in-alerting/smart-alerts/components/smart-alert-dialog/simple/SimpleModeContainer

--- packages/in-alerting/smart-alerts/components/smart-alert-dialog/simple/SimpleModeContainer.tsx
+++ packages/in-alerting/smart-alerts/components/smart-alert-dialog/simple/SimpleModeContainer.tsx
@@ -6,12 +6,23 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { SmartAlertErrorMessages } from 'in-alerting/smart-alerts/components/smart-alert-dialog/components/SmartAlertErrorMessages';
+import {
+  MessageType,
+  SmartAlertErrorMessages
+} from 'in-alerting/smart-alerts/components/smart-alert-dialog/components/SmartAlertErrorMessages';
+import { StepConfigs } from 'in-components/BlueprintFormMultistep/StepConfigs';
 import StepProgressBar from 'in-components/StepProgressBar';
 
 import locals from 'in-alerting/smart-alerts/components/smart-alert-dialog/simple/SimpleModeContainer.mless';
 
-export default function SimpleModeContainer(props) {
+interface SimpleModeContainerProps {
+  step: number;
+  stepConfigs: StepConfigs;
+  stepRenderers: ((props?: SimpleModeContainerProps) => React.ReactNode)[];
+  messages: MessageType[];
+}
+
+export default function SimpleModeContainer(props: SimpleModeContainerProps) {
   const { stepConfigs, stepRenderers, step, messages } = props;
 
   return (
@@ -35,7 +46,7 @@ export default function SimpleModeContainer(props) {
   );
 }
 
-function mapTitles(stepConfigs) {
+function mapTitles(stepConfigs: StepConfigs) {
   return stepConfigs.map(stepConfig => stepConfig.title);
 }
 
