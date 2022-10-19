3f2c8087e3ad89d537e30295c51fe053a8fff160
Mansoor Pervaiz <mansoor.pervaiz@instana.com>
2022-10-19 03:17:55 -0400

    Replace InfraMetricKpiCard with BigNumberKpiCard

--- packages/in-kubernetes/Dashboards/Node/tabs/Summary.js
+++ packages/in-kubernetes/Dashboards/Node/tabs/Summary.js
@@ -7,26 +7,33 @@ import React, { Fragment } from 'react';
 
 import { Card } from '@instana/components';
 
+// TODO: will use this with TSX
+// import {AggregationType, ResultType} from "@instana/types";
+import { blue } from 'in-custom-dashboards/widgets/BigNumber/comparisonColors';
 import {
   LogsChartInteractionWrapper,
   andQuery,
   kubernetesClusterTagEquals,
   tagEquals
 } from 'in-kubernetes/Dashboards/commonComponents/LogsChartInteractionWrapper';
+import { source } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/metrics';
 import MissingK8sPermissions from 'in-kubernetes/Dashboards/commonComponents/MissingK8sPermissions';
+import { toBackendQueryModel } from 'in-components/QueryBuilder/transformation/backendQueryModel';
 import ConditionsTableCard from 'in-kubernetes/Dashboards/commonComponents/ConditionsTableCard';
 import K8DashboardsMarkerLanes from 'in-kubernetes/Dashboards/K8DashboardsMarkerLanes';
 import { resourceQuotaBytes, resourceQuotaNumber } from 'in-kubernetes/formatters';
 import Chart from 'in-infrastructure/components/InfrastructureMetricChartBehavior';
 import { valueMissingPlaceholder } from 'in-components/valueMissingPlaceholder';
 import { zeroDecimalPlaces, percentage } from 'in-services/formatters/number';
-import InfraMetricKpiCard from 'in-components/KpiCard/InfraMetricKpiCard';
+import BigNumberKpiCard from 'in-components/KpiCard/BigNumberKpiCard';
 import { getNodeDashboard } from 'in-kubernetes/navigation/paths';
 import KpiGridRow from 'in-components/KpiGridRow/KpiGridRow';
 import { formatDuration } from 'in-services/formatters/date';
+import useTimeShiftConfig from 'in-hooks/useTimeShiftConfig';
 import { Row, Col } from 'in-components/layout/Grid';
 import KpiCard from 'in-components/KpiCard/KpiCard';
 import Capitalize from 'in-components/Capitalize';
+import { plugins } from 'in-forge/constants';
 import theme from 'in-themes';
 import { t } from 'in-i18n';
 
@@ -36,6 +43,52 @@ export default function Summary({ timeConfig, data: node }) {
 
   const clusterTag = kubernetesClusterTagEquals(node.clusterId);
   const workloadTag = tagEquals('kubernetes.node.name', node.name);
+  const tagFilterExpression = toBackendQueryModel(andQuery(clusterTag, workloadTag));
+  const timeShift = useTimeShiftConfig();
+  const type = plugins.kubernetesNode;
+
+  const kpiWidth = 2;
+
+  // TODO: Use for tsx
+  // const defaultBigNumberMetricConfig = {
+  //   source,
+  //   aggregation: 'MEAN' as AggregationType,
+  //   tagFilterExpression,
+  //   type,
+  //   timeShift,
+  //   timeConfig,
+  //   resultType: 'SINGLE_NUMBER' as ResultType
+  // };
+  const defaultBigNumberMetricConfig = {
+    source,
+    aggregation: 'MEAN',
+    tagFilterExpression,
+    type,
+    timeShift,
+    timeConfig,
+    resultType: 'SINGLE_NUMBER'
+  };
+
+  // TODO: Use for tsx
+  //  rename to isNodeMetric and change the type maybe
+  // const isContainerMetric = {
+  //   /* use this configuration on containers of this pod (which can be of type docker, containerd or crio)
+  //     type filtering must be disabled and cross series aggregation uses SUM */
+  //   type: undefined,
+  //   crossSeriesAggregation: 'SUM' as AggregationType
+  // };
+
+  const isContainerMetric = {
+    /* use this configuration on containers of this pod (which can be of type docker, containerd or crio)
+      type filtering must be disabled and cross series aggregation uses SUM */
+    type: undefined,
+    crossSeriesAggregation: 'SUM'
+  };
+
+  const comparisonColors = {
+    comparisonDecreaseColor: blue.id,
+    comparisonIncreaseColor: blue.id
+  };
 
   return (
     <Fragment>
@@ -61,46 +114,80 @@ export default function Summary({ timeConfig, data: node }) {
           borderless
         />
       </KpiGridRow>
-
       <Row>
-        <Col lg={2}>
-          <InfraMetricKpiCard
+        <Col lg={kpiWidth}>
+          <BigNumberKpiCard
             title={t('in-kubernetes:dashboards.cpuRequestsAlloc')}
-            snapshotId={snapshotId}
-            metric="required_cpu_percentage"
             formatter={percentage.detailed}
+            config={{
+              metricConfiguration: {
+                metric: 'required_cpu_percentage',
+                ...defaultBigNumberMetricConfig,
+                ...isContainerMetric
+              },
+              ...comparisonColors
+            }}
+            raw
           />
         </Col>
-        <Col lg={2}>
-          <InfraMetricKpiCard
+        <Col lg={kpiWidth}>
+          <BigNumberKpiCard
             title={t('in-kubernetes:dashboards.cpuLimitsAlloc')}
-            snapshotId={snapshotId}
-            metric="limit_cpu_percentage"
             formatter={percentage.detailed}
+            config={{
+              metricConfiguration: {
+                metric: 'limit_cpu_percentage',
+                ...defaultBigNumberMetricConfig,
+                ...isContainerMetric
+              },
+              ...comparisonColors
+            }}
+            raw
           />
         </Col>
-        <Col lg={2}>
-          <InfraMetricKpiCard
+        <Col lg={kpiWidth}>
+          <BigNumberKpiCard
             title={t('in-kubernetes:dashboards.memoryRequestsAlloc')}
-            snapshotId={snapshotId}
-            metric="required_mem_percentage"
             formatter={percentage.detailed}
+            config={{
+              metricConfiguration: {
+                metric: 'required_mem_percentage',
+                ...defaultBigNumberMetricConfig,
+                ...isContainerMetric
+              },
+              ...comparisonColors
+            }}
+            raw
           />
         </Col>
-        <Col lg={2}>
-          <InfraMetricKpiCard
+        <Col lg={kpiWidth}>
+          <BigNumberKpiCard
             title={t('in-kubernetes:dashboards.memoryLimits')}
-            snapshotId={snapshotId}
-            metric="limit_mem_percentage"
             formatter={percentage.detailed}
+            config={{
+              metricConfiguration: {
+                metric: 'limit_mem_percentage',
+                ...defaultBigNumberMetricConfig,
+                ...isContainerMetric
+              },
+              ...comparisonColors
+            }}
+            raw
           />
         </Col>
-        <Col lg={2}>
-          <InfraMetricKpiCard
+        <Col lg={kpiWidth}>
+          <BigNumberKpiCard
             title={t('in-kubernetes:dashboards.podsAlloc')}
-            snapshotId={snapshotId}
-            metric="alloc_pods_percentage"
             formatter={percentage.detailed}
+            config={{
+              metricConfiguration: {
+                metric: 'alloc_pods_percentage',
+                ...defaultBigNumberMetricConfig,
+                ...isContainerMetric
+              },
+              ...comparisonColors
+            }}
+            raw
           />
         </Col>
       </Row>
