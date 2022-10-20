be5d2f4a068ddcf046dc342aaad9213e24b7b6de
Bryson Chisholm <bryson.chisholm@ibm.com>
2022-10-20 10:32:04 -0400

    rename memoizeReferencedEntitiesObservable

---
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Alerts/components/memoizeReferencedEntitiesObservable.ts
@@ -0,0 +1,15 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import memoize from 'in-services/util/memoizingObservableGenerator';
+
+export default function createMemoizedObservableForReferencedEntities(createObservable, tti = 60000) {
+  return memoize(
+    createObservable,
+    // generate cache ID by concatenating all referenced IDs
+    arrayOfIds => (arrayOfIds == null ? 'null' : arrayOfIds.join(':')),
+    tti
+  );
+}
