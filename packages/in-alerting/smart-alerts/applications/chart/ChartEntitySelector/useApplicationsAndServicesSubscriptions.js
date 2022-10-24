Neethu9074/carbon

commit 33c1bcca726e4147132ef914c9f35ec4bda6c78a

add lazy-loading for to reduce number of backend calls in Alert Details view (#10059)
* investigation, and initial fix by adding lazy-loading

* refactor: a more functional approach

* support showing the services for 1 AP

* refactor: there is no need to use React.useMemo for caching isQueryValid

* handling invalid Tagfilter expression

packages/in-alerting/smart-alerts/applications/chart/ChartEntitySelector/useApplicationsAndServicesSubscriptions.js
@@ -3,9 +3,7 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-import { useMemo } from 'react';
-
-import { combineLatest, just } from '@instana/observables';
+import { combineLatest } from '@instana/observables';
 import { useObservable } from '@instana/hooks';
 
 import {
@@ -16,9 +14,7 @@ import useIsTagFilterFormModelValid from 'in-alerting/smart-alerts/applications/
 import { getEntitySelectionAsTagFilterFormModel } from 'in-alerting/smart-alerts/applications/data/entitySelection';
 import { getQueryBuilderForAlertType } from 'in-alerting/smart-alerts/applications/components/AlertQueryBuilder';
 import { joinExpressions } from 'in-components/QueryBuilder/transformation/formModel';
-import { isLoading, hasError, errorWithData, success } from 'in-services/util/result';
 import getApplication from 'in-applications/subscriptions/getApplication';
-import { pendingResult } from 'in-services/fixedObjects';
 
 export default function useApplicationsAndServicesSubscriptions({
   alertConfigWithFormModel,
@@ -34,41 +30,51 @@ export default function useApplicationsAndServicesSubscriptions({
     threshold
   } = alertConfigWithFormModel;
 
+  const { isQueryValid } = getQueryBuilderForAlertType(rule.alertType, threshold.type);
+
   // only pass the user-defined part of the query, because the generated part is valid anyway, and the validation
   // would reject the entity-filter anyway, because the user is not allowed to use them
-  const { isQueryValid } = useMemo(() => {
-    return getQueryBuilderForAlertType(rule.alertType, threshold.type);
-  }, [rule.alertType, threshold.type]);
   const isTagFilterFormModelValid = useIsTagFilterFormModelValid(tagFilterExpression, isQueryValid);
 
-  const fetchAppsAndServices = applicationIds.map(applicationId => {
-    const scopeDownEnrichedTagFilterFormModel = joinExpressions({
+  function scopedDownTagFilterExpression(applicationId) {
+    return joinExpressions({
       expressions: [
         getEnrichedFiltersForApplication(alertConfigWithFormModel, applicationId),
-        getEntitySelectionAsTagFilterFormModel(applications, boundaryScope)
+        getEntitySelectionAsTagFilterFormModel(applications, boundaryScope, applicationId)
       ]
     });
-    return combineLatest([
-      getApplication({ id: applicationId }),
-      isTagFilterFormModelValid
-        ? getServiceList(queryWindowSize, scopeDownEnrichedTagFilterFormModel, includeSynthetic)
-        : just(pendingResult)
-    ]).map(([app, services]) => {
-      if (isLoading(app) || isLoading(services)) return pendingResult;
-      if (hasError(app) || hasError(services)) {
-        return errorWithData([...app.errors, ...services.errors], {
-          app: app.data,
-          services: services.data?.items
-        });
-      }
-      return success({
-        app: app.data,
-        services: services.data?.items
-      });
-    });
-  });
+  }
 
-  return useObservable(combineLatest(fetchAppsAndServices), [
+  const fetchAppsOnly = applicationIds //
+    .map(applicationId =>
+      getApplication({ id: applicationId }) //
+        .map(result => ({
+          ...result,
+          data: {
+            app: result.data,
+            services: () =>
+              getServiceList(queryWindowSize, scopedDownTagFilterExpression(applicationId), includeSynthetic)
+          }
+        }))
+    );
+
+  const fetchAppsServices = applicationIds //
+    .map(applicationId =>
+      getServiceList(queryWindowSize, scopedDownTagFilterExpression(applicationId), includeSynthetic) //
+        .map(result => ({
+          ...result,
+          data: {
+            app: {
+              id: applicationId
+            },
+            services: result?.data?.items ?? []
+          }
+        }))
+    );
+
+  const isOnlyOneAP = applicationIds.length === 1;
+
+  return useObservable(isTagFilterFormModelValid && combineLatest(isOnlyOneAP ? fetchAppsServices : fetchAppsOnly), [
     isTagFilterFormModelValid,
     applications,
     isQueryValid,

packages/in-alerting/smart-alerts/applications/chart/ChartEntitySelector/createOption_test.js
@@ -57,21 +57,21 @@ describe('in-alerting/smart-alerts/applications/chart/ChartEntitySelector/create
       const list = createOptionsList([apListWithOneEntry], [dummyId], false, isSelectServiceLevel, {});
 
       expect(list).toMatchInlineSnapshot(`
-              Array [
-                Object {
-                  "children": Array [
-                    Object {
-                      "appId": "0815",
-                      "icon": "lib_application_service",
-                      "id": "0815",
-                      "label": "testService",
-                      "type": "SERVICE",
-                    },
-                  ],
-                  "label": "Services:",
-                },
-              ]
-          `);
+        Array [
+          Object {
+            "children": Array [
+              Object {
+                "appId": "0815",
+                "icon": "lib_application_service",
+                "id": "0815",
+                "label": "testService",
+                "type": "SERVICE",
+              },
+            ],
+            "label": "Services:",
+          },
+        ]
+      `);
     });
   });
 
@@ -101,12 +101,9 @@ describe('in-alerting/smart-alerts/applications/chart/ChartEntitySelector/create
             "children": Array [
               Object {
                 "appId": "0815",
-                "breadcrumbAndLabel": "0815",
-                "children": Array [],
                 "icon": "lib_application_service",
                 "id": "0815",
                 "label": "testService",
-                "loadChildren": undefined,
                 "type": "SERVICE",
               },
             ],
@@ -137,12 +134,9 @@ describe('in-alerting/smart-alerts/applications/chart/ChartEntitySelector/create
             "children": Array [
               Object {
                 "appId": "0815",
-                "breadcrumbAndLabel": "0815",
-                "children": undefined,
                 "icon": "lib_application_service",
                 "id": "0815",
                 "label": "testService",
-                "loadChildren": [Function],
                 "type": "SERVICE",
               },
             ],

packages/in-alerting/smart-alerts/applications/chart/ChartEntitySelector/createOptions.js
@@ -40,25 +40,35 @@ export function createOptionsList(
     return createAPsList(applicationList);
   }
 
-  const createServicesAndEndpointsList = isSelectServiceLevel
-    ? (app, services) => mapServicesToOptions(app, services)
-    : (app, services) =>
-        mapServicesWithEndpointsToOptions(
-          app,
-          services,
-          timeConfig,
-          boundaryScope,
-          includeSynthetic,
-          tagFilterExpression,
-          applications
-        );
+  const createServicesAndEndpointsListMapper = appId =>
+    isSelectServiceLevel
+      ? servicesResult => ({
+          ...servicesResult,
+          data: {
+            items: mapServicesToOptions(appId, servicesResult?.data?.items)
+          }
+        })
+      : servicesResult => ({
+          ...servicesResult,
+          data: {
+            items: mapServicesWithEndpointsToOptions(
+              appId,
+              servicesResult?.data?.items,
+              timeConfig,
+              boundaryScope,
+              includeSynthetic,
+              tagFilterExpression,
+              applications
+            )
+          }
+        });
 
   if (applicationIds.length === 1) {
     const { app, services } = applicationList.map(({ data }) => data)[0];
     return [
       {
         label: t('in-alerting:smartAlerts.applications.chart.entitySelection.services'),
-        children: createServicesAndEndpointsList(app, services)
+        children: mapServicesToOptions(app.id, services)
       }
     ];
   }
@@ -75,7 +85,7 @@ export function createOptionsList(
           label: app.label,
           icon: 'lib_application',
           type: 'APPLICATION',
-          children: createServicesAndEndpointsList(app, services)
+          loadChildren: () => services().map(createServicesAndEndpointsListMapper(app.id))
         }))
     }
   ];
@@ -96,21 +106,20 @@ export function createAPsList(applicationList) {
     });
 }
 
-function mapServicesToOptions(app, services) {
-  return (services ?? []) //
-    .map(({ service }) => ({
-      appId: app.id,
-      id: service.id,
-      icon: 'lib_application_service',
-      label: service.label,
-      type: 'SERVICE'
-    }));
+function mapServicesToOptions(appId, services) {
+  return (services ?? []).map(({ service }) => ({
+    appId,
+    id: service.id,
+    icon: 'lib_application_service',
+    label: service.label,
+    type: 'SERVICE'
+  }));
 }
 
 const hasNoEndpoints = metrics => metrics?.endpoints?.[0]?.[1] === 0;
 
 function mapServicesWithEndpointsToOptions(
-  app,
+  appId,
   services,
   timeConfig,
   boundaryScope,
@@ -118,46 +127,48 @@ function mapServicesWithEndpointsToOptions(
   tagFilterExpression,
   applications
 ) {
-  return (services ?? []) //
-    .map(({ service, metrics }) => ({
-      appId: app.id,
-      id: service.id,
-      breadcrumbAndLabel: service.id, // used as a header above endpoints-list
-      icon: 'lib_application_service',
-      label: service.label,
-      type: 'SERVICE',
-      children: hasNoEndpoints(metrics) ? [] : undefined, // undefined will be replaced in case list will have been fetched
-      loadChildren: hasNoEndpoints(metrics)
-        ? undefined // optimisation, when we already know it has no endpoints
-        : () =>
-            fetchEndpoints({
-              applicationId: app.id,
-              serviceId: service.id,
-              boundaryScope,
-              tagFilterFormModel: tagFilterExpression,
-              applications,
-              timeConfig,
-              includeSynthetic
-            }) //
-              .map(({ data, progress }) => ({
-                data: {
-                  items: mapEndpointItemsToOptions(app, service, data?.items)
-                },
-                progress
-              }))
+  function loadServiceEndpoints(service) {
+    return fetchEndpoints({
+      applicationId: appId,
+      serviceId: service.id,
+      boundaryScope,
+      tagFilterFormModel: tagFilterExpression,
+      applications,
+      timeConfig,
+      includeSynthetic
+    }).map(({ data, progress }) => ({
+      data: {
+        items: mapEndpointItemsToOptions(appId, service, data?.items)
+      },
+      progress
     }));
+  }
+
+  const serviceWithEndpointMetricsMapper = ({ service, metrics }) => ({
+    appId,
+    id: service.id,
+    breadcrumbAndLabel: service.id, // used as a header above endpoints-list
+    icon: 'lib_application_service',
+    label: service.label,
+    type: 'SERVICE',
+    children: hasNoEndpoints(metrics) ? [] : undefined, // undefined will be replaced in case list will have been fetched
+    loadChildren: hasNoEndpoints(metrics)
+      ? undefined // optimisation, when we already know it has no endpoints
+      : () => loadServiceEndpoints(service)
+  });
+
+  return (services ?? []).map(serviceWithEndpointMetricsMapper);
 }
 
-function mapEndpointItemsToOptions(app, service, itemsWithEndpoints) {
-  return (itemsWithEndpoints ?? []) //
-    .map(({ endpoint }) => ({
-      serviceId: service.id,
-      appId: app.id,
-      label: endpoint.label,
-      id: endpoint.id,
-      type: 'ENDPOINT',
-      icon: 'lib_application_endpoint'
-    }));
+function mapEndpointItemsToOptions(appId, service, itemsWithEndpoints) {
+  return (itemsWithEndpoints ?? []).map(({ endpoint }) => ({
+    serviceId: service.id,
+    appId,
+    label: endpoint.label,
+    id: endpoint.id,
+    type: 'ENDPOINT',
+    icon: 'lib_application_endpoint'
+  }));
 }
 
 export const loadingOptions = [

commit 2d405905c0b1caa6e08260884b1df893b304bd8c

Fix AP SA dialog crashing in a state of an invalid/partial QB query (#10380)
bug was introduced in #10059 because we are trying to convert an invalid/incomplete
tag-filters form model to a backend model since that change.

packages/in-alerting/smart-alerts/applications/chart/ChartEntitySelector/useApplicationsAndServicesSubscriptions.js
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-import { combineLatest } from '@instana/observables';
+import { combineLatest, just } from '@instana/observables';
 import { useObservable } from '@instana/hooks';
 
 import {
@@ -15,6 +15,7 @@ import { getEntitySelectionAsTagFilterFormModel } from 'in-alerting/smart-alerts
 import { getQueryBuilderForAlertType } from 'in-alerting/smart-alerts/applications/components/AlertQueryBuilder';
 import { joinExpressions } from 'in-components/QueryBuilder/transformation/formModel';
 import getApplication from 'in-applications/subscriptions/getApplication';
+import { pendingResult } from 'in-services/fixedObjects';
 
 export default function useApplicationsAndServicesSubscriptions({
   alertConfigWithFormModel,
@@ -45,32 +46,33 @@ export default function useApplicationsAndServicesSubscriptions({
     });
   }
 
-  const fetchAppsOnly = applicationIds //
-    .map(applicationId =>
-      getApplication({ id: applicationId }) //
-        .map(result => ({
-          ...result,
-          data: {
-            app: result.data,
-            services: () =>
-              getServiceList(queryWindowSize, scopedDownTagFilterExpression(applicationId), includeSynthetic)
-          }
-        }))
-    );
+  const getServiceListIfValid = applicationId => {
+    return isTagFilterFormModelValid
+      ? getServiceList(queryWindowSize, scopedDownTagFilterExpression(applicationId), includeSynthetic)
+      : just(pendingResult);
+  };
 
-  const fetchAppsServices = applicationIds //
-    .map(applicationId =>
-      getServiceList(queryWindowSize, scopedDownTagFilterExpression(applicationId), includeSynthetic) //
-        .map(result => ({
-          ...result,
-          data: {
-            app: {
-              id: applicationId
-            },
-            services: result?.data?.items ?? []
-          }
-        }))
-    );
+  const fetchAppsOnly = applicationIds.map(applicationId =>
+    getApplication({ id: applicationId }).map(result => ({
+      ...result,
+      data: {
+        app: result.data,
+        services: () => getServiceListIfValid(applicationId)
+      }
+    }))
+  );
+
+  const fetchAppsServices = applicationIds.map(applicationId =>
+    getServiceListIfValid(applicationId).map(result => ({
+      ...result,
+      data: {
+        app: {
+          id: applicationId
+        },
+        services: result?.data?.items ?? []
+      }
+    }))
+  );
 
   const isOnlyOneAP = applicationIds.length === 1;
 