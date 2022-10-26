21bc37a742702004d89f2c95ef26d7e940f6abd5
kavyanekkalapu <knekkal@us.ibm.com>
2022-10-26 15:12:22 -0400

    Update AssociatedActions.tsx

--- packages/in-events/components/AutomationActions/AssociatedActions.tsx
+++ packages/in-events/components/AutomationActions/AssociatedActions.tsx
@@ -4,15 +4,20 @@
  * Copyright IBM Corp. 2022
  */
 
+import { filter } from 'lodash';
 import React from 'react';
 
+import { Observable } from '@instana/observables';
 import { useObservable } from '@instana/hooks';
 import { Button } from '@instana/components';
 
+import createMemoizedObservableForReferencedEntities from 'in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Alerts/components/memoizeReferencedEntitiesObservable';
 import ActionAssociationDialogWrapper from 'in-events/components/AutomationActions/action_associations_dialog/ActionAssociationDialogWrapper';
 import ActionTable from 'in-settings/tabs/TeamSettings/pages/automation/ActionCatalog/ActionTable';
 import { getCustomEventActions, getBuiltinEventActions } from 'in-api/eventSpecifications';
 import { addActiveDialog, close } from 'in-components/DialogPresenter/store';
+import { getAllActionsWithAISuggestions } from 'in-api/automation';
+import { alwaysEmptyArray } from 'in-services/fixedStreams';
 import { Event, VolatileId, Action } from 'in-types';
 import { t } from 'in-i18n';
 
@@ -33,6 +38,21 @@ export default function AssociatedActions({ event, volatileId }: Props) {
   const observable = isCustom ? getCustomEventActions : getBuiltinEventActions;
   const actions =
     useObservable<Action[], [string]>(() => observable(eventSpecificationId), [eventSpecificationId]) ?? [];
+  const selectedActions: string[] = actions.map(action => action.id);
+  const eventName: string | undefined = event?.problem?.problemText;
+  const eventDescription: string | undefined = event?.problem?.fixSuggestion;
+
+  const getSelectedActionsForEvent = createMemoizedObservableForReferencedEntities(function(selectedActions: string[]) {
+    if (selectedActions.length === 0) {
+      return (alwaysEmptyArray as unknown) as Observable<Action[]>;
+    }
+    // null is treated as a pending result when converting the HTTP response into a result
+    return getAllActionsWithAISuggestions(eventName, eventDescription).map(action =>
+      filter(action, function(app) {
+        return selectedActions.indexOf(app.id) >= 0;
+      })
+    );
+  });
 
   return (
     <div>
@@ -43,7 +63,7 @@ export default function AssociatedActions({ event, volatileId }: Props) {
         event={event}
         rightHeader={<RightHeader event={event} actions={actions} isCustom={isCustom} />}
         volatileId={volatileId}
-        loadEntities={() => observable(eventSpecificationId)}
+        loadEntities={() => getSelectedActionsForEvent(selectedActions)}
         scored
       />
     </div>
