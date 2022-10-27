2ee82396c47a209a8b7d480eb0d36a34fbd3c319
Jonathan Kim <Jonathan.Kim1@ibm.com>
2022-10-27 11:11:13 -0700

    added timeshift for daemonset

--- packages/in-kubernetes/Dashboards/DaemonSet/tabs/index.js
+++ packages/in-kubernetes/Dashboards/DaemonSet/tabs/index.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 
+import SummaryWithoutTimeShift from 'in-kubernetes/Dashboards/DaemonSet/tabs/SummaryWithoutTimeShift';
 import { EventsWithoutNamespace } from 'in-kubernetes/Dashboards/commonComponents/commonTabs/Events';
 import Services from 'in-kubernetes/Dashboards/commonComponents/commonTabs/Services';
 import { daemonSetDashboardFullyQualified } from 'in-kubernetes/navigation/paths';
@@ -13,13 +14,14 @@ import { WorkloadTab } from 'in-kubernetes/Dashboards/commonComponents/Tabs';
 import Pods from 'in-kubernetes/Dashboards/commonComponents/commonTabs/Pods';
 import Summary from 'in-kubernetes/Dashboards/DaemonSet/tabs/Summary';
 import Details from 'in-kubernetes/Dashboards/DaemonSet/tabs/Details';
+import { k8sTimeShiftEnabled } from 'in-services/featureFlags';
 import { t } from 'in-i18n';
 
 export default [
   {
     label: t('in-kubernetes:dashboards.summary'),
     path: `${daemonSetDashboardFullyQualified}/summary`,
-    component: Summary
+    component: k8sTimeShiftEnabled ? Summary : SummaryWithoutTimeShift
   },
   {
     label: t('in-kubernetes:dashboards.details'),
