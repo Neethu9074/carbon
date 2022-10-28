d061aaca6eb2393c3212906a610bbd71cc6102b6
Lucas De Carvalho Bueno Santos <Lucas.Santos@ibm.com>
2022-10-28 10:29:33 +0200

    Add new internal component Delete with tootltip (#10353)
    * Add new internal component Delete with tootltip
    
    * Add necessary translations and remove duplicates from in-settings/i18n/en-US.json
    
    * Create a component for the Delete Icon with Tooltip
    
    * Add tooltip functionality to default and owner groups
    
    * Change API of the Delete component
    
    * Write tests for the Delete component
    
    * Adjust Delete component to return the correct function
    
    * Adjust Delete_test to incorporate doDelete logic
    
    * refactor DeleteIcon
    
    * change Groups to use i18n context
    
    * Add data-testid attributes for tests
    
    * Rewrite Delete_test to use react-testing-library
    
    * add a comment explaining the test in Delete_test
    
    * Fix tests using uncleared mocks in Delete_test
    
    * Remove unnecessary spans for dialogMessage
    
    Co-authored-by: Andrei Karas <Andrei.Karas@ibm.com>

---
+++ packages/in-settings/components/ApiList/sharedComponents/Delete/index.tsx
@@ -0,0 +1,9 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2022
+ */
+
+import { Delete } from 'in-settings/components/ApiList/sharedComponents/Delete/Delete';
+
+export default Delete;
