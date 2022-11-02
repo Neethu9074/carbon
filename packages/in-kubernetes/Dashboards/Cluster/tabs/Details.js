7ee28fb096808a8569ea1c93de6ed65eb5cdd09a
Nathan Fisher <Nathan.Fisher@ibm.com>
2022-11-02 12:21:54 -0400

    Add node coverage to cluster detail (#10456)

--- packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
+++ packages/in-kubernetes/Dashboards/Cluster/tabs/Details.js
@@ -5,21 +5,33 @@
 
 import React, { Fragment } from 'react';
 
+import { useObservable } from '@instana/hooks';
+
+import getKubernetesClusterItemCounters from 'in-kubernetes/subscriptions/getKubernetesClusterItemCounters';
 import ComponentStatusTable from 'in-kubernetes/Dashboards/Cluster/tabs/ComponentStatusTable';
 import Debugging from 'in-kubernetes/Dashboards/Cluster/tabs/Debugging';
+import { pendingResult } from 'in-services/fixedObjects';
 import { Row, Col } from 'in-components/layout/Grid';
 
-export default function Details({ data: cluster }) {
+export default function Details({ data: cluster, timeConfig }) {
+  const clusterId = cluster.id;
+  const counters =
+    useObservable(
+      getKubernetesClusterItemCounters({ clusterId, timeConfig }),
+      Object.values({ clusterId, timeConfig })
+    ) ?? pendingResult;
+
   return (
     <Fragment>
       <Row>
         <Col lg={12}>
-          <ComponentStatusTable cluster={cluster} />
+          <Debugging cluster={cluster} counters={counters?.data} />
         </Col>
       </Row>
+
       <Row>
         <Col lg={12}>
-          <Debugging cluster={cluster} />
+          <ComponentStatusTable cluster={cluster} />
         </Col>
       </Row>
     </Fragment>
