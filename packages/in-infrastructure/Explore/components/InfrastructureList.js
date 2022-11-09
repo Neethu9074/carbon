bbd4c1af3bfb0eda564aa3fe47ee5d2de71da3e5
itai <itai.wiseman@ibm.com>
2022-11-08 23:14:41 -0500

    adding spark chart to EE infrastructure list

--- packages/in-infrastructure/Explore/components/InfrastructureList.js
+++ packages/in-infrastructure/Explore/components/InfrastructureList.js
@@ -21,6 +21,7 @@ import EntityLink from 'in-components/EntityLink/EntityLink';
 import { isTechnicalError } from 'in-services/util/error';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 import { mapData } from 'in-services/util/result';
+import SparkChart from 'in-components/SparkChart';
 import { noop } from 'in-services/util/function';
 import { t } from 'in-i18n';
 
@@ -63,7 +64,7 @@ export default function InfrastructureList({
 
   const columnDefinitions = [
     getLabelColumn({ timeConfig }, tracking?.onNavigateToEntity),
-    ...getMetricColumns({ metrics, sortable: showHeader, metricMetadatas })
+    ...getMetricColumns({ metrics, sortable: showHeader, metricMetadatas, timeConfig })
   ];
 
   return (
@@ -192,12 +193,11 @@ InfrastructureList.propTypes = {
   metricCatalog: rpt.object
 };
 
-function getMetricColumns({ metrics, sortable, metricMetadatas }) {
+function getMetricColumns({ metrics, sortable, metricMetadatas, timeConfig }) {
   return metrics.map(({ metric, aggregation }) => {
     const id = getMetricKey(metric, aggregation);
     const metadata = mapData(metricMetadatas, data => data[metric]);
     const label = mapData(metadata, data => data?.label);
-    const formatter = mapData(metadata, data => data?.formatter).data;
     const isKpi = mapData(metadata, data => data?.isKpi).data || false;
     return {
       id,
@@ -212,11 +212,26 @@ function getMetricColumns({ metrics, sortable, metricMetadatas }) {
       defaultDisabled: !isKpi,
       headCellProps: { className: locals.metricLabel },
       getContent(item) {
-        if (metric === 'count') {
-          return <span>1</span>;
-        }
+        const id = getMetricKey(metric, aggregation);
+        const metadata = mapData(metricMetadatas, data => data[metric]);
+        const label = mapData(metadata, data => data?.label);
+        const renderedLabel = <MetricLabel label={label} aggregation={aggregation} />;
+        const formatter = mapData(metadata, data => data?.formatter).data;
         const kpi = average(item.metrics[id]);
-        return <span>{(kpi && formatter && formatter(kpi)) || '--'}</span>;
+        const percentageMetric = mapData(metadata, data => data?.percentageMetric).data;
+        const granularity = getGranularity(timeConfig);
+        return (
+          <SparkChart
+            horizontalMetricValue={(kpi && formatter && formatter(kpi)) || '--'}
+            percentageMetric={percentageMetric}
+            metrics={item.metrics[id]}
+            tooltipFormatter={formatter}
+            aggregation={aggregation}
+            timeConfig={timeConfig}
+            rollup={granularity}
+            label={renderedLabel}
+          />
+        );
       }
     };
   });
