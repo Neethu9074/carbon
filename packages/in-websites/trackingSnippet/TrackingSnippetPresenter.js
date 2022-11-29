e33134b8627333eda09b29e4b1da2aa842c65374
Bincy Daniel <Bincy.Daniel@ibm.com>
2022-11-29 17:11:51 +0530

    Track Session will be set by clicking either the checkbox or the label

--- packages/in-websites/trackingSnippet/TrackingSnippetPresenter.js
+++ packages/in-websites/trackingSnippet/TrackingSnippetPresenter.js
@@ -9,7 +9,6 @@ import { SvgIcon, Link } from '@instana/components';
 
 import { getTrackingSnippet } from 'in-websites/trackingSnippet';
 import CheckboxFancy from 'in-components/form/CheckboxFancy';
-import Label from 'in-components/form/Label';
 import Tooltip from 'in-components/Tooltip';
 import Code from 'in-components/Code';
 import { t } from 'in-i18n';
@@ -26,20 +25,22 @@ export default function TrackingSnippetPresenter({ websiteId, trackSessions, set
           <CheckboxFancy
             id="trackSessions"
             checked={trackSessions}
+            label={
+              <div className={locals.label}>
+                {t('in-websites:trackingSnippet.trackingSnippetPresenterLabelTrackSessions')}&nbsp;
+                <Tooltip content={t('in-websites:trackingSnippet.trackingSnippetPresenterTooltip')}>
+                  <Link
+                    href="https://www.ibm.com/docs/en/obi/current?topic=websites-javascript-agent-api#session-tracking"
+                    external
+                    className={locals.helpWrapper}
+                  >
+                    <SvgIcon type="lib_help_error_help_outline" size="xs" className={locals.help} />
+                  </Link>
+                </Tooltip>
+              </div>
+            }
             onChange={e => setTrackSessions(e.target.checked)}
           />
-          <Label htmlFor="trackSessions" className={locals.label}>
-            {t('in-websites:trackingSnippet.trackingSnippetPresenterLabelTrackSessions')}&nbsp;
-            <Tooltip content={t('in-websites:trackingSnippet.trackingSnippetPresenterTooltip')}>
-              <Link
-                href="https://www.ibm.com/docs/en/obi/current?topic=websites-javascript-agent-api#session-tracking"
-                external
-                className={locals.helpWrapper}
-              >
-                <SvgIcon type="lib_help_error_help_outline" size="xs" className={locals.help} />
-              </Link>
-            </Tooltip>
-          </Label>
         </div>
       </div>
       <div className={locals.snippet}>
