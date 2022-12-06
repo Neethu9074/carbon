eadf8a907084d237747becd36d26c32b6a777495
Robert Hostlowsky <robert.hostlowsky@ibm.com>
2022-12-06 16:12:53 +0100

    feat-flag websiteSmartAlertsAdaptiveBaselineEnabled

--- packages/in-alerting/smart-alerts/websites/advanced/AdvancedModeContainer.js
+++ packages/in-alerting/smart-alerts/websites/advanced/AdvancedModeContainer.js
@@ -32,7 +32,7 @@ import TimeThresholdConfig from 'in-alerting/smart-alerts/websites/advanced/Time
 import { onThresholdTypeChange } from 'in-alerting/smart-alerts/websites/form/thresholdTypeForm';
 import { ThresholdSection } from 'in-alerting/smart-alerts/websites/advanced/ThresholdSection';
 import { getBlueprintConfig } from 'in-alerting/smart-alerts/websites/data/blueprintConfig';
-import { websiteAdaptiveBaselineSmartAlertsEnabled } from 'in-services/featureFlags';
+import { websiteSmartAlertsAdaptiveBaselineEnabled } from 'in-services/featureFlags';
 import { HISTORIC_BASELINE } from 'in-alerting/smart-alerts/data/thresholdTypes';
 import LightCard from 'in-alerting/components/LightCard/LightCard';
 import StepsContainer from 'in-components/StepsContainer';
@@ -84,7 +84,7 @@ export default function AdvancedModeContainer(props) {
                 updateForm={updateForm}
                 setSliderState={setSliderState}
               />
-              {websiteAdaptiveBaselineSmartAlertsEnabled && blueprintConfig?.baselineEnabled && (
+              {websiteSmartAlertsAdaptiveBaselineEnabled && blueprintConfig?.baselineEnabled && (
                 <LightCard
                   title={t(
                     'in-alerting:smartAlerts.applications.advanced.advancedModeContainer.threshold.staticOrAdaptiveTitle'
