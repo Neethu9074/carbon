d668b0bbc00e8ada87001789a92ce123f64c2035
Shivangi Mehra <shivangi.mehra@ibm.com>
2022-12-07 11:00:51 +0530

    Removal of duplicates

--- packages/in-openstack/Dashboards/Instances/tabs/Summary.js
+++ packages/in-openstack/Dashboards/Instances/tabs/Summary.js
@@ -26,16 +26,15 @@ export default function Summary({ timeConfig, data: instance }) {
     <Fragment>
       <KpiGridRow sizes={[4, 4, 4]}>
         <KpiCard title={t('in-openstack:name')} value={instance.name} raw borderless />
-        <KpiCard title={t('in-openstack:id')} value={instance.id} raw borderless />
         <KpiCard title={t('in-openstack:hostName')} value={instance.openstackItem.host} raw borderless />
-      </KpiGridRow>
-      <KpiGridRow sizes={[2, 2, 2, 2, 2, 2]}>
         <KpiCard
           title={t('in-openstack:availabilityZone')}
           value={instance.openstackItem.availabilityZone}
           raw
           borderless
         />
+      </KpiGridRow>
+      <KpiGridRow sizes={[2, 2, 2, 2, 2]}>
         <KpiCard title={t('in-openstack:hostIP')} value={instance.openstackItem.hostIP} raw borderless />
         <KpiCard title={t('in-openstack:flavorName')} value={instance.openstackItem.flavor} raw borderless />
         <KpiCard title={t('in-openstack:imageName')} value={instance.openstackItem.image} raw borderless />
