b8264f56c3e74d83bf6191aeb9752092b46faaaf
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2023-01-05 11:55:33 -0500

    replace recompose

--- packages/in-profiling/analyze/AnalyzeView/AnalyzeView.js
+++ packages/in-profiling/analyze/AnalyzeView/AnalyzeView.js
@@ -3,7 +3,6 @@
  * (c) Copyright Instana Inc.
  */
 
-import { compose, withProps } from 'recompose';
 import React from 'react';
 
 import ProfiledProcesses from 'in-profiling/analyze/AnalyzeView/ProfiledProcesses/ProfiledProcesses';
@@ -13,30 +12,30 @@ import EmptyAnalyzeView from 'in-profiling/analyze/AnalyzeView/EmptyAnalyzeView'
 import WithEmptyStateFallback from 'in-components/WithEmptyStateFallback';
 import { getTimeConfig } from 'in-stores/time/config';
 import SetBodyColor from 'in-components/SetBodyColor';
-import withUrlState from 'in-hoc/withUrlState';
+import useUrlState from 'in-hooks/useUrlState';
 import Footer from 'in-components/Footer';
 
-export default compose(
-  withUrlState({
-    bind: [analyzeDataSourceUrlParameter],
-    reducerName: 'onChange'
-  }),
-  withProps(({ location }) => ({
-    timeConfig: getTimeConfig(location)
-  }))
-)(props => (
-  <WithEmptyStateFallback
-    center={false}
-    getHasDataToRender={() => getHasDataToRender(props)}
-    FallbackComponent={EmptyAnalyzeView}
-    type="Profiles"
-  >
-    <ProfiledProcesses {...props} />
-    <SetBodyColor color="#fff" />
-    <Footer />
-  </WithEmptyStateFallback>
-));
+export default function AnalyzeView(props) {
+  const urlStateConfig = {
+    bind: [analyzeDataSourceUrlParameter]
+  };
+  const { location } = props;
+  const [{ dataSource }, onChange] = useUrlState(urlStateConfig);
 
+  return (
+    <WithEmptyStateFallback
+      center={false}
+      getHasDataToRender={() => getHasDataToRender(getTimeConfig(location))}
+      FallbackComponent={EmptyAnalyzeView}
+      type="Profiles"
+    >
+      <ProfiledProcesses {...props} timeConfig={getTimeConfig(location)} dataSource={dataSource} onChange={onChange} />
+      <SetBodyColor color="#fff" />
+      <Footer />
+    </WithEmptyStateFallback>
+  );
+}
+// );
 function getHasDataToRender({ timeConfig }) {
   return getProfiledProcessesAvailable({ timeConfig }).map(
     result => !result.data || (result.data && result.data.containsProfiledProcesses)
