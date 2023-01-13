f71556e4220d16170aa954a0dff3d01e0f7034e9
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2023-01-13 12:59:05 +0100

    Update packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/CustomEventForm.js

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/CustomEventForm.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/Events/CustomEventForm.js
@@ -74,7 +74,7 @@ export default function CustomEventForm({
   }, [form]);
 
   const customMetricsForPlugin = useObservable(() => {
-    if (entityType) {
+    if (isCustomDataSourceSelected(form) && entityType) {```
       return getCustomMetricsOptionsForPluginObservable(entityType);
     }
   }, [entityType]);
