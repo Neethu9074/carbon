93abb6ec4ce44b8a15384a692b88041064ff8a34
YONG HU SUN <sunyongh@cn.ibm.com>
2023-01-13 12:58:09 +0800

    display destination kind

--- packages/in-forge/tracing/ibm-mq/IbmmqSpanDetailView.js
+++ packages/in-forge/tracing/ibm-mq/IbmmqSpanDetailView.js
@@ -10,10 +10,12 @@ import { Dl, Di } from 'in-components/HorizontalDescriptionList';
 import { t } from 'in-i18n';
 
 export default function IbmmqSpanDetailView({ span }) {
+  const destinationKind = span.getIn(['data', 'ibm-mq', 'destination', 'kind']);
   return (
     <div>
       <Dl>
         <Di title={t('in-forge:tracing.ibmmq.titleDestination')}>{span.getIn(['data', 'ibm-mq', 'queue'])}</Di>
+        {destinationKind != null && <Di title={t('in-forge:tracing.ibmmq.titleDestinationKind')}>{destinationKind}</Di>}
         <Di title={t('in-forge:tracing.ibmmq.titleMsgOperation')}>{span.getIn(['data', 'sdk', 'custom', 'tags', 'spanType'])}</Di>
         <ErrorDescriptionItem error={span.getIn(['data', 'sdk', 'custom', 'tags', 'error'])} />
       </Dl>
