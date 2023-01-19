6b1d93212e5c2c96c7570b234c8cbb439d273362
Bryson Chisholm <bryson.chisholm@ibm.com>
2023-01-19 16:30:05 -0500

    fix tests

--- packages/in-settings/tabs/TeamSettings/pages/automation/ActionCatalog/ParameterDialog.tsx
+++ packages/in-settings/tabs/TeamSettings/pages/automation/ActionCatalog/ParameterDialog.tsx
@@ -164,14 +164,6 @@ const MetaDataSection = ({ parameter, parameterForm, setParameterForm }: Section
               }
             />
           </Col>
-          {/* <Col>
-            <CheckboxFancy
-              asRadioButton
-              checked={type.value === 'dynamic'}
-              label={t('in-settings:tabs.dynamic')}
-              onChange={() => onParameterChange({ fieldName: 'type', value: 'dynamic', setParameterForm, parameter })}
-            />
-          </Col> */}
           <Col>
             <CheckboxFancy
               asRadioButton
