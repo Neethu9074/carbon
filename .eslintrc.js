c0868b7921281871fa85121404e6f3e87b1db4f8
Jonathan Kim <Jonathan.Kim1@ibm.com>
2023-01-23 10:13:15 -0700

    fixing require issue by adding /* global require:false */

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,7 @@ module.exports = {
       configFile: __dirname + '/babel.config.js'
     }
   },
-  env: {
-    browser: true,
-    node: true
-  },
+
   extends: ['@instana/eslint-config-commons', 'eslint:recommended', 'prettier', 'plugin:react-hooks/recommended'],
 
   plugins: ['react', 'jest', 'babel', 'header', 'import'],
