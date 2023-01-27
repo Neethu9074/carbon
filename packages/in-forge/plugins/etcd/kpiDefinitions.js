f864af40904e098e44d45e481fb96821901fc52a
Albert Chung <alchung@us.ibm.com>
2023-01-27 04:08:14 -0500

    Return etcd nodes when etcd is clicked on entity explorer (#10992)
    * Return etcd nodes when etcd is clicked on entity explorer
    
    * Remove plugins.etcd.receivedRequests & plugins.etcd.receivedBytesSecond
    
    Co-authored-by: Albert Chung <albertchung@Alberts-MacBook-Pro-2.local>

--- packages/in-forge/plugins/etcd/kpiDefinitions.js
+++ packages/in-forge/plugins/etcd/kpiDefinitions.js
@@ -3,18 +3,4 @@
  * (c) Copyright Instana Inc.
  */
 
-import { bytesZeroDecimalPlaces, zeroDecimalPlaces } from 'in-services/formatters/number';
-import { t } from 'in-i18n';
-
-export default [
-  {
-    label: t('in-forge:plugins.etcd.receivedRequests'),
-    metric: 'requests_received',
-    formatter: zeroDecimalPlaces
-  },
-  {
-    label: t('in-forge:plugins.etcd.receivedBytesSecond'),
-    metric: 'bytes_per_sec_received',
-    formatter: bytesZeroDecimalPlaces
-  }
-];
+export default [];
