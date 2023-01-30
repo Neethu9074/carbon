905d0a546eac4fe0560869603d1a63537df7ee18
Albert Chung <albertchung@Alberts-MacBook-Pro-2.local>
2023-01-30 09:19:38 -0500

    Remove default metrics for google PubSub, topic and RunServiceRevision

--- packages/in-forge/plugins/googleCloudRunServiceRevision/kpiDefinitions.js
+++ packages/in-forge/plugins/googleCloudRunServiceRevision/kpiDefinitions.js
@@ -3,18 +3,4 @@
  * (c) Copyright Instana Inc.
  */
 
-import { millis, number } from 'in-services/formatters/number';
-import { t } from 'in-i18n';
-
-export default [
-  {
-    label: t('in-forge:plugins.googleCloudRunServiceRevision.requestCount'),
-    metric: 'request_count',
-    formatter: number.compact
-  },
-  {
-    label: t('in-forge:plugins.googleCloudRunServiceRevision.requestLatencyP99'),
-    metric: 'request_latencies_p99',
-    formatter: millis.compact
-  }
-];
+export default [];
