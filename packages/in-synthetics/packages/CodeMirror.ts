f13ee6820256b1fd58678a679e77f43decf50bbe
Sandra S <Sandra.S@ibm.com>
2023-02-03 20:11:27 +0530

    Fix for unexpected shortcuts popup getting popped up on typing '/' (#11063)

--- packages/in-synthetics/packages/CodeMirror.ts
+++ packages/in-synthetics/packages/CodeMirror.ts
@@ -5,9 +5,10 @@
  */
 
 import { ReactCodeMirrorProps, useCodeMirror } from '@uiw/react-codemirror';
+import * as events from '@uiw/codemirror-extensions-events';
 import { javascript } from '@codemirror/lang-javascript';
 import { bbedit } from '@uiw/codemirror-theme-bbedit';
 import { EditorView } from '@codemirror/view';
 
 export type CodeProps = ReactCodeMirrorProps;
-export { javascript, bbedit, useCodeMirror, EditorView };
+export { javascript, bbedit, useCodeMirror, EditorView, events };
