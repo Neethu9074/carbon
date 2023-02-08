64e7dc79675b43acfc5bb0f0d8007b2326010be9
Pradosh E <Pradosh.E@ibm.com>
2023-02-08 23:48:27 +0530

    Refactoring : Converting to typescript (#11125)
    * extract ScopeColumn and start migrating to TS
    
    * converting to type script - refactoring

--- packages/in-alerting/smart-alerts/websites/Alerts.js
+++ packages/in-alerting/smart-alerts/websites/Alerts.js
@@ -3,11 +3,8 @@
  * (c) Copyright Instana Inc.
  */
 
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-
-import { SvgIcon } from '@instana/components';
 
 import {
   websitesAlertingListAlertResumed,
@@ -23,29 +20,24 @@ import {
 import { alertCreated as alertCreatedMatrixParam, alertId as alertIdMatrixParam } from 'in-websites/navigation/matrix';
 import { humanReadableThresholdOperator } from 'in-alerting/smart-alerts/components/dialog/advanced/thresholdFormData';
 import { STATIC_THRESHOLD, ADAPTIVE_BASELINE, HISTORIC_BASELINE } from 'in-alerting/smart-alerts/data/thresholdTypes';
-import { getQueryBuilderForBeaconType } from 'in-alerting/smart-alerts/websites/components/AlertQueryBuilder';
-import { getLimitedNumberOfFilters, getFiltersCount } from 'in-alerting/smart-alerts/websites/limitedFilters';
 import { getBlueprintConfig } from 'in-alerting/smart-alerts/websites/data/blueprintConfig';
 import { getAggregationText } from 'in-alerting/smart-alerts/components/utils/formUtils';
 import { alertsTab, alertsTabDetailsFullyQualified } from 'in-websites/navigation/paths';
-import { fromBackendModel } from 'in-components/QueryBuilder/transformation/formModel';
 import AlertBaseList from 'in-alerting/smart-alerts/components/AlertsBaseList';
+import ScopeColumn from 'in-alerting/smart-alerts/websites/list/ScopeColumn';
 import { DAILY } from 'in-alerting/smart-alerts/data/seasonalities';
 import { setOrDeleteMatrixKey } from 'in-stores/navigation/matrix';
 import { mutateUrl } from 'in-stores/navigation/navigation';
-import Tooltip from 'in-components/Tooltip/Tooltip';
 import Footer from 'in-components/Footer/Footer';
 import { role } from 'in-stores/user';
 import { t, Trans } from 'in-i18n';
-
-import locals from './Alerts.mless';
 
 function getColumnDefinitions(websiteLabel) {
   return [
     {
       id: 'filters',
       label: t('in-websites:websiteDashboard.tabs.alerts.alertsLabelFilters'),
-      getContent: entity => getFiltersContent(entity, websiteLabel)
+      getContent: entity => <ScopeColumn config={entity} websiteLabel={websiteLabel} />
     }
   ];
 }
@@ -101,70 +93,6 @@ Alerts.propTypes = {
   websiteLabel: PropTypes.string.isRequired,
   websiteId: PropTypes.string.isRequired
 };
-
-function getFiltersContent(config, websiteLabel) {
-  const tagFilterExpression = fromBackendModel(config.tagFilterExpression);
-  const pages = tagFilterExpression.filter(
-    filter => filter.name === 'beacon.page.name' && filter.operator !== 'NOT_EQUAL'
-  );
-  const otherTagFiltersCount = tagFilterExpression.length - pages.length;
-
-  const filterCount = getFiltersCount(tagFilterExpression);
-
-  const maxFilterToDisplay = 3;
-  const filtersToDisplay = getLimitedNumberOfFilters(tagFilterExpression, maxFilterToDisplay);
-
-  const blueprintConfig = getBlueprintConfig(config.rule.alertType);
-  const beaconType = blueprintConfig.getBeaconType(config.rule.metricName);
-  const { QueryBuilder } = getQueryBuilderForBeaconType(beaconType);
-
-  return (
-    <div className={locals.filters}>
-      {websiteLabel && (
-        <span
-          className={classNames({
-            [locals.centered]: true,
-            [locals.space]: pages.length === 0,
-            [locals.divider]: pages.length > 0
-          })}
-        >
-          <SvgIcon className={locals.filterIcon} type="lib_website" />
-          {websiteLabel}
-        </span>
-      )}
-      {pages &&
-        pages.map((page, i) => (
-          <span className={classNames(locals.centered, locals.space)} key={i}>
-            <SvgIcon className={locals.filterIcon} type="lib_website_page_load" />
-            {page.stringValue}
-          </span>
-        ))}
-      {otherTagFiltersCount >= 1 && (
-        <Tooltip
-          themeStyle="light"
-          content={
-            <div>
-              <QueryBuilder value={filtersToDisplay} readOnly />
-              {filterCount > maxFilterToDisplay &&
-                t('in-websites:websiteDashboard.tabs.alerts.moreFiltersWithCount', {
-                  count: filterCount - maxFilterToDisplay
-                })}
-            </div>
-          }
-          align="topMiddle"
-          delay={500}
-        >
-          <span className={locals.centered}>
-            <SvgIcon className={locals.filterIcon} type="lib_actions_filter" />
-            {t('in-websites:websiteDashboard.tabs.alerts.filter', {
-              count: filterCount
-            })}
-          </span>
-        </Tooltip>
-      )}
-    </div>
-  );
-}
 
 function getSubtitle(rule, threshold) {
   const { alertType, aggregation, metricName } = rule;
