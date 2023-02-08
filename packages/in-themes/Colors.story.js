ba5d17c8366b78cb603608b3a2ab6fb9e2e9a6e1
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2023-02-08 10:30:56 +0100

    Enable ESBuild in local development + fix bug (#11108)
    * do not accidentally access not initialised variable
    
    * do not reference invalid components in storybook
    
    * enable eslint errors on use-before-define
    
    * temporary disable eslint warning on existing "working" code.
    
    Needs a short testing, to adapt and fix the code...
    
    * activate ESBuild for local development (yarn dev) only
    
    * Apply suggestions from code review
    
    Co-authored-by: Oliver Munkebo <Oliver.Munkebo@ibm.com>
    
    Co-authored-by: Oliver Munkebo <Oliver.Munkebo@ibm.com>

--- packages/in-themes/Colors.story.js
+++ packages/in-themes/Colors.story.js
@@ -8,9 +8,7 @@ import React from 'react';
 import { colorTranslation, getColor } from 'in-applications/endpointTypes';
 import theme from 'in-themes';
 
-export default {
-  component: Colors
-};
+export default {};
 
 export const Colors = () => {
   return (
