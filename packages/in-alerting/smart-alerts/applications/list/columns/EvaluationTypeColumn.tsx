98176af67e07d7110e87bc7e8939f9cf05f36469
Pradosh E <Pradosh.E@ibm.com>
2023-02-14 13:02:49 +0530

    chore(TS migration): migrate in-alerting/smart-alerts/applications/list/columns/EvaluationTypeColumn

--- packages/in-alerting/smart-alerts/applications/list/columns/EvaluationTypeColumn.tsx
+++ packages/in-alerting/smart-alerts/applications/list/columns/EvaluationTypeColumn.tsx
@@ -3,34 +3,33 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-import PropTypes from 'prop-types';
 import React from 'react';
+
+import { ApplicationAlertConfig } from '@instana/types';
 
 import alertEvaluationTypes, {
   PER_AP
 } from 'in-alerting/smart-alerts/applications/dialog/advanced/EvaluationSwitch/alertEvaluationTypes';
+import DefaultCell from 'in-alerting/smart-alerts/components/list/DefaultCell';
 import { t } from 'in-i18n';
 
-import locals from 'in-alerting/smart-alerts/applications/list/columns/ListColumns.mless';
-
-export default function EvaluationTypeColumn({ config, isGlobalSmartAlertConfig }) {
+export default function EvaluationTypeColumn({
+  config,
+  isGlobalSmartAlertConfig
+}: {
+  config: ApplicationAlertConfig;
+  isGlobalSmartAlertConfig: boolean;
+}) {
   const { evaluationType = PER_AP } = config;
   const evaluationInfo = alertEvaluationTypes[evaluationType];
   return (
-    <div className={locals.column}>
-      <div className={locals.name}>
-        {isGlobalSmartAlertConfig
+    <DefaultCell
+      title={
+        isGlobalSmartAlertConfig
           ? t('in-alerting:smartAlerts.globalApplicationSmartAlert')
-          : t('in-alerting:smartAlerts.applicationSmartAlert')}
-      </div>
-      {evaluationInfo && <div className={locals.nameSubtext}>{evaluationInfo.columnText}</div>}
-    </div>
+          : t('in-alerting:smartAlerts.applicationSmartAlert')
+      }
+      subtitle={evaluationInfo.columnText}
+    />
   );
 }
-
-EvaluationTypeColumn.propTypes = {
-  config: PropTypes.shape({
-    evaluationType: PropTypes.string.isRequired
-  }).isRequired,
-  isGlobalSmartAlertConfig: PropTypes.bool
-};
