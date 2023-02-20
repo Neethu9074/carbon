966ab638e5a7cbcc95d09c3ed0a089598f2d6aa7
Esteban Diaz Gallego <ediazgallego@ibm.com>
2023-02-20 11:48:24 -0500

    Add pop-up message in SynMon main page about permission (#11253)

---
+++ packages/in-synthetics/dashboards/global/tabs/tests/components/PopupMessage.tsx
@@ -0,0 +1,35 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { Trans } from '@instana/i18n-react';
+import { Link } from '@instana/components';
+
+import locals from './PopupMessage.mless';
+
+export default function PoPMessage() {
+  const popDocsUrl = 'https://www.ibm.com/docs/SSE1JP5_current/src/pages/synthetic_monitoring/synmon_permissions.html';
+  const docLinkComponent = (
+    <Trans
+      i18nKey="in-synthetics:dashboard.testList.popDialog.popUpDialog"
+      components={{
+        documentationLink: (
+          <Link href={popDocsUrl} external>
+            &nbsp;
+          </Link>
+        )
+      }}
+    />
+  );
+
+  return (
+    <div className={locals.container}>
+      <h1 className={locals.title}>{docLinkComponent}</h1>
+      {/* <div className={locals.controls}>{docLinkComponent}</div> */}
+    </div>
+  );
+}
