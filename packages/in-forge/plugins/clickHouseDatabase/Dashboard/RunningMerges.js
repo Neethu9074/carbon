ee24c7d83021bb9a1f56f471e579049a74890f41
Timo Hillerns <timo.hillerns@ibm.com>
2023-02-23 10:47:40 +0100

    WIP

--- packages/in-forge/plugins/clickHouseDatabase/Dashboard/RunningMerges.js
+++ packages/in-forge/plugins/clickHouseDatabase/Dashboard/RunningMerges.js
@@ -105,53 +105,66 @@ const cols = [
   }
 ];
 
+/*
 export function getRunningMerges(snapshot) {
   () => createAgentResponseObservable({action: 'clickHouse.getRunningMerges',
                                                             target: snapshot.get('volatileId'),
                                                             args: {}
                                                            }).once(response => {console.log('response')});
 }
-
 export default function RunningMerges({ snapshot, timeConfig }) {
   const response = useObservable(getRunningMerges(snapshot), []);
   console.log(response);
   //const response = getRunningMerges(snapshot);
   let content = null;
-  if (timeConfig.autoRefresh == false) {
-    content = (
-      <DashboardNotification type="info">Running merges list is only available in live mode.</DashboardNotification>
-    );
-  //console.log(response);
-  } else if (response == null) {
-    content = <LoadingIndicator />;
-  } else if (response.error) {
-    content = (
-      <DashboardNotification type="danger">Failed to retrieve running merges: {response.error}</DashboardNotification>
-    );
-  } else {
-    const data = JSON.parse(response.data);
-    const rows = data.data.map((r, i) => ({
-      key: String(i),
-      ...r
-    }));
-    if (rows.length === 0) {
+  */
+export default connectTo(
+  ({ snapshot, timeConfig }) => ({
+    response:
+      timeConfig.focusedMoment == null &&
+      getAgentResponse({
+        action: 'clickHouse.getRunningMerges',
+        target: snapshot.get('volatileId'),
+        args: {}
+      })
+  }),
+  function RunningMerges({ timeConfig, response }) {
+    let content = null;
+    if (timeConfig.autoRefresh == false) {
       content = (
-        <DashboardNotification type="info">No running merges</DashboardNotification>
+        <DashboardNotification type="info">Running merges list is only available in live mode.</DashboardNotification>
+      );
+    //console.log(response);
+    } else if (response == null) {
+      content = <LoadingIndicator />;
+    } else if (response.error) {
+      content = (
+        <DashboardNotification type="danger">Failed to retrieve running merges: {response.error}</DashboardNotification>
       );
     } else {
-      content = (
-        <Table
-          withoutPadding
-          cardTitle={t('in-forge:plugins.clickhouseDatabase.dashboard.titleRunningMerges')}
-          cols={cols}
-          rows={rows}
-          maxItemsPerPage={25}
-          initialSortColumn={5}
-          initialSortDirection="desc"
-        />
-      );
+      const data = JSON.parse(response.data);
+      const rows = data.data.map((r, i) => ({
+        key: String(i),
+        ...r
+      }));
+      if (rows.length === 0) {
+        content = (
+          <DashboardNotification type="info">No running merges</DashboardNotification>
+        );
+      } else {
+        content = (
+          <Table
+            withoutPadding
+            cardTitle={t('in-forge:plugins.clickhouseDatabase.dashboard.titleRunningMerges')}
+            cols={cols}
+            rows={rows}
+            maxItemsPerPage={25}
+            initialSortColumn={5}
+            initialSortDirection="desc"
+          />
+        );
+      }
     }
+    return content;
   }
-
-  return content;
-}
+);
