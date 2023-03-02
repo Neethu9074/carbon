d7393ad4f6f498b4d954efa52fad9d492f3fe246
Luka Martinovic <Luka.Martinovic@ibm.com>
2023-03-02 11:43:54 +0100

    feat: localisation for faceted search items in logging (#11290)

--- packages/in-logging/analyze/AnalyzeView/utils/facetedSearchItems.tsx
+++ packages/in-logging/analyze/AnalyzeView/utils/facetedSearchItems.tsx
@@ -4,6 +4,8 @@
  * Copyright IBM Corp. 2022
  */
 
+import { t } from '@instana/i18n-react';
+
 import FacetedFilterRenderer from 'in-logging/analyze/AnalyzeView/FacetedFilterRenderer';
 import { LOG_LEVEL, LOG_SERVICE_NAME, LOG_STREAM_NAME } from 'in-logging/queryBuilder';
 import { getLabel, getMetric } from 'in-logging/analyze/AnalyzeView/utils/index';
@@ -12,21 +14,21 @@ import { FacetedSearchItem } from 'in-components/AnalyzeView/StateManagement';
 export const facetedSearchItems = [
   {
     renderer: FacetedFilterRenderer,
-    title: 'Log levels',
+    title: t('in-logging:logLevels'),
     tag: LOG_LEVEL,
     getSuggestionName: getLabel,
     getMetric
   },
   {
     renderer: FacetedFilterRenderer,
-    title: 'Stream',
+    title: t('in-logging:stream'),
     tag: LOG_STREAM_NAME,
     getSuggestionName: getLabel,
     getMetric
   },
   {
     renderer: FacetedFilterRenderer,
-    title: 'Service',
+    title: t('in-logging:service'),
     tag: LOG_SERVICE_NAME,
     getSuggestionName: getLabel,
     entity: 'DESTINATION',
