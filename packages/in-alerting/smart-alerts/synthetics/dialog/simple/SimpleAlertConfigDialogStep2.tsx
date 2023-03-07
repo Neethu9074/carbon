09949971af40a4713488a3ee4d63fd9c3f8a9ee6
angel-neethu <angel.neethu.ej@ibm.com>
2023-03-07 20:15:44 +0530

    chore(TS migration): migrate in-alerting/smart-alerts/synthetics/components/AlertTagFilterExpressionConfig and in-alerting/smart-alerts/synthetics/dialog/simple/SimpleAlertConfigDialogStep2

--- packages/in-alerting/smart-alerts/synthetics/dialog/simple/SimpleAlertConfigDialogStep2.tsx
+++ packages/in-alerting/smart-alerts/synthetics/dialog/simple/SimpleAlertConfigDialogStep2.tsx
@@ -4,15 +4,29 @@
  * Copyright IBM Corp. 2023
  */
 
+import { MapForm } from 'formalistic';
 import React from 'react';
 
 import AlertTagFilterExpressionConfig from 'in-alerting/smart-alerts/synthetics/components/AlertTagFilterExpressionConfig';
 import SimpleModeStepContentWrapper from 'in-components/BlueprintFormMultistep/SimpleModeStepContentWrapper';
+import { QueryBuilderComponent } from 'in-components/QueryBuilder';
 import { t } from 'in-i18n';
 
 import locals from 'in-alerting/smart-alerts/components/dialog/simple/SimpleAlertConfigDialogStep2.mless';
 
-export default function SimpleAlertConfigDialogStep2({ form, updateForm, QueryBuilderComponent }) {
+export interface SimpleAlertConfigDialogStep2Props {
+  form: MapForm;
+  updateForm?: (form: MapForm) => void;
+  QueryBuilderComponent: QueryBuilderComponent;
+  headerTransparent?: boolean;
+}
+
+export default function SimpleAlertConfigDialogStep2({
+  form,
+  updateForm,
+  QueryBuilderComponent,
+  headerTransparent = false
+}: SimpleAlertConfigDialogStep2Props) {
   return (
     <SimpleModeStepContentWrapper headline={t('in-alerting:smartAlerts.synthetics.simple.scopeHeadline')}>
       <div className={locals.alertLocationFiltersWrapper}>
@@ -20,6 +34,7 @@ export default function SimpleAlertConfigDialogStep2({ form, updateForm, QueryBu
           form={form}
           updateForm={updateForm}
           QueryBuilderComponent={QueryBuilderComponent}
+          headerTransparent={headerTransparent}
         />
       </div>
     </SimpleModeStepContentWrapper>
