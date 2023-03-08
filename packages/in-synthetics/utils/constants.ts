4e98b24707eba30308c0395b9b1f83be98622590
Sandra S <Sandra.S@ibm.com>
2023-03-08 20:49:43 +0530

    Adding filter related pagination resetting url parameters to Applications > Smart Alerts section (#11401)

--- packages/in-synthetics/utils/constants.ts
+++ packages/in-synthetics/utils/constants.ts
@@ -285,42 +285,36 @@ export type Timing = {
   value: number;
 };
 
+export const syntheticTypesUrlParameter = {
+  path: pathSegment,
+  name: 'syntheticTypes',
+  as: 'syntheticTypes',
+  initialState: [],
+  parser: buildJsonParser([]),
+  serializer: buildJsonSerializer()
+};
+
+export const locationsUrlParameter = {
+  path: pathSegment,
+  name: 'locationIds',
+  as: 'locationIds',
+  initialState: [],
+  parser: buildJsonParser([]),
+  serializer: buildJsonSerializer()
+};
+
+export const applicationsUrlParameter = {
+  path: pathSegment,
+  name: 'applicationIds',
+  as: 'applicationIds',
+  initialState: [],
+  parser: buildJsonParser([]),
+  serializer: buildJsonSerializer()
+};
+
 export const filterUrlStateDefinition = {
-  bind: [
-    createSyntheticTypesUrlParameter(),
-    createLocationsUrlParameter(),
-    {
-      path: pathSegment,
-      name: 'applicationIds',
-      as: 'applicationIds',
-      initialState: [],
-      parser: buildJsonParser([]),
-      serializer: buildJsonSerializer()
-    }
-  ]
+  bind: [syntheticTypesUrlParameter, locationsUrlParameter, applicationsUrlParameter]
 } as Options<UrlState>;
-
-export function createSyntheticTypesUrlParameter() {
-  return {
-    path: pathSegment,
-    name: 'syntheticTypes',
-    as: 'syntheticTypes',
-    initialState: [],
-    parser: buildJsonParser([]),
-    serializer: buildJsonSerializer()
-  };
-}
-
-export function createLocationsUrlParameter() {
-  return {
-    path: pathSegment,
-    name: 'locationIds',
-    as: 'locationIds',
-    initialState: [],
-    parser: buildJsonParser([]),
-    serializer: buildJsonSerializer()
-  };
-}
 
 export interface PresenterProps extends FilterState {
   result: Result<PaginatedResult<TestResultListItem>>;
