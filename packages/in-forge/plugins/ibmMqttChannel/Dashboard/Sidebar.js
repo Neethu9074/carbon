9aa23ddd0abd16a5c124f6018bcef73a8e898930
XING TIAN <tianxing@cn.ibm.com>
2023-03-08 23:22:56 +0800

    Update packages/in-forge/plugins/ibmMqttChannel/Dashboard/Sidebar.js
    Co-authored-by: Robert Hostlowsky <Robert.Hostlowsky@ibm.com>

--- packages/in-forge/plugins/ibmMqttChannel/Dashboard/Sidebar.js
+++ packages/in-forge/plugins/ibmMqttChannel/Dashboard/Sidebar.js
@@ -8,7 +8,7 @@ import React, { Fragment } from 'react';
 import ServiceInstancesList from 'in-sdk/components/sidebar/ServiceInstancesList';
 import Collapsible from 'in-sdk/components/sidebar/Collapsible';
 import { t } from 'in-i18n';
-import Info from '../Info';
+import Info from 'in-forge/plugins/ibmMqttChannel/Info'
 
 export default function IbmMqChannelSidebar({ snapshot }) {
   return (
