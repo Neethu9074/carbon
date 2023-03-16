5355e431ecada79fab29bccb13016154216c82df
Lars Schmidt <Lars.Schmidt1@ibm.com>
2023-03-16 12:47:32 +0100

    adjust Areas to a single import (#11503)

--- packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/index.tsx
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/index.tsx
@@ -4,11 +4,38 @@
  * Copyright IBM Corp. 2023
  */
 
-export { AnalyticsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Analytics/AnalyticsSection';
-export { ApplicationsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Applications/ApplicationsSection';
-export { EventsAndAlertsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/EventsAndAlerts/EventsAndAlertsSection';
-export { InfrastructureSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Infrastructure/InfrastructureSection';
-export { GlobalFunctionsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/GlobalFunctions/GlobalFunctionsSection';
-export { MobileAppsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/MobileApps/MobileAppsSection';
-export { PlatformsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Platforms/PlatformsSection';
-export { WebsitesSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Websites/WebsitesSection';
+import React from 'react';
+
+import { PermissionSetWithRoles } from '@instana/types';
+import { Ul } from '@instana/components';
+
+import { EventsAndAlertsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/EventsAndAlerts/EventsAndAlertsSection';
+import { GlobalFunctionsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/GlobalFunctions/GlobalFunctionsSection';
+import { InfrastructureSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Infrastructure/InfrastructureSection';
+import { ApplicationsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Applications/ApplicationsSection';
+import { MobileAppsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/MobileApps/MobileAppsSection';
+import { AnalyticsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Analytics/AnalyticsSection';
+import { PlatformsSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Platforms/PlatformsSection';
+import { WebsitesSection } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas/Websites/WebsitesSection';
+import { RolesAndAccessScopeContext } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/context';
+
+interface RolesAndAccessScopeOverviewProps {
+  permissionsSet: PermissionSetWithRoles;
+}
+
+export default function RolesAndAccessScopeOverview({ permissionsSet }: RolesAndAccessScopeOverviewProps) {
+  return (
+    <Ul>
+      <RolesAndAccessScopeContext.Provider value={{ permissionsSet: permissionsSet }}>
+        <WebsitesSection />
+        <MobileAppsSection />
+        <ApplicationsSection />
+        <PlatformsSection />
+        <InfrastructureSection />
+        <AnalyticsSection />
+        <EventsAndAlertsSection />
+        <GlobalFunctionsSection />
+      </RolesAndAccessScopeContext.Provider>
+    </Ul>
+  );
+}
--- packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/RoleAndAccessScopeColumns.tsx
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/RoleAndAccessScopeColumns.tsx
@@ -7,25 +7,14 @@
 import { MapForm } from 'formalistic';
 import React from 'react';
 
-import { Button, Message, Ul } from '@instana/components';
 import { PermissionSetWithRoles } from '@instana/types';
+import { Button } from '@instana/components';
 
-import {
-  AnalyticsSection,
-  ApplicationsSection,
-  EventsAndAlertsSection,
-  InfrastructureSection,
-  GlobalFunctionsSection,
-  MobileAppsSection,
-  PlatformsSection,
-  WebsitesSection
-} from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas';
 import EditAccessScopeDialog from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/components/EditAccessScope';
-import { RolesAndAccessScopeContext } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/context';
+import RolesAndAccessScopeOverview from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/Areas';
 import { getField } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/form';
 import { addActiveDialog, close } from 'in-components/DialogPresenter/store';
 import LightCard from 'in-alerting/components/LightCard/LightCard';
-import { RESTRICTED_ACCESS } from 'in-stores/permission';
 import { Col } from 'in-components/layout/Grid/Grid';
 import { t } from 'in-i18n';
 
@@ -40,8 +29,6 @@ interface RoleAndAccessScopeColumnsProps extends FormControlProps {
 
 export default function RoleAndAccessScopeColumns({ form, setForm, readOnly, onSave }: RoleAndAccessScopeColumnsProps) {
   const permissionSetField = getField<PermissionSetWithRoles>(form, 'permissionSet');
-
-  const includesRestrictedAccess = permissionSetField?.value?.permissions?.includes(RESTRICTED_ACCESS);
 
   const openAccessScopeDialog = () => {
     addActiveDialog(
@@ -70,21 +57,7 @@ export default function RoleAndAccessScopeColumns({ form, setForm, readOnly, onS
           )
         }
       >
-        {!includesRestrictedAccess && <Message type="warning">{t('in-stores:permissionRestrictedWarning')}</Message>}
-        {includesRestrictedAccess && permissionSetField && (
-          <Ul>
-            <RolesAndAccessScopeContext.Provider value={{ permissionsSet: permissionSetField.value }}>
-              <WebsitesSection />
-              <MobileAppsSection />
-              <ApplicationsSection />
-              <PlatformsSection />
-              <InfrastructureSection />
-              <AnalyticsSection />
-              <EventsAndAlertsSection />
-              <GlobalFunctionsSection />
-            </RolesAndAccessScopeContext.Provider>
-          </Ul>
-        )}
+        {permissionSetField && <RolesAndAccessScopeOverview permissionsSet={permissionSetField.value} />}
       </LightCard>
     </Col>
   );
