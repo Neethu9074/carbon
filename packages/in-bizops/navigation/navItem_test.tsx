965bb9ab0faccc99ccd17f59e38c8dce88d0e3f0
Maezlin Avila Taylor <maezlin@ibm.com>
2023-03-24 09:20:23 -0500

    rename bizops permissions test files

--- packages/in-bizops/navigation/navItem_test.tsx
+++ packages/in-bizops/navigation/navItem_test.tsx
@@ -10,6 +10,11 @@ import React from 'react';
 // @ts-expect-error module need to be translated to TS
 import ViewSwitcher from 'in-components/MainNavigation/components/ViewSwitcher/';
 
+// jest.mock('in-services/featureFlags', () => ({
+//   __esModule: true,
+//   ...jest.requireActual('in-services/featureFlags')
+// }));
+
 jest.mock('in-services/featureFlags', () => ({
   get businessObservabilityEnabled() {
     return true;
@@ -17,7 +22,12 @@ jest.mock('in-services/featureFlags', () => ({
 }));
 
 describe('packages/in-components/MainNavigation/components/ViewSwitcher', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it('Checks the BizOps link is in the main avigation pane when the feature flag is set', () => {
+    // const featureFlags = jest.requireMock('in-services/featureFlags');
+    // featureFlags.businessObservabilityEnabled = true;
     render(<ViewSwitcher />);
     screen.getByRole('link', { name: 'lib_navigation_stan instana Inc.' });
     screen.getByRole('link', { name: 'lib_bizops' }).click();
