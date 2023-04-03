ae033ee46085437747e5c4fce9423cbdfc50b05c
Bryson Chisholm <bryson.chisholm@ibm.com>
2023-04-03 16:02:15 -0400

    use gray tags to fix accessibility

--- packages/in-automation/ActionCatalog/Tag.tsx
+++ packages/in-automation/ActionCatalog/Tag.tsx
@@ -6,18 +6,8 @@
 
 import React from 'react';
 
-// @ts-expect-error
-import { getColorPool } from 'in-services/util/ColorGenerator';
-import { lighten } from 'in-services/formatters/color';
-
 import locals from './Tag.mless';
 
 export default function Tag({ tag }: { tag: string }) {
-  const color = getColorPool('tags').getColorHex(tag);
-
-  return (
-    <div className={locals.automationTag} style={{ background: lighten(color, 0.1), color: color }}>
-      {tag}
-    </div>
-  );
+  return <div className={locals.automationTag}>{tag}</div>;
 }
