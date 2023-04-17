3f91d167465d3b6d6077a6f3ba3ab276f5b6cd25
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2023-04-17 10:22:58 -0400

    fix websiteBar item for meta

--- packages/in-websites/analyze/AnalyzeView/WebsiteKeyValueBarItem.js
+++ packages/in-websites/analyze/AnalyzeView/WebsiteKeyValueBarItem.js
@@ -3,106 +3,74 @@
  * (c) Copyright Instana Inc.
  */
 
-import React, { forwardRef } from 'react';
+import React from 'react';
 
-import KeyValueBarOverlayBehavior from 'in-analyze/components/filterBar/KeyValueBarItem/KeyValueBarOverlayBehavior';
+import KeyValueBarItemBehavior from 'in-analyze/components/filterBar/KeyValueBarItem/KeyValueBarItemBehavior';
 import getWebsiteBeaconGroups from 'in-websites/subscriptions/getWebsiteBeaconGroups';
-import BarItem from 'in-analyze/components/filterBar/BarItem/BarItem';
-import Overlay from 'in-components/overlays/Overlay';
 
 export default function WebsiteKeyValueBarItem(props) {
-  const { timeConfig, tagFilters, tag, key } = props;
-
-  function mapData(result) {
-    if (!result.data) {
-      return result;
-    }
-
-    return {
-      progress: result.progress,
-      errors: result.errors,
-      time: result.time,
-      data: result.data.items.map(item => JSON.parse(item.name))
-    };
-  }
-
-  function getKeySuggestions(timeConfig, tagFilters, tag) {
-    return getWebsiteBeaconGroups({
-      timeConfig: timeConfig,
-      tagFilters: tagFilters,
-      metrics: {
-        beaconCount: {
-          metric: 'beaconCount',
-          aggregation: 'SUM'
-        }
-      },
-      order: {
-        by: 'beaconCount',
-        direction: 'DESC'
-      },
-      pagination: {
-        retrievalSize: 200
-      },
-      group: {
-        groupbyTag: tag
-      }
-    }).map(mapData);
-  }
-
-  function getValueSuggestions(timeConfig, tagFilters, tag, key) {
-    return getWebsiteBeaconGroups({
-      timeConfig: timeConfig,
-      tagFilters: tagFilters,
-      metrics: {
-        beaconCount: {
-          metric: 'beaconCount',
-          aggregation: 'SUM'
-        }
-      },
-      order: {
-        by: 'beaconCount',
-        direction: 'DESC'
-      },
-      pagination: {
-        retrievalSize: 200
-      },
-      group: {
-        groupbyTag: tag,
-        groupbyTagSecondLevelKey: key
-      }
-    }).map(mapData);
-  }
-
   return (
-    <Overlay
-      withoutWrapper
-      content={KeyValueBarOverlayBehavior}
+    <KeyValueBarItemBehavior
       serializeFilter
-      getKeySuggestions={getKeySuggestions(timeConfig, tagFilters, tag)}
-      getValueSuggestions={getValueSuggestions(timeConfig, tagFilters, tag, key)}
-      props={props}
-      align="bottomMiddle"
-    >
-      {overlayProps => (
-        <Content
-          serializeFilter
-          {...props}
-          getKeySuggestions={getKeySuggestions(timeConfig, tagFilters, tag)}
-          getValueSuggestions={getValueSuggestions(timeConfig, tagFilters, tag, key)}
-          {...overlayProps}
-        />
-      )}
-    </Overlay>
+      getKeySuggestions={({ timeConfig, tagFilters, tag }) => {
+        return getWebsiteBeaconGroups({
+          timeConfig: timeConfig,
+          tagFilters: tagFilters,
+          metrics: {
+            beaconCount: {
+              metric: 'beaconCount',
+              aggregation: 'SUM'
+            }
+          },
+          order: {
+            by: 'beaconCount',
+            direction: 'DESC'
+          },
+          pagination: {
+            retrievalSize: 200
+          },
+          group: {
+            groupbyTag: tag
+          }
+        }).map(mapData);
+      }}
+      getValueSuggestions={({ timeConfig, tagFilters, tag, key }) => {
+        return getWebsiteBeaconGroups({
+          timeConfig: timeConfig,
+          tagFilters: tagFilters,
+          metrics: {
+            beaconCount: {
+              metric: 'beaconCount',
+              aggregation: 'SUM'
+            }
+          },
+          order: {
+            by: 'beaconCount',
+            direction: 'DESC'
+          },
+          pagination: {
+            retrievalSize: 200
+          },
+          group: {
+            groupbyTag: tag,
+            groupbyTagSecondLevelKey: key
+          }
+        }).map(mapData);
+      }}
+      {...props}
+    />
   );
 }
 
-const Content = forwardRef(function Content(props, ref) {
-  const { label, toggle, isOpen, tagFilters, tag } = props;
-  const hasFilters = tagFilters.reduce((agg, f) => agg || f.name === tag, false);
+function mapData(result) {
+  if (!result.data) {
+    return result;
+  }
 
-  return (
-    <BarItem showArrow isOpen={isOpen} active={isOpen || hasFilters} onClick={toggle} ref={ref}>
-      {label}
-    </BarItem>
-  );
-});
+  return {
+    progress: result.progress,
+    errors: result.errors,
+    time: result.time,
+    data: result.data.items.map(item => JSON.parse(item.name))
+  };
+}
