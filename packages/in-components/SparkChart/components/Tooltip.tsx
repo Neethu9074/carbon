06d254585de87b2574a4e8246091d582ee1dad11
Thiemo-Krause <thiemo.krause@ibm.com>
2023-04-19 11:30:35 +0200

    chore(TS migration): migrate in-components/SparkChart/components/Tooltip

--- packages/in-components/SparkChart/components/Tooltip.tsx
+++ packages/in-components/SparkChart/components/Tooltip.tsx
@@ -6,57 +6,82 @@
 import { clamp, isEqual } from 'lodash';
 import React from 'react';
 
-import { on } from '@instana/observables';
+import { Disposable, on } from '@instana/observables';
+import { TimeConfig } from '@instana/types';
 
+import { MetricDataPoint, MetricDataSeries } from 'in-components/Chart/types';
+import { FormatterFn } from 'in-stores/metric/formatters';
 import createScale from 'in-services/scale';
+import { Nullish } from 'in-types';
 
 import locals from './Tooltip.mless';
 
 const tooltipToFocusedMomentMargin = 10;
 
-export default class Tooltip extends React.Component {
+interface HighlightedMoment {
+  y: number;
+  x: number;
+  xDomain: number;
+}
+
+interface Props {
+  timeConfig: TimeConfig;
+  width: number;
+  metrics: MetricDataSeries;
+  tooltipFormatter: FormatterFn;
+}
+
+interface State {
+  highlightedMoment: HighlightedMoment | null;
+}
+
+export default class Tooltip extends React.Component<Props, State> {
+  onMouseMoveSubscription?: Disposable | null;
+  onMouseLeaveSubscription?: Disposable | null;
+  glassPane?: HTMLElement | Nullish;
+
   xScale = createScale();
 
-  state = {
+  state: State = {
     highlightedMoment: null
   };
 
   componentDidMount() {
     this.updateScaleFromProps(this.props);
-    this.onMouseMoveSubscription = on(this.glassPane, 'mousemove').subscribe(this.onMouseMove);
-    this.onMouseLeaveSubscription = on(this.glassPane, 'mouseleave').subscribe(this.onMouseLeave);
+    this.onMouseMoveSubscription = on(this.glassPane!, 'mousemove').subscribe(this.onMouseMove as (e: Event) => void);
+    this.onMouseLeaveSubscription = on(this.glassPane!, 'mouseleave').subscribe(this.onMouseLeave);
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps: Props) {
     if (!isEqual(prevProps, this.props)) {
       this.updateScaleFromProps(this.props);
     }
   }
 
   componentWillUnmount() {
-    this.onMouseMoveSubscription.dispose();
+    this.onMouseMoveSubscription?.dispose();
     this.onMouseMoveSubscription = null;
-    this.onMouseLeaveSubscription.dispose();
+    this.onMouseLeaveSubscription?.dispose();
     this.onMouseLeaveSubscription = null;
   }
 
   render() {
     const nearestDataPoint = this.calculateNearestDataPoint();
-    let xPositionOnCanvas = null;
-    let tooltipStyle;
+    let xPositionOnCanvas: number | null = null;
+    let tooltipStyle: React.CSSProperties | undefined;
     if (nearestDataPoint) {
       xPositionOnCanvas = this.xScale.getRange(nearestDataPoint[0]);
 
       if (this.cursorHasCrossedHalfOfTheCanvas(xPositionOnCanvas)) {
         tooltipStyle = {
           // We clamp between -9 and 1 to make sure the tooltip doesn't clip through the boundary of the Li element
-          top: clamp(this.state.highlightedMoment.y, -9, 1),
+          top: clamp(this.state.highlightedMoment!.y, -9, 1),
           right: this.props.width - xPositionOnCanvas + tooltipToFocusedMomentMargin
         };
       } else {
         tooltipStyle = {
           // We clamp between -9 and 1 to make sure the tooltip doesn't clip through the boundary of the Li element
-          top: clamp(this.state.highlightedMoment.y, -9, 1),
+          top: clamp(this.state.highlightedMoment!.y, -9, 1),
           left: xPositionOnCanvas + tooltipToFocusedMomentMargin
         };
       }
@@ -76,16 +101,16 @@ export default class Tooltip extends React.Component {
             this.glassPane = glassPane;
           }}
         >
-          {nearestDataPoint ? <div className={locals.line} style={{ left: xPositionOnCanvas }} /> : null}
+          {nearestDataPoint ? <div className={locals.line} style={{ left: xPositionOnCanvas! }} /> : null}
         </div>
       </div>
     );
   }
 
-  updateScaleFromProps({ timeConfig, width }) {
+  updateScaleFromProps({ timeConfig, width }: Props): void {
     this.xScale.setRangeFrom(2);
     this.xScale.setRangeTo(width - 2);
-    const to = timeConfig.to;
+    const to = timeConfig.to ?? 0;
     this.xScale.setDomainFrom(to - timeConfig.windowSize);
     this.xScale.setDomainTo(to);
   }
@@ -97,7 +122,7 @@ export default class Tooltip extends React.Component {
     return this.getNearestDataPointForXPosition(this.state.highlightedMoment.x);
   }
 
-  onMouseMove = e => {
+  onMouseMove = (e: MouseEvent) => {
     if (e.offsetX < this.xScale.getRangeFrom() || e.offsetX > this.xScale.getRangeTo()) {
       return;
     }
@@ -117,7 +142,7 @@ export default class Tooltip extends React.Component {
     });
   };
 
-  getNearestDataPointForXPosition = xPositionOnCanvas => {
+  getNearestDataPointForXPosition = (xPositionOnCanvas: number): MetricDataPoint | null => {
     const metrics = this.props.metrics;
     if (metrics.length === 0) {
       return null;
@@ -139,7 +164,7 @@ export default class Tooltip extends React.Component {
     return nearestDataPoint;
   };
 
-  cursorHasCrossedHalfOfTheCanvas(cursorXPosition) {
+  cursorHasCrossedHalfOfTheCanvas(cursorXPosition: number): boolean {
     const fullWidth = this.xScale.getRangeTo() - this.xScale.getRangeFrom();
     return cursorXPosition > fullWidth / 2;
   }
