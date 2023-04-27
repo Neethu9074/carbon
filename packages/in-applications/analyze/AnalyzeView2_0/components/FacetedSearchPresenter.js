Neethu9074/carbon

commit 7d6acddcaf9b42b8b291a7074a298a1e62fe15b9

Update filter to support hiddenCalls (#11717)
* chore: fixed wrong types and imports

* feat: refactored FacetedSearchHeader to be a dumb component

* chore: refactored FacetedSearchPresenters to handle totalActiveCount

* chore: pass totaltActiveCount through to the header

* feat: refactor StageManagement to handle HiddenCalls in the urlState as well

* feat: added feature to reset and merge multiple urlStates at the same time

* chore: write utility function for configuredFacetTags

* chore: remove function in useUrlState

* refactor: use setUrlState instead of getChangeAsUrl

* style: remove important rule from css

* refactor: cleaned up code redundancy

packages/in-applications/analyze/AnalyzeView2_0/components/FacetedSearchPresenter.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 
+import { getTotalActiveFacetItems } from 'in-components/AnalyzeView/utils';
 import FacetedSearch from 'in-components/AnalyzeView/FacetedSearch';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 
@@ -18,12 +19,32 @@ export function FacetedSearchPresenter(props) {
     facets,
     filteringTagCatalog,
     formModel,
-    getFacetedSearchSuggestions
+    getFacetedSearchSuggestions,
+    hiddenCalls,
+    resetFacets,
+    resetHiddenCalls
   } = props;
+
+  let totalActiveCount = getTotalActiveFacetItems(facetedSearchItems, facets);
+
+  if (hiddenCalls.includeSynthetic) {
+    totalActiveCount += 1;
+  }
+
+  if (hiddenCalls.includeInternal) {
+    totalActiveCount += 1;
+  }
+
+  function resetFacetsAndHiddenCalls() {
+    resetFacets?.();
+    resetHiddenCalls?.({ hiddenCalls: {} });
+  }
 
   return facetedSearchItems?.length > 0 ? (
     <FacetedSearch
       {...props}
+      resetFacets={resetFacetsAndHiddenCalls}
+      totalActiveCount={totalActiveCount}
       getSuggestions={({ tag, entity }) =>
         getFacetedSearchSuggestions({
           timeConfig,

packages/in-mobile-apps/analyze/AnalyzeView2_0/components/FacetedSearchPresenter.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 
+import { getTotalActiveFacetItems } from 'in-components/AnalyzeView/utils';
 import FacetedSearch from 'in-components/AnalyzeView/FacetedSearch';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 
@@ -21,9 +22,12 @@ export function FacetedSearchPresenter(props) {
     getFacetedSearchSuggestions
   } = props;
 
+  const totalActiveCount = getTotalActiveFacetItems(facetedSearchItems, facets);
+
   return facetedSearchItems?.length > 0 ? (
     <FacetedSearch
       {...props}
+      totalActiveCount={totalActiveCount}
       getSuggestions={({ tag, entity }) =>
         getFacetedSearchSuggestions({
           timeConfig,

packages/in-websites/analyze/AnalyzeView2_0/components/FacetedSearchPresenter.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 
+import { getTotalActiveFacetItems } from 'in-components/AnalyzeView/utils';
 import FacetedSearch from 'in-components/AnalyzeView/FacetedSearch';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 
@@ -21,9 +22,12 @@ export function FacetedSearchPresenter(props) {
     getFacetedSearchSuggestions
   } = props;
 
+  const totalActiveCount = getTotalActiveFacetItems(facetedSearchItems, facets);
+
   return facetedSearchItems?.length > 0 ? (
     <FacetedSearch
       {...props}
+      totalActiveCount={totalActiveCount}
       getSuggestions={({ tag, entity }) =>
         getFacetedSearchSuggestions({
           timeConfig,

packages/in-applications/analyze/AnalyzeView2_0/AnalyzeView.js
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc. 2021
  */
 
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import { sortBy } from 'lodash';
 
 import {
@@ -137,17 +137,21 @@ export default function ApplicationsAnalyzeView() {
     alreadyConvertedAnalyticsWithHiddenTagsLocation(location)
   );
 
-  const onChangeHiddenCalls = hiddenCalls => {
-    onChange({ hiddenCalls });
-  };
+  const onChangeHiddenCalls = useCallback(
+    hiddenCalls => {
+      onChange({ hiddenCalls });
+    },
+    [onChange]
+  );
 
   const onChangeFastQueryModeEnabled = fastQueryModeEnabled => {
     ua2FastQueryModeChangedTracker({ dataSource, enabled: fastQueryModeEnabled });
     onChange({ fastQueryModeEnabled });
   };
-  const dataSourceConfigurations = useMemo(() => getDataSourceConfigurations({ hiddenCalls, onChangeHiddenCalls }), [
-    hiddenCalls
-  ]);
+  const dataSourceConfigurations = useMemo(
+    () => getDataSourceConfigurations({ hiddenCalls, onChangeHiddenCalls }),
+    [hiddenCalls, onChangeHiddenCalls]
+  );
 
   const tagCatalog = useTagCatalog(dataSource === 'traces' ? getTracesTagCatalog : getCallsTagCatalog);
 
@@ -186,6 +190,7 @@ export default function ApplicationsAnalyzeView() {
             onChangeFastQueryModeEnabled={onChangeFastQueryModeEnabled}
             useLastValidStateWhenErroneous
             getCustomGroupingTagFilter={getCustomGroupingTagFilter}
+            resetHiddenCalls={onChange}
           />
         ) : (
           <Results
@@ -196,6 +201,7 @@ export default function ApplicationsAnalyzeView() {
             fastQueryModeEnabled={fastQueryModeEnabled}
             onChangeFastQueryModeEnabled={onChangeFastQueryModeEnabled}
             useLastValidStateWhenErroneous
+            resetHiddenCalls={onChange}
           />
         )
       }

packages/in-components/AnalyzeView/FacetedFilters/FacetedFilterRangeInput.js
@@ -122,9 +122,9 @@ export default function FacetedFilterRangeInput({
       </Row>
       {isValidNumberRange(minInput, maxInput) && (
         <div className={locals.buttonRow}>
-          <Link href={resetFacets?.(tag)} className={locals.clearFacet}>
-            {t('in-components:analyze.clearFacet')}
-          </Link>
+          <span className={locals.clearFacet}>
+            <Link onClick={() => resetFacets?.(tag)}>{t('in-components:analyze.clearFacet')}</Link>
+          </span>
         </div>
       )}
     </FacetedExpandableCard>

packages/in-components/AnalyzeView/FacetedFilters/FacetedFilterRangeInput.mless
@@ -37,12 +37,13 @@
 
   .clearFacet {
     .typoSettings();
-    color: @lib__colors__teal800;
+    color: var(--ids-color-option-teal-500);
 
     &:hover {
       background: transparent;
-      color: @lib__colors__teal800;
+      color: var(--ids-color-option-teal-500);
       text-decoration: none;
+      cursor: pointer;
     }
   }
 }

packages/in-components/AnalyzeView/FacetedFilters/FacetedSearchHeader.js
@@ -11,12 +11,7 @@ import { t } from 'in-i18n';
 
 import locals from 'in-components/AnalyzeView/FacetedFilters/FacetedSearchHeader.mless';
 
-export default function FacetedSearchHeader({ facets = {}, facetedSearchItems = [], resetFacets }) {
-  const configuredFacetTags = facetedSearchItems.map(item => item.tag);
-  const totalActiveCount = Object.keys(facets)
-    .filter(facetItem => configuredFacetTags.indexOf(facetItem) !== -1)
-    .reduce((acc, curr) => (acc += facets[curr]?.length), 0);
-
+export default function FacetedSearchHeader({ resetFacets, totalActiveCount }) {
   return (
     <div className={locals.header}>
       <Stack distribution={'spaceBetween'} direction={'horizontal'} align={'center'}>
@@ -24,9 +19,9 @@ export default function FacetedSearchHeader({ facets = {}, facetedSearchItems = 
           {t('in-components:analyze.activeFacets', { count: totalActiveCount })}
         </span>
         {totalActiveCount > 0 && (
-          <Link href={resetFacets?.()} className={locals.clearAll}>
-            {t('in-components:analyze.clearFacets')}
-          </Link>
+          <span className={locals.clearAll}>
+            <Link onClick={() => resetFacets()}>{t('in-components:analyze.clearFacets')}</Link>
+          </span>
         )}
       </Stack>
     </div>

commit f22cf2fb8a94ca50524bacecbee302fe8825ba34

Fix clearing sidebar filters in UA (#11866)
# Why

Clearing a single sidebar filter clears all filters and hidden call settings. This regression was introduced with #11717.

# What

Fixing `resetFacetsAndHiddenCalls` by supporting the optional `tag` parameter, which is used when a single sidebar filter should be cleared.


# References

- https://instana.kanbanize.com/ctrl_board/66/cards/125814/details/

packages/in-applications/analyze/AnalyzeView2_0/components/FacetedSearchPresenter.js
@@ -35,9 +35,11 @@ export function FacetedSearchPresenter(props) {
     totalActiveCount += 1;
   }
 
-  function resetFacetsAndHiddenCalls() {
-    resetFacets?.();
-    resetHiddenCalls?.({ hiddenCalls: {} });
+  function resetFacetsAndHiddenCalls(tag) {
+    resetFacets?.(tag);
+    if (tag == null) {
+      resetHiddenCalls?.({ hiddenCalls: {} });
+    }
   }
 
   return facetedSearchItems?.length > 0 ? (