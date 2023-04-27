4869888c714291b02df2e852a4e5f4ad4047bc68
shyluibm <92717364+shyluibm@users.noreply.github.com>
2023-04-27 17:40:09 +0530

    comment

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -80,6 +80,6 @@ module.exports = {
   limitVisibleNestingLevelsInTraceEnabled: true,
   oracleRacMonitoringEnabled: false,
   recurrentMaintenanceWindows: true,
-  playwithEnabled: false,
+  playwithEnabled: true,
   kongEnabled: false
 };
