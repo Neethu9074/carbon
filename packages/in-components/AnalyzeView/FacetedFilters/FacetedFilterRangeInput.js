5a7189566da989ef18b217827bac925778477a13
Stepan Herold <Stepan.Herold@ibm.com>
2023-04-28 14:38:51 +0200

    Reverting Update filter to support hiddenCalls #11717 (#11897)
    # What
    
    Reverting new behaviour introduced with https://github.ibm.com/instana/ui-client/pull/11717 and two follow up bug fixes 
    https://github.ibm.com/instana/ui-client/pull/11863 and https://github.ibm.com/instana/ui-client/pull/11866.
    
    Some of the changes had to be retained in order to resolve linter warnings.
    
    # References
    
    - https://instana.kanbanize.com/ctrl_board/66/cards/126032/details/
    - https://instana.kanbanize.com/ctrl_board/66/cards/125785/details/
    - https://instana.kanbanize.com/ctrl_board/66/cards/125814/details/

--- packages/in-components/AnalyzeView/FacetedFilters/FacetedFilterRangeInput.js
+++ packages/in-components/AnalyzeView/FacetedFilters/FacetedFilterRangeInput.js
@@ -122,9 +122,9 @@ export default function FacetedFilterRangeInput({
       </Row>
       {isValidNumberRange(minInput, maxInput) && (
         <div className={locals.buttonRow}>
-          <span className={locals.clearFacet}>
-            <Link onClick={() => resetFacets?.(tag)}>{t('in-components:analyze.clearFacet')}</Link>
-          </span>
+          <Link href={resetFacets?.(tag)} className={locals.clearFacet}>
+            {t('in-components:analyze.clearFacet')}
+          </Link>
         </div>
       )}
     </FacetedExpandableCard>
