f269d99818898e493d542e6ac57ae9b8654f47be
Pradosh E <Pradosh.E@ibm.com>
2023-05-01 20:32:27 +0530

    allow all threshold operator for all BP in web and Ap smart alert (#11847)

--- packages/in-alerting/smart-alerts/websites/dialog/advanced/StatusCodeThresholdCondition.js
+++ packages/in-alerting/smart-alerts/websites/dialog/advanced/StatusCodeThresholdCondition.js
@@ -30,7 +30,7 @@ export default function StatusCodeThresholdCondition({ form, blueprintConfig, up
           updateForm(form.updateIn(['rule', 'metricName'], f => f.setValue(value).setTouched(true)));
         }}
       />
-      <ThresholdOperatorDropDown form={form} updateForm={updateForm} />
+      <ThresholdOperatorDropDown form={form} updateForm={updateForm} allOptions />
       <ThresholdValueInputWithValidationMessage
         max={maxValue}
         form={form}
