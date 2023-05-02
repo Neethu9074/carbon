75047e70b59c8690fd486e7becc03d806b928613
Oliver Munkebo <oliver.munkebo@ibm.com>
2023-05-02 14:35:36 +0200

    chore(TS Migration): migrated packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusServiceLocator.ts

--- packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusServiceLocator.ts
+++ packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusServiceLocator.ts
@@ -4,19 +4,20 @@
  */
 
 // there is no null service needed, so always use the default implementation
+// @ts-expect-error - Will fix in next PR
 import createNullService from 'in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusService';
 import BaseServiceLocator from 'in-applications/FlowMap/serviceLocator/BaseServiceLocator';
 
-export default class EventBusServiceLocator extends BaseServiceLocator {
+export default class EventBusServiceLocator extends BaseServiceLocator<any> {
   constructor() {
     super(createNullService);
   }
 
-  emit(msg, payload) {
+  emit(msg: string, payload: boolean) {
     return this.service.emit(msg, payload);
   }
 
-  on(msg) {
+  on(msg: string) {
     return this.service.on(msg);
   }
 }
