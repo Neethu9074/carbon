b148a3610db12920f34c3f45b61768211453d53c
Oliver Munkebo <oliver.munkebo@ibm.com>
2023-05-03 10:45:59 +0200

    chore(TS Migration) Migrated packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusService.ts

--- packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusService.ts
+++ packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusService.ts
@@ -3,16 +3,17 @@
  * (c) Copyright Instana Inc.
  */
 
+// @ts-expect-error This will be fixed in UI-foundation
 import RoEmitter from '@instana/roemitter';
 
-export default function createEventBusService(id) {
-  let eventEmitter = new RoEmitter(`global_event_bus:${id}`);
+export default function createEventBusService() {
+  let eventEmitter = new RoEmitter(`global_event_bus`);
 
-  function emit(msg, payload) {
+  function emit(msg: string, payload: boolean) {
     return eventEmitter.emit(msg, payload);
   }
 
-  function on(msg) {
+  function on(msg: string) {
     return eventEmitter.on(msg);
   }
 
