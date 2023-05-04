7dfa52354e5edcdf253db8c58a1356bf51d3d4b7
Lars Schmidt <Lars.Schmidt1@ibm.com>
2023-05-04 18:06:20 +0200

    remove not needed CAN_VIEW from fallback group (#11934)

--- packages/in-stores/permission.ts
+++ packages/in-stores/permission.ts
@@ -638,8 +638,4 @@ export const productOwnerPermissions = getProductPermissions().filter(permission
 export const productNonOwnerPermissions = getProductPermissions().filter(permission => !permission.isOwnerPermission);
 export const productRestrictions = getProductRestrictions();
 export const apiTokenPermissions = getProductPermissions().filter(permission => permission.keyForApiTokenApi != '');
-export const fallBackPermissions = [
-  ...LimitedAccessScopes,
-  Capability.CAN_VIEW_LOGS,
-  Capability.CAN_VIEW_TRACE_DETAILS
-];
+export const fallBackPermissions = [...LimitedAccessScopes];
