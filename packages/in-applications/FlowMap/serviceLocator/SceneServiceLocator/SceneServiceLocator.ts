49b652cf45f5bcb61dde47d00a979a480cebfee1
Oliver Munkebo <oliver.munkebo@ibm.com>
2023-05-04 15:12:55 +0200

    chore(TS Migration): Migrated packages/in-applications/FlowMap/serviceLocator/SceneServiceLocator/SceneServiceLocator.ts

--- packages/in-applications/FlowMap/serviceLocator/SceneServiceLocator/SceneServiceLocator.ts
+++ packages/in-applications/FlowMap/serviceLocator/SceneServiceLocator/SceneServiceLocator.ts
@@ -3,10 +3,11 @@
  * (c) Copyright Instana Inc.
  */
 
+// @ts-expect-error will migrate in future commit
 import createNullService from 'in-applications/FlowMap/serviceLocator/SceneServiceLocator/NullService';
 import BaseServiceLocator from 'in-applications/FlowMap/serviceLocator/BaseServiceLocator';
 
-export default class SceneServiceLocator extends BaseServiceLocator {
+export default class SceneServiceLocator extends BaseServiceLocator<any> {
   constructor() {
     super(createNullService);
   }
