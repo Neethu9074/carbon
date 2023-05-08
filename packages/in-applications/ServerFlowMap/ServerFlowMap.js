b70185b4609e6400ca5f48cd073a6dc0bdb495e1
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2023-05-08 14:20:01 +0200

    chore: removed recompose from ServerFlowMap (#11953)

--- packages/in-applications/ServerFlowMap/ServerFlowMap.js
+++ packages/in-applications/ServerFlowMap/ServerFlowMap.js
@@ -3,9 +3,41 @@
  * (c) Copyright Instana Inc.
  */
 
-import { compose, renameProp } from 'recompose';
+import React from 'react';
 
 import withFlowMapResultState from 'in-applications/ServerFlowMap/withFlowMapResultState';
 import FlowMap3DPresentation from 'in-applications/FlowMap';
 
-export default compose(withFlowMapResultState(), renameProp('height', 'customHeight'))(FlowMap3DPresentation);
+function ServerFlowMap(props) {
+  const {
+    height,
+    rootNodeData,
+    serviceId,
+    applicationId,
+    endpointId,
+    timeConfig,
+    getFlowNodes,
+    collapseLeft,
+    collapseRight,
+    flowMapStateVersion,
+    flowMapState
+  } = props;
+
+  return (
+    <FlowMap3DPresentation
+      customHeight={height}
+      flowMapStateVersion={flowMapStateVersion}
+      flowMapState={flowMapState}
+      rootNodeData={rootNodeData}
+      serviceId={serviceId}
+      applicationId={applicationId}
+      endpointId={endpointId}
+      timeConfig={timeConfig}
+      getFlowNodes={getFlowNodes}
+      collapseLeft={collapseLeft}
+      collapseRight={collapseRight}
+    />
+  );
+}
+
+export default withFlowMapResultState()(ServerFlowMap);
