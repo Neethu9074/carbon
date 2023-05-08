5a67ff9aca18d457ed2552a19c2a62633c1b8e54
lisr <lsrlibj@cn.ibm.com>
2023-05-08 13:27:23 +0800

    chore(TS migration): migrate in-mobile-apps/tagFiltersHoc

--- packages/in-mobile-apps/tagFiltersHoc.ts
+++ packages/in-mobile-apps/tagFiltersHoc.ts
@@ -3,74 +3,87 @@
  * (c) Copyright Instana Inc.
  */
 
-import { withProps } from 'recompose';
+import { TagFilter, TagFilterOperator } from '@instana/types';
 
+import { DashboardTagFiltersTracker } from 'in-mobile-apps/tracker';
 import { noop } from 'in-services/util/function';
 
-export const tagFilterManipulators = ({ tagFiltersTrackers }) =>
-  withProps(({ tagFilters, setTagFilters, group }) => {
-    const trackedSetTagFilters = newTagFilters => {
-      setTagFilters(newTagFilters);
-      tagFiltersTrackers.set({
-        filters: newTagFilters,
-        group: group
-      });
-    };
+interface TagFilterManipulator {
+  setTagFilters: (v: Array<TagFilter>) => void;
+  addTagFilter: (v: TagFilter) => void;
+  upsertTagFilter: (v: TagFilter) => void;
+  removeTagFilter: (name: string, operator: TagFilterOperator | null) => void;
+  clearTagFilters: () => void;
+}
 
-    return {
-      setTagFilters: trackedSetTagFilters,
-      removeTagFilter(name, operator) {
-        setTagFilters(tagFilters.filter(f => f.name !== name || (operator != null && f.operator !== operator)));
-        const before = tagFilters.filter(f => f.name === name && (operator == null || f.operator === operator));
-        if (before.length > 0) {
-          tagFiltersTrackers.remove({
-            name,
-            filter: before[0],
-            group: group
-          });
-        } else {
-          tagFiltersTrackers.remove({
-            name,
-            group: group
-          });
-        }
-      },
-      addTagFilter(newTagFilter) {
-        setTagFilters(tagFilters.concat(newTagFilter));
-        tagFiltersTrackers.add({
-          name: newTagFilter.name,
-          filter: newTagFilter,
+export function useTagFilterManipulators(
+  tagFiltersTrackers: DashboardTagFiltersTracker,
+  tagFilters: Array<TagFilter>,
+  setTagFilters: (v: Array<TagFilter>) => void,
+  group: string
+): TagFilterManipulator {
+  const trackedSetTagFilters = (newTagFilters: Array<TagFilter>) => {
+    setTagFilters(newTagFilters);
+    tagFiltersTrackers.set({
+      filters: newTagFilters,
+      group: group
+    });
+  };
+
+  return {
+    setTagFilters: trackedSetTagFilters,
+    removeTagFilter(name, operator) {
+      setTagFilters(tagFilters.filter(f => f.name !== name || (operator != null && f.operator !== operator)));
+      const before = tagFilters.filter(f => f.name === name && (operator == null || f.operator === operator));
+      if (before.length > 0) {
+        tagFiltersTrackers.remove({
+          name,
+          filter: before[0],
           group: group
         });
-      },
-      upsertTagFilter(newTagFilter) {
-        setTagFilters(
-          tagFilters
-            .filter(f => f.name !== newTagFilter.name || f.operator !== newTagFilter.operator)
-            .concat(newTagFilter)
-        );
-        const before = tagFilters.filter(f => f.name === newTagFilter.name && f.operator === newTagFilter.operator);
-        if (before.length > 0) {
-          tagFiltersTrackers.change({
-            before: before[0],
-            after: newTagFilter,
-            group: group
-          });
-        } else {
-          tagFiltersTrackers.add({
-            filter: newTagFilter,
-            group: group
-          });
-        }
-      },
-      clearTagFilters() {
-        setTagFilters([]);
-        tagFiltersTrackers.clear({
+      } else {
+        tagFiltersTrackers.remove({
+          name,
           group: group
         });
       }
-    };
-  });
+    },
+    addTagFilter(newTagFilter) {
+      setTagFilters(tagFilters.concat(newTagFilter));
+      tagFiltersTrackers.add({
+        name: newTagFilter.name,
+        filter: newTagFilter,
+        group: group
+      });
+    },
+    upsertTagFilter(newTagFilter) {
+      setTagFilters(
+        tagFilters
+          .filter(f => f.name !== newTagFilter.name || f.operator !== newTagFilter.operator)
+          .concat(newTagFilter)
+      );
+      const before = tagFilters.filter(f => f.name === newTagFilter.name && f.operator === newTagFilter.operator);
+      if (before.length > 0) {
+        tagFiltersTrackers.change({
+          before: before[0],
+          after: newTagFilter,
+          group: group
+        });
+      } else {
+        tagFiltersTrackers.add({
+          filter: newTagFilter,
+          group: group
+        });
+      }
+    },
+    clearTagFilters() {
+      setTagFilters([]);
+      tagFiltersTrackers.clear({
+        group: group
+      });
+    }
+  };
+}
 
 export const noopTagFilterTrackers = {
   add: noop,
