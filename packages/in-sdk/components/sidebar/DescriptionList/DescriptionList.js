ddb45aa8164a675fb8dc986d1838c18cbd58f29d
Punit Bhardwaj <Punit.Kumar@ibm.com>
2023-05-17 12:37:36 +0530

    added descriptionList.js

---
+++ packages/in-sdk/components/sidebar/DescriptionList/DescriptionList.js
@@ -0,0 +1,50 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+/* eslint-disable react/no-multi-comp */
+import React, { Fragment } from 'react';
+import classNames from 'classnames';
+
+import locals from './DescriptionList.mless';
+
+export function DescriptionList({ children, className }) {
+  return (
+    <Fragment>
+      <dl className={classNames(locals.descriptionList, className)}>{children}</dl>
+      <div className={locals.descriptionListNoDetails}>No details available.</div>
+    </Fragment>
+  );
+}
+
+export function DescriptionItem({ title, children, onClick }) {
+  if (isItemEmpty(children)) {
+    return null;
+  }
+
+  return (
+    <div className={locals.descriptionListItem}>
+      <dt className={locals.itemTitle}>{title}</dt>
+      <dd className={locals.itemText} onClick={onClick}>
+        {children}
+      </dd>
+    </div>
+  );
+}
+
+function isItemEmpty(children) {
+  if (children === null || children === undefined) {
+    return true;
+  }
+
+  // React avoids using an array of children when there is only one child
+  // node. In these cases, we are going to have a string
+  if (typeof children === 'string') {
+    return children.length === 0;
+  }
+
+  // For any other cases, we are currently not able to tell whether it is
+  // empty, e.g. for child components.
+  return false;
+}
