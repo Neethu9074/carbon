863b5835efad25f3c22621e80c96d3929bc0796f
Juan Burgos <jlburgos@us.ibm.com>
2023-05-17 09:09:40 -0400

    Removing unused variable

--- packages/in-logging/queryBuilder.ts
+++ packages/in-logging/queryBuilder.ts
@@ -92,7 +92,6 @@ export const ID_KUBERNETES_NAMESPACE = 'id.kubernetesNamespace';
 export const ID_KUBERNETES_DEPLOYMENT = 'id.kubernetesDeployment';
 export const ID_DOCKER = 'id.docker';
 export const ID_CONTAINERD = 'id.containerd';
-export const CONTAINERD_NAME = 'containerd.name';
 
 export const kubernetesEntitySnapshotIds = [
   KUBERNETES_CLUSTER_SNAPSHOT_ID,
