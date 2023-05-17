bb8f0ea6d404b95899c673dde675ec8cd5eb3f27
Thiemo Krause <Thiemo.Krause@ibm.com>
2023-05-17 14:36:51 +0200

    feat(service-levels): implement slo dashboard header component (#12033)
    This also addresses an issue with the DashboardHeader component not
    using the correct theme settings for its loading skeletons, which
    resulted in bad contrast.
    And a small issue in TabView introduced by PR #12000 which caused it to
    be stuck in a loading state if no result$ prop was provided

---
+++ packages/in-service-levels/components/SloDashboard/components/SloDashboardMetaInfo.tsx
@@ -0,0 +1,29 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { ServiceLevelObjectiveConfiguration } from '@instana/types';
+import { Stack } from '@instana/components';
+
+import SloEntityInfo from 'in-service-levels/components/SloList/components/SloEntityInfo';
+import { SloTagsList } from 'in-service-levels/components/TagsList/SloTagsList';
+import { LabeledEntity } from 'in-service-levels/types';
+
+interface SloDashboardMetaInfoProps {
+  configuration: ServiceLevelObjectiveConfiguration;
+  entity?: LabeledEntity;
+}
+
+export default function SloDashboardMetaInfo({ configuration, entity }: SloDashboardMetaInfoProps) {
+  const { tags, entity: sloEntity } = configuration;
+  return (
+    <Stack direction="horizontal">
+      {entity && <SloEntityInfo entity={entity} entityType={sloEntity.type} />}
+      <SloTagsList tags={tags} />
+    </Stack>
+  );
+}

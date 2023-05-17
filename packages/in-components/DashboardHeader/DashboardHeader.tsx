Neethu9074/carbon

commit 5db881da690ee1e9d72c2d838fccac5d801f7891

Chore ts migrate tab view (#12000)
* chore(TS migration): rename in-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore

* chore(TS migration): rename in-components/DashboardErroneousResultPresenter

* chore(TS migration): migrate in-components/DashboardErroneousResultPresenter

* chore(TS migration): rename in-components/Loading/DefaultLoadingDashboard

* chore(TS migration): migrate in-components/Loading/DefaultLoadingDashboard

* chore(TS migration): rename in-components/ErrorBoundary

* chore(TS migration): migrate in-components/ErrorBoundary

* chore(TS migration): rename in-components/LocationAwareTabView/components/Swtich

* chore(TS migration): migrate in-components/LocationAwareTabView/components/Switch

* chore(TS migration): rename in-components/SecondLevelNavigation

* chore(TS migration): migrate in-components/SecondLevelNavigation

* chore(TS migration): rename in-components/LocationAwareTabView/components/Header

* chore(TS migration): migrate in-components/LocationAwareTabView/components/Header

* chore(TS migration): rename in-components/breadcrumb/stores/breadcrumbs

* chore(TS migration): migrate in-components/breadcrumb/stores/breadcrumbs

* chore(TS migration): rename in-components/breadcrumb/BreadcrumbHeader

* chore(TS migration): migrate in-components/breadcrumb/BreadcrumbHeader

* chore(TS migration): rename in-components/LocationAwareTabView/TabView

* chore(TS migration): migrate in-components/LocationAwareTabView/TabView

packages/in-components/DashboardHeader/DashboardHeader.tsx
@@ -16,8 +16,8 @@ import TimeSelection from 'in-components/time/TimeSelection/TimeSelection';
 import PlayWithHeader from 'in-new-components/Demo/PlayWithHeader';
 import { playwithEnabled } from 'in-services/featureFlags';
 import Tooltip from 'in-components/Tooltip/Tooltip';
+import { Nullish, Result } from 'in-types';
 import Title from 'in-components/Title';
-import { Result } from 'in-types';
 import { t } from 'in-i18n';
 
 import locals from './DashboardHeader.mless';
@@ -47,7 +47,7 @@ export interface ContextConfiguration {
 
 export interface DashboardHeaderProps {
   theme?: keyof typeof themes;
-  result?: Result<any>;
+  result?: Result<any> | Nullish;
   icon?: string;
   renderIcon?: (() => JSX.Element) | typeof getSkeletonIcon;
   title: string;

packages/in-applications/Dashboards/service/tabs/troubleshooting/TroubleShootingChart.tsx
@@ -11,7 +11,6 @@ import {
   createFormModelFromSyntheticOption,
   createHiddenCallsFromSyntheticOption
 } from 'in-applications/Dashboards/commonComponents/includeSyntheticCalls';
-// @ts-expect-error
 import { isInternalVisible$ } from 'in-components/MainNavigation/components/ViewSwitcher/isInternalVisibleStore';
 import { useLinkToAnalyze as useLinkToApplicationAnalyze } from 'in-applications/navigation/paths';
 import UnifiedMetricsChart from 'in-custom-dashboards/widgets/Chart/UnifiedMetricsChart';

packages/in-automation/AutomationTabs/ViewSwitcher.tsx
@@ -8,7 +8,6 @@ import React from 'react';
 
 import { useObservable } from '@instana/hooks';
 
-// @ts-expect-error Module needs to be translated to TS
 import { SecondLevelNavigation, SecondLevelNavigationItem } from 'in-components/SecondLevelNavigation';
 import DashboardHeaderModule, { themes } from 'in-components/DashboardHeader/DashboardHeaderModule';
 import DashboardHeaderShadowModule from 'in-components/DashboardHeader/DashboardHeaderShadowModule';
@@ -28,7 +27,7 @@ const dashboardHeaderProps = {
   }
 };
 export default function ViewSwitcher() {
-  const isCatalogActive = useObservable(isView(actionCatalogPath), []);
+  const isCatalogActive = useObservable(isView(actionCatalogPath), []) ?? false;
 
   return (
     <>

packages/in-bizops/components/ViewSwitcher.tsx
@@ -6,7 +6,6 @@
 
 import React from 'react';
 
-// @ts-expect-error Module needs to be translated to TS
 import { SecondLevelNavigation, SecondLevelNavigationItem } from 'in-components/SecondLevelNavigation';
 import DashboardHeaderModule, { themes } from 'in-components/DashboardHeader/DashboardHeaderModule';
 import DashboardHeaderShadowModule from 'in-components/DashboardHeader/DashboardHeaderShadowModule';

packages/in-bizops/dashboards/summary/BusinessProcessSummary.tsx
@@ -11,12 +11,11 @@ import { TimeConfig } from '@instana/types';
 
 // @ts-expect-error Module needs to be translated to TS
 import ApplicationEntityHealthIndicatorBehavior from 'in-applications/components/ApplicationEntityHealthIndicatorBehavior';
-// @ts-expect-error Module needs to be translated to TS
-import TabView from 'in-components/LocationAwareTabView/TabView';
 import HealthIndicatorButtonPresenter from 'in-components/health/HealthIndicatorButtonPresenter';
 import DashboardHeader, { DashboardHeaderProps } from 'in-components/DashboardHeader';
 import { useLocation } from 'in-stores/navigation/LocationStateProvider';
 import { businessProcessDashboard } from 'in-bizops/navigation/paths';
+import TabView from 'in-components/LocationAwareTabView/TabView';
 import { getMatrixParameter } from 'in-stores/navigation/matrix';
 import ViewTrackingMeta from 'in-components/ViewTrackingMeta';
 import tabs from 'in-bizops/dashboards/summary/tabs/index';
@@ -54,6 +53,7 @@ export default function BusinessProcessDashboard() {
           pageRootName: 'Business Process Dashboard'
         }}
       />
+      {/* @ts-expect-error -- This is clearly wrong usage of the component, likely from testing */}
       <TabView HeaderComponent={Header} location={location} tabs={tabs} props={props} result$={''} withProps={''} />
     </>
   );

packages/in-bizops/navigation/routes.tsx
@@ -12,7 +12,6 @@ import BizActivitiesList from 'promise-loader?global,bizops!in-bizops/lists/BizA
 import SmartAlertsList from 'promise-loader?global,bizops!in-bizops/lists/SmartAlertsList';
 //@ts-ignore
 import BusinessProcessSummaryDashboard from 'promise-loader?global,bizops!in-bizops/dashboards/summary/BusinessProcessSummary';
-// @ts-expect-error module need to be translated to TS
 import { Route } from 'react-router-dom';
 import React from 'react';
 

packages/in-components/DashboardErroneousResultPresenter/DashboardErroneousResultPresenter.tsx
@@ -5,10 +5,10 @@
 
 import React from 'react';
 
-import ErroneousResultPresenter from 'in-components/Errors/ErroneousResultPresenter';
+import ErroneousResultPresenter, { Props } from 'in-components/Errors/ErroneousResultPresenter';
 
 import locals from './DashboardErroneousResultPresenter.mless';
 
-export default function DashboardErroneousResultPresenter(props) {
+export default function DashboardErroneousResultPresenter(props: Props) {
   return <ErroneousResultPresenter {...props} className={locals.wrapper} />;
 }

packages/in-components/ErrorBoundary.tsx
@@ -3,10 +3,11 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { ErrorInfo } from 'react';
 
 import { generateUniqueShortId } from '@instana/utils';
 import { createLogger } from '@instana/logger';
+import { t } from '@instana/i18n-react';
 
 import { addMessage } from 'in-components/MessageFlyout/stores/messages';
 import { ineum } from 'in-services/tracking/ineum';
@@ -16,8 +17,25 @@ const logger = createLogger('in-component.ErrorBoundary');
 
 const messageId = 'errorBoundary-uncaught-error';
 
-export default class ErrorBoundary extends React.Component {
-  constructor(props) {
+interface ReportArgs {
+  name: string;
+  errorId: string;
+  meta: Record<string, string | number | boolean> & { errorId: string };
+  error: Error;
+  info: ErrorInfo;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+type Props = React.PropsWithChildren<{
+  name: string;
+  meta?: Record<string, string | number | boolean>;
+}>;
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
     this.state = { hasError: false };
   }
@@ -26,9 +44,9 @@ export default class ErrorBoundary extends React.Component {
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
+  componentDidCatch(error: Error, info: ErrorInfo) {
     const errorId = generateUniqueShortId();
-    const args = {
+    const args: ReportArgs = {
       name: this.props.name,
       errorId,
       meta: {
@@ -52,15 +70,15 @@ export default class ErrorBoundary extends React.Component {
   }
 }
 
-function reportErrorToInstana({ meta, error, info }) {
+function reportErrorToInstana({ meta, error, info }: ReportArgs) {
   ineum('reportError', error, {
     componentStack: info.componentStack,
     meta: meta
   });
 }
 
-function reportErrorToConsole({ name, error, info }) {
-  const message = ['An unhandled error occurred within the React component tree'];
+function reportErrorToConsole({ name, error, info }: ReportArgs) {
+  const message: any[] = ['An unhandled error occurred within the React component tree'];
   if (name) {
     message.push(`which was caught at boundary ${name}.`);
   }
@@ -70,11 +88,11 @@ function reportErrorToConsole({ name, error, info }) {
   logger.error.apply(logger, message);
 }
 
-function reportErrorToEndUser({ errorId }) {
+function reportErrorToEndUser({ errorId }: ReportArgs) {
   addMessage(
     {
       type: 'danger',
-      title: <Trans i18nKey="in-components:errorBoundary.message.title" />,
+      title: t('in-components:errorBoundary.message.title'),
       content: <Trans i18nKey="in-components:errorBoundary.message.content" values={{ errorId }} />
     },
     messageId

commit bb8f0ea6d404b95899c673dde675ec8cd5eb3f27

feat(service-levels): implement slo dashboard header component (#12033)
This also addresses an issue with the DashboardHeader component not
using the correct theme settings for its loading skeletons, which
resulted in bad contrast.
And a small issue in TabView introduced by PR #12000 which caused it to
be stuck in a loading state if no result$ prop was provided

packages/in-components/DashboardHeader/DashboardHeader.tsx
@@ -49,15 +49,15 @@ export interface DashboardHeaderProps {
   theme?: keyof typeof themes;
   result?: Result<any> | Nullish;
   icon?: string;
-  renderIcon?: (() => JSX.Element) | typeof getSkeletonIcon;
+  renderIcon?: () => React.ReactNode;
   title: string;
-  renderTimeSelection?: (props: any) => JSX.Element;
-  label: string | JSX.Element;
+  renderTimeSelection?: (props: any) => React.ReactNode;
+  label: string | React.ReactNode;
   labelForTitle?: string;
-  renderMetaInformation?: ((props: any) => JSX.Element) | typeof getSkeletonButton;
-  renderButtonLine?: ((props: any) => JSX.Element) | typeof getSkeletonButton;
-  renderButtonLineSecondary?: ((props: any) => JSX.Element) | typeof getSkeletonButton;
-  renderTopLevelButtonLine?: ((props: any) => JSX.Element) | typeof getSkeletonButton;
+  renderMetaInformation?: (props: any) => React.ReactNode;
+  renderButtonLine?: (props: any) => React.ReactNode;
+  renderButtonLineSecondary?: (props: any) => React.ReactNode;
+  renderTopLevelButtonLine?: (props: any) => React.ReactNode;
   hideUrlShortener?: boolean;
   contextConfigurations?: ContextConfiguration[];
   className?: string;
@@ -96,7 +96,7 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
   const isLoading = result && result.data == null;
 
   if (isLoading) {
-    label = getSkeletonLabel();
+    label = getSkeletonLabel(props);
 
     if (renderButtonLine || renderButtonLineSecondary) {
       renderButtonLine = getSkeletonButton;
@@ -109,7 +109,7 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
       renderTopLevelButtonLine = getSkeletonButton;
     }
     if (!icon || renderIcon) {
-      renderIcon = getSkeletonIcon;
+      renderIcon = () => getSkeletonIcon(props);
     }
   }
 
@@ -194,16 +194,16 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
   );
 }
 
-function getSkeletonButton() {
-  return <LoadingSkeleton className={locals.buttonSkeleton} />;
+function getSkeletonButton({ theme }: DashboardHeaderProps) {
+  return <LoadingSkeleton className={locals.buttonSkeleton} darkMode={theme !== 'light'} />;
 }
 
-function getSkeletonLabel() {
-  return <LoadingSkeleton className={locals.labelSkeleton} />;
+function getSkeletonLabel({ theme }: DashboardHeaderProps) {
+  return <LoadingSkeleton className={locals.labelSkeleton} darkMode={theme !== 'light'} />;
 }
 
-function getSkeletonIcon() {
-  return <LoadingSkeleton className={locals.iconSkeleton} />;
+function getSkeletonIcon({ theme }: DashboardHeaderProps) {
+  return <LoadingSkeleton className={locals.iconSkeleton} darkMode={theme !== 'light'} />;
 }
 
 function Context(props: ContextProps) {

packages/in-service-levels/components/AnalyzeSloEventsButton.tsx
@@ -1,0 +1,54 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import {
+  isApplicationSloEntity,
+  isWebsiteSloEntity,
+  ServiceLevelObjectiveConfiguration,
+  SloEntity
+} from '@instana/types';
+import { Button, Typography } from '@instana/components';
+import { t } from '@instana/i18n-react';
+
+import { getIconByType, getLabelByType } from 'in-analyze/AnalyzeView/dataSources';
+
+interface AnalyzeSloCallsButtonProps {
+  configuration: ServiceLevelObjectiveConfiguration;
+}
+
+export default function AnalyzeSloEventsButton({ configuration }: AnalyzeSloCallsButtonProps) {
+  return (
+    <Button
+      kind="primary"
+      icon={getIconType(configuration.entity)}
+      disabled // This is disabled for now until the full functionality is implemented
+    >
+      <Typography variant="body-regular" onDark>
+        {t('in-service-levels:analyzeSloEventsButton.analyze', { entity: getLabel(configuration.entity) })}
+      </Typography>
+    </Button>
+  );
+}
+
+function getIconType(entity: SloEntity): string | undefined {
+  if (isApplicationSloEntity(entity)) {
+    return getIconByType('calls', 'application');
+  } else if (isWebsiteSloEntity(entity)) {
+    return getIconByType(entity.beaconType, 'website');
+  }
+  return undefined;
+}
+
+function getLabel(entity: SloEntity): string | undefined {
+  if (isApplicationSloEntity(entity)) {
+    return getLabelByType('calls');
+  } else if (isWebsiteSloEntity(entity)) {
+    return getLabelByType(entity.beaconType);
+  }
+  return undefined;
+}

packages/in-service-levels/components/SloDashboard/components/SloDashboardHeader.tsx
@@ -6,8 +6,33 @@
 
 import React from 'react';
 
-import DashboardHeader from 'in-components/DashboardHeader';
+import { Result } from '@instana/types';
+import { t } from '@instana/i18n-react';
 
-export default function SloDashboardHeader(props: any) {
-  return <DashboardHeader {...props} title={'foo title'} label={'fooo label'} icon="lib_service_level" />;
+import SloDashboardMetaInfo from 'in-service-levels/components/SloDashboard/components/SloDashboardMetaInfo';
+import AnalyzeSloEventsButton from 'in-service-levels/components/AnalyzeSloEventsButton';
+import { SloTabData } from 'in-service-levels/components/SloDashboard/tabs';
+import DashboardHeader from 'in-components/DashboardHeader';
+import { Nullish } from 'in-types';
+
+interface SloDashboardHeaderProps {
+  result: Result<SloTabData> | Nullish;
+}
+
+export default function SloDashboardHeader({ result }: SloDashboardHeaderProps) {
+  const { configuration, entity } = result?.data ?? {};
+  const { name } = configuration ?? {};
+
+  return (
+    <DashboardHeader
+      title={t('in-service-levels:general.serviceLevelObjective')}
+      label={name ?? ''}
+      icon="lib_service_level"
+      result={result}
+      renderMetaInformation={() =>
+        configuration && <SloDashboardMetaInfo configuration={configuration} entity={entity} />
+      }
+      renderButtonLine={() => configuration && <AnalyzeSloEventsButton configuration={configuration} />}
+    />
+  );
 }

packages/in-service-levels/components/SloDashboard/components/SloDashboardMetaInfo.tsx
@@ -1,0 +1,29 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { ServiceLevelObjectiveConfiguration } from '@instana/types';
+import { Stack } from '@instana/components';
+
+import SloEntityInfo from 'in-service-levels/components/SloList/components/SloEntityInfo';
+import { SloTagsList } from 'in-service-levels/components/TagsList/SloTagsList';
+import { LabeledEntity } from 'in-service-levels/types';
+
+interface SloDashboardMetaInfoProps {
+  configuration: ServiceLevelObjectiveConfiguration;
+  entity?: LabeledEntity;
+}
+
+export default function SloDashboardMetaInfo({ configuration, entity }: SloDashboardMetaInfoProps) {
+  const { tags, entity: sloEntity } = configuration;
+  return (
+    <Stack direction="horizontal">
+      {entity && <SloEntityInfo entity={entity} entityType={sloEntity.type} />}
+      <SloTagsList tags={tags} />
+    </Stack>
+  );
+}

packages/in-service-levels/components/SloDashboard/components/SloSummary.tsx
@@ -6,14 +6,13 @@
 
 import React from 'react';
 
-import { ServiceLevelObjectiveConfiguration } from '@instana/types';
-
+import { SloTabData } from 'in-service-levels/components/SloDashboard/tabs';
 import { Nullish } from 'in-types';
 
 interface SloSummaryProps {
-  data: ServiceLevelObjectiveConfiguration | Nullish;
+  data: SloTabData | Nullish;
 }
 
 export default function SloSummary({ data }: SloSummaryProps) {
-  return <div>{data?.id}</div>;
+  return <div>{data?.configuration.id}</div>;
 }

packages/in-service-levels/components/SloDashboard/tabs.ts
@@ -9,12 +9,19 @@ import { ServiceLevelObjectiveConfiguration } from '@instana/types';
 import { serviceLevelsObjectiveSummaryFullyQualified } from 'in-service-levels/navigation/path';
 import SloSummary from 'in-service-levels/components/SloDashboard/components/SloSummary';
 import { Tab } from 'in-components/LocationAwareTabView/types';
+import { LabeledEntity } from 'in-service-levels/types';
 
-const tabs: Tab<ServiceLevelObjectiveConfiguration, {}>[] = [
+export interface SloTabData {
+  configuration: ServiceLevelObjectiveConfiguration;
+  entity: LabeledEntity;
+}
+
+const tabs: Tab<SloTabData, {}>[] = [
   {
     label: 'Summary',
     path: serviceLevelsObjectiveSummaryFullyQualified,
-    component: SloSummary
+    component: SloSummary,
+    hideTabLabelWhenAlone: true
   }
 ];
 

packages/in-service-levels/components/SloList/components/SloTagsColumnContent.tsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 
-import { SloTagsList } from 'in-service-levels/components/SloList/components/TagsList/SloTagsList';
+import { SloTagsList } from 'in-service-levels/components/TagsList/SloTagsList';
 import { SloListItem } from 'in-service-levels/components/SloList/SloList';
 
 interface Props {

packages/in-service-levels/components/TagsList/SloTagsList.tsx
@@ -9,16 +9,13 @@ import classNames from 'classnames';
 
 import { Typography, useTheme } from '@instana/components';
 
-import {
-  getTagsThatFitAfterResize,
-  getTagsThatFitIntoMaxWidth
-} from 'in-service-levels/components/SloList/components/TagsList/utils';
-import { tagsCssGap } from 'in-service-levels/components/SloList/components/TagsList/constants';
+import { getTagsThatFitAfterResize, getTagsThatFitIntoMaxWidth } from 'in-service-levels/components/TagsList/utils';
+import { tagsCssGap } from 'in-service-levels/components/TagsList/constants';
 import useResizeObserver from 'in-hooks/useResizeObserver';
 import Tooltip from 'in-components/Tooltip';
-import Pill from 'in-components/Pill/Pill';
+import Pill from 'in-components/Pill';
 
-import locals from 'in-service-levels/components/SloList/components/TagsList/SloTagsList.mless';
+import locals from 'in-service-levels/components/TagsList/SloTagsList.mless';
 
 export type TagsType = {
   text: string;

packages/in-service-levels/components/TagsList/utils.ts
@@ -4,8 +4,8 @@
  * Copyright IBM Corp. 2023
  */
 
-import { TagsType } from 'in-service-levels/components/SloList/components/TagsList/SloTagsList';
-import { tagsCssGap } from 'in-service-levels/components/SloList/components/TagsList/constants';
+import { TagsType } from 'in-service-levels/components/TagsList/SloTagsList';
+import { tagsCssGap } from 'in-service-levels/components/TagsList/constants';
 
 export const getTagsThatFitIntoMaxWidth = (tags: TagsType, targetWidth: number) => {
   const tagsThatFit: TagsType = [];

packages/in-service-levels/hooks/useSloEntitiesLabels.ts
@@ -51,7 +51,7 @@ export default function useSloEntitiesLabels(
   return resultToFetchedStateResponse(result);
 }
 
-function loadEntity(entity: SloEntity): Observable<Result<MonitoredEntity>> {
+export function loadEntity(entity: SloEntity): Observable<Result<MonitoredEntity>> {
   let id = '';
   if (isApplicationSloEntity(entity)) {
     id = entity.applicationId;

packages/in-service-levels/views/ServiceLevelsObjectiveDashboard.tsx
@@ -6,13 +6,21 @@
 
 import React from 'react';
 
+import { Result, ServiceLevelObjectiveConfiguration } from '@instana/types';
+import { combineLatest, just, Observable } from '@instana/observables';
+
 import SloDashboardHeader from 'in-service-levels/components/SloDashboard/components/SloDashboardHeader';
 import { defaultServiceLevelObjectiveUrlParameters } from 'in-service-levels/navigation/urlParameters';
+import tabs, { SloTabData } from 'in-service-levels/components/SloDashboard/tabs';
 import { getSloConfiguration } from 'in-service-levels/api/configuration';
+import { loadEntity } from 'in-service-levels/hooks/useSloEntitiesLabels';
 import { useLocation } from 'in-stores/navigation/LocationStateProvider';
-import tabs from 'in-service-levels/components/SloDashboard/tabs';
 import TabView from 'in-components/LocationAwareTabView/TabView';
+import { hasError, isLoading } from 'in-services/util/result';
+import { pendingResult } from 'in-services/fixedObjects';
+import { LabeledEntity } from 'in-service-levels/types';
 import useUrlState from 'in-hooks/useUrlState';
+import { all } from 'in-hooks/utils/progress';
 
 export default function ServiceLevelsObjectiveDashboard() {
   const location = useLocation();
@@ -21,12 +29,28 @@ export default function ServiceLevelsObjectiveDashboard() {
   });
 
   return (
-    <TabView
-      location={location}
-      result$={getSloConfiguration(sloId)}
-      HeaderComponent={SloDashboardHeader}
-      tabs={tabs}
-      props={{}}
-    />
+    <TabView location={location} result$={getData(sloId)} HeaderComponent={SloDashboardHeader} tabs={tabs} props={{}} />
   );
+}
+
+function getData(sloId: string): Observable<Result<SloTabData>> {
+  return getSloConfiguration(sloId)
+    .flatMap((result): Observable<[Result<ServiceLevelObjectiveConfiguration>, Result<LabeledEntity>]> => {
+      if (isLoading(result) || hasError(result)) {
+        return just([result, pendingResult]);
+      }
+      return combineLatest([just(result), loadEntity(result.data!.entity)]);
+    })
+    .map((results): Result<SloTabData> => {
+      if (isLoading(...results)) return pendingResult as Result<SloTabData>;
+
+      return {
+        progress: all(...results.map(r => r.progress)),
+        data: {
+          configuration: results[0].data!,
+          entity: results[1]?.data!
+        },
+        errors: results.flatMap(r => r.errors)
+      };
+    });
 }

packages/in-service-levels/components/TagsList/utils_test.ts
@@ -4,11 +4,8 @@
  * Copyright IBM Corp. 2023
  */
 
-import {
-  getTagsThatFitIntoMaxWidth,
-  getTagsThatFitAfterResize
-} from 'in-service-levels/components/SloList/components/TagsList/utils';
-import { TagsType } from 'in-service-levels/components/SloList/components/TagsList/SloTagsList';
+import { getTagsThatFitIntoMaxWidth, getTagsThatFitAfterResize } from 'in-service-levels/components/TagsList/utils';
+import { TagsType } from 'in-service-levels/components/TagsList/SloTagsList';
 
 const mockTags = [
   {