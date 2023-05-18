776ba1a57c1a18170a7aaeb89906cda21c9127d0
Sreejith Nair <Sreejith.Nair@ibm.com>
2023-05-18 12:38:19 +0530

    Rac enhancement (#11998)
    * Oracle RAC Enhancement
    
    * feature flag removed
    
    * removed unused keys from the translation file

---
+++ packages/in-forge/plugins/oracleDB/Dashboard/Tables/UserCallsCommitsRollbacksDetailsTable.tsx
@@ -0,0 +1,184 @@
+/*
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import { TimeConfig } from '@instana/types';
+
+import Chart from 'in-infrastructure/components/InfrastructureMetricChartBehavior';
+import { SnapshotData } from 'in-stores/snapshot/snapshot';
+import { number } from 'in-services/formatters/number';
+import { emptyMap } from 'in-services/fixedImmutables';
+import Table from 'in-sdk/components/dashboard/Table';
+import { t } from 'in-i18n';
+
+interface DatasourcesTableProps {
+  snapshot: SnapshotData;
+  timeConfig: TimeConfig;
+}
+
+interface Row {
+  key: string;
+  instanceDetails: Map<string, string | number>;
+  timeConfig: TimeConfig;
+  snapshotId: string;
+}
+
+const cols = [
+  {
+    title: t('in-forge:plugins.oracleDB.instanceID'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: Row) {
+        return row.key;
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.oracleDB.userCalls'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row: Row) {
+        return row.snapshotId;
+      },
+      getMetricName(row: Row) {
+        return `stats.systemStats.${row.key}.userCalls`;
+      },
+      getContent: number.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.oracleDB.recursiveCalls'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row: Row) {
+        return row.snapshotId;
+      },
+      getMetricName(row: Row) {
+        return `stats.systemStats.${row.key}.recursiveCalls`;
+      },
+      getContent: number.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.oracleDB.userCommits'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row: Row) {
+        return row.snapshotId;
+      },
+      getMetricName(row: Row) {
+        return `stats.systemStats.${row.key}.userCommits`;
+      },
+      getContent: number.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.oracleDB.userRollbacks'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row: Row) {
+        return row.snapshotId;
+      },
+      getMetricName(row: Row) {
+        return `stats.systemStats.${row.key}.userRollbacks`;
+      },
+      getContent: number.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  },
+  {
+    title: t('in-forge:plugins.oracleDB.userLogOns'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row: Row) {
+        return row.snapshotId;
+      },
+      getMetricName(row: Row) {
+        return `stats.systemStats.${row.key}.userLogOns`;
+      },
+      getContent: number.compact,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  }
+];
+
+export default function DatasourcesTable({ snapshot, timeConfig }: DatasourcesTableProps) {
+  const snapshotId = snapshot.get('id');
+  const rows = snapshot
+    .getIn(['data', 'instanceDetails'], emptyMap)
+    .map((instanceDetails: Object, key: string) => {
+      return {
+        key,
+        instanceDetails,
+        timeConfig,
+        snapshotId
+      };
+    })
+    .valueSeq()
+    .toArray();
+
+  if (rows.length === 0) {
+    return null;
+  }
+
+  return (
+    <Table
+      withoutPadding
+      cardTitle={t('in-forge:plugins.oracleDB.userCallsCommitsRollbacks', {
+        len: rows.length
+      })}
+      cols={cols}
+      rows={rows}
+      getRowDetails={getRowDetails}
+    />
+  );
+}
+
+function getRowDetails(row: Row) {
+  return (
+    <div>
+      <Chart
+        snapshotId={row.snapshotId}
+        timeConfig={row.timeConfig}
+        y1={{
+          formatter: number.compact,
+          metrics: [
+            `stats.systemStats.${row.key}.userCalls`,
+            `stats.systemStats.${row.key}.recursiveCalls`,
+            `stats.systemStats.${row.key}.userCommits`,
+            `stats.systemStats.${row.key}.userRollbacks`,
+            `stats.systemStats.${row.key}.userLogOns`
+          ],
+          labels: [
+            t('in-forge:plugins.oracleDB.userCalls'),
+            t('in-forge:plugins.oracleDB.recursiveCalls'),
+            t('in-forge:plugins.oracleDB.userCommits'),
+            t('in-forge:plugins.oracleDB.userRollbacks'),
+            t('in-forge:plugins.oracleDB.userLogOns')
+          ],
+          type: 'line'
+        }}
+        margins={{
+          left: 90,
+          right: 90
+        }}
+      />
+    </div>
+  );
+}
