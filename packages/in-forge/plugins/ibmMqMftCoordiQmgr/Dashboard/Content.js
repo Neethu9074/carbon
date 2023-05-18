bc9615537574f507d0bf762f967cfbf891ae6935
LI JIAN WANG <wlijian@cn.ibm.com>
2023-05-18 17:47:52 +0800

    re-order statistic metrics (#12049)

--- packages/in-forge/plugins/ibmMqMftCoordiQmgr/Dashboard/Content.js
+++ packages/in-forge/plugins/ibmMqMftCoordiQmgr/Dashboard/Content.js
@@ -35,10 +35,10 @@ export default function IbmMqMftCoordiQmgrDashboard({ snapshot, timeConfig }) {
           y1={{
             formatter: number.detailed,
             tooltipFormatter: number.detailed,
-            metrics: [`successfulTransfers`, `failedTransfers`, `partiallySuccessfulTransfers`, `cancelledTransfers`],
+            metrics: [`partiallySuccessfulTransfers`, `successfulTransfers`, `failedTransfers`, `cancelledTransfers`],
             labels: [
-              t('in-forge:plugins.ibmMqMftCoordiQmgr.dashboard.successfulTransfers'),
               t('in-forge:plugins.ibmMqMftCoordiQmgr.dashboard.partiallySuccessfulTransfers'),
+              t('in-forge:plugins.ibmMqMftCoordiQmgr.dashboard.successfulTransfers'),
               t('in-forge:plugins.ibmMqMftCoordiQmgr.dashboard.failedTransfers'),
               t('in-forge:plugins.ibmMqMftCoordiQmgr.dashboard.cancelledTransfers')
             ],
