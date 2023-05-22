2fefa026bc4f64ef6ebca7fd2f22a22f20feeb13
shyluibm <92717364+shyluibm@users.noreply.github.com>
2023-05-22 15:05:31 +0530

    The review comments
    Signed-off-by: shyluibm <92717364+shyluibm@users.noreply.github.com>

--- packages/in-new-components/Demo/PlayWithHeader.tsx
+++ packages/in-new-components/Demo/PlayWithHeader.tsx
@@ -18,45 +18,45 @@ export default function PlayWithHeader() {
   return (
     <Sticky
       header={
-        playwithEnabled ? (
-          <div className={locals.playWithInstana}>
-            <div className={locals.headerHeading}>
-              <Typography onDark variant="heading-600">
-                {t('in-new-components:playwithinstana.title')}
-              </Typography>
+        <>
+          {playwithEnabled && (
+            <div className={locals.playWithInstana}>
+              <div className={locals.headerHeading}>
+                <Typography onDark variant="heading-600">
+                  {t('in-new-components:playwithinstana.title')}
+                </Typography>
+              </div>
+
+              <div className={locals.readyToMonitor}>
+                <Typography onDark variant="body-bold">
+                  {t('in-new-components:playwithinstana.content')}?
+                </Typography>
+              </div>
+
+              <Button
+                id="free_trial"
+                kind="secondary"
+                size="compact"
+                className={locals.buttonFreeTrial}
+                target="_blank"
+                href="https://www.instana.com/trial/"
+              >
+                {t('in-new-components:playwithinstana.freetrial')}
+              </Button>
+
+              <Button
+                id="schedule_demo"
+                kind="secondary"
+                size="compact"
+                className={locals.buttonDemo}
+                target="_blank"
+                href="https://www.instana.com/schedule-demo/"
+              >
+                {t('in-new-components:playwithinstana.bookdemo')}
+              </Button>
             </div>
-
-            <div className={locals.readyToMonitor}>
-              <Typography onDark variant="body-bold">
-                {t('in-new-components:playwithinstana.content')}?
-              </Typography>
-            </div>
-
-            <Button
-              id="free_trial"
-              kind="secondary"
-              size="compact"
-              className={locals.buttonFreeTrial}
-              target="_blank"
-              href="https://www.instana.com/trial/"
-            >
-              {t('in-new-components:playwithinstana.freetrial')}
-            </Button>
-
-            <Button
-              id="schedule_demo"
-              kind="secondary"
-              size="compact"
-              className={locals.buttonDemo}
-              target="_blank"
-              href="https://www.instana.com/schedule-demo/"
-            >
-              {t('in-new-components:playwithinstana.bookdemo')}
-            </Button>
-          </div>
-        ) : (
-          <></>
-        )
+          )}
+        </>
       }
     />
   );
