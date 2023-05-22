116ae9353fc92c831a7350fdd080bd1911b197c9
shyluibm <92717364+shyluibm@users.noreply.github.com>
2023-05-22 16:58:41 +0530

    comment

--- packages/in-server/src/services/csp.js
+++ packages/in-server/src/services/csp.js
@@ -15,6 +15,7 @@ if (isRequiringInstanaRocks()) {
 
 if (serverConfig.mixpanelToken) {
   allowedScriptOrigins.push('https://cdn.mxpnl.com');
+  allowedScriptOrigins.push('https://cdn.walkme.com');
 }
 
 if (serverConfig.appcuesId) {
