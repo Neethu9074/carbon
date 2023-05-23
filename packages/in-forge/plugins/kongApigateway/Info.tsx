c4435367ced9a9033d307cb96f5397d49fbebb84
Punit Bhardwaj <Punit.Kumar@ibm.com>
2023-05-23 18:08:05 +0530

    review comments suggestion fixed

--- packages/in-forge/plugins/kongApigateway/Info.tsx
+++ packages/in-forge/plugins/kongApigateway/Info.tsx
@@ -10,22 +10,25 @@ import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/Desc
 import { t } from 'in-i18n';
 
 interface KongInfoProps {
-  snapshot: any;
+  snapshot: SnapshotData;
+}
+interface SnapshotData {
+  data: KongSnapshot;
+}
+interface KongSnapshot {
+  hostName: string;
+  nodeId: string;
+  luaVersion: string;
+  kongVersion: string;
 }
 
-export default function KongInfo({ snapshot }: KongInfoProps): JSX.Element {
-  const data = snapshot.get('data');
-
+export default function KongInfo({ snapshot: { data: kong } }: KongInfoProps): JSX.Element {
   return (
     <DescriptionList>
-      <DescriptionItem title={t('in-forge:plugins.kongApigateway.hostName')}>{data.get('hostName')}</DescriptionItem>
-      <DescriptionItem title={t('in-forge:plugins.kongApigateway.nodeId')}>{data.get('nodeId')}</DescriptionItem>
-      <DescriptionItem title={t('in-forge:plugins.kongApigateway.luaVersion')}>
-        {data.get('luaVersion')}
-      </DescriptionItem>
-      <DescriptionItem title={t('in-forge:plugins.kongApigateway.kongVersion')}>
-        {data.get('kongVersion')}
-      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.kongApigateway.hostName')}>{kong.hostName}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.kongApigateway.nodeId')}>{kong.nodeId}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.kongApigateway.luaVersion')}>{kong.luaVersion}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.kongApigateway.kongVersion')}>{kong.kongVersion}</DescriptionItem>
     </DescriptionList>
   );
 }
