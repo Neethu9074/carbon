4873e46ec55d8c4b9396892226dda906ce251bb7
Punit Bhardwaj <Punit.Kumar@ibm.com>
2023-05-24 13:06:55 +0530

    camelcase issue fixed

--- packages/in-forge/plugins/kongApigateway/kpiDefinitions.js
+++ packages/in-forge/plugins/kongApigateway/kpiDefinitions.js
@@ -9,13 +9,13 @@ import { t } from 'in-i18n';
 
 export default [
   {
-    label: t('in-forge:plugins.kongApigateway.Nginxrunning'),
+    label: t('in-forge:plugins.kongApigateway.nginxRunning'),
     metric: 'nginxTimers.running',
     formatter: number.compact
   },
   {
     metric: 'nginxTimers.pending',
-    label: t('in-forge:plugins.kongApigateway.Nginxpending'),
+    label: t('in-forge:plugins.kongApigateway.nginxPending'),
     formatter: number.compact
   }
 ];
