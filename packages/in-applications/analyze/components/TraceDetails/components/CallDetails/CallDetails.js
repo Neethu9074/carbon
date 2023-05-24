1ae7a3d6396c7bad34c1f812c22f1263e69da3a3
Oliver Munkebo <Oliver.Munkebo@ibm.com>
2023-05-24 14:39:52 +0200

    fix: adjusted the conditions to read properly (#12101)

--- packages/in-applications/analyze/components/TraceDetails/components/CallDetails/CallDetails.js
+++ packages/in-applications/analyze/components/TraceDetails/components/CallDetails/CallDetails.js
@@ -36,7 +36,7 @@ export default function CallDetails(props) {
 
   const websiteBeaconResult =
     useObservable(() => {
-      if (rootCall && rootCall.id === callId && callId === 'ROOT') {
+      if (rootCall && (rootCall.id === callId || callId === 'ROOT')) {
         return getCorrelatedWebsiteBeacons({
           traceId,
           correlationId: correlationType === 'web' ? correlationId : null,
@@ -47,7 +47,7 @@ export default function CallDetails(props) {
 
   const mobileAppBeaconResult =
     useObservable(() => {
-      if (rootCall && rootCall.id === callId && callId === 'ROOT') {
+      if (rootCall && (rootCall.id === callId || callId === 'ROOT')) {
         return getMobileAppBeacons({
           tagFilters: [{ name: 'mobileBeacon.backend.traceId', stringValue: traceId, operator: 'EQUALS' }],
           timeConfig: {
