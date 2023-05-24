32e77b8cbf1b6dd9543f2c4f55f401e238bf9cb7
shyluibm <92717364+shyluibm@users.noreply.github.com>
2023-05-24 11:24:28 +0530

    The inner object is replaced
    Signed-off-by: shyluibm <92717364+shyluibm@users.noreply.github.com>

--- packages/in-server/src/routes/product.js
+++ packages/in-server/src/routes/product.js
@@ -179,6 +179,7 @@ router.get('/', async (req, res) => {
         user: userStr,
         permissions: permissions,
         config: JSON.stringify(clientConfig),
+        playwithinstanaEnabled: JSON.stringify(clientConfig).featureFlags.playwithEnabled,
         build: stringifiedBuildInformation,
         searchFields: searchFieldsStr,
         settings: userSettings,
