77351c5c85a1a78285ae58e9e0d668ae35caba28
Thiemo Krause <Thiemo.Krause@ibm.com>
2023-06-02 11:22:04 +0200

    feat(service-levels): add marker lanes to slo dashboard (#12174)

---
+++ packages/in-service-levels/components/SloDashboard/components/chart/SloDashboardMarkerLanes/WebsiteSloMarkerLanes.tsx
@@ -0,0 +1,56 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { WebsiteSloEntity } from '@instana/types';
+
+import type { SloDashboardMarkerLanesProps } from 'in-service-levels/components/SloDashboard/components/chart/SloDashboardMarkerLanes/SloDashboardMarkerLanes';
+// @ts-expect-error -- Ts migration already in progress
+import ReleasesLane from 'in-components/Chart/markerLanes/ReleasesLane/ReleasesLane';
+// @ts-expect-error -- Ts migration already in progress
+import AlertsLane from 'in-components/Chart/markerLanes/AlertsLane/AlertsLane';
+import MarkerLanesPresenter from 'in-components/Chart/markerLanes/MarkerLanesPresenter';
+import getWebsiteAlertClusters from 'in-websites/subscriptions/getWebsiteAlertClusters';
+
+interface WebsiteSloMarkerLanesProps extends Omit<SloDashboardMarkerLanesProps, 'configuration'> {
+  entity: WebsiteSloEntity;
+}
+
+export default function WebsiteSloMarkerLanes({
+  granularity,
+  timeConfig,
+  entity,
+  chartContentPosition,
+  chartWidth = 0,
+  chartBucketWidth = 0,
+  chartHeight = 0,
+  timeAxisHeight = 0,
+  markerPaneHeight = 0
+}: WebsiteSloMarkerLanesProps) {
+  const { websiteId } = entity;
+
+  return (
+    <MarkerLanesPresenter
+      granularity={granularity}
+      timeConfig={timeConfig}
+      chartWidth={chartWidth}
+      chartHeight={chartHeight}
+      chartBucketWidth={chartBucketWidth}
+      chartContentPosition={chartContentPosition}
+      timeAxisHeight={timeAxisHeight}
+      markerPaneHeight={markerPaneHeight}
+    >
+      <ReleasesLane />
+      <AlertsLane
+        config={{
+          websiteId
+        }}
+        getAlerts={getWebsiteAlertClusters}
+      />
+    </MarkerLanesPresenter>
+  );
+}
