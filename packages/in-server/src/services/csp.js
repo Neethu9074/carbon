9587df02ee4d4ddaada044339cff62f697412974
shyluibm <92717364+shyluibm@users.noreply.github.com>
2023-06-05 16:07:27 +0530

    The csp fix and feature flag
    Signed-off-by: shyluibm <92717364+shyluibm@users.noreply.github.com>

--- packages/in-server/src/services/csp.js
+++ packages/in-server/src/services/csp.js
@@ -16,7 +16,10 @@ if (isRequiringInstanaRocks()) {
 if (serverConfig.mixpanelToken) {
   allowedScriptOrigins.push('https://cdn.mxpnl.com');
 }
+
 allowedScriptOrigins.push('https://cdn.walkme.com');
+allowedScriptOrigins.push('https://playerserver.walkme.com');
+allowedScriptOrigins.push('https://ec.walkme.com');
 
 if (serverConfig.appcuesId) {
   allowedScriptOrigins.push('https://fast.appcues.com');
