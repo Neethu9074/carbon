7075564827663d150afc3701d3d222475906479c
Pradosh E <Pradosh.E@ibm.com>
2023-06-05 16:41:26 +0530

    Mobile time threshold (#12194)
    * Mobile time threshold
    
    * Fixing the metric name issue.
    
    * updating the order of alert channel and time threshold.

--- packages/in-alerting/smart-alerts/mobileApp/dialog/advanced/AdvancedModeContainer.tsx
+++ packages/in-alerting/smart-alerts/mobileApp/dialog/advanced/AdvancedModeContainer.tsx
@@ -12,14 +12,18 @@ import {
 } from 'in-alerting/smart-alerts/components/dialog/AlertConfigDialogPresenter';
 //@ts-expect-error
 import BluePrintSelectionSection from 'in-alerting/smart-alerts/mobileApp/dialog/advanced/BluePrintSelectionSection';
-import StepsContainer from 'in-components/StepsContainer';
+import TimeThresholdConfig from 'in-alerting/smart-alerts/mobileApp/dialog/advanced/TimeThresholdConfig';
 import ConfigureAlertChannel from 'in-alerting/smart-alerts/components/dialog/ConfigureAlertChannel';
+import { fieldTouchedAndInvalid } from 'in-alerting/smart-alerts/components/utils/formUtils';
+import { getBlueprintConfig } from 'in-alerting/smart-alerts/mobileApp/data/blueprintConfig';
+import StepsContainer from 'in-components/StepsContainer';
 import { t } from 'in-i18n';
 
 export default function AdvancedModeContainer(props: AlertConfigDialogPresenterProps & MainDialogControl) {
   const { form, onChange, setSliderState, updateForm, setCustomSlideInHeaderConfig } = props;
   const ruleForm = form.get('rule');
   const alertType = ruleForm.get('alertType').value;
+  const blueprintConfig = getBlueprintConfig(alertType);
 
   return (
     <StepsContainer
@@ -39,6 +43,23 @@ export default function AdvancedModeContainer(props: AlertConfigDialogPresenterP
                 setSliderState={setSliderState}
               />
             </>
+          )
+        },
+        {
+          scrollId: '4',
+          label: t('in-alerting:smartAlerts.mobileApp.advanced.timeThresholdLabel'),
+          title: t('in-alerting:smartAlerts.mobileApp.advanced.timeThresholdTitle'),
+          valid:
+            !fieldTouchedAndInvalid(form.get('timeThreshold')?.get('users')) &&
+            !fieldTouchedAndInvalid(form.get('timeThreshold')?.get('userPercentage')),
+          content: (
+            <TimeThresholdConfig
+              form={form}
+              onChange={onChange}
+              updateForm={updateForm}
+              impactTimeThresholdDisabled={blueprintConfig.impactTimeThresholdDisabled}
+              hasUserImpactOption
+            />
           )
         },
         {
