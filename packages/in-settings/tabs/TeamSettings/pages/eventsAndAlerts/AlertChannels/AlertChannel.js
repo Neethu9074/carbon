86607727a2732cefa74f8f5972da20b16613bcc7
Dan Stingaciu <Dan.Stingaciu@ibm.com>
2023-06-05 11:06:11 -0400

    Z chat ops alert channel implementation (#12207)
    * Initial channel config for z chat ops
    
    * working prototype on ui side
    
    * added create entity function
    
    * Updated config to add show/hide field and updated field names to match backend
    
    * Added styling to zchatops channel form and changed zChatOpsIncidentsUrl to zchatOpsIncidentsUrl
    
    * hiding bearer auth token from now on
    
    * Updated lang file to match release-251
    
    * updated lang file
    
    * Changed from channelNames to channels and updated lang file
    
    * Added beta badge to z chat ops
    
    * Added placeholder text and link to google form
    
    * Updated Z ChatOps URL label

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/AlertChannels/AlertChannel.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/AlertChannels/AlertChannel.js
@@ -181,7 +181,7 @@ function getPropertyValue(entity, key) {
     return getConfig(entity).label;
   }
 
-  if (key === 'password') {
+  if (key === 'password' || key === 'bearerAuthToken') {
     return Array(entity.get(key)?.length ?? 0).join('*');
   }
 
