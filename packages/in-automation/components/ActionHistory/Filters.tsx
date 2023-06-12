f5bc586bf281d134f778e20d30f218c1f8b19f61
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2023-06-12 11:41:45 -0400

    Update Filters.tsx

--- packages/in-automation/components/ActionHistory/Filters.tsx
+++ packages/in-automation/components/ActionHistory/Filters.tsx
@@ -36,7 +36,7 @@ export default function Filters({ setFilter, actionTypes, actionStatuses }: Filt
           { label: t('in-automation:actionHistory.success'), value: 'SUCCESS' },
           { label: t('in-automation:actionHistory.failed'), value: 'FAILED' },
           { label: t('in-automation:actionHistory.inProgress'), value: 'IN_PROGRESS' },
-          { label: t('in-automation:actionHistory.unknown'), value: 'UNKNOWN' }
+          { label: t('in-automation:actionHistory.unknown'), value: 'STATUS_UNKNOWN' }
         ]}
       />
       <Spacer horizontal="small" />
