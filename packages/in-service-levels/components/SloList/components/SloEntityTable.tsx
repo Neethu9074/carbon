dfb1202d139611c1944df7dfc38960bde0b6aea0
BlesyMathew <blessymathew2016@gmail.com>
2023-06-16 14:27:56 +0530

    improvised-code

--- packages/in-service-levels/components/SloList/components/SloEntityTable.tsx
+++ packages/in-service-levels/components/SloList/components/SloEntityTable.tsx
@@ -45,7 +45,7 @@ export default function SloEntityTable({ form, entityList, onChange, progress, q
     ? form.getIn([sloEntityKey, sloApplicationIdKey]).value
     : form.getIn([sloEntityKey, sloWebsiteIdKey]).value;
 
-  const isDataAvailable = entityList?.length ?? 0 > 0;
+  const isDataAvailable = entityList !== undefined && entityList.length > 0;
   const loadMoreData = () => {
     setNext(next + dataPerRow);
   };
@@ -53,30 +53,24 @@ export default function SloEntityTable({ form, entityList, onChange, progress, q
   if (!isDataAvailable) return <NoDataAvailable height={160} text={t('in-service-levels:general.noData')} />;
 
   return (
-    <>
-      {entityList?.length === 0 ||
-        (entityList === undefined && <NoDataAvailable height={160} text={t('in-service-levels:general.noData')} />)}
-      {entityList && (
-        <Ul>
-          {entityList
-            .filter(({ label }) => label.includes(query))
-            .slice(0, next)
-            .map(entityData => {
-              return (
-                <Li onClick={() => onChange(entityData)} key={entityData.id}>
-                  <Stack direction="horizontal">
-                    <CheckboxFancy asRadioButton checked={entityData.id === entityId} onChange={noop} />
-                    <div className={locals.checkBoxItem}>{entityData.label}</div>
-                  </Stack>
-                </Li>
-              );
-            })}
+    <Ul>
+      {entityList
+        .filter(({ label }) => label.includes(query))
+        .slice(0, next)
+        .map(entityData => {
+          return (
+            <Li onClick={() => onChange(entityData)} key={entityData.id}>
+              <Stack direction="horizontal">
+                <CheckboxFancy asRadioButton checked={entityData.id === entityId} onChange={noop} />
+                <div className={locals.checkBoxItem}>{entityData.label}</div>
+              </Stack>
+            </Li>
+          );
+        })}
 
-          <Li onClick={loadMoreData}>
-            <div className={locals.loadMore}>{t('in-service-levels:general.loadMore')}</div>
-          </Li>
-        </Ul>
-      )}
-    </>
+      <Li onClick={loadMoreData} className={locals.loadMore}>
+        {t('in-service-levels:general.loadMore')}
+      </Li>
+    </Ul>
   );
 }
