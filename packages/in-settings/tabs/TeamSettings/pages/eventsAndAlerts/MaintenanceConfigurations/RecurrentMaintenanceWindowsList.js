0c5e991e849494171ac7409a5b1207571ff71d04
Dan Stingaciu <Dan.Stingaciu@ibm.com>
2023-06-22 11:12:52 -0400

    Added timezoneId and UTC/timezone related fixes (#12375)
    * Changed to use occurence to display dates in table, fixed error
    
    * fixe for UTC
    
    * Changed to use occurence to display dates in table, fixed error
    
    * fixe for UTC
    
    * added timezoneId field to scheduling object for MW
    
    * Added checking for UTC option
    
    * Reverted ff
    
    * Added timzone message and added additional checks for parsing time in case we're using UTC time
    
    * reverted FF
    
    * Added check that entity timezone exists
    
    * Added check to see if parsed date is NaN before trying to convert it to zonedTimeToUTC
    
    * enabled FF by default
    
    * enabled ff in more places
    
    * Added additional check for NaN dates in parseDateTimeAsUtc
    
    * Updated comment

--- packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/MaintenanceConfigurations/RecurrentMaintenanceWindowsList.js
+++ packages/in-settings/tabs/TeamSettings/pages/eventsAndAlerts/MaintenanceConfigurations/RecurrentMaintenanceWindowsList.js
@@ -43,8 +43,8 @@ import theme from 'in-themes';
 import { t } from 'in-i18n';
 
 export default function RecurrentMaintenanceWindowsList(props) {
-  const getStartAsString = getFormattedDateTimeFromFirstWindow.bind(null, 'start');
-  const getEndAsString = getFormattedDateTimeFromFirstWindow.bind(null, 'end');
+  const getStartAsString = entity => getFormattedDateTimeOccurence(entity, 'start');
+  const getEndAsString = entity => getFormattedDateTimeOccurence(entity, 'end');
   const [saved, setSaved] = useState(false);
   const allMaintenanceConfigs = useObservable(getMaintenanceConfigsMutableV2, [saved]) ?? indeterminateProgress;
 
@@ -248,16 +248,16 @@ const columnDefinitions = [
     label: t('in-settings:tabs.startTime'),
     ellipsis: true,
     getValue(entity) {
-      if (entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) {
+      if ((entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) || !entity.occurrence) {
         return '';
       }
       return getDateTimeFromFirstWindow('start', entity);
     },
     getContent(entity) {
-      if (entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) {
+      if ((entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) || !entity.occurrence) {
         return '';
       }
-      return getFormattedDateTimeFromFirstWindow('start', entity);
+      return formatDateTime(new Date(entity.occurrence.start));
     }
   },
   {
@@ -268,10 +268,10 @@ const columnDefinitions = [
       return getEndTime(entity);
     },
     getContent(entity) {
-      if (entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) {
+      if ((entity.state === 'UNSCHEDULED' && entity.scheduling.start === 1) || !entity.occurrence) {
         return '';
       }
-      return formatDateTime(getEndTime(entity));
+      return formatDateTime(new Date(entity.occurrence.end));
     }
   },
   {
@@ -347,17 +347,21 @@ const DisplayMWTypes = ({ mwTypeView, setMwTypeView, numbers }) => {
   );
 };
 
-function getFormattedDateTimeFromFirstWindow(key, entity) {
-  if (entity.scheduling && entity.scheduling.start) {
-    return formatDateTime(new Date(entity.scheduling.start));
+function getFormattedDateTimeOccurence(entity, startOrEnd) {
+  if (entity.occurrence) {
+    if (startOrEnd === 'start' && entity.occurrence.start) {
+      return formatDateTime(new Date(entity.occurrence.start));
+    } else if (startOrEnd === 'end' && entity.occurrence.end) {
+      return formatDateTime(new Date(entity.occurrence.end));
+    }
   } else {
     return null;
   }
 }
 
 function getDateTimeFromFirstWindow(key, entity) {
-  if (entity.scheduling && entity.scheduling.start) {
-    return entity.scheduling.start;
+  if (entity.occurrence && entity.occurrence.start) {
+    return entity.occurrence.start;
   } else {
     return null;
   }
