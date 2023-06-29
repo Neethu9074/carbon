bad04b0c0d522466c7e2cbd30ad5c1aaf77b02a1
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2023-06-29 16:12:17 -0400

    enable feature flag

--- packages/in-services/featureFlags.ts
+++ packages/in-services/featureFlags.ts
@@ -146,7 +146,7 @@ export const allowDownloadMetricsFromCharts = isInstanaEmail;
 
 export const perSecondAggregationEnabled = isFeatureFlagEnabled('perSecondAggregationEnabled', false);
 
-export const actionAutomationEnabled = isFeatureFlagEnabled('actionAutomationEnabled', false);
+export const actionAutomationEnabled = isFeatureFlagEnabled('actionAutomationEnabled', true);
 
 export const pluginMetricStatisticsEnabled = isFeatureFlagEnabled('pluginMetricStatisticsEnabled', false);
 
