1223fab906f595bc07e9f89e10998d3c168c0689
Philip Crombach <philip.Crombach@de.ibm.com>
2023-06-30 16:28:14 +0200

    feat(personal-api-tokens): 121986 creation with dialogue (#12482)
    # Why
    Align personal API token UI with Groups UI.
    
    # What
    - streamline UI for creating new personal API tokens
    - migrate personalApiToken API to TS

--- packages/in-settings/tabs/UserSettings/api/personalApiToken.ts
+++ packages/in-settings/tabs/UserSettings/api/personalApiToken.ts
@@ -3,16 +3,32 @@
  * (c) Copyright Instana Inc.
  */
 
-import { create } from '@instana/observables';
+import { Observable, create } from '@instana/observables';
 
 import { getHeader as getCsrfHeader } from 'in-services/security/csrf';
 import createObservable from 'in-services/http/observableHttpResult';
 import memoize from 'in-services/util/memoizingObservableGenerator';
+import { Response } from 'in-services/http/types';
 import http from 'in-services/http';
 
 const refreshSignal = create().emit(true);
 function refresh() {
   refreshSignal.emit(true);
+}
+
+/**
+ * Model for a PersonalApiToken
+ * @property tokenId id of token
+ * @property accessGrantingToken actual token
+ * @property name name of token
+ * @property userId id of user associated with token
+ * @property tenantUnitId of current token
+ */
+export interface PersonalApiToken {
+  readonly tokenId: string;
+  readonly accessGrantingToken: string;
+  readonly name: string;
+  readonly userId: string;
 }
 
 export const getPersonalApiTokensOfUserAsResultObservable = memoize(
@@ -21,10 +37,10 @@ export const getPersonalApiTokensOfUserAsResultObservable = memoize(
   60000
 );
 
-function getPersonalApiTokensOfUserInternal(userId) {
+function getPersonalApiTokensOfUserInternal(userId: string) {
   return refreshSignal.flatMap(() =>
     createObservable(
-      http({
+      http<PersonalApiToken[]>({
         method: 'GET',
         maxRetries: 3,
         url: `/api/settings/personal-api-tokens?userId=${userId}`
@@ -35,10 +51,10 @@ function getPersonalApiTokensOfUserInternal(userId) {
 
 export const getPersonalApiTokenAsResultObservable = memoize(getPersonalApiTokenInternal, id => id, 60000);
 
-function getPersonalApiTokenInternal(id) {
+function getPersonalApiTokenInternal(id: string) {
   return refreshSignal.flatMap(() =>
     createObservable(
-      http({
+      http<PersonalApiToken[]>({
         method: 'GET',
         maxRetries: 3,
         url: `/api/settings/personal-api-tokens/${encodeURIComponent(id)}`
@@ -47,8 +63,8 @@ function getPersonalApiTokenInternal(id) {
   );
 }
 
-export function createPersonalApiToken(personalApiToken) {
-  return http({
+export function createPersonalApiToken(personalApiToken: PersonalApiToken): Observable<PersonalApiToken> {
+  return http<PersonalApiToken>({
     method: 'POST',
     headers: getCsrfHeader(),
     maxRetries: 0,
@@ -57,8 +73,8 @@ export function createPersonalApiToken(personalApiToken) {
   }).map(mapAndRefresh);
 }
 
-export function savePersonalApiToken(personalApiToken) {
-  return http({
+export function savePersonalApiToken(personalApiToken: PersonalApiToken): Observable<PersonalApiToken> {
+  return http<PersonalApiToken>({
     method: 'PUT',
     maxRetries: 3,
     headers: getCsrfHeader(),
@@ -67,8 +83,8 @@ export function savePersonalApiToken(personalApiToken) {
   }).map(mapAndRefresh);
 }
 
-export function deletePersonalApiToken(id) {
-  return http({
+export function deletePersonalApiToken(id: string): Observable<void> {
+  return http<void>({
     method: 'DELETE',
     maxRetries: 3,
     headers: getCsrfHeader(),
@@ -76,7 +92,7 @@ export function deletePersonalApiToken(id) {
   }).map(mapAndRefresh);
 }
 
-function mapAndRefresh(response) {
+function mapAndRefresh<T>(response: Response<T>): T {
   refresh();
   return response.body;
 }
---
+++ packages/in-settings/tabs/UserSettings/pages/PersonalApiTokens/CreatePersonalApiToken/CreatePersonalApiToken_test.tsx
@@ -0,0 +1,129 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import { create } from '@instana/observables';
+
+import {
+  PersonalApiToken,
+  createPersonalApiToken as createToken
+} from 'in-settings/tabs/UserSettings/api/personalApiToken';
+import CreatePersonalApiToken from './CreatePersonalApiToken';
+import { t } from 'in-i18n';
+
+jest.mock('in-i18n', () => ({
+  t: (key: string) => key
+}));
+
+jest.mock('in-settings/tabs/UserSettings/api/personalApiToken');
+
+describe('in-settings/tabs/UserSettings/pages/PersonalApiTokens/CreatePersonalApiToken', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  const mockCreation = (ok: boolean = true) => {
+    const res = create();
+    let emitted: PersonalApiToken | null;
+    if (ok) {
+      emitted = { accessGrantingToken: 'my-token', name: 'my-name', tokenId: 'my-id', userId: 'my-user' };
+      res.emit(emitted);
+    } else {
+      res.emitError(new Error('dummy error'));
+      emitted = null;
+    }
+
+    // @ts-expect-error jest api apparently not supported by TS
+    createToken.mockReturnValue(res);
+    return emitted;
+  };
+
+  it('should render the create form', async () => {
+    const onClose = jest.fn();
+    const { getByText, getByLabelText } = render(<CreatePersonalApiToken onClose={onClose} />);
+
+    // headline for create-form
+    expect(getByText('in-settings:tabs.createPersonalApiToken')).toBeInTheDocument();
+    // label for token name
+    expect(getByText('in-settings:tabs.createPersonalApiTokenNameDescription')).toBeInTheDocument();
+    // input for token name
+    expect(getByLabelText('in-settings:tabs.createPersonalApiTokenNameDescription')).toBeInTheDocument();
+
+    expect(getByText('forms.actions.save')).toBeInTheDocument();
+    expect(getByText('forms.actions.cancel')).toBeInTheDocument();
+    expect(onClose).not.toHaveBeenCalled();
+  });
+
+  it('should allow to cancel whilst being on the create form', async () => {
+    const onClose = jest.fn();
+    const { getByText } = render(<CreatePersonalApiToken onClose={onClose} />);
+
+    const closeBtn = getByText('forms.actions.cancel');
+    fireEvent.click(closeBtn);
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render an error message if the creation fails', () => {
+    const onClose = jest.fn();
+    mockCreation(false);
+
+    const { container, getByLabelText, getByText } = render(<CreatePersonalApiToken onClose={onClose} />);
+
+    const input = getByLabelText('in-settings:tabs.createPersonalApiTokenNameDescription');
+    fireEvent.change(input, { target: { value: '123' } });
+    fireEvent.submit(container.querySelector('form') as HTMLFormElement);
+
+    waitFor(() => expect(getByText('in-components:error.erroneousResultPresenterMessage')).toBeInTheDocument());
+    expect(createToken).toHaveBeenCalledTimes(1);
+    expect(onClose).not.toHaveBeenCalled();
+  });
+
+  it('should render token details after a successful creation', async () => {
+    const onClose = jest.fn();
+    const token = mockCreation();
+
+    const { container, getByLabelText, queryByText, getByText } = render(<CreatePersonalApiToken onClose={onClose} />);
+
+    const input = getByLabelText('in-settings:tabs.createPersonalApiTokenNameDescription');
+    fireEvent.change(input, { target: { value: '123' } });
+    fireEvent.submit(container.querySelector('form') as HTMLFormElement);
+
+    expect(createToken).toHaveBeenCalled();
+    expect(queryByText(t('in-components:error.erroneousResultPresenterMessage'))).not.toBeInTheDocument();
+
+    expect(onClose).not.toHaveBeenCalled();
+    expect(getByText('in-settings:tabs.createdPersonalApiToken')).toBeInTheDocument();
+    expect(queryByText('in-settings:tabs.createPersonalApiToken')).not.toBeInTheDocument();
+
+    expect(getByText('in-settings:tabs.personalApiTokenDescription')).toBeInTheDocument();
+    expect(getByText('in-settings:tabs.displayName')).toBeInTheDocument();
+    expect(getByText('in-settings:tabs.token')).toBeInTheDocument();
+    expect(getByText(token!.name)).toBeInTheDocument();
+
+    const closeBtn = getByText('forms.actions.close');
+    fireEvent.click(closeBtn);
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle a submit click for the token creation', async () => {
+    const onClose = jest.fn();
+    mockCreation();
+
+    const { getByLabelText, queryByText, getByText } = render(<CreatePersonalApiToken onClose={onClose} />);
+
+    const input = getByLabelText('in-settings:tabs.createPersonalApiTokenNameDescription');
+    fireEvent.change(input, { target: { value: '123' } });
+    const submitBtn = getByText('forms.actions.save') as HTMLButtonElement;
+    fireEvent.click(submitBtn);
+
+    expect(createToken).toHaveBeenCalled();
+    expect(queryByText(t('in-components:error.erroneousResultPresenterMessage'))).not.toBeInTheDocument();
+
+    expect(onClose).not.toHaveBeenCalled();
+  });
+});
