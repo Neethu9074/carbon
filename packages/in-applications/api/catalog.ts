3e2abaa3c4b85907ddb1da9a5230d2b88ab99495
BlesyMathew <blessymathew2016@gmail.com>
2023-07-14 21:38:13 +0530

    removed-unwanted-changes

--- packages/in-applications/api/catalog.ts
+++ packages/in-applications/api/catalog.ts
@@ -12,24 +12,28 @@ import http from 'in-services/http';
 
 const basePath = '/api/application-monitoring/catalog';
 
-export const getApplicationTagCatalog =
-  ({ dataSource, useCase }: { dataSource: ApplicationDataSource; useCase: CatalogUseCase }) =>
-  ({ timeConfig }: { timeConfig: TimeConfig }): Observable<Result<TagCatalog>> => {
-    // round down the from timestamp to the beginning of the week to make the caching more efficient
-    const from = timeConfig ? roundDownToWeek((timeConfig.to || Date.now()) - timeConfig.windowSize) : undefined;
+export const getApplicationTagCatalog = ({
+  dataSource,
+  useCase
+}: {
+  dataSource: ApplicationDataSource;
+  useCase: CatalogUseCase;
+}) => ({ timeConfig }: { timeConfig: TimeConfig }): Observable<Result<TagCatalog>> => {
+  // round down the from timestamp to the beginning of the week to make the caching more efficient
+  const from = timeConfig ? roundDownToWeek((timeConfig.to || Date.now()) - timeConfig.windowSize) : undefined;
 
-    return isTroubleshootingModeEnabled$.flatMap(includeInternalTags =>
-      http<TagCatalog>({
-        method: 'GET',
-        maxRetries: 3,
-        url: basePath,
-        mapToResultObject: true,
-        queryParams: {
-          from,
-          dataSource,
-          useCase,
-          includeInternalTags
-        }
-      })
-    );
-  };
+  return isTroubleshootingModeEnabled$.flatMap(includeInternalTags =>
+    http<TagCatalog>({
+      method: 'GET',
+      maxRetries: 3,
+      url: basePath,
+      mapToResultObject: true,
+      queryParams: {
+        from,
+        dataSource,
+        useCase,
+        includeInternalTags
+      }
+    })
+  );
+};
