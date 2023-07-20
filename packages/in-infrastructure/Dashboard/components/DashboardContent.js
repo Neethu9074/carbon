cf4419c1efb431e006fc62201ef7f1e051a073e2
Jean Detoeuf <Jean.Detoeuf1@ibm.com>
2023-07-20 14:46:45 +0200

    get metric explosion as a global processing issue (#12725)

--- packages/in-infrastructure/Dashboard/components/DashboardContent.js
+++ packages/in-infrastructure/Dashboard/components/DashboardContent.js
@@ -9,7 +9,6 @@ import { fromPromise, combineLatest, just } from '@instana/observables';
 import { useObservable } from '@instana/hooks';
 
 import AgentMonitoringIssueNotifications from 'in-infrastructure/Dashboard/components/AgentMonitoringIssueNotifications';
-import MetricExplosionNotification from 'in-infrastructure/Dashboard/components/MetricExplosionNotification';
 import { selectedSnapshot$, selectedSnapshotId$, getSnapshotVersions } from 'in-stores/snapshot';
 import DashboardHeader from 'in-infrastructure/Dashboard/components/DashboardHeader';
 import SidebarContent from 'in-map/components/MapSidebar/components/SidebarContent';
@@ -116,7 +115,6 @@ export default connectTo(
               </div>
               <div className={locals.content}>
                 <AgentMonitoringIssueNotifications snapshot={snapshot} timeConfig={timeConfig} />
-                <MetricExplosionNotification snapshot={snapshot} />
                 <Jail component={DashboardImpl} props={{ snapshot, timeConfig }} />
               </div>
             </div>
