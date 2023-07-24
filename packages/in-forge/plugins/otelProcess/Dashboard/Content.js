b970d2fc055d29450cd87e7d776f77ebce3a0567
Jean Detoeuf <Jean.Detoeuf1@ibm.com>
2023-07-24 12:00:36 +0200

    Fix broken non-linux otelhost icons and remove hostsnapshot in otelprocess (#12662) (#12744)
    * Remove hostsnashot avoid blank otelprocess widget
    
    * Fix broken non-linux host icons
    
    * Rename os.name to ostype for otel host plugin
    
    * Change snapshot to snapshotOrPlugin
    
    Co-authored-by: Zhi Li <bjlizhi@cn.ibm.com>

--- packages/in-forge/plugins/otelProcess/Dashboard/Content.js
+++ packages/in-forge/plugins/otelProcess/Dashboard/Content.js
@@ -11,72 +11,62 @@ import { KpiSection, KpiKeyValue } from 'in-sdk/components/dashboard/KpiSection'
 import PluginDashboardsMarkerLanes from 'in-forge/PluginDashboardsMarkerLanes';
 import OtelProcessesList from 'in-forge/plugins/otelProcess/OtelProcessesList';
 import DashboardSection from 'in-sdk/components/dashboard/DashboardSection';
-import getHostSnapshotId from 'in-subscription/getHostSnapshotId';
 import Columize from 'in-sdk/components/dashboard/Columize';
 import MetricValue from 'in-components/MetricValue';
-import { getSnapshot } from 'in-stores/snapshot';
-import connectTo from 'in-hoc/connectTo';
 import { t } from 'in-i18n';
 
-export default connectTo(
-  ({ snapshot }) => ({ hostSnapshot: getHostSnapshotId(snapshot).flatMap(getSnapshot) }),
-
-  function ProcessDashboard({ snapshot, timeConfig, hostSnapshot }) {
-    if (!hostSnapshot) {
-      return null;
-    }
-    const snapshotId = snapshot.get('id');
-    return (
-      <div>
-        <KpiSection>
-          <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_user')}>
-            <MetricValue snapshotId={snapshotId} metric="cpu_time.user" formatter={timeBySecondsTwoDecimalPlaces} />
-          </KpiKeyValue>
-          <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_system')}>
-            <MetricValue snapshotId={snapshotId} metric="cpu_time.system" formatter={timeBySecondsTwoDecimalPlaces} />
-          </KpiKeyValue>
-          <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_wait')}>
-            <MetricValue snapshotId={snapshotId} metric="cpu_time.wait" formatter={timeBySecondsTwoDecimalPlaces} />
-          </KpiKeyValue>
-        </KpiSection>
-        <Columize>
-          <DashboardSection title={t('in-forge:plugins.otelProcess.dashboard.memory')}>
-            <Chart
-              snapshotId={snapshotId}
-              timeConfig={timeConfig}
-              y1={{
-                min: 0,
-                formatter: bytesTwoDecimalPlaces,
-                metrics: ['memory.usage', 'memory.virtual'],
-                labels: [
-                  t('in-forge:plugins.otelProcess.dashboard.memoryUsage'),
-                  t('in-forge:plugins.otelProcess.dashboard.memoryVirtual')
-                ],
-                type: 'line'
-              }}
-              renderPostChartContent={PluginDashboardsMarkerLanes}
-            />
-          </DashboardSection>
-          <DashboardSection title={t('in-forge:plugins.otelProcess.dashboard.disk')}>
-            <Chart
-              snapshotId={snapshotId}
-              timeConfig={timeConfig}
-              y1={{
-                min: 0,
-                formatter: bytesTwoDecimalPlaces,
-                metrics: ['disk_io.read', 'disk_io.write'],
-                labels: [
-                  t('in-forge:plugins.otelProcess.dashboard.diskRead'),
-                  t('in-forge:plugins.otelProcess.dashboard.diskWrite')
-                ],
-                type: 'line'
-              }}
-              renderPostChartContent={PluginDashboardsMarkerLanes}
-            />
-          </DashboardSection>
-        </Columize>
-        <OtelProcessesList snapshotId={snapshotId} />
-      </div>
-    );
-  }
-);
+export default function ProcessDashboard({ snapshot, timeConfig }) {
+  const snapshotId = snapshot.get('id');
+  return (
+    <div>
+      <KpiSection>
+        <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_user')}>
+          <MetricValue snapshotId={snapshotId} metric="cpu_time.user" formatter={timeBySecondsTwoDecimalPlaces} />
+        </KpiKeyValue>
+        <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_system')}>
+          <MetricValue snapshotId={snapshotId} metric="cpu_time.system" formatter={timeBySecondsTwoDecimalPlaces} />
+        </KpiKeyValue>
+        <KpiKeyValue label={t('in-forge:plugins.otelProcess.dashboard.cpu_time_wait')}>
+          <MetricValue snapshotId={snapshotId} metric="cpu_time.wait" formatter={timeBySecondsTwoDecimalPlaces} />
+        </KpiKeyValue>
+      </KpiSection>
+      <Columize>
+        <DashboardSection title={t('in-forge:plugins.otelProcess.dashboard.memory')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            y1={{
+              min: 0,
+              formatter: bytesTwoDecimalPlaces,
+              metrics: ['memory.usage', 'memory.virtual'],
+              labels: [
+                t('in-forge:plugins.otelProcess.dashboard.memoryUsage'),
+                t('in-forge:plugins.otelProcess.dashboard.memoryVirtual')
+              ],
+              type: 'line'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+        <DashboardSection title={t('in-forge:plugins.otelProcess.dashboard.disk')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            y1={{
+              min: 0,
+              formatter: bytesTwoDecimalPlaces,
+              metrics: ['disk_io.read', 'disk_io.write'],
+              labels: [
+                t('in-forge:plugins.otelProcess.dashboard.diskRead'),
+                t('in-forge:plugins.otelProcess.dashboard.diskWrite')
+              ],
+              type: 'line'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+      </Columize>
+      <OtelProcessesList snapshotId={snapshotId} />
+    </div>
+  );
+}
