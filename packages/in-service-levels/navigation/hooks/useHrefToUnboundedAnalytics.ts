e943fbe65449ac85d157f059a48c175f77fa8b16
Thiemo Krause <Thiemo.Krause@ibm.com>
2023-08-04 14:02:21 +0200

    fix(service-levels): support custom indicators for links to UA (#12858)
    The typing of the ServiceLevelIndicators was a little incorrect and
    guaranteed the presence of the blueprint field, while in reality it
    wasn't available for custom event based indicators, which don't use a
    blueprint.
    
    This now ensures the link generation falls back to call count and beacon
    count metrics with SUM aggregation when encountering a missing
    blueprint. This will be improved upon in the future once proper typing
    for these cases is available

--- packages/in-service-levels/navigation/hooks/useHrefToUnboundedAnalytics.ts
+++ packages/in-service-levels/navigation/hooks/useHrefToUnboundedAnalytics.ts
@@ -109,7 +109,6 @@ function getLocationToUnboundedAnalytics({
     return getWebsiteEntityHref({
       entity,
       indicator,
-      blueprint,
       timeConfig,
       tagFilterExpression,
       location
@@ -127,11 +126,11 @@ interface NavigationType {
 
 interface BaseGenerateHrefProps extends NavigationType {
   tagFilterExpression: TagFilterExpression;
-  blueprint: BlueprintType;
   timeConfig: TimeConfig;
 }
 
 interface GetApplicationSloHrefProps extends BaseGenerateHrefProps {
+  blueprint: BlueprintType;
   entity: ApplicationSloEntity;
 }
 
@@ -163,12 +162,12 @@ function getWebsiteEntityHref({
   location,
   entity,
   indicator,
-  blueprint,
   timeConfig,
   tagFilterExpression
 }: GetWebsiteSloHrefProps): Location {
   const { beaconType } = entity;
-  const aggregation = isTimeBasedSli(indicator) ? indicator.aggregation : 'MEAN';
+  const { blueprint } = indicator;
+  const aggregation = isTimeBasedSli(indicator) ? indicator.aggregation : undefined;
   const analyzeParameters = createParameters(websiteAnalyzePath);
 
   return updateLocationForWebsiteEntity({
@@ -182,9 +181,16 @@ function getWebsiteEntityHref({
   });
 }
 
-const applicationChartMetrics: Record<BlueprintType, [string, string]> = Object.freeze({
+type MetricAggregationTuple = [string, AggregationType];
+
+const applicationChartMetrics: Record<BlueprintType, MetricAggregationTuple> = Object.freeze({
   latency: ['latency', 'DISTRIBUTION'],
   availability: ['calls', 'SUM']
+});
+
+const websiteChartMetrics: Record<BlueprintType, MetricAggregationTuple> = Object.freeze({
+  latency: ['beaconDuration', 'MEAN'],
+  availability: ['beaconErrorRate', 'MEAN']
 });
 
 interface UpdateLocationForEntityProps {
@@ -212,7 +218,7 @@ function updateLocationForApplicationEntity({
   const hasEndpoint = endpointId !== undefined;
   const groupbyTag = hasEndpoint ? 'endpoint.name' : 'service.name';
   const groupBy = createGroupBy(groupbyTag, entityTypes.DESTINATION);
-  const chartedMetrics = [createChartedMetric(...applicationChartMetrics[blueprint])];
+  const chartedMetrics = [createChartedMetric(...getApplicationMetric(blueprint))];
 
   const newLocation = setDefaultMatrixParameter({
     location,
@@ -229,7 +235,7 @@ function updateLocationForApplicationEntity({
 }
 
 interface UpdateLocationForWebsiteEntityProps extends UpdateLocationForEntityProps {
-  aggregation: AggregationType;
+  aggregation?: AggregationType;
   beaconType: WebsiteBeaconType;
 }
 
@@ -242,10 +248,10 @@ function updateLocationForWebsiteEntity({
   tagFilterExpression,
   analyzeParameters
 }: UpdateLocationForWebsiteEntityProps): Location {
-  const isLatencyBlueprint = blueprint === 'latency';
-  const metricId = isLatencyBlueprint ? 'beaconDuration' : 'beaconErrorRate';
-  const chartedMetrics = [createChartedMetric(metricId, aggregation)];
-  const fields = [createMetricField(metricId, aggregation)];
+  const [metricId, defaultAggregation] = getWebsiteMetric(blueprint);
+  const chartedAggregation = aggregation ?? defaultAggregation;
+  const chartedMetrics = [createChartedMetric(metricId, chartedAggregation)];
+  const fields = [createMetricField(metricId, chartedAggregation)];
   const groupBy = createGroupBy('beacon.location.url');
 
   const newLocation = setDefaultMatrixParameter({
@@ -300,4 +306,20 @@ function setDefaultMatrixParameter({
   );
 
   return newLocation;
+}
+
+function getApplicationMetric(blueprint?: BlueprintType): MetricAggregationTuple {
+  if (!blueprint || !Object.keys(applicationChartMetrics).includes(blueprint ?? '')) {
+    // Fall back to plain calls in case of an unsupported blueprint type
+    return applicationChartMetrics['availability'];
+  }
+  return applicationChartMetrics[blueprint];
+}
+
+function getWebsiteMetric(blueprint?: BlueprintType): MetricAggregationTuple {
+  if (!blueprint || !Object.keys(websiteChartMetrics).includes(blueprint ?? '')) {
+    // Fall back to plain beaconCount in case of an unsupported blueprint type
+    return ['beaconCount', 'SUM'];
+  }
+  return websiteChartMetrics[blueprint];
 }
