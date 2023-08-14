99077f9af88c8f53e5b61505fb2222f939e7970b
Jeffrey Meng <Jeffrey.Meng@ibm.com>
2023-08-14 10:59:02 -0700

    fix: add beta badge to associated actions card

--- packages/in-automation/AssociatedActions/RecommendedActionsCard.tsx
+++ packages/in-automation/AssociatedActions/RecommendedActionsCard.tsx
@@ -63,6 +63,8 @@ export default function RecommendedActionsCard({ event, volatileId, reload, setR
     <>
       {error && <NotificationComponent failure>{t('in-automation:failedToSaveAssocation')}</NotificationComponent>}
       <ActionTable
+        title={t('in-automation:associatedActions')}
+        isBeta
         noDataMessage={t('in-automation:noRecommendedActionsAvailable')}
         showActionLink
         event={event}
@@ -70,6 +72,7 @@ export default function RecommendedActionsCard({ event, volatileId, reload, setR
         pageSize={5}
         isSearchable={false}
         loadEntities={() => getUnusedSuggestedActions}
+        rightHeader={<></>}
         scored
         tableActions={{
           select: {
