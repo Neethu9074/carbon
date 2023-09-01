2916234c95775942f0ef77118d23b0f4b5d36a48
Jing Jing Zhang <zhangjingjing@Jings-MacBook-Pro.local>
2023-09-01 17:55:46 +0800

    Update for review comments

--- packages/in-forge/plugins/ibmMqQueueManager/Info.js
+++ packages/in-forge/plugins/ibmMqQueueManager/Info.js
@@ -6,13 +6,10 @@
 import React from 'react';
 
 import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
-import List from 'in-sdk/components/sidebar/List';
 import { t } from 'in-i18n';
 
 export default function Info({ snapshot }) {
   const data = snapshot.get('data');
-  const standbyNodes = snapshot.getIn(['data', 'standbyNodes']);
-  const elsewhereNodes = snapshot.getIn(['data', 'elsewhereNodes']);
 
   return (
     <DescriptionList>
@@ -37,24 +34,6 @@ export default function Info({ snapshot }) {
       <DescriptionItem title={t('in-forge:plugins.ibmMqQueueManager.maxHandles')}>
         {data.get('maxHandles')}
       </DescriptionItem>
-      <DescriptionItem title={t('in-forge:plugins.ibmMqQueueManager.haType')}>{data.get('haType')}</DescriptionItem>
-      <DescriptionItem title={t('in-forge:plugins.ibmMqQueueManager.runningNode')}>
-        {data.get('runningNode')}
-      </DescriptionItem>
-      {standbyNodes && (
-        <DescriptionItem title={t('in-forge:plugins.ibmMqQueueManager.standbyNodes')}>
-          {standbyNodes.map((item, index) => (
-            <List.Item key={index}>{item}</List.Item>
-          ))}
-        </DescriptionItem>
-      )}
-      {elsewhereNodes && (
-        <DescriptionItem title={t('in-forge:plugins.ibmMqQueueManager.elsewhereNodes')}>
-          {elsewhereNodes.map((item, index) => (
-            <List.Item key={index}>{item}</List.Item>
-          ))}
-        </DescriptionItem>
-      )}
     </DescriptionList>
   );
 }
