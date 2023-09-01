044993c4dbf16300e65f7e8a0fa5d602cad96cdd
Benjamin Kan <kan@ibm.com>
2023-09-01 10:11:58 -0700

    Basic handling of an SLO issue in the UI (#13089)
    * Basic handling of an SLO issue in the UI
    
    * refactor(service-levels): minor cleanup and restructuring
    
    Co-authored-by: Thiemo-Krause <thiemo.krause@ibm.com>

---
+++ packages/in-events/components/EventContent/SloEventContent.tsx
@@ -0,0 +1,39 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2022
+ */
+
+import React from 'react';
+
+import { Card } from '@instana/components';
+
+import SloScopePath from 'in-alerting/smart-alerts/slo/components/SloScopePath';
+import ProblemDescription from 'in-events/components/legacy/ProblemDescription';
+import { Row, Col } from 'in-components/layout/Grid';
+import { EventOrMap } from 'in-events/types';
+import { t } from 'in-i18n';
+
+interface Props {
+  event: EventOrMap;
+}
+
+export default function SloEventContent({ event }: Props) {
+  const fixSuggestion = event.getIn(['problem', 'fixSuggestion'], '');
+  const sloId = event.getIn(['metadata', 'sloId']);
+  const entityLabel = event.getIn(['metadata', 'entityLabel'], '');
+
+  return (
+    <>
+      <Row withoutSideMargin>
+        <Col xs>
+          <Card title={t('in-events:titleDescription')}>
+            <SloScopePath sloId={sloId} sloLabel={entityLabel} />
+
+            <ProblemDescription fixSuggestion={fixSuggestion} className="in-event-view-event-content" />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
