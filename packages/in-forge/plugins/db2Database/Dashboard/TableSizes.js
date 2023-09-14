7babd26a2f693ced6c124f653b8d259f4cdb447a
Shibu <n.shibu@ibm.com>
2023-09-14 19:00:52 +0530

    Runstats,tableutilities,Tablesize sort order and Reorg changes

--- packages/in-forge/plugins/db2Database/Dashboard/TableSizes.js
+++ packages/in-forge/plugins/db2Database/Dashboard/TableSizes.js
@@ -1,6 +1,7 @@
 /*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc.
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
  */
 
 import React from 'react';
@@ -121,8 +122,8 @@ export default connectTo(
         }
         cols={cols}
         rows={rows}
-        initialSortColumn={0}
-        initialSortDirection="asc"
+        initialSortColumn={4}
+        initialSortDirection="desc"
       />
     );
   }
