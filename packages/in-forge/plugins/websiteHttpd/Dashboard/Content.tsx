78273a90c9215ed52f5ee8e1beca7afa7cbbf8c3
ZHI HONG WANG <wangzhih@cn.ibm.com>
2023-09-28 18:38:32 +0800

    EUM Sensor 1.0.3: Add sensor for measuring website auto instrumentation
    * EUM Sensor 1.0.3: Add sensor for measuring website auto instrumentation
    
    * Add links to website dashboard on sidebar and content of entity dashboard
    
    * Fix content style etc.
    
    * Lift @instana/components version to 2.17.0
    
    * Revise code owners

---
+++ packages/in-forge/plugins/websiteHttpd/Dashboard/Content.tsx
@@ -0,0 +1,37 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { Button } from '@instana/components';
+
+import DashboardNotification from 'in-sdk/components/dashboard/DashboardNotification';
+import { useLinkToWebsite } from 'in-websites/navigation/paths';
+import { t } from 'in-i18n';
+
+import locals from './Content.mless';
+
+export default function EumWebsiteHttpdDashboard({ snapshot }: { snapshot: any}) {
+  const websiteId = snapshot.get('data').get('key');
+  const websiteHref = useLinkToWebsite(websiteId);
+
+  return (
+    <>
+      {websiteId && websiteHref && (
+        <div className={locals.mainSection}>
+          <Button kind="primary" icon="lib_website" href={websiteHref}>
+            {t('in-forge:plugins.eum.dashboard.openDashboard')}
+          </Button>
+        </div>
+      )}
+
+      <DashboardNotification type="info">
+        {t('in-forge:plugins.eum.dashboard.thereIsNoFurtherInformationAboutThisEntity')}
+      </DashboardNotification>
+    </>
+  );
+}
+
---
+++ packages/in-forge/plugins/websiteHttpd/Info.tsx
@@ -0,0 +1,51 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
+import { formatDateTime, fromNowAccurately } from 'in-services/formatters/date';
+import { useLinkToWebsite } from 'in-websites/navigation/paths';
+import { emptyList } from 'in-services/fixedImmutables';
+import { t } from 'in-i18n';
+
+export default function EumWebsiteInfo({ snapshot }: { snapshot: any }) {
+  const data = snapshot.get('data');
+  const websiteId = data.get('key');
+  const websiteHref = useLinkToWebsite(websiteId);
+  const lastModified = data.get('lastModified');
+
+  return (
+    <DescriptionList>
+      <DescriptionItem title={t('in-forge:plugins.eum.websiteName')}>
+        <a href={websiteHref}>{data.get('name')}</a>
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.webserverType')}>{data.get('wsType')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.pid')}>{data.get('pid')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.websitekey')}>{websiteId}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.enabled')}>
+        {data.get('enabled') ? t('in-forge:plugins.eum.true') : t('in-forge:plugins.eum.false')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.aggressive')}>
+        {data.get('aggressive') ? t('in-forge:plugins.eum.true') : t('in-forge:plugins.eum.false')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.contentTypes')}>
+        {data.get('contentTypes', emptyList).join(', ')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.trackSessions')}>
+        {data.get('trackSessions') ? t('in-forge:plugins.eum.true') : t('in-forge:plugins.eum.false')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.jsAgentUrl')}>{data.get('jsAgentUrl')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.reportingUrl')}>{data.get('reportingUrl')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.eum.mode')}>{data.get('mode')}</DescriptionItem>
+      {lastModified && (
+        <DescriptionItem title={t('in-forge:plugins.eum.lastModified')}>
+          {formatDateTime(lastModified)} ({fromNowAccurately(lastModified)})
+        </DescriptionItem>
+      )}
+    </DescriptionList>
+  );
+}
