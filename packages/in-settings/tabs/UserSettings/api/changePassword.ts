f7b20612bed11e32c9759041c425ff736f89faed
Philip Crombach <philip.Crombach@de.ibm.com>
2023-09-28 13:13:14 +0200

    chore(password-change): remove unintentional committed (#13399)
    # Why
    This was unintentionally committed...
    
    # What
    removal

--- packages/in-settings/tabs/UserSettings/api/changePassword.ts
+++ packages/in-settings/tabs/UserSettings/api/changePassword.ts
@@ -6,23 +6,12 @@
 import { getHeader as getCsrfHeader } from 'in-services/security/csrf';
 import http from 'in-services/http';
 
-/*
-  private final String password;
-  @NotBlank
-  @Length(min = 3, max = 128)
-  private final String newPassword;
-  @NotBlank
-  @Length(min = 3, max = 128)
-  private final String repeatedPassword;
-*/
-
 export interface ChangePasswordRequest {
   readonly password: string;
   readonly newPassword: string;
   readonly repeatedPassword: string;
 }
 
-// TODO sends a redirect
 export function changePassword(data: ChangePasswordRequest) {
   return http({
     method: 'POST',
