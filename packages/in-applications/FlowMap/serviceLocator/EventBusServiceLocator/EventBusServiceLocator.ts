d9b6f167743372d52823a9353e3949d5a7b41e1b
Thiemo Krause <Thiemo.Krause@ibm.com>
2023-10-04 20:34:31 +0200

    chore(dependencies): upgrade @instana/roemitter (#13436)
    This brings in ts support for the roemitter. A little bit of cleanup to
    adjust to the new types was also necessary

--- packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusServiceLocator.ts
+++ packages/in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusServiceLocator.ts
@@ -3,22 +3,26 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Subject } from '@instana/observables';
+
 // there is no null service needed, so always use the default implementation
 import createNullService, {
   EventBusService
 } from 'in-applications/FlowMap/serviceLocator/EventBusServiceLocator/EventBusService';
 import BaseServiceLocator from 'in-applications/FlowMap/serviceLocator/BaseServiceLocator';
 
-export default class EventBusServiceLocator extends BaseServiceLocator<EventBusService> {
+export default class EventBusServiceLocator<Topics extends Record<string, boolean>> extends BaseServiceLocator<
+  EventBusService<Topics>
+> {
   constructor() {
     super(createNullService);
   }
 
-  emit(msg: string, payload: boolean) {
+  emit<Topic extends keyof Topics>(msg: Extract<Topic, string>, payload: Topics[Topic]) {
     return this.service.emit(msg, payload);
   }
 
-  on(msg: string) {
+  on<Topic extends keyof Topics>(msg: Extract<Topic, string>): Subject<Topics[Topic]> {
     return this.service.on(msg);
   }
 }
