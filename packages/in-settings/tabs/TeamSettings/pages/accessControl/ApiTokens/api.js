c9ba7f1f61be979757251b6061662cfa12f6427d
Priyanka-Gupta28 <priyanka.gupta28@ibm.com>
2023-10-09 11:53:29 +0530

    working on 140014

--- packages/in-settings/tabs/TeamSettings/pages/accessControl/ApiTokens/api.js
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/ApiTokens/api.js
@@ -3,19 +3,8 @@
  * (c) Copyright Instana Inc.
  */
 
-/* import { create } from '@instana/observables'; */
-
-/* import createObservable from 'in-services/http/observableHttpResult';
-import memoize from 'in-services/util/memoizingObservableGenerator'; */
-import http from 'in-services/http';
 import { getHeader as getCsrfHeader } from 'in-services/security/csrf';
-
-/* const refreshSignal = create().emit(true);
-
-export const getApiTokensAsResultObservable = memoize(getApiTokensAsResultObservableInternal, () => '', 60000);
-function getApiTokensAsResultObservableInternal() {
-  return refreshSignal.flatMap(() => createObservable(getApiTokens()));
-} */
+import http from 'in-services/http';
 
 export function getApiTokens() {
   return http({
