a4c92a6bca112659bf3de5d5ad9ba9f43139232f
angel-neethu <angel.neethu.ej@ibm.com>
2023-10-12 16:54:36 +0530

    chore(TS migration): migrate in-components/QueryBuilder/ConjunctionTagSelectorOverlay/ConjunctionTagSelectorOverlay

--- packages/in-components/QueryBuilder/ConjunctionTagSelectorOverlay/ConjunctionTagSelectorOverlay.tsx
+++ packages/in-components/QueryBuilder/ConjunctionTagSelectorOverlay/ConjunctionTagSelectorOverlay.tsx
@@ -3,23 +3,34 @@
  * (c) Copyright Instana Inc.
  */
 
-import PropTypes from 'prop-types';
 import React from 'react';
 
+//@ts-expect-error TS migration needed
 import ConjunctionsAndBrackets from 'in-components/QueryBuilder/ConjunctionTagSelectorOverlay/ConjunctionsAndBrackets';
+//@ts-expect-error TS migration needed
 import TagSelectorOverlay from 'in-components/TagSelectorOverlay/TagSelectorOverlay';
+import { EnrichedTagCatalog } from 'in-services/tags/tagCatalog';
+import { FormModelElement } from '../transformation/formModel';
+import { TagFilter } from 'in-types';
 
+interface ConjunctionTagSelectorOverlayProps {
+  tagCatalog: any;
+  onChange: (arg: FormModelElement) => void;
+  close: VoidFunction;
+  withoutOrConjunction?: boolean;
+  withoutBrackets?: boolean;
+}
 export default function ConjunctionTagSelectorOverlay({
   tagCatalog,
   onChange,
   close,
   withoutOrConjunction = false,
   withoutBrackets = false
-}) {
+}: ConjunctionTagSelectorOverlayProps) {
   return (
     <>
       <ConjunctionsAndBrackets
-        onChange={v => {
+        onChange={(v: FormModelElement) => {
           onChange(v);
           close();
         }}
@@ -28,13 +39,13 @@ export default function ConjunctionTagSelectorOverlay({
       />
 
       <TagSelectorOverlay
-        onChange={({ name }) => {
+        onChange={({ name }: { name: string }) => {
           onChange({
             type: 'TAG_FILTER',
             name,
             operator: 'EQUALS',
             value: setDefaultValueWhenTagTypeBoolean(name, tagCatalog)
-          });
+          } as TagFilter);
         }}
         close={close}
         tagCatalog={tagCatalog}
@@ -43,7 +54,7 @@ export default function ConjunctionTagSelectorOverlay({
   );
 }
 
-function setDefaultValueWhenTagTypeBoolean(tagName, tagCatalog) {
+function setDefaultValueWhenTagTypeBoolean(tagName: string, tagCatalog: EnrichedTagCatalog) {
   const tagTreeNode = tagCatalog.tagsByName[tagName];
   if (tagTreeNode.type === 'BOOLEAN') {
     return true;
@@ -51,11 +62,3 @@ function setDefaultValueWhenTagTypeBoolean(tagName, tagCatalog) {
 
   return undefined;
 }
-
-ConjunctionTagSelectorOverlay.propTypes = {
-  tagCatalog: PropTypes.any.isRequired,
-  onChange: PropTypes.func.isRequired,
-  close: PropTypes.func.isRequired,
-  withoutOrConjunction: PropTypes.bool,
-  withoutBrackets: PropTypes.bool
-};
