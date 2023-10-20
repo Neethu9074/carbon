db486c6d8639ae98307dba441462f3986a5ce58d
Esteban Diaz Gallego <ediazgallego@ibm.com>
2023-10-20 15:19:03 -0400

    KB-141540 Integrate Timeline information into by page subrows. (#13628)

--- packages/in-synthetics/dashboards/details/components/Entry.tsx
+++ packages/in-synthetics/dashboards/details/components/Entry.tsx
@@ -1,7 +1,7 @@
 /*
  * IBM Confidential
  * PID 5737-N85, 5900-AG5
- * Copyright IBM Corp. 2022
+ * Copyright IBM Corp. 2023
  */
 
 import React, { Fragment, useState } from 'react';
@@ -10,53 +10,78 @@ import { toInteractiveElement } from '@instana/components';
 import { t } from '@instana/i18n-react';
 
 import { bytesZeroDecimalPlaces, millisToTwoDecimalSeconds } from 'in-services/formatters/number';
+import BrowserTimeline from 'in-synthetics/dashboards/details/components/browser/BrowserTimeline';
 import { Di, Dl } from 'in-components/HorizontalDescriptionList/HorizontalDescriptionList';
 import KeyValueHeader from 'in-synthetics/dashboards/details/components/KeyValueHeader';
 import ToggleIcon from 'in-synthetics/dashboards/details/components/ToggleIcon';
 import Timings from 'in-synthetics/dashboards/details/components/Timings';
 import { getType, types } from 'in-synthetics/utils/browserFileTypes';
+import useResizeObserverCustom from 'in-hooks/useResizeObserver';
 import { TestResultEntry } from 'in-synthetics/utils/constants';
 import { Row, Col } from 'in-components/layout/Grid';
 import Tooltip from 'in-components/Tooltip/Tooltip';
 import Pill from 'in-components/Pill';
 
-import locals from './Subtransaction.mless';
+import locals from 'in-synthetics/dashboards/details/components/Entry.mless';
 
-type EntryProps = {
+interface EntryProps {
   entry: TestResultEntry;
-};
+  earliestTimestamp: number;
+  endTimestamp: number;
+}
 
-export default function Entry({ entry }: EntryProps) {
+const Entry = ({ entry, earliestTimestamp, endTimestamp }: EntryProps) => {
+  const { width, ref } = useResizeObserverCustom<HTMLDivElement>();
   const [expanded, setExpanded] = useState(false);
+  const type = getType(entry.response.content.type.toLowerCase());
+  // @ts-expect-error Element has any type
+  const typeDefinition = types[type];
 
   return (
-    <div className={locals.subtransaction}>
-      <Tooltip
-        content={
-          expanded
-            ? t('in-synthetics:dashboard.detailsPage.showLessSubDetails')
-            : t('in-synthetics:dashboard.detailsPage.showMoreSubDetails')
-        }
-        align="topMiddle"
-      >
-        <div
-          className={locals.header}
-          {...toInteractiveElement({
-            ariaLabel: expanded
-              ? t('in-synthetics:dashboard.detailsPage.showLessSubDetails')
-              : t('in-synthetics:dashboard.detailsPage.showMoreSubDetails'),
-            onDefaultInteraction: () => setExpanded(!expanded)
-          })}
-        >
-          <div className={locals.leftHeader}>
-            <TypeHeader entry={entry} />
-            <LeftHeader entry={entry} />
-          </div>
-          <div className={locals.rightHeader}>
-            <ToggleIcon expanded={expanded} />
-          </div>
+    <div
+      {...toInteractiveElement({
+        ariaLabel: expanded
+          ? t('in-synthetics:dashboard.detailsPage.showLessSubDetails')
+          : t('in-synthetics:dashboard.detailsPage.showMoreSubDetails'),
+        onDefaultInteraction: () => setExpanded(!expanded)
+      })}
+    >
+      <div className={locals.entry}>
+        <div ref={ref} className={locals.leftHeader}>
+          <Tooltip content={typeDefinition.long} align="rightMiddle">
+            <Pill color={typeDefinition.color} className={locals.type}>
+              {typeDefinition.short}
+            </Pill>
+          </Tooltip>
+          <Fragment>
+            <KeyValueHeader
+              value={''}
+              label={entry.request.url || t('in-synthetics:dashboard.detailsPage.noDataAvailable.notFound')}
+            />
+            <KeyValueHeader
+              label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.status')}
+              value={entry.response.status}
+            />
+            <KeyValueHeader
+              label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.size')}
+              value={`${bytesZeroDecimalPlaces(entry.response.size)}`}
+            />
+            <KeyValueHeader
+              label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.time')}
+              value={`${millisToTwoDecimalSeconds(entry.time)}`}
+            />
+            <BrowserTimeline
+              width={(width || 1500) / 2}
+              entriesToRender={[entry]}
+              earliestTimestamp={earliestTimestamp}
+              endTimestamp={endTimestamp}
+            />
+          </Fragment>
         </div>
-      </Tooltip>
+        <div className={locals.rightHeader}>
+          <ToggleIcon expanded={expanded} />
+        </div>
+      </div>
       {expanded && (
         <div className={locals.body}>
           <EntryBody entry={entry} />
@@ -64,45 +89,13 @@ export default function Entry({ entry }: EntryProps) {
       )}
     </div>
   );
+};
+
+interface EntryBodyProps {
+  entry: TestResultEntry;
 }
 
-function TypeHeader({ entry }: EntryProps) {
-  const type = getType(entry.response.content.type.toLowerCase());
-  // @ts-expect-error Element has any type
-  const typeDefinition = types[type];
-  return (
-    <Tooltip content={typeDefinition.long} align="rightMiddle">
-      <Pill color={typeDefinition.color} className={locals.type}>
-        {typeDefinition.short}
-      </Pill>
-    </Tooltip>
-  );
-}
-
-function LeftHeader({ entry }: EntryProps) {
-  return (
-    <Fragment>
-      <KeyValueHeader
-        value={''}
-        label={entry.request.url || t('in-synthetics:dashboard.detailsPage.noDataAvailable.notFound')}
-      />
-      <KeyValueHeader
-        label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.status')}
-        value={entry.response.status}
-      />
-      <KeyValueHeader
-        label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.size')}
-        value={`${bytesZeroDecimalPlaces(entry.response.size)}`}
-      />
-      <KeyValueHeader
-        label={t('in-synthetics:dashboard.detailsPage.browserDetails.entry.time')}
-        value={`${millisToTwoDecimalSeconds(entry.time)}`}
-      />
-    </Fragment>
-  );
-}
-
-function EntryBody({ entry }: EntryProps) {
+const EntryBody = ({ entry }: EntryBodyProps) => {
   const { blocked, dns, connect, ssl, send, wait, receive } = entry.timings;
   const {
     url,
@@ -157,9 +150,7 @@ function EntryBody({ entry }: EntryProps) {
     <Fragment>
       <Row>
         <Col lg={4}>
-          <h2 className={locals.bodyHeader}>
-            {t('in-synthetics:dashboard.detailsPage.browserDetails.body.request.title')}
-          </h2>
+          <h2>{t('in-synthetics:dashboard.detailsPage.browserDetails.body.request.title')}</h2>
           <Dl>
             <Di title={t('in-synthetics:dashboard.detailsPage.browserDetails.body.request.url')}>
               <a href={url} rel="noopener noreferrer" target="_blank">
@@ -179,9 +170,7 @@ function EntryBody({ entry }: EntryProps) {
           </Dl>
         </Col>
         <Col lg={4}>
-          <h2 className={locals.bodyHeader}>
-            {t('in-synthetics:dashboard.detailsPage.browserDetails.body.response.title')}
-          </h2>
+          <h2>{t('in-synthetics:dashboard.detailsPage.browserDetails.body.response.title')}</h2>
           <Di title={t('in-synthetics:dashboard.detailsPage.browserDetails.body.response.redirectUrl')}>
             <a href={redirectURL} rel="noopener noreferrer" target="_blank">
               {redirectURL || t('in-synthetics:dashboard.detailsPage.noDataAvailable.notFound')}
@@ -210,12 +199,12 @@ function EntryBody({ entry }: EntryProps) {
           </Di>
         </Col>
         <Col lg={4}>
-          <h2 className={locals.bodyHeader}>
-            {t('in-synthetics:dashboard.detailsPage.browserDetails.body.timings.title')}
-          </h2>
+          <h2>{t('in-synthetics:dashboard.detailsPage.browserDetails.body.timings.title')}</h2>
           <Timings timings={timings} duration={entry.time} />
         </Col>
       </Row>
     </Fragment>
   );
-}
+};
+
+export default Entry;
