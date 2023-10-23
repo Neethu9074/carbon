2078f8cf9aee693eb4f7125fb16bcaff4aa8f785
bjlizhi <bjlizhi@cn.ibm.com>
2023-10-23 14:45:54 +0800

    Add more metrics

--- packages/in-forge/plugins/oTelDatabase/Info.tsx
+++ packages/in-forge/plugins/oTelDatabase/Info.tsx
@@ -13,11 +13,27 @@ export default function OpenTelemetryInfo({ snapshot }: { snapshot: SnapshotData
   const data = snapshot.get('data');
   return (
     <DescriptionList>
-      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.hostName')}>
-        {data.get('resource.hostname')}
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.entityType')}>
+        {data.get('resource.db.entity.type')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.databaseSystem')}>
+        {data.get('resource.db.system')}
       </DescriptionItem>
       <DescriptionItem title={t('in-forge:plugins.oTelDatabase.serviceName')}>
         {data.get('resource.service.name')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.address')}>
+        {data.get('resource.server.address')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.name')}>{data.get('resource.db.name')}</DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.instanceId')}>
+        {data.get('resource.service.instance.id')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.port')}>
+        {data.get('resource.server.port')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.oTelDatabase.version')}>
+        {data.get('resource.db.version')}
       </DescriptionItem>
       <DescriptionItem title={t('in-forge:plugins.oTelDatabase.kind')}>{data.get('kind')}</DescriptionItem>
     </DescriptionList>
