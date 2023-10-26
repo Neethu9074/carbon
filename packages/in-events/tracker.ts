07fd6ba559e112bb98b91b9fd2a17dd1a2bad9f7
Dan Stingaciu <Dan.Stingaciu@ibm.com>
2023-10-26 14:17:48 -0400

    created tracking events for mixpanel that will be used in PR#13679 (#13692)

--- packages/in-events/tracker.ts
+++ packages/in-events/tracker.ts
@@ -8,9 +8,22 @@ import {
   track,
   EVENT_RCA_SUGGESTION_HELPFUL,
   EVENT_RCA_SUGGESTION_UNHELPFUL,
-  EVENT_RCA_EXPANDED_CARD
+  EVENT_RCA_EXPANDED_CARD,
+  EVENT_FEEDBACK_SUBMIT,
+  EVENT_FEEDBACK_CLOSED_MANUALLY,
+  EVENT_FEEDBACK_NEXT,
+  EVENT_FEEDBACK_SKIP,
+  EVENT_FEEDBACK_POSITIVE,
+  EVENT_FEEDBACK_NEGATIVE
 } from 'in-services/tracking/tracking';
 
 export const helpfulRCASuggestionTracker = (e: Object) => track(EVENT_RCA_SUGGESTION_HELPFUL, e);
 export const unhelpfulRCASuggestionTracker = (e: Object) => track(EVENT_RCA_SUGGESTION_UNHELPFUL, e);
 export const expandedRCAEventCardTracker = (e: Object) => track(EVENT_RCA_EXPANDED_CARD, e);
+
+export const eventFeedbackSubmitTracker = (e: Object) => track(EVENT_FEEDBACK_SUBMIT, e);
+export const eventFeedbackClosedManuallyTracker = (e: Object) => track(EVENT_FEEDBACK_CLOSED_MANUALLY, e);
+export const eventFeedbackNextTracker = (e: Object) => track(EVENT_FEEDBACK_NEXT, e);
+export const eventFeedbackSkipTracker = (e: Object) => track(EVENT_FEEDBACK_SKIP, e);
+export const eventFeedbackPositiveTracker = (e: Object) => track(EVENT_FEEDBACK_POSITIVE, e);
+export const eventFeedbackNegativeTracker = (e: Object) => track(EVENT_FEEDBACK_NEGATIVE, e);
