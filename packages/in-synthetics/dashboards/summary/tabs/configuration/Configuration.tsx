ed962fc48277ffcf6d3eb2216f717cd7a240b906
Sandra S <Sandra.S@ibm.com>
2023-11-14 13:25:45 +0530

    KB-145776: Improvements to Delete Test and Delete Location dialogs (#13839)

--- packages/in-synthetics/dashboards/summary/tabs/configuration/Configuration.tsx
+++ packages/in-synthetics/dashboards/summary/tabs/configuration/Configuration.tsx
@@ -29,6 +29,7 @@ import { TestResponse } from 'in-synthetics/utils/constants';
 import Header from 'in-components/workspace/Header/Header';
 import Label from 'in-components/form/Label/Label';
 import Input from 'in-components/form/Input/Input';
+import { isBlank } from 'in-services/util/string';
 import Dialog from 'in-components/Dialog/Dialog';
 import { removeTest } from 'in-synthetics/api';
 import Tooltip from 'in-components/Tooltip';
@@ -134,6 +135,7 @@ const Configuration = ({ test, setReloadCount }: ConfigurationProps) => {
             })}
             <Input
               name="typingValidation"
+              placeholder={syntheticValidation}
               value={validationInputValue}
               disabled={isDeleting}
               onChange={({ target }: React.ChangeEvent<HTMLInputElement>) => {
@@ -150,7 +152,7 @@ const Configuration = ({ test, setReloadCount }: ConfigurationProps) => {
             onClick={() => {
               doDeleteAction(testId);
             }}
-            disabled={validationInputValue !== syntheticValidation}
+            disabled={validationInputValue !== syntheticValidation || isBlank(reasonInputValue)}
             kind="danger"
           >
             {t('in-synthetics:dashboard.configuration.dialog.deleteButton')}
