631fe0e7bbbc51f9c41b55e7dcb6a6b62ff78da4
Sandra S <Sandra.S@ibm.com>
2023-11-16 00:50:35 +0530

    KB-145980: Delete location action should only be available to the Private location type (#13857)
    * KB-145980: Delete location action should only be available to the Private location type
    
    * KB-145980: Adding tooltip for actions column of managed PoPs

--- packages/in-synthetics/dashboards/global/tabs/locations/components/columnDefinitions.tsx
+++ packages/in-synthetics/dashboards/global/tabs/locations/components/columnDefinitions.tsx
@@ -24,6 +24,7 @@ import { ServerTablePresenterProps } from 'in-components/tables/ServerTable/Serv
 import { getDashboardLink } from 'in-stores/navigation/paths/dashboardPaths';
 import { physicalDashboardPath } from 'in-stores/navigation/paths/mainPaths';
 import { ColumnDefinition } from 'in-components/tables/ServerTable/types';
+import Tooltip from 'in-components/Tooltip';
 import { role } from 'in-stores/user';
 import { t } from 'in-i18n';
 
@@ -238,12 +239,18 @@ if (role?.canConfigureSyntheticLocations) {
     label: t('in-synthetics:dashboard.testList.action'),
     sortable: false,
     getContent(item: LocationListItem, { result }) {
-      return (
+      return item.type === 'Private' ? (
         <HorizontalFlexWrapper>
           <div>
             <LocationListActionsColumn item={item} isLoading={result?.progress?.loading ?? false} />
           </div>
         </HorizontalFlexWrapper>
+      ) : (
+        <div className={locals.action}>
+          <Tooltip content={t('in-synthetics:dashboard.locationList.deleteRestrictedDescription')}>
+            <span>{t('in-synthetics:dashboard.locationList.noActionsAllowed')}</span>
+          </Tooltip>
+        </div>
       );
     }
   });
