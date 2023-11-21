f49749e9f2290699b4906873dbbbfcdacac96488
YONG HU SUN <sunyongh@cn.ibm.com>
2023-11-21 11:10:28 +0800

    correct message destination for GET spans

--- packages/in-forge/tracing/ibm-mq/IbmmqSpanDetailView.js
+++ packages/in-forge/tracing/ibm-mq/IbmmqSpanDetailView.js
@@ -10,12 +10,23 @@ import { Dl, Di } from 'in-components/HorizontalDescriptionList';
 import { t } from 'in-i18n';
 
 export default function IbmmqSpanDetailView({ span }) {
+  const operation = span.getIn(['data', 'ibm-mq', 'operation']);
   return (
     <div>
       <Dl>
-        <Di title={t('in-forge:tracing.ibmmq.titleDestination')}>{span.getIn(['data', 'ibm-mq', 'queue'])}</Di>
-        <Di title={t('in-forge:tracing.ibmmq.titleDestinationKind')}>{span.getIn(['data', 'ibm-mq', 'destination', 'kind'], 'queue')}</Di>
-        <Di title={t('in-forge:tracing.ibmmq.titleMsgOperation')}>{span.getIn(['data', 'sdk', 'custom', 'tags', 'spanType'])}</Di>
+        <Di title={t('in-forge:tracing.ibmmq.titleDestination')}>
+          {operation == 'PUT'
+            ? span.getIn(['data', 'ibm-mq', 'queue'])
+            : span.getIn(['data', 'sdk', 'custom', 'tags', 'exitContextApplName'])}
+        </Di>
+        {operation == 'PUT' ? (
+          <Di title={t('in-forge:tracing.ibmmq.titleDestinationKind')}>
+            {span.getIn(['data', 'ibm-mq', 'destination', 'kind'], 'queue')}
+          </Di>
+        ) : null}
+        <Di title={t('in-forge:tracing.ibmmq.titleMsgOperation')}>
+          {span.getIn(['data', 'sdk', 'custom', 'tags', 'spanType'])}
+        </Di>
         <ErrorDescriptionItem error={span.getIn(['data', 'sdk', 'custom', 'tags', 'error'])} />
       </Dl>
     </div>
