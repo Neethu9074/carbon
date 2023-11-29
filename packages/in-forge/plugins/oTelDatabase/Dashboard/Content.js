b69a984426a699528d68a391861d5ab3b33e5cee
bjlizhi <bjlizhi@cn.ibm.com>
2023-11-29 11:26:32 +0800

    If no metrics display the widget

--- packages/in-forge/plugins/oTelDatabase/Dashboard/Content.js
+++ packages/in-forge/plugins/oTelDatabase/Dashboard/Content.js
@@ -25,205 +25,233 @@ import { t } from 'in-i18n';
 
 export default function OTelDatabaseDashboard({ snapshot, timeConfig }) {
   const snapshotId = snapshot.get('id');
+  const metricIds = snapshot.get('metricIds');
+
   return (
     <div>
       <KpiSection>
-        <KpiKeyValue
-          label={
-            t('in-forge:plugins.oTelDatabase.dashboard.instanceActive') +
-            ' / ' +
-            t('in-forge:plugins.oTelDatabase.dashboard.instance')
-          }
-        >
-          <MetricValue snapshotId={snapshotId} metric="db.instance.active.count" formatter={number.compact} /> /{' '}
-          <MetricValue snapshotId={snapshotId} metric="db.instance.count" formatter={number.compact} />
-        </KpiKeyValue>
-
-        <KpiKeyValue
-          label={
-            t('in-forge:plugins.oTelDatabase.dashboard.sessionActive') +
-            ' / ' +
-            t('in-forge:plugins.oTelDatabase.dashboard.session')
-          }
-        >
-          <MetricValue snapshotId={snapshotId} metric="db.session.active.count" formatter={number.compact} /> /{' '}
-          <MetricValue snapshotId={snapshotId} metric="db.session.count" formatter={number.compact} />
-        </KpiKeyValue>
+        {metricIds.includes('db.instance.active.count') && metricIds.includes('db.instance.count') === true && (
+          <KpiKeyValue
+            label={
+              t('in-forge:plugins.oTelDatabase.dashboard.instanceActive') +
+              ' / ' +
+              t('in-forge:plugins.oTelDatabase.dashboard.instance')
+            }
+          >
+            <MetricValue snapshotId={snapshotId} metric="db.instance.active.count" formatter={number.compact} /> /{' '}
+            <MetricValue snapshotId={snapshotId} metric="db.instance.count" formatter={number.compact} />
+          </KpiKeyValue>
+        )}
+        {metricIds.includes('db.session.active.count') && metricIds.includes('db.session.count') === true && (
+          <KpiKeyValue
+            label={
+              t('in-forge:plugins.oTelDatabase.dashboard.sessionActive') +
+              ' / ' +
+              t('in-forge:plugins.oTelDatabase.dashboard.session')
+            }
+          >
+            <MetricValue snapshotId={snapshotId} metric="db.session.active.count" formatter={number.compact} /> /{' '}
+            <MetricValue snapshotId={snapshotId} metric="db.session.count" formatter={number.compact} />
+          </KpiKeyValue>
+        )}
       </KpiSection>
       <KpiSection>
-        <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.transaction')}>
-          <MetricValue snapshotId={snapshotId} metric="db.transaction.count" formatter={number.compact} />
-        </KpiKeyValue>
-
-        <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.tps')}>
-          <MetricValue snapshotId={snapshotId} metric="db.transaction.rate" formatter={number.detailed} />
-        </KpiKeyValue>
-
-        <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.sql')}>
-          <MetricValue snapshotId={snapshotId} metric="db.sql.count" formatter={number.compact} />
-        </KpiKeyValue>
-
-        <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')}>
-          <MetricValue snapshotId={snapshotId} metric="db.sql.rate" formatter={number.detailed} />
-        </KpiKeyValue>
+        {metricIds.includes('db.transaction.count') === true && (
+          <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.transaction')}>
+            <MetricValue snapshotId={snapshotId} metric="db.transaction.count" formatter={number.compact} />
+          </KpiKeyValue>
+        )}
+        {metricIds.includes('db.transaction.rate') === true && (
+          <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.tps')}>
+            <MetricValue snapshotId={snapshotId} metric="db.transaction.rate" formatter={number.detailed} />
+          </KpiKeyValue>
+        )}
+        {metricIds.includes('db.sql.count') === true && (
+          <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.sql')}>
+            <MetricValue snapshotId={snapshotId} metric="db.sql.count" formatter={number.compact} />
+          </KpiKeyValue>
+        )}
+        {metricIds.includes('db.sql.rate') === true && (
+          <KpiKeyValue label={t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')}>
+            <MetricValue snapshotId={snapshotId} metric="db.sql.rate" formatter={number.detailed} />
+          </KpiKeyValue>
+        )}
       </KpiSection>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sessions')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.compact,
-              metrics: ['db.session.count', 'db.session.active.count'],
-              labels: [
-                t('in-forge:plugins.oTelDatabase.dashboard.total'),
-                t('in-forge:plugins.oTelDatabase.dashboard.active')
-              ],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.session.count') && metricIds.includes('db.session.active.count') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sessions')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.compact,
+                metrics: ['db.session.count', 'db.session.active.count'],
+                labels: [
+                  t('in-forge:plugins.oTelDatabase.dashboard.total'),
+                  t('in-forge:plugins.oTelDatabase.dashboard.active')
+                ],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.transaction')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.compact,
-              metrics: ['db.transaction.count'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.transactionCount')],
-              type: 'line'
-            }}
-            y2={{
-              min: 0,
-              formatter: seconds.detailed,
-              metrics: ['db.transaction.latency'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.averageLatency')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.tps')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.detailed,
-              metrics: ['db.transaction.rate'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.tps')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.transaction.count') && metricIds.includes('db.transaction.latency') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.transaction')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.compact,
+                metrics: ['db.transaction.count'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.transactionCount')],
+                type: 'line'
+              }}
+              y2={{
+                min: 0,
+                formatter: seconds.detailed,
+                metrics: ['db.transaction.latency'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.averageLatency')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
+        {metricIds.includes('db.transaction.rate') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.tps')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.detailed,
+                metrics: ['db.transaction.rate'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.tps')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sql')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.compact,
-              metrics: ['db.sql.count'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.sqlCount')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.detailed,
-              metrics: ['db.sql.rate'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.sql.count') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sql')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.compact,
+                metrics: ['db.sql.count'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.sqlCount')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
+        {metricIds.includes('db.sql.rate') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.detailed,
+                metrics: ['db.sql.rate'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.sqlPerSecond')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.task')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.compact,
-              metrics: ['db.task.wait_count'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.taskWait_count')],
-              type: 'line'
-            }}
-            y2={{
-              min: 0,
-              formatter: number.compact,
-              metrics: ['db.task.avg_wait_time'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.avg_wait_time')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.task.wait_count') && metricIds.includes('db.task.avg_wait_time') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.task')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.compact,
+                metrics: ['db.task.wait_count'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.taskWait_count')],
+                type: 'line'
+              }}
+              y2={{
+                min: 0,
+                formatter: number.compact,
+                metrics: ['db.task.avg_wait_time'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.avg_wait_time')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.ioPerSecond')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: bytesTwoDecimalPlaces,
-              metrics: ['db.io.read.rate', 'db.io.write.rate'],
-              labels: [
-                t('in-forge:plugins.oTelDatabase.dashboard.read'),
-                t('in-forge:plugins.oTelDatabase.dashboard.write')
-              ],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.io.read.rate') && metricIds.includes('db.io.write.rate') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.ioPerSecond')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: bytesTwoDecimalPlaces,
+                metrics: ['db.io.read.rate', 'db.io.write.rate'],
+                labels: [
+                  t('in-forge:plugins.oTelDatabase.dashboard.read'),
+                  t('in-forge:plugins.oTelDatabase.dashboard.write')
+                ],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
       <Columize>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.cpuUtilization')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.detailed,
-              metrics: ['db.cpu.utilization'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.cpuUtilization')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
-        <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.memUtilization')}>
-          <Chart
-            snapshotId={snapshotId}
-            timeConfig={timeConfig}
-            y1={{
-              min: 0,
-              formatter: number.detailed,
-              metrics: ['db.mem.utilization'],
-              labels: [t('in-forge:plugins.oTelDatabase.dashboard.memUtilization')],
-              type: 'line'
-            }}
-            renderPostChartContent={PluginDashboardsMarkerLanes}
-          />
-        </DashboardSection>
+        {metricIds.includes('db.cpu.utilization') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.cpuUtilization')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.detailed,
+                metrics: ['db.cpu.utilization'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.cpuUtilization')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
+        {metricIds.includes('db.mem.utilization') === true && (
+          <DashboardSection title={t('in-forge:plugins.oTelDatabase.dashboard.memUtilization')}>
+            <Chart
+              snapshotId={snapshotId}
+              timeConfig={timeConfig}
+              y1={{
+                min: 0,
+                formatter: number.detailed,
+                metrics: ['db.mem.utilization'],
+                labels: [t('in-forge:plugins.oTelDatabase.dashboard.memUtilization')],
+                type: 'line'
+              }}
+              renderPostChartContent={PluginDashboardsMarkerLanes}
+            />
+          </DashboardSection>
+        )}
       </Columize>
 
       <DiskTable snapshot={snapshot} timeConfig={timeConfig} />
