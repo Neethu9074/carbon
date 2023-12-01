bb952d54964cd823b88142aba98383f089d920f8
Benjamin Kan <kan@ibm.com>
2023-12-01 03:00:13 -0800

    Fix number formatting in EUMHP internal dashboards (#13611)
    * Fix number formatting in EUMHP internal dashboards
    
    * Fix broken ADHA CPU Load internal dashboard
    
    * Add instances section to EUMHP dashboard

--- packages/in-internal/monitoringUnit/eum/EumHealthProcessor.js
+++ packages/in-internal/monitoringUnit/eum/EumHealthProcessor.js
@@ -3,20 +3,51 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { getDropwizardWithContext } from 'in-internal/monitoringUnit/dataRetrieval';
 import Chart from 'in-infrastructure/components/InfrastructureMetricChartBehavior';
 import LoadingIndicator from 'in-components/LoadingIndicators/LoadingIndicator';
+import { physicalDashboardPath } from 'in-stores/navigation/paths/mainPaths';
 import DashboardSection from 'in-sdk/components/dashboard/DashboardSection';
+import { millis, number } from 'in-services/formatters/number';
 import Columize from 'in-sdk/components/dashboard/Columize';
 import { compareIgnoreCase } from 'in-services/util/string';
 import ExpandableCard from 'in-components/ExpandableCard';
-import { number } from 'in-services/formatters/number';
+import Table from 'in-sdk/components/dashboard/Table';
 import { Row, Col } from 'in-components/layout/Grid';
 import { timeConfig$ } from 'in-stores/time/config';
 import connectTo from 'in-hoc/connectTo';
 import { t } from 'in-i18n';
+
+const hostViewCols = [
+  {
+    title: t('in-internal:monitoringUnit.eum.eumHealthProcessor.host'),
+    type: 'snapshotLink',
+    typeArgs: {
+      pathname: physicalDashboardPath,
+      getSnapshotId(row) {
+        return row.host.get('id');
+      }
+    }
+  },
+  {
+    title: t('in-internal:monitoringUnit.eum.eumHealthProcessor.hostCpuLoad'),
+    type: 'metric',
+    typeArgs: {
+      getSnapshotId(row) {
+        return row.host.get('id');
+      },
+      getMetricName() {
+        return `load.1min`;
+      },
+      getContent: number.detailed,
+      getTimeWindowAggregation() {
+        return 'mean';
+      }
+    }
+  }
+];
 
 export default connectTo(
   {
@@ -51,7 +82,7 @@ export default connectTo(
           </DashboardSection>
         </Columize>
 
-        <Row key={`website`}>
+        <Row key="website">
           <Col lg={12}>
             <ExpandableCard title={t('in-internal:monitoringUnit.eum.eumHealthProcessor.websiteMonitoring')}>
               <Columize>
@@ -81,7 +112,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.incoming.processed_website_monitoring_beacons.calls`),
                       labels,
                       type: 'stackedArea'
@@ -95,7 +126,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.incoming.processed_website_monitoring_beacons.errors`
                       ),
@@ -113,7 +144,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.matched_website_monitoring_beacons.calls`),
                       labels,
                       type: 'stackedArea'
@@ -129,7 +160,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.matched_website_monitoring_beacons.errors`),
                       labels,
                       type: 'stackedArea'
@@ -145,7 +176,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.incoming.matched_website_monitoring_beacons.calls`),
                       labels,
                       type: 'stackedArea'
@@ -161,7 +192,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.incoming.matched_website_monitoring_beacons.errors`),
                       labels,
                       type: 'stackedArea'
@@ -179,7 +210,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.processing.matched_website_monitoring_beacons.calls`),
                       labels,
                       type: 'stackedArea'
@@ -195,7 +226,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.processing.matched_website_monitoring_beacons.errors`
                       ),
@@ -213,7 +244,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.processing.evaluated_website_health_buckets.calls`),
                       labels,
                       type: 'stackedArea'
@@ -227,7 +258,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.processing.evaluated_website_health_buckets.errors`),
                       labels,
                       type: 'stackedArea'
@@ -243,7 +274,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.website_events.calls`),
                       labels,
                       type: 'stackedArea'
@@ -257,7 +288,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.website_events.errors`),
                       labels,
                       type: 'stackedArea'
@@ -269,7 +300,7 @@ export default connectTo(
           </Col>
         </Row>
 
-        <Row key={`website`}>
+        <Row key="mobile">
           <Col lg={12}>
             <ExpandableCard title={t('in-internal:monitoringUnit.eum.eumHealthProcessor.mobileAppMonitoring')}>
               <Columize>
@@ -299,7 +330,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.incoming.processed_mobile_app_monitoring_beacons.calls`
                       ),
@@ -315,7 +346,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.incoming.processed_mobile_app_monitoring_beacons.errors`
                       ),
@@ -333,7 +364,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.outgoing.matched_mobile_app_monitoring_beacons.calls`
                       ),
@@ -351,7 +382,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.outgoing.matched_mobile_app_monitoring_beacons.errors`
                       ),
@@ -369,7 +400,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.incoming.matched_mobile_app_monitoring_beacons.calls`
                       ),
@@ -387,7 +418,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.incoming.matched_mobile_app_monitoring_beacons.errors`
                       ),
@@ -407,7 +438,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.processing.matched_mobile_app_monitoring_beacons.calls`
                       ),
@@ -425,7 +456,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.processing.matched_mobile_app_monitoring_beacons.errors`
                       ),
@@ -443,7 +474,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.processing.evaluated_mobile_app_health_buckets.calls`
                       ),
@@ -459,7 +490,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(
                         () => `metrics.meters.KPI.processing.evaluated_mobile_app_health_buckets.errors`
                       ),
@@ -477,7 +508,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.mobile_app_events.calls`),
                       labels,
                       type: 'stackedArea'
@@ -491,7 +522,7 @@ export default connectTo(
                     timeConfig={timeConfig}
                     y1={{
                       min: 0,
-                      formatter: number.perSecond.compact,
+                      formatter: number.perSecond.detailed,
                       metrics: rows.map(() => `metrics.meters.KPI.outgoing.mobile_app_events.errors`),
                       labels,
                       type: 'stackedArea'
@@ -502,10 +533,73 @@ export default connectTo(
             </ExpandableCard>
           </Col>
         </Row>
+
+        <Row key="instances">
+          <Col lg={12}>
+            <DashboardSection title={`eum-health-processors (${rows.length})`}>
+              <Table cols={hostViewCols} rows={rows} getRowDetails={getRowDetails} />
+            </DashboardSection>
+          </Col>
+        </Row>
       </div>
     );
   }
 );
+
+function getRowDetails(row) {
+  return (
+    <Fragment>
+      <DashboardSection title={t('in-internal:monitoringUnit.eum.eumHealthProcessor.hostLoad')}>
+        <Chart
+          snapshotId={row.host.get('id')}
+          timeConfig={row.timeConfig}
+          minRollup={5000}
+          y1={{
+            min: 0,
+            formatter: number.detailed,
+            tooltipFormatter: number.detailed,
+            metrics: ['load.1min'],
+            labels: [t('in-internal:monitoringUnit.eum.eumHealthProcessor.hostCpuLoad')],
+            type: 'stackedArea'
+          }}
+        />
+      </DashboardSection>
+
+      {row.jvm.getIn(['data', 'jvm.collectors']) ? (
+        <DashboardSection title={t('in-internal:monitoringUnit.eum.eumHealthProcessor.garbageCollection')}>
+          <Chart
+            snapshotId={row.jvm.get('id')}
+            timeConfig={row.timeConfig}
+            y1={{
+              metrics: row.jvm
+                .getIn(['data', 'jvm.collectors'])
+                .map(name => 'gc.' + name + '.time')
+                .toArray(),
+              labels: row.jvm
+                .getIn(['data', 'jvm.collectors'])
+                .map(name => name + ' Time')
+                .toArray(),
+              type: 'line',
+              formatter: millis.fixedDetailed
+            }}
+            y2={{
+              metrics: row.jvm
+                .getIn(['data', 'jvm.collectors'])
+                .map(name => 'gc.' + name + '.inv')
+                .toArray(),
+              labels: row.jvm
+                .getIn(['data', 'jvm.collectors'])
+                .map(name => name + ' Invocations')
+                .toArray(),
+              type: 'point',
+              formatter: number.detailed
+            }}
+          />
+        </DashboardSection>
+      ) : null}
+    </Fragment>
+  );
+}
 
 export function getLabel(row) {
   return row.host.get('label').replace('.instana.io', '').replace('ip-', '');
