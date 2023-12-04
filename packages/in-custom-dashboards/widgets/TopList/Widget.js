c4888da80e8594b8e24f50be82c929ce3ab17009
Tácio Belmonte <Tacio.Belmonte@ibm.com>
2023-12-04 17:24:09 +0100

    limit toplist to always display the maxresults number (#14057)

--- packages/in-custom-dashboards/widgets/TopList/Widget.js
+++ packages/in-custom-dashboards/widgets/TopList/Widget.js
@@ -72,6 +72,7 @@ export default function ListWidget({ config, title, actions, dragHandle }) {
 
 export function ListWidgetRenderer({ result, isErroneous, tagCatalog, config, title, actions, dragHandle }) {
   const theme = useTheme();
+  const { maxResults } = config.metricConfiguration.grouping[0];
   const hasApproximateData =
     result?.data?.filter(elem => elem?.resultPrecisionDetails?.resultPrecision === 'PRECISION_APPROXIMATE').length > 0;
 
@@ -79,7 +80,7 @@ export function ListWidgetRenderer({ result, isErroneous, tagCatalog, config, ti
     <TopListCardPresenter
       title={title}
       result={result}
-      getItemsFromResult={result => result.data}
+      getItemsFromResult={result => result.data.slice(0, maxResults)}
       getMetricValueFromItem={(selectedMetric, item) => item.values?.[0]?.[1]}
       selectedMetricFormatter={metricValue => getFormatter(config.formatter)(metricValue)}
       selectedMetricColor={isErroneous ? theme.ids.color.option.red['500'] : null}
