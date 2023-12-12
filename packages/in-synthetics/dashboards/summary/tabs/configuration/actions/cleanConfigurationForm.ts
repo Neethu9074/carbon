8aadfd207e4f99e321724faa86d79aaf5fbe106d
Esteban Diaz Gallego <ediazgallego@ibm.com>
2023-12-12 11:27:56 -0500

    KB-148250 Browser property is being modified when editing a Browser Script test. (#14095)
    * KB-148250: Include browser property if present when test type is browserScript
    
    * KB-148250: Properties to analyze
    
    * Revert "KB-148250: Properties to analyze"
    
    This reverts commit 026c0438715f98fd98d096e6debb8f4904bee4b5.
    
    * KB-148250: Include Webpage type tests to persist the browser property
    
    Co-authored-by: Carol Zhang <czhang@us.ibm.com>

--- packages/in-synthetics/dashboards/summary/tabs/configuration/actions/cleanConfigurationForm.ts
+++ packages/in-synthetics/dashboards/summary/tabs/configuration/actions/cleanConfigurationForm.ts
@@ -9,45 +9,75 @@ import { MapForm, createField } from 'formalistic';
 import { BrowserScriptConfiguration, HttpScriptConfiguration, SyntheticTest } from '@instana/types';
 
 import hasEmptyStrings from 'in-synthetics/utils/hasEmptyStrings';
-import { isBlank } from 'in-services/util/string';
+import { isBlank, isNotBlank } from 'in-services/util/string';
 
 const cleanConfigurationForm = (form: MapForm<any>, test: SyntheticTest): SyntheticTest => {
   const testId: string = test.id || '';
   const isActive: boolean = test.active;
   const { retries, timeout, retryInterval, markSyntheticCall } = test.configuration;
+  const isBrowserScript = form.get('configuration').get('syntheticType').value === 'BrowserScript';
+  const isHTTPScript = form.get('configuration').get('syntheticType').value === 'HTTPScript';
+  const isWebpage = ['WebpageAction', 'WebpageScript'].includes(form.get('configuration').get('syntheticType').value);
 
   let testConfig: SyntheticTest;
   let updatedForm: MapForm<any>;
-  updatedForm = ['HTTPScript', 'BrowserScript'].includes(form.get('configuration').get('syntheticType').value)
-    ? form.put('active', createField({ value: isActive })).put(
-        'configuration',
-        form
-          .get('configuration')
-          .put('retries', createField({ value: retries }))
-          .put('timeout', createField({ value: timeout }))
-          .put('retryInterval', createField({ value: retryInterval }))
-          .put('markSyntheticCall', createField({ value: markSyntheticCall }))
-          .put(
-            'scriptType',
-            createField({
-              value: (test.configuration as BrowserScriptConfiguration | HttpScriptConfiguration).scriptType
-            })
-          )
-      )
-    : form.put('active', createField({ value: isActive })).put(
-        'configuration',
-        form
-          .get('configuration')
-          .put('retries', createField({ value: retries }))
-          .put('timeout', createField({ value: timeout }))
-          .put('retryInterval', createField({ value: retryInterval }))
-          .put('markSyntheticCall', createField({ value: markSyntheticCall }))
-      );
 
+  // Add common properties
+  updatedForm =
+    isHTTPScript || isBrowserScript
+      ? form.put('active', createField({ value: isActive })).put(
+          'configuration',
+          form
+            .get('configuration')
+            .put('retries', createField({ value: retries }))
+            .put('timeout', createField({ value: timeout }))
+            .put('retryInterval', createField({ value: retryInterval }))
+            .put('markSyntheticCall', createField({ value: markSyntheticCall }))
+            .put(
+              'scriptType',
+              createField({
+                value: (test.configuration as BrowserScriptConfiguration | HttpScriptConfiguration).scriptType
+              })
+            )
+        )
+      : form.put('active', createField({ value: isActive })).put(
+          'configuration',
+          form
+            .get('configuration')
+            .put('retries', createField({ value: retries }))
+            .put('timeout', createField({ value: timeout }))
+            .put('retryInterval', createField({ value: retryInterval }))
+            .put('markSyntheticCall', createField({ value: markSyntheticCall }))
+        );
+
+  // Add browser property if present in browser script test
+  // @ts-expect-error browser property could be present
+  if (isNotBlank(test.configuration?.browser) && (isBrowserScript || isWebpage)) {
+    updatedForm = form.put('active', createField({ value: isActive })).put(
+      'configuration',
+      form
+        .get('configuration')
+        .put('retries', createField({ value: retries }))
+        .put('timeout', createField({ value: timeout }))
+        .put('retryInterval', createField({ value: retryInterval }))
+        .put('markSyntheticCall', createField({ value: markSyntheticCall }))
+        .put(
+          'scriptType',
+          createField({
+            value: (test.configuration as BrowserScriptConfiguration | HttpScriptConfiguration).scriptType
+          })
+        )
+        //@ts-expect-error browser property could be present
+        .put('browser', createField({ value: test.configuration?.browser }))
+    );
+  }
+
+  // Remove applicationId property if not present
   if (isBlank(updatedForm.get('applicationId').value)) {
     updatedForm = updatedForm.remove('applicationId');
   }
 
+  // Add default customProperties
   if (hasEmptyStrings(updatedForm.get('customProperties').value)) {
     updatedForm = updatedForm.put('customProperties', createField({ value: {} }));
   }
