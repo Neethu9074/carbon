7ac5e2c5a7c6d6cc351957899bb40787156e1e42
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2024-01-02 17:25:03 +0530

    chnaged predictiveTrigger to null bu default (#14188)

--- packages/in-alerting/smart-alerts/infrastructure/form/alertFormDefinition.ts
+++ packages/in-alerting/smart-alerts/infrastructure/form/alertFormDefinition.ts
@@ -51,7 +51,7 @@ export default function alertFormDefinition(
     granularity = 600000,
     groupBy = [],
     name = '',
-    predictiveTrigger = { timeToFailure: 5 },
+    predictiveTrigger = null,
     severity = severityWarning,
     tagFilterExpression,
     id = ''
