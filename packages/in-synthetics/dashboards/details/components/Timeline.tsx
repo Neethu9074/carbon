8047d7f1185498b66feb7fed6d4cec3176a1df5f
Sandra-S <sandra.s@ibm.com>
2024-01-04 17:52:06 +0530

    KB-147491: Fixing inconsistency for data.subtransactions

--- packages/in-synthetics/dashboards/details/components/Timeline.tsx
+++ packages/in-synthetics/dashboards/details/components/Timeline.tsx
@@ -71,7 +71,7 @@ export default function Timeline({ details, startTime, finishTime }: TimelinePro
           <div className={locals.overviewChartContainer}>
             {
               // @ts-expect-error Object is possibly undefined
-              data.subtransactions?.length > 0 && data?.subtransactions[0]?.properties != null && (
+              data?.subtransactions?.length > 0 && data?.subtransactions[0]?.properties != null && (
                 <OverviewChart
                   subtransactions={filteredSubtransactions}
                   earliestTimestamp={startTime}
