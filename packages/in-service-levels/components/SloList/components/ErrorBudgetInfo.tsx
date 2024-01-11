70481c4466204821ad47807e64cebc5efb6fc900
BLESSY MATHEW <BLESSY.Mathew@ibm.com>
2024-01-11 15:36:28 +0530

    fix(slo-remErrorBudget-Unit):unit fixed for event-based (#14261)
    * fix(slo-remErrorBudget-Unit):unit fixed for event-based
    
    * fix(slo-remErrorBudget-Unit):unit fixed for event-based-review coments

--- packages/in-service-levels/components/SloList/components/ErrorBudgetInfo.tsx
+++ packages/in-service-levels/components/SloList/components/ErrorBudgetInfo.tsx
@@ -6,8 +6,7 @@
 
 import React from 'react';
 
-import { ServiceLevelObjectiveConfiguration } from '@instana/types';
-import { isEventBasedSli } from '@instana/types/typeDefinitions';
+import { isEventBasedSli, isCustomEventBasedSli, ServiceLevelObjectiveConfiguration } from '@instana/types';
 import { formatDuration } from '@instana/format-date';
 import { KeyValue } from '@instana/components';
 import { t } from '@instana/i18n-react';
@@ -45,9 +44,10 @@ export default function ErrorBudgetInfo({ configuration, remainingErrorBudget }:
         context: indicatorType,
         entityType,
         remaining: remainingErrorBudget,
-        remainingFormatted: isEventBasedSli(indicator)
-          ? number.compact(remainingErrorBudget)
-          : minutes.fixedCompact(remainingErrorBudget),
+        remainingFormatted:
+          isEventBasedSli(indicator) || isCustomEventBasedSli(indicator)
+            ? number.compact(remainingErrorBudget)
+            : minutes.fixedCompact(remainingErrorBudget),
         budget: minutes.fixedCompact(minutesInTimeWindow)
       })}
       inverted
