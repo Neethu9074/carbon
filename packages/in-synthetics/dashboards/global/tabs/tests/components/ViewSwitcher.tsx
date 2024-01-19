5b9f48e3ae021ab1efaaaa57a1fc3e31bb380b75
Esteban Diaz Gallego <ediazgallego@ibm.com>
2024-01-19 10:06:52 -0500

    KB-150640: Feature flag protection added

--- packages/in-synthetics/dashboards/global/tabs/tests/components/ViewSwitcher.tsx
+++ packages/in-synthetics/dashboards/global/tabs/tests/components/ViewSwitcher.tsx
@@ -14,6 +14,7 @@ import DashboardHeaderShadowModule from 'in-components/DashboardHeader/Dashboard
 import PopDeployButton from 'in-synthetics/dashboards/global/tabs/tests/components/PopDeployButton';
 import getPoPInstallationProperties from 'in-synthetics/subscriptions/getPoPInstallationProperties';
 import NewLocationButton from 'in-synthetics/createLocation/NewLocationButton';
+import { syntheticInstanaHostedPoPEnabled } from 'in-services/featureFlags';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import DashboardHeader from 'in-components/DashboardHeader';
 import * as paths from 'in-synthetics/navigation/paths';
@@ -73,7 +74,7 @@ export default function ViewSwitcher() {
               syntheticAcceptorURL={popProperties.data?.syntheticAcceptorURL || ''}
             />
           )}
-          <NewLocationButton />
+          {syntheticInstanaHostedPoPEnabled && <NewLocationButton />}
         </div>
       </DashboardHeaderModule>
       <DashboardHeaderShadowModule />
