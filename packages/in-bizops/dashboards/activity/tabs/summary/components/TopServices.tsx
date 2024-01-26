45136b23183357efe3f7f16404ac503fbb6eae52
KYLE BLAUER <kgblauer@us.ibm.com>
2024-01-26 14:32:16 -0600

    152248 Bizops golden signals boilerplate (#14424)
    * generic errors and latency
    
    * KPI cards
    
    * process page changes
    
    * activity page changes
    
    * i18n
    
    * latency comment fix
    
    * update copyright year, standardize names

---
+++ packages/in-bizops/dashboards/activity/tabs/summary/components/TopServices.tsx
@@ -0,0 +1,12 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import React from 'react';
+
+// This chart displays the top services within an activity
+export default function TopServices() {
+  return <p>Top services</p>;
+}
