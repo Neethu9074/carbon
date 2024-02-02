b682abe24dad07853fbf1295a9c0b20e9cfd1a59
HUAI NAN ZHOU <zhouhn@cn.ibm.com>
2024-02-01 22:18:39 -0500

    add custom metric for website monitoring in custom dashboard (#14519)

--- packages/in-websites/analyze/AnalyzeView/metrics.js
+++ packages/in-websites/analyze/AnalyzeView/metrics.js
@@ -200,6 +200,14 @@ const errorRate = {
   tag: 'beacon.error.count'
 };
 
+const customMetric = {
+  metric: 'customMetric',
+  label: t('in-websites:analyze.analyzeView.availableMetricsLabelCustomMetric'),
+  formatter: number,
+  supportedAggregations: ['SUM', 'MEAN', 'MAX', 'MIN', 'P25', 'P50', 'P75', 'P90', 'P95', 'P98', 'P99'],
+  preferredRenderer: Renderer.stackedBar
+};
+
 export const availableMetrics = {
   pageLoad: [
     newNumberMetric({
@@ -549,6 +557,7 @@ export const availableMetrics = {
     ),
     uniqueUsers,
     uniqueSessions,
-    uniqueUsersOrSessions
+    uniqueUsersOrSessions,
+    customMetric
   ]
 };
