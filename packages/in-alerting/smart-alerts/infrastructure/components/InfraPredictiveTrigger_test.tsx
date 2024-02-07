26d344a96f79edcd2dfe25d0dd31ccf212304451
Pradosh E <Pradosh.E@ibm.com>
2024-02-07 16:44:41 +0530

    few unit tests for Infra componenets (#14536)
    * few unit tests for Infra componenets
    
    * Update the test cases
    
    * review suggestions

---
+++ packages/in-alerting/smart-alerts/infrastructure/components/InfraPredictiveTrigger_test.tsx
@@ -0,0 +1,26 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import InfraPredictiveTrigger from 'in-alerting/smart-alerts/infrastructure/components/InfraPredictiveTrigger';
+//@ts-expect-error
+import { alertConfig } from 'in-alerting/smart-alerts/infrastructure/data/testData';
+import alertFormDefinition from 'in-alerting/smart-alerts/infrastructure/form/alertFormDefinition';
+import { t } from 'in-i18n';
+
+describe('Render InfraPredictiveTrigger : in-alerting/smart-alerts/infrastructure/components/InfraPredictiveTrigger', () => {
+  const form = alertFormDefinition(alertConfig, false);
+  const updateForm = jest.fn();
+  it('Check if component rendered in UI', () => {
+    render(<InfraPredictiveTrigger form={form} updateForm={updateForm} />);
+    expect(
+      screen.getByText(t('in-alerting:smartAlerts.infrastructure.advancedModeContainer.predictiveTrigger.title'))
+    ).toBeInTheDocument();
+    expect(screen.getByText('30 minutes')).toBeInTheDocument();
+  });
+});
---
+++ packages/in-alerting/smart-alerts/infrastructure/components/InfraScopePath_test.tsx
@@ -0,0 +1,18 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import InfraScopePath from 'in-alerting/smart-alerts/infrastructure/components/InfraScopePath';
+
+describe('Render InfraScopePath : in-alerting/smart-alerts/infrastructure/components/InfraScopePath', () => {
+  it('Check if component rendered in UI', () => {
+    render(<InfraScopePath iconName="lib_infra_instanaAgent" infraName="Instana Agent" />);
+    expect(screen.getByText('Instana Agent')).toBeInTheDocument();
+    expect(screen.getAllByLabelText('lib_infra_instanaAgent')).toHaveLength(1);
+  });
+});
