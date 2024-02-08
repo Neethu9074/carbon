c47c08835e076e8c31b80eb1889c7e5b8e20af85
Shams Sohrab <Shams.Sohrab@ibm.com>
2024-02-08 11:32:25 +0100

    fixed toggle for cross series aggregation (#14599)

--- packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/FormComponent.js
+++ packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/FormComponent.js
@@ -8,12 +8,12 @@ import React, { useEffect } from 'react';
 import { Spacer, Stack, Toggle } from '@instana/components';
 
 import MetricSelectionCategoryOverlay from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/MetricSelectionCategoryOverlay';
-import { useTagFilterExpressionState } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/tagFilterUtils/useTagFilterExpressionState';
 import {
   autoFormatterTimeSeriesEnabled,
   lastValueForNonTimeSeriesWidgetEnabled,
   multiGroupTimeSeriesEnabled
 } from 'in-services/featureFlags';
+import { useTagFilterExpressionState } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/tagFilterUtils/useTagFilterExpressionState';
 import { regexValidationError } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/regexValidator';
 import { formCallbacks } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/formStateManagement';
 import getMetricInCatalog from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/getMetricInCatalog';
@@ -243,7 +243,7 @@ export default function FormComponent({
                         id="metric-configurator-cross-series-aggregation"
                         checked={isSumCrossSeriesAggregation}
                         disabled={!isCrossSeriesSumAggregationToggleEnabled}
-                        onChange={e => setIsSumCrossSeriesAggregation(e.target.value)}
+                        onChange={e => setIsSumCrossSeriesAggregation(e.target.checked)}
                       />
                     </span>
                   </Tooltip>
