5c665ab3f135e14b0ece133919bd50116e420234
Sandra S <Sandra.S@ibm.com>
2024-02-13 19:28:06 +0530

    KB-152704: Unit test cases for new location (#14460)

---
+++ packages/in-synthetics/createLocation/CreateNewLocationDialog_test.tsx
@@ -0,0 +1,64 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+// import getSyntheticDatacenterDeployment from 'in-synthetics/subscriptions/getSyntheticDatacenterDeployment';
+import createNewLocationForm from 'in-synthetics/createLocation/createLocationForm';
+import CreateNewLocationDialogPresenter from 'in-synthetics/createLocation/CreateNewLocationDialogPresenter';
+import DeployTabSelection from 'in-synthetics/createLocation/steps/DeployTabSelection';
+import ManagedLocation from 'in-synthetics/createLocation/steps/ManagedLocation';
+
+describe('Synthetic New Location Dialog', () => {
+  const updateForm = jest.fn();
+  const setSimpleModeStep = jest.fn();
+  const onClose = jest.fn();
+
+  it('Renders the new location dialog title correctly', () => {
+    render(
+      <CreateNewLocationDialogPresenter
+        form={createNewLocationForm('private')}
+        onClose={onClose}
+        formId={'new-location-form'}
+        updateForm={updateForm}
+        simpleModeStep={0}
+        setSimpleModeStep={setSimpleModeStep}
+      />
+    );
+    expect(screen.getByText('New Location')).toBeInTheDocument();
+  });
+
+  it('Renders PrivateLocation component correctly', () => {
+    const { container } = render(<DeployTabSelection downloadKey={''} agentKey={''} syntheticAcceptorURL={''} />);
+    expect(screen.getByText('Synthetic PoP Commands')).toBeInTheDocument();
+    expect(container.getElementsByTagName('li').length).toBe(2);
+    expect(container.getElementsByTagName('li')[0]).toHaveTextContent('Simple');
+    expect(container.getElementsByTagName('li')[1]).toHaveTextContent('Redis TLS');
+    expect(
+      screen.getByRole('button', {
+        name: 'Copy'
+      })
+    ).toBeTruthy();
+    expect(
+      screen.getByRole('button', {
+        name: 'Copy'
+      })
+    ).not.toBeDisabled();
+  });
+
+  it('Renders ManagedLocation component correctly', () => {
+    const { container } = render(<ManagedLocation form={createNewLocationForm('managed')} updateForm={updateForm} />);
+
+    expect(screen.getByText('Instana Managed Datacenters')).toBeInTheDocument();
+
+    expect(container.getElementsByTagName('th').length).toBe(4);
+    expect(container.getElementsByTagName('th')[0]).toHaveTextContent('Datacenter Code');
+    expect(container.getElementsByTagName('th')[1]).toHaveTextContent('Datacenter Name');
+    expect(container.getElementsByTagName('th')[2]).toHaveTextContent('Provider');
+    expect(container.getElementsByTagName('th')[3]).toHaveTextContent('Status');
+  });
+});
