60f3fe84045de9e35519e6c7040e550884222b17
BlesyMathew <blessymathew2016@gmail.com>
2024-02-16 18:50:55 +0530

    bug(in-service-levels):removed issue for label

--- packages/in-service-levels/components/ConfigDialog/components/DialogSections/SloObjectiveSection/TimeWindowSelector.tsx
+++ packages/in-service-levels/components/ConfigDialog/components/DialogSections/SloObjectiveSection/TimeWindowSelector.tsx
@@ -109,18 +109,19 @@ export default function TimeWindowSelector() {
               }}
             />
 
-            <label>
-              <Stack direction="horizontal" gap="xsmall" inline align="center">
-                <TimeInput
-                  value={timeField.value}
-                  onChange={time => {
-                    onChange(['objective', 'startTimestamp', 'time'], () => timeField.setValue(time).setTouched(true));
-                  }}
-                  hasError={!timeField.valid && timeField.touched}
-                />
-                <SvgIcon type="lib_datetime_time" />
-              </Stack>
-            </label>
+            <Stack direction="horizontal" gap="xsmall" inline align="center">
+              <TimeInput
+                id="time"
+                value={timeField.value}
+                onChange={time => {
+                  onChange(['objective', 'startTimestamp', 'time'], () => timeField.setValue(time).setTouched(true));
+                }}
+                hasError={!timeField.valid && timeField.touched}
+              />
+              <label htmlFor="time">
+                <SvgIcon aria-label="time_icon" type="lib_datetime_time" />
+              </label>
+            </Stack>
           </Stack>
           {!isDateFieldValid &&
             dateField.messages.map(({ message }, index) => (
