9aa01406badfe7ac4f9a36d724d2d52a3cb17720
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2024-02-22 11:45:06 -0500

    add doc link test

--- packages/in-automation/ActionCatalog/TestActionButton.tsx
+++ packages/in-automation/ActionCatalog/TestActionButton.tsx
@@ -7,16 +7,21 @@
 import { MapForm } from 'formalistic';
 import React from 'react';
 
+import { themes } from '@instana/design-tokens';
+import { SvgIcon } from '@instana/components';
+import { Link } from '@instana/components';
 import { Action } from '@instana/types';
 
+import { isDocLink, isManual, getDocLinkFromFields } from 'in-automation/ActionCatalog/shared';
 import RunActionDialog from 'in-automation/RunActionDialog/RunActionDialog';
-import { isDocLink, isManual } from 'in-automation/ActionCatalog/shared';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { SetFormFunction } from 'in-settings/hooks/useEntityForm';
 import IconButton from 'in-components/IconButton/IconButton';
 import Tooltip from 'in-components/Tooltip/Tooltip';
 import { NewAction } from 'in-automation/api';
 import { t } from 'in-i18n';
+
+import locals from './TestActionButton.mless';
 
 interface TestActionButtonProps {
   action: NewAction | Action;
@@ -42,6 +47,24 @@ export default function TestActionButton({ action, form, setForm }: TestActionBu
               addActiveDialog(<RunActionDialog test action={action as Action} volatileId={{}} />);
             }}
           />
+        </Tooltip>
+      )}
+      {isDocLink(action.type) && (
+        <Tooltip content={t('in-automation:ActionCatalog.test')} delay={500}>
+          <Link
+            className={locals.testDocLink}
+            target="_blank"
+            onClick={() => {
+              if (form && setForm && !form!.hierarchyValid) {
+                setForm(form.setTouched(true, { recurse: true }));
+                return;
+              }
+            }}
+            href={getDocLinkFromFields(action.fields).value}
+          >
+            {' '}
+            <SvgIcon type="lib_actions_play" color={themes.default.ids.color.option.blue['400']} />
+          </Link>
         </Tooltip>
       )}
     </>
