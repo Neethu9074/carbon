463bceee6f52cf64a90c4fea0de40135aee92811
BLESSY MATHEW <74411288+BlesyMathew@users.noreply.github.com>
2024-03-06 17:16:18 +0530

    chore(in-service-levels):added approximate icon

--- packages/in-service-levels/components/SloDashboard/components/chart/IndicatorChart/components/TimeBasedAvailabilityIndicatorChart.tsx
+++ packages/in-service-levels/components/SloDashboard/components/chart/IndicatorChart/components/TimeBasedAvailabilityIndicatorChart.tsx
@@ -25,23 +25,23 @@ import {
   lineWithThreshold,
   thresholdMetricId
 } from 'in-service-levels/components/SloDashboard/components/chart/renderer/lineWithThreshold';
+import { IndicatorChartProps } from 'in-service-levels/components/SloDashboard/components/chart/IndicatorChart/IndicatorChart';
 // @ts-expect-error needs migration
 import zoomInAction from 'in-components/Chart/components/ContextMenu/actions/zoomIn';
-import { IndicatorChartProps } from 'in-service-levels/components/SloDashboard/components/chart/IndicatorChart/IndicatorChart';
 import SloDashboardMarkerLanes from 'in-service-levels/components/SloDashboard/components/chart/SloDashboardMarkerLanes';
 import { copyFirstBucketOfSubsequentDataSeries } from 'in-service-levels/components/SloDashboard/components/chart/utils';
-import { calculateTrafficGranularity } from 'in-service-levels/utils/time';
+import useContextAwareSloTimeWindowConfig from 'in-service-levels/hooks/useContextAwareSloTimeWindowConfig';
 import getUnifiedMetrics, { UnifiedMetricsResult } from 'in-subscription/getUnifiedMetrics';
 import useSliMetricConfiguration from 'in-service-levels/hooks/useSliMetricConfiguration';
 import useSloTimeWindowContext from 'in-service-levels/hooks/useSloTimeWindowContext';
 import { applicationMetrics, websiteMetrics } from 'in-service-levels/metrics';
+import useSloZoomInAction from 'in-service-levels/hooks/useSloZoomInAction';
+import { calculateTrafficGranularity } from 'in-service-levels/utils/time';
 import ResultAwareChart from 'in-components/Chart/ResultAwareChart';
 import { ServiceLevelErrors } from 'in-service-levels/constants';
 import { MetricDataSeries } from 'in-components/Chart/types';
 import { percentage } from 'in-services/formatters/number';
 import { pendingResult } from 'in-services/fixedObjects';
-import useSloZoomInAction from 'in-service-levels/hooks/useSloZoomInAction';
-import useContextAwareSloTimeWindowConfig from 'in-service-levels/hooks/useContextAwareSloTimeWindowConfig';
 
 const metricId = 'availability';
 
@@ -79,6 +79,11 @@ export default function TimeBasedAvailabilityIndicatorChart({
     <ResultAwareChart
       config={{
         title: t('in-service-levels:sloDashboard.components.indicatorChart.title'),
+        renderHistoricDataIndicator: true,
+        hasApproximateData: true,
+        approximateTooltipText: t(
+          'in-service-levels:sloDashboard.components.indicatorChart.components.approximateTooltip'
+        ),
         primaryContextMenuAction: sloZoomInAction.name,
         additionalContextMenuButtons: [sloZoomInAction],
         excludedContextMenuActions: [zoomInAction.name],
--- packages/in-service-levels/components/SloDashboard/components/chart/TrafficChart.tsx
+++ packages/in-service-levels/components/SloDashboard/components/chart/TrafficChart.tsx
@@ -16,6 +16,7 @@ import {
   UnifiedMetricConfiguration
 } from '@instana/types';
 import { t } from '@instana/i18n-react';
+
 // eslint-disable-next-line no-restricted-imports -- We cant specifically allow parts of a otherwise restricted package
 import SloDashboardMarkerLanes from 'in-service-levels/components/SloDashboard/components/chart/SloDashboardMarkerLanes';
 import {
@@ -24,17 +25,17 @@ import {
 } from 'in-service-levels/components/SloDashboard/components/chart/utils';
 // @ts-expect-error needs migration
 import zoomInAction from 'in-components/Chart/components/ContextMenu/actions/zoomIn';
-import { calculateTrafficGranularity } from 'in-service-levels/utils/time';
+import useContextAwareSloTimeWindowConfig from 'in-service-levels/hooks/useContextAwareSloTimeWindowConfig';
 import useTimeWindowAwareSloChartMetrics from 'in-service-levels/hooks/useTimeWindowAwareSloChartMetrics';
 import useBasicTagFilterExpression from 'in-service-levels/navigation/hooks/useBasicFilterExpression';
 import useSloTimeWindowContext from 'in-service-levels/hooks/useSloTimeWindowContext';
 import { applicationMetrics, websiteMetrics } from 'in-service-levels/metrics';
+import useSloZoomInAction from 'in-service-levels/hooks/useSloZoomInAction';
+import { calculateTrafficGranularity } from 'in-service-levels/utils/time';
 import ResultAwareChart from 'in-components/Chart/ResultAwareChart';
 import { ServiceLevelErrors } from 'in-service-levels/constants';
 import lineRenderer from 'in-components/Chart/renderer/line';
 import { number } from 'in-services/formatters/number';
-import useSloZoomInAction from 'in-service-levels/hooks/useSloZoomInAction';
-import useContextAwareSloTimeWindowConfig from 'in-service-levels/hooks/useContextAwareSloTimeWindowConfig';
 
 interface TrafficChartProps {
   configuration: ServiceLevelObjectiveConfiguration;
@@ -54,16 +55,17 @@ export default function TrafficChart({ configuration }: TrafficChartProps) {
     timeConfig,
     timeWindows,
     granularity
-    );
+  );
 
-    const label = getMetricLabels(entity);
-    const metrics = copyFirstBucketOfSubsequentDataSeries(metricResult?.metrics);
-
+  const label = getMetricLabels(entity);
+  const metrics = copyFirstBucketOfSubsequentDataSeries(metricResult?.metrics);
 
   return (
     <ResultAwareChart
       config={{
         title: t('in-service-levels:sloDashboard.components.trafficChart.title'),
+        renderHistoricDataIndicator: true,
+        hasApproximateData: true,
         primaryContextMenuAction: sloZoomInAction.name,
         additionalContextMenuButtons: [sloZoomInAction],
         excludedContextMenuActions: [zoomInAction.name],
