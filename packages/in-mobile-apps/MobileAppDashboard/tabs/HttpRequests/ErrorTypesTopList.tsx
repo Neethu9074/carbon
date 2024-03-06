737462528819a6248ed689ec794b86ecceb44dfb
zhouhn <zhouhn@cn.ibm.com>
2024-03-06 20:44:11 +0800

    chore(TS migration): migrate packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/ErrorTypesTopList

--- packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/ErrorTypesTopList.tsx
+++ packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/ErrorTypesTopList.tsx
@@ -5,15 +5,22 @@
 
 import React from 'react';
 
+import { AggregationType } from '@instana/types';
 import { Link } from '@instana/components';
 
+// @ts-expect-error Could not find a declaration file for module
 import getMobileAppPaginatedBeaconGroups from 'in-mobile-apps/subscriptions/getMobileAppPaginatedBeaconGroups';
+// @ts-expect-error Could not find a declaration file for module
 import { translateDemocratisationTagFiltersToFormModel } from 'in-mobile-apps/tags';
+// @ts-expect-error Could not find a declaration file for module
 import TopListCardPresenter from 'in-components/TopListCard/TopListCardPresenter';
+// @ts-expect-error Could not find a declaration file for module
 import { TopListWithUrlState } from 'in-components/TopListWithUrlState';
-import { useLinkToAnalyze } from 'in-mobile-apps/navigation/paths';
+// @ts-expect-error Could not find a declaration file for module
 import useTagCatalog from 'in-mobile-apps/hooks/useTagCatalog';
+import { useLinkToAnalyze } from 'in-mobile-apps/navigation/paths';
 import { number } from 'in-services/formatters/number';
+import { TagFilter, TimeConfig } from 'in-types';
 import { t } from 'in-i18n';
 
 const metrics = ['beaconCount'];
@@ -21,13 +28,21 @@ const labels = ['Calls'];
 const aggregations = ['SUM'];
 const formatters = [number.compact];
 
+export interface ErrorTypesTopListProp {
+  mobileAppId: string;
+  mobileAppLabel: string;
+  timeConfig: TimeConfig;
+  tagFilters: Array<TagFilter>;
+  renderHistoricDataIndicator: boolean;
+}
+
 export default function ErrorTypesTopList({
   mobileAppId,
   mobileAppLabel,
   timeConfig,
   tagFilters,
   renderHistoricDataIndicator
-}) {
+}: ErrorTypesTopListProp) {
   return (
     <TopListWithUrlState
       title={t('in-mobile-apps:dashboard.tabs.errorTypesTitle')}
@@ -49,7 +64,14 @@ export default function ErrorTypesTopList({
   );
 }
 
-function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggregation }) {
+interface GetListProps {
+  tagFilters: Array<TagFilter>;
+  timeConfig: TimeConfig;
+  selectedMetric: string;
+  selectedMetricAggregation: AggregationType;
+}
+
+function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggregation }: GetListProps) {
   return getMobileAppPaginatedBeaconGroups({
     tagFilters,
     timeConfig,
@@ -73,7 +95,13 @@ function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggrega
   });
 }
 
-function ViewAll({ tagFilters, mobileAppLabel, className }) {
+interface ViewAllProps {
+  mobileAppLabel: string;
+  tagFilters: Array<TagFilter>;
+  className: string;
+}
+
+function ViewAll({ tagFilters, mobileAppLabel, className }: ViewAllProps) {
   const tagCatalogHttpRequest = useTagCatalog('httpRequest');
   const getLinkToMobileAppAnalyze = useLinkToAnalyze();
 
@@ -100,7 +128,16 @@ function ViewAll({ tagFilters, mobileAppLabel, className }) {
   );
 }
 
-function Label({ item, mobileAppLabel, tagFilters }) {
+interface ItemWithName {
+  name: string;
+}
+interface LabelProps {
+  item: ItemWithName;
+  mobileAppLabel: string;
+  tagFilters: Array<TagFilter>;
+}
+
+function Label({ item, mobileAppLabel, tagFilters }: LabelProps) {
   const tagCatalogHttpRequest = useTagCatalog('httpRequest');
   const getLinkToMobileAppAnalyze = useLinkToAnalyze();
 
@@ -118,7 +155,13 @@ function Label({ item, mobileAppLabel, tagFilters }) {
         getLinkToMobileAppAnalyze({
           formModel: translateDemocratisationTagFiltersToFormModel({
             mobileAppLabel,
-            tagFilters: tagFilters.concat({ name: 'mobileBeacon.error.type', stringValue: label, operator: 'EQUALS' }),
+            tagFilters: tagFilters.concat({
+              name: 'mobileBeacon.error.type',
+              stringValue: label,
+              operator: 'EQUALS',
+              type: 'TAG_FILTER',
+              entity: 'NOT_APPLICABLE'
+            }),
             tagCatalog: tagCatalogHttpRequest
           }),
           beaconType: 'httpRequest',
@@ -131,6 +174,10 @@ function Label({ item, mobileAppLabel, tagFilters }) {
   );
 }
 
-function Metric({ formattedMetricValue }) {
+interface MetricProps {
+  formattedMetricValue: any;
+}
+
+function Metric({ formattedMetricValue }: MetricProps) {
   return formattedMetricValue;
 }
