872df4c45937e44d149115d96dad21af97311c65
zhouhn <zhouhn@cn.ibm.com>
2024-03-06 20:54:56 +0800

    chore(TS migration): migrate packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/LocationsTopList

--- packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/LocationsTopList.tsx
+++ packages/in-mobile-apps/MobileAppDashboard/tabs/HttpRequests/LocationsTopList.tsx
@@ -5,15 +5,23 @@
 
 import React from 'react';
 
+import { AggregationType } from '@instana/types';
 import { Link } from '@instana/components';
 
+// @ts-expect-error Could not find a declaration file for module
 import getMobileAppPaginatedBeaconGroups from 'in-mobile-apps/subscriptions/getMobileAppPaginatedBeaconGroups';
+// @ts-expect-error Could not find a declaration file for module
 import { translateDemocratisationTagFiltersToFormModel } from 'in-mobile-apps/tags';
+// @ts-expect-error Could not find a declaration file for module
 import TopListCardPresenter from 'in-components/TopListCard/TopListCardPresenter';
+// @ts-expect-error Could not find a declaration file for module
 import { TopListWithUrlState } from 'in-components/TopListWithUrlState';
-import { useLinkToAnalyze } from 'in-mobile-apps/navigation/paths';
+// @ts-expect-error Could not find a declaration file for module
 import useTagCatalog from 'in-mobile-apps/hooks/useTagCatalog';
+import { useLinkToAnalyze } from 'in-mobile-apps/navigation/paths';
+import { UrlMatrixParamConfig } from 'in-applications/types';
 import { ms, number } from 'in-services/formatters/number';
+import { TagFilter, TimeConfig } from 'in-types';
 import { t } from 'in-i18n';
 
 const metrics = ['beaconCount', 'beaconDuration', 'beaconErrorCount'];
@@ -25,6 +33,15 @@ const labels = [
 const aggregations = ['SUM', 'MEAN', 'SUM'];
 const formatters = [number.compact, ms.compact, number.compact];
 
+export interface LocationsTopListProp {
+  mobileAppId: string;
+  mobileAppLabel: string;
+  timeConfig: TimeConfig;
+  tagFilters: Array<TagFilter>;
+  urlMatrixParamConfig?: UrlMatrixParamConfig;
+  renderHistoricDataIndicator: boolean;
+}
+
 export default function LocationsTopList({
   mobileAppId,
   mobileAppLabel,
@@ -32,7 +49,7 @@ export default function LocationsTopList({
   tagFilters,
   urlMatrixParamConfig,
   renderHistoricDataIndicator
-}) {
+}: LocationsTopListProp) {
   return (
     <TopListWithUrlState
       title={t('in-mobile-apps:dashboard.tabs.pathsTitle')}
@@ -55,7 +72,14 @@ export default function LocationsTopList({
   );
 }
 
-function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggregation }) {
+interface GetListProps {
+  tagFilters: Array<TagFilter>;
+  timeConfig: TimeConfig;
+  selectedMetric: string;
+  selectedMetricAggregation: AggregationType;
+}
+
+function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggregation }: GetListProps) {
   return getMobileAppPaginatedBeaconGroups({
     tagFilters,
     timeConfig,
@@ -79,7 +103,13 @@ function getList({ tagFilters, timeConfig, selectedMetric, selectedMetricAggrega
   });
 }
 
-function ViewAll({ tagFilters, mobileAppLabel, className }) {
+interface ViewAllProps {
+  mobileAppLabel: string;
+  tagFilters: Array<TagFilter>;
+  className: string;
+}
+
+function ViewAll({ tagFilters, mobileAppLabel, className }: ViewAllProps) {
   const tagCatalogHttpRequest = useTagCatalog('httpRequest');
   const getLinkToMobileAppAnalyze = useLinkToAnalyze();
 
@@ -106,7 +136,17 @@ function ViewAll({ tagFilters, mobileAppLabel, className }) {
   );
 }
 
-function Label({ item, mobileAppLabel, tagFilters }) {
+interface ItemWithName {
+  name: string;
+}
+
+interface LabelProps {
+  item: ItemWithName;
+  mobileAppLabel: string;
+  tagFilters: Array<TagFilter>;
+}
+
+function Label({ item, mobileAppLabel, tagFilters }: LabelProps) {
   const tagCatalogHttpRequest = useTagCatalog('httpRequest');
   const getLinkToMobileAppAnalyze = useLinkToAnalyze();
 
@@ -124,7 +164,13 @@ function Label({ item, mobileAppLabel, tagFilters }) {
         getLinkToMobileAppAnalyze({
           formModel: translateDemocratisationTagFiltersToFormModel({
             mobileAppLabel,
-            tagFilters: tagFilters.concat({ name: 'mobileBeacon.http.path', stringValue: label, operator: 'EQUALS' }),
+            tagFilters: tagFilters.concat({
+              name: 'mobileBeacon.http.path',
+              stringValue: label,
+              operator: 'EQUALS',
+              type: 'TAG_FILTER',
+              entity: 'NOT_APPLICABLE'
+            }),
             tagCatalog: tagCatalogHttpRequest
           }),
           beaconType: 'httpRequest',
@@ -137,6 +183,10 @@ function Label({ item, mobileAppLabel, tagFilters }) {
   );
 }
 
-function Metric({ formattedMetricValue }) {
+interface MetricProps {
+  formattedMetricValue: any;
+}
+
+function Metric({ formattedMetricValue }: MetricProps) {
   return formattedMetricValue;
 }
