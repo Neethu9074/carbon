3797cad497a7180de113747d0a7ae98c81a08d6a
Thamjith Thaha <Thamjith.Thaha@ibm.com>
2024-03-07 20:47:24 +0530

    bug(agent-details): fixed a bug where the event not tracked the agent catalog properly (#14977)

--- packages/in-waiting-for-deployment/components/OnboardingWidget/OnboardingWidget.js
+++ packages/in-waiting-for-deployment/components/OnboardingWidget/OnboardingWidget.js
@@ -39,12 +39,9 @@ export default function OnboardingWidget(props) {
     props.Renderer || (agentInstallationV2Enabled ? OnboardingWidgetPresenterV2 : OnboardingWidgetPresenter);
   const trackingService = createTracker(props.trackingIdPrefix);
   const [{ selectedEntry, selectedSubEntry, query }, setUrlState] = useUrlState(urlStateDefinition);
+
   useEffect(() => {
-    if (agentInstallationV2Enabled) {
-      trackingService.catalogPageOpened();
-    } else {
-      trackingService.dialogOpened();
-    }
+    trackingService.dialogOpened();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
