2119704bf7c2b128610253b4ce79ecdd4b5cb827
Thamjith Thaha <Thamjith.Thaha@ibm.com>
2024-03-12 16:33:16 +0530

    Fix conflicts with release 269 (#15034)
    * [webfm] 9 translation
    
    de/es/fr/it/ja/ko/pt_BR/zh_CN/zh_TW translation check-in by SHRUTHI
    RAO(shruthra@in.ibm.com) using WebFM tool.
    
    * Added a missed mixpanel event. (#15026)
    
    * bug(agent-details): fixed a bug where the event not tracked the agent catalog properly (#14977)
    
    * fix(mix panel): event dialogOpened wrapped in a condition for older onboarding scrren
    
    * feat(agent details): added mixpanel to "view deployed agents" button click
    
    * fix(agent details): moved the mixpanel property to the outside render
    
    * 157571 error changing limited access release pr (#15031)
    
    * updated group deletion message
    
    * added contributor aps to access all payload
    
    * 156854 limited ap access type rel269 (#15035)
    
    * updated contribution filter messages
    
    * added case changes
    
    ---------
    
    Co-authored-by: Robert-Hostlowsky <robert.hostlowsky@ibm.com>
    Co-authored-by: Tácio Belmonte <Tacio.Belmonte@ibm.com>
    Co-authored-by: Rosemaria Antony <Rosemaria.Antony@ibm.com>

--- packages/in-plg/components/GetDeployedAgents/GetDeployedAgents.tsx
+++ packages/in-plg/components/GetDeployedAgents/GetDeployedAgents.tsx
@@ -11,6 +11,7 @@ import { Button } from '@instana/components';
 
 import LayoutSection from 'in-plg/pages/onboarding/Layout/LayoutSection';
 import { AgentSnapshotResponse } from 'in-plg/api/AgentSnapshot';
+import createTracker from 'in-waiting-for-deployment/tracker';
 import { getAgentSnapshots } from 'in-plg/api/NetworkUtil';
 import { t } from 'in-i18n';
 
@@ -18,6 +19,8 @@ interface GetDeployedAgentsProps {
   agent: string;
   fromOnboarding?: boolean;
 }
+
+const trackingService = createTracker('agent.installation');
 
 const GetDeployedAgents = ({ agent, fromOnboarding = false }: GetDeployedAgentsProps): JSX.Element | null => {
   const DEPLOYED_AGENT_CHECK_INTERVAL: number = 10000;
@@ -57,6 +60,7 @@ const GetDeployedAgents = ({ agent, fromOnboarding = false }: GetDeployedAgentsP
         <Button
           disabled={!deployedAgentsCount}
           href={`/#/physical?q=${agent}&timeline.to&timeline.fm&timeline.ar=true`}
+          onClick={() => trackingService.deployAgentsButtonClicked()}
         >
           {t('in-plg:agentDetails.common.viewDeployedAgents')}
         </Button>
--- packages/in-waiting-for-deployment/components/OnboardingWidget/OnboardingWidget.js
+++ packages/in-waiting-for-deployment/components/OnboardingWidget/OnboardingWidget.js
@@ -41,7 +41,9 @@ export default function OnboardingWidget(props) {
   const [{ selectedEntry, selectedSubEntry, query }, setUrlState] = useUrlState(urlStateDefinition);
 
   useEffect(() => {
-    trackingService.dialogOpened();
+    if (!agentInstallationV2Enabled) {
+      trackingService.dialogOpened();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
