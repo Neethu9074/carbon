b43abd0c190c7a0d549a134ec0997ccf300801b8
Andreas Schmitt <andreas.schmitt@de.ibm.com>
2024-03-12 13:07:27 +0100

    fix description

--- packages/in-settings/hooks/useDebounce.ts
+++ packages/in-settings/hooks/useDebounce.ts
@@ -8,7 +8,7 @@ import { useEffect, useMemo, useRef } from 'react';
 import { debounce } from 'lodash';
 
 /**
- * React hook that delays the exution of the provided function.
+ * React hook that delays the execution of the provided function.
  *
  * @param func Function to be invoked after specified delay.
  * @param delay Time in ms until the function is invoked.
