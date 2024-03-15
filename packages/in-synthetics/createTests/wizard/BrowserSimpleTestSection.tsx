5d6edfb22eb5c2368edbe0e2b3d81227626f978e
Sandra S <Sandra.S@ibm.com>
2024-03-15 16:03:23 +0530

    chore(SonarQube): Wizard mode createTests code smells (#15011)

--- packages/in-synthetics/createTests/wizard/BrowserSimpleTestSection.tsx
+++ packages/in-synthetics/createTests/wizard/BrowserSimpleTestSection.tsx
@@ -4,7 +4,7 @@
  * Copyright IBM Corp. 2023
  */
 
-import { Field, Item, MapForm } from 'formalistic';
+import { Field, MapForm } from 'formalistic';
 import React from 'react';
 
 import { t } from '@instana/i18n-react';
@@ -37,7 +37,7 @@ const BrowserSimpleTestSection = ({ urlField, updateForm, form }: BrowserSimpleT
             value={field.value}
             onChange={({ target }: React.ChangeEvent<HTMLInputElement>) => {
               updateForm(
-                form.updateIn(['configuration', 'url'], (field: Item) =>
+                form.updateIn(['configuration', 'url'], field =>
                   (field as Field<string>).setValue(target?.value).setTouched(true)
                 )
               );
