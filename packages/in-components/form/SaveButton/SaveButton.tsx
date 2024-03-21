5f620e3fa2b33ef04545e5e7c62b91a10459bcb9
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2024-03-21 15:21:48 +0100

    chore(deps): update ui-foundation (#15159)

--- packages/in-components/form/SaveButton/SaveButton.tsx
+++ packages/in-components/form/SaveButton/SaveButton.tsx
@@ -53,7 +53,6 @@ export default function SaveButton({
       icon={isSaving ? 'lib_actions_loading' : icon}
       iconSpinning={isSaving}
       className={className}
-      // @ts-expect-error: Property 'autoFocus' does not exist on type 'IntrinsicAttributes & PropsType & RefAttributes<any>'
       autoFocus={autoFocus}
     >
       {children}
