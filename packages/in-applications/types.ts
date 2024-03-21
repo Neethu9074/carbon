f10c799efeff262f634a3141b47cfd9576a4f78d
Caner Gözübüyük <caner.goez@ibm.com>
2024-03-21 16:32:05 +0100

    chore(TS-migration): migrate packages/in-applications/ApplicationMap/misc/layouting/nodeHelper

--- packages/in-applications/types.ts
+++ packages/in-applications/types.ts
@@ -15,27 +15,27 @@ export interface UrlMatrixParamConfig {
 export type WithLabel = Result<Application | Endpoint | ServiceLabel>;
 
 // #region Application Map
-export interface ApplicationMapNode {
+export interface APMapNode {
   id: string;
   x: number;
   y: number;
 }
 
-export interface ApplicationMapGraphNode extends ApplicationMapNode {
+export interface APMapGraphNode extends APMapNode {
   size: number;
-  inNode: ApplicationMapNode;
+  inNode: APMapNode;
   fixed: boolean;
   fr_x: number;
   fr_y: number;
   fr: { dx: number; dy: number };
 }
 
-export interface ApplicationMapEdge {
+export interface APMapEdge {
   id: string;
-  from: ApplicationMapNode;
-  to: ApplicationMapNode;
+  from: APMapNode;
+  to: APMapNode;
 }
-export interface ApplicationMapGraphEdge {
+export interface APMapGraphEdge {
   id: number;
   source: string;
   target: string;
@@ -47,9 +47,9 @@ export interface ApplicationMapNodePosition {
 }
 
 export interface ApplicationMapGraph {
-  nodes: ApplicationMapGraphNode[];
-  nodeMap: Map<string, ApplicationMapGraphNode>;
-  edges: ApplicationMapGraphEdge[];
+  nodes: APMapGraphNode[];
+  nodeMap: Map<string, APMapGraphNode>;
+  edges: APMapGraphEdge[];
 }
 
 export interface ApplicationMapDimensions {
@@ -57,5 +57,26 @@ export interface ApplicationMapDimensions {
   minY: number;
   maxX: number;
   maxY: number;
+}
+
+export interface TransformedAPMapNode {
+  name: string;
+  rank: number;
+  inNode: APMapNode;
+  outgoingConnections: TransformedAPMapNode[];
+  incomingConnections: TransformedAPMapNode[];
+  __touched: boolean;
+  x?: number;
+  y?: number;
+}
+
+export interface APMapEdgeLookUpTable {
+  incoming: Record<string, string>;
+  outgoing: Record<string, string>;
+}
+
+export interface APMapGraphColumnInfo {
+  rank: number;
+  nodes: TransformedAPMapNode[];
 }
 // #endregion
