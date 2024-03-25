8a6c821905db03bd8b3f06f2c60d6af190033860
Caner Gözübüyük <caner.goez@ibm.com>
2024-03-25 10:58:21 +0100

    chore(ts-migration): migrate in-applications/FlowMap/misc/ParticleEmitter/callsToParticlesCalculator

--- packages/in-applications/FlowMap/misc/ParticleEmitter/callsToParticlesCalculator.ts
+++ packages/in-applications/FlowMap/misc/ParticleEmitter/callsToParticlesCalculator.ts
@@ -3,10 +3,10 @@
  * (c) Copyright Instana Inc.
  */
 
-export default function calculate(calls) {
-  if (!calls || calls < 0) {
+export default function calculate(numberOfCalls: number) {
+  if (!numberOfCalls || numberOfCalls < 0) {
     return 0;
   }
 
-  return Math.log10(calls);
+  return Math.log10(numberOfCalls);
 }
