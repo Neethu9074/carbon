c33936cb9e1dc56c79a5f38e9e1e5f056248cb99
Divya G <Divya.G6@ibm.com>
2024-03-26 15:01:38 +0530

    adding more properties

--- packages/in-services/tracking/segment/PageTracker.ts
+++ packages/in-services/tracking/segment/PageTracker.ts
@@ -6,15 +6,27 @@
 
 import { useEffect } from 'react';
 
+import { combineLatest } from '@instana/observables';
+
+//import getAccount from 'in-subscription/getAccount';
+import { playwithEnabled } from 'in-services/featureFlags';
 //@ts-expect-error
 import { Segment, commonProperties } from 'in-services/tracking/segment/SegmentInit';
-import { playwithEnabled } from 'in-services/featureFlags';
+import getUsageInfo from 'in-subscription/getUsageInfo';
+import { getTenantsWithUnits } from 'in-api/account';
+import { find } from 'in-services/arrayUtils';
 import { config } from 'in-services/config';
+import { tenant } from 'in-stores/user';
 
 interface SegmentEventTrackerProps {
   parentProductArea: string;
   parentPageName: string;
 }
+
+let productPlanType: string;
+let instanceId: string;
+let tenantName: string;
+let tenantUnitName: string;
 
 const segment = Segment();
 const PageTracker = ({ parentProductArea, parentPageName }: SegmentEventTrackerProps) => {
@@ -25,21 +37,65 @@ const PageTracker = ({ parentProductArea, parentPageName }: SegmentEventTrackerP
     if (!segment) {
       return;
     }
+
     const url = window.location.href;
     const path = window.location.pathname;
-    const productPlanType = 'NFR';
-    const instanceId = config.tenantUnit;
-    segment.page('', parentPageName, {
-      url,
-      path,
-      parentProductArea,
-      parentPageName,
-      productPlanType,
-      instanceId,
-      commonProperties
+
+    combineLatest([
+      getTenantsWithUnits(),
+      //@ts-expect-error
+      getUsageInfo()
+    ]).once(([tenantWithUnits, usageInfo]) => {
+      //@ts-expect-error
+      productPlanType = getLicenseTypeForSegment(usageInfo?.activeLicenseType);
+      //@ts-expect-error
+      const units = tenantWithUnits[tenant.name];
+
+      if (!units) {
+        return;
+      }
+      //@ts-expect-error
+      const currentUnit = find(units, unit => unit.tenantUnitName === config.tenantUnit);
+      if (currentUnit) {
+        //@ts-expect-error
+        instanceId = currentUnit.tenantUnitId;
+        //@ts-expect-error
+        tenantUnitName = currentUnit.tenantUnitName;
+        //@ts-expect-error
+        tenantName = currentUnit.tenantName;
+      }
+      segment.page('', parentPageName, {
+        url,
+        path,
+        parentProductArea,
+        parentPageName,
+        productPlanType,
+        instanceId,
+        tenantName,
+        tenantUnitName,
+        commonProperties
+      });
     });
   }, [parentProductArea, parentPageName]);
   return null; // SegmentEventTracker does not render anything
+
+  function getLicenseTypeForSegment(currentActiveLicense: string) {
+    switch (currentActiveLicense) {
+      case 'selfService':
+        return 'trial';
+      case 'quota':
+        return 'POC';
+      case 'free_not_for_resale':
+        return 'NFR';
+      case 'paidPerUse':
+      case 'hostBasedPaid':
+        return 'subscription';
+      case 'paid-paygo':
+        return 'paygo';
+      default:
+        return currentActiveLicense;
+    }
+  }
 };
 
 export default PageTracker;
