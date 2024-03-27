60f5e98b905b44196b424ba3c88ec3e97492fec8
Andreas Schmitt <andreas.schmitt@de.ibm.com>
2024-03-27 12:53:44 +0100

    incorporate review comment

--- packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/components/PermissionSection.tsx
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/components/PermissionSection.tsx
@@ -122,7 +122,7 @@ export default function PermissionSection<I extends Object, FORM_TYPE extends Ma
     // Application
     if (applicationContributionFilterEnabled && productArea === ProductArea.APPLICATION) {
       // Only show applications with contributor access for access all
-      if (limitation === ScopedPermissionItem.ACCESS_ALL) {
+      if (limitation === ScopedPermissionItem.ACCESS_ALL && role === AreaRoleWithContributor.CONTRIBUTOR) {
         return entityIds.filter(scopeBinding => scopeBinding.scopeRoleId === ScopeRoles.Contributor);
       } else if (limitation === ScopedPermissionItem.LIMITED_ACCESS) {
         const newScopeRoleId = role === AreaRoleWithContributor.OWNER ? ScopeRoles.Owner : ScopeRoles.Viewer;
