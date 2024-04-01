570eaf1b21f4f551e0bb48ac836cdd2c3f6256f8
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2024-04-01 16:23:52 -0400

    rename event name

--- packages/in-automation/tracker.ts
+++ packages/in-automation/tracker.ts
@@ -16,7 +16,7 @@ import {
   AUTOMATION_POLICY_CREATE,
   AUTOMATION_POLICY_EDIT,
   CLICK_TURBO_LINK_FOR_DETAILS,
-  EXECUTE_TURBO_ACTION
+  VIEW_TURBO_ACTION
 } from 'in-services/tracking/tracking';
 
 export const runActionTracker = (e: Object) => track(AUTOMATION_ACTION_RUN, e);
@@ -30,4 +30,4 @@ export const createPolicyTracker = (e: Object) => track(AUTOMATION_POLICY_CREATE
 export const createBulkPoliciesTracker = (e: Object) => track(AUTOMATION_POLICY_CREATE, e);
 export const editPolicyTracker = (e: Object) => track(AUTOMATION_POLICY_EDIT, e);
 export const clickTurboLinkForDetailsTracker = (e: Object) => track(CLICK_TURBO_LINK_FOR_DETAILS, e);
-export const viewTurboActionTracker = (e: Object) => track(EXECUTE_TURBO_ACTION, e);
+export const viewTurboActionTracker = (e: Object) => track(VIEW_TURBO_ACTION, e);
--- packages/in-services/tracking/eventNames.ts
+++ packages/in-services/tracking/eventNames.ts
@@ -397,7 +397,7 @@ export const AUTOMATION_ACTION_HISTORY_VIEW = 'automation.action.history.view';
 export const AUTOMATION_ACTION_HISTORY_INSTANCE_VIEW = 'automation.action.history.instance.view';
 export const AUTOMATION_ACTION_HISTORY_FEEDBACK_SEND = 'automation.action.history.feedback.used';
 export const CLICK_TURBO_LINK_FOR_DETAILS = 'click.turbo.link.for.details';
-export const EXECUTE_TURBO_ACTION = 'execute.turbo.action';
+export const VIEW_TURBO_ACTION = 'view.turbo.action';
 export const AUTOMATION_POLICY_CREATE = 'automation.policy.create';
 export const AUTOMATION_POLICY_EDIT = 'automation.policy.edit';
 
