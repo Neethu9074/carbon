b6d5df086d6aaf9255374b322a9eea50a607e3bb
kmabhijith <K.M.Abhijith@ibm.com>
2024-04-01 16:11:23 +0530

    DB2LUW Sensor 1.0.55 : Add Db2-pureScale Group-BufferPool metrics.

--- packages/in-forge/plugins/db2Database/Dashboard/PkgCacheTable.js
+++ packages/in-forge/plugins/db2Database/Dashboard/PkgCacheTable.js
@@ -11,13 +11,15 @@ import PluginDashboardsMarkerLanes from 'in-forge/PluginDashboardsMarkerLanes';
 import { getRawPayloadWithTimestamp } from 'in-stores/snapshot';
 import { kiloBytes } from 'in-services/formatters/number';
 import Table from 'in-sdk/components/dashboard/Table';
+import { formatSql } from 'in-forge/tracing/jdbc/sql';
 import connectTo from 'in-hoc/connectTo';
+import Code from 'in-components/Code';
 import { t } from 'in-i18n';
 
 let snapshotProps = {};
 const cols = [
   {
-    title: t('in-forge:plugins.db2Database.scaHostId'),
+    title: t('in-forge:plugins.db2Database.pkgHostId'),
     type: 'string',
     typeArgs: {
       getValue(row) {
@@ -26,23 +28,14 @@ const cols = [
     }
   },
   {
-    title: t('in-forge:plugins.db2Database.scaHostName'),
-    type: 'string',
-    typeArgs: {
-      getValue(row) {
-        return row.value;
-      }
-    }
-  },
-  {
-    title: t('in-forge:plugins.db2Database.currentCfScaSize'),
+    title: t('in-forge:plugins.db2Database.numExcecutionPkg'),
     type: 'metric',
     typeArgs: {
       getSnapshotId(row) {
         return row.snapshotId;
       },
       getMetricName(row) {
-        return `db2cfsca.${row.key}.currentCfScaSize`;
+        return `pkgCache.${row.key}.numExcecutionPkg`;
       },
       getContent: kiloBytes.detailed,
       getTimeWindowAggregation() {
@@ -51,14 +44,14 @@ const cols = [
     }
   },
   {
-    title: t('in-forge:plugins.db2Database.configuredCfScaSize'),
+    title: t('in-forge:plugins.db2Database.reclaimWaitTimePkg'),
     type: 'metric',
     typeArgs: {
       getSnapshotId(row) {
         return row.snapshotId;
       },
       getMetricName(row) {
-        return `db2cfsca.${row.key}.configuredCfScaSize`;
+        return `pkgCache.${row.key}.reclaimWaitTimePkg`;
       },
       getContent: kiloBytes.detailed,
       getTimeWindowAggregation() {
@@ -67,14 +60,14 @@ const cols = [
     }
   },
   {
-    title: t('in-forge:plugins.db2Database.targetedCfScaSize'),
+    title: t('in-forge:plugins.db2Database.avgExcecutionTimePkg'),
     type: 'metric',
     typeArgs: {
       getSnapshotId(row) {
         return row.snapshotId;
       },
       getMetricName(row) {
-        return `db2cfsca.${row.key}.targetedCfScaSize`;
+        return `pkgCache.${row.key}.avgExcecutionTimePkg`;
       },
       getContent: kiloBytes.detailed,
       getTimeWindowAggregation() {
@@ -84,7 +77,7 @@ const cols = [
   }
 ];
 
-function Db2CfScaTable({ data, keys }) {
+function PkgCacheTable({ data, keys }) {
   if (!data) {
     return null;
   }
@@ -114,7 +107,7 @@ function Db2CfScaTable({ data, keys }) {
   return (
     <Table
       withoutPadding
-      cardTitle={t('in-forge:plugins.db2Database.dashboard.purescaleScaTable')}
+      cardTitle={t('in-forge:plugins.db2Database.dashboard.pureScalePkgCacheTable')}
       cols={cols}
       rows={rows}
       getRowDetails={getDetails}
@@ -125,14 +118,19 @@ function Db2CfScaTable({ data, keys }) {
 export default connectTo(props => {
   snapshotProps = props;
   return {
-    data: getRawPayloadWithTimestamp(props.snapshotId, 'db2cfsca_extracted'),
-    keys: getRawPayloadWithTimestamp(props.snapshotId, 'db2CfScaId')
+    data: getRawPayloadWithTimestamp(props.snapshotId, 'pkgCache_extracted'),
+    keys: getRawPayloadWithTimestamp(props.snapshotId, 'pkgCacheId')
   };
-}, Db2CfScaTable);
+}, PkgCacheTable);
+
+function extractQuery(row) {
+  return row.value ? formatSql(row.value) : t('in-forge:plugins.db2Database.errorMessage');
+}
 
 function getDetails(row) {
   return (
     <div>
+      <Code code={extractQuery(row)} lang="sql" softWrap />
       <Chart
         snapshotId={row.snapshotId}
         timeConfig={row.timeConfig}
@@ -140,14 +138,14 @@ function getDetails(row) {
           min: 0,
           formatter: kiloBytes.detailed,
           metrics: [
-            'db2cfsca.' + row.key + '.currentCfScaSize',
-            'db2cfsca.' + row.key + '.configuredCfScaSize',
-            'db2cfsca.' + row.key + '.targetedCfScaSize'
+            'pkgCache.' + row.key + '.numExcecutionPkg',
+            'pkgCache.' + row.key + '.reclaimWaitTimePkg',
+            'pkgCache.' + row.key + '.avgExcecutionTimePkg'
           ],
           labels: [
-            t('in-forge:plugins.db2Database.currentCfScaSize'),
-            t('in-forge:plugins.db2Database.configuredCfScaSize'),
-            t('in-forge:plugins.db2Database.targetedCfScaSize')
+            t('in-forge:plugins.db2Database.numExcecutionPkg'),
+            t('in-forge:plugins.db2Database.reclaimWaitTimePkg'),
+            t('in-forge:plugins.db2Database.avgExcecutionTimePkg')
           ],
           type: 'line'
         }}
