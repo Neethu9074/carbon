09584d8300296ecfb25109cf7ef1a3edcba568ed
zhouhn <zhouhn@cn.ibm.com>
2024-04-01 17:17:29 +0800

    chore(TS migration): migrate packages/in-mobile-apps/subscriptions/getMobileAppBeacons

--- packages/in-mobile-apps/subscriptions/getMobileAppBeacons.ts
+++ packages/in-mobile-apps/subscriptions/getMobileAppBeacons.ts
@@ -1,11 +1,17 @@
 /*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc.
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
  */
+
+import { GetMobileAppBeaconsQuery, Result, CursorPaginatedResult, MobileAppBeaconsItem } from '@instana/types';
 
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
 
-export default createResultSubscriptionFactory({
+export default createResultSubscriptionFactory<
+  GetMobileAppBeaconsQuery,
+  Result<CursorPaginatedResult<MobileAppBeaconsItem>>
+>({
   eventId: 'getMobileAppBeacons',
   disposeSubscriptionOnDocumentHidden: false,
   trackSubscriptionStatistics: true
