236cb36fe1027177651b9316b92cd9215539a174
zhouhn <zhouhn@cn.ibm.com>
2024-04-02 17:37:17 +0800

    chore(TS migration): migrate in-websites/subscriptions/getWebsiteSubdivisions

--- packages/in-websites/subscriptions/getWebsiteSubdivisions.ts
+++ packages/in-websites/subscriptions/getWebsiteSubdivisions.ts
@@ -3,9 +3,14 @@
  * (c) Copyright Instana Inc.
  */
 
+import { GetWebsiteSubdivisionsQuery, Result, PaginatedResult, WebsiteSubdivisionsItem } from '@instana/types';
+
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
 
-export default createResultSubscriptionFactory({
+export default createResultSubscriptionFactory<
+  GetWebsiteSubdivisionsQuery,
+  Result<PaginatedResult<WebsiteSubdivisionsItem>>
+>({
   eventId: 'getWebsiteSubdivisions',
   trackSubscriptionStatistics: true
 });
