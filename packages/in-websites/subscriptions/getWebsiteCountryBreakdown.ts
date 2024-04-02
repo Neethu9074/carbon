aed81dd2b7b9509a812f017fdefe0fb8ff22dda7
zhouhn <zhouhn@cn.ibm.com>
2024-04-02 17:09:04 +0800

    chore(TS migration): migrate in-websites/subscriptions/getWebsiteCountryBreakdown

--- packages/in-websites/subscriptions/getWebsiteCountryBreakdown.ts
+++ packages/in-websites/subscriptions/getWebsiteCountryBreakdown.ts
@@ -3,9 +3,14 @@
  * (c) Copyright Instana Inc.
  */
 
+import { GetWebsiteCountryBreakdownQuery, Result, PaginatedResult, WebsiteCountryBreakdown } from '@instana/types';
+
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
 
-export default createResultSubscriptionFactory({
+export default createResultSubscriptionFactory<
+  GetWebsiteCountryBreakdownQuery,
+  Result<PaginatedResult<WebsiteCountryBreakdown>>
+>({
   eventId: 'getWebsiteCountryBreakdown',
   trackSubscriptionStatistics: true
 });
