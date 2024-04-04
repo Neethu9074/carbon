c8107141a6a6d3e0125ac8b780e4fde7a45fba3c
Laszlo Toeroek <laszlo.toeroek@ibm.com>
2024-04-04 06:23:47 -0400

    Introduce support for `end_of_life_php_version` monitoring issue

---
+++ packages/in-forge/plugins/phpRuntimePlatform/agentMonitoringIssueDefinitions.js
@@ -0,0 +1,25 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2023
+ */
+
+import React from 'react';
+
+import { t, Trans } from 'in-i18n';
+
+export default {
+  end_of_life_php_version: {
+    issueDescription: {
+      Component: function endOfLifePhpVersion({ url, version }) {
+        return (
+          <span>
+            <Trans i18nKey="in-forge:plugins.phpRuntimePlatform.endOfLifePhpVersion" values={{ url, version }} />
+          </span>
+        );
+      }
+    },
+  explanationLinkLabel: t('in-forge:plugins.phpRuntimePlatform.supportedVersionsPage'),
+    explanationLinkHref: `https://www.php.net/supported-versions.php`
+  }
+};
