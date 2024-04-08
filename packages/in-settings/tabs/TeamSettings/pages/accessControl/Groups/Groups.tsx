4328373eb49c8fe0894f9eabd32d7cbe79ff8edc
Andreas Schmitt <andreas.schmitt@de.ibm.com>
2024-04-08 17:27:29 +0200

    chore(TS migration): migrate packages/in-settings/tabs/TeamSettings/pages/accessControl/Groups/Groups

--- packages/in-settings/tabs/TeamSettings/pages/accessControl/Groups/Groups.tsx
+++ packages/in-settings/tabs/TeamSettings/pages/accessControl/Groups/Groups.tsx
@@ -1,134 +1,169 @@
 /*
- * (c) Copyright IBM Corp. 2021
+ * (c) Copyright IBM Corp. 2024
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { ColumnizedContent, KeyValue, Ul, Li } from '@instana/components';
-import { Button } from '@instana/legacy';
+import { Observable, create } from '@instana/observables';
+import { ApiGroup, PermissionSet } from '@instana/types';
+import { KeyValue, Link } from '@instana/components';
 
 import {
   getEntityIdView,
+  getEntityHref,
   teamSettingsAccessControlGroups,
   teamSettingsAccessControlGroupNew
 } from 'in-settings/navigation/paths';
 import { ProductAreaPermissionMap } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/constants';
-import CustomDeleteGroupMessage from 'in-settings/tabs/TeamSettings/pages/accessControl/Groups/CustomDeleteGroupMessage';
 import { ScopeRoles } from 'in-settings/tabs/TeamSettings/pages/accessControl/RolesAndAccessScope/constants';
-import { getGroupsAsResultObservable, deleteGroup } from 'in-settings/tabs/TeamSettings/api/groups';
-import Delete from 'in-settings/components/ApiList/sharedComponents/Delete';
+import { deleteGroup, getGroupAsResultObservable } from 'in-settings/tabs/TeamSettings/api/groups';
+import List, { defaultHeaderWithCount } from 'in-settings/components/List';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
-import WithSubscript from 'in-settings/components/WithSubscript';
-import { ownerRoleId, defaultRoleId } from 'in-stores/user';
-import ApiList from 'in-settings/components/ApiList';
-import Title from 'in-components/Title/Title';
-import { t } from 'in-i18n';
+import { TenantsWithUnits, getTenantsWithUnits } from 'in-api/account';
+import { config } from 'in-services/config';
+import { Trans, t } from 'in-i18n';
 
 export const groupNameDefault = 'Default';
 export const groupNameOwner = 'Owner';
 
-export default function Groups() {
+const tableActions = {
+  delete: {
+    deleteEntity: ({ id }: ApiGroup) => deleteGroup(id),
+    deleteProtection: ({ name }: ApiGroup) => name === groupNameDefault || name === groupNameOwner
+  }
+};
+
+const getEntityName = (entity: ApiGroup) => {
+  return t('in-settings:tabs.groupEntityName', { entityName: entity.name });
+};
+
+const determineAccess = (permissionSet: PermissionSet) => {
+  for (const [, { limitation }] of Object.entries(ProductAreaPermissionMap)) {
+    if (limitation && permissionSet.permissions?.includes(limitation)) {
+      return t('in-settings:tabs.limitedAccess');
+    }
+  }
+
+  return t('in-settings:tabs.accessAll');
+};
+
+const defaultColumnDefinitions = [
+  {
+    id: 'name',
+    label: t('in-settings:tabs.name'),
+    getContent: ({ name, id }: ApiGroup) => (
+      <Link href={getEntityIdView(teamSettingsAccessControlGroups, id)}>
+        <KeyValue value={name} label={''} inverted accentuated />
+      </Link>
+    )
+  },
+  {
+    id: 'members',
+    label: t('in-settings:tabs.numberOfMembers'),
+    getContent: ({ members }: ApiGroup) => members?.length
+  },
+  {
+    id: 'access',
+    label: t('in-settings:tabs.access'),
+    width: 15,
+    getContent: ({ permissionSet }: ApiGroup) => determineAccess(permissionSet),
+    getValue: ({ permissionSet }: ApiGroup) => determineAccess(permissionSet)
+  }
+];
+
+const loadEntities = (): Observable<ApiGroup[]> => {
+  const observer = create<ApiGroup[]>();
+  getGroupAsResultObservable('').subscribe(next => {
+    if (next.progress?.loading) return;
+    if (next.data) {
+      observer.emit(next.data);
+    } else if (next.errors) {
+      observer.emitError(next.errors);
+    }
+  });
+  return observer;
+};
+
+const Groups = () => {
+  const { goToPath } = useNavigation();
+  const [tenantWithUnits, setTenantsWithUnits] = useState({});
+  const [columnDefinitions, setColumnDefinitions] = useState(defaultColumnDefinitions);
+
+  useEffect(() => {
+    const result$ = getTenantsWithUnits();
+    result$.once(data => {
+      if (data[config.tenant]?.length > 0) {
+        const fetchedTenantWithUnits = data[config.tenant][0] as TenantsWithUnits;
+        setTenantsWithUnits(fetchedTenantWithUnits);
+
+        if (columnDefinitions?.length > 2) {
+          const newColumnDefinitions = [...columnDefinitions];
+          // Update access label with tenant unit
+          newColumnDefinitions[2].label =
+            fetchedTenantWithUnits?.[config.tenant]?.length > 1
+              ? t('in-settings:tabs.accessForTenantUnit', { tenantUnit: config?.tenantUnit, tenant: config?.tenant })
+              : t('in-settings:tabs.access');
+          setColumnDefinitions(newColumnDefinitions);
+        }
+      }
+    });
+  }, [columnDefinitions]);
+
+  const getDialogMessage = (group: ApiGroup) => {
+    const contributorApplicationIds = group?.permissionSet?.applicationIds?.filter(
+      g => g.scopeRoleId === ScopeRoles.Contributor
+    );
+    const isContributorApplicationIdPresent =
+      Array.isArray(contributorApplicationIds) && contributorApplicationIds.length > 0;
+
+    return (
+      <>
+        {Array.isArray(tenantWithUnits) && tenantWithUnits.length > 1 ? (
+          <Trans i18nKey="in-settings:tabs.deleteGroupMessage" values={{ groupName: group?.name }} />
+        ) : (
+          <Trans i18nKey="in-settings:components.confirmRemoveItem" values={{ itemName: group?.name }} />
+        )}
+        {isContributorApplicationIdPresent && (
+          <>
+            <br />
+            <Trans
+              i18nKey="in-settings:tabs.thisWillRemoveContributionFilterMessage"
+              values={{ apCount: contributorApplicationIds.length }}
+            />
+          </>
+        )}
+      </>
+    );
+  };
+
   return (
     <>
-      <Title title={t('in-settings:tabs.groups')} />
-      <ApiList
-        ListRenderer={ListRenderer}
-        getItems={getGroupsAsResultObservable}
-        deleteItem={deleteGroup}
-        itemName="Group"
-        searchFields={['name']}
-        orderBy="name"
-        renderAdditionalHeaderContent={AdditionalHeaderContent}
+      <List
+        title={t('in-settings:tabs.groups')}
+        getHeader={defaultHeaderWithCount(t('in-settings:tabs.groups'))}
+        getEntityName={getEntityName}
+        columnDefinitions={columnDefinitions}
+        tableActions={tableActions}
+        loadEntities={loadEntities}
+        initialOrderBy="name"
+        onCreateNew={() => goToPath(teamSettingsAccessControlGroupNew)}
+        labelNew={t('in-settings:tabs.addGroup')}
+        searchAttributes={['name']}
+        searchPlaceholder={t('in-settings:components.search')}
+        onRowClick={(group: ApiGroup) => {
+          goToPath(getEntityHref(teamSettingsAccessControlGroups, group.id));
+        }}
+        customDeleteTooltipMessage={(group: ApiGroup) =>
+          group?.name === groupNameDefault || group?.name === groupNameOwner
+            ? t('in-settings:tabs.groupDeleteTooltip', { context: group.name })
+            : ''
+        }
+        customDialogMessage={(group: ApiGroup) => getDialogMessage(group)}
         boundedPath="/groups"
       />
     </>
   );
-}
+};
 
-function ListRenderer({ items, deleteItem, currentDeletingItemIds }) {
-  return (
-    <Ul>
-      {items.map(group => (
-        <Li key={group.id} href$={getEntityIdView(teamSettingsAccessControlGroups, group.id)}>
-          <ColumnizedContent
-            columnDefinitions={columnDefinitions}
-            group={group}
-            currentDeletingItemIds={currentDeletingItemIds}
-            deleteItem={deleteItem}
-            isDisabled={group.id === ownerRoleId || group.id === defaultRoleId}
-          />
-        </Li>
-      ))}
-    </Ul>
-  );
-}
-
-function AdditionalHeaderContent() {
-  const { createHrefToPath } = useNavigation();
-  return (
-    <Button
-      kind="action"
-      href={createHrefToPath(teamSettingsAccessControlGroupNew)}
-      icon="lib_openclose_add_circle_outline"
-    >
-      {t('in-settings:tabs.addGroup')}
-    </Button>
-  );
-}
-
-const columnDefinitions = [
-  {
-    getContent({ group }) {
-      const isLimitedAccessGroup = () => {
-        for (const [, { limitation }] of Object.entries(ProductAreaPermissionMap)) {
-          if (limitation && group.permissionSet.permissions?.includes(limitation)) return true;
-        }
-        return false;
-      };
-      return (
-        <WithSubscript subscript={isLimitedAccessGroup() ? t('in-settings:tabs.limitedAccess') : null}>
-          {group.name}
-        </WithSubscript>
-      );
-    }
-  },
-  {
-    width: '8rem',
-    getContent({ group }) {
-      return <KeyValue value={group.members.length} label={t('in-settings:tabs.users')} accentuated />;
-    }
-  },
-  {
-    width: '2rem',
-    getContent({ group, deleteItem, currentDeletingItemIds, isDisabled }) {
-      const isDefaultOrOwnerGroup = group.name === groupNameDefault || group.name === groupNameOwner;
-      const tooltipContent = isDefaultOrOwnerGroup
-        ? t('in-settings:tabs.groupDeleteTooltip', { context: group.name })
-        : undefined;
-
-      const contributorApplicationIds = group?.permissionSet?.applicationIds?.filter(
-        g => g.scopeRoleId === ScopeRoles.Contributor
-      );
-      const isContributorApplicationIdPresent =
-        Array.isArray(contributorApplicationIds) && contributorApplicationIds.length > 0;
-
-      return (
-        <Delete
-          disabled={isDisabled}
-          itemName={group.name}
-          tooltipContent={tooltipContent}
-          doDelete={() => deleteItem(group.id)}
-          isDeleting={currentDeletingItemIds.has(group.id)}
-          dialogMessage={
-            <CustomDeleteGroupMessage
-              group={group}
-              isContributorApplicationIdPresent={isContributorApplicationIdPresent}
-              apCount={contributorApplicationIds.length}
-            />
-          }
-        />
-      );
-    }
-  }
-];
+export default Groups;
