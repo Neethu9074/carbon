075a762354dfbdf5a6acb198ed62f6cf48a4abc0
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2024-04-10 13:57:00 -0400

    fix turbo
    fix turbo

--- packages/in-automation/RunActionDialog/RunActionDialog.tsx
+++ packages/in-automation/RunActionDialog/RunActionDialog.tsx
@@ -65,6 +65,7 @@ import getAgentSnapshotsInTimeframe, { OUT } from 'in-subscription/getAgentSnaps
 import FormFooter, { CancelButton } from 'in-components/form/FormFooter/FormFooter';
 import { ActionInstance } from 'in-automation/subscriptions/submitActionExecution';
 import { Action, Event, ParameterValue, VolatileId, Policy } from 'in-types';
+import { refresh } from 'in-automation/AutomationCard/useScoredActions';
 import { notBlankValidator } from 'in-services/validators/string';
 import SaveButton from 'in-components/form/SaveButton/SaveButton';
 import { Option, Options } from 'in-components/ComboBox/ComboBox';
@@ -87,7 +88,6 @@ interface RunActionDialogProps {
   policy?: NewPolicy;
   executePolicy?: Policy;
   handleSave?: (params: ParameterValue[], volatileId: VolatileId) => void;
-  triggerReload?: (n: number) => void;
 }
 
 export default function RunActionDialog({
@@ -97,8 +97,7 @@ export default function RunActionDialog({
   test,
   policy,
   handleSave,
-  executePolicy,
-  triggerReload
+  executePolicy
 }: RunActionDialogProps) {
   const [actionInstanceId, setActionInstanceId] = useState('');
   const [error, setError] = useState('');
@@ -166,8 +165,7 @@ export default function RunActionDialog({
                   event,
                   policy,
                   handleSave,
-                  executePolicy,
-                  triggerReload
+                  executePolicy
                 })
               }
             />
@@ -280,7 +278,6 @@ interface OnSaveParams extends Pick<RunActionDialogProps, 'action' | 'event'> {
   policy?: NewPolicy;
   handleSave?: (params: ParameterValue[], volatileId: VolatileId) => void;
   executePolicy?: Policy;
-  triggerReload?: (n: number) => void;
 }
 
 function onSave({
@@ -294,8 +291,7 @@ function onSave({
   event,
   policy,
   handleSave,
-  executePolicy,
-  triggerReload
+  executePolicy
 }: OnSaveParams) {
   // when user have single turbonomic agent we just show it as static text and run action. we do not have any form.valid case in that scenario.
   // when user have multiple turbonomic agents, we show dropdown with agents and, we have to execute below code in that scenario.
@@ -378,8 +374,8 @@ function onSave({
       setActionInstanceId(response?.actionInstanceId);
     } else {
       setActionInstanceId(response.actionInstanceId);
-      if (triggerReload && isExternal(action.type)) {
-        triggerReload(Math.random());
+      if (isExternal(action.type)) {
+        refresh();
       }
     }
   };
