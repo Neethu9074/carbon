455680774525b388b98aefb768c2c191a776ee73
DIANA Costantino <dcostant@us.ibm.com>
2024-04-12 12:17:27 -0400

    feat(numberinput): move Input to ui-foundation

--- packages/in-components/form/Input/Input.tsx
+++ packages/in-components/form/Input/Input.tsx
@@ -1,10 +1,8 @@
 /*
- * IBM Confidential
- * PID 5737-N85, 5900-AG5
- * Copyright IBM Corp. 2024
+ * (c) Copyright IBM Corp. 2021
+ * (c) Copyright Instana Inc.
  */
 
-// Implementation moved to ui-foundataion
 import type { InputProps } from '@instana/components';
 import { Input } from '@instana/components';
 
