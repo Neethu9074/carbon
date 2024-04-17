babaa352c506d39f105fd46ea4c0ea1548729008
Pradosh E <Pradosh.E@ibm.com>
2024-04-17 19:43:15 +0530

    removing action tab from template (#15530)

--- packages/in-alerting/smart-alerts/applications/tearSheet/steps/TearSheetStepConfigs.tsx
+++ packages/in-alerting/smart-alerts/applications/tearSheet/steps/TearSheetStepConfigs.tsx
@@ -31,12 +31,6 @@ export const stepConfigs = [
     title: t('in-alerting:smartAlerts.applications.tearSheet.step4Title'),
     validateIntermediately: [],
     isOptional: true
-  },
-  {
-    title: t('in-alerting:smartAlerts.applications.tearSheet.step6Title'),
-    validateIntermediately: [],
-    hidden: true,
-    isOptional: true
   }
 ];
 
@@ -49,8 +43,7 @@ export const getStepRenderers = (props: AlertConfigTearSheetWithThresholdProps) 
   () => <AlertingTearSheetContent title={stepConfigs[1].title}>{''}</AlertingTearSheetContent>,
   () => <AlertingTearSheetContent title={stepConfigs[2].title}>{''}</AlertingTearSheetContent>,
   () => <AlertingTearSheetContent title={stepConfigs[3].title}>{''}</AlertingTearSheetContent>,
-  () => <AlertingTearSheetContent title={stepConfigs[4].title}>{''}</AlertingTearSheetContent>,
-  () => <AlertingTearSheetContent title={stepConfigs[5].title}>{''}</AlertingTearSheetContent>
+  () => <AlertingTearSheetContent title={stepConfigs[4].title}>{''}</AlertingTearSheetContent>
 ];
 
 export const getFooterActions = (editMode: boolean | undefined, backOrCancel: (oldStep: number) => void) => [
