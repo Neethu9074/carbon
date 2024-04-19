1ebe5ee38b830fc73e417556dfcd24b22eadaef6
Anju Shivan <Anju.Shivan@ibm.com>
2024-04-19 14:05:03 +0530

    chore(TS migration): migrate packages/in-sdk/components/sidebar/List/List

--- packages/in-sdk/components/sidebar/List/List.tsx
+++ packages/in-sdk/components/sidebar/List/List.tsx
@@ -4,15 +4,15 @@
  */
 
 /* eslint-disable react/no-multi-comp */
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import locals from './List.mless';
 
-export default function List({ children }) {
+export default function List({ children }: { children: ReactNode }): JSX.Element {
   return <ul className={locals.list}>{children}</ul>;
 }
 
-List.Item = function ListItem({ children, onClick }) {
+List.Item = function ListItem({ children, onClick }: { children: ReactNode; onClick?: () => void }): JSX.Element {
   if (__DEV__ && onClick) {
     throw new Error(
       'Illegal and old usage of an onClick handler on the List component. ' +
