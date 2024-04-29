8dc7f7ab18e3550415d39260de6bc0e7f8c29ec6
KYLE BLAUER <kgblauer@us.ibm.com>
2024-04-29 10:43:18 -0500

    Bizops activity golden signals spark charts (#15548)
    * activities spark charts
    
    * null check
    
    * add duration unit

--- packages/in-bizops/dashboards/summary/tabs/activities/Activities.tsx
+++ packages/in-bizops/dashboards/summary/tabs/activities/Activities.tsx
@@ -102,11 +102,32 @@ function getBusinessProcessActivityList({
     metrics: {
       count: {
         metric: 'activities_count',
-        aggregation: 'DISTINCT_COUNT'
+        aggregation: 'DISTINCT_COUNT',
+        granularity: 0
       },
       timeseries_counts: {
         metric: 'activities_count',
         aggregation: 'DISTINCT_COUNT',
+        granularity: getChartGranularity(timeConfig)
+      },
+      duration: {
+        metric: 'activityDuration',
+        aggregation: 'MEAN',
+        granularity: 0
+      },
+      timeseries_duration: {
+        metric: 'activityDuration',
+        aggregation: 'MEAN',
+        granularity: getChartGranularity(timeConfig)
+      },
+      call_latency: {
+        metric: 'call_latency',
+        aggregation: 'MEAN',
+        granularity: 0
+      },
+      timeseries_latency: {
+        metric: 'call_latency',
+        aggregation: 'MEAN',
         granularity: getChartGranularity(timeConfig)
       }
     },
