b35ad7f2e429791bbe2ef4d39516daa2822f1efb
KYLE BLAUER <kgblauer@us.ibm.com>
2024-04-29 11:45:15 -0500

    272 bizops cherry pick (#15729)
    * Bizops activity golden signals spark charts (#15548)
    
    * activities spark charts
    
    * null check
    
    * add duration unit
    
    * Bizops services page (#15616)
    
    * column defs
    
    * new page
    
    * service page
    
    * view all
    
    * i18n
    
    * remove css
    
    * remove commented
    
    * add service links

--- packages/in-bizops/dashboards/summary/tabs/activities/columnDefinitions.tsx
+++ packages/in-bizops/dashboards/summary/tabs/activities/columnDefinitions.tsx
@@ -17,13 +17,13 @@ import {
   businessProcessDashboard
 } from 'in-bizops/navigation/paths';
 import { ServerTablePresenterProps } from 'in-components/tables/ServerTable/ServerTablePresenter';
+import { millis, number, timeByMillisZeroDecimalPlaces } from 'in-services/formatters/number';
 import { getMatrixParameter, setOrDeleteMatrixKey } from 'in-stores/navigation/matrix';
 import { ColumnDefinition } from 'in-components/tables/ServerTable/types';
 import { getTimeConfigAlignedToResultTime } from 'in-stores/time/config';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import { selectBizopsProcessActivitiesTracker } from 'in-bizops/tracker';
 import { getChartGranularity } from 'in-stores/metric/metric';
-import { number } from 'in-services/formatters/number';
 import { t } from 'in-i18n';
 
 import locals from './columnDefinitions.mless';
@@ -68,37 +68,53 @@ export const activitiesColumnDefinitions: ColumnDefinition<BusinessActivityItem,
           //@ts-expect-error
           timeConfig={getTimeConfigAlignedToResultTime(timeConfig, result)}
           aggregation="DISTINCT_COUNT"
-          metrics={item.metrics.timeseries_counts}
-          metric={item.metrics.count}
+          metrics={item.metrics?.timeseries_counts}
+          metric={item.metrics?.count}
           tooltipFormatter={number.compact}
         />
       );
     }
-  }
-  /*
+  },
   {
-    id: 'duration', //TODO: Fix once backend support exists
+    id: 'duration',
     sortable: true,
     defaultOrderDirection: 'DESC',
     label: t('in-bizops:lists.durationLabel'),
-    getContent() {
+    getContent(item: BusinessActivityItem, { timeConfig, result }) {
       return (
-        <div>
-          <h4>1</h4>
-        </div>
+        <SparkChart
+          loading={false}
+          rollup={getChartGranularity(timeConfig)}
+          //@ts-ignore
+          timeConfig={getTimeConfigAlignedToResultTime(timeConfig, result)}
+          aggregation="MEAN"
+          metrics={item.metrics?.timeseries_duration}
+          metric={item.metrics?.duration}
+          tooltipFormatter={timeByMillisZeroDecimalPlaces}
+        />
       );
     }
   },
   {
-    id: 'health', //TODO: Fix once backend support exists
+    id: 'latency',
     sortable: true,
     defaultOrderDirection: 'DESC',
-    label: t('in-bizops:lists.healthLabel'),
-    getContent() {
-      return <h4>Health</h4>;
+    label: t('in-bizops:lists.latencyLabel'),
+    getContent(item: BusinessActivityItem, { timeConfig, result }) {
+      return (
+        <SparkChart
+          loading={false}
+          rollup={getChartGranularity(timeConfig)}
+          //@ts-ignore
+          timeConfig={getTimeConfigAlignedToResultTime(timeConfig, result)}
+          aggregation="MEAN"
+          metrics={item.metrics?.timeseries_latency}
+          metric={item.metrics?.call_latency}
+          tooltipFormatter={millis.compact}
+        />
+      );
     }
   }
-  */
 ];
 
 function ActivityLink(activityName: string | undefined, activityId: string | undefined) {
