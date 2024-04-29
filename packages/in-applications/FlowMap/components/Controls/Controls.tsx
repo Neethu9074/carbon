250822ade8b42f7e3f720b7d9c592591e72c23ed
Nick Vargas <navargas@us.ibm.com>
2024-04-29 10:10:26 -0700

    Disable carbon content switcher in FlowMap (#15727)

--- packages/in-applications/FlowMap/components/Controls/Controls.tsx
+++ packages/in-applications/FlowMap/components/Controls/Controls.tsx
@@ -6,7 +6,6 @@
 import React, { Fragment, useEffect } from 'react';
 
 import { ResultPrecisionDetails } from '@instana/types';
-import { ButtonGroup } from '@instana/components';
 import { LinkProps } from '@instana/components';
 import { useObservable } from '@instana/hooks';
 
@@ -21,6 +20,7 @@ import VerticalControlsPresenter from 'in-components/MapControls/VerticalControl
 import { getServiceLocators } from 'in-applications/FlowMap/serviceLocator/serviceLocator';
 import MultiLineToolTipIcon from 'in-components/MultiLineToolTipIcon/MultiLineToolTipIcon';
 import MapButtonGroup from 'in-components/MapControls/ButtonGroup';
+import ButtonGroup from 'in-components/ButtonGroup/ButtonGroup';
 import Button from 'in-components/MapControls/Button';
 import Tooltip from 'in-components/Tooltip';
 import { t } from 'in-i18n';
@@ -132,6 +132,8 @@ function HeatmapButtons({ serviceLocatorUid, entity }: HeatmapButtonsProps) {
 
   return (
     <ButtonGroup
+      // ContentSwitcher not used because it is not toggleable
+      // switch to different component or use a default "None" option
       buttonPropsList={[
         {
           text: t('in-applications:labelCalls'),
