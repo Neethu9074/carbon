9237e9820e56508eee415d4bcbdd9fcadcb03786
DIANA Costantino <dcostant@us.ibm.com>
2024-04-30 13:11:20 -0400

    feat(carboninput): move to ui-foundation

--- packages/in-components/form/Input/Input.tsx
+++ packages/in-components/form/Input/Input.tsx
@@ -3,6 +3,7 @@
  * (c) Copyright Instana Inc.
  */
 
+// Implementation moved to ui-foundataion
 import type { InputProps } from '@instana/components';
 import { Input } from '@instana/components';
 
