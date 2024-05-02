907ed8b450d5bcbb651f1c36ef846203b90d6469
Pradosh E <Pradosh.E@ibm.com>
2024-05-02 12:12:18 +0530

    Adding Time threshold section in smart alert Full screen (#15645)
    * Adding Time threshold section in smart alert Full screen
    
    * adjusting css to give more space to time threshold sliders

--- packages/in-alerting/smart-alerts/applications/tearSheet/steps/AlertConfigTearSheetStep4.js
+++ packages/in-alerting/smart-alerts/applications/tearSheet/steps/AlertConfigTearSheetStep4.js
@@ -14,15 +14,17 @@ import LogsThresholdCondition from 'in-alerting/smart-alerts/applications/tearSh
 import StaticOrAdaptiveSwitch from 'in-alerting/smart-alerts/applications/dialog/advanced/StaticOrAdaptiveThresholdSwitch/StaticOrAdaptiveSwitch';
 import ChartViewConfiguratorWithEntitySelection from 'in-alerting/smart-alerts/applications/chart/ChartViewConfiguratorWithEntitySelection';
 import ApplicationAlertingChartWithErrorMessage from 'in-alerting/smart-alerts/applications/chart/ApplicationAlertingChartWithErrorMessage';
+import TimeThresholdConfigPresenter from 'in-alerting/smart-alerts/components/tearSheet/TimeThresholdConfig/TimeThresholdConfigPresenter';
+import { ADAPTIVE_BASELINE, HISTORIC_BASELINE, STATIC_THRESHOLD } from 'in-alerting/smart-alerts/data/thresholdTypes';
 import HistoricBaselineErrorMessage from 'in-alerting/smart-alerts/components/dialog/HistoricBaselineErrorMessage';
 import AdaptiveBaselineErrorMessage from 'in-alerting/smart-alerts/components/dialog/AdaptiveBaselineErrorMessage';
 import EntitySelectionFormUpdater from 'in-alerting/smart-alerts/applications/chart/EntitySelectionFormUpdater';
 import IncompleteChartPlaceholder from 'in-alerting/smart-alerts/components/dialog/IncompleteChartPlaceholder';
 import { onThresholdTypeChange } from 'in-alerting/smart-alerts/applications/form/thresholdTypeForm';
-import { ADAPTIVE_BASELINE, HISTORIC_BASELINE } from 'in-alerting/smart-alerts/data/thresholdTypes';
 import { getBlueprintConfig } from 'in-alerting/smart-alerts/applications/data/blueprintConfig';
 import AlertTypeSwitch from 'in-alerting/smart-alerts/applications/components/AlertTypeSwitch';
 import TearSheetStepContentWrapper from 'in-alerting/components/TearSheetStepContentWrapper';
+import { oneMinuteGranularityForStaticThresholdEnabled } from 'in-services/featureFlags';
 import { t } from 'in-i18n';
 
 import locals from './AlertConfigTearSheetStep4.mless';
@@ -32,6 +34,7 @@ export default function AlertConfigTearSheetStep4(props) {
     form,
     updateForm,
     editMode,
+    onChange,
     onChartViewConfigChange,
     selectedChartViewConfigIndex,
     isGlobalSmartAlert,
@@ -50,23 +53,23 @@ export default function AlertConfigTearSheetStep4(props) {
 
   return (
     <>
-      <TearSheetStepContentWrapper
-        headline={t('in-alerting:smartAlerts.applications.tearSheet.threshold.title')}
-        description={t('in-alerting:smartAlerts.applications.tearSheet.threshold.description')}
-      >
-        {blueprintConfig?.baselineEnabled && (
-          <div className={locals.container}>
-            <span className={locals.label}>Threshold Type</span>
-            <StaticOrAdaptiveSwitch
-              form={form}
-              setForm={updateForm}
-              onThresholdTypeChange={onThresholdTypeChange}
-              isTearSheet
-            />
-          </div>
-        )}
+      <div className={locals.container40_60}>
+        <TearSheetStepContentWrapper
+          headline={t('in-alerting:smartAlerts.applications.tearSheet.threshold.title')}
+          description={t('in-alerting:smartAlerts.applications.tearSheet.threshold.description')}
+        >
+          {blueprintConfig?.baselineEnabled && (
+            <div className={locals.container}>
+              <span className={locals.label}>Threshold Type</span>
+              <StaticOrAdaptiveSwitch
+                form={form}
+                setForm={updateForm}
+                onThresholdTypeChange={onThresholdTypeChange}
+                isTearSheet
+              />
+            </div>
+          )}
 
-        <>
           {!ruleComplete ? (
             <IncompleteChartPlaceholder message={blueprintConfig.incompleteRuleMessage} />
           ) : (
@@ -86,8 +89,23 @@ export default function AlertConfigTearSheetStep4(props) {
           )}
           {thresholdType === HISTORIC_BASELINE && <HistoricBaselineErrorMessage thresholdResult={thresholdResult} />}
           {thresholdType === ADAPTIVE_BASELINE && <AdaptiveBaselineErrorMessage thresholdResult={thresholdResult} />}
-        </>
-      </TearSheetStepContentWrapper>
+        </TearSheetStepContentWrapper>
+        <TearSheetStepContentWrapper
+          headline={t('in-alerting:smartAlerts.applications.tearSheet.timeThreshold.title')}
+          description={t('in-alerting:smartAlerts.applications.tearSheet.timeThreshold.description')}
+        >
+          <TimeThresholdConfigPresenter
+            form={form}
+            onChange={onChange}
+            updateForm={updateForm}
+            impactTimeThresholdDisabled={blueprintConfig.impactTimeThresholdDisabled}
+            hasTraceImpactOption
+            oneMinuteGranularityAllowed={
+              thresholdType === STATIC_THRESHOLD && oneMinuteGranularityForStaticThresholdEnabled
+            }
+          />
+        </TearSheetStepContentWrapper>
+      </div>
       <TearSheetStepContentWrapper
         headline={t('in-alerting:smartAlerts.applications.tearSheet.alertChart.title')}
         description={t('in-alerting:smartAlerts.applications.tearSheet.alertChart.description')}
