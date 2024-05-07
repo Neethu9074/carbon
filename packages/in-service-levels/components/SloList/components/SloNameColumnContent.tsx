aa7b2c3e790a9ef6b4b9a62ed87943a6faf174ef
BLESSY MATHEW <74411288+BlesyMathew@users.noreply.github.com>
2024-05-07 17:38:25 +0530

    fix(slo):added condition

--- packages/in-service-levels/components/SloList/components/SloNameColumnContent.tsx
+++ packages/in-service-levels/components/SloList/components/SloNameColumnContent.tsx
@@ -20,7 +20,9 @@ export default function SloNameColumnContent({ item }: Props) {
   const { configuration, status } = item;
   const { name, target } = configuration;
   return (
-    <SeverityIndicatorCellContentWrapper severity={status ? calculateSeverity({ status, target }) : undefined}>
+    <SeverityIndicatorCellContentWrapper
+      severity={status !== undefined ? calculateSeverity({ status, target }) : undefined}
+    >
       <Typography variant="body-regular">{name}</Typography>
     </SeverityIndicatorCellContentWrapper>
   );
