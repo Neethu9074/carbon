63dd81c57f4d155514c19c63be83367262975f8e
Irfad K P <IRFAD.K.P@ibm.com>
2024-05-07 17:48:01 +0530

    updated tracker  for  removing violations (#15822)
    updated tracker in to page tracker for violations

--- packages/in-services/tracking/segment/PageTracker.ts
+++ packages/in-services/tracking/segment/PageTracker.ts
@@ -63,7 +63,7 @@ const PageTracker = ({ parentProductArea, parentPageName }: SegmentEventTrackerP
         tenantUnitName = currentUnit.tenantUnitName;
       }
       userId = customRealmName + '-' + instanceId;
-      segment.track('Page Viewed', {
+      segment.page('Page Viewed', {
         UT30: ut30,
         instanceId: instanceId,
         instanceName: tenantUnitName,
