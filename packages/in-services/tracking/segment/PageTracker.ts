3b0bdf065b01abcb366f2d9f7d09c41f7283a2fb
Divya G <Divya.G6@ibm.com>
2024-05-08 13:24:57 +0530

    send user role and user id to segment (#15874)
    * send user properties to segment
    
    * adding user properties to segment

--- packages/in-services/tracking/segment/PageTracker.ts
+++ packages/in-services/tracking/segment/PageTracker.ts
@@ -26,6 +26,7 @@ interface SegmentEventTrackerProps {
 }
 
 interface currentUnitProps {
+  tenantId: string;
   tenantUnitId: string;
   tenantUnitName: string;
   tenantName: string;
@@ -35,6 +36,8 @@ let productPlanType: string;
 let instanceId: string;
 let tenantUnitName: string;
 let userId: string;
+let tenantId: string;
+
 interface UsageInfoProps {
   activeLicenseType: string;
 }
@@ -61,6 +64,7 @@ const PageTracker = ({ parentProductArea, parentPageName }: SegmentEventTrackerP
       }
       const currentUnit: currentUnitProps = find(units, unit => unit.tenantUnitName === config.tenantUnit)!;
       if (currentUnit) {
+        tenantId = currentUnit.tenantId;
         instanceId = currentUnit.tenantUnitId;
         tenantUnitName = currentUnit.tenantUnitName;
       }
@@ -76,13 +80,10 @@ const PageTracker = ({ parentProductArea, parentPageName }: SegmentEventTrackerP
         productCodeType: productCodeType,
         productPlanType: productPlanType,
         productTitle: productTitle,
-        tenantId: instanceId,
+        tenantId: tenantId,
         url: url,
-        user: {
-          bluemixId: userId,
-          role: userSelfDefinedRole,
-          tenantId: instanceId
-        }
+        'user.bluemixId': userId,
+        'user.role': userSelfDefinedRole
       });
     });
   }, [parentProductArea, parentPageName]);
