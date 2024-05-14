11cf0229d8c573ebaaf170eb07ddbf89ced7a311
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2024-05-14 17:36:03 -0400

    add count for automation tabs

--- packages/in-automation/AutomationCard/AutomationCard.tsx
+++ packages/in-automation/AutomationCard/AutomationCard.tsx
@@ -16,6 +16,7 @@ import AutomationPolicies from 'in-automation/AutomationCard/AutomationPolicies'
 import { recommendedActionsTabClickTracker } from 'in-automation/tracker';
 import usePolicies from 'in-automation/AutomationCard/usePolicies';
 import { actionAutomationEnabled } from 'in-services/featureFlags';
+import useHistory from 'in-automation/AutomationCard/useHistory';
 import useTrigger from 'in-automation/AutomationCard/useTrigger';
 import { Col, Row } from 'in-components/layout/Grid/Grid';
 import { Event, VolatileId } from 'in-types';
@@ -28,17 +29,26 @@ export type SetActiveKey = (str: ButtonKey) => void;
 interface AutomationCardButtonGroupProps {
   activeKey: ButtonKey;
   setActiveKey: SetActiveKey;
+  policyCount: number | undefined;
+  recommendedActionsCount: number | undefined;
+  actionHistoryCount: number | undefined;
 }
 
-function AutomationCardButtonGroup({ activeKey, setActiveKey }: AutomationCardButtonGroupProps) {
+function AutomationCardButtonGroup({
+  activeKey,
+  setActiveKey,
+  policyCount,
+  recommendedActionsCount,
+  actionHistoryCount
+}: AutomationCardButtonGroupProps) {
   const buttonProps = [
     {
-      text: t('in-automation:automationPolicies'),
+      text: t('in-automation:automationPoliciesWithCount', { count: policyCount ?? '' }),
       key: 'automationPolicies',
       onClick: () => setActiveKey('automationPolicies')
     },
     {
-      text: t('in-automation:recommendedActions'),
+      text: t('in-automation:recommendedActionsWithCount', { count: recommendedActionsCount ?? '' }),
       key: 'recommendedActions',
       onClick: () => {
         setActiveKey('recommendedActions');
@@ -49,7 +59,7 @@ function AutomationCardButtonGroup({ activeKey, setActiveKey }: AutomationCardBu
 
   if (role?.canViewAutomationActionInstances) {
     buttonProps.push({
-      text: t('in-automation:actionHistory.actionHistory'),
+      text: t('in-automation:actionHistory.actionHistoryWithCount', { count: actionHistoryCount ?? '' }),
       key: 'actionHistory',
       onClick: () => setActiveKey('actionHistory')
     });
@@ -70,14 +80,22 @@ interface AutomationCardProps {
 function AutomationCard({ volatileId, event }: AutomationCardProps) {
   const [activeKey, setActiveKey] = useState<ButtonKey>('automationPolicies');
   const policies = usePolicies({ event });
+  const historyCount = useHistory({ eventId: event.id });
   const trigger = useTrigger({ event });
   const actions = useScoredActions({ event, trigger });
   const recommendedActions = useRecommendedScoredActions({ actions, policies });
+
   return (
     <Row withoutSideMargin>
       <Col xs>
         <Card>
-          <AutomationCardButtonGroup activeKey={activeKey} setActiveKey={setActiveKey} />
+          <AutomationCardButtonGroup
+            activeKey={activeKey}
+            setActiveKey={setActiveKey}
+            policyCount={policies?.data?.length}
+            recommendedActionsCount={recommendedActions?.data?.length}
+            actionHistoryCount={historyCount}
+          />
           {(activeKey === 'automationPolicies' || activeKey === 'recommendedActions') && <Spacer vertical="small" />}
           {activeKey === 'automationPolicies' && (
             <AutomationPolicies
--- packages/in-automation/AutomationCard/AutomationCard_test.tsx
+++ packages/in-automation/AutomationCard/AutomationCard_test.tsx
@@ -54,8 +54,7 @@ describe('AutomationCard', () => {
 
   it('initializes with automationPolicies as the default active key', () => {
     render(<AutomationCard {...props} />);
-    const allMatches = screen.getAllByText(t('in-automation:automationPolicies'));
-    expect(allMatches.length).toBe(1);
+    const allMatches = screen.getAllByText('Automation Policies ()');
     fireEvent.click(allMatches[0]);
     const heading = screen.getByText('Automation Policies ()', { selector: 'h2' });
     expect(heading).toBeInTheDocument();
@@ -63,7 +62,7 @@ describe('AutomationCard', () => {
 
   it('updates to show recommendedActions content when active key is changed', () => {
     render(<AutomationCard {...props} />);
-    const allMatches = screen.getAllByText(t('in-automation:recommendedActions'));
+    const allMatches = screen.getAllByText('Recommended Actions ()');
     fireEvent.click(allMatches[0]);
     const heading = screen.getByText('Recommended Actions ()', { selector: 'h2' });
     expect(heading).toBeInTheDocument();
@@ -71,7 +70,7 @@ describe('AutomationCard', () => {
 
   it('updates to show action history content when active key is changed', () => {
     render(<AutomationCard {...props} />);
-    const allMatches = screen.getAllByText(t('in-automation:actionHistory.actionHistory'));
+    const allMatches = screen.getAllByText(t('Action History ()'));
     fireEvent.click(allMatches[0]);
     const heading = screen.getByText('Action History', { selector: 'h1' });
     expect(heading).toBeInTheDocument();
