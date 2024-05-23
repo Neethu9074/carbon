5f81c395069763247e50ea103cc2900085c6e54b
Anju Shivan <Anju.Shivan@ibm.com>
2024-05-23 14:01:45 +0530

    chore(TS migration): migrate packages/in-applications/creation/components/InboundAllCalls

--- packages/in-applications/creation/components/InboundAllCalls.tsx
+++ packages/in-applications/creation/components/InboundAllCalls.tsx
@@ -3,15 +3,21 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Field, MapForm } from 'formalistic';
 import React from 'react';
 
 import InboundOrAllCallsChoiceVertical from 'in-applications/Dashboards/commonComponents/inboundOrAllCalls/InboundOrAllCallsChoiceVertical';
+//@ts-expect-error Need to migrate to TS
 import { applicationCreationBoundaryScopeSelect } from 'in-applications/creation/tracker';
 import FormGroup from 'in-components/form/FormGroup';
 
 import locals from './InboundAllCalls.mless';
 
-export default function InboundAllCalls({ form, updateForm }) {
+interface InboundAllCallsProps {
+  form: MapForm<any>;
+  updateForm: (form: MapForm<any>) => void;
+}
+export default function InboundAllCalls({ form, updateForm }: InboundAllCallsProps): JSX.Element {
   const boundaryScopeField = form.get('boundaryScope');
 
   return (
@@ -21,7 +27,11 @@ export default function InboundAllCalls({ form, updateForm }) {
           boundaryScope={boundaryScopeField.value}
           onBoundaryStateChange={value => {
             applicationCreationBoundaryScopeSelect({ value });
-            updateForm(form.updateIn(['boundaryScope'], field => field.setValue(value.boundaryScope).setTouched(true)));
+            updateForm(
+              form.updateIn(['boundaryScope'], field =>
+                (field as Field<string>).setValue(value.boundaryScope).setTouched(true)
+              )
+            );
           }}
         />
       </FormGroup>
