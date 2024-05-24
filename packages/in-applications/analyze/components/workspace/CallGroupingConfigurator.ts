e00f0fa700730a79c0da7dc668306d7dd410548f
Caner Gözübüyük <caner.goez@ibm.com>
2024-05-24 16:41:00 +0200

    chore(TS migration): migrate packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.ts

--- packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.ts
+++ packages/in-applications/analyze/components/workspace/CallGroupingConfigurator.ts
@@ -3,33 +3,33 @@
  * (c) Copyright Instana Inc.
  */
 
+import { TimeConfig } from '@instana/types';
+
+// @ts-expect-error needs TS migration
 import { createGroupingConfigurator } from 'in-components/GroupingConfigurator';
 import getTagSuggestions from 'in-applications/subscriptions/getTagSuggestions';
 import { getApplicationTagCatalog } from 'in-applications/api/catalog';
+import { AdditionalTagSuggestionProps } from 'in-applications/types';
+import { GetTagSuggestionsProps } from 'in-components/QueryBuilder';
 import { CALLS } from 'in-applications/analyze/metrics';
 
-const {
-  getTagCatalog: getTagCatalogInternal,
-  GroupingConfigurator,
-  isGroupingConfigurationValid: isGroupingConfigurationValidInternal
-} = createGroupingConfigurator({
-  getTagCatalog: props => getApplicationTagCatalog({ dataSource: CALLS, useCase: 'GROUPING' })(props),
-  getSuggestions: args => {
+const { GroupingConfigurator } = createGroupingConfigurator<AdditionalTagSuggestionProps>({
+  getTagCatalog: ({ timeConfig }: { timeConfig: TimeConfig }) =>
+    getApplicationTagCatalog({ dataSource: CALLS, useCase: 'GROUPING' })({ timeConfig }),
+  getSuggestions: (args: GetTagSuggestionsProps<AdditionalTagSuggestionProps>) => {
     return getTagSuggestions({
       entity: args.entity,
-      propose: args.propose,
       tagFilterExpression: args.tagFilterExpression,
       tagName: args.name,
-      value: args.value,
       filter: {
-        timeConfig: args.timeConfig
-      }
+        timeConfig: args.timeConfig,
+        includeInternalCalls: args.includeInternal,
+        includeSyntheticCalls: args.includeSynthetic,
+        useLongTermDataOnly: false
+      },
+      requestingSecondaryKeySuggestions: false
     });
   }
 });
 
 export default GroupingConfigurator;
-
-export const getGroupingTagCatalog = getTagCatalogInternal;
-
-export const isCallGroupingConfigurationValid = params => isGroupingConfigurationValidInternal(...params);
--- packages/in-applications/analyze/metrics.ts
+++ packages/in-applications/analyze/metrics.ts
@@ -4,12 +4,11 @@
  */
 
 // @ts-expect-error needs migration
-import CallGroupingConfigurator from 'in-applications/analyze/components/workspace/CallGroupingConfigurator';
-// @ts-expect-error needs migration
 import { newTimeMetric, wrapToDiscardNegativeValues } from 'in-analyze/metricDefinitionHelpers';
 // @ts-expect-error needs migration
 import TraceQueryBuilder from 'in-applications/analyze/components/workspace/TraceQueryBuilder';
 import TraceGroupingConfigurator from 'in-applications/analyze/components/workspace/TraceGroupingConfigurator';
+import CallGroupingConfigurator from 'in-applications/analyze/components/workspace/CallGroupingConfigurator';
 import CallQueryBuilder from 'in-applications/analyze/components/workspace/CallQueryBuilder';
 import { callClickedTracker, traceClickedTracker } from 'in-analyze/tracker';
 import { millis, number, percentage } from 'in-services/formatters/number';
