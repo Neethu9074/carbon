1f14272eb7cb4d5f9a7c81a24fe6aac5a823f393
BLESSY MATHEW <74411288+BlesyMathew@users.noreply.github.com>
2024-05-27 11:12:13 +0530

    feat(slo-alerting):added smart alerts in config psge

--- packages/in-service-levels/components/SloDashboard/components/SloConfigurationDetails.tsx
+++ packages/in-service-levels/components/SloDashboard/components/SloConfigurationDetails.tsx
@@ -7,15 +7,21 @@
 import React, { useEffect } from 'react';
 
 import { Card, Ul } from '@instana/components';
+import { t } from '@instana/i18n-react';
 
 import ScopeSection from 'in-service-levels/components/SloDashboard/components/configuration/ScopeSection/ScopeSection';
 import ObjectiveSection from 'in-service-levels/components/SloDashboard/components/configuration/ObjectiveSection';
 import IndicatorSection from 'in-service-levels/components/SloDashboard/components/configuration/IndicatorSection';
 import SloActionButtons from 'in-service-levels/components/SloDashboard/components/configuration/SloActionButtons';
 import EntitySection from 'in-service-levels/components/SloDashboard/components/configuration/EntitySection';
+import FloatingActionButtons from 'in-components/FloatingActionButton/FloatingActionButtons';
+import FloatingActionButton from 'in-components/FloatingActionButton/FloatingActionButton';
+import CreateSmartAlertDialog from 'in-alerting/smart-alerts/slo/CreateSmartAlertDialog';
 import { useSloTrackers } from 'in-service-levels/hooks/SloTrackerProvider';
 import { SloTabData } from 'in-service-levels/components/SloDashboard/tabs';
+import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { SLO_CONFIG_VIEW } from 'in-services/tracking/eventNames';
+import { sloSmartAlertsEnabled } from 'in-services/featureFlags';
 import TagList from 'in-components/TagsList/TagList';
 import { Nullish } from 'in-types';
 
@@ -28,11 +34,26 @@ export interface SloConfigurationDetailsContentProps {
 }
 
 export default function SloConfigurationDetails({ data }: SloConfigurationDetailsProps) {
+  const openCreateSmartAlertDialog = () =>
+    addActiveDialog(<CreateSmartAlertDialog preselectedSloId={data?.configuration.id} />);
+
   if (!data) {
     return null;
   }
 
-  return <SloConfigurationDetailsContent data={data} />;
+  return (
+    <>
+      {sloSmartAlertsEnabled && (
+        <FloatingActionButtons>
+          <FloatingActionButton icon="lib_alerts_create" kind="primaryv2" onClick={openCreateSmartAlertDialog}>
+            {t('in-service-levels:general.addButtonLabel', { context: 'smartAlert' })}
+          </FloatingActionButton>
+        </FloatingActionButtons>
+      )}
+
+      <SloConfigurationDetailsContent data={data} />
+    </>
+  );
 }
 
 function SloConfigurationDetailsContent({ data }: SloConfigurationDetailsContentProps) {
--- packages/in-service-levels/components/SloDashboard/components/SloSmartAlerts.tsx
+++ packages/in-service-levels/components/SloDashboard/components/SloSmartAlerts.tsx
@@ -6,10 +6,16 @@
 
 import React from 'react';
 
+import { t } from '@instana/i18n-react';
+
 import SloSmartAlertDetails from 'in-service-levels/components/SloDashboard/components/SloSmartAlertDetails';
 import { serviceLevelsObjectiveAlertDetailsFullyQualified } from 'in-service-levels/navigation/path';
+import FloatingActionButtons from 'in-components/FloatingActionButton/FloatingActionButtons';
+import FloatingActionButton from 'in-components/FloatingActionButton/FloatingActionButton';
+import CreateSmartAlertDialog from 'in-alerting/smart-alerts/slo/CreateSmartAlertDialog';
 import { SloTabData } from 'in-service-levels/components/SloDashboard/tabs';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
+import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { sloSmartAlertsEnabled } from 'in-services/featureFlags';
 import Alerts from 'in-alerting/smart-alerts/slo/Alerts';
 import { Nullish } from 'in-types';
@@ -22,10 +28,23 @@ interface SloAlertsWrapperProps {
 }
 
 export default function SloSmartAlerts({ data }: SloAlertsWrapperProps) {
+  const openCreateSmartAlertDialog = () =>
+    addActiveDialog(<CreateSmartAlertDialog preselectedSloId={data?.configuration.id} />);
   if (!data) {
     return null;
   }
-  return <SloSmartAlertsContent data={data} />;
+  return (
+    <>
+      {sloSmartAlertsEnabled && (
+        <FloatingActionButtons>
+          <FloatingActionButton icon="lib_alerts_create" kind="primaryv2" onClick={openCreateSmartAlertDialog}>
+            {t('in-service-levels:general.addButtonLabel', { context: 'smartAlert' })}
+          </FloatingActionButton>
+        </FloatingActionButtons>
+      )}
+      <SloSmartAlertsContent data={data} />
+    </>
+  );
 }
 
 function SloSmartAlertsContent({ data }: Required<SloAlertsProps>) {
