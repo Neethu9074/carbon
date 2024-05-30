8c8d6dbcd1c582d78532290da11b77d74eb1733c
Emil Pradeep <Emil.Pradeep@ibm.com>
2024-05-30 14:41:44 +0530

    removing Headertile calls to unblock release 275 (#16160)

--- packages/in-plg/components/WelcomeHeader/WelcomeHeader.tsx
+++ packages/in-plg/components/WelcomeHeader/WelcomeHeader.tsx
@@ -6,59 +6,59 @@
 
 import React from 'react';
 
-import { DashboardButton, HeaderItemTile, HeaderTile, Stack } from '@instana/components';
+import { DashboardButton, Stack } from '@instana/components';
 import { t } from '@instana/i18n-react';
 
+// import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
+// import { openAssistMe } from 'in-plg/components/AssistMe/AssistMe';
+import DatePicker from 'in-plg/components/DatePicker/DatePicker';
 // @ts-expect-error missing a type definition for it
 import UrlShortenerOverlay from 'in-components/DashboardHeader/UrlShortener/UrlShortenerOverlay';
 import { track, URL_SHORTENER_OPEN } from 'in-services/tracking/tracking';
-import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
-import { openAssistMe } from 'in-plg/components/AssistMe/AssistMe';
-import DatePicker from 'in-plg/components/DatePicker/DatePicker';
 import { shareAndInviteEnabled } from 'in-services/featureFlags';
 import Overlay from 'in-components/overlays/Overlay';
-import { user } from 'in-stores/user';
+
+// import { user } from 'in-stores/user';
 
 export default function WelcomeHeader() {
-  // @ts-expect-error The User type needs to be updated.
-  const headerTitle = `${t('in-plg:welcomepage.heading')}, ${user?.fullName ?? ''}!`;
-  const { createHrefToPath } = useNavigation();
+  // const headerTitle = `${t('in-plg:welcomepage.heading')}, ${user?.fullName ?? ''}!`;
+  // const { createHrefToPath } = useNavigation();
 
-  const tileData = [
-    {
-      key: 'consumeData',
-      title: t('in-plg:welcomepage.consumeData.title'),
-      description: t('in-plg:welcomepage.consumeData.description'),
-      buttonName: t('in-plg:welcomepage.consumeData.buttonName'),
-      buttonType: t('in-plg:welcomepage.consumeData.buttonType')
-    },
-    {
-      key: 'inviteTeammates',
-      title: t('in-plg:welcomepage.inviteTeammates.title'),
-      description: t('in-plg:welcomepage.inviteTeammates.description'),
-      buttonName: t('in-plg:welcomepage.inviteTeammates.buttonName'),
-      buttonType: t('in-plg:welcomepage.inviteTeammates.buttonType')
-    },
-    {
-      key: 'nextSteps',
-      title: t('in-plg:welcomepage.nextSteps.title'),
-      description: t('in-plg:welcomepage.nextSteps.description'),
-      buttonName: t('in-plg:welcomepage.nextSteps.buttonName'),
-      buttonType: t('in-plg:welcomepage.nextSteps.buttonType')
-    }
-  ];
+  // const tileData = [
+  //   {
+  //     key: 'consumeData',
+  //     title: t('in-plg:welcomepage.consumeData.title'),
+  //     description: t('in-plg:welcomepage.consumeData.description'),
+  //     buttonName: t('in-plg:welcomepage.consumeData.buttonName'),
+  //     buttonType: t('in-plg:welcomepage.consumeData.buttonType')
+  //   },
+  //   {
+  //     key: 'inviteTeammates',
+  //     title: t('in-plg:welcomepage.inviteTeammates.title'),
+  //     description: t('in-plg:welcomepage.inviteTeammates.description'),
+  //     buttonName: t('in-plg:welcomepage.inviteTeammates.buttonName'),
+  //     buttonType: t('in-plg:welcomepage.inviteTeammates.buttonType')
+  //   },
+  //   {
+  //     key: 'nextSteps',
+  //     title: t('in-plg:welcomepage.nextSteps.title'),
+  //     description: t('in-plg:welcomepage.nextSteps.description'),
+  //     buttonName: t('in-plg:welcomepage.nextSteps.buttonName'),
+  //     buttonType: t('in-plg:welcomepage.nextSteps.buttonType')
+  //   }
+  // ];
 
-  function createRedirectHref(currentTile: string) {
-    if (currentTile == 'consumeData') {
-      return createHrefToPath('/agents/installation');
-    } else {
-      return createHrefToPath('/config/team/accessControl/users');
-    }
-  }
+  // function createRedirectHref(currentTile: string) {
+  //   if (currentTile == 'consumeData') {
+  //     return createHrefToPath('/agents/installation');
+  //   } else {
+  //     return createHrefToPath('/config/team/accessControl/users');
+  //   }
+  // }
 
   return (
     <div data-search-context={t('in-plg:assistme.dataSearchContext.gettingStarted')}>
-      <HeaderTile headerTitle={headerTitle} datepicker={<DatePickerHeader />}>
+      {/* <HeaderTile headerTitle={headerTitle} datepicker={<DatePickerHeader />}>
         {tileData.map(({ title, description, buttonName, buttonType, key }) => (
           <HeaderItemTile
             key={key}
@@ -75,7 +75,7 @@ export default function WelcomeHeader() {
             }}
           />
         ))}
-      </HeaderTile>
+      </HeaderTile> */}
     </div>
   );
 }
