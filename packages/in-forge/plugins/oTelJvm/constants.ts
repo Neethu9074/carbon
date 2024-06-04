d10fef7e5beec12f1fbaf67ec0df4533b6a8d7d6
RUI LIU <liurui@cn.ibm.com>
2024-06-04 19:15:51 +0800

    Support longer distance between datapoints for OTel plugins (#16184)
    * Support longer distance between datapoints for OTel
    
    * Specify distanceBetweenDatapointsInMillis as the expected time between data points
    
    Allow up to 4 missing data points to be on par with current strategy
    
    * use millis from distance or rollup whichever is greater
    
    * recalculate max millis between data points on every config
    
    * Fix defect when this.distanceBetweenDatapointsInMillis is undefined
    
    ---------
    
    Co-authored-by: Jean Detoeuf <Jean.Detoeuf1@ibm.com>

--- packages/in-forge/plugins/oTelJvm/constants.ts
+++ packages/in-forge/plugins/oTelJvm/constants.ts
@@ -5,4 +5,5 @@
 
 // Metrics collection intervals for database are usually not more than 2 minutes (120 seconds).
 // 130000 ms is big enough to search the latest metric with a 10 seconds buffer.
-export const WINDOW_FOR_LATEST_METRIC = 180000;
+export const WINDOW_FOR_LATEST_METRIC = 180_000;
+export const DISTANCE_BETWEEN_DATAPOINTS = 60_000;
