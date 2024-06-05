9ddee3fb13c27eddfb8af3e21f2a2cbc96dd573f
Luka Martinović <Luka.Martinovic@ibm.com>
2024-06-05 10:52:59 +0200

    Refactor agent dashboard links  (#16208)
    * refactor: agent dashboard links
    
    * refactor: remove unused code

--- packages/in-stores/navigation/paths/dashboardPaths.ts
+++ packages/in-stores/navigation/paths/dashboardPaths.ts
@@ -3,24 +3,15 @@
  * (c) Copyright Instana Inc.
  */
 
-import { navigationParameters$, mutateUrl, getModifiedUrlStream } from 'in-stores/navigation/navigation';
+import { useCallback } from 'react';
+
+import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import { urlQueryKeys, setTimeConfig } from 'in-stores/time/config';
 import { emptyObject } from 'in-services/fixedObjects';
 import { Location } from 'in-stores/navigation/types';
 import { TimeConfig } from 'in-types';
-import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
-import { useCallback } from 'react';
 
 export const classicDashboard = '/dashboard';
-
-export function goToDashboard(snapshotId: string) {
-  mutateUrl(params => {
-    const view = getActiveView(params);
-    params.pathname = `/${view}${classicDashboard}`;
-    params.query.snapshotId = snapshotId;
-    return params;
-  });
-}
 
 interface GetDashboardLinkConfig {
   windowSize?: number;
@@ -35,63 +26,8 @@ interface GetLinkToSnapshotInCurrentViewConfig {
   timeConfig?: TimeConfig;
 }
 
-export function getDashboardLink(
-  snapshotId: string,
-  { windowSize, to, focusedMoment, pathname, autoRefresh, timeConfig }: GetDashboardLinkConfig = {}
-) {
-  return getModifiedUrlStream(params => {
-    if (pathname) {
-      params.pathname = pathname;
-    } else {
-      const view = getActiveView(params);
-      params.pathname = `/${view}${classicDashboard}`;
-    }
-    if (windowSize != null) {
-      params.query[urlQueryKeys.windowSize] = String(windowSize);
-    }
-    if (to !== undefined) {
-      params.query[urlQueryKeys.to] = to == null ? '' : String(to);
-    }
-    if (focusedMoment !== undefined) {
-      params.query[urlQueryKeys.focusedMoment] = focusedMoment == null ? '' : String(focusedMoment);
-    }
-    if (autoRefresh !== undefined) {
-      params.query[urlQueryKeys.autoRefresh] = String(Boolean(autoRefresh));
-    }
-    if (timeConfig) {
-      setTimeConfig(params, timeConfig);
-    }
-    params.query.snapshotId = snapshotId;
-  });
-}
-
-export const isDashboardOpen$ = navigationParameters$
-  .map(params => {
-    return /\/[a-z]+\/dashboard/i.test(params.pathname);
-  })
-  .distinct();
-
-export function getCloseDashboardLink() {
-  return getModifiedUrlStream(params => {
-    params.pathname = params.pathname.replace(/\/dashboard/i, '');
-  });
-}
-
 function getActiveView(params: Location) {
   return params.pathname.replace(/\/dashboard($|\/.*)/, '').replace(/^\//, '');
-}
-
-export function getLinkToSnapshotInCurrentView(
-  snapshotId: string,
-  { timeConfig }: GetLinkToSnapshotInCurrentViewConfig = emptyObject
-) {
-  return getModifiedUrlStream(params => {
-    params.query.snapshotId = snapshotId;
-
-    if (timeConfig) {
-      setTimeConfig(params, timeConfig);
-    }
-  });
 }
 
 export const useGetDashboardLink = () => {
