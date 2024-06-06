165774ef6cb00f32f3a07d214ac2e3734ddd687c
HUAI NAN ZHOU <zhouhn@cn.ibm.com>
2024-06-06 08:26:57 -0400

    Group mobile app crashes by key information (#16223)
    * group mobile app crashes by key information
    
    * update label

--- packages/in-mobile-apps/tags.js
+++ packages/in-mobile-apps/tags.js
@@ -77,7 +77,7 @@ export const defaultGroupings = {
     groupbyTag: 'mobileBeacon.customEvent.name'
   },
   crash: {
-    groupbyTag: 'mobileBeacon.error.message'
+    groupbyTag: 'mobileBeacon.crash.keyInformation'
   }
 };
 
@@ -126,7 +126,8 @@ export const availableGroupingTags = {
     ...commonGroupingTags,
     'mobileBeacon.error.message',
     'mobileBeacon.error.type',
-    'mobileBeacon.stackTrace'
+    'mobileBeacon.stackTrace',
+    'mobileBeacon.crash.keyInformation'
   ].sort()
 };
 
