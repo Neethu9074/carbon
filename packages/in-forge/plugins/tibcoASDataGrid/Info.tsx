cac0dbf820add71a416440aad9f43ac77107ba52
Ajith Aravindan <Ajith.Aravindan@ibm.com>
2024-06-10 14:44:15 +0530

    Tibco ActiveSpaces: Adding data grid dashboard (#16110)

---
+++ packages/in-forge/plugins/tibcoASDataGrid/Info.tsx
@@ -0,0 +1,20 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import React from 'react';
+
+import { DescriptionList, DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList/DescriptionList';
+import { SnapshotData } from 'in-stores/snapshot/snapshot';
+import { t } from 'in-i18n';
+
+export default function Info({ snapshot }: { snapshot: SnapshotData }) {
+  const data = snapshot.get('data');
+  return (
+    <DescriptionList>
+      <DescriptionItem title={t('in-forge:plugins.tibcoASDataGrid.gridName')}>{data.get('gridName')}</DescriptionItem>
+    </DescriptionList>
+  );
+}
