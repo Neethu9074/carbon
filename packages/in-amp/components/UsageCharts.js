769a3a16e5d7a4c16e1042bab6fe4b5d95997252
Sebastian Wegmann <wegmase@de.ibm.com>
2024-06-10 17:32:20 +0200

    Move chart and stack lines

--- packages/in-amp/components/UsageCharts.js
+++ packages/in-amp/components/UsageCharts.js
@@ -10,7 +10,7 @@ import { Card } from '@instana/components';
 
 import SubViewHeader from 'in-settings/components/SubViewHeader';
 import SectionLine from 'in-settings/components/SectionLine';
-import Renderer from 'in-components/Chart/renderer/Renderer';
+import { stackedArea } from 'in-stores/metric/renderer';
 import UsageChart from 'in-amp/components/UsageChart';
 import { Row, Col } from 'in-components/layout/Grid';
 import { carbonAlert } from 'in-themes/chartColors';
@@ -70,6 +70,41 @@ export default function UsageCharts({
           </Card>
         </Col>
       </Row>
+      <Row>
+        <Col xs={6}>
+          <Card>
+            <SubViewHeader>
+              Data usage
+              <Tooltip content={t('in-amp:components.usageCharts.dataUsageHelperText')} align="rightMiddle">
+                <SvgIcon type="lib_help_error_info_outline" size="s" color="#172429" />
+              </Tooltip>
+            </SubViewHeader>
+            <UsageChart
+              windowSize={windowSize}
+              showAggregatedMetrics={showAggregatedMetrics}
+              y1={{
+                ...tenantUnit,
+                metrics: ['licensed_data', 'data_ingested_total'],
+                labels: ['Entitled', 'total'],
+                colors: [carbonAlert.red60, carbonAlert.blue70]
+              }}
+              y2={{
+                ...tenantUnit,
+                renderer: stackedArea.id,
+                metrics: [
+                  'bytes_ingested_infrastructure',
+                  'bytes_ingested_traces',
+                  'bytes_ingested_synthetics',
+                  'bytes_ingested_eum_mobile',
+                  'bytes_ingested_eum_website'
+                ],
+                labels: ['infrastructure', 'traces', 'synthetics', 'eum mobile', 'eum website'],
+                colors: ['#17A1E6', '', '', '']
+              }}
+            />
+          </Card>
+        </Col>
+      </Row>
       <br />
       {showAggregatedMetrics && hasSyntheticAddons && (
         <>
@@ -98,43 +133,6 @@ export default function UsageCharts({
                     metrics: ['syntheticstotal'],
                     labels: [t('in-amp:components.usageCharts.consumedUnits')],
                     colors: ['#17A1E6']
-                  }}
-                />
-              </Card>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={12}>
-              <Card>
-                <SubViewHeader>
-                  Data Injested
-                  <Tooltip content={t('in-amp:components.usageCharts.helperText')} align="rightMiddle">
-                    <SvgIcon type="lib_help_error_info_outline" size="s" color="#172429" />
-                  </Tooltip>
-                </SubViewHeader>
-                <UsageChart
-                  windowSize={windowSize}
-                  showAggregatedMetrics={showAggregatedMetrics}
-                  y1={{
-                    ...tenantUnit,
-                    renderer: Renderer.stackedArea,
-                    metrics: ['licensed_data', 'data_ingested_total'],
-                    labels: ['Entitled', 'total'],
-                    colors: [carbonAlert.red60, carbonAlert.blue70]
-                  }}
-                  y2={{
-                    ...tenantUnit,
-                    renderer: Renderer.stackedArea,
-                    metrics: [
-                      'bytes_ingested_infrastructure',
-                      'bytes_ingested_traces',
-                      'bytes_ingested_synthetics',
-                      'bytes_ingested_eum_mobile',
-                      'bytes_ingested_eum_website'
-                    ],
-                    labels: ['infrastructure', 'traces', 'synthetics', 'eum mobile', 'eum website'],
-                    colors: ['#17A1E6', '', '', '']
                   }}
                 />
               </Card>
