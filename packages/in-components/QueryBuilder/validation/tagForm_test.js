e3834b5c8304b20ec5cb796b07ed261a4c8ea228
Will James <Will.James@ibm.com>
2024-06-19 12:39:43 -0500

    INSTA-8637 Ensure tagDefinition is not leaked into URL and API (#16431)
    # Why
    
    #16042 introduced server side tag catalog search. However, this resulted in leaking the tag definition into the URL and API queries even when the backend search was not being used (e.g. in the AP area).
    
    # What
    
    - Prevent leakage when the backend search is not being used (e.g. when feature flag is off or for other product areas)
    - Minimize the tag definition when the feature flag is on so that only the required information is added to the URL
    
    # References
    
    - [Story](https://jsw.ibm.com/browse/INSTA-8637)

--- packages/in-components/QueryBuilder/validation/tagForm_test.js
+++ packages/in-components/QueryBuilder/validation/tagForm_test.js
@@ -130,7 +130,7 @@ describe('in-components/QueryBuilder/validation/tagForm', () => {
     expect(changedForm.get('entity')).to.equal(undefined);
   });
 
-  it('must set tagDefinition to new tag definition when changing tag name', () => {
+  it('must set tagDefinition to new minimal tag definition when changing tag name', () => {
     const form = createTagForm(tagCatalog, {
       name: 'name',
       operator: CONTAINS,
@@ -139,9 +139,9 @@ describe('in-components/QueryBuilder/validation/tagForm', () => {
     });
     expect(form.hierarchyValid).to.equal(true);
 
-    const changedForm = changeName(tagCatalog, form, 'application');
+    const changedForm = changeName(tagCatalog, form, 'application', { name: 'application', type: STRING });
     expect(changedForm.hierarchyValid).to.equal(true);
     expect(changedForm.get('tagDefinition').value.name).to.equal('application');
     expect(changedForm.get('tagDefinition').value.type).to.equal(STRING);
-  })
+  });
 });
