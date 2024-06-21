bc33f2a5f69d2859f9423c67b8fc2de5c2c78948
Nickolas Tang <nickolas.tang@uk.ibm.com>
2024-06-21 12:28:11 +0100

    feat(description-list): carbon migration in-sdk

--- packages/in-sdk/components/sidebar/ProcessStartedAtDescriptionItem.js
+++ packages/in-sdk/components/sidebar/ProcessStartedAtDescriptionItem.js
@@ -5,27 +5,29 @@
 
 import React from 'react';
 
+import { DescriptionItem } from '@instana/components';
+
 import getSnapshotFromHierarchyByPlugin from 'in-sdk/components/hoc/getSnapshotFromHierarchyByPlugin';
 import { formatDateTime, fromNowAccurately } from 'in-services/formatters/date';
-import { DescriptionItem } from 'in-sdk/components/sidebar/DescriptionList';
 import { plugins } from 'in-forge/constants';
 import { t } from 'in-i18n';
 
-export default getSnapshotFromHierarchyByPlugin(plugins.process, function ProcessStartedAtDescriptionItem({
-  processSnapshot
-}) {
-  if (!processSnapshot) {
-    return null;
-  }
+export default getSnapshotFromHierarchyByPlugin(
+  plugins.process,
+  function ProcessStartedAtDescriptionItem({ processSnapshot }) {
+    if (!processSnapshot) {
+      return null;
+    }
 
-  const start = processSnapshot.getIn(['data', 'start']);
-  if (!start) {
-    return null;
-  }
+    const start = processSnapshot.getIn(['data', 'start']);
+    if (!start) {
+      return null;
+    }
 
-  return (
-    <DescriptionItem title={t('in-sdk:sidebar.processStartedTitle')}>
-      {formatDateTime(start)} ({fromNowAccurately(start)})
-    </DescriptionItem>
-  );
-});
+    return (
+      <DescriptionItem title={t('in-sdk:sidebar.processStartedTitle')}>
+        {formatDateTime(start)} ({fromNowAccurately(start)})
+      </DescriptionItem>
+    );
+  }
+);
