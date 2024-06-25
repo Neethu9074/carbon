df8a0099fc018df86e0aa863d53ef84ef37767dc
Aleix Costa <acostare@ibm.com>
2024-06-25 16:04:46 +0100

    fix: more style adjustments

--- packages/in-settings/tabs/TeamSettings/pages/logManagement/Splunk/Splunk.js
+++ packages/in-settings/tabs/TeamSettings/pages/logManagement/Splunk/Splunk.js
@@ -26,6 +26,8 @@ import Label from 'in-components/form/Label';
 import Title from 'in-components/Title';
 import { t } from 'in-i18n';
 
+import locals from './SplunkForm.mless';
+
 const block = 'in-ui-config';
 
 const logger = createLogger('splunkConfig');
@@ -97,7 +99,7 @@ export default class Splunk extends React.Component {
     const enabled = form ? form.get('enabled').value : null;
 
     return (
-      <SettingsDetailPage>
+      <SettingsDetailPage className={locals.page}>
         <Title title={t('in-settings:tabs.configureSplunk')} />
         <IntegrationsBreadcumb />
         <SubViewHeader>{t('in-settings:tabs.configureYourSplunkSettings')}</SubViewHeader>
