04a74c2fc9cb7447d2c118aa4e956d141bbd9bfb
Dan Stingaciu <dan.stingaciu@ibm.com>
2024-06-25 14:13:56 -0400

    Resolved merge conflicts

--- packages/in-services/featureFlags.ts
+++ packages/in-services/featureFlags.ts
@@ -39,6 +39,7 @@ export const infraMetricsWidgetEnabled = isFeatureFlagEnabled('infraMetricsWidge
 export const infraExploreFilterEmptyValueEnabled = isFeatureFlagEnabled('infraExploreFilterEmptyValueEnabled');
 export const persistentVolumeSupportEnabled = isFeatureFlagEnabled('persistentVolumeSupportEnabled');
 export const ampCompanyInfoEnabled = isFeatureFlagEnabled('ampCompanyInfoEnabled');
+export const segmentAnalyticsEnabled = isFeatureFlagEnabled('segmentAnalyticsEnabled', false);
 export const ampEnabled = isFeatureFlagEnabled('ampEnabled', true);
 export const pcfEnabled = isFeatureFlagEnabled('pcfEnabled');
 export const phmcEnabled = isFeatureFlagEnabled('phmcEnabled');
