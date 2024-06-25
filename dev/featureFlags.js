ee65fe28f7ae56d80efa6c7ddf72be6b04ceb0f5
Dan Stingaciu <dan.stingaciu@ibm.com>
2024-06-25 10:34:03 -0400

    resolved merge conflicts from merging orbital removal changes

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -126,7 +126,6 @@ module.exports = {
   automationActionInstanceFeedbackEnabled: false,
   infraExploreFilterEmptyValueEnabled: true,
   weaselSubresourceIntegrityEnabled: true,
-  orbitalEnabled: false,
   carbonTileEnabled: true,
   carbonStepProgressBarEnabled: true,
   thresholdCustomDashboardsEnabled: true,
--- packages/in-server/src/services/config.js
+++ packages/in-server/src/services/config.js
@@ -25,7 +25,6 @@ exports.getClientConfig = (req, tenant, unit) => {
     tenantUnit: unit,
     featureFlags: featureFlags,
     configuration: configuration,
-    orbitalSpaceID: serverConfig.clientConfig.orbitalSpaceID,
 
     agentEndpoint: reportingEndpoints.agentEndpoint,
     agentEndpointPort: reportingEndpoints.port,
