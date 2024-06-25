92a26b0abb82886e350027667e5a3ad113ffff34
Luka Martinovic <Luka.Martinovic@ibm.com>
2024-06-25 13:56:14 +0200

    feat: update docs link for logging

--- packages/in-analyze/components/AnalyzeHeader/constants.ts
+++ packages/in-analyze/components/AnalyzeHeader/constants.ts
@@ -55,6 +55,6 @@ export const dataSourceSources: DataSourceMatrix[] = [
 ];
 
 export const analyzeDocs = {
-  logs: 'https://www.ibm.com/docs/en/instana-observability/current?topic=apis-logging',
+  logs: 'https://ibm.biz/instana-logging',
   infrastructure: 'https://www.ibm.com/docs/en/instana-observability/current?topic=analytics-analyze-infrastructure'
 } as Partial<Record<Entity, string>>;
