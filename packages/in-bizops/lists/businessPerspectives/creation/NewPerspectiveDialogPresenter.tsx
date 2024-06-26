9c4b0eda62299f04bbf2ba9a37ab09c61f48f435
Mai Hameed <MaiHameed@ibm.com>
2024-06-26 12:24:34 -0400

    fix:(INSTA-7019): polish perspectives part 4 (#16553)
    * fix new perspective modal styling
    
    * text value updates
    
    * added error message on failed perspective creation
    
    * label -> name
    
    * added different states to the live process list based on query validation
    
    * added proper text for different live list states
    
    * fix i18n

--- packages/in-bizops/lists/businessPerspectives/creation/NewPerspectiveDialogPresenter.tsx
+++ packages/in-bizops/lists/businessPerspectives/creation/NewPerspectiveDialogPresenter.tsx
@@ -7,16 +7,6 @@
 import React, { useState } from 'react';
 import { MapForm } from 'formalistic';
 
-import {
-  Order,
-  Pagination,
-  TimeConfig,
-  BusinessDataQuery,
-  TagFilterExpressionElementUnion,
-  BusinessProcess,
-  PaginatedResult,
-  Result
-} from '@instana/types';
 import { useObservable } from '@instana/hooks';
 
 // @ts-expect-error Need to translate file to TS
@@ -25,15 +15,15 @@ import { NewPerspectiveFormStepOne } from 'in-bizops/lists/businessPerspectives/
 import { NewPerspectiveFormStepTwo } from 'in-bizops/lists/businessPerspectives/creation/NewPerspectiveFormStepTwo';
 import createNewPerspectiveForm from 'in-bizops/lists/businessPerspectives/creation/createNewPerspectiveForm';
 import { toBackendQueryModel } from 'in-components/QueryBuilder/transformation/backendQueryModel';
-import { FormModelElement } from 'in-components/QueryBuilder/transformation/formModel';
 import { businessPerspectiveDashboard, summaryTab } from 'in-bizops/navigation/paths';
-import getBusinessProcesses from 'in-bizops/subscriptions/getBusinessProcesses';
 import DialogWithSlideInView from 'in-components/Dialog/DialogWithSlideInView';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
+import { addMessage } from 'in-components/MessageFlyout/stores/messages';
 import { getBusinessMonitoringTagCatalog } from 'in-bizops/api/catalog';
 import { createBusinessPerspective } from 'in-bizops/api/perspectives';
 import { setOrDeleteMatrixKey } from 'in-stores/navigation/matrix';
 import { close } from 'in-components/DialogPresenter/store';
+import { TIMEOUT_IN_MS } from 'in-bizops/utils/constants';
 import { pendingResult } from 'in-services/fixedObjects';
 import { PerspectiveItem } from 'in-bizops/utils/types';
 import useTimeConfig from 'in-hooks/useTimeConfig';
@@ -58,12 +48,6 @@ export function NewPerspectiveDialogPresenter() {
 
   const blueprintCatalogResult = useObservable(getBusinessMonitoringTagCatalog(), []) ?? pendingResult;
 
-  const tagFilterExpression: FormModelElement[] = form.get('tagFilterExpression').value;
-  const processesLiveList: Result<PaginatedResult<BusinessProcess>> = useProcessesLiveList(
-    tagFilterExpression,
-    timeConfig
-  );
-
   function onCreate(form: MapForm<any>) {
     const requestBody: PerspectiveItem = {
       name: form.get('perspectiveName').value,
@@ -77,80 +61,78 @@ export function NewPerspectiveDialogPresenter() {
     Callback functions for the backend after the UI submits
     an API request to create a new perspective
   */
-  //TODO! Implement
   function onSuccess(item: PerspectiveItem) {
+    close();
     location.pathname = `${businessPerspectiveDashboard}${summaryTab}`;
     setOrDeleteMatrixKey(location, businessPerspectiveDashboard, 'perspectiveId', item.id);
+    setOrDeleteMatrixKey(location, businessPerspectiveDashboard, 'perspectiveName', item.name);
     navigate(location);
-    close();
+    addMessage({
+      type: 'info',
+      title: t('in-bizops:perspectives.successMessages.businessPerspectiveCreated'),
+      content: t('in-bizops:perspectives.successMessages.perspectiveWasSuccessfullyCreated', {
+        perspectiveName: item.name
+      }),
+      timeout: TIMEOUT_IN_MS
+    });
   }
 
-  function onError() {}
+  function onError() {
+    close();
+    addMessage({
+      type: 'danger',
+      title: t('in-bizops:perspectives.errorMessages.createOperationFailed'),
+      content: t('in-bizops:perspectives.errorMessages.unableToCreatePerspective'),
+      timeout: TIMEOUT_IN_MS
+    });
+  }
 
   return (
     <DialogWithSlideInView
       title={t('in-bizops:perspectives.dialog.title')}
-      className={locals.dialog}
       onClose={close}
       doNotCloseOnOutsideClick
+      withoutBodyPadding
     >
-      <SimpleModePageNavigation
-        onClose={close}
-        stepConfigs={stepConfigs}
-        form={form}
-        formId={formId}
-        updateForm={updateForm}
-        simpleModeStep={step}
-        setSimpleModeStep={setStep}
-        onCreate={() => {
-          onCreate(form);
-        }} //! TODO This is where the UI should send the request to create the perspective to the backend, and handle errors
-        onStepChanged={noop}
-        renderStep={(step: number) => {
-          switch (step) {
-            case 0:
-              return (
-                <NewPerspectiveFormStepOne
-                  form={form}
-                  updateForm={updateForm}
-                  blueprintCatalogResult={blueprintCatalogResult}
-                  processesLiveList={processesLiveList}
-                />
-              );
-            case 1:
-              return (
-                <NewPerspectiveFormStepTwo form={form} updateForm={updateForm} processesLiveList={processesLiveList} />
-              );
-            default:
-              return null;
-          }
-        }}
-      />
+      <div className={locals.dialog}>
+        <SimpleModePageNavigation
+          onClose={close}
+          stepConfigs={stepConfigs}
+          form={form}
+          formId={formId}
+          updateForm={updateForm}
+          simpleModeStep={step}
+          setSimpleModeStep={setStep}
+          onCreate={() => {
+            onCreate(form);
+          }}
+          onStepChanged={noop}
+          renderStep={(step: number) => {
+            switch (step) {
+              case 0:
+                return (
+                  <NewPerspectiveFormStepOne
+                    form={form}
+                    updateForm={updateForm}
+                    blueprintCatalogResult={blueprintCatalogResult}
+                    timeConfig={timeConfig}
+                  />
+                );
+              case 1:
+                return (
+                  <NewPerspectiveFormStepTwo
+                    form={form}
+                    updateForm={updateForm}
+                    blueprintCatalogResult={blueprintCatalogResult}
+                    timeConfig={timeConfig}
+                  />
+                );
+              default:
+                return null;
+            }
+          }}
+        />
+      </div>
     </DialogWithSlideInView>
   );
-}
-
-function useProcessesLiveList(
-  tagFilterExpressionFormModel: FormModelElement[],
-  timeConfig: TimeConfig
-): Result<PaginatedResult<BusinessProcess>> {
-  const pagination: Pagination = {
-    page: 1,
-    pageSize: 5
-  };
-  const order: Order = {
-    by: 'process_name',
-    direction: 'ASC'
-  };
-  const tagFilterExpression: TagFilterExpressionElementUnion = toBackendQueryModel(tagFilterExpressionFormModel);
-
-  const businessDataQuery: BusinessDataQuery = {
-    dataType: 'PROCESS',
-    metrics: {},
-    pagination: pagination,
-    tagFilterExpression: tagFilterExpression,
-    timeConfig: timeConfig,
-    order: order
-  };
-  return useObservable(getBusinessProcesses(businessDataQuery), [tagFilterExpressionFormModel]) ?? pendingResult;
 }
