0eeb81e9527170e5847624689f7b470f7eb283f9
Irfad K P <IRFAD.K.P@ibm.com>
2024-07-01 18:04:25 +0530

    Resolved merge conflicts from merging segment feature flag (#16639)

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -41,6 +41,7 @@ module.exports = {
   ampCompanyInfoEnabled: true,
   ampEnabled: true,
   persistentVolumeSupportEnabled: true,
+  segmentAnalyticsEnabled: false,
   loggingEnabled: true,
   logWidgetsEnabled: true,
   carbonShellEnabled: true,
