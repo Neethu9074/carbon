e38b792dcba6bf2fe7059e2fd01314ad60eb3548
Gouri-Hariharan1 <gouri.hariharan1@ibm.com>
2024-07-02 11:48:15 +0530

    ios stack trace in code component

--- packages/in-mobile-apps/analyze/SessionView/tabs/Summary/Beacon/components/StackTrace/RawStack.tsx
+++ packages/in-mobile-apps/analyze/SessionView/tabs/Summary/Beacon/components/StackTrace/RawStack.tsx
@@ -4,21 +4,21 @@
  * Copyright IBM Corp. 2022
  */
 
-import { startsWith } from 'lodash';
+//import { startsWith } from 'lodash';
 import React from 'react';
-
-import { SvgIcon } from '@instana/components';
 
 import {
   StackTraceThreadDesc,
   StackTraceThreadFrameDesc,
   StackTraceThreadFrameType
 } from 'in-mobile-apps/analyze/SessionView/tabs/Summary/Beacon/components/StackTrace/BeaconStackParser';
-import HorizontalFlexWrapper from 'in-components/layout/HorizontalFlexWrapper';
+//import HorizontalFlexWrapper from 'in-components/layout/HorizontalFlexWrapper';
 import { removeBlankLines } from 'in-services/util/string';
 import Code from 'in-components/Code';
 
 import locals from './RawStack.mless';
+
+//import { SvgIcon } from '@instana/components';
 
 export type RawStackFormat = 'raw' | 'stack-java' | 'stack-json';
 
@@ -42,29 +42,32 @@ export default function RawStack(props: RawStackProp) {
     const items = props.data.stack?.st ?? [];
     const analyzeFrameFunc = props.data.analyzeFrame;
     let userFrameFound = false;
-    return (
-      <div>
-        {items.map((line, i) => {
-          const frameType = analyzeFrameFunc?.(line);
-          let icon = <div className={locals.iconPlaceholder} />;
-          if (!userFrameFound && frameType == 'user') {
-            userFrameFound = true;
-            icon = <SvgIcon className={locals.icon} type="lib_arrow_short_right" size="xs" />;
-          }
+    let renderedContent = ``;
+    items.forEach(line => {
+      const frameType = analyzeFrameFunc?.(line);
+      let icon = '  ';
+      if (!userFrameFound && frameType == 'user') {
+        userFrameFound = true;
+        icon = `->`;
+      }
 
-          return (
-            <div key={i} className={locals.line}>
-              <HorizontalFlexWrapper className={locals.signature}>
-                {icon}
-                <MethodName frameType={frameType} methodName={line.t || line.f} />
-                <At />
-                <FileNameAndLine file={line.n} line={line.o} />
-              </HorizontalFlexWrapper>
-              <span className={locals.rightmost}>{line.a}</span>
-            </div>
-          );
-        })}
-      </div>
+      const fileName = line.n ?? '<unknown>';
+      const lineNum = line.o ? `:${line.o}` : '';
+      const linef = line.f ?? '<unknown>';
+      let lineContent = `${icon}${line.t || linef} at ${fileName}${lineNum}`;
+      const argument = line.a;
+      lineContent += `(${argument})`;
+      renderedContent += `${lineContent}\n`;
+    });
+
+    return (
+      <Code
+        wrapperClassName={locals.code}
+        showLineNumbers={false}
+        code={renderedContent}
+        // @ts-expect-error Code does support Java, but the types are incomplete
+        lang={props.data?.format === 'stack-java' ? 'java' : 'java'}
+      />
     );
   }
 
@@ -77,31 +80,4 @@ export default function RawStack(props: RawStackProp) {
       lang={props.data?.format === 'stack-java' ? 'java' : 'raw'}
     />
   );
-}
-
-function MethodName(props: { methodName?: string; frameType?: StackTraceThreadFrameType }) {
-  const name = props.methodName ?? '<unknown>';
-  let className: string;
-
-  if (props.frameType === 'user') {
-    className = locals.methodNameHighlight;
-  } else if (startsWith(name, '<')) {
-    className = locals.methodNameInvalid;
-  } else {
-    className = locals.methodName;
-  }
-  return <span className={className}>{name}</span>;
-}
-
-function FileNameAndLine(props: { file?: string; line?: string }) {
-  return (
-    <span className={locals.fileName}>
-      {props.file ?? '<unknown>'}
-      {props.line ? `:${props.line}` : ''}
-    </span>
-  );
-}
-
-function At() {
-  return <span className={locals.at}>at</span>;
 }
