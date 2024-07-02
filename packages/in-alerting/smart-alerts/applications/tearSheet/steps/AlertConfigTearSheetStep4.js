d6c96cae3cfac59b1986f7948902cb18e2659d6f
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2024-07-02 17:52:40 +0530

    Added separation between the sections in the Alerting tearsheet. (#16667)
    displayed a seperation between teh section in step 4

--- packages/in-alerting/smart-alerts/applications/tearSheet/steps/AlertConfigTearSheetStep4.js
+++ packages/in-alerting/smart-alerts/applications/tearSheet/steps/AlertConfigTearSheetStep4.js
@@ -178,6 +178,7 @@ export default function AlertConfigTearSheetStep4(props) {
             }
           />
         </TearSheetStepContentWrapper>
+        <span className={locals.seperator} />
         <TearSheetStepContentWrapper
           headline={t('in-alerting:smartAlerts.applications.tearSheet.timeThreshold.title')}
           description={t('in-alerting:smartAlerts.applications.tearSheet.timeThreshold.description')}
