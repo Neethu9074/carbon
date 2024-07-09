9563ac5eaf3ec130c124d8b55ce11e0e051be79e
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2024-07-09 13:00:04 +0530

    Added test cases for AlertConfigHelper and GroupByHelper (#16645)

---
+++ packages/in-alerting/smart-alerts/applications/tearSheet/components/AlertConfigHelper_test.js
@@ -0,0 +1,246 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import {
+  getHeaderTitle,
+  toAlertConfig,
+  fromAlertConfig,
+  generateAlertConfig,
+  getApplicationAlertConfig
+} from 'in-alerting/smart-alerts/applications/tearSheet/components/AlertConfigHelper';
+import { HISTORIC_BASELINE, STATIC_THRESHOLD } from 'in-alerting/smart-alerts/data/thresholdTypes';
+import { duplicateAlertConfig } from 'in-alerting/smart-alerts/components/dialog/sharedFunctions';
+import { createSmartAlertForm } from 'in-alerting/smart-alerts/applications/form/smartAlertForm';
+import { DAILY } from 'in-alerting/smart-alerts/data/seasonalities';
+import { t } from 'in-i18n';
+
+const alertConfigData = {
+  evaluationType: 'PER_AP',
+  granularity: 600000,
+  id: '',
+  includeInternal: false,
+  includeSynthetic: false,
+  name: 'Calls are slower than usual',
+  readOnly: false,
+  alertChannelIds: [],
+  applicationId: undefined,
+  applications: {},
+  boundaryScope: 'INBOUND',
+  builtIn: undefined,
+  created: 0,
+  customPayloadFields: [],
+  description: 'Calls are slower or equal to  ms based on latency (90th).',
+  enabled: true,
+  tagFilterExpression: {
+    elements: [],
+    logicalOperator: 'AND',
+    type: 'EXPRESSION'
+  },
+  timeThreshold: {
+    timeWindow: 600000,
+    type: 'violationsInSequence'
+  },
+  triggering: false,
+  rule: {
+    aggregation: 'P90',
+    alertType: 'slowness',
+    metricName: 'latency'
+  },
+  severity: 5,
+  threshold: {
+    lastUpdated: 0,
+    operator: '>=',
+    type: 'staticThreshold',
+    value: null
+  }
+};
+
+describe('in-alerting/smart-alerts/applications/tearSheet/components/AlertConfigHelper', () => {
+  describe('getDetailedMetricTooltipValueFormatter', () => {
+    it('returns "Edit alert"', () => {
+      const result = getHeaderTitle(true, true, false);
+      expect(result).toBe(
+        t('in-alerting:smartAlerts.components.smartAlertDialog.alertConfigDialogPresenterTitleEditAlert_Global')
+      );
+    });
+
+    it('returns "Create new alert"', () => {
+      const result = getHeaderTitle(false, false, false);
+      expect(result).toBe(
+        t('in-alerting:smartAlerts.components.smartAlertDialog.alertConfigDialogPresenterTitleCreateNewAlert_Local')
+      );
+    });
+
+    it('returns "Migrate to smart alerts"', () => {
+      const result = getHeaderTitle(true, false, true);
+      expect(result).toBe(t('in-alerting:smartAlerts.migration.migrateButton'));
+    });
+  });
+
+  describe('getDetailedMetricTooltipValueFormatter', () => {
+    const alertConfig = {
+      threshold: {
+        type: 'staticThreshold'
+      }
+    };
+    //@ts-expect-error
+    const form = createSmartAlertForm(alertConfig, false, false);
+
+    const alertConfigFormData = toAlertConfig(form);
+
+    expect(alertConfigFormData).toEqual(alertConfigData);
+  });
+
+  describe('fromAlertConfig', () => {
+    it('returns an object with the correct properties', () => {
+      const alertConfig = {
+        ...alertConfigData,
+        rule: {
+          aggregation: 'P90',
+          alertType: 'statusCode',
+          metricName: 'latency',
+          statusCodeEnd: 123,
+          statusCodeStart: 123
+        }
+      };
+
+      const expectedResult = {
+        ...alertConfig,
+        rule: {
+          statusCode: { statusCodeEnd: 123, statusCodeStart: 123 },
+          aggregation: 'P90',
+          alertType: 'statusCode',
+          metricName: 'latency'
+        }
+      };
+      expect(fromAlertConfig(alertConfig)).toEqual(expectedResult);
+    });
+  });
+
+  describe('generateAlertConfig', () => {
+    it('should return a static threshold config ', () => {
+      const config = generateAlertConfig(true);
+      expect(config).toEqual({
+        threshold: {
+          type: STATIC_THRESHOLD
+        }
+      });
+    });
+
+    it('should return a historic baseline threshold config', () => {
+      const config = generateAlertConfig(false, '123', 'scope', '0', '0', false);
+      expect(config).toEqual({
+        boundaryScope: 'scope',
+        threshold: { type: HISTORIC_BASELINE, value: 0, seasonality: DAILY },
+        calculateThresholdOnBackend: true,
+        includeSynthetic: false,
+        applications: {
+          123: {
+            applicationId: '123',
+            inclusive: false,
+            services: {
+              0: {
+                endpoints: {
+                  0: {
+                    endpointId: '0',
+                    inclusive: true
+                  }
+                },
+                inclusive: false,
+                serviceId: '0'
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+
+  describe('getApplicationAlertConfig', () => {
+    const applicationIds = {
+      applicationId: 'AP-123',
+      serviceId: 'SER-123',
+      endpointId: 'END-123'
+    };
+    const isGlobalSmartAlert = true;
+
+    const migrateAlertConfig = {};
+    const boundaryScope = 'ALL';
+
+    it('test edit mode', () => {
+      const applicationMode = {
+        editMode: true
+      };
+
+      const result = getApplicationAlertConfig(
+        isGlobalSmartAlert,
+        alertConfigData,
+        migrateAlertConfig,
+        boundaryScope,
+        applicationIds,
+        applicationMode
+      );
+
+      expect(result).toEqual(alertConfigData);
+    });
+
+    it('test migration mode', () => {
+      const applicationMode = {
+        migrationMode: true
+      };
+      const migrateAlertConfig = {};
+
+      const result = getApplicationAlertConfig(
+        isGlobalSmartAlert,
+        alertConfigData,
+        migrateAlertConfig,
+        boundaryScope,
+        applicationIds,
+        applicationMode
+      );
+
+      expect(result).toEqual(migrateAlertConfig);
+    });
+
+    it('test duplicate mode', () => {
+      const applicationMode = {
+        duplicateMode: true
+      };
+      const result = getApplicationAlertConfig(
+        isGlobalSmartAlert,
+        alertConfigData,
+        migrateAlertConfig,
+        boundaryScope,
+        applicationIds,
+        applicationMode
+      );
+
+      expect(result).toEqual(duplicateAlertConfig(alertConfigData));
+    });
+
+    it('test alertConfig', () => {
+      const applicationMode = {};
+      const result = getApplicationAlertConfig(
+        isGlobalSmartAlert,
+        alertConfigData,
+        migrateAlertConfig,
+        boundaryScope,
+        applicationIds,
+        applicationMode
+      );
+
+      expect(result).toEqual(
+        generateAlertConfig(
+          isGlobalSmartAlert,
+          applicationIds?.applicationId,
+          boundaryScope,
+          applicationIds?.serviceId,
+          applicationIds?.endpointId
+        )
+      );
+    });
+  });
+});
