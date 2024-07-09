6280bee8e8f17354da34cf2422732ff38a276476
swatipandey&2001 <swatpand@in.ibm.com>
2024-07-09 13:06:34 +0530

    add theme support

--- storybook/config/preview.js
+++ storybook/config/preview.js
@@ -5,30 +5,28 @@
 
 /* eslint-env node */
 /* import-sort-ignore */
-// import { BADGE } from '@geometricpanda/storybook-addon-badges';
 import { DocsContainer } from '@storybook/addon-docs';
 import { ThemeProvider } from '@instana/components';
 import React from 'react';
 
 // ################################################
 // Start: Initialize Instana specific globals
-// import './i18n';
-// import './globals';
-// import './globalTagDefinition';
+import './i18n';
+import './globals';
+import './globalTagDefinition';
 
 // End: Initialize Instana specific globals
 // ################################################
-// import OverlayPresenter from 'in-components/overlays/OverlayPresenter';
-// import TooltipPresenter from 'in-components/Tooltip/TooltipPresenter';
+import OverlayPresenter from 'in-components/overlays/OverlayPresenter';
+import TooltipPresenter from 'in-components/Tooltip/TooltipPresenter';
 
 import 'in-themes/foundation.less';
 import '@instana/legacy/esm/index.css';
 import '@instana/components/esm/index.css';
 
 import theme from './theme';
-import './reboot.css';
 import './i18n';
-// import locals from './config.mless';
+import locals from './config.mless';
 
 // globals
 window.__DEV__ = true;
@@ -36,14 +34,6 @@ window.__DEV__ = true;
 const SUPPORTED_THEMES = [
   { name: 'default', info: 'Instana' },
   { name: 'g10', info: 'Carbon' }
-];
-
-export const decorators = [
-  (Story, { globals }) => (
-    <ThemeProvider theme={globals.theme}>
-      <Story />
-    </ThemeProvider>
-  )
 ];
 
 export const globalTypes = {
@@ -59,82 +49,25 @@ export const globalTypes = {
   }
 };
 
-// export const decorators = [
-//   story => (
-//     <ThemeProvider theme="default">
-//       <TooltipPresenter />
-//       <OverlayPresenter />
-//       <div id="main" role="main" className={locals.root}>
-//         {story()}
-//       </div>
-//     </ThemeProvider>
-//   )
-// ];
-
-// export const parameters = {
-//   options: {
-//     theme: {
-//       brandTitle: 'Instana',
-//       ...themes.light
-//     }
-//   },
-//   actions: { argTypesRegex: '^on[A-Z].*' },
-
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/
-//     }
-//   },
-
-//   docs: {
-//     container: DocsContainer,
-//     page: DocsPage
-//   }
-// };
+export const decorators = [
+  (Story, { globals }) => (
+    <ThemeProvider theme={globals.theme}>
+      <TooltipPresenter />
+      <OverlayPresenter />
+      <div id="main" role="main" className={locals.root}>
+        <Story />
+      </div>
+    </ThemeProvider>
+  )
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  // Make docs panel the primary one
-  previewTabs: { 'storybook/docs/panel': { index: -1 } },
 
-  // badgesConfig: {
-  //   [BADGE.BETA]: {
-  //     styles: {
-  //       backgroundColor: '#D2EBFD',
-  //       borderColor: '#D2EBFD',
-  //       color: '#4397F7'
-  //     },
-  //     title: 'Beta'
-  //   },
-  //   [BADGE.DEPRECATED]: {
-  //     styles: {
-  //       backgroundColor: '#FDF1E7',
-  //       borderColor: '#FDF1E7',
-  //       color: '#EE7F31'
-  //     },
-  //     title: 'Deprecated'
-  //   }
-  // },
-
-  // More options: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#sorting-stories
-  options: {
-    storySort: {
-      order: [
-        'Getting Started',
-        ['Home'],
-        'Formatting',
-        'Global',
-        'Design Guidelines',
-        'Design Tokens',
-        ['General Usage', 'Colors IDS', 'Colors CDS'],
-        'Carbon',
-        'Components',
-        ['Overview'],
-        'Patterns',
-        'Hooks',
-        'Utils'
-      ]
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
     }
   },
 
@@ -152,8 +85,70 @@ export const parameters = {
         </DocsContainer>
       );
     }
-  },
-
-  // Set docs page as default
-  viewMode: 'docs'
+  }
 };
+
+// export const parameters = {
+//   actions: { argTypesRegex: '^on[A-Z].*' },
+//   // Make docs panel the primary one
+//   previewTabs: { 'storybook/docs/panel': { index: -1 } },
+
+//   // badgesConfig: {
+//   //   [BADGE.BETA]: {
+//   //     styles: {
+//   //       backgroundColor: '#D2EBFD',
+//   //       borderColor: '#D2EBFD',
+//   //       color: '#4397F7'
+//   //     },
+//   //     title: 'Beta'
+//   //   },
+//   //   [BADGE.DEPRECATED]: {
+//   //     styles: {
+//   //       backgroundColor: '#FDF1E7',
+//   //       borderColor: '#FDF1E7',
+//   //       color: '#EE7F31'
+//   //     },
+//   //     title: 'Deprecated'
+//   //   }
+//   // },
+
+//   // More options: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#sorting-stories
+//   options: {
+//     storySort: {
+//       order: [
+//         'Getting Started',
+//         ['Home'],
+//         'Formatting',
+//         'Global',
+//         'Design Guidelines',
+//         'Design Tokens',
+//         ['General Usage', 'Colors IDS', 'Colors CDS'],
+//         'Carbon',
+//         'Components',
+//         ['Overview'],
+//         'Patterns',
+//         'Hooks',
+//         'Utils'
+//       ]
+//     }
+//   },
+
+//   docs: {
+//     theme,
+//     toc: {
+//       title: 'Table of Contents',
+//       headingSelector: 'h1, h2, h3'
+//     },
+//     container: ({ children, ...rest }) => {
+//       const { context: globals } = rest;
+//       return (
+//         <DocsContainer {...rest}>
+//           <ThemeProvider theme={globals.theme}>{children}</ThemeProvider>
+//         </DocsContainer>
+//       );
+//     }
+//   },
+
+//   // Set docs page as default
+//   viewMode: 'docs'
+// };
