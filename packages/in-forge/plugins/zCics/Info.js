b3bf833ee0e4d40b96915d605e73071b1d635af9
Ashish Sharma <Ashish.Sharma6@ibm.com>
2024-07-09 14:51:48 +0530

    Omegamon - CICS Improvement - INSTA-7282 (#16703)
    * IBM Z Omegamon CICS Metrics Enhancements
    
    * Omegamon - CICS Improvement - INSTA-7282
    
    * Update headers
    
    * Revert custom styling
    
    * Restructure the charts
    
    * Remove TPM from en-US

--- packages/in-forge/plugins/zCics/Info.js
+++ packages/in-forge/plugins/zCics/Info.js
@@ -30,6 +30,12 @@ export default function Info({ snapshot }) {
       <DescriptionItem title={t('in-forge:plugins.zCics.cicsplexName')}>
         {data.get('CICSplex_Region_Overview.cicsplex_name')}
       </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.zCics.serviceClassName')}>
+        {data.get('CICSplex_Region_Overview.service_class_name')}
+      </DescriptionItem>
+      <DescriptionItem title={t('in-forge:plugins.zCics.summaryType')}>
+        {data.get('CICSplex_Region_Overview.summary_type')}
+      </DescriptionItem>
     </DescriptionList>
   );
 }
