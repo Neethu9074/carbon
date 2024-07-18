1a76216419e7602c237757f00fc253248ca390ae
Zhen Gao <gaozhenz@cn.ibm.com>
2024-07-18 14:43:24 +0800

    Update metrics definitions for otel-entities.

--- packages/in-forge/plugins/oTelLLM/metricDefinitions.ts
+++ packages/in-forge/plugins/oTelLLM/metricDefinitions.ts
@@ -4,4 +4,15 @@
  * Copyright IBM Corp. 2024
  */
 
-export default [];
+// @ts-ignore
+import { number } from 'in-services/formatters/number';
+import { t } from 'in-i18n';
+
+export default [
+  {
+    metrics: ['llm.status'],
+    labels: [t('in-forge:plugins.oTelLLM.llm_status')],
+    category: [t('in-forge:plugins.oTelJvm.label_category_LLM')],
+    formatter: number
+  }
+];
