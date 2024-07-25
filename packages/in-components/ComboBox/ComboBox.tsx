48c1b97aa489715c59cb24857c6b230843d19a8e
DIANA Costantino <dcostant@us.ibm.com>
2024-07-25 09:17:37 -0400

    feat(combobox): carbon version

--- packages/in-components/ComboBox/ComboBox.tsx
+++ packages/in-components/ComboBox/ComboBox.tsx
@@ -1,93 +1,26 @@
 /*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc.
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
  */
 
-import Select from 'react-select';
-import { isEqual } from 'lodash';
 import React from 'react';
 
-// Carbon version of ComboBox for single select
-import { ComboBox as CarbonComboBox, ComboBoxProps as CarbonComboBoxProps } from '@instana/components';
-
 import { carbonComboBoxEnabled } from 'in-services/featureFlags';
-import { Nullish } from 'in-types';
-import { t } from 'in-i18n';
+import { default as LegacyComboBox } from './LegacyComboBox';
+import { default as CarbonComboBox } from './CarbonComboBox';
+import { ComboBoxProps } from './types';
 
-import './DropDownDirection.less';
-import './ComboBox.less';
+export type {Option, Options, ComboBoxProps} from './types';
+import { hasMultipleValuesSelected } from './LegacyComboBox';
+export { hasMultipleValuesSelected };
 
-export interface Option {
-  label: string;
-  value: string;
-  isDisabled?: boolean;
-}
-
-export type Options = ReadonlyArray<Option>;
-
-export function hasMultipleValuesSelected(value: Option | Options | null): value is Options {
-  return Array.isArray(value);
-}
-
-interface ComboBoxProps {
-  id?: string;
-  name?: string;
-  isClearable?: boolean;
-  options: Options;
-  value: string | ReadonlyArray<string> | Nullish;
-  defaultValue?: any;
-  className?: string;
-  placeholder?: React.ReactNode;
-  onChange: (option: Option | Options | null) => void;
-  autoComplete?: string;
-  autoFocus?: boolean;
-  openMenuOnFocus?: boolean;
-  isMulti?: boolean;
-  disabled?: boolean;
-  isOptionDisabled?: (option: Option) => boolean;
-  isSearchable?: boolean;
-  isDisabled?: boolean;
-  components?: any;
-  carbonVariant?: boolean;
-}
 
 export default function ComboBox({ isClearable = true, ...props }: ComboBoxProps): JSX.Element {
-  const value =
-    props.options?.filter((option: Option) =>
-      Array.isArray(props.value) ? props.value.includes(option.value) : option.value === props.value
-    ) ?? null;
-
-  if (!props?.isMulti && (carbonComboBoxEnabled || props.carbonVariant)) {
-    const cprops: CarbonComboBoxProps = { ...props, value: props.value as Option | null | undefined };
-    return <CarbonComboBox {...cprops} />;
+  if (!props?.isMulti && carbonComboBoxEnabled) {
+    console.log()
+    return <CarbonComboBox {...props} isClearable={isClearable} />;
   }
-
-  return (
-    <Select
-      {...props}
-      isClearable={isClearable}
-      classNamePrefix="Select"
-      aria-label={props.name ?? 'label'}
-      className={`${props.className} Select`}
-      placeholder={props.placeholder ? props.placeholder : t('in-components:comboBox.placeholderSelect')}
-      onChange={(option: Option | Options | null) => {
-        // Do not propagate the event, unless the value really changed. This will prevent unnecessary reloads.
-        if (Array.isArray(option)) {
-          if (
-            !isEqual(
-              option.map(o => o?.value),
-              props.value
-            )
-          ) {
-            props.onChange(option);
-          }
-        } else {
-          if (!(option instanceof Array) && !isEqual(option?.value, props.value)) {
-            props.onChange(option);
-          }
-        }
-      }}
-      value={value}
-    />
-  );
+  return <LegacyComboBox {...props} isClearable={isClearable} />;
 }
+
