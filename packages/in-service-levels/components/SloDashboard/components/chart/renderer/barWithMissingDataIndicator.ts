0bf837508f962c858b6201b9412a7e18a4c3bd6b
BLESSY MATHEW <74411288+BlesyMathew@users.noreply.github.com>
2024-08-14 17:09:54 +0530

    chores(slo):adjusted-bar-chart-in-render

--- packages/in-service-levels/components/SloDashboard/components/chart/renderer/barWithMissingDataIndicator.ts
+++ packages/in-service-levels/components/SloDashboard/components/chart/renderer/barWithMissingDataIndicator.ts
@@ -9,52 +9,42 @@ import {
   renderMissingDataIndicator
 } from 'in-service-levels/components/SloDashboard/components/chart/renderer/missingDataIndicator';
 import { RenderWithMissingDataIndicatorProps } from 'in-service-levels/components/SloDashboard/components/chart/renderer/lineWithMissingDataIndicator';
-import { Renderer } from 'in-components/Chart/renderer/types';
+import { DataSeries, RenderAxis, RenderConfig, RenderProps, Renderer } from 'in-components/Chart/renderer/types';
+import Configuration from 'in-components/Chart/Configuration';
 import renderer from 'in-components/Chart/renderer/Renderer';
+import { ScaleType } from 'in-services/scale/scale';
+
+interface BarProps {
+  config: RenderConfig;
+  axis?: RenderAxis;
+  dataSeries: DataSeries;
+  scale: ScaleType;
+  colors?: string;
+}
+
+interface ExtendedRenderProps extends RenderProps {
+  metrics: DataSeries[];
+}
 
 function createBarWithMissingDataIndicatorRenderer({
   firstCollectedMetricTimestamp = 0
 }: RenderWithMissingDataIndicatorProps): Renderer {
   return {
     id: 'barWithMissingDataIndicator',
-    render: ({ config, axis }: any) => {
-      // const filtered = Array.from(config.filteredDataSeries).map(item => parseInt(item.split('-')[1]));
-      // function getLastNumber(str: string): number {
-      //   const parts = str.split('-'); // Split the string by '-'
-      //   return parseInt(parts[parts.length - 1], 10 || 0); // Get the last part and convert it to a number
-      // }
-      // console.log('config.filteredDataSerieslastNumbers', config.userFilteredDataSeries);
-      // Iterate over the Set and extract the last number from each string
-      // const lastNumbers: number[] = Array.from<string>(config.filteredDataSeries).map(getLastNumber);
+    render: data => {
+      const { config, axis, dataSeries, colors, scale }: BarProps = data;
 
-      // Log the result
-      // console.log('lastNumbers', lastNumbers); // Output: [1, 3, 10]
+      const filteredValue = (config as unknown as Configuration).filteredDataSeries.size;
 
-      // function removeIndices(metrics: any[], indices: any[]) {
-      //   // Sort the indices in descending order to handle shifting issues
-      //   // indices.sort((a: number, b: number) => b - a);
-      //   console.log('indices', indices);
-      //   // Remove each specified index
-      //   indices.forEach((index: number) => {
-      //     if (index >= 0 && index < metrics.length) {
-      //       metrics.splice(index, 1);
-      //     }
-      //   });
-      //   console.log('indicesssss', indices);
-      //   console.log('indicesssssME', metrics);
-
-      //   return metrics;
-      // }
-      // console.log('axis.metrics', axis.metrics);
-      // const l = removeIndices(axis.metrics, lastNumbers);
-      // console.log('LOTTT', l);
       renderer.bar.render({
-        metrics: axis.metrics,
-        colors100: config.y1.colors,
-        scale: config.scales.y1,
+        metrics: filteredValue === 1 ? [dataSeries] : axis!.metrics,
+        colors100: colors,
+        scale: scale,
         config,
-        axis: { dynamicCalculatedBlockSizeMillis: config.granularity, ...axis }
-      } as any);
+        axis: { dynamicCalculatedBlockSizeMillis: config.granularity, ...axis },
+        dataSeries,
+        color: colors!
+      } as ExtendedRenderProps);
 
       if (timeWindowIncludesFirstCollectionTimestamp(firstCollectedMetricTimestamp, config.timeConfig)) {
         renderMissingDataIndicator(config, firstCollectedMetricTimestamp);
