49bebae7f01dfca825766bd7f725c7c4f6a96c68
DIANA Costantino <dcostant@us.ibm.com>
2024-08-26 14:01:15 -0400

    fix(combobox): add custom filtering

--- packages/in-components/ComboBox/CarbonComboBox.tsx
+++ packages/in-components/ComboBox/CarbonComboBox.tsx
@@ -17,11 +17,20 @@ export default function ComboBox({ ...props }: ComboBoxProps): JSX.Element {
   const itemToElement = (item: any) => {
     return props.components.Option({ data: item, getValue: () => {} });
   };
+  const shouldFilterItem = (input: any) => {
+    const { inputValue, item, itemToString } = input;
+    const result =
+      inputValue && inputValue !== '' && item && props.options
+        ? itemToString(item).toLowerCase().includes(inputValue.toLowerCase())
+        : true;
+    return result;
+  };
   const cprops: CarbonComboBoxProps = {
     ...props,
     placeholder: props.placeholder ? props.placeholder : t('in-components:comboBox.placeholderSelect'),
     value: selopt as Option,
-    itemToElement: props.components?.Option ? itemToElement : undefined
+    itemToElement: props.components?.Option ? itemToElement : undefined,
+    shouldFilterItem: shouldFilterItem
   };
   return <CarbonComboBox {...cprops} />;
 }
