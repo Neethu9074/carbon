852836799cb76ae53c01f1a9d4ac4c1886e60c13
Padmini Krishnamurthy <Padmini.Krishnamurthy@ibm.com>
2024-09-04 15:40:09 +0530

    SAP ABAP 1.0.29: Enhance error logging and add system configuration and printer metrics (#17539)
    * SAP ABAP 1.0.29: Enhance error logging and add system configuration and printer metrics
    
    * Modify row names
    
    * Added event for spool metric and update error
    
    * Rearranged update error metrics order
    
    * introduce error traces in ap
    
    * Address review comments
    
    * Use sentence case
    
    ---------
    
    Co-authored-by: Harikrishnan Nair K G <harikrishnan@harikrihnansmbp.in.ibm.com>
    Co-authored-by: Vipin Menon <vipinmv1@in.ibm.com>

---
+++ packages/in-sap/Dashboards/SapAbapInstanceSensor/tabs/UserConfigurationChanges.tsx
@@ -0,0 +1,116 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2024
+ */
+
+import React from 'react';
+
+import { useObservable } from '@instana/hooks';
+
+// @ts-expect-error needs TS migration
+import { SnapshotData, getRawPayloadWithTimestamp } from 'in-stores/snapshot';
+import Table from 'in-sdk/components/dashboard/Table';
+import { shorten } from 'in-services/util/string';
+import { t } from 'in-i18n';
+
+interface userProfileRow {
+  key: string;
+  configDetail: Map<string, object>;
+}
+
+const cols = [
+  {
+    title: t('in-sap:dashboards.client'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('MANDT') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.clientName'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('MTEXT') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.clientRole'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('CCCATEGORY') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.objectChanges'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('CCCORACTIV') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.logicalSystem'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('LOGSYS') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.changeBy'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('CHANGEUSER') as any, 128);
+      }
+    }
+  },
+  {
+    title: t('in-sap:dashboards.changedOn'),
+    type: 'string',
+    typeArgs: {
+      getValue(row: userProfileRow) {
+        return shorten(row.configDetail.get('CHANGEDATE') as any, 128);
+      }
+    }
+  }
+];
+
+export default function UserConfigurationChanges({ snapshotId }: SnapshotData) {
+  const data = useObservable(() => getRawPayloadWithTimestamp(snapshotId, 'userChanges'), [snapshotId]);
+  if (!data) {
+    return null;
+  }
+
+  const configDetails = (data as SnapshotData).get('raw_payload');
+  if (configDetails.size === 0) {
+    return null;
+  }
+
+  const rows: userProfileRow[] = configDetails.toArray().map((configDetail: any, idx: any) => {
+    return {
+      key: String(idx),
+      configDetail
+    };
+  });
+
+  return (
+    <Table
+      withoutPadding
+      cardTitle={t('in-sap:dashboards.clientConfigurationChanges')}
+      cols={cols}
+      rows={rows}
+      initialSortColumn={6}
+      initialSortDirection="desc"
+    />
+  );
+}
