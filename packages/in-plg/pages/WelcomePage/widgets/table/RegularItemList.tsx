cb8a5cb077ebd604924b91febd64d4f80f4e3821
Emil Pradeep <Emil.Pradeep@ibm.com>
2024-09-04 12:08:28 +0530

    added loading state for all tables (#17561)
    * added loading state for all tables
    
    * corrected default loading items count

--- packages/in-plg/pages/WelcomePage/widgets/table/RegularItemList.tsx
+++ packages/in-plg/pages/WelcomePage/widgets/table/RegularItemList.tsx
@@ -10,6 +10,7 @@ import { DashboardTableRow as Row, DashboardTableCell as Cell } from '@instana/c
 import { TimeConfig } from '@instana/types';
 
 import { ColumnDefinitionItem } from 'in-plg/pages/WelcomePage/widgets/types/DashboardTypeDefiniton';
+import { DEFAULT_NUMBER_SKELETON_ROWS } from 'in-plg/pages/WelcomePage/widgets/utils/WidgetUtil';
 import LoadingTableList from 'in-plg/pages/WelcomePage/widgets/table/LoadingTableList';
 import { getItemId } from 'in-plg/pages/WelcomePage/widgets/utils/WidgetUtil';
 import ErrorList from 'in-components/lists/List/sharedComponents/ErrorList';
@@ -33,6 +34,7 @@ export default function RegularItemList({
   widgetName
 }: RegItemListProps) {
   if (!result || isLoading(result)) {
+    numSkeletonRows = favIds ? DEFAULT_NUMBER_SKELETON_ROWS - favIds.length : DEFAULT_NUMBER_SKELETON_ROWS;
     return <LoadingTableList numSkeletonRows={numSkeletonRows} numSkeletonColumns={columnDefinitions.length} />;
   }
   if (hasError(result)) {
