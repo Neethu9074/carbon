a1067d9f7265df0107a2ffd067dd2a769911b714
DIANA Costantino <dcostant@us.ibm.com>
2024-09-06 14:32:58 -0400

    feat(dateinput): feat flag

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -68,7 +68,7 @@ module.exports = {
   carbonRadioButtonEnabled: true,
   carbonCheckboxEnabled: true,
   carbonComboBoxEnabled: true,
-  carbonDateInputEnable: false,
+  carbonDateInputEnabled: true,
   pseudoLanguageEnabled: true,
   applicationHealthOverviewEnabled: true,
   agentInstallViewRestrictedToIBMSaas: false,
