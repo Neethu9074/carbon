e8b29acd0ae8eab315e13ae5c467a38398587663
Josh King <jdking@us.ibm.com>
2024-09-09 08:46:53 -0500

    feat(recalcbutton): tooltip displays even on disabled

--- packages/in-alerting/smart-alerts/components/dialog/advanced/RecalculateBaselineButton.tsx
+++ packages/in-alerting/smart-alerts/components/dialog/advanced/RecalculateBaselineButton.tsx
@@ -8,6 +8,7 @@ import React, { useRef } from 'react';
 
 import { IconButton } from '@instana/components';
 
+import Tooltip from 'in-components/Tooltip';
 import { t } from 'in-i18n';
 
 import locals from 'in-alerting/smart-alerts/components/dialog/advanced/RecalculateBaselineButton.mless';
@@ -24,29 +25,34 @@ export default function RecalculateBaselineButton({ updateForm, editMode, form }
     return null;
   }
 
+  const isDisabled = isRecalculated.current || !form?.get('threshold').get('baseline')?.touched;
+  const toolTipText =
+    (!isDisabled && t('in-alerting:smartAlerts.components.smartAlertDialog.buttonRecalculateBaseline')) ||
+    t('in-alerting:smartAlerts.components.smartAlertDialog.buttonRecalculateBaselineDisabled');
+
   return (
-    <div className={locals.buttonWrapper}>
-      <IconButton
-        type="lib_actions_refresh"
-        size={'compact'}
-        kind={'primary'}
-        align={'top-end'}
-        onClick={() => {
-          isRecalculated.current = true;
-          updateForm(
-            form
-              .updateIn(['hiddenFields', 'calculateThresholdOnBackend'], field =>
-                (field as Field<boolean>).setValue(true)
-              )
-              .updateIn(['threshold', 'baseline'], field => {
-                return (field as Field<any>).setValue(null).setTouched(false);
-              })
-          );
-        }}
-        disabled={isRecalculated.current || !form?.get('threshold').get('baseline')?.touched}
-        iconDescription={t('in-alerting:smartAlerts.components.smartAlertDialog.buttonRecalculateBaseline')}
-        isWrapperedByTooltip
-      />
-    </div>
+    <Tooltip align="topRight" content={toolTipText}>
+      <div className={locals.buttonWrapper}>
+        <IconButton
+          type="lib_actions_refresh"
+          size={'compact'}
+          kind={'primary'}
+          onClick={() => {
+            isRecalculated.current = true;
+            updateForm(
+              form
+                .updateIn(['hiddenFields', 'calculateThresholdOnBackend'], field =>
+                  (field as Field<boolean>).setValue(true)
+                )
+                .updateIn(['threshold', 'baseline'], field => {
+                  return (field as Field<any>).setValue(null).setTouched(false);
+                })
+            );
+          }}
+          disabled={isDisabled}
+          iconDescription={t('in-alerting:smartAlerts.components.smartAlertDialog.buttonRecalculateBaseline')}
+        />
+      </div>
+    </Tooltip>
   );
 }
