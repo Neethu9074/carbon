0661d8ae90c6872e1242a928e79d3b0de2f9b143
Pradosh E <Pradosh.E@ibm.com>
2024-09-12 22:31:44 +0530

    updating default FF for log SA (#17746)

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -663,7 +663,7 @@ module.exports = exports = [
   {
     uiClientKey: 'logSmartAlertsEnabled',
     instanaCtlKey: 'feature.log.smart.alerts.enabled',
-    defaultValue: false
+    defaultValue: true
   },
   {
     uiClientKey: 'applicationSmartAlertFullScreenDesignEnabled',
