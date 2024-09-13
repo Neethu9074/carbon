d852957ade6cf6780a31fde272702a67e132a8ca
Divya G <Divya.G6@ibm.com>
2024-09-13 09:28:45 +0530

    remove icon color from third party icons in agent list page (#17741)
    remove icon colour

--- packages/in-plg/pages/onboarding/icons/GoogleCloudIcon.tsx
+++ packages/in-plg/pages/onboarding/icons/GoogleCloudIcon.tsx
@@ -6,29 +6,8 @@
 
 import React from 'react';
 
+import { SvgIcon } from '@instana/components';
+
 export default function GoogleCloudIcon() {
-  return (
-    <svg
-      id="Layer_1"
-      data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
-      width="24px"
-      height="24px"
-      viewBox="0 0 24 24"
-    >
-      <path
-        d="M15,8H15l0,0,2.64-2.64v0a8.6,8.6,0,0,0-14,4.19l.15,0a6.18,6.18,0,0,1,3.38-1,7,7,0,0,1,1.21.11h0A4.77,4.77,0,0,1,15,8Z"
-        fill="#ea4335"
-      />
-      <path
-        d="M20.27,9.49a8.55,8.55,0,0,0-2.6-4.18L15,8a4.77,4.77,0,0,1,1.75,3.78v.48a2.39,2.39,0,0,1,0,4.78H12v3.79h0l0,0h4.78A6.21,6.21,0,0,0,20.27,9.49Z"
-        fill="#4285f4"
-      />
-      <path d="M7.18,20.82H12V17H7.18a2.33,2.33,0,0,1-1-.21L3.43,19.55v0a6.16,6.16,0,0,0,3.75,1.24Z" fill="#34a853" />
-      <path
-        d="M7.18,8.42A6.21,6.21,0,0,0,3.43,19.56L6.2,16.79a2.39,2.39,0,1,1,3.16-3.16l2.77-2.78a6.2,6.2,0,0,0-5-2.43Z"
-        fill="#fbbc05"
-      />
-    </svg>
-  );
+  return <SvgIcon type="lib_google_cloud" />;
 }
