1f72971e37743559dc65f4dfdc751fa22447fb7f
DIANA Costantino <dcostant@us.ibm.com>
2024-09-16 11:01:31 -0400

    feat(datepicker): turn on dev

--- dev/featureFlags.js
+++ dev/featureFlags.js
@@ -68,7 +68,7 @@ module.exports = {
   carbonRadioButtonEnabled: true,
   carbonCheckboxEnabled: true,
   carbonComboBoxEnabled: true,
-  carbonDateInputEnabled: false,
+  carbonDateInputEnabled: true,
   carbonExpandableGroupEnabled: true,
   pseudoLanguageEnabled: true,
   applicationHealthOverviewEnabled: true,
