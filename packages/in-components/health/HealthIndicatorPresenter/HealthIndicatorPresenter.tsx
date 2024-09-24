265eaa57a313c8dcbe9ca32ee4bd504ab49d09f7
Josh King <jdking@us.ibm.com>
2024-09-24 12:46:22 -0500

    fix(statusicon): update table icon sizing per carbon design

--- packages/in-components/health/HealthIndicatorPresenter/HealthIndicatorPresenter.tsx
+++ packages/in-components/health/HealthIndicatorPresenter/HealthIndicatorPresenter.tsx
@@ -22,6 +22,8 @@ interface Props {
   refSetter?: LegacyRef<HTMLAnchorElement>;
   onClick?: () => void;
   tooltipLabel?: string;
+  tableUsage?: boolean;
+  buttonSize?: 'regular' | 'xs' | 'l' | undefined;
 }
 
 export default function HealthIndicatorPresenter({
@@ -30,46 +32,74 @@ export default function HealthIndicatorPresenter({
   active,
   refSetter,
   onClick,
-  tooltipLabel: explanation
+  tooltipLabel: explanation,
+  // Status Icons in table are 16px sizing so this toggles the size
+  // 16px by default and 20px if false
+  tableUsage = true,
+  // Define the IconButton size you want to use
+  // 32px, 40px, 48px
+  buttonSize = 'regular'
 }: Props) {
   const tooltipLabel = explanation ?? getTooltipLabel(openIssues, maxSeverity);
 
+  // Green status!
   if (openIssues === 0) {
     return (
       <Tooltip content={tooltipLabel} delay={500}>
-        <IconButton type="lib_uncheck" color={themes.default.ids.color.option.green[500]} className={locals.okayIcon} />
+        <IconButton
+          type="lib_uncheck"
+          color={themes.default.ids.color.option.green[500]}
+          iconSize={buttonSize}
+          className={classNames({
+            [locals.okayIcon]: true,
+            [locals.carbonStatusTableSizing]: tableUsage
+          })}
+        />
       </Tooltip>
     );
   }
 
+  // Warnings or Error status
   const color = active ? '#031F29' : getDesignLibraryColorBySeverity(maxSeverity);
   const type = getDesignLibrarySeverityIcon(maxSeverity);
 
-  return (
-    <a
-      href=""
-      onClick={e => {
-        e.preventDefault();
-        e.stopPropagation();
-        if (onClick) {
-          onClick();
-        }
-      }}
-      className={locals.badge}
-      ref={refSetter}
-    >
-      <Tooltip content={tooltipLabel} delay={500}>
-        <IconButton
-          type={type}
-          color={color}
-          className={classNames({
-            [locals.icon]: true,
-            [locals.iconWarning]: !(maxSeverity > 5) && maxSeverity !== 0
-          })}
-        />
-      </Tooltip>
-    </a>
+  const statusContents = (
+    <Tooltip content={tooltipLabel} delay={500}>
+      <IconButton
+        type={type}
+        color={color}
+        iconSize={buttonSize}
+        className={classNames({
+          [locals.icon]: true,
+          [locals.carbonStatusTableSizing]: tableUsage,
+          [locals.iconWarning]: !(maxSeverity > 5) && maxSeverity !== 0
+        })}
+      />
+    </Tooltip>
   );
+
+  // If no refSetter is passed we dont we can simply render the status icon as is
+  // because there is no overlay to render.  Allows for more usability of this component
+  if (refSetter) {
+    return (
+      <a
+        href=""
+        onClick={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          if (onClick) {
+            onClick();
+          }
+        }}
+        className={locals.badge}
+        ref={refSetter}
+      >
+        {statusContents}
+      </a>
+    );
+  } else {
+    return statusContents;
+  }
 }
 
 function getTooltipLabel(openIssues: number, maxSeverity: number): string {
