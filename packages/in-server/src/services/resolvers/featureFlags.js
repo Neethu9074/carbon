f2f7cc6ede5c803075e2eb3f0877d8823b9cb9cf
Pradosh E <Pradosh.E@ibm.com>
2024-09-30 19:05:30 +0530

    Fixing the instanaCtlKey in FF (#17966)
    * Fixing the instanaCtlKey in FF
    
    * fixing typo
    
    * Updating instanaCtlKey for FF :  applicationSmartAlertFullScreenDesignEnabled

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -642,7 +642,7 @@ module.exports = exports = [
   },
   {
     uiClientKey: 'applicationSmartAlertFullScreenDesignEnabled',
-    instanaCtlKey: 'application.smart.alerts.full.screen.design.enabled',
+    instanaCtlKey: 'feature.application.smart.alert.full.screen.design.enabled',
     defaultValue: false
   },
   {
@@ -752,7 +752,7 @@ module.exports = exports = [
   },
   {
     uiClientKey: 'applicationSmartAlertDialogView',
-    instanaCtlKey: 'application.smart.alerts.dialog.view',
+    instanaCtlKey: 'feature.application.smart.alert.dialog.view',
     defaultValue: true
   },
   {
