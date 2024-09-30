8add267557ade60c7e4f48c878af2ddef7ef6466
Luka Martinovic <Luka.Martinovic@ibm.com>
2024-09-30 11:22:12 +0200

    feat: remove RUs from log volume report

--- packages/in-settings/tabs/TeamSettings/pages/logManagement/LogVolume/types.ts
+++ packages/in-settings/tabs/TeamSettings/pages/logManagement/LogVolume/types.ts
@@ -11,7 +11,7 @@ interface LogVolumeDataWithSums {
   year: number;
   totalVolume: VolumeUnits;
   retentionPeriods: {
-    [key: string]: { label: string; volumeGB: number; volumeRU: number }[];
+    [key: string]: { label: string; volumeGB: number }[];
   };
   partialSums: {
     days30: VolumeUnits;
@@ -33,7 +33,6 @@ interface LogVolumeDataWithoutSums {
 
 export interface VolumeUnits {
   gb: number;
-  ru: number;
 }
 
 export type LogVolumeData = LogVolumeDataWithSums | LogVolumeDataWithoutSums;
