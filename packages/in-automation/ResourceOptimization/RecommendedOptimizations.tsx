6d9319dde0a0e6f592987b118448a78a2c0ce600
bin zhong <bzhong@us.ibm.com>
2024-10-01 18:17:39 -0500

    feat(turbo): add charts visual and info panel to resource optimization tab in application details

--- packages/in-automation/ResourceOptimization/RecommendedOptimizations.tsx
+++ packages/in-automation/ResourceOptimization/RecommendedOptimizations.tsx
@@ -20,16 +20,26 @@ import useServerTableUrlState, {
 import ServerTablePresenter, { ServerTablePresenterProps } from 'in-components/tables/ServerTable/ServerTablePresenter';
 import useNavigateToActionDetails from 'in-automation/navigation/hooks/useNavigateToActionDetails';
 import { usePaginatedScoredActions } from 'in-automation/AutomationCard/useScoredActions';
+import { generateMetrics, fixedTimestamp } from 'in-test/util/generateMetrics';
+import { FormatterObject, MetricDataSeries } from 'in-components/Chart/types';
 import { ColumnDefinition } from 'in-components/tables/ServerTable/types';
+import InfoPanel from 'in-automation/components/InfoPanel/InfoPanel';
 import { TypeFilter } from 'in-automation/ActionTable/tableFilters';
 import { TriggerSpecification } from 'in-automation/Policies/types';
+import ResultAwareChart from 'in-components/Chart/ResultAwareChart';
 import { isExternal } from 'in-automation/ActionCatalog/shared';
+import { getChartGranularity } from 'in-stores/metric/metric';
+import Renderer from 'in-components/Chart/renderer/Renderer';
 import { Event, Result, VolatileId } from 'in-types';
+import { chartColors } from 'in-themes/chartColors';
 import { mapData } from 'in-services/util/result';
+import { success } from 'in-services/util/result';
 import { ScoredAction } from 'in-automation/api';
 import { recommendedList } from './testData';
 import { role } from 'in-stores/user';
 import { t } from 'in-i18n';
+
+import locals from './RecommendedOptimizations.mless';
 
 const pathSegment = '/RecommendedOptimizations';
 const matrixPrefix = '';
@@ -115,6 +125,27 @@ interface RecommendedOptimizationsProps {
   trigger: Result<TriggerSpecification>;
 }
 
+const oneSecond = 1000;
+const oneMinute = oneSecond * 60;
+const timeConfig = generateTimeframe(oneMinute);
+const granularity = getChartGranularity(timeConfig);
+
+function generateTimeframe(windowSize: number) {
+  return {
+    windowSize,
+    to: fixedTimestamp,
+    autoRefresh: false
+  };
+}
+
+function generateMultipleMetrics(numSeries: number, numMetrics: number, maxValue: number, windowSize: number) {
+  const series = [];
+  for (let i = 0; i < numSeries; i++) {
+    series[i] = generateMetrics(numMetrics, maxValue, windowSize) as MetricDataSeries;
+  }
+  return series;
+}
+
 export default function RecommendedOptimizations({
   volatileId,
   event,
@@ -131,6 +162,14 @@ export default function RecommendedOptimizations({
   const { page, pageSize, orderBy, orderDirection, query } = serverTableUrlState;
   const navigateToActionDetails = useNavigateToActionDetails();
   recommendedOptimizations = recommendedOptimizations || recommendedList;
+
+  const colorPalette = [
+    chartColors.fiveColorPalette[1],
+    chartColors.fiveColorPalette[2],
+    chartColors.fiveColorPalette[0],
+    chartColors.fiveColorPalette[3],
+    chartColors.fiveColorPalette[4]
+  ];
 
   const { filteredActions, types, setTypes } = useFilters({
     recommendedOptimizations,
@@ -152,35 +191,123 @@ export default function RecommendedOptimizations({
   };
 
   return (
-    <ServerTablePresenter<ScoredAction, RecommendedOptimizationsTableProps>
-      columnDefinitions={columnDefinitions}
-      volatileId={volatileId}
-      event={event}
-      trigger={trigger}
-      fixedLayout
-      leftHeader={
-        <Typography variant="heading-300">
-          {result?.progress?.loading
-            ? t('in-automation:recommendedOptimizations')
-            : t('in-automation:recommendedOptimizationsWithCount', { count: totalHits })}
-        </Typography>
-      }
-      onChange={setServerTableUrlState}
-      orderBy={orderBy}
-      orderDirection={orderDirection}
-      searchMaxWidth={180}
-      page={page}
-      pageSize={pageSize}
-      query={query}
-      result={result}
-      rightHeader={
-        <Stack direction="horizontal">
-          <TypeFilter type={types} setType={params => setTypes({ types: params.types })} showExternal />
-          <Spacer horizontal="small" />
-        </Stack>
-      }
-      searchPlaceholder={t('in-automation:searchOptimizations')}
-      onRowClick={!role?.canConfigureAutomationPolicies ? handleRowClick : undefined}
-    />
+    <div className={locals.contentContainer}>
+      <InfoPanel
+        content={{
+          //text not finalized, no i18n yet
+          title: 'Resource optimizations, powered by Turbonomic',
+          columns: [
+            {
+              title: 'Set up integration',
+              text: 'You can automate actions to comply with service level objectives and improve business efficiency.',
+              link: {
+                url: '#',
+                label: 'Set up'
+              }
+            },
+            {
+              title: 'Upgrade to Instana Premium',
+              text: 'You can automate actions to comply with service levels and improve business efficiency.',
+              link: {
+                url: '#',
+                label: 'Upgrade'
+              }
+            },
+            {
+              title: 'View documentation',
+              text: 'You can automate actions to comply with service levels and improve business efficiency.',
+              link: {
+                url: '#',
+                label: 'View docs'
+              }
+            }
+          ]
+        }}
+      />
+      <div className={locals.charts}>
+        <div className={locals.box1}>
+          <ResultAwareChart
+            result={success({})}
+            config={{
+              title: t('in-automation:actionCategory'),
+              timeConfig: generateTimeframe(oneMinute),
+              y1: {
+                renderer: Renderer.pie,
+                labels: ['Performance', 'Prevention', 'Efficiency', 'Savings', 'Compliance'],
+                metricIds: [],
+                metrics: generateMultipleMetrics(5, 30, 10, oneMinute),
+
+                colors: colorPalette,
+                formatter: ((x: any) => x) as unknown as FormatterObject
+              }
+            }}
+          />
+        </div>
+        <div style={{ width: '65%', height: '295', display: 'none' }}>
+          <ResultAwareChart
+            result={success({})}
+            config={{
+              extendBar: true,
+              granularity: granularity,
+              title: 'Action types',
+              timeConfig: timeConfig,
+              y1: {
+                renderer: Renderer.bar,
+                labels: ['Type'],
+                metricIds: ['Move', 'Buy', 'Save'],
+                metrics: [generateMetrics(12, 100, oneMinute) as MetricDataSeries],
+                colors: colorPalette
+              }
+            }}
+          />
+        </div>
+        <div className={locals.box2}>
+          <ResultAwareChart
+            result={success({})}
+            config={{
+              title: 'Resource utilization',
+              timeConfig: generateTimeframe(oneMinute),
+              y1: {
+                renderer: Renderer.stackedArea,
+                labels: ['CPU', 'Memory', 'Disk'],
+                metricIds: [],
+                metrics: generateMultipleMetrics(3, 30, 10, oneMinute),
+                colors: colorPalette
+              }
+            }}
+          />
+        </div>
+      </div>
+      <ServerTablePresenter<ScoredAction, RecommendedOptimizationsTableProps>
+        columnDefinitions={columnDefinitions}
+        volatileId={volatileId}
+        event={event}
+        trigger={trigger}
+        fixedLayout
+        leftHeader={
+          <Typography variant="heading-300">
+            {result?.progress?.loading
+              ? t('in-automation:recommendedOptimizations')
+              : t('in-automation:recommendedOptimizationsWithCount', { count: totalHits })}
+          </Typography>
+        }
+        onChange={setServerTableUrlState}
+        orderBy={orderBy}
+        orderDirection={orderDirection}
+        searchMaxWidth={180}
+        page={page}
+        pageSize={pageSize}
+        query={query}
+        result={result}
+        rightHeader={
+          <Stack direction="horizontal">
+            <TypeFilter type={types} setType={params => setTypes({ types: params.types })} showExternal />
+            <Spacer horizontal="small" />
+          </Stack>
+        }
+        searchPlaceholder={t('in-automation:searchOptimizations')}
+        onRowClick={!role?.canConfigureAutomationPolicies ? handleRowClick : undefined}
+      />
+    </div>
   );
 }
--- packages/in-automation/components/InfoPanel/InfoPanel.tsx
+++ packages/in-automation/components/InfoPanel/InfoPanel.tsx
@@ -6,12 +6,13 @@
 
 import React, { useState } from 'react';
 
-import { Stack, DashboardButton, SvgIconSizes } from '@instana/components';
+import { DashboardButton, SvgIconSizes, Link } from '@instana/components';
 import { t } from '@instana/i18n-react';
 
 import locals from './InfoPanel.mless';
 
 interface contentColumn {
+  title: string;
   text?: string;
   link?: {
     label: string;
@@ -25,6 +26,7 @@ interface infoPanelProps {
   collapsible?: boolean;
   showLabel?: string;
   hideLabel?: string;
+  i18nLib?: string;
   expanded?: string | boolean;
   content: {
     title?: string;
@@ -34,10 +36,11 @@ interface infoPanelProps {
 
 export default function InfoPanel({
   id = 'infoPanel',
-  ariaLabel = 'Task guidance',
+  i18nLib = 'in-automation:infoPanel.',
+  ariaLabel = 'taskGuidance',
   collapsible = true,
-  showLabel = 'Show tasks',
-  hideLabel = 'Hide tasks',
+  showLabel = 'showTasks',
+  hideLabel = 'hideTasks',
   expanded = true,
   content
 }: infoPanelProps) {
@@ -50,29 +53,37 @@ export default function InfoPanel({
   };
 
   return (
-    <section id={id} aria-label={ariaLabel}>
-      <h1>{content.title ?? null}</h1>
+    <section id={id} aria-label={t(i18nLib + ariaLabel)} className={locals.panel}>
+      <h1 className={locals.contentTitle}>{content.title ?? null}</h1>
       {isExpanded && (
-        <div className={locals.carouselStack}>
-          <p>column text</p>
+        <div className={locals.contentContainer}>
+          {content.columns.map(item => {
+            return (
+              <div className={locals.columnContainer}>
+                <div>
+                  <div className={locals.columnTitle}>{item?.title}</div>
+                  <div className={locals.columnText}>{item?.text}</div>
+                </div>
+                <Link className={locals.linkButton} href={item?.link?.url} linkIconType="lib_arrow_short_right">
+                  <span>{item?.link?.label}</span>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       )}
       {collapsible && (
-        <div className={`${locals.toolbarSection} ${!isExpanded ? locals.expanded : ''}`}>
-          <Stack direction="horizontal" align="center" distribution="spaceBetween">
-            <Stack align="start">
-              <DashboardButton
-                kind="ghost"
-                size="sm"
-                className={locals.hideButton}
-                icon={isExpanded ? 'lib_arrow_expand_up' : 'lib_arrow_expand_down'}
-                iconSize={SvgIconSizes.s}
-                onClick={toggleVisibility}
-              >
-                <p> {t(isExpanded ? hideLabel : showLabel)} </p>
-              </DashboardButton>
-            </Stack>
-          </Stack>
+        <div id="infoPanelFooter" className={locals.buttonSection}>
+          <DashboardButton
+            kind="ghost"
+            size="sm"
+            className={locals.hideButton}
+            icon={isExpanded ? 'lib_arrow_expand_up' : 'lib_arrow_expand_down'}
+            iconSize={SvgIconSizes.s}
+            onClick={toggleVisibility}
+          >
+            <span> {t(isExpanded ? i18nLib + hideLabel : i18nLib + showLabel)} </span>
+          </DashboardButton>
         </div>
       )}
     </section>
