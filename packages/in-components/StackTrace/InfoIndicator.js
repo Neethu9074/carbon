ee4175308ef0ed69b54786db45622f41257698bd
Gouri Hariharan <Gouri.Hariharan1@ibm.com>
2024-10-07 14:51:41 +0530

    INSTA-15394: Fix Websites Stack Trace tooltip (#18053)
    * modify tooltip
    
    * using iconButton component

--- packages/in-components/StackTrace/InfoIndicator.js
+++ packages/in-components/StackTrace/InfoIndicator.js
@@ -5,9 +5,8 @@
 
 import React from 'react';
 
-import { IconButton, Button } from '@instana/components';
+import { IconButton } from '@instana/components';
 
-import { carbonButtonEnabled } from 'in-services/featureFlags';
 import Tooltip from 'in-components/Tooltip';
 
 import locals from './InfoIndicator.mless';
@@ -15,19 +14,7 @@ import locals from './InfoIndicator.mless';
 export default function InfoIndicator({ href, href$, children, target = 'blank' }) {
   return (
     <Tooltip content={<div className={locals.content}>{children}</div>}>
-      {href || href$ ? (
-        <Button
-          kind="action"
-          icon="lib_infra_unknownIcon"
-          href={href$ ?? href}
-          target={target}
-          {...(carbonButtonEnabled ? { hasIconOnly: true } : {})}
-        >
-          {''}
-        </Button>
-      ) : (
-        <IconButton type="lib_infra_unknownIcon" />
-      )}
+      <IconButton type="lib_infra_unknownIcon" href={href$ ?? href} target={target} />
     </Tooltip>
   );
 }
