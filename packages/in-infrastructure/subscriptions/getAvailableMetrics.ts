c521e17421931253e9108ec7dad6114598f0ff4c
Axel Niedenhoff <wi95nied@de.ibm.com>
2024-10-24 21:46:24 +0200

    INSTA-17904: Hide some plugins from custom dashboards as well (#18300)
    INSTA-9454: Hide some plugins from custom dashboards as well

--- packages/in-infrastructure/subscriptions/getAvailableMetrics.ts
+++ packages/in-infrastructure/subscriptions/getAvailableMetrics.ts
@@ -6,7 +6,16 @@
 import { AvailableMetrics, GetAvailableMetricsQuery, Result } from '@instana/types';
 
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
+import { hiddenPlugins } from 'in-forge/constants';
+import { mapData } from 'in-services/util/result';
 
 export default createResultSubscriptionFactory<GetAvailableMetricsQuery, Result<AvailableMetrics>>({
-  eventId: 'infrastructure.getAvailableMetrics'
+  eventId: 'infrastructure.getAvailableMetrics',
+  mapResult: filterOutHiddenPlugins
 });
+
+function filterOutHiddenPlugins(result: Result<AvailableMetrics>): Result<AvailableMetrics> {
+  return mapData(result, data => ({
+    metrics: data.metrics?.filter(item => !hiddenPlugins.includes(item.ownerType))
+  }));
+}
--- packages/in-infrastructure/subscriptions/getMetricCatalog.ts
+++ packages/in-infrastructure/subscriptions/getMetricCatalog.ts
@@ -3,9 +3,28 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Result, GetInfraMetricsCatalogQuery, MetricCatalog, MetricTreeLevel } from 'in-types';
 import { createResultSubscriptionFactory } from 'in-subscription/resultSubscriptions';
-import { Result, GetInfraMetricsCatalogQuery, MetricCatalog } from 'in-types';
+import { hiddenPlugins } from 'in-forge/constants';
+import { mapData } from 'in-services/util/result';
 
 export default createResultSubscriptionFactory<GetInfraMetricsCatalogQuery, Result<MetricCatalog>>({
-  eventId: 'infrastructure.getMetricCatalog'
+  eventId: 'infrastructure.getMetricCatalog',
+  mapResult: filterOutHiddenPlugins
 });
+
+function filterOutHiddenPlugins(result: Result<MetricCatalog>): Result<MetricCatalog> {
+  result.data?.tree?.forEach(item =>
+    item.children.splice(
+      0,
+      item.children.length,
+      ...item.children.filter(
+        e => e.type === 'LEVEL' && !hiddenPlugins.includes((e as MetricTreeLevel).levelType || '')
+      )
+    )
+  );
+  return mapData(result, data => ({
+    list: data.list?.filter(item => !hiddenPlugins.includes(item.type || '')),
+    tree: data.tree
+  }));
+}
