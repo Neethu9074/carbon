9eee8cde1a1eaf4ca8470c02172e90fc3065144e
Sebastian Wegmann <wegmase@de.ibm.com>
2024-10-24 18:12:48 +0200

    Enable cumulative mode on prem

--- packages/in-amp/components/presentationPresets.js
+++ packages/in-amp/components/presentationPresets.js
@@ -3,7 +3,6 @@
  * (c) Copyright Instana Inc.
  */
 
-import { onPremLicenseInformationEnabled } from 'in-services/featureFlags';
 import { t } from 'in-i18n';
 
 const presets = [
@@ -17,8 +16,7 @@ const presets = [
     label: t('in-amp:components.presentationPresets.cumulative'),
     isDisabled: timeRange => {
       // Disable cumulated setting for on-premise scenarios
-      if (onPremLicenseInformationEnabled) return true;
-      else return 'this_month' !== timeRange && 'last_month' !== timeRange;
+      return 'this_month' !== timeRange && 'last_month' !== timeRange;
     }
   }
 ];
