583eeecfe5c3ff38cb00545dfd1f8a8637485c2e
Chun-Wah Chung <chunwahchung@ibm.com>
2024-11-01 16:31:22 -0400

    use subscribe-event to get state info

--- packages/in-events/components/EventsListRowDense.js
+++ packages/in-events/components/EventsListRowDense.js
@@ -6,19 +6,24 @@
 import classNames from 'classnames';
 import React from 'react';
 
+import { useObservable } from '@instana/hooks';
 import { Tr, Td } from '@instana/legacy';
 
 import { getEventSeverityLabelWithEventType } from 'in-stores/events';
 import { formatDateTime } from 'in-services/formatters/date';
 import EventIcon from 'in-events/components/EventIcon';
+import { getEvent } from 'in-stores/events';
 
 import locals from './EventsListRowDense.mless';
 
 export default function EventListRowDense({ event, active, onClick, timeConfig }) {
+  const updatedEventInfo = useObservable(getEvent(event.id), [event]);
+  const eventToUse = updatedEventInfo ? updatedEventInfo : event;
+
   return (
     <Tr size="compact" active={active} onClick={onClick}>
       <Td>
-        <EventIcon event={event} tooltipLabel={getEventSeverityLabelWithEventType(event, timeConfig)} />
+        <EventIcon event={eventToUse} tooltipLabel={getEventSeverityLabelWithEventType(eventToUse, timeConfig)} />
       </Td>
       <Td>
         <div
