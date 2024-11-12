d4f929927728702fb8db83d675c1b8a30709ec24
DIANA Costantino <dcostant@us.ibm.com>
2024-11-12 08:21:13 -0500

    chore(form): remove ff

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -371,6 +371,16 @@ module.exports = exports = [
     defaultValue: false
   },
   {
+    uiClientKey: 'bizopsProcessStackEnabled',
+    instanaCtlKey: 'feature.bizops.process.stack.enabled',
+    defaultValue: false
+  },
+  {
+    uiClientKey: 'bizopsActivityStackEnabled',
+    instanaCtlKey: 'feature.bizops.activity.stack.enabled',
+    defaultValue: false
+  },
+  {
     uiClientKey: 'limitVisibleNestingLevelsInTraceEnabled',
     instanaCtlKey: 'feature.trace.limit.visible.nesting.levels.enabled',
     defaultValue: false
@@ -591,19 +601,10 @@ module.exports = exports = [
     instanaCtlKey: 'feature.carbon.iconbutton.enabled',
     defaultValue: true
   },
+
   {
     uiClientKey: 'carbonMoreMenuEnabled',
     instanaCtlKey: 'feature.carbon.more.menu.enabled',
-    defaultValue: true
-  },
-  {
-    uiClientKey: 'carbonLoadingSkeletonFeatureEnabled',
-    instanaCtlKey: 'feature.carbon.loading.skeleton.enabled',
-    defaultValue: true
-  },
-  {
-    uiClientKey: 'carbonLoadingSpinnerEnabled',
-    instanaCtlKey: 'feature.carbon.loading.spinner.enabled',
     defaultValue: true
   },
   {
