7e0f4b58f0adc7c09ad52c5aa9cca646ebacd19f
ANTHONY CUNNINGHAM <ANTHONCU@ie.ibm.com>
2024-11-12 14:53:58 +0000

    chore(TS migration): 13 files in in-components (#18561)
    * chore(TS migration): rename packages/in-components/Chart/renderer/pie
    
    * chore(TS migration): rename packages/in-components/Chart/renderer/clear
    
    * chore(TS migration): rename packages/in-components/Chart/actions/viewInAnalytics
    
    * chore(TS migration): rename packages/in-components/Profiling/components/MethodName
    
    * chore(TS migration): rename packages/in-components/LocationAwareTabView/components/ContentWrapper
    
    * chore(TS migration): rename packages/in-components/ApplicationHealthOverview/ApplicationsHealthIndicatorBar
    
    * chore(TS migration): rename packages/in-components/EntityPageMainNotification/EntityPageMainNotificationLightCardV2
    
    * chore(TS migration): rename packages/in-components/DownloadButton/DownloadButton
    
    * chore(TS migration): rename packages/in-components/Chart/renderer/tickLines
    
    * chore(TS migration): migrate packages/in-components/Chart/renderer/tickLines
    
    * chore(TS migration): migrate packages/in-components/DownloadButton/DownloadButton
    
    * chore(TS migration): migrate packages/in-components/EntityPageMainNotification/EntityPageMainNotificationLightCardV2
    
    * chore(TS migration): migrate packages/in-components/LocationAwareTabView/components/ContentWrapper
    
    * chore(TS migration): migrate packages/in-components/Profiling/components/MethodName
    
    * chore(TS migration): migrate packages/in-components/Chart/actions/viewInAnalytics
    
    * chore(TS migration): migrate packages/in-components/Chart/renderer/clear
    
    * chore(TS migration): migrate packages/in-components/ApplicationHealthOverview/ApplicationsHealthIndicatorBar
    
    * chore(TS migration): migrate packages/in-components/Chart/renderer/Render
    
    * TS Migrations
    
    * TS Migrations Use Pascal Case
    
    * [15:44:06] ModuleNotFoundError: Module not found: Error: Can't resolve 'in-components/DownloadButton' in
    
    * Reverting mless absolute import
    
    * fix download button import

--- packages/in-components/Chart/actions/viewInAnalytics.ts
+++ packages/in-components/Chart/actions/viewInAnalytics.ts
@@ -3,11 +3,13 @@
  * (c) Copyright Instana Inc.
  */
 
+import { Observable } from '@instana/observables';
+
 import { t } from 'in-i18n';
 
 export const actionName = 'analyze';
 
-export function getButton({ getHref$ }) {
+export function getButton({ getHref$ }: { getHref$: () => Observable<string> }) {
   return {
     name: actionName,
     icon: 'lib_analyze',
