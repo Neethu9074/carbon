6cb38fec7d908231de6b5136de7d6ea83a3859cd
Heba Philip <hebaphilip@ibm.com>
2024-11-19 17:45:15 +0530

    INSTA-13150: Remove mixpanel from in-mobile-apps (#18698)
    Remove mixpanel from in-mobile-apps

--- packages/in-websites/tracker.ts
+++
@@ -1,8 +0,0 @@
-/*
- * (c) Copyright IBM Corp. 2021
- * (c) Copyright Instana Inc.
- */
-
-import { track, ANALYZE_UA2_CHART_CHANGED } from 'in-services/tracking/tracking';
-
-export const ua2ChartChangedTracker = (e?: Object) => track(ANALYZE_UA2_CHART_CHANGED, e);
