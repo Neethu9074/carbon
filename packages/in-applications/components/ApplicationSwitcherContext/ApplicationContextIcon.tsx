91d017b1474f50f39e759fa4071e673203ceadb8
Anthony Cunningham <anthoncu@ie.ibm.com>
2024-11-20 09:39:44 +0000

    chore(TS migration): migrate packages/in-applications/components/ApplicationSwitcherContext/ApplicationContextIcon

--- packages/in-applications/components/ApplicationSwitcherContext/ApplicationContextIcon.tsx
+++ packages/in-applications/components/ApplicationSwitcherContext/ApplicationContextIcon.tsx
@@ -5,16 +5,19 @@
 
 import React from 'react';
 
+import { EntityHealthInfo } from '@instana/types';
 import { SvgIcon } from '@instana/components';
 
+// @ts-expect-error import WithApplicationHealthIndicationBehaviour from 'in-components/health/WithHealthIndication/WithApplicationHealthIndicationBehaviour';
 import WithApplicationHealthIndicationBehaviour from 'in-components/health/WithHealthIndication/WithApplicationHealthIndicationBehaviour';
+// @ts-expect-error import WithHealthIndication from 'in-components/health/WithHealthIndication';
 import WithHealthIndication from 'in-components/health/WithHealthIndication';
 
-export default function ServiceContextIcon({ className, applicationId }) {
+export default function ServiceContextIcon({ className, applicationId }: { className: string; applicationId: string }) {
   return (
     <WithApplicationHealthIndicationBehaviour
       applicationId={applicationId}
-      render={healthInfo => (
+      render={(healthInfo: EntityHealthInfo) => (
         <WithHealthIndication healthInfo={healthInfo} iconSize="l">
           <SvgIcon className={className} type="lib_application" size="l" />
         </WithHealthIndication>
