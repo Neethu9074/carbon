3bbced78fb5491fd347bcdae71436392d995c44d
bin zhong <bzhong@us.ibm.com>
2024-12-03 07:56:51 -0800

    feat(turbo): new feature flag to control events page resource actions, revert auto-imports-order

--- packages/in-events/components/EventsList.js
+++ packages/in-events/components/EventsList.js
@@ -26,8 +26,8 @@ import { aqmDataGridEventTableEnabled, carbonTableEnabled } from 'in-services/fe
 import useTimeConfigUpdatingScale from 'in-events/components/useTimeConfigUpdatingScale';
 import { manuallyCloseEventEnabled, multiCloseEnabled } from 'in-services/featureFlags';
 import MultiCloseIssueConfigForm from 'in-events/components/MultiCloseIssueConfigForm';
-import EventsTable from 'in-events/components/EventsPage/EventsTable/EventsTable';
 import FailedIncidentsList from 'in-events/components/FailedIncidentsList.tsx';
+import EventsTable from 'in-events/components/EventsPage/EventsTable/EventsTable';
 import { stopPropagationAndPreventDefault } from 'in-services/util/function';
 import { addMessage } from 'in-components/MessageFlyout/stores/messages';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
