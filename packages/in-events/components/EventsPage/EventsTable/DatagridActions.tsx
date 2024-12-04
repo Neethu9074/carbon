03cdd9b1bda50bc4f2ad43199162673b7078fff0
HAU-WEN CHANG <changha@us.ibm.com>
2024-12-03 16:03:03 -0800

    chore(TS migration): migrate packages/in-events/components/EventsPage/EventsTable/DatagridActions
    Co-authored-by: Dan-Stingaciu <dan.stingaciu@ibm.com>

--- packages/in-events/components/EventsPage/EventsTable/DatagridActions.tsx
+++ packages/in-events/components/EventsPage/EventsTable/DatagridActions.tsx
@@ -6,21 +6,20 @@
 
 // eslint-disable-next-line no-restricted-imports
 import { IconButton, TableToolbarContent } from '@carbon/react';
+import { DataGridState } from '@carbon/ibm-products/lib/components/Datagrid/types';
+import { useFilterContext } from '@carbon/ibm-products';
 import { Filter } from '@carbon/react/icons';
 import React from 'react';
 
-import { useFilterContext } from '@instana/ibm-products';
-
 import locals from './EventsTable.mless';
 
-export const DatagridActions = datagridState => {
-  const { filterProps } = datagridState;
+export const DatagridActions = (datagridState: DataGridState) => {
   const { setPanelOpen } = useFilterContext();
-
+  const label = datagridState.filterProps?.panelIconDescription || '';
   return (
     <>
       <TableToolbarContent className={locals.toolbarContent}>
-        <IconButton label={filterProps.panelIconDescription} kind="ghost" onClick={() => setPanelOpen(open => !open)}>
+        <IconButton label={label} kind="ghost" onClick={() => setPanelOpen((open: boolean) => !open)}>
           <Filter />
         </IconButton>
       </TableToolbarContent>
