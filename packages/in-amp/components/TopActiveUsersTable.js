d0c22b72799888f6c368d944c3f1ba3ea4d5df3b
SHRUTHI RAO <shruthra@in.ibm.com>
2024-12-05 14:56:29 +0530

    remove legacy table code and feature flag in in-amp component - INSTA-21098 (#18902)
    * remove legacy table code and feature flag
    
    * remove legacy table code and feature flag

--- packages/in-amp/components/TopActiveUsersTable.js
+++ packages/in-amp/components/TopActiveUsersTable.js
@@ -6,10 +6,8 @@
 import React from 'react';
 
 import { DataTable as CarbonDataTable } from '@instana/components';
-import { Table, Thead, Tbody, Tr, Th, Td } from '@instana/legacy';
 
 import NoDataAvailable from 'in-components/Errors/NoDataAvailable';
-import { carbonTableEnabled } from 'in-services/featureFlags';
 import { compare } from 'in-services/util/number';
 import { t } from 'in-i18n';
 
@@ -28,97 +26,45 @@ export default function TopActiveUsersTable({ accountInfo }) {
   }
   const result = dataSet[Object.keys(dataSet)[0]];
 
-  if (carbonTableEnabled) {
-    const carbonHeaders = [
-      {
-        key: 'name',
-        header: t('in-amp:components.activationAdoption.topUsersTable.name')
-      },
-      {
-        key: 'organizationRole',
-        header: t('in-amp:components.activationAdoption.topUsersTable.organizationRole')
-      },
-      {
-        key: 'daysActive',
-        header: t('in-amp:components.activationAdoption.topUsersTable.daysActive')
-      }
-    ];
-
-    if (Object.keys(result).length === 0) {
-      return (
-        <div>
-          <NoDataAvailable
-            text={t('in-amp:components.activationAdoption.topUsersTable.noDataAvailable')}
-            height={300}
-          />
-        </div>
-      );
+  const carbonHeaders = [
+    {
+      key: 'name',
+      header: t('in-amp:components.activationAdoption.topUsersTable.name')
+    },
+    {
+      key: 'organizationRole',
+      header: t('in-amp:components.activationAdoption.topUsersTable.organizationRole')
+    },
+    {
+      key: 'daysActive',
+      header: t('in-amp:components.activationAdoption.topUsersTable.daysActive')
     }
-
-    const carbonRows = Object.keys(result)
-      .sort((a, b) => compare(result[b], result[a]))
-      .map(entry => {
-        const values = entry.split('#');
-        return {
-          ['name']:
-            values[1] === '[not provided]'
-              ? t('in-amp:components.activationAdoption.topUsersTable.notProvided')
-              : values[1],
-          ['organizationRole']: values[2],
-          ['daysActive']: result[entry]
-        };
-      });
-    return (
-      <>
-        <CarbonDataTable headers={carbonHeaders} rows={carbonRows} isSearchEnabled={false} />
-      </>
-    );
-  }
+  ];
 
   if (Object.keys(result).length === 0) {
     return (
-      <Table>
-        <Thead>
-          <Tr size="compact">
-            <Th>{t('in-amp:components.activationAdoption.topUsersTable.noDataAvailable')}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr size="compact">
-            <Td>{t('in-amp:components.activationAdoption.topUsersTable.noTopActiveUsers')}</Td>
-          </Tr>
-        </Tbody>
-      </Table>
+      <div>
+        <NoDataAvailable text={t('in-amp:components.activationAdoption.topUsersTable.noTopActiveUsers')} height={300} />
+      </div>
     );
   }
 
+  const carbonRows = Object.keys(result)
+    .sort((a, b) => compare(result[b], result[a]))
+    .map(entry => {
+      const values = entry.split('#');
+      return {
+        ['name']:
+          values[1] === '[not provided]'
+            ? t('in-amp:components.activationAdoption.topUsersTable.notProvided')
+            : values[1],
+        ['organizationRole']: values[2],
+        ['daysActive']: result[entry]
+      };
+    });
   return (
-    <Table>
-      <Thead>
-        <Tr size="compact">
-          <Th>{t('in-amp:components.activationAdoption.topUsersTable.name')}</Th>
-          <Th>{t('in-amp:components.activationAdoption.topUsersTable.organizationRole')}</Th>
-          <Th>{t('in-amp:components.activationAdoption.topUsersTable.daysActive')}</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {Object.keys(result)
-          .sort((a, b) => compare(result[b], result[a]))
-          .map(entry => {
-            const values = entry.split('#');
-            return (
-              <Tr key={values[0]} size="compact">
-                <Td>
-                  {values[1] === '[not provided]'
-                    ? t('in-amp:components.activationAdoption.topUsersTable.notProvided')
-                    : values[1]}
-                </Td>
-                <Td>{values[2]}</Td>
-                <Td>{result[entry]}</Td>
-              </Tr>
-            );
-          })}
-      </Tbody>
-    </Table>
+    <>
+      <CarbonDataTable headers={carbonHeaders} rows={carbonRows} isSearchEnabled={false} />
+    </>
   );
 }
