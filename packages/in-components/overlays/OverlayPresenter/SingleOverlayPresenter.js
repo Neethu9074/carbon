fe11bbd0fe47d0f3e5090ecbb3a571282811960b
Josh King <jdking@us.ibm.com>
2024-12-11 13:20:10 -0600

    fix(overlay): update attributes per a11y

--- packages/in-components/overlays/OverlayPresenter/SingleOverlayPresenter.js
+++ packages/in-components/overlays/OverlayPresenter/SingleOverlayPresenter.js
@@ -119,7 +119,8 @@ export default connectTo(
           })}
           onMouseEnter={autoOpen ? delayedOpen : undefined}
           onMouseLeave={autoClose ? delayedClose : undefined}
-          tabIndex={'0'}
+          tabIndex={'-1'}
+          role="dialog"
         >
           <Content {...props} />
         </div>
