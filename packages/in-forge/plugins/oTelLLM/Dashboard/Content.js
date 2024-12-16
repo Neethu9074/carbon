2d98f8664f2c72628e6101732d3b334969d52366
Lakshmi Priya S <Lakshmi.Priya.S@ibm.com>
2024-12-16 16:51:08 +0530

    INSTA-17654 Split tokens and cost into input and output values

--- packages/in-forge/plugins/oTelLLM/Dashboard/Content.js
+++ packages/in-forge/plugins/oTelLLM/Dashboard/Content.js
@@ -26,7 +26,11 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
   const snapshotId = snapshot.get('id');
   const metricIds = snapshot.get('metricIds');
   const tokens = metricIds.filter(metric => metric.includes('llm.usage.total_tokens')).toArray();
+  const inputTokens = metricIds.filter(metric => metric.includes('llm.usage.input_tokens')).toArray();
+  const outputTokens = metricIds.filter(metric => metric.includes('llm.usage.output_tokens')).toArray();
   const costs = metricIds.filter(metric => metric.includes('llm.usage.cost')).toArray();
+  const inputCosts = metricIds.filter(metric => metric.includes('llm.usage.input_cost')).toArray();
+  const outputCosts = metricIds.filter(metric => metric.includes('llm.usage.output_cost')).toArray();
   const count = metricIds.filter(metric => metric.includes('llm.request.count')).toArray();
   const durations = metricIds.filter(metric => metric.includes('llm.response.duration')).toArray();
   const getDashboardLink = useGetDashboardLink();
@@ -88,20 +92,14 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
           formatter="number.compact"
         />
         <TotalUsageBigNumber
-          title={t('in-forge:plugins.oTelLLM.dashboard.inputToken')}
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalInputToken')}
           metricName="metrics.gauges.llm.usage.input_tokens"
           tagFilter={instanceId}
           formatter="number.compact"
         />
         <TotalUsageBigNumber
-          title={t('in-forge:plugins.oTelLLM.dashboard.totalCost')}
-          metricName="metrics.gauges.llm.usage.cost"
-          tagFilter={instanceId}
-          formatter="scale.compact"
-        />
-        <TotalUsageBigNumber
-          title={t('in-forge:plugins.oTelLLM.dashboard.totalCount')}
-          metricName="metrics.sums.llm.request.count"
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalOutputToken')}
+          metricName="metrics.gauges.llm.usage.output_tokens"
           tagFilter={instanceId}
           formatter="number.compact"
         />
@@ -115,11 +113,71 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
           formatter="number.compact"
         />
         <TopListByModel
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalInputTokenByModel')}
+          metricName="metrics.gauges.llm.usage.input_tokens"
+          tagFilter={instanceId}
+          formatter="number.compact"
+        />
+        <TopListByModel
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalOutputTokenByModel')}
+          metricName="metrics.gauges.llm.usage.output_tokens"
+          tagFilter={instanceId}
+          formatter="number.compact"
+        />
+      </Columize>
+
+      <Columize>
+        <TotalUsageBigNumber
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalCost')}
+          metricName="metrics.gauges.llm.usage.cost"
+          tagFilter={instanceId}
+          formatter="scale.compact"
+        />
+        <TotalUsageBigNumber
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalInputCost')}
+          metricName="metrics.gauges.llm.usage.input_cost"
+          tagFilter={instanceId}
+          formatter="scale.compact"
+        />
+        <TotalUsageBigNumber
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalOutputCost')}
+          metricName="metrics.gauges.llm.usage.output_cost"
+          tagFilter={instanceId}
+          formatter="scale.compact"
+        />
+      </Columize>
+
+      <Columize>
+        <TopListByModel
           title={t('in-forge:plugins.oTelLLM.dashboard.totalCostByModel')}
           metricName="metrics.gauges.llm.usage.cost"
           tagFilter={instanceId}
           formatter="scale.compact"
         />
+        <TopListByModel
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalInputCostByModel')}
+          metricName="metrics.gauges.llm.usage.input_cost"
+          tagFilter={instanceId}
+          formatter="scale.compact"
+        />
+        <TopListByModel
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalOutputCostByModel')}
+          metricName="metrics.gauges.llm.usage.output_cost"
+          tagFilter={instanceId}
+          formatter="scale.compact"
+        />
+      </Columize>
+
+      <Columize>
+        <TotalUsageBigNumber
+          title={t('in-forge:plugins.oTelLLM.dashboard.totalCount')}
+          metricName="metrics.sums.llm.request.count"
+          tagFilter={instanceId}
+          formatter="number.compact"
+        />
+      </Columize>
+
+      <Columize>
         <TopListByModel
           title={t('in-forge:plugins.oTelLLM.dashboard.totalCountByModel')}
           metricName="metrics.sums.llm.request.count"
@@ -138,11 +196,53 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
               min: 0,
               formatter: number.detailed,
               metrics: tokens ? tokens : [],
-              labels: tokens?.map(matric => {
-                if (matric.split('.').length > 3) {
-                  return matric.split('.')[3];
+              labels: tokens?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
                 }
                 return t('in-forge:plugins.oTelLLM.dashboard.token');
+              }),
+              type: 'line',
+              aggregation: 'sum'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+        <DashboardSection title={t('in-forge:plugins.oTelLLM.dashboard.inputToken')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            minRollup={minRollup}
+            y1={{
+              min: 0,
+              formatter: number.detailed,
+              metrics: inputTokens ? inputTokens : [],
+              labels: inputTokens?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
+                }
+                return t('in-forge:plugins.oTelLLM.dashboard.inputToken');
+              }),
+              type: 'line',
+              aggregation: 'sum'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+        <DashboardSection title={t('in-forge:plugins.oTelLLM.dashboard.outputToken')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            minRollup={minRollup}
+            y1={{
+              min: 0,
+              formatter: number.detailed,
+              metrics: outputTokens ? outputTokens : [],
+              labels: outputTokens?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
+                }
+                return t('in-forge:plugins.oTelLLM.dashboard.outputToken');
               }),
               type: 'line',
               aggregation: 'sum'
@@ -161,11 +261,53 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
               min: 0,
               formatter: scale.detailed,
               metrics: costs ? costs : [],
-              labels: costs?.map(matric => {
-                if (matric.split('.').length > 3) {
-                  return matric.split('.')[3];
+              labels: costs?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
                 }
                 return t('in-forge:plugins.oTelLLM.dashboard.cost');
+              }),
+              type: 'line',
+              aggregation: 'sum'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+        <DashboardSection title={t('in-forge:plugins.oTelLLM.dashboard.inputCost')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            minRollup={minRollup}
+            y1={{
+              min: 0,
+              formatter: scale.detailed,
+              metrics: inputCosts ? inputCosts : [],
+              labels: inputCosts?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
+                }
+                return t('in-forge:plugins.oTelLLM.dashboard.inputCost');
+              }),
+              type: 'line',
+              aggregation: 'sum'
+            }}
+            renderPostChartContent={PluginDashboardsMarkerLanes}
+          />
+        </DashboardSection>
+        <DashboardSection title={t('in-forge:plugins.oTelLLM.dashboard.outputCost')}>
+          <Chart
+            snapshotId={snapshotId}
+            timeConfig={timeConfig}
+            minRollup={minRollup}
+            y1={{
+              min: 0,
+              formatter: scale.detailed,
+              metrics: outputCosts ? outputCosts : [],
+              labels: outputCosts?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
+                }
+                return t('in-forge:plugins.oTelLLM.dashboard.outputCost');
               }),
               type: 'line',
               aggregation: 'sum'
@@ -184,9 +326,9 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
               min: 0,
               formatter: number.detailed,
               metrics: count ? count : [],
-              labels: count?.map(matric => {
-                if (matric.split('.').length > 3) {
-                  return matric.split('.')[3];
+              labels: count?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
                 }
                 return t('in-forge:plugins.oTelLLM.dashboard.count');
               }),
@@ -207,9 +349,9 @@ export default function OTelLLMDashboard({ snapshot, timeConfig }) {
               min: 0,
               formatter: millis.detailed,
               metrics: durations ? durations : [],
-              labels: durations?.map(matric => {
-                if (matric.split('.').length > 3) {
-                  return matric.split('.')[3];
+              labels: durations?.map(metric => {
+                if (metric.split('.').length > 3) {
+                  return metric.split('.')[3];
                 }
                 return t('in-forge:plugins.oTelLLM.dashboard.duration');
               }),
