362119495f05c107b2864ad70300145229f95479
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2024-12-17 13:47:08 +0530

    fixed tab selection issue in Infra SA listing page (#18998)
    fix

--- packages/in-infrastructure/tableView/components/ViewSwitcher.tsx
+++ packages/in-infrastructure/tableView/components/ViewSwitcher.tsx
@@ -46,7 +46,7 @@ export default function InfrastructureViewSwitcher({ showSearchBar = true, theme
         <SecondLevelNavigationItem
           href={createHrefToPath(physicalTablePath)}
           label={t('in-infrastructure:tableView.comparisonTable')}
-          isActive={isTableActive === null ? undefined : isAlertActive}
+          isActive={isTableActive ?? false}
         />
         {infraSmartAlertsEnabled && !role?.limitedInfrastructureScope && (
           <SecondLevelNavigationItem
