4d4d04a12201278a9a18fd5f2ff98f8f76bd84ed
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2025-01-08 09:39:31 -0500

    fix issue INSTA-20902

--- packages/in-automation/utils/action.ts
+++ packages/in-automation/utils/action.ts
@@ -24,5 +24,5 @@ export const aiOriginatedMetadata = {
 
 export const isNotEditable = (action: Action | NewAction, isCopy: boolean) =>
   ((action.metadata?.builtIn ?? false) && !isCopy) ||
-  action.type === ACTION_TYPE.ANSIBLE ||
+  (action.type === ACTION_TYPE.ANSIBLE && !role?.canConfigureAutomationActions) ||
   !role?.canConfigureAutomationActions;
