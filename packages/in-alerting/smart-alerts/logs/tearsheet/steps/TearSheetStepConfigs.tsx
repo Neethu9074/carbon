1fbc269b88674c74757dec81e9d603dde7d0b5cd
Pradosh E <Pradosh.E@ibm.com>
2025-01-10 20:47:01 +0530

    integrate step 4 alert channel into log SA full screen. (#19052)
    * integrate step 4 alert channel into log SA full screen.
    
    * carbon tearsheet related changes for step 4
    
    * addressing review comments. - space issue

--- packages/in-alerting/smart-alerts/logs/tearsheet/steps/TearSheetStepConfigs.tsx
+++ packages/in-alerting/smart-alerts/logs/tearsheet/steps/TearSheetStepConfigs.tsx
@@ -7,6 +7,7 @@
 import AlertConfigTearSheetStep1 from 'in-alerting/smart-alerts/logs/tearsheet/steps/AlertConfigTearSheetStep1';
 import AlertConfigTearSheetStep2 from 'in-alerting/smart-alerts/logs/tearsheet/steps/AlertConfigTearSheetStep2';
 import AlertConfigTearSheetStep3 from 'in-alerting/smart-alerts/logs/tearsheet/steps/AlertConfigTearSheetStep3';
+import AlertConfigTearSheetStep4 from 'in-alerting/smart-alerts/logs/tearsheet/steps/AlertConfigTearSheetStep4';
 import { t } from 'in-i18n';
 
 export const stepConfigsForCarbonTearSheet = [
@@ -24,25 +25,10 @@ export const stepConfigsForCarbonTearSheet = [
     title: t('in-alerting:smartAlerts.logs.tearSheet.steps.step3Title'),
     validateIntermediately: [['name']],
     component: AlertConfigTearSheetStep3
-  }
-];
-
-export const stepConfigs = [
-  {
-    title: t('in-alerting:smartAlerts.logs.tearSheet.steps.step1Title'),
-    validateIntermediately: []
-  },
-
-  {
-    title: t('in-alerting:smartAlerts.logs.tearSheet.steps.step2Title'),
-    validateIntermediately: [['name']]
-  },
-  {
-    title: t('in-alerting:smartAlerts.logs.tearSheet.steps.step3Title'),
-    validateIntermediately: []
   },
   {
     title: t('in-alerting:smartAlerts.logs.tearSheet.steps.step4Title'),
-    validateIntermediately: []
+    validateIntermediately: [],
+    component: AlertConfigTearSheetStep4
   }
 ];
