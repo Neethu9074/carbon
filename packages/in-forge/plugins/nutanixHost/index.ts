aa03225df3158fb518829c52a29bb7791e5beb2f
Shibu N <n.shibu@ibm.com>
2025-01-28 14:49:20 +0530

    Nutanix Sensor - Changes for Infra view and Defect fixes for VM in Off state

---
+++ packages/in-forge/plugins/nutanixHost/index.ts
@@ -0,0 +1,18 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2025
+ */
+
+// @ts-expect-error needs TS migration
+import { registerSnapshotDefinition } from 'in-sdk/snapshot';
+import metricDefinitions from 'in-forge/plugins/nutanixHost/metricDefinitions';
+import kpiDefinitions from 'in-forge/plugins/nutanixHost/kpiDefinitions';
+import { plugins } from 'in-forge/constants';
+
+registerSnapshotDefinition({
+  plugin: plugins.nutanixHost,
+  kpiDefinitions,
+  metricDefinitions,
+  getIconType: () => 'nutanix'
+});
