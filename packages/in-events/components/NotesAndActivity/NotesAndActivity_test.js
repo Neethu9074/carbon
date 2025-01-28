28fce838a18b644bb01e440a4cbbea213596fd2b
Josh King <jdking@us.ibm.com>
2025-01-28 11:40:23 -0600

    chore(notesactivity): add in tests for empty state

--- packages/in-events/components/NotesAndActivity/NotesAndActivity_test.js
+++ packages/in-events/components/NotesAndActivity/NotesAndActivity_test.js
@@ -10,10 +10,12 @@ import React from 'react';
 
 import { IconButton, CarbonSearch, CarbonInlineLoading, CarbonLayer, CarbonModal } from '@instana/components';
 
+import { NotesAndActivity, EmptyState } from 'in-events/components/NotesAndActivity/NotesAndActivity';
 import { CommentInput } from 'in-events/components/NotesAndActivity/components/CommentInput';
 import { QuickActions } from 'in-events/components/NotesAndActivity/components/QuickActions';
 import { CommentList } from 'in-events/components/NotesAndActivity/components/CommentList';
-import { NotesAndActivity } from 'in-events/components/NotesAndActivity/NotesAndActivity';
+
+import locals from './NotesAndActivity.mless';
 
 jest.mock('in-services/featureFlags');
 
@@ -83,5 +85,25 @@ describe('NotesAndActivity', () => {
     expect(wrapper.find(CommentInput)).toHaveLength(1);
     expect(wrapper.find(QuickActions)).toHaveLength(1);
     expect(wrapper.find(CarbonModal)).toHaveLength(1);
+  });
+});
+
+describe('EmptyState', () => {
+  it('renders correctly without errors', () => {
+    shallow(<EmptyState />);
+  });
+
+  it('renders the correct empty state header text', () => {
+    const getByText = shallow(<EmptyState />);
+    expect(getByText.find(`h3.${locals.emptyHeader}`)).toHaveLength(1);
+    expect(getByText.find(`h3.${locals.emptyHeader}`).text()).toEqual('No activity yet');
+  });
+
+  it('renders the correct empty state info text', () => {
+    const getByText = shallow(<EmptyState />);
+    expect(getByText.find(`p.${locals.emptyInfo}`)).toHaveLength(1);
+    expect(getByText.find(`p.${locals.emptyInfo}`).text()).toEqual(
+      'There are no notes or activity to show yet. Use the Add comment field to share knowledge or an update for this incident with your team.'
+    );
   });
 });
