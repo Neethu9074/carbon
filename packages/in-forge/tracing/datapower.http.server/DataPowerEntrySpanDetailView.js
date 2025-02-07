004342fb96993ab05e639c5ef5b80dcf1c64a1ed
Swati Pandey <swatpand@in.ibm.com>
2025-02-07 10:59:21 +0530

    add eslint in-forge - INSTA-23297 (#19535)
    add eslint

--- packages/in-forge/tracing/datapower.http.server/DataPowerEntrySpanDetailView.js
+++ packages/in-forge/tracing/datapower.http.server/DataPowerEntrySpanDetailView.js
@@ -4,6 +4,6 @@
  * Copyright IBM Corp. 2023
  */
 
-import HttpSpanDetailView from '../http/HttpSpanDetailView';
+import HttpSpanDetailView from 'in-forge/tracing/http/HttpSpanDetailView';
 
 export default HttpSpanDetailView;
