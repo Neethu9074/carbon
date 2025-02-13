f6a1a5751df40af978eb4fe959f60ef8d334fb7c
Ashok Saravanan <ashok.saravanan24@ibm.com>
2025-02-13 15:18:03 +0000

    fixes for burnrate alertwindow error and style fixes (#19628)
    * fixes for burnrate alertwindow error and style fixes
    
    * fix header

--- packages/in-alerting/smart-alerts/slo/components/BurnRateBlueprintSectionContent.tsx
+++ packages/in-alerting/smart-alerts/slo/components/BurnRateBlueprintSectionContent.tsx
@@ -21,6 +21,8 @@ import useSloConfigurations from 'in-service-levels/hooks/useSloConfigurations';
 import ValidationBlock from 'in-components/form/ValidationBlock';
 import { Trans, t } from 'in-i18n';
 
+import locals from './BurnRateBlueprintSectionContent.mless';
+
 export default function BurnRateBlueprintSectionContent() {
   const { form, onChange } = useSloAlertFormContext();
 
@@ -96,6 +98,7 @@ export default function BurnRateBlueprintSectionContent() {
             <Stack direction="horizontal">
               <NumberInput
                 size="md"
+                className={locals.alertWindowInput}
                 invalid={
                   longTimeWindowDurationValue > maxAllowedTimeWindow ||
                   !isLongTimeWindowDurationFieldValid ||
@@ -104,9 +107,9 @@ export default function BurnRateBlueprintSectionContent() {
                 value={longTimeWindowDurationValue}
                 min={0}
                 max={1000}
-                onChange={(_: React.ChangeEvent<HTMLInputElement>, { value }: { value: number }) => {
+                onChange={({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
                   onChange(['burnRateTimeWindows', 'longTimeWindow', 'duration'], () =>
-                    longTimeWindowDurationField.setValue(value).setTouched(true)
+                    longTimeWindowDurationField.setValue(+value).setTouched(true)
                   );
                 }}
                 id="slo-alerting-burn-rate-long-window"
@@ -155,6 +158,7 @@ export default function BurnRateBlueprintSectionContent() {
             <Stack direction="horizontal">
               <NumberInput
                 size="md"
+                className={locals.alertWindowInput}
                 invalid={
                   shortTimeWindowDurationInMilliseconds > maxAllowedTimeWindow ||
                   !isShortTimeWindowDurationFieldValid ||
@@ -163,9 +167,9 @@ export default function BurnRateBlueprintSectionContent() {
                 value={shortTimeWindowDurationValue}
                 min={0}
                 max={10000}
-                onChange={(_: React.ChangeEvent<HTMLInputElement>, { value }: { value: number }) => {
+                onChange={({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
                   onChange(['burnRateTimeWindows', 'shortTimeWindow', 'duration'], () =>
-                    shortTimeWindowDurationField.setValue(value).setTouched(true)
+                    shortTimeWindowDurationField.setValue(+value).setTouched(true)
                   );
                 }}
                 id="slo-alerting-burn-rate-short-window"
@@ -223,8 +227,8 @@ export default function BurnRateBlueprintSectionContent() {
               value={thresholdField.value}
               min={0}
               max={100}
-              onChange={(_: React.ChangeEvent<HTMLInputElement>, { value }: { value: number }) =>
-                onChange(['threshold'], () => thresholdField.setValue(value).setTouched(true))
+              onChange={({ target: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+                onChange(['threshold'], () => thresholdField.setValue(+value).setTouched(true))
               }
             />
           </Trans>
