60cbf46c9194666b62ee7ce71b999a7e79aa52f0
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2025-02-19 09:49:23 -0500

    fix ansible timeout field

--- packages/in-automation/utils/actionField.ts
+++ packages/in-automation/utils/actionField.ts
@@ -611,7 +611,7 @@ export interface CommentProps {
 }
 export type TicketTypes = OpenProps | CloseProps | CommentProps | OpenGLProps | OpenJiraProps;
 
-function createTimeoutField(value: string): Field {
+export function createTimeoutField(value: string): Field {
   return {
     value,
     description: 'timeout of the action execution in seconds',
