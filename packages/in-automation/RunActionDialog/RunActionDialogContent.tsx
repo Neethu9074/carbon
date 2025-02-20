d227863e2c19298737af07a3f19694083f7378a6
Kavya Nekkalapu <knekkal@us.ibm.com>
2025-02-20 11:16:21 -0500

    Fix policies agent selection (#19699)
    fix policies agent

--- packages/in-automation/RunActionDialog/RunActionDialogContent.tsx
+++ packages/in-automation/RunActionDialog/RunActionDialogContent.tsx
@@ -88,7 +88,8 @@ export default function RunActionDialogContent({
   policy,
   isSaving = false
 }: RunActionDialogContentProps) {
-  if (!form || isSaving) return <LoadingIndicator size="xxl" />;
+  if (!form || isSaving || agentSnapShots?.progress?.loading) return <LoadingIndicator size="xxl" />;
+
   if (error && !actionInstanceId) {
     return (
       <>
@@ -186,7 +187,9 @@ function AgentSelection({
       )
     );
   }, [agentSnapShots?.data?.online]);
-  const options =
+
+  if (!hostSnapshots) return null;
+  const sortedOptions =
     hostSnapshots
       ?.map(({ hostSnapshot, agent }) => {
         const isTriggeringAgent = agent.volatileId?.host_id === volatileId.host_id;
@@ -198,12 +201,9 @@ function AgentSelection({
       })
       .sort((a, b) => a.label.localeCompare(b.label)) ?? [];
 
-  if (policy) {
-    options.push({
-      value: TRIGGERING_AGENT,
-      label: t('in-automation:policies.triggeringAgent')
-    });
-  }
+  const options = policy
+    ? [...sortedOptions, { value: TRIGGERING_AGENT, label: t('in-automation:policies.triggeringAgent') }]
+    : sortedOptions;
 
   return (
     <>
