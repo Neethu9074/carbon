19e3be34364082de19f3e20f6768ec59cf95a69a
Anvar pk <anvarpk@ibm.com>
2025-02-20 19:25:03 +0530

    INSTA-25250: add generate with watsonx button for manual type (#19712)
    * INSTA-25250: generate with watsonx button for manual type
    
    * INSTA-25250: generate with watsonx button for manual type

--- packages/in-automation/ActionDashboard/ActionConfiguration/ActionDetailsCard.tsx
+++ packages/in-automation/ActionDashboard/ActionConfiguration/ActionDetailsCard.tsx
@@ -4,6 +4,7 @@
  * Copyright IBM Corp. 2025
  */
 
+import classNames from 'classnames';
 import React from 'react';
 
 import {
@@ -21,8 +22,11 @@ import {
 } from '@instana/components';
 import { Action } from '@instana/types';
 
+import GenerateAIScriptActionDialog from 'in-automation/AutomationCard/GenerateAI/GenerateScriptAction/GenerateAIScriptActionDialog';
+import { useActionFormContext } from 'in-automation/ActionCatalog/useActionForm/useActionForm';
+import { ACTION_TYPE, NO_FIELD_VALUE } from 'in-automation/constants';
+import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { ActionFormEntity } from 'in-automation/ActionCatalog/types';
-import { NO_FIELD_VALUE } from 'in-automation/constants';
 import { Nullish } from 'in-types';
 import { t } from 'in-i18n';
 
@@ -35,7 +39,6 @@ interface ActionConfigurationProps {
 export default function ActionDetailsCard({ data }: ActionConfigurationProps) {
   if (!data) return null;
   const { name, description, tags } = data;
-
   const renderTags = tags?.length
     ? tags.map(tag => (
         <CarbonTag key={tag} type="gray" size="md">
@@ -68,8 +71,25 @@ export default function ActionDetailsCard({ data }: ActionConfigurationProps) {
 }
 
 function ActionConfigurationActions() {
+  const { form } = useActionFormContext();
+  const manualContent = form.get('manualContent').value;
+  const type = form.get('type').value;
+  const actionName = form.get('name').value;
   return (
     <CarbonStack orientation="horizontal">
+      {type === ACTION_TYPE.MANUAL && (
+        <CarbonButton
+          className={classNames(local.ghostBtn, local.watsonxBtn)}
+          kind="ghost"
+          size="sm"
+          onClick={() => {
+            addActiveDialog(<GenerateAIScriptActionDialog manualContent={manualContent} actionName={actionName} />);
+          }}
+          renderIcon={() => <SvgIcon type="lib_launch_ai" size="xs" />}
+        >
+          {t('in-automation:generateWithWatsonx')}
+        </CarbonButton>
+      )}
       <CarbonIconButton label={t('in-automation:copy')} kind="ghost" size="sm">
         <SvgIcon type="lib_actions_copy" size="xs" />
       </CarbonIconButton>
@@ -80,7 +100,7 @@ function ActionConfigurationActions() {
         <SvgIcon type="lib_actions_delete" size="xs" />
       </CarbonIconButton>
       <CarbonButton
-        className={local.testActionButton}
+        className={classNames(local.ghostBtn, local.testActionButton)}
         kind="ghost"
         size="sm"
         renderIcon={() => <SvgIcon type="lib_actions_play" size="xs" color="#fff" />}
