3211f93400f56a6b7b120cc83acf944b1a17d300
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-02-25 13:23:24 +0000

    Remove empty array KpiDefinitions file

--- packages/in-forge/plugins/aliCloudOssBucket/index.js
+++ packages/in-forge/plugins/aliCloudOssBucket/index.js
@@ -4,12 +4,10 @@
  */
 
 import metricDefinitions from 'in-forge/plugins/aliCloudOssBucket/metricDefinitions';
-import kpiDefinitions from 'in-forge/plugins/aliCloudOssBucket/kpiDefinitions';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
 import { plugins } from 'in-forge/constants';
 
 registerSnapshotDefinition({
   plugin: plugins.aliCloudOssBucket,
-  kpiDefinitions,
   metricDefinitions
 });
