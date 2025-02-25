53a586ed2955ab5a33af1695b46ff8a33b09e584
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-02-25 13:25:11 +0000

    Remove empty array KpiDefinitions file

--- packages/in-forge/plugins/aliCloudRocketMqGroup/index.js
+++ packages/in-forge/plugins/aliCloudRocketMqGroup/index.js
@@ -4,12 +4,10 @@
  */
 
 import metricDefinitions from 'in-forge/plugins/aliCloudRocketMqGroup/metricDefinitions';
-import kpiDefinitions from 'in-forge/plugins/aliCloudRocketMqGroup/kpiDefinitions';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
 import { plugins } from 'in-forge/constants';
 
 registerSnapshotDefinition({
   plugin: plugins.aliCloudRocketMqGroup,
-  kpiDefinitions,
   metricDefinitions
 });
