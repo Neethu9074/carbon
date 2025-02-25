b660b929137ca71fd2a9a1eeebb440e53dce4e61
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-02-25 13:28:12 +0000

    Remove empty array KpiDefinitions file

--- packages/in-forge/plugins/availabilityZone/index.js
+++ packages/in-forge/plugins/availabilityZone/index.js
@@ -4,12 +4,10 @@
  */
 
 import metricDefinitions from 'in-forge/plugins/availabilityZone/metricDefinitions';
-import kpiDefinitions from 'in-forge/plugins/availabilityZone/kpiDefinitions';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
 import { plugins } from 'in-forge/constants';
 
 registerSnapshotDefinition({
   plugin: plugins.availabilityZone,
-  kpiDefinitions,
   metricDefinitions
 });
