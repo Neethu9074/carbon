df12b70e3c8100ff857019ca58eb7253e4dd8c92
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-02-25 13:17:13 +0000

    Remove empty array KpiDefinitions file

--- packages/in-forge/plugins/aceIntegrationNode/index.js
+++ packages/in-forge/plugins/aceIntegrationNode/index.js
@@ -4,7 +4,6 @@
  */
 
 import metricDefinitions from 'in-forge/plugins/aceIntegrationNode/metricDefinitions';
-import kpiDefinitions from 'in-forge/plugins/aceIntegrationNode/kpiDefinitions';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
 import { plugins } from 'in-forge/constants';
 import { t } from 'in-i18n';
@@ -12,7 +11,6 @@ import { t } from 'in-i18n';
 registerSnapshotDefinition({
   plugin: plugins.aceIntegrationNode,
 
-  kpiDefinitions,
   metricDefinitions,
   technologyDescriptor: {
     label: t('in-forge:tracingTypeName_ace')
