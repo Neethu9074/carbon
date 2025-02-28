170ee73b14b9370e51fd0331060505b5c55fa977
Jean Detoeuf <Jean.Detoeuf1@ibm.com>
2025-02-28 10:18:54 +0100

    INSTA-26948 show hierarchy using entity time instead of call start time (#19848)
    show hierarchy using entity time instead of call start time

--- packages/in-applications/analyze/components/TraceDetails/components/CallDetails/components/ServiceComponent.js
+++ packages/in-applications/analyze/components/TraceDetails/components/CallDetails/components/ServiceComponent.js
@@ -123,7 +123,7 @@ export default function ServiceComponent({ call, websiteBeacon, mobileAppBeacon 
                 snapshotId={destinationSnapshotId}
                 calculateHierarchy
                 pathname={physicalDashboardPath}
-                timeConfig={getResolvedTimeConfig(timeConfig, call.start)}
+                timeConfig={getResolvedTimeConfig(timeConfig, destinationEntity.time)}
               />
             )}
           </ExpandableGroup>
@@ -251,7 +251,7 @@ export default function ServiceComponent({ call, websiteBeacon, mobileAppBeacon 
                       entity={sourceEntity}
                       plugin={sourceEntity && sourceEntity.plugin}
                       physicalContext={sourcePhysicalContext}
-                      timeConfig={getResolvedTimeConfig(timeConfig, call.start)}
+                      timeConfig={getResolvedTimeConfig(timeConfig, sourceEntity.time)}
                     />
                   </ExpandableGroup>
                 )}
@@ -310,7 +310,7 @@ export default function ServiceComponent({ call, websiteBeacon, mobileAppBeacon 
                   entity={destinationEntity}
                   plugin={destinationEntity && destinationEntity.plugin}
                   physicalContext={destinationPhysicalContext}
-                  timeConfig={getResolvedTimeConfig(timeConfig, call.start)}
+                  timeConfig={getResolvedTimeConfig(timeConfig, destinationEntity.time)}
                 />
               )}
             </ExpandableGroup>
