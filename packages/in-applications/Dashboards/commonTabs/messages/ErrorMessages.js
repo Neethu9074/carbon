e4b5e632feb4b3cca4b0e6b232761a9ab1290801
Anju Shivan <Anju.Shivan@ibm.com>
2025-02-28 20:43:45 +0530

    Replace connectTo in Error messages-INSTA-10589 (#19849)
    Replace connectTo in Error messages

--- packages/in-applications/Dashboards/commonTabs/messages/ErrorMessages.js
+++ packages/in-applications/Dashboards/commonTabs/messages/ErrorMessages.js
@@ -6,30 +6,47 @@
 import React, { Fragment } from 'react';
 import { get } from 'lodash';
 
+import { useObservable } from '@instana/hooks';
+
 import ErrorMessagesTable from 'in-applications/Dashboards/commonTabs/messages/components/ErrorMessagesTable';
 import getEndpointInfo from 'in-applications/subscriptions/getEndpointInfo';
 import getServiceLabel from 'in-applications/subscriptions/getServiceLabel';
 import getApplication from 'in-applications/subscriptions/getApplication';
 import Footer from 'in-components/Footer/Footer';
-import connectTo from 'in-hoc/connectTo';
 
-export default connectTo(
-  props => ({
-    applicationName: props.applicationId ? getApplication({ id: props.applicationId }).map(getLabel) : null,
-    serviceName: props.serviceId ? getServiceLabel({ id: props.serviceId }).map(getLabel) : null,
-    endpointName: props.endpointId ? getEndpointInfo({ id: props.endpointId }).map(getLabel) : null
-  }),
-  function ErrorMessages({ boundaryScope: urlBoundaryScope, data: application, ...props }) {
-    const boundaryScope = urlBoundaryScope || application.boundaryScope;
+export default function ErrorMessages({
+  boundaryScope: urlBoundaryScope,
+  data: application,
+  applicationId,
+  serviceId,
+  endpointId,
+  timeConfig
+}) {
+  const applicationName = useObservable(applicationId ? getApplication({ id: applicationId }).map(getLabel) : null, [
+    applicationId
+  ]);
+  const serviceName = useObservable(serviceId ? getServiceLabel({ id: serviceId }).map(getLabel) : null, [serviceId]);
+  const endpointName = useObservable(endpointId ? getEndpointInfo({ id: endpointId }).map(getLabel) : null, [
+    endpointId
+  ]);
+  const boundaryScope = urlBoundaryScope || application.boundaryScope;
 
-    return (
-      <Fragment>
-        <ErrorMessagesTable boundaryScope={boundaryScope} {...props} />
-        <Footer />
-      </Fragment>
-    );
-  }
-);
+  return (
+    <Fragment>
+      <ErrorMessagesTable
+        applicationId={applicationId}
+        applicationName={applicationName}
+        boundaryScope={boundaryScope}
+        endpointName={endpointName}
+        endpointId={endpointId}
+        serviceId={serviceId}
+        serviceName={serviceName}
+        timeConfig={timeConfig}
+      />
+      <Footer />
+    </Fragment>
+  );
+}
 
 function getLabel(result) {
   return get(result, ['data', 'label'], null);
