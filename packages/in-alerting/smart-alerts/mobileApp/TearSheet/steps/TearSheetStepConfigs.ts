0fe0c15a2aba58be7a0ead0aa7891d5b626de9f6
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2025-03-05 19:18:29 +0530

    Mobile app full screen dilaog step4 - Alert properties (#19831)
    * Alert properties
    
    * type fix

--- packages/in-alerting/smart-alerts/mobileApp/TearSheet/steps/TearSheetStepConfigs.ts
+++ packages/in-alerting/smart-alerts/mobileApp/TearSheet/steps/TearSheetStepConfigs.ts
@@ -8,6 +8,7 @@
 import AlertConfigTearSheetStep3 from 'in-alerting/smart-alerts/mobileApp/TearSheet/steps/AlertConfigTearSheetStep3';
 import AlertConfigTearSheetStep1 from 'in-alerting/smart-alerts/mobileApp/TearSheet/steps/AlertConfigTearSheetStep1';
 import AlertConfigTearSheetStep2 from 'in-alerting/smart-alerts/mobileApp/TearSheet/steps/AlertConfigTearSheetStep2';
+import AlertConfigTearSheetStep4 from 'in-alerting/smart-alerts/mobileApp/TearSheet/steps/AlertConfigTearSheetStep4';
 import { t } from 'in-i18n';
 
 export const stepConfigsForCarbonTearSheet = [
@@ -29,5 +30,10 @@ export const stepConfigsForCarbonTearSheet = [
     title: t('in-alerting:smartAlerts.mobileApp.tearSheet.step3.title'),
     validateIntermediately: [],
     component: AlertConfigTearSheetStep3
+  },
+  {
+    title: t('in-alerting:smartAlerts.mobileApp.tearSheet.step4.title'),
+    validateIntermediately: [],
+    component: AlertConfigTearSheetStep4
   }
 ];
