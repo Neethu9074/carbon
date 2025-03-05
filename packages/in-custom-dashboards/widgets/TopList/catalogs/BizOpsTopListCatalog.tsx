55647e6a725694557865764239d954afcebc4b19
KYLE BLAUER <kgblauer@us.ibm.com>
2025-03-05 03:30:27 -0600

    [26334] Bizops custom dashboard add call type tag to activity list (#19834)
    * fix custom dashboard activity link
    
    * replace analyze link generation
    
    * add internal call type value

--- packages/in-custom-dashboards/widgets/TopList/catalogs/BizOpsTopListCatalog.tsx
+++ packages/in-custom-dashboards/widgets/TopList/catalogs/BizOpsTopListCatalog.tsx
@@ -6,18 +6,23 @@
 
 import React from 'react';
 
-import { BizOpsUnifiedMetricConfiguration } from '@instana/types';
+import { BizOpsUnifiedMetricConfiguration, TagFilterExpressionElementUnion } from '@instana/types';
 
 // @ts-expect-error Need to translate module to TS
 import { ListWidgetRenderer } from 'in-custom-dashboards/widgets/TopList/Widget';
 import { getTagCatalog as getBizOpsTagCatalog } from 'in-bizops/lists/businessPerspectives/components/BusinessProcessQueryBuilder';
 import useTopListResultData from 'in-custom-dashboards/widgets/TopList/useTopListResultData';
+import { NOT_APPLICABLE } from 'in-components/QueryBuilder/tagFilter/entities';
 import { WidgetProps } from 'in-custom-dashboards/widgets/types';
 import useTagCatalog from 'in-applications/hooks/useTagCatalog';
 import { pendingResult } from 'in-services/fixedObjects';
 
 interface Config {
   metricConfiguration: BizOpsUnifiedMetricConfiguration;
+}
+
+interface CallTypeProps {
+  tagFilterExpression: TagFilterExpressionElementUnion | undefined;
 }
 
 const BizOpsTopListCatalog = ({
@@ -29,6 +34,31 @@ const BizOpsTopListCatalog = ({
   filterResult
 }: WidgetProps<Config>) => {
   const result = useTopListResultData(config, timeConfig) ?? pendingResult;
+
+  const callTypeExpression: CallTypeProps = {
+    tagFilterExpression: {
+      elements: [
+        {
+          name: 'call.type',
+          operator: 'EQUALS',
+          value: 'BATCH',
+          entity: NOT_APPLICABLE,
+          type: 'TAG_FILTER'
+        },
+        {
+          name: 'call.type',
+          operator: 'EQUALS',
+          value: 'INTERNAL',
+          entity: NOT_APPLICABLE,
+          type: 'TAG_FILTER'
+        }
+      ],
+      logicalOperator: 'OR',
+      type: 'EXPRESSION'
+    }
+  };
+
+  config.metricConfiguration = { ...config.metricConfiguration, ...callTypeExpression };
 
   return (
     <ListWidgetRenderer
