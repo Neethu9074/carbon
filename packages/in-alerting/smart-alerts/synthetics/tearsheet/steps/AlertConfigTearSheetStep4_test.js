7029c3dc0252345a2807a06a6ff78734c20e0872
Pradosh E <Pradosh.E@ibm.com>
2025-03-06 13:21:16 +0530

    Adding unit test coverage for tearsheet step component in synthetics. (#19739)
    adding unit test coverage for tearsheet step component in synthetics.

---
+++ packages/in-alerting/smart-alerts/synthetics/tearsheet/steps/AlertConfigTearSheetStep4_test.js
@@ -0,0 +1,28 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2025
+ */
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import AlertConfigTearSheetStep4 from 'in-alerting/smart-alerts/synthetics/tearsheet/steps/AlertConfigTearSheetStep4';
+import alertFormDefinition from 'in-alerting/smart-alerts/synthetics/form/alertDialogFormDefinition';
+import { initialConfig } from 'in-alerting/smart-alerts/synthetics/data/alertConfig.json';
+import { t } from 'in-i18n';
+
+describe('AlertConfigTearSheetStep4 : in-alerting/smart-alerts/infrastructure/tearsheet/steps/AlertConfigTearSheetStep4', () => {
+  const onChange = jest.fn();
+  const form = alertFormDefinition(initialConfig);
+
+  it('should render Alert channel section', async () => {
+    render(<AlertConfigTearSheetStep4 form={form} onChange={onChange} />);
+    expect(screen.getByText(t('in-alerting:smartAlerts.synthetics.tearSheet.step4.header'))).toBeInTheDocument();
+    expect(screen.getByText(t('in-alerting:smartAlerts.synthetics.tearSheet.step4.description'))).toBeInTheDocument();
+    const createBtn = screen.getByRole('button', {
+      name: t('in-alerting:smartAlerts.components.smartAlertDialog.createAlertChannelTitle')
+    });
+    expect(createBtn).toBeTruthy();
+  });
+});
