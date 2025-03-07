fdbee50a9f11c6d7f9ab6b11e330c5a36613115d
Anju Shivan <Anju.Shivan@ibm.com>
2025-03-07 16:30:40 +0530

    INSTA-10589-Replace connectTo in metrics.js (#19939)
    Replace connectTo in metrics.js

--- packages/in-applications/FlowMap/components/Node/components/Metrics.js
+++ packages/in-applications/FlowMap/components/Node/components/Metrics.js
@@ -6,24 +6,20 @@
 import classNames from 'classnames';
 import React from 'react';
 
+import { useObservable } from '@instana/hooks';
+
 import { number, meanLatency, percentage } from 'in-services/formatters/number';
-import connectTo from 'in-hoc/connectTo';
 
 import locals from './Metrics.mless';
 
-export default connectTo(
-  props => ({
-    metrics: props.entity.events$.on('metricValues')
-  }),
-  function Metrics(props) {
-    const { className, metrics = {} } = props;
-
-    return (
-      <div className={classNames(locals.metrics, className)}>
-        <span>{metrics.calls != undefined ? number.compact(metrics.calls) : '--'}</span>
-        <span>{metrics.latency != undefined ? meanLatency.detailed(metrics.latency) : '--'}</span>
-        <span>{metrics.errors != undefined ? percentage.detailed(metrics.errors) : '--'}</span>
-      </div>
-    );
-  }
-);
+export default function Metrics(props) {
+  const { className, entity } = props;
+  const metrics = useObservable(entity.events$.on('metricValues'), [entity]) ?? {};
+  return (
+    <div className={classNames(locals.metrics, className)}>
+      <span>{metrics.calls != undefined ? number.compact(metrics.calls) : '--'}</span>
+      <span>{metrics.latency != undefined ? meanLatency.detailed(metrics.latency) : '--'}</span>
+      <span>{metrics.errors != undefined ? percentage.detailed(metrics.errors) : '--'}</span>
+    </div>
+  );
+}
