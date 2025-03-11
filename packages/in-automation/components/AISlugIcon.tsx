2b6ddacb4c6fd76c1d5f13e8d3f15fcbe7e73759
KAVYA NEKKALAPU <knekkal@us.ibm.com>
2025-03-11 10:36:46 -0400

    push fixes

--- packages/in-automation/components/AISlugIcon.tsx
+++ packages/in-automation/components/AISlugIcon.tsx
@@ -4,36 +4,6 @@
  * Copyright IBM Corp. 2024
  */
 
-// import React from 'react';
-
-// import { Tooltip as CarbonTooltip } from '@instana/components';
-
-// import { t } from 'in-i18n';
-
-// export default function AISlugIcon() {
-//   return (
-//     <CarbonTooltip content={t('in-automation:AISlugContent')}>
-//       <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <g id="AI slug">
-//           <rect x="0.5" y="0.5" width="31" height="31" fill="white" fillOpacity="0.01" />
-//           <rect x="0.5" y="0.5" width="31" height="31" stroke="#161616" />
-//           <path
-//             id="AI"
-//             d="M17.6582 21H15.4662L14.5702 18.152H10.6022L9.72219 21H7.57819L11.3222 9.832H13.9462L17.6582 21ZM14.0582 16.344L12.6182 11.752H12.5382L11.1142 16.344H14.0582ZM23.9122 21H18.8402V19.304H20.3122V11.528H18.8402V9.832H23.9122V11.528H22.4242V19.304H23.9122V21Z"
-//             fill="#161616"
-//           />
-//         </g>
-//       </svg>
-//     </CarbonTooltip>
-//   );
-// }
-
-/*
- * IBM Confidential
- * PID 5737-N85, 5900-AG5
- * Copyright IBM Corp. 2024
- */
-
 import React, { useState } from 'react';
 
 import { SvgIcon, CarbonPopover, CarbonPopoverContent, IconButton, Link } from '@instana/components';
@@ -44,10 +14,16 @@ import locals from './AISlugIcon.mless';
 
 // Basic function for the rendering of the AI Popover and its inner contents
 // Each AI Popover holds his own open state so they are independent of each other
-export default function AISlugIcon({ actionType }: { actionType: 'manual' | 'script' }) {
+export default function AISlugIcon({
+  actionType,
+  align = 'right-start'
+}: {
+  actionType: 'manual' | 'script' | 'aiGenerated';
+  align?: 'right-start' | 'left-start';
+}) {
   const [showPop, setShowPop] = useState(false);
   return (
-    <CarbonPopover open={showPop} align={'right-start'} className={locals.blueArrow} caret>
+    <CarbonPopover open={showPop} align={align} className={locals.blueArrow} caret>
       <SvgIcon
         type={'lib_ai_slug'}
         size="s"
@@ -68,7 +44,9 @@ export default function AISlugIcon({ actionType }: { actionType: 'manual' | 'scr
             }}
           />
         </div>
-        {actionType === 'manual' ? <AIExplainedContentManualAction /> : <AIExplainedContentScriptAction />}
+        {actionType === 'manual' && <AIExplainedContentManualAction />}
+        {actionType === 'script' && <AIExplainedContentScriptAction />}
+        {actionType === 'aiGenerated' && <AIExplainedContentPreGeneratedActions />}
       </CarbonPopoverContent>
     </CarbonPopover>
   );
@@ -90,19 +68,19 @@ function AIExplainedContentManualAction() {
         </div>
         <div className={locals.bullet}>
           {'- '}
-          <div style={{ paddingLeft: '.5rem' }}>
+          <div>
             <Trans i18nKey={t('in-automation:AITooltip.manualSection.section2.section2EventName')} />
           </div>
         </div>
         <div className={locals.bullet}>
           {'- '}
-          <div style={{ paddingLeft: '.5rem' }}>
+          <div>
             <Trans i18nKey={t('in-automation:AITooltip.manualSection.section2.section2Description')} />
           </div>
         </div>
         <div className={locals.bullet}>
           {'- '}
-          <div style={{ paddingLeft: '.5rem' }}>
+          <div>
             <Trans i18nKey={t('in-automation:AITooltip.manualSection.section2.section2Entitytype')} />
           </div>
         </div>
@@ -144,6 +122,18 @@ function AIExplainedContentScriptAction() {
       </div>
       <div className={locals.aimodellink}>
         <div>{t('in-automation:AITooltip.scriptSection.section3.section3Text')}</div>
+      </div>
+    </div>
+  );
+}
+
+function AIExplainedContentPreGeneratedActions() {
+  return (
+    <div className={locals.popOverWrapper}>
+      <div className={locals.popupScriptDescription}>
+        {t('in-automation:AITooltip.aiExplained')}
+        <div className={locals.popSumTitle}> {t('in-automation:AITooltip.preGenerated.title')}</div>
+        <div>{t('in-automation:AITooltip.preGenerated.text')}</div>
       </div>
     </div>
   );
