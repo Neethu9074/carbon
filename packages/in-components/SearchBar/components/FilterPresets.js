73abff4106288eb4381d094b1419876712e95741
Jason McGuirk <Jason.McGuirk@ibm.com>
2025-03-17 18:33:38 +0000

    refactoring filterPresets.js -  INSTA-12125 (#17365)
    refactoring filterPresets.js

--- packages/in-components/SearchBar/components/FilterPresets.js
+++ packages/in-components/SearchBar/components/FilterPresets.js
@@ -3,7 +3,9 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import { useObservable } from '@instana/hooks';
 
 import { filters$, refresh, remove } from 'in-components/SearchBar/stores/filters';
 import { togglePresets } from 'in-components/SearchBar/stores/presetsVisibility';
@@ -13,7 +15,6 @@ import SaveDialog from 'in-components/SearchBar/components/SaveDialog';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import throttleNextFrame from 'in-services/util/throttleNextFrame';
 import { setValues } from 'in-components/SearchBar/stores/dialog';
-import connectTo from 'in-hoc/connectTo';
 import { t } from 'in-i18n';
 
 import './FilterPresets.less';
@@ -21,95 +22,82 @@ import './FilterPresets.less';
 const block = 'in-search-presets';
 const wgtDropdown = 'widget-dropdown-position';
 
-export default connectTo(
-  {
-    filters: filters$
-  },
-  class extends React.Component {
-    static displayName = 'FilterPresets';
-
-    componentDidMount() {
-      this.onMouseUp = throttleNextFrame(this.onMouseUp);
-
-      // Delay listener registration. If we would do this synchronously,
-      // we would receive the click event which opened this dialog and
-      // the dialog would be immediately closed.
-      setTimeout(() => {
-        window.addEventListener('click', this.onMouseUp, false);
-        this.registered = true;
-      }, 0);
-
-      refresh();
+export default function FilterPresets(props) {
+  const { manageFiltersDisabled, setFilter } = props;
+  const filters = useObservable(filters$, []);
+  const menu = useRef();
+  const onMouseUp = e => {
+    if (!menu.current) {
+      return;
     }
 
-    componentWillUnmount() {
-      window.removeEventListener('click', this.onMouseUp, false);
+    const rect = menu.current.getBoundingClientRect();
+    if (e.clientX > rect.right || e.clientX < rect.left || e.clientY < rect.top || e.clientY > rect.bottom) {
+      // the click was done outside this component so close it
+      togglePresets();
     }
+  };
+  useEffect(() => {
+    const onMouseUpEvent = throttleNextFrame(onMouseUp);
 
-    render() {
-      const { filters, manageFiltersDisabled, setFilter } = this.props;
-      return (
-        <section
-          className={!manageFiltersDisabled ? block : `${block} ${wgtDropdown}`}
-          ref={menu => (this.menu = menu)}
-        >
-          <MenuHeading className={`${block}__heading`}>
-            {t('in-components:searchBar.filterPresetsMenuHeading')}
-          </MenuHeading>
-          <ul className={`${block}__preset-list`}>
-            {filters.size === 0 ? (
-              <span className={`${block}__no-filters-help-text`}>
-                {t('in-components:searchBar.filterPresetsSaveFilters')}
-              </span>
-            ) : null}
-            {filters.toArray().map(filter => (
-              <li key={filter.get('id')} className={`${block}__preset-item`}>
-                <UserFilterLink onClick={togglePresets} filter={filter} setFilter={setFilter} />
+    // Delay listener registration. If we would do this synchronously,
+    // we would receive the click event which opened this dialog and
+    // the dialog would be immediately closed.
+    setTimeout(() => {
+      window.addEventListener('click', onMouseUpEvent, false);
+    }, 0);
 
-                {!manageFiltersDisabled && (
-                  <div className={`${block}__item-actions`}>
-                    <a
-                      href=""
-                      onClick={e => {
-                        e.preventDefault();
-                        edit(filter);
-                      }}
-                      className={`${block}__edit`}
-                    >
-                      {t('in-components:searchBar.filterPresetsEditFilter')}
-                    </a>
-                    <a
-                      href=""
-                      onClick={e => {
-                        e.preventDefault();
-                        remove(filter.get('id'), filter.get('name'), filter.get('definition'));
-                      }}
-                      className={`${block}__remove`}
-                    >
-                      {t('in-components:searchBar.filterPresetsRemoveFilter')}
-                    </a>
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </section>
-      );
-    }
+    refresh();
 
-    onMouseUp = e => {
-      if (!this.menu) {
-        return;
-      }
-
-      const rect = this.menu.getBoundingClientRect();
-      if (e.clientX > rect.right || e.clientX < rect.left || e.clientY < rect.top || e.clientY > rect.bottom) {
-        // the click was done outside this component so close it
-        togglePresets();
-      }
+    return () => {
+      window.removeEventListener('click', onMouseUpEvent, false);
     };
-  }
-);
+  }, []);
+
+  return (
+    <section className={!manageFiltersDisabled ? block : `${block} ${wgtDropdown}`} ref={menu}>
+      <MenuHeading className={`${block}__heading`}>{t('in-components:searchBar.filterPresetsMenuHeading')}</MenuHeading>
+      <ul className={`${block}__preset-list`}>
+        {filters && filters.size === 0 ? (
+          <span className={`${block}__no-filters-help-text`}>
+            {t('in-components:searchBar.filterPresetsSaveFilters')}
+          </span>
+        ) : null}
+        {filters &&
+          filters.toArray().map(filter => (
+            <li key={filter.get('id')} className={`${block}__preset-item`}>
+              <UserFilterLink onClick={togglePresets} filter={filter} setFilter={setFilter} />
+
+              {!manageFiltersDisabled && (
+                <div className={`${block}__item-actions`}>
+                  <a
+                    href=""
+                    onClick={e => {
+                      e.preventDefault();
+                      edit(filter);
+                    }}
+                    className={`${block}__edit`}
+                  >
+                    {t('in-components:searchBar.filterPresetsEditFilter')}
+                  </a>
+                  <a
+                    href=""
+                    onClick={e => {
+                      e.preventDefault();
+                      remove(filter.get('id'), filter.get('name'), filter.get('definition'));
+                    }}
+                    className={`${block}__remove`}
+                  >
+                    {t('in-components:searchBar.filterPresetsRemoveFilter')}
+                  </a>
+                </div>
+              )}
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
 
 function edit(filter) {
   setValues(filter.get('id'), filter.get('name'), filter.get('definition'));
