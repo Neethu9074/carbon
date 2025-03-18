0c6540bca5c998883914f6af7c5a8f03b717fe19
Bryson Chisholm <bryson.chisholm@ibm.com>
2025-03-18 13:29:25 -0400

    pass aggregation to slo backend when fetch latency chart and change formatter for latency chart to prevent rounding out important info

--- packages/in-service-levels/components/SloDashboard/components/chart/IndicatorChart/components/TimeBasedLatencyIndicatorChart.tsx
+++ packages/in-service-levels/components/SloDashboard/components/chart/IndicatorChart/components/TimeBasedLatencyIndicatorChart.tsx
@@ -60,12 +60,12 @@ export default function TimeBasedLatencyIndicatorChart({
   title,
   configuration
 }: TimeBasedLatencyIndicatorChartProps) {
-  const { threshold } = indicator;
+  const { threshold, aggregation } = indicator;
   const sloZoomInAction = useSloZoomInAction();
   const { timeWindows, timeWindowColors } = useSloTimeWindowContext();
   const timeConfig = useContextAwareSloTimeWindowConfig();
   const granularity = calculateSloGranularity(timeConfig);
-  const result = useTimeBasedIndicatorMetrics({ configuration, granularity, timeWindows });
+  const result = useTimeBasedIndicatorMetrics({ configuration, granularity, timeWindows, aggregation });
 
   const metrics = result.data?.filter(r => r.id.startsWith('timeWindow')) ?? [];
   const metricValues = copyFirstBucketOfSubsequentDataSeries(metrics.map(metric => metric.values as MetricDataSeries));
@@ -104,7 +104,7 @@ export default function TimeBasedLatencyIndicatorChart({
           metrics: [...filteredData.slice(timeWindowStartIndex), thresholdMetrics],
           labels: [...timeWindowsWithData.map(() => metricLabel), t('in-service-levels:general.metrics.threshold')],
           colors: [...windowColorsWithData, themes.default.ids.color.option.red['500']],
-          formatter: millis.compact,
+          formatter: millis.detailed,
           renderer
         },
         timeConfig,
