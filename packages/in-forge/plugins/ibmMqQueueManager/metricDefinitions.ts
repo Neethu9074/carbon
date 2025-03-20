942d77903b8ff5d543eebbf346ba81610f3b2d33
Geetha Byranna <geetha.byranna@ibm.com>
2025-03-20 16:25:45 +0530

    INSTA-31414: Updated the IBMMQ cluster status metric

--- packages/in-forge/plugins/ibmMqQueueManager/metricDefinitions.ts
+++ packages/in-forge/plugins/ibmMqQueueManager/metricDefinitions.ts
@@ -48,5 +48,17 @@ export default [
     labels: [t('in-forge:plugins.ibmMqQueueManager.uncommittedMessages')],
     min: 0,
     formatter: number
+  },
+  {
+    metrics: ['clusterMetricStatus'],
+    labels: [t('in-forge:plugins.ibmMqQueueManager.clusterMetric')],
+    min: 0,
+    formatter: number
+  },
+  {
+    metrics: ['clusterSuspendIndicatorStatus'],
+    labels: [t('in-forge:plugins.ibmMqQueueManager.clusterSuspendIndicator')],
+    min: 0,
+    formatter: number
   }
 ];
