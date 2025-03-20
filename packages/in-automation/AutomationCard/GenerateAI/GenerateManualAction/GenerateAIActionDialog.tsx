18d3cd24d467f2aec1024078a5c76f7d8241d3e7
Kavya Nekkalapu <knekkal@us.ibm.com>
2025-03-20 04:54:10 -0400

    Fix ai generation dialog flag (#20166)
    * Fix ai generation dialog
    
    * Fix tests

--- packages/in-automation/AutomationCard/GenerateAI/GenerateManualAction/GenerateAIActionDialog.tsx
+++ packages/in-automation/AutomationCard/GenerateAI/GenerateManualAction/GenerateAIActionDialog.tsx
@@ -31,7 +31,6 @@ import { getActionNameExists, saveNewAction, saveNewPolicy } from 'in-automation
 import { setActiveKey } from 'in-automation/AutomationCard/AutomationCardButtonGroup';
 import { refresh as refreshPolicies } from 'in-automation/AutomationCard/usePolicies';
 import LeftRightPadding from 'in-components/layout/LeftRightPadding/LeftRightPadding';
-import { automationActionAiGenerationUnitEnabled } from 'in-services/featureFlags';
 import { AIActionContent } from 'in-automation/subscriptions/generateAIAction';
 import { StepConfigs } from 'in-components/BlueprintFormMultistep/StepConfigs';
 import DialogWithSlideInView from 'in-components/Dialog/DialogWithSlideInView';
@@ -58,9 +57,7 @@ function getStepConfigs(hasOotbActions: boolean) {
   const actionSteps: StepConfigs = [
     {
       title: hasOotbActions
-        ? automationActionAiGenerationUnitEnabled
-          ? t('in-automation:GenerateAIActionDialog.step1Title')
-          : t('in-automation:GenerateAIActionDialog.step1TitleSelectAction')
+        ? t('in-automation:GenerateAIActionDialog.step1Title')
         : t('in-automation:GenerateAIActionDialog.step1TitleGenerateAction')
     },
     {
