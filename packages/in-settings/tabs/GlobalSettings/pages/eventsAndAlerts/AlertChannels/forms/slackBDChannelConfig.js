6b003b9b29d618e9637f983a5c476a592addebcf
Josh King <jdking@us.ibm.com>
2025-03-25 11:36:46 -0500

    feat(slackapp): update url

--- packages/in-settings/tabs/GlobalSettings/pages/eventsAndAlerts/AlertChannels/forms/slackBDChannelConfig.js
+++ packages/in-settings/tabs/GlobalSettings/pages/eventsAndAlerts/AlertChannels/forms/slackBDChannelConfig.js
@@ -213,16 +213,15 @@ function Form({ form, onChange }) {
               });
               const id = form.get('id') ? form.get('id').value : generateUniqueShortId();
               const integrationBaseUrl = window.instana.config.integrationBaseUrl;
+              const butlerDomain = `https://${window.instana.config.butlerDomain}`;
               const params = new URLSearchParams({
-                endpoint: integrationBaseUrl,
+                endpoint: butlerDomain,
                 tenant: window.instana.config.tenant,
                 unit: window.instana.config.tenantUnit,
                 id: id,
                 name: form.get('name').value
               });
-
               const url = `${integrationBaseUrl}/integration/slack/bidirectional/install?${params.toString()}`;
-
               window.open(url, '_blank');
             }}
           >
