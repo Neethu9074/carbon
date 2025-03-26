e65383f3fe460b5b9eec6955c7beb09293f21b8e
Aleix Costa <acostare@ibm.com>
2025-03-26 10:10:58 +0000

    feat: improvement in Got To Logs button (#20198)
    * feat: improvement in Got To Logs button
    
    * fix: key error mesage
    
    * fix: remove unused prop
    
    * fix: remove link and add style
    
    * feat: make the button dinamic according to logs
    
    * fix: analyzeLogsButton
    
    ---------
    
    Co-authored-by: Luka Martinovic <Luka.Martinovic@ibm.com>

--- packages/in-forge/plugins/containerd/index.js
+++ packages/in-forge/plugins/containerd/index.js
@@ -16,6 +16,7 @@ import { isWithinKubernetes } from 'in-forge/plugins/containerd/util';
 import { containerInfoEnabled } from 'in-services/featureFlags';
 import { hasInfrastructureAccess } from 'in-stores/permission';
 import { registerSnapshotDefinition } from 'in-sdk/snapshot';
+import { CONTAINERD_ID } from 'in-logging/queryBuilder';
 import { plugins } from 'in-forge/constants';
 
 registerSnapshotDefinition({
@@ -34,7 +35,13 @@ registerSnapshotDefinition({
     return [
       {
         getObservables,
-        render: props => <LoggingIntegrationButtonsRenderer {...props} addMargin />,
+        render: props => (
+          <LoggingIntegrationButtonsRenderer
+            {...props}
+            tagFilter={{ name: CONTAINERD_ID, value: props.dockerContainerId }}
+            addMargin
+          />
+        ),
         props: {
           isWithinKubernetes: isWithinKubernetes(snapshot),
           dockerContainerId: snapshot.getIn(['data', 'id']),
---
+++ packages/in-integrations/logging/analyzeLogs/analyzeLogsButton.tsx
@@ -0,0 +1,48 @@
+/*
+ * (c) Copyright IBM Corp. 2025
+ * (c) Copyright Instana Inc.
+ */
+
+import React from 'react';
+
+import { CarbonMenuItem } from '@instana/components';
+
+import { getEmptyTagFilterExpression } from 'in-components/QueryBuilder/tagFilter/emptyTagFilterExpression';
+import { toBackendQueryModel } from 'in-components/QueryBuilder/transformation/backendQueryModel';
+import { getValueMatchTagFilter, ReducedTagFilterWithDefaults } from 'in-logging/queryBuilder';
+import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
+import { useLinkToLogs } from 'in-logging/navigation/paths';
+import { TagFilterExpression, TimeConfig } from 'in-types';
+import { useHasLogs } from 'in-logging/hooks';
+import { t } from 'in-i18n';
+
+import locals from 'in-integrations/logging/analyzeLogs/analyzeLogsButton.mless';
+
+interface AnalyzeLogsButtonProps {
+  timeConfig: TimeConfig;
+  tagFilter?: ReducedTagFilterWithDefaults;
+}
+
+export default function AnalyzeLogsButton({ timeConfig, tagFilter }: AnalyzeLogsButtonProps) {
+  const { goToPath } = useNavigation();
+
+  let tagFilterExpression: TagFilterExpression = getEmptyTagFilterExpression();
+
+  if (tagFilter) {
+    tagFilterExpression =
+      tagFilter && (toBackendQueryModel([getValueMatchTagFilter(tagFilter)]) as TagFilterExpression);
+  }
+
+  const hasLogs = useHasLogs({ tagFilterExpression, timeConfig });
+
+  const hostRef = useLinkToLogs({ tagFilterExpression });
+
+  return (
+    <>
+      {hasLogs && (
+        <CarbonMenuItem label={t('in-integrations:logging.analyzeLogs')} onClick={() => goToPath(hostRef.slice(2))} />
+      )}
+      <li className={locals.integrations}>{t('in-integrations:logging.integrations')}</li>
+    </>
+  );
+}
