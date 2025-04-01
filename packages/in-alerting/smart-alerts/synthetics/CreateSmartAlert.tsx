93fdd497ffcb176fd7de3d4d6697216ed123dd02
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2025-04-01 22:49:18 +0530

    added chooser dialog to choose between old and new full screen dialog in synthetic SA for create option (#20214)
    choose diloag for create btn

--- packages/in-alerting/smart-alerts/synthetics/CreateSmartAlert.tsx
+++ packages/in-alerting/smart-alerts/synthetics/CreateSmartAlert.tsx
@@ -7,7 +7,6 @@
 import React from 'react';
 
 import { Button } from '@instana/components';
-import { Stack } from '@instana/components';
 
 import {
   syntheticSmartAlertFullScreenDesignEnabled,
@@ -15,14 +14,13 @@ import {
   smartAlertCarbonTableEnabled
 } from 'in-services/featureFlags';
 import { useSmartAlertCreateUrl } from 'in-alerting/smart-alerts/synthetics/hooks/useSmartAlertCreateUrl';
-import FloatingActionButtonMenu from 'in-components/FloatingActionButton/FloatingActionButtonMenu';
 import CreateSmartAlertDialog from 'in-alerting/smart-alerts/synthetics/CreateSmartAlertDialog';
 import { getSmartAlertDisplayMode } from 'in-alerting/smart-alerts/utils/smartAlertViewUtils';
-import FloatingActionButtons from 'in-components/FloatingActionButton/FloatingActionButtons';
 import { FULLSCREEN, SIMPLE, CHOICE_DIALOG } from 'in-alerting/smart-alerts/data/constants';
 import ViewSelectorDialog from 'in-alerting/components/Dialog/ViewSelectorDialog';
 import { useSegmentTracking } from 'in-services/tracking/useSegmentTracking';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
+import { ADVANCED } from 'in-alerting/smart-alerts/data/constants';
 import { ALERTING_CREATE } from 'in-services/tracking/eventNames';
 import { t } from 'in-i18n';
 
@@ -31,7 +29,6 @@ import locals from 'in-alerting/smart-alerts/synthetics/CreateSmartAlert.mless';
 export interface CreateSmartAlertProps {
   testId?: string;
   isCarbonTableView?: boolean;
-  withOutFloatingBtnMenu?: boolean;
 }
 
 const alertDisplayMode = getSmartAlertDisplayMode(
@@ -39,47 +36,36 @@ const alertDisplayMode = getSmartAlertDisplayMode(
   syntheticSmartAlertFullScreenDesignEnabled
 );
 
-export default function CreateSmartAlert({ testId, withOutFloatingBtnMenu = false }: CreateSmartAlertProps) {
+export default function CreateSmartAlert({ testId }: CreateSmartAlertProps) {
   const { trackCta } = useSegmentTracking();
-  const labelNew = t('in-alerting:smartAlerts.labelNew');
   const getLinkToCreateSmartAlert = useSmartAlertCreateUrl({});
-  const handleButtonClick = () => {
-    trackCta(ALERTING_CREATE, { dialogMode: SIMPLE });
+
+  const openCreateSmartAlertDialog = () => {
     addActiveDialog(<CreateSmartAlertDialog testId={testId} />);
   };
 
+  const handleButtonClick = () => {
+    trackCta(ALERTING_CREATE, { dialogMode: SIMPLE });
+    openCreateSmartAlertDialog();
+  };
+
   if (syntheticSmartAlertFullScreenDesignEnabled) {
-    if (withOutFloatingBtnMenu) {
-      return (
-        <Stack align="end">
-          <Button icon="lib_alerts_create" onClick={() => handleButtonClick()}>
-            {t('in-alerting:smartAlerts.addSmartAlert')}
-          </Button>
-          <Button
-            icon="lib_alerts_create"
-            href={getLinkToCreateSmartAlert}
-            onClick={() => trackCta(ALERTING_CREATE, { dialogMode: FULLSCREEN })}
-          >
-            {`${t('in-alerting:smartAlerts.addSmartAlert')} ${labelNew}`}
-          </Button>
-        </Stack>
-      );
-    }
     return (
-      <FloatingActionButtons>
-        <FloatingActionButtonMenu>
-          <Button icon="lib_alerts_create" onClick={() => handleButtonClick()}>
-            {t('in-alerting:smartAlerts.addSmartAlert')}
-          </Button>
-          <Button
-            icon="lib_alerts_create"
-            href={getLinkToCreateSmartAlert}
-            onClick={() => trackCta(ALERTING_CREATE, { dialogMode: FULLSCREEN })}
-          >
-            {`${t('in-alerting:smartAlerts.addSmartAlert')} ${labelNew}`}
-          </Button>
-        </FloatingActionButtonMenu>
-      </FloatingActionButtons>
+      <Button
+        icon="lib_alerts_create"
+        onClick={() =>
+          addActiveDialog(
+            <ViewSelectorDialog
+              trackCta={trackCta}
+              openOldDialog={openCreateSmartAlertDialog}
+              getLinkToCreateSmartAlert={getLinkToCreateSmartAlert}
+              mode={ADVANCED}
+            />
+          )
+        }
+      >
+        {t('in-alerting:smartAlerts.synthetics.addSmartAlert')}
+      </Button>
     );
   }
 
