feaa322d16d8762db3b1b409e5ea1f6373857345
Nick Vargas <navargas@us.ibm.com>
2025-04-02 13:21:39 -0700

    Enable incident summarization feature flag (#20389)

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -549,7 +549,7 @@ module.exports = exports = [
   {
     uiClientKey: 'incidentSummarizationEnabled',
     instanaCtlKey: 'feature.incident.ai.summarization.enabled',
-    defaultValue: false
+    defaultValue: true
   },
   {
     uiClientKey: 'eventsAIChatEnabled',
