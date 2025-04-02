74dc4a2520970912070f2be5a59702585efea175
Josh King <jdking@us.ibm.com>
2025-04-02 11:23:53 -0500

    feat(slackapp): turn on slack application

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -664,7 +664,7 @@ module.exports = exports = [
   {
     uiClientKey: 'bidirectionalSlackEnabled',
     instanaCtlKey: 'feature.alert.channel.bidirectional.slack.enabled',
-    defaultValue: false
+    defaultValue: true
   },
   {
     uiClientKey: 'analyzeRelatedInstancesButtonEnabled',
