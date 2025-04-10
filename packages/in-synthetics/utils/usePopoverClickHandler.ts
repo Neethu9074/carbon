769029bf68e31cf0c7cee5af1be8e71da2731cff
Sandra S <Sandra.S@ibm.com>
2025-04-10 18:11:00 +0530

    INSTA-33348: Improving Results List page behaviour during DNS test (#20468)

---
+++ packages/in-synthetics/utils/usePopoverClickHandler.ts
@@ -0,0 +1,49 @@
+/*
+ * IBM Confidential
+ * PID 5737-N85, 5900-AG5
+ * Copyright IBM Corp. 2025
+ */
+
+import { useEffect, useCallback, useState, useRef } from 'react';
+
+/**
+ * Handler to deal with clicks outside or inside the component
+ * Can handle problems with stoppropagation and still
+ * close the popover on clicks outside
+ * @returns {{open: boolean, toggle: Callback, ref: HTMLDivElement}}
+ */
+export default function usePopoverClickHandler() {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const toggle = () => {
+    const isOpen = open;
+    setOpen(!isOpen);
+  };
+
+  const callbackToggle = useCallback(toggle, [open]);
+
+  const clickOutside = (event: Event) => {
+    if (open) {
+      // if click outside - close
+      const inside = ref.current?.contains(event?.target as Node);
+      if (!inside) callbackToggle();
+    }
+  };
+
+  const callbackClickOutside = useCallback(clickOutside, [ref, callbackToggle, open]);
+
+  useEffect(() => {
+    const handler = (event: Event) => {
+      callbackClickOutside(event);
+    };
+
+    window.addEventListener?.('click', handler, true);
+
+    return () => {
+      window.removeEventListener?.('click', handler);
+    };
+  }, [callbackClickOutside]);
+
+  return { open, toggle, ref };
+}
