64bd88e0a82ad7daa08782f656a04eec6c491cc6
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-04-30 15:55:09 +0100

    chore(TS migration): pre-migrate packages/in-components/time/TimeSelectionDialogPresenter/convertToScopes_test

--- packages/in-components/time/TimeSelectionDialogPresenter/convertToScopes_test.js
+++ packages/in-components/time/TimeSelectionDialogPresenter/convertToScopes_test.js
@@ -5,6 +5,7 @@
 
 import { expect } from 'chai';
 
+// @ts-expect-error import convertToScopes from 'in-components/time/TimeSelectionDialogPresenter/convertToScopes';
 import convertToScopes from 'in-components/time/TimeSelectionDialogPresenter/convertToScopes';
 
 describe('in-components/time/convertToScopes', () => {
