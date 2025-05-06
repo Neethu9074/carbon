82ee352f5c6dea1247104b75fe134dd48f4f314c
Rosemaria Antony <Rosemaria.Antony@ibm.com>
2025-05-06 10:15:38 +0530

    show team focus dropdown (#20714)
    * show team focus dropdown
    
    * add translation
    
    * format changes

--- packages/in-api/teams.ts
+++ packages/in-api/teams.ts
@@ -4,13 +4,16 @@
  * Copyright IBM Corp. 2025
  */
 
+import { Result, TeamOverview, TeamTag } from '@instana/types';
 import { Observable, create } from '@instana/observables';
-import { Result, TeamOverview } from '@instana/types';
 
+import { getHeader as getCsrfHeader } from 'in-services/security/csrf';
 import memoize from 'in-services/util/memoizingObservableGenerator';
 import http from 'in-services/http';
 
 export const refreshSignal = create<string | string[]>().emit('');
+
+const teamFocusUrl = 'api/settings/rbac/teams/focus';
 
 function getTeamsOverviewInternal(): Observable<Result<TeamOverview[]>> {
   return http<TeamOverview[]>({
@@ -27,3 +30,31 @@ export const getTeamsOverview = memoize(
   () => 'Teams',
   60000
 );
+
+export function getTeamsByUserId(): Observable<Result<TeamTag[]>> {
+  return http<TeamTag[]>({
+    method: 'GET',
+    maxRetries: 3,
+    url: teamFocusUrl,
+    mapToResultObject: true,
+    treat400AsError: true
+  });
+}
+
+export function updateTeamFocus(teamId: string) {
+  return http({
+    method: 'PUT',
+    maxRetries: 3,
+    url: `${teamFocusUrl}/${teamId}`,
+    headers: getCsrfHeader()
+  });
+}
+
+export function deleteTeamFocus() {
+  return http({
+    method: 'DELETE',
+    maxRetries: 3,
+    url: teamFocusUrl,
+    headers: getCsrfHeader()
+  });
+}
