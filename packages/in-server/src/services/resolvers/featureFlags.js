04e97418a4f42ca16997b46c994993f4253e28b5
Aishwarya Guda <Aishwarya.Guda@ibm.com>
2025-05-07 15:47:30 -0700

    Disable incident summarization feature flag (#20799)

--- packages/in-server/src/services/resolvers/featureFlags.js
+++ packages/in-server/src/services/resolvers/featureFlags.js
@@ -579,7 +579,7 @@ module.exports = exports = [
   {
     uiClientKey: 'incidentSummarizationEnabled',
     instanaCtlKey: 'feature.incident.ai.summarization.enabled',
-    defaultValue: true
+    defaultValue: false
   },
   {
     uiClientKey: 'eventsAIChatEnabled',
