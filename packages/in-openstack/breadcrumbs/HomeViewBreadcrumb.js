1738782b1169e07bbaea568005041732cb7862b5
Aleix Costa <acostare@ibm.com>
2025-05-16 11:22:02 +0100

    INSTA-30480: remove deprecated getView (#20565)

--- packages/in-openstack/breadcrumbs/HomeViewBreadcrumb.js
+++ packages/in-openstack/breadcrumbs/HomeViewBreadcrumb.js
@@ -6,10 +6,14 @@
 import React from 'react';
 
 import { regionListFullyQualified } from 'in-openstack/navigation/paths';
+import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import Breadcrumb from 'in-components/breadcrumb/Breadcrumb';
-import { getView } from 'in-stores/navigation';
 import { t } from 'in-i18n';
 
 export default function HomeViewBreadcrumb() {
-  return <Breadcrumb href$={getView(regionListFullyQualified)}>{t('in-openstack:breadcrumbs.regions')}</Breadcrumb>;
+  const { createHrefToPath } = useNavigation();
+
+  return (
+    <Breadcrumb href={createHrefToPath(regionListFullyQualified)}>{t('in-openstack:breadcrumbs.regions')}</Breadcrumb>
+  );
 }
