24100d6b02d5237ae788fd52db3b97590cd84770
Josh King <jdking@us.ibm.com>
2025-05-20 17:07:22 -0500

    feat(aichat): steam text

--- packages/in-events/components/AIChat/NLGResponse.js
+++ packages/in-events/components/AIChat/NLGResponse.js
@@ -4,7 +4,7 @@
  * Copyright IBM Corp. 2025
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { t } from 'in-i18n';
 
@@ -12,17 +12,49 @@ import locals from './NLGResponse.mless';
 
 const NLGResponse = ({ messageItem }) => {
   const text = messageItem?.user_defined?.text || '';
-  const [show, setShow] = useState(false);
+  // const [show, setShow] = useState(false);
+  const [streamedText, setStreamedText] = useState('');
+  const [index, setIndex] = useState(0)
+  // const [isPaused, setIsPaused] = useState(false)
+  // let index = 0
+  // let isPaused = false;
+  // let stream  = false;
 
   // If show is true, show ALL text, otherwise just first 200 + ellipsis
-  const textToDisplay = (show && text) || `${text.substring(0, 250)}...`;
+  // const textToDisplay = (show && text) || `${text.substring(0, 250)}...`;
+
+  useEffect(() => {
+      console.log('useEffect', streamedText, text)
+      if(streamedText != text) {
+        console.log('inside')
+        streamText();
+      }
+    }, [streamedText]);
+
+  function streamText() {
+    if (index < text.length) {
+      setStreamedText(`${streamedText+text.charAt(index)}`)
+      setIndex(index + 1)
+      // Introduce pauses using specific characters or conditions
+      if (text.charAt(index - 1) === '.') {
+        // setIsPaused(true);
+        setTimeout(() => {
+          // setIsPaused(false);
+          streamText();
+        }, 500); // Pause for 500ms after a period or newline
+      } else {
+        setTimeout(streamText, 1000); // Continue typing
+      }
+    }
+  }
 
   return (
     <div>
-      {textToDisplay}
+      {streamedText}
+      {/* {textToDisplay}
       <div onClick={() => setShow(!show)} className={locals.showHidButton}>
-        {(show && t('in-events:aichat.showLess')) || t('in-events:aichat.showMore')}
-      </div>
+        {!stream && ((show && t('in-events:aichat.showLess')) || t('in-events:aichat.showMore'))}
+      </div> */}
     </div>
   );
 };
