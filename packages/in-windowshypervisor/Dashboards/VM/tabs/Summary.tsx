b8ab4d6274aabe6431754ab5ff63e877e7738a83
Aditya-Dahiya <aditya.dahiya@ibm.com>
2025-06-01 12:54:41 +0530

    Windows Hypervisor R2 metrics

--- packages/in-windowshypervisor/Dashboards/VM/tabs/Summary.tsx
+++ packages/in-windowshypervisor/Dashboards/VM/tabs/Summary.tsx
@@ -11,6 +11,7 @@ import { TimeConfig } from '@instana/types';
 import KpiGridRow from 'in-components/KpiGridRow/KpiGridRow';
 import { SnapshotData } from 'in-stores/snapshot/snapshot';
 import KpiCard from 'in-components/KpiCard/KpiCard';
+import Capitalize from 'in-components/Capitalize';
 import { t } from 'in-i18n';
 
 export interface SummaryData {
@@ -22,7 +23,9 @@ export default function Summary({ data: vm }: SummaryData) {
   return (
     <>
       <KpiGridRow sizes={[3, 3, 3, 3]}>
-        <KpiCard title={t('in-windowshypervisor:dashboards.vm.macAddress')} value={vm.macAddress} />
+        <KpiCard title={t('in-windowshypervisor:dashboards.vm.macAddress')}>
+          <Capitalize>{vm.macAddress}</Capitalize>
+        </KpiCard>
         <KpiCard title={t('in-windowshypervisor:dashboards.vm.ramAssigned')} value={vm.memoryAssigned} />
         <KpiCard title={t('in-windowshypervisor:dashboards.vm.status')} value={vm.status} />
         <KpiCard title={t('in-windowshypervisor:dashboards.vm.state')} value={vm.state} />
