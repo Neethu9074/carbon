647be0fadd869042e3cd9498db9543467e26cbcc
KYLE BLAUER <kgblauer@us.ibm.com>
2025-06-03 12:07:02 -0500

    INSTA-38864 business metrics suggestion call and entity selection (#21154)
    suggestion call and entity selection

--- packages/in-bizops/components/businessMetricGroupingConfigurator.ts
+++ packages/in-bizops/components/businessMetricGroupingConfigurator.ts
@@ -4,39 +4,44 @@
  * Copyright IBM Corp. 2025
  */
 
+import { GetBusinessMetricsTagsSuggestionsQuery } from '@instana/types/typeDefinitions';
 import { TimeConfig } from '@instana/types';
 
 // @ts-expect-error needs TS migration
 import { createDynamicGroupingConfigurator } from 'in-components/GroupingConfigurator';
+import getBusinessMetricTagSuggestions from 'in-bizops/subscriptions/getBusinessMetricTagSuggestions';
 import getBusinessMetricsTagsCatalog from 'in-bizops/subscriptions/getBusinessMetricsTagsCatalog';
-import getBizOpsTagSuggestions from 'in-bizops/subscriptions/getBizOpsTagSuggestions';
 import { GetTagSuggestionsProps } from 'in-components/QueryBuilder';
-import { GetBizOpsTagSuggestionQuery } from 'in-bizops/utils/types';
 
 interface GetTagCatalogProps {
   metric: string;
   timeConfig: TimeConfig;
 }
 
-const { GroupingConfigurator } = createDynamicGroupingConfigurator<{ metric: string }>({ // additional tag catalog props
+const { GroupingConfigurator } = createDynamicGroupingConfigurator<{ metric: string }>({
   getTagCatalog: ({ metric, timeConfig }: GetTagCatalogProps) =>
     getBusinessMetricsTagsCatalog({
       metricName: metric,
       timeConfig: timeConfig
     }),
   getSuggestions: (params: GetTagSuggestionsProps) => {
-    const { tagFilterExpression, tagName, timeConfig, propose, key, value, entity } = params;
-    const query: GetBizOpsTagSuggestionQuery = {
-      entity: entity,
-      tagFilterExpression: tagFilterExpression,
-      tagName: tagName,
+    const { timeConfig, propose, key, value } = params;
+    const query: GetBusinessMetricsTagsSuggestionsQuery = {
+      // TODO:  Need to pass metric in from the grouping configurator,
+      // but the prop doesn't currently exist.
+      metric: 'metric',
       propose: propose,
       key: key,
       value: value,
-      timeConfig: timeConfig
+      timeConfig: timeConfig,
+      pagination: {
+        page: 1,
+        pageSize: 50
+      }
     };
-    return getBizOpsTagSuggestions(query);
-  }
+    return getBusinessMetricTagSuggestions(query);
+  },
+  disableEntitySelection: true
 });
 
 export default GroupingConfigurator;
