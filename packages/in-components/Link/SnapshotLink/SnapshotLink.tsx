b701255a3eccc9b18307535216c54775c8900369
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-06-06 16:29:51 +0100

    chore(TS migration): migrate packages/in-components/Link/SnapshotLink/SnapshotLink

--- packages/in-components/Link/SnapshotLink/SnapshotLink.tsx
+++ packages/in-components/Link/SnapshotLink/SnapshotLink.tsx
@@ -3,7 +3,7 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { Link } from '@instana/components';
 
@@ -11,7 +11,7 @@ import { useGetLinkToSnapshotInCurrentView } from 'in-stores/navigation/paths/da
 
 import locals from './SnapshotLink.mless';
 
-export default function SnapshotLink({ snapshotId, children }) {
+export default function SnapshotLink({ snapshotId, children }: { snapshotId: string; children: ReactNode }) {
   const href = useGetLinkToSnapshotInCurrentView(snapshotId);
 
   return (
