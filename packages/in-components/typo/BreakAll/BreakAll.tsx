4eaec9ea21ad680e9586f091fd0a6f69ceefb4bf
Anthony Cunningham <anthoncu@ie.ibm.com>
2025-06-06 17:31:04 +0100

    chore(TS migration): migrate packages/in-components/typo/BreakAll/BreakAll

--- packages/in-components/typo/BreakAll/BreakAll.tsx
+++ packages/in-components/typo/BreakAll/BreakAll.tsx
@@ -3,10 +3,10 @@
  * (c) Copyright Instana Inc.
  */
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import './BreakAll.less';
 
-export default function BreakAll({ children }) {
+export default function BreakAll({ children }: { children: ReactNode }) {
   return <span className="in-break-all">{children}</span>;
 }
