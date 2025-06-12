e32a9c488e3686f5381328b5e1c1dc764e602e89
Aleix Costa <acostare@ibm.com>
2025-06-12 12:53:45 +0100

    INSTA-30480: remove getView and use useNav (#20576)

--- packages/in-vsphere/breadcrumbs/HomeViewBreadcrumb.js
+++ packages/in-vsphere/breadcrumbs/HomeViewBreadcrumb.js
@@ -6,13 +6,15 @@
 import React from 'react';
 
 import { datacenterListFullyQualified } from 'in-vsphere/navigation/paths';
+import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import Breadcrumb from 'in-components/breadcrumb/Breadcrumb';
-import { getView } from 'in-stores/navigation';
 import { t } from 'in-i18n';
 
 export default function HomeViewBreadcrumb() {
+  const { createHrefToPath } = useNavigation();
+
   return (
-    <Breadcrumb href$={getView(datacenterListFullyQualified)}>
+    <Breadcrumb href={createHrefToPath(datacenterListFullyQualified)}>
       {t('in-vsphere:breadcrumbs.vSphereDatacenters')}
     </Breadcrumb>
   );
