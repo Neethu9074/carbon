5e12cc08373b631643bd28c98677866a5a241012
Shams Sohrab <Shams.Sohrab@ibm.com>
2025-06-16 22:46:40 +0200

    INSTA-26133 UI: Add pollRate support for curated dashboards timeseries graphs (#21377)
    * Increase the distance between data points based on poll rate
    
    * Update packages/in-infrastructure/components/InfrastructureMetricChartBehavior.js
    
    Co-authored-by: Jean Detoeuf <Jean.Detoeuf1@ibm.com>
    
    ---------
    
    Co-authored-by: Jean Detoeuf <Jean.Detoeuf1@ibm.com>

--- packages/in-infrastructure/components/InfrastructureMetricChartBehavior.js
+++ packages/in-infrastructure/components/InfrastructureMetricChartBehavior.js
@@ -156,10 +156,10 @@ class InfrastructureMetricChartBehavior extends React.Component {
     this.setupMetricSubscriptionsForAxis(this.y2, queues.y2);
 
     this.subscriptions.push(
-      this.queues$.throttle(1000).subscribe(({ queues, dataHolders }) => {
+      this.queues$.throttle(1000).subscribe(({ queues, dataHolders, pollRate }) => {
         const y1Metrics = this.getMetricsFromQueue(queues.y1, dataHolders.y1, this.y1);
         const y2Metrics = this.getMetricsFromQueue(queues.y2, dataHolders.y2, this.y2);
-        this.setState({ y1Metrics, y2Metrics });
+        this.setState({ y1Metrics, y2Metrics, pollRate });
       })
     );
   };
@@ -189,8 +189,10 @@ class InfrastructureMetricChartBehavior extends React.Component {
   onNewDataPoints = (dataPoints, axisIndex, queue) => {
     // data points are not guaranteed to be filled
     if (dataPoints) {
-      queue.addDataPoints(axisIndex, dataPoints);
-      this.queues$.emit({ queues: this.queues, dataHolders: this.dataHolders });
+      let data = dataPoints.data ?? dataPoints;
+      let pollRate = dataPoints.pollRate ?? 1;
+      queue.addDataPoints(axisIndex, data);
+      this.queues$.emit({ queues: this.queues, dataHolders: this.dataHolders, pollRate: pollRate });
     }
   };
 
@@ -247,7 +249,7 @@ class InfrastructureMetricChartBehavior extends React.Component {
       hasActionlane,
       distanceBetweenDatapointsInMillis
     } = this;
-    const { y1Metrics = [], y2Metrics = [] } = this.state;
+    const { y1Metrics = [], y2Metrics = [], pollRate } = this.state;
 
     y1.metrics = y1Metrics;
     if (y2) {
@@ -271,10 +273,20 @@ class InfrastructureMetricChartBehavior extends React.Component {
         originalTimeConfig={originalTimeConfig ?? this.props.timeConfig}
         additionalContextMenuButtons={additionalContextMenuButtons}
         wiggleRoom={10000}
-        distanceBetweenDatapointsInMillis={distanceBetweenDatapointsInMillis}
+        distanceBetweenDatapointsInMillis={getDistanceBetweenDataPointsInMillis(
+          distanceBetweenDatapointsInMillis,
+          pollRate
+        )}
       />
     );
   }
+}
+
+function getDistanceBetweenDataPointsInMillis(distanceBetweenDataPoints, pollRate) {
+  if (distanceBetweenDataPoints) {
+    return distanceBetweenDataPoints;
+  }
+  return pollRate ? pollRate * 1000 : undefined;
 }
 
 function mapAxis(axis) {
