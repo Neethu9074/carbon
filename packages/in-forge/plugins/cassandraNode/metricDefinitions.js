aad59e44e327eaea27ba58fa26ad7a867e1c5397
Aysha Humaira S <Aysha.S@ibm.com>
2025-06-18 12:32:01 +0530

    TombstoneCount metrics

--- packages/in-forge/plugins/cassandraNode/metricDefinitions.js
+++ packages/in-forge/plugins/cassandraNode/metricDefinitions.js
@@ -120,5 +120,16 @@ export default [
     category: [t('in-forge:plugins.cassandraNode.keyspaces')],
     min: 0,
     formatter: muSecondsToMillis
+  },
+  {
+    metric: getDynamicMetricMatch(
+      'tombstoneCount',
+      'tombstone',
+      t('in-forge:plugins.cassandraNode.labelkeyspaceTable')
+    ),
+    label: t('in-forge:plugins.cassandraNode.labeltombstoneCount'),
+    category: [t('in-forge:plugins.cassandraNode.labelkeyspaceTable')],
+    min: 0,
+    formatter: number
   }
 ];
