b692d62b25ae78d712c2e965ab0464f2d5e2a226
Anju S S <anju.ss@ibm.com>
2025-06-26 16:45:10 +0530

    fix alignment for all resolutions

--- packages/in-service-levels/components/SloDashboard/components/SloDashboardMetaInfo/SloDashboardMetaInfo.tsx
+++ packages/in-service-levels/components/SloDashboard/components/SloDashboardMetaInfo/SloDashboardMetaInfo.tsx
@@ -36,7 +36,7 @@ export default function SloDashboardMetaInfo({
   return (
     <div className={locals.metaInfo}>
       <Grid className={locals.gridContainer}>
-        <Column sm={4} md={6} lg={5}>
+        <Column sm={4} md={6} lg={9} xlg={7}>
           {entities && (
             <SloEntityInfo
               entities={entities}
@@ -48,7 +48,7 @@ export default function SloDashboardMetaInfo({
             />
           )}
         </Column>
-        <Column sm={4} md={8} lg={11}>
+        <Column sm={4} md={8} lg={7} xlg={9}>
           {showTag && (
             <TagSet
               overflowClassName={locals.tagSet}
