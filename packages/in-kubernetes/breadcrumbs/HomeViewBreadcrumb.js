8964264b1410fd97115cd7b1494533bbd8093381
Aleix Costa <acostare@ibm.com>
2025-06-30 10:27:33 +0100

    INSTA-30480: remove deprecated function (#20564)

--- packages/in-kubernetes/breadcrumbs/HomeViewBreadcrumb.js
+++ packages/in-kubernetes/breadcrumbs/HomeViewBreadcrumb.js
@@ -6,12 +6,15 @@
 import React from 'react';
 
 import { clusterListFullyQualified } from 'in-kubernetes/navigation/paths';
+import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 import Breadcrumb from 'in-components/breadcrumb/Breadcrumb';
-import { getView } from 'in-stores/navigation';
 import { t } from 'in-i18n';
 
 export default function HomeViewBreadcrumb() {
+  const { createHrefToPath } = useNavigation();
   return (
-    <Breadcrumb href$={getView(clusterListFullyQualified)}>{t('in-kubernetes:breadcrumbs.kubernetes')}</Breadcrumb>
+    <Breadcrumb href={createHrefToPath(clusterListFullyQualified)}>
+      {t('in-kubernetes:breadcrumbs.kubernetes')}
+    </Breadcrumb>
   );
 }
