5c5b7f83854e5a44e648b7bb3f565885a5a4a8d5
ANTHONY CUNNINGHAM <ANTHONCU@ie.ibm.com>
2025-07-01 12:33:07 +0100

    Restore IcicleChart colours to old carbonChart colours (#21501)

--- packages/in-applications/analyze/AnalyzeView2_0/components/TraceDetailView/TraceDetailView.tsx
+++ packages/in-applications/analyze/AnalyzeView2_0/components/TraceDetailView/TraceDetailView.tsx
@@ -38,9 +38,9 @@ import { productAreas } from 'in-services/tracking/productAreas';
 import ViewTrackingMeta from 'in-components/ViewTrackingMeta';
 import { hasError, isLoading } from 'in-services/util/result';
 import DashboardHeader from 'in-components/DashboardHeader';
+import {getColorChart} from 'in-applications/endpointTypes';
 import { pageNames } from 'in-services/tracking/pageNames';
 import { pendingResult } from 'in-services/fixedObjects';
-import { getColor } from 'in-applications/endpointTypes';
 import { chartColors } from 'in-themes/chartColors';
 import useTimeConfig from 'in-hooks/useTimeConfig';
 import { hours, seconds } from 'in-services/time';
@@ -220,7 +220,7 @@ function getColorByEndpoint({ service, endpoint, traceId }: { service: Service; 
 }
 
 function getColorByEndpointType({ endpoint }: { endpoint: Endpoint }) {
-  return getColor(endpoint ? endpoint.type : 'UNDEFINED');
+  return getColorChart(endpoint ? endpoint.type : 'UNDEFINED');
 }
 
 function Header(props: DashboardButtonProps & { result: Result<TraceSummary> }) {
--- packages/in-applications/analyze/components/TraceDetails/components/CallTree/stories/IcicleChart.stories.js
+++ packages/in-applications/analyze/components/TraceDetails/components/CallTree/stories/IcicleChart.stories.js
@@ -10,7 +10,7 @@ import { themes } from '@instana/design-tokens';
 import TraceExamples from 'in-applications/analyze/components/TraceDetails/components/CallTree/stories/TraceExamplesComponent';
 import ServerIcicleChart from 'in-applications/analyze/components/TraceDetails/components/IcicleChart/ServerIcicleChart';
 import IcicleChart from 'in-applications/analyze/components/TraceDetails/components/IcicleChart';
-import { getColor } from 'in-applications/endpointTypes';
+import {getColorChart} from 'in-applications/endpointTypes';
 import { always } from 'in-services/fixedStreams';
 
 export default {
@@ -19,7 +19,7 @@ export default {
 
 export function Default() {
   const getColorByEndpointType = ({ endpoint }) =>
-    !endpoint || !endpoint.type ? themes.default.ids.color.option.neutral['500'] : getColor(endpoint.type);
+    !endpoint || !endpoint.type ? themes.default.ids.color.option.neutral['500'] : getColorChart(endpoint.type);
 
   return (
     <TraceExamples
