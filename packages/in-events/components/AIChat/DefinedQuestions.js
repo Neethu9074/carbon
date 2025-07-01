38815c5887713474c9af6351ea587f0e88503e87
Josh King <jdking@us.ibm.com>
2025-07-01 16:33:54 -0500

    feat(promptlibrary): add events category (#21612)

--- packages/in-events/components/AIChat/DefinedQuestions.js
+++ packages/in-events/components/AIChat/DefinedQuestions.js
@@ -36,6 +36,18 @@ export const promptLibrary = [
       'What is the sum of aggregated CPU requests for kubernetes deployment <app-name> in namespace <namespace-name> for last <duration> hours?',
       'What is the count of pods for deployments labeled as environment=<environment-name> in the namespace <namespace-name>?'
     ]
+  },
+  {
+    kind: 'Events',
+    questions: [
+      'What are todays open incidents for app <app-name>?',
+      'List all the incidents in the last hour grouped by app <app-name>.',
+      'How many incidents have occurred on service <service-name> in the last week?',
+      'How many application events generated in the last 45 minutes had a "high error rate" problem.',
+      'Show me kubernetes pod changes grouped by label <label>.',
+      'How many JVM incidents are still open?',
+      'Show me closed incidents from the last <number> days?'
+    ]
   }
 ];
 
