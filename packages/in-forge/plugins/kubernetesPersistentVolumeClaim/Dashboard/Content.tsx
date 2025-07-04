f975f4bbd2d61d0a758d2cf35343b8ee50043b30
Tácio Belmonte <Tacio.Belmonte@ibm.com>
2025-07-04 15:59:41 +0200

    INSTA-39331: redirect pvc in Infrastructure area to new k8s dashboard (#21652)
    * INSTA-39331: redirect pvc in Infrastructure area to new k8s dashboard
    
    * update copyright
    
    * add redirection behind feature flag
    
    * add timeConfig to redirection url

--- packages/in-forge/plugins/kubernetesPersistentVolumeClaim/Dashboard/Content.tsx
+++ packages/in-forge/plugins/kubernetesPersistentVolumeClaim/Dashboard/Content.tsx
@@ -1,15 +1,15 @@
 /*
  * IBM Confidential
  * PID 5737-N85, 5900-AG5
- * Copyright IBM Corp. 2023
+ * Copyright IBM Corp. 2025
  */
 
 import React from 'react';
 
 import DefaultDashboard from 'in-infrastructure/Dashboard/components/DefaultDashboard';
+import { usePersistentVolumeClaimDashboard } from 'in-kubernetes/navigation/paths';
 // @ts-expect-error
 import RedirectWithHash from 'in-components/RedirectWithHash';
-import { usePersistentVolumeDashboard } from 'in-kubernetes/navigation/paths';
 import { persistentVolumeSupportEnabled } from 'in-services/featureFlags';
 import { SnapshotItem as BaseSnapshotItem, TimeConfig } from 'in-types';
 
@@ -23,8 +23,8 @@ interface Props {
   timeConfig: TimeConfig;
 }
 
-export default function KubernetesPersistentVolumeDashboard({ snapshot, timeConfig }: Props) {
-  const url: string = usePersistentVolumeDashboard(snapshot.get('id'), { timeConfig });
+export default function KubernetesPersistentVolumeClaimDashboard({ snapshot, timeConfig }: Props) {
+  const url: string = usePersistentVolumeClaimDashboard(snapshot.get('id'), { timeConfig });
   const [base, queryString] = url.split('?');
   const params = new URLSearchParams(queryString);
   params.delete('snapshotId');
