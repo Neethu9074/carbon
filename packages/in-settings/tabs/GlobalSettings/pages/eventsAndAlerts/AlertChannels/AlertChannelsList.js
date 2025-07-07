37ea9bbc94a2e9c1a9b1544b3ceec9b553dddd71
Angel Neethu EJ <Angel.Neethu.Ej@ibm.com>
2025-07-07 14:29:57 +0530

    alert channel selection is crashing on search in both old and new UI (#21657)
    * crash fixed
    
    * added comment

--- packages/in-settings/tabs/GlobalSettings/pages/eventsAndAlerts/AlertChannels/AlertChannelsList.js
+++ packages/in-settings/tabs/GlobalSettings/pages/eventsAndAlerts/AlertChannels/AlertChannelsList.js
@@ -49,11 +49,11 @@ export default function AlertChannelsList({
   getHeader = defaultGetHeader(inSelectListDialog, tableActions)
 }) {
   const BOUNDED_PATH = '/channels';
-  const { location } = useNavigation();
+  const { location, createHrefToPath } = useNavigation();
   const channelListColumnDefinitions =
     alertChannelPerSeverityEnabled && detailView
-      ? [...columnDefinitions(hasRowNavigation), ...columnDefinitionsAlertLevel(alertChannels)]
-      : columnDefinitions(hasRowNavigation);
+      ? [...columnDefinitions(hasRowNavigation, createHrefToPath), ...columnDefinitionsAlertLevel(alertChannels)]
+      : columnDefinitions(hasRowNavigation, createHrefToPath);
   const [{ query }] = useUrlState({
     bind: [
       {
@@ -107,15 +107,15 @@ export default function AlertChannelsList({
   );
 }
 
-export function columnDefinitions(hasRowNavigation) {
+// removed 'createHrefToPath', which was initialised within the GetContent, because UI was crashing with an error while searching: : Rendered more hooks than during the previous render.
+export function columnDefinitions(hasRowNavigation, createHrefToPath) {
   const columns = [
     {
       id: 'name',
       label: t('in-settings:tabs.name'),
       width: 35,
       ellipsis: true,
-      getContent: function GetContent(entity) {
-        const { createHrefToPath } = useNavigation();
+      getContent: entity => {
         return (
           <WithSubscript subscript={getKind(entity)}>
             {hasRowNavigation ? (
