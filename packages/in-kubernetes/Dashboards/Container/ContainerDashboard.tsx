4110ff54a21802c58acd05532ce4d05d08f95236
Minghong Xu <Minghong.Xu@ibm.com>
2025-07-23 10:38:10 -0400

    refactor the code
    Signed-off-by: Minghong Xu <Minghong.Xu@ibm.com>

--- packages/in-kubernetes/Dashboards/Container/ContainerDashboard.tsx
+++ packages/in-kubernetes/Dashboards/Container/ContainerDashboard.tsx
@@ -32,7 +32,7 @@ import { getContainerIconByPlugin } from 'in-kubernetes/utils';
 import { clusterIdUrlParameter, namespaceIdUrlParameter, nodeIdUrlParameter } from 'in-kubernetes/navigation/urlParameters';
 import Capitalize from 'in-components/Capitalize';
 import { t } from 'in-i18n';
-import { TimeConfig, EntityHealthInfo } from 'in-types';
+import { TimeConfig, EntityHealthInfo } from '@instana/types';
 
 const pathSegment = '/containers';
 const matrixPrefix = 'container.';
@@ -243,21 +243,9 @@ function getTableData({
     timeConfig
   };
 
-  // Add optional filters (backend will be modified to support these without podId)
   if (clusterId) filter.clusterId = clusterId;
   if (namespaceId) filter.namespaceId = namespaceId;
   if (nodeId) filter.nodeId = nodeId;
-
-  // NOTE: podId is intentionally NOT included for standalone containers page
-  //
-  // BACKEND REQUIREMENT:
-  // The backend event 'getNativeKubernetesContainers' needs to be modified to:
-  // 1. Make podId parameter optional
-  // 2. When podId is provided -> return containers for that pod (existing behavior)
-  // 3. When podId is NOT provided -> return containers filtered by clusterId/namespaceId/nodeId
-  // 4. Support pagination, sorting, and filtering across multiple pods
-  //
-  // This will enable the standalone containers page to work properly.
 
   return getOtelKubernetesContainers({
     pagination: {
