ef050fa05be7bfcd8a771613b628724f8fe9865e
Aleix Costa <acostare@ibm.com>
2025-07-25 12:47:20 +0100

    fix: lost change from previous migration (#21801)

--- packages/in-hooks/useUrlState.ts
+++ packages/in-hooks/useUrlState.ts
@@ -9,7 +9,6 @@ import { isEqual } from 'lodash';
 // This will be addressed via https://instana.kanbanize.com/ctrl_board/103/cards/102691/details/
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
 // eslint-disable-next-line import/no-deprecated
-import { getModifiedUrl } from 'in-stores/navigation';
 import { getMatrixParameter, setOrDeleteMatrixKey } from 'in-stores/navigation/matrix';
 import { addReset, removeReset } from 'in-stores/navigation/urlParameterResets';
 import { Location, ParameterDefinition } from 'in-stores/navigation/types';
@@ -46,7 +45,7 @@ export default function useUrlState<State extends StateWithoutGuarantees>({
   replaceHistory = true
 }: Options<State>): UrlStateReturn<State> {
   const location = useLocation();
-  const { navigate } = useNavigation();
+  const { createHref, navigate} = useNavigation();
   const [state, setState] = useState<StateWithoutGuarantees>(
     () => determineStateChange(bind, location, emptyObject) || emptyObject
   );
@@ -140,14 +139,12 @@ export default function useUrlState<State extends StateWithoutGuarantees>({
 
   const exposedGetStateChangeUrl = useCallback(
     (change: Partial<State>) => {
-      // This will be addressed via https://instana.kanbanize.com/ctrl_board/103/cards/102691/details/
-      // eslint-disable-next-line import/no-deprecated
-      return getModifiedUrl(location, location => {
-        const newState = reducer(state as State, change);
-        modifyLocation(bind, newState, location);
-      });
+      const newState = reducer(state as State, change);
+      modifyLocation(bind, newState, location);
+
+      return createHref(location);
     },
-    [state, reducer, bind, location]
+    [reducer, state, bind, location, createHref]
   );
 
   return [state as State, exposedSetState, exposedGetStateChangeUrl];
