887602682955416fcc159aa5c0dd6dbc5fedeff6
Rithika JR <Rithika.JR@ibm.com>
2025-07-28 16:54:27 +0530

    Side Panel was not shown in Restricted Usage view (#21972)
    * Fix side panel issue in restricted usage
    
    * Remove onPrem flag check

--- packages/in-amp/pages/AccountAndBilling/tabs/Usage/UsageView.tsx
+++ packages/in-amp/pages/AccountAndBilling/tabs/Usage/UsageView.tsx
@@ -9,15 +9,19 @@ import React from 'react';
 // @ts-expect-error needs TS migration
 import SideNavigationAndContent from 'in-components/layout/SideNavigationAndContent/SideNavigationAndContent.js';
 // @ts-expect-error needs TS migration
+import RestrictedTechnologiesReporting from 'in-amp/components/RestrictedTechnologiesReporting';
+// @ts-expect-error needs TS migration
 import WithAccountInformation from 'in-amp/components/WithAccountInformation';
+//@ts-expect-error - needs TS migration
+import RestrictedUsage from 'in-amp/components/RestrictedUsage';
 import ConsumptionOverview from 'in-amp/pages/AccountAndBilling/tabs/Usage/ConsumptionOverview';
 // @ts-expect-error needs TS migration
 import UsageCharts from 'in-amp/components/UsageCharts';
 import { addOns, ampUsage, consumptionOverview, dataUsage } from 'in-amp/navigation/paths';
 // @ts-expect-error needs TS migration
 import Usage from 'in-amp/components/Usage';
-import { onPremLicenseInformationEnabled } from 'in-services/featureFlags';
 import AddOns from 'in-amp/pages/AccountAndBilling/tabs/Usage/AddOns';
+import { ampCompanyInfoEnabled } from 'in-services/featureFlags';
 import { t } from 'in-i18n';
 
 type AccountInfoProps = {
@@ -37,10 +41,10 @@ function View(props: any) {
     {
       path: dataUsage,
       label: t('in-amp:accountAndBilling.tabs.dataUsage'),
-      component: Usage
+      component: ampCompanyInfoEnabled ? Usage : RestrictedUsage
     }
   ];
-  if (props.hasPaidLicenses && !onPremLicenseInformationEnabled) {
+  if (props.hasPaidLicenses && ampCompanyInfoEnabled) {
     pages.push(
       {
         path: consumptionOverview,
