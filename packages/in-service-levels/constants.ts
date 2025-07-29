c5ec480964209ca3b209a16fbaed5e1cba8cc160
Anju S S <anju.ss@ibm.com>
2025-07-29 16:07:24 +0530

    add utc to constants

--- packages/in-service-levels/constants.ts
+++ packages/in-service-levels/constants.ts
@@ -66,4 +66,4 @@ export const SloTimeWindowTypes = Object.freeze({
 
 export const sloStatuses = ['red', 'green'] as const;
 
-export const utcLabel = 'UTC' as const;
+export const utcLabel = 'UTC';
