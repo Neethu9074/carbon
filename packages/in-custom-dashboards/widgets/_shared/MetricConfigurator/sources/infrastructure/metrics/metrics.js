8e19f3f90bbdba275ceb90a230a1e9e2879f8efc
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2025-08-08 13:19:44 +0200

    Revert "INSTA-48689 test for infrastructure permission - TS019962581" (#22154)
    Revert "INSTA-48689 test for infrastructure permission - TS019962581 (#22142)"
    
    This reverts commit 6d9c44ea06854eeee85d0b6568cb0ac7ca3b13bd.

--- packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/metrics.js
+++ packages/in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/metrics.js
@@ -7,7 +7,6 @@ export { configureChart } from 'in-custom-dashboards/widgets/_shared/MetricConfi
 
 import FormComponent from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/FormComponent';
 import { infraMetricsWidgetEnabled } from 'in-services/featureFlags';
-import { hasInfrastructureAccess } from 'in-stores/permission';
 import { t } from 'in-i18n';
 
 export { createForm } from 'in-custom-dashboards/widgets/_shared/MetricConfigurator/sources/infrastructure/metrics/form';
@@ -19,4 +18,4 @@ export const label =
   ' ' +
   (infraMetricsWidgetEnabled ? '' : t('in-custom-dashboards:widgets.srcInfrastructure.metrics.comingSoon'));
 export const disabled = !infraMetricsWidgetEnabled;
-export const visible = hasInfrastructureAccess;
+export const visible = true;
