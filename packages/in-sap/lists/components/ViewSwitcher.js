ef6476eef0aa981e91f7200b184cc065df3f023f
Aleix Costa <acostare@ibm.com>
2025-08-13 12:25:51 +0100

    feat: remove isView - INSTA-49057 (#22159)
    * feat: remove isView
    
    * chore: changes copyright header
    
    * refactor: simplify creating new href url with createHrefToPath helper
    
    ---------
    
    Co-authored-by: Robert Hostlowsky <Robert.Hostlowsky@ibm.com>

--- packages/in-sap/lists/components/ViewSwitcher.js
+++ packages/in-sap/lists/components/ViewSwitcher.js
@@ -6,60 +6,51 @@
 
 import React from 'react';
 
+import { SecondLevelNavigation, SecondLevelNavigationItem } from '@instana/components';
+
 import {
   sapSystemListFullyQualified,
   sapInstanceListFullyQualified,
   sapDbInstanceListFullyQualified
 } from 'in-sap/navigation/paths';
-import { SecondLevelNavigation, SecondLevelNavigationItem } from '@instana/components';
 import DashboardHeaderShadowModule from 'in-components/DashboardHeader/DashboardHeaderShadowModule';
 import DashboardHeaderModule, { themes } from 'in-components/DashboardHeader/DashboardHeaderModule';
-// TODO: fix the depreciation to use useNavigate instead
-import { isView } from 'in-stores/navigation/navigation';
-import DashboardHeader from 'in-components/DashboardHeader';
-import connectTo from 'in-hoc/connectTo';
-import { t } from 'in-i18n';
 import { useNavigation } from 'in-stores/navigation/hooks/useNavigation';
+import DashboardHeader from 'in-components/DashboardHeader';
+import { t } from 'in-i18n';
 
+export default function SapViewSwitcher() {
+  const { location, createHrefToPath, matchLocation } = useNavigation();
+  const isSapAbapSystemViewActive = matchLocation(sapSystemListFullyQualified);
+  const isSapAbapInstanceViewActive = matchLocation(sapInstanceListFullyQualified);
+  const isSapDbInstanceViewActive = matchLocation(sapDbInstanceListFullyQualified);
 
-export default connectTo(
-  {
-    isSapAbapSystemViewActive: isView(sapSystemListFullyQualified),
-    isSapAbapInstanceViewActive: isView(sapInstanceListFullyQualified),
-    isSapDbInstanceViewActive: isView(sapDbInstanceListFullyQualified)
-  },
-  function SapViewSwitcher({ isSapAbapSystemViewActive, isSapAbapInstanceViewActive, isSapDbInstanceViewActive }) {
-    const {location, createHref} = useNavigation()
-    const targetLocationSystem = {...location, pathname: `${sapSystemListFullyQualified}`}
-    const targetLocationInstance = {...location, pathname: `${sapInstanceListFullyQualified}`}
-    const targetLocationDbInstance = {...location, pathname: `${sapDbInstanceListFullyQualified}`}
-    return (
-      <>
-        <DashboardHeader icon="lib_sap" label={t('in-sap:SAPHeader')} title={t('in-sap:SAPHeader')} />
-        <DashboardHeaderModule theme={themes.light}>
-          <SecondLevelNavigation>
-            <SecondLevelNavigationItem
-              href={createHref(targetLocationSystem)}
-              icon="lib_sap_host"
-              label={t('in-sap:abapSystems')}
-              isActive={isSapAbapSystemViewActive}
-            />
-            <SecondLevelNavigationItem
-              href={createHref(targetLocationInstance)}
-              icon="lib_sap_instances"
-              label={t('in-sap:abapOrJavaInstances')}
-              isActive={isSapAbapInstanceViewActive}
-            />
-            <SecondLevelNavigationItem
-              href={createHref(targetLocationDbInstance)}
-              icon="lib_sap_dbms"
-              label={t('in-sap:sapdbinstance')}
-              isActive={isSapDbInstanceViewActive}
-            />
-          </SecondLevelNavigation>
-        </DashboardHeaderModule>
-        <DashboardHeaderShadowModule />
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <DashboardHeader icon="lib_sap" label={t('in-sap:SAPHeader')} title={t('in-sap:SAPHeader')} />
+      <DashboardHeaderModule theme={themes.light}>
+        <SecondLevelNavigation>
+          <SecondLevelNavigationItem
+            href={createHrefToPath(sapSystemListFullyQualified)}
+            icon="lib_sap_host"
+            label={t('in-sap:abapSystems')}
+            isActive={isSapAbapSystemViewActive}
+          />
+          <SecondLevelNavigationItem
+            href={createHrefToPath(sapInstanceListFullyQualified)}
+            icon="lib_sap_instances"
+            label={t('in-sap:abapOrJavaInstances')}
+            isActive={isSapAbapInstanceViewActive}
+          />
+          <SecondLevelNavigationItem
+            href={createHrefToPath(sapDbInstanceListFullyQualified)}
+            icon="lib_sap_dbms"
+            label={t('in-sap:sapdbinstance')}
+            isActive={isSapDbInstanceViewActive}
+          />
+        </SecondLevelNavigation>
+      </DashboardHeaderModule>
+      <DashboardHeaderShadowModule />
+    </>
+  );
+}
