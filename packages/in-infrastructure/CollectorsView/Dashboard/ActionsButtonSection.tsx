9dbdbadfee0362a10bd2fdbc41324a3560e9661f
JINSONG WANG <jswang@ibm.com>
2025-08-22 15:20:48 -0700

    Replace track func with trackCta func

--- packages/in-infrastructure/CollectorsView/Dashboard/ActionsButtonSection.tsx
+++ packages/in-infrastructure/CollectorsView/Dashboard/ActionsButtonSection.tsx
@@ -9,26 +9,27 @@ import React from 'react';
 import { IconButton } from '@instana/components';
 
 import {
-  track,
   OTEL_COLLECTOR_RESTART_CLICKED,
   OTEL_COLLECTOR_EDIT_CONFIGURATION_CLICKED
 } from 'in-services/tracking/tracking';
 import EditConfigurationDialog from 'in-infrastructure/CollectorsView/Dashboard/EditConfigurationDialog';
 import { SnapshotItem } from 'in-infrastructure/CollectorsView/Dashboard/CollectorDashboard';
 import { restartOtelCollector } from 'in-forge/plugins/instanaAgent/selfMonitoring';
+import { useSegmentTracking } from 'in-services/tracking/useSegmentTracking';
 import { addActiveDialog } from 'in-components/DialogPresenter/store';
 import { t } from 'in-i18n';
 
 import locals from './ActionsButtonSection.mless';
 
 export default function ActionsButtonSection({ snapshot }: { snapshot: SnapshotItem }) {
+  const { trackCta } = useSegmentTracking();
   return (
     <div className={locals.actionButtonsContainer}>
       <IconButton
         type="lib_actions_refresh"
         size="compact"
         onClick={() => {
-          track(OTEL_COLLECTOR_RESTART_CLICKED);
+          trackCta(OTEL_COLLECTOR_RESTART_CLICKED);
           restartOtelCollector(snapshot);
         }}
         isWrapperedByTooltip
@@ -38,7 +39,7 @@ export default function ActionsButtonSection({ snapshot }: { snapshot: SnapshotI
         type="lib_actions_settings_edit"
         size="compact"
         onClick={() => {
-          track(OTEL_COLLECTOR_EDIT_CONFIGURATION_CLICKED);
+          trackCta(OTEL_COLLECTOR_EDIT_CONFIGURATION_CLICKED);
           addActiveDialog(<EditConfigurationDialog />);
         }}
         isWrapperedByTooltip
