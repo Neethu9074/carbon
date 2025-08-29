7da91dc70297d4710cb265986c2a2e1e9c845a93
Robert Hostlowsky <Robert.Hostlowsky@ibm.com>
2025-08-29 15:33:55 +0200

    chore(deps): update ui-foundation (#22477)
    * chore(deps): update ui-foundation
    
    * fix unit test
    
    ---------
    
    Co-authored-by: DIANA Costantino <dcostant@us.ibm.com>

--- packages/in-plg/components/AssistMe/AssistMe_test.tsx
+++ packages/in-plg/components/AssistMe/AssistMe_test.tsx
@@ -5,6 +5,7 @@
  */
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import AssistMe from 'in-plg/components/AssistMe/AssistMe';
@@ -17,9 +18,19 @@ describe('AssistMe Component', () => {
     expect(assistMeButton).toHaveAttribute('id', 'wm-getanswers');
   });
 
-  test('has tooltip with correct content', () => {
+  test('has tooltip with correct content', async () => {
+    const event = userEvent.setup();
     render(<AssistMe />);
-    const tooltipContent = screen.getByText('Guided answers here');
+
+    const assistMeButton = screen.getByRole('button');
+    expect(assistMeButton).toBeInTheDocument();
+    expect(assistMeButton).toHaveAttribute('id', 'wm-getanswers');
+
+    // Hover over the button to show the tooltip
+    await event.hover(assistMeButton);
+
+    // Use findByText instead of getByText to handle any delay in tooltip appearing
+    const tooltipContent = await screen.findByText('Guided answers here');
     expect(tooltipContent).toBeInTheDocument();
   });
 });
