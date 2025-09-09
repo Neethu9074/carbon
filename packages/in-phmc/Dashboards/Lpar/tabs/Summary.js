714b6245936d026469d4ecb492020ed5a2a5de06
Neha Ghongade <Neha.Ghongade@ibm.com>
2025-09-09 18:59:45 +0530

    incorporated changes

--- packages/in-phmc/Dashboards/Lpar/tabs/Summary.js
+++ packages/in-phmc/Dashboards/Lpar/tabs/Summary.js
@@ -20,7 +20,7 @@ export default function Summary({ timeConfig, data: lpar }) {
   const snapshotId = lpar.id;
   return (
     <Fragment>
-      <KpiGridRow sizes={[1.8, 1.8, 1.8, 1.8, 1.8, 1.8, 1.8]}>
+      <KpiGridRow sizes={[2, 2, 2, 2, 2, 2, 2]}>
         <KpiCard title={t('in-phmc:partitionId')} value={lpar.partitionId} raw borderless />
         <KpiCard title={t('in-phmc:dashboards.name')} value={lpar.name} raw borderless />
         <KpiCard title={t('in-phmc:state')} value={lpar.state} raw borderless />
